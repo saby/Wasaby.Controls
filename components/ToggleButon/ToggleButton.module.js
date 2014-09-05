1ff50fe73e2d697dcbfd38937f48c0411865537e
iv.cheremushkin <ccherya@gmail.com>
2014-09-05 16:18:09 +0400

    Button, ToggleButton, CheckBox. NumberTextBox fix

--- components/ToggleButon/ToggleButton.module.js
+++ components/ToggleButon/ToggleButton.module.js
@@ -17,13 +17,20 @@ define('js!SBIS3.CONTROLS.ToggleButton', ['js!SBIS3.CONTROLS.ToggleButtonBase', 
    var ToggleButton = ToggleButtonBase.extend( /** @lends SBIS3.CONTROLS.ToggleButton.prototype */ {
       _dotTplFn: dotTplFn,
       $protected: {
+         _button: null,
          _options: {
 
          }
       },
 
       $constructor: function() {
+         var self = this;
+         this._button = this._container;
+      },
 
+      setCaption: function(captionTxt){
+         ToggleButton.superclass.setCaption.call(this, captionTxt);
+         this._button.html(captionTxt || '');
       }
 
    });
