97d0c31e86b101b55cbfff4412d47f34de158e47
iv.cheremushkin <ccherya@gmail.com>
2014-09-10 13:00:49 +0400

    сделал проще

--- components/SwitcherDoubleOnline/SwitcherDoubleOnline.module.js
+++ components/SwitcherDoubleOnline/SwitcherDoubleOnline.module.js
@@ -39,19 +39,11 @@ define('js!SBIS3.Engine.SwitcherDoubleOnline', ['js!SBIS3.CONTROLS.SwitcherBase'
          var self = this;
          SwitcherDoubleOnline.superclass.setState.call(this,state);
          if (state == 'on'){
-            if (self._options.disposition == 'vertical'){
-               self._switcher.addClass('controls-SwitcherDoubleOnline-top-toggled').removeClass('controls-SwitcherDoubleOnline-bottom-toggled');
-            } else {
-               self._switcher.addClass('controls-SwitcherDoubleOnline-right-toggled').removeClass('controls-SwitcherDoubleOnline-left-toggled');
-            }
+            self._switcher.addClass('controls-SwitcherDoubleOnline-on-toggled').removeClass('controls-SwitcherDoubleOnline-off-toggled');
             self._textOffContainer.addClass('controls-SwitcherDoubleOnline__unselected');
             self._textOnContainer.removeClass('controls-SwitcherDoubleOnline__unselected');
          } else {
-            if (self._options.disposition == 'vertical'){
-               self._switcher.addClass('controls-SwitcherDoubleOnline-bottom-toggled').removeClass('controls-SwitcherDoubleOnline-top-toggled');
-            } else {
-            self._switcher.addClass('controls-SwitcherDoubleOnline-left-toggled').removeClass('controls-SwitcherDoubleOnline-right-toggled');
-            }
+            self._switcher.addClass('controls-SwitcherDoubleOnline-off-toggled').removeClass('controls-SwitcherDoubleOnline-on-toggled');
             self._textOnContainer.addClass('controls-SwitcherDoubleOnline__unselected');
             self._textOffContainer.removeClass('controls-SwitcherDoubleOnline__unselected');
          }
