af99ce0db1ce231b4c28d43a73dc09eda41c9130
Мануйлов А.С <as.manuylov@tensor.ru>
2014-09-18 15:35:06 +0400

    new Themes

---
+++ buildThemes.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var path = require('path');
+
+var themesDir = path.join(__dirname, 'themes');
+
+// порядок сборки стилей контролов
+var order = ['Button', 'ToggleButton'];
+
+var dirWalker = function (dir) {
+   var files = fs.readdirSync(dir);
+   var countThemes = files.length;
+   for (var i = 0; i < countThemes; i++) {
+      var folderPath = path.join(themesDir, files[i]);
+      if (fs.statSync(folderPath).isDirectory()) {
+         // папка стилей темы
+         var cssPath = path.join(folderPath, 'css');
+         fs.writeFile(path.join(folderPath,'result', files[i] + '.css'), '');
+         for (var j = 0; j < order.length; j++) {
+            var controlCss = path.join(cssPath, order[j]) + '.css';
+            var data = fs.readFileSync(controlCss);
+            fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), '/* ' + order[j] + ' */' + '\n\n');
+            fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), data + '\n\n', function (err) {
+               if (err) console.log(err);
+               console.log('It\'s saved!');
+            });
+         }
+      }
+   }
+};
+
+dirWalker(themesDir);
