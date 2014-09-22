0b2a9260ef4eceadd4f8edd574163bddc2c6d2b9
Мануйлов А.С <as.manuylov@tensor.ru>
2014-09-22 16:41:52 +0400

    controls-Button__active

--- components/ToggleButon/ToggleButton.module.js
+++ components/ToggleButon/ToggleButton.module.js
@@ -26,6 +26,12 @@ define('js!SBIS3.CONTROLS.ToggleButton', ['js!SBIS3.CONTROLS.ToggleButtonBase', 
       $constructor: function() {
          var self = this;
          this._button = this._container;
+         this._container.mouseup(function () {
+            self._container.removeClass('controls-Button__active');
+         }).mousedown(function () {
+               self._container.addClass('controls-Button__active');
+            });
+
       },
 
       setCaption: function(captionTxt){
