135dbc20f3f8f36fc1b4d4b67f428bedb5b21e83
Мануйлов А.С <as.manuylov@tensor.ru>
2014-09-23 17:28:24 +0400

    тема demo. вынес стили TabButtons и исправил билд

--- buildThemes.js
+++ buildThemes.js
@@ -4,7 +4,10 @@ var path = require('path');
 var themesDir = path.join(__dirname, 'themes');
 
 // порядок сборки стилей контролов
-var order = ['Button', 'ToggleButton', 'RadioButton', 'SwitcherDoubleOnline', 'Switcher', 'TextBox', 'PasswordTextBox', 'NumberTextBox'];
+var order = {
+   demo: ['Button', 'ToggleButton', 'RadioButton', 'SwitcherDoubleOnline', 'Switcher', 'TextBox', 'PasswordTextBox', 'NumberTextBox', 'TabButton'],
+   online: ['Button', 'ToggleButton', 'RadioButton', 'SwitcherDoubleOnline', 'Switcher', 'TextBox', 'PasswordTextBox', 'NumberTextBox']
+};
 
 var dirWalker = function (dir) {
    var files = fs.readdirSync(dir);
@@ -15,10 +18,10 @@ var dirWalker = function (dir) {
          // папка стилей темы
          var cssPath = path.join(folderPath, 'css');
          fs.writeFile(path.join(folderPath, 'result', files[i] + '.css'), '');
-         for (var j = 0; j < order.length; j++) {
-            var controlCss = path.join(cssPath, order[j]) + '.css';
+         for (var j = 0; j < order[files[i]].length; j++) {
+            var controlCss = path.join(cssPath, order[files[i]][j]) + '.css';
             var data = fs.readFileSync(controlCss);
-            fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), '/* ' + order[j] + ' */' + '\n\n');
+            fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), '/* ' + order[files[i]][j] + ' */' + '\n\n');
             fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), data + '\n\n', function (err) {
                if (err) console.log(err);
                console.log('It\'s saved!');
