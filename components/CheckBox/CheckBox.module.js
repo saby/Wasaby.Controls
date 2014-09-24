559724d7c2926d8c07aff90df0413f3536db2ad6
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2014-09-24 10:18:25 +0400

    перенесла описание метода setCaption

--- components/CheckBox/CheckBox.module.js
+++ components/CheckBox/CheckBox.module.js
@@ -28,7 +28,19 @@ define('js!SBIS3.CONTROLS.CheckBox', ['js!SBIS3.CONTROLS.ToggleButtonBase', 'htm
          var self = this;
          this._checkBoxCaption = $('.js-controls-CheckBox__caption', self._container);
       },
-
+     /**
+      * Установить текст подписи флага.
+      * @param {String} captionTxt Текст подписи флага.
+      * @example
+      * Из массива names установить подписи каждому флагу.
+      * <pre>
+      *    //names - массив с названиями товаров
+      *    //flags - массив объектов-флагов
+      *    $ws.helpers.forEach(names, function(name, index) {
+      *       flags[index].setCaption(name);
+      *    });
+      * </pre>
+      */
       setCaption: function(captionTxt){
          CheckBox.superclass.setCaption.call(this,captionTxt);
          this._checkBoxCaption.html(captionTxt || '');
