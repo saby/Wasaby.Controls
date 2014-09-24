0c0284047801513c97d7cc44116770018ef0136d
Мануйлов А.С <as.manuylov@tensor.ru>
2014-09-24 11:48:03 +0400

    новый билд стилей Presto

--- buildThemes.js
+++ buildThemes.js
@@ -6,6 +6,7 @@ var themesDir = path.join(__dirname, 'themes');
 // порядок сборки стилей контролов
 var order = {
    demo: ['Button', 'ToggleButton', 'RadioButton', 'SwitcherDoubleOnline', 'Switcher', 'TextBox', 'PasswordTextBox', 'NumberTextBox', 'TabButton', 'CheckBox', 'ColorPicker', 'ComboBox'],
+   presto: ['Button', 'TextBox'],
    online: ['Button', 'ToggleButton', 'RadioButton', 'SwitcherDoubleOnline', 'Switcher', 'TextBox', 'PasswordTextBox', 'NumberTextBox', 'CheckBox']
 };
 
@@ -16,13 +17,12 @@ var dirWalker = function (dir) {
       var folderPath = path.join(themesDir, files[i]);
       if (fs.statSync(folderPath).isDirectory()) {
          // папка стилей темы
-         var cssPath = path.join(folderPath, 'css');
-         fs.writeFile(path.join(folderPath, 'result', files[i] + '.css'), '');
+         fs.writeFile(path.join(folderPath, files[i] + '.css'), '');
          for (var j = 0; j < order[files[i]].length; j++) {
-            var controlCss = path.join(cssPath, order[files[i]][j]) + '.css';
+            var controlCss = path.join(folderPath, order[files[i]][j]) + '.css';
             var data = fs.readFileSync(controlCss);
-            fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), '/* ' + order[files[i]][j] + ' */' + '\n\n');
-            fs.appendFile(path.join(folderPath, 'result', files[i] + '.css'), data + '\n\n', function (err) {
+            fs.appendFile(path.join(folderPath, files[i] + '.css'), '/* ' + order[files[i]][j] + ' */' + '\n\n');
+            fs.appendFile(path.join(folderPath, files[i] + '.css'), data + '\n\n', function (err) {
                if (err) console.log(err);
                console.log('It\'s saved!');
             });
