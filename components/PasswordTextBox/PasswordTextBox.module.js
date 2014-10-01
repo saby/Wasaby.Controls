b41de9fd768bceed4573d4501857386b010ff326
Мануйлов А.С <as.manuylov@tensor.ru>
2014-10-01 12:19:11 +0400

    Вернул шаблон для ввода пароля, чтобы убрать из опций тип поля ввода. В остальных местах остался type="text"

--- components/PasswordTextBox/PasswordTextBox.module.js
+++ components/PasswordTextBox/PasswordTextBox.module.js
@@ -2,7 +2,7 @@
  * Created by iv.cheremushkin on 28.08.2014.
  */
 
-define('js!SBIS3.CONTROLS.PasswordTextBox', ['js!SBIS3.CONTROLS.TextBox'], function (TextBox) {
+define('js!SBIS3.CONTROLS.PasswordTextBox', ['js!SBIS3.CONTROLS.TextBox', 'html!SBIS3.CONTROLS.PasswordTextBox'], function (TextBox, dotTplFn) {
 
    'use strict';
    /**
@@ -14,13 +14,8 @@ define('js!SBIS3.CONTROLS.PasswordTextBox', ['js!SBIS3.CONTROLS.TextBox'], funct
     */
    var PasswordTextBox;
    PasswordTextBox = TextBox.extend(/** @lends SBIS3.CONTROLS.PasswordTextBox.prototype */ {
-      $protected: {
-         _options: {
-            inputType: 'password'
-         }
-      },
+      _dotTplFn: dotTplFn,
       $constructor: function() {
-         this.getContainer().addClass('controls-PasswordTextBox');
          //TODO: избавиться от фикса высоты поля ввода пароля в IE>8
          if ($ws._const.browser.isIE && ($ws._const.browser.isIE8 || $ws._const.browser.isIE9 || $ws._const.browser.isIE10 || $ws._const.browser.isModernIE)) {
             this.getContainer().find('.controls-TextBox__field').addClass('controls-TextBox__field__fixIE');
