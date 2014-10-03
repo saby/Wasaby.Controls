d1f0fa78afdb89ce2d30ce4ca2731f90d8c73630
iv.cheremushkin <ccherya@gmail.com>
2014-10-03 15:17:31 +0400

    Добавил в popupMixin опции выравнивая

--- components/_PopupMixin/_PopupMixin.module.js
+++ components/_PopupMixin/_PopupMixin.module.js
@@ -28,25 +28,59 @@ define('js!SBIS3.CONTROLS._PopupMixin', [], function() {
       $protected: {
          _options: {
             /**
-             * @cfg {Number} Отступ слева
+             * @cfg {Object} Отступы
              */
-            left: null,
+            offset: {
+               top: null,
+               left: null
+            },
 
             /**
-             * @cfg {Number} Отступ сверху
+             * @typedef {Object} CornerEnum
+             * @variant tl верхний левый
+             * @variant tr верхний правый
+             * @variant br нижний правый
+             * @variant bl нижний левый
              */
-            top: null,
+            /**
+             * @cfg {CornerEnum} По какому углу таргета выравнивание
+             */
+            corner: null,
+
+            verticalAlign: {
+               /**
+                * @typedef {Object} VerticalAlignEnum
+                * @variant top
+                * @variant bottom
+                */
+               /**
+                * @cfg {VerticalAlignEnum} Вертикальное выравнивание контрола
+                */
+               side:null,
+               /**
+                * @cfg {Number} отступ
+                */
+               offset: 0
+            },
 
             /**
-             * @cfg {Number} Отступ справа
+             * @cfg {Object} От какого угла идет отсчет координат
              */
-            right: null,
-
-            /**
-             * @cfg {Number} Отступ снизу
-             */
-            bottom: null,
-
+            horizontalAlign: {
+               /**
+                * @typedef {Object} HorizontalAlignEnum
+                * @variant right
+                * @variant left
+                */
+               /**
+                * @cfg {HorizontalAlignEnum}
+                */
+               side:null,
+               /**
+                * @cfg {Number} отступ
+                */
+               offset: 0
+            },
             /**
              * @cfg {String|jQuery|HTMLElement} элемент, относительно которого позиционируется всплывающее окно
              */
@@ -59,10 +93,8 @@ define('js!SBIS3.CONTROLS._PopupMixin', [], function() {
          this.hide();
          container.css('position', 'absolute');
          container.appendTo('body');
-
          var zIndex = zIndexManager.getNext();
          container.css('zIndex', zIndex);
-
          this.recalcPosition();
       },
 
@@ -90,20 +122,86 @@ define('js!SBIS3.CONTROLS._PopupMixin', [], function() {
             'top' : 0
          };
          if (this._options.target) {
-            offset = this._options.target.offset();
-            offset.top += this._options.target.outerHeight();
-            //Проверям убираемся ли в экран
-            if ($(document).height() <= this._container.outerHeight() + offset.top){
-               offset.top -= ( this._options.target.height() + this._container.outerHeight());
-            }
-            if ($(document).width() <= this._container.outerWidth() + offset.left){
-               offset.left += this._options.target.outerWidth() - this._container.outerWidth();
-            }
+            offset = this._getGeneralOffset();
          }
          this._container.css({
-            'left' : offset.left + this._options.left,
-            'top' : offset.top + this._options.top
+            'left' : offset.left + this._options.offset.left,
+            'top' : offset.top + this._options.offset.top
          });
+      },
+
+      _getGeneralOffset: function(){
+         var offset = {
+            'left' : 0,
+            'top' : 0
+            },
+            buf;
+
+         buf = this._getOffsetByCorner(this._options.corner);
+         offset.left = buf.left;
+         offset.top = buf.top;
+         buf = this._getAlignOffset(this._options.verticalAlign.side,this._options.horizontalAlign.side);
+         offset.left += buf.left;
+         offset.top += buf.top;
+         offset = this._displaySizeCorrection(offset);
+         //console.log(offset);
+         return offset;
+      },
+
+      // Вычисляем сдвиг в зависимости от выравнивания
+      _getAlignOffset: function(vert, horiz){
+         var offset = {
+            'top': 0,
+            'left': 0
+         };
+
+         if (vert == 'bottom'){
+            offset.top -= this._container.height();
+         }
+         if (horiz == 'right'){
+            offset.left -= this._container.width();
+         }
+
+         offset.left += this._options.horizontalAlign.offset;
+         offset.top += this._options.verticalAlign.offset;
+         return offset;
+      },
+
+      // Вычисляем сдвиг в зависимости от угла
+      _getOffsetByCorner: function(corner){
+         var offset = this._options.target.offset(),
+             height = this._options.target.outerHeight(),
+             width = this._options.target.width();
+         switch (corner){
+            case 'tr': offset.left += width;
+               return offset;
+            case 'bl': offset.top += height;
+               return offset;
+            case 'br': offset.top += height;
+               offset.left+= width;
+               return offset;
+            default : return offset; //tl
+         }
+      },
+
+      //Проверям убираемся ли в экран
+      _displaySizeCorrection: function(offset){
+         var buf = {
+            'top': 0,
+            'left': 0
+            };
+
+         if ($(document).height() <= this._container.outerHeight() + offset.top){
+            //offset.top -= ( this._options.target.height() + this._container.outerHeight());
+            buf = this._getAlignOffset('bottom',this._options.horizontalAlign.side);
+            offset.top += ( buf.top - this._options.target.height());
+         }
+         if ($(document).width() <= this._container.outerWidth() + offset.left){
+            //offset.left += this._options.target.outerWidth() - this._container.outerWidth();
+            buf = this._getAlignOffset(this._options.verticalAlign.side,'right');
+            offset.left += ( buf.left + this._options.target.width());
+         }
+         return offset;
       },
 
       before : {
