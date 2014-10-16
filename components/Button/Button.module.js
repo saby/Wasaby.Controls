52f0ebd9fa609bf30b84392666f56e3228397996
kraynovdo <do.kraynov@tensor.ru>
2014-10-16 15:26:53 +0400

    Кнопка по началу с caption

--- components/Button/Button.module.js
+++ components/Button/Button.module.js
@@ -12,6 +12,10 @@ define('js!SBIS3.CONTROLS.Button', ['js!SBIS3.CONTROLS.ButtonBase', 'html!SBIS3.
     * @class SBIS3.CONTROLS.Button
     * @extends SBIS3.CONTROLS.ButtonBase
     * @control
+    * @initial
+    * <component data-component='SBIS3.CONTROLS.Button' style='width: 100px'>
+    *    <option name='caption' value='Кнопка'></option>
+    * </component>
     * @public
     * @category Buttons
     */
