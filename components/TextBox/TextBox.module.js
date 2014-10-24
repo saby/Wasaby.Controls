20c60bdfa302f8e175772a49e394572cd6d3bf42
iv.cheremushkin <ccherya@gmail.com>
2014-10-24 14:03:55 +0400

    setText fix перенес _propertyDataBinding в DataBindMixin

--- components/TextBox/TextBox.module.js
+++ components/TextBox/TextBox.module.js
@@ -114,7 +114,10 @@ define('js!SBIS3.CONTROLS.TextBox', ['js!SBIS3.CONTROLS.TextBoxBase','html!SBIS3
       },
 
       _keyUpBind: function() {
-         this.setText(this._inputField.val());
+         var newText = this._inputField.val();
+         if (newText != this._options.text) {
+            TextBox.superclass.setText.call(this, newText);
+         }
       },
 
       _keyDownBind: function() {
@@ -128,6 +131,16 @@ define('js!SBIS3.CONTROLS.TextBox', ['js!SBIS3.CONTROLS.TextBoxBase','html!SBIS3
             text = String.trim(text);
          }
         return text;
+      },
+
+      _setEnabled : function(enabled) {
+         TextBox.superclass._setEnabled.call(this, enabled);
+         if (enabled == false) {
+            this._inputField.attr('readonly', 'readonly')
+         }
+         else {
+            this._inputField.removeAttr('readonly');
+         }
       }
    });
 
