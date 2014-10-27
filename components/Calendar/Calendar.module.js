8015b2b02eb6330ed9e843f01ecc7e950aadf013
Frolenko.IO <io.frolenko@tensor.ru>
2014-10-27 15:45:06 +0300

    123

--- components/Calendar/Calendar.module.js
+++ components/Calendar/Calendar.module.js
@@ -60,7 +60,6 @@ define(
 
          /**
           * Обновить таблицу календарных дней в соответствии с текущим значением дочернего MonthPicker'a
-          * @param date
           * @private
           */
          _refreshCalendarTable: function(){
