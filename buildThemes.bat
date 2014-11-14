3985779e2464af988ac4f7fe44ae42526b13a04d
kraynovdo <do.kraynov@tensor.ru>
2014-11-14 16:55:24 +0300

    добавлена либа для генерации css

--- buildThemes.bat
+++ buildThemes.bat
@@ -5,7 +5,7 @@ set themespath=.\themes\
 for /r "%themespath%" /d %%i in (*) do (
    if exist "%%i\%%~ni.less" (
       echo %%i\%%~ni.less
-      call .\sbis3-ws\build\lib\duncansmart-less\lessc.cmd "%%i\%%~ni.less" "%%i\%%~ni.css"
+      call .\build\duncansmart-less\lessc.cmd "%%i\%%~ni.less" "%%i\%%~ni.css"
    )
 )
 
