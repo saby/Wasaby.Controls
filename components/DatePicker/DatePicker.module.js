af08c37ca4f2ae6f35a3b3c780bd58206a8f1965
Frolenko.IO <io.frolenko@tensor.ru>
2014-11-17 16:48:56 +0300

    1. DatePicker.module.js :: Возвращены @variant в описание опции mask 2. MonthPicker.module.js :: конструкции создания странных дат типа new DatePicker.module.js(2015, 0, 1, 23, 59, 59, 999) убраны в методы _getLastDay и _getFirstDay с подробным описанием

--- components/DatePicker/DatePicker.module.js
+++ components/DatePicker/DatePicker.module.js
@@ -87,6 +87,32 @@ define(
             /**
              * @cfg {String} Формат отображения даты, на базе которой будет создана html-разметка и в соответствии с которой
              * будет определён весь функционал. Должна представлять собой одну из масок в массиве допустимых маск.
+             * <wiTag group="Отображение" page=1>
+             * @variant 'DD.MM.YYYY',
+             * @variant 'DD.MM.YY',
+             * @variant 'DD.MM',
+             * @variant 'YYYY-MM-DD',
+             * @variant 'YY-MM-DD',
+             * @variant 'HH:II:SS.UUU',
+             * @variant 'HH:II:SS',
+             * @variant 'HH:II',
+             * @variant 'DD.MM.YYYY HH:II:SS.UUU',
+             * @variant 'DD.MM.YYYY HH:II:SS',
+             * @variant 'DD.MM.YYYY HH:II',
+             * @variant 'DD.MM.YY HH:II:SS.UUU',
+             * @variant 'DD.MM.YY HH:II:SS',
+             * @variant 'DD.MM.YY HH:II',
+             * @variant 'DD.MM HH:II:SS.UUU',
+             * @variant 'DD.MM HH:II:SS',
+             * @variant 'DD.MM HH:II',
+             * @variant 'YYYY-MM-DD HH:II:SS.UUU',
+             * @variant 'YYYY-MM-DD HH:II:SS',
+             * @variant 'YYYY-MM-DD HH:II',
+             * @variant 'YY-MM-DD HH:II:SS.UUU',
+             * @variant 'YY-MM-DD HH:II:SS',
+             * @variant 'YY-MM-DD HH:II',
+             * @variant 'YYYY',
+             * @variant 'MM/YYYY'
              */
             mask: 'DD.MM.YY',
             /**
