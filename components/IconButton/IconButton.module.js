12be4a190f0f17b4606de39c88ae59dfc1f4a7b3
kraynovdo <do.kraynov@tensor.ru>
2014-11-27 15:40:04 +0300

    Компонент кнопка в виде иконки

---
+++ components/IconButton/IconButton.module.js
@@ -0,0 +1,41 @@
+/**
+ * Модуль 'Компонент кнопка'.
+ *
+ * @description
+ */
+define('js!SBIS3.CONTROLS.IconButton', ['js!SBIS3.CORE.Control', 'js!SBIS3.CONTROLS._ClickMixin', 'html!SBIS3.CONTROLS.IconButton'], function(Control, _ClickMixin, dotTplFn) {
+
+   'use strict';
+
+   /**
+    * Контрол, отображающий обычную кнопку
+    * @class SBIS3.CONTROLS.IconButton
+    * @extends SBIS3.CONTROLS.ButtonBase
+    * @control
+    * @initial
+    * <component data-component='SBIS3.CONTROLS.IconButton' style='width: 100px'>
+    *    <option name='caption' value='Кнопка'></option>
+    * </component>
+    * @public
+    * @category Buttons
+    */
+
+   var IconButton = Control.Control.extend([_ClickMixin], /** @lends SBIS3.CONTROLS.IconButton.prototype */ {
+      _dotTplFn : dotTplFn,
+      $protected: {
+         _options: {
+            /**
+             * @cfg {String}  Путь до иконки
+             * Путь задаётся относительно корня сайта либо через sprite.
+             * @see setIcon
+             * @see getIcon
+             * @editor ImageEditor
+             */
+            icon: ''
+         }
+      }      
+   });
+
+   return IconButton;
+
+});
