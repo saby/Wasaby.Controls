b4d7fe498b4da77bbe70355e9e7c31031266f6d3
iv.cheremushkin <ccherya@gmail.com>
2014-12-03 16:51:01 +0300

    модальный PopupMixin

--- components/_PopupMixin/_PopupMixin.module.js
+++ components/_PopupMixin/_PopupMixin.module.js
@@ -2,7 +2,7 @@
  * Created by iv.cheremushkin on 12.08.2014.
  */
 
-define('js!SBIS3.CONTROLS._PopupMixin', ['js!SBIS3.CONTROLS.ControlHierarchyManager'], function (ControlHierarchyManager) {
+define('js!SBIS3.CONTROLS._PopupMixin', ['js!SBIS3.CONTROLS.ControlHierarchyManager', 'js!SBIS3.CORE.ModalOverlay'], function (ControlHierarchyManager, ModalOverlay) {
 
    if (typeof window !== 'undefined') {
       var eventsChannel = $ws.single.EventBus.channel('WindowChangeChannel');
@@ -84,7 +84,11 @@ define('js!SBIS3.CONTROLS._PopupMixin', ['js!SBIS3.CONTROLS.ControlHierarchyMana
             /**
              * @cfg {Boolean} закрывать или нет при клике мимо
              */
-            closeByExternalClick: false
+            closeByExternalClick: false,
+            /**
+             * @cfg {Boolean} Является модальным или нет
+             */
+            isModal: false
          }
       },
 
@@ -543,13 +547,23 @@ define('js!SBIS3.CONTROLS._PopupMixin', ['js!SBIS3.CONTROLS.ControlHierarchyMana
          },
          init: function(){
             ControlHierarchyManager.addNode(this, this.getParent());
+         },
+         hide: function(){
+            var zIndex = this._container.css('zIndex');
+            ControlHierarchyManager.zIndexManager.setFree(zIndex);
+            // Убираем оверлей
+            if (this._options.isModal) {
+               var pos = Array.indexOf($ws.single.WindowManager._modalIndexes, zIndex);
+               $ws.single.WindowManager._modalIndexes.splice(pos, 1);
+               pos = Array.indexOf($ws.single.WindowManager._visibleIndexes, zIndex);
+               $ws.single.WindowManager._visibleIndexes.splice(pos, 1);
+               ModalOverlay.adjust();
+            }
          }
       },
 
       before: {
          destroy: function () {
-            var zIndex = this._container.css('zIndex');
-            ControlHierarchyManager.zIndexManager.setFree(zIndex);
             ControlHierarchyManager.removeNode(this);
             $ws.single.EventBus.channel('WindowChangeChannel').unsubscribe('onWindowResize', this._resizeHandler, this);
             $ws.single.EventBus.channel('WindowChangeChannel').unsubscribe('onWindowScroll', this._scrollHandler, this);
@@ -560,6 +574,14 @@ define('js!SBIS3.CONTROLS._PopupMixin', ['js!SBIS3.CONTROLS.ControlHierarchyMana
                left: '-1000px',
                top: '-1000px'
             });
+            var zIndex = ControlHierarchyManager.zIndexManager.getNext();
+            this._container.css('zIndex', zIndex);
+            //Показываем оверлей
+            if (this._options.isModal) {
+               $ws.single.WindowManager._modalIndexes.push(zIndex);
+               $ws.single.WindowManager._visibleIndexes.push(zIndex);
+               ModalOverlay.adjust();
+            }
          }
       },
 
