51c8012f15e128d9185107a160b970081e870ec6
iv.cheremushkin <ccherya@gmail.com>
2015-01-23 15:24:21 +0300

    MenuButtonMixin

--- components/MenuLink/MenuLink.module.js
+++ components/MenuLink/MenuLink.module.js
@@ -1,4 +1,4 @@
-define('js!SBIS3.CONTROLS.MenuLink', ['js!SBIS3.CONTROLS.ButtonBase', 'js!SBIS3.CONTROLS.CollectionMixin', 'js!SBIS3.CONTROLS.PickerMixin', 'js!SBIS3.CONTROLS.ContextMenu', 'html!SBIS3.CONTROLS.MenuLink'], function(ButtonBase, CollectionMixin, PickerMixin, ContextMenu, dotTplFn) {
+define('js!SBIS3.CONTROLS.MenuLink', ['js!SBIS3.CONTROLS.Link', 'js!SBIS3.CONTROLS.CollectionMixin', 'js!SBIS3.CONTROLS.PickerMixin', 'js!SBIS3.CONTROLS.MenuButtonMixin', 'js!SBIS3.CONTROLS.ContextMenu'], function(Link, CollectionMixin, PickerMixin, MenuButtonMixin, ContextMenu) {
 
    'use strict';
 
@@ -11,8 +11,7 @@ define('js!SBIS3.CONTROLS.MenuLink', ['js!SBIS3.CONTROLS.ButtonBase', 'js!SBIS3.
     * @mixes SBIS3.CONTROLS.PickerMixin
     */
 
-   var MenuLink = ButtonBase.extend( [PickerMixin, CollectionMixin], /** @lends SBIS3.Engine.Link.prototype */ {
-      _dotTplFn: dotTplFn,
+   var MenuLink = Link.extend( [PickerMixin, CollectionMixin, MenuButtonMixin], /** @lends SBIS3.Engine.Link.prototype */ {
       $protected: {
          _zIndex: '',
          _options: {
@@ -27,9 +26,9 @@ define('js!SBIS3.CONTROLS.MenuLink', ['js!SBIS3.CONTROLS.ButtonBase', 'js!SBIS3.
                self.togglePicker();
             });
          } else {
-            if (this.getItems().getNextItem()['handler']) {
+            if (this.getItems().getNextItem().handler) {
                this.subscribe('onActivated', function () {
-                  this.getItems().getNextItem()['handler']();
+                  this.getItems().getNextItem().handler();
                });
             }
          }
@@ -39,48 +38,8 @@ define('js!SBIS3.CONTROLS.MenuLink', ['js!SBIS3.CONTROLS.ButtonBase', 'js!SBIS3.
          return new ContextMenu(this._setPickerConfig());
       },
 
-      _setPickerConfig: function(){
-         return {
-            parent: this.getParent(),
-            context: this.getParent() ? this.getParent().getLinkedContext() : {},
-            element: $('<div></div>'),
-            target : this.getContainer(),
-            items: this._options.items,
-            corner : 'bl',
-            hierField: 'par',
-            verticalAlign: {
-               side: 'top'
-            },
-            horizontalAlign: {
-               side: 'left'
-            },
-            closeByExternalClick: true,
-            targetPart: true
-         };
-      },
-
-      showPicker: function(){
-         MenuLink.superclass.showPicker.call(this);
-         this._container.css('z-index', this._picker._container.css('z-index') + 1);
-      },
-
-      hidePicker: function(){
-         MenuLink.superclass.hidePicker.call(this);
-         this._container.css('z-index', this._zIndex);
-      },
-
-      _setWidth: function(){
-         var self = this;
-         this._picker.getContainer().css({
-            'min-width': self._container.outerWidth() - this._border + 15
-         });
-      },
-
       _setPickerContent: function(){
          var self = this;
-         this._picker.subscribe('onClose', function(){
-            self._closeHandler();
-         });
          this._picker._container.addClass('controls-MenuLink__Menu');
          var header= $('<div class="controls-MenuLink__header"></div>');
          header.append(this._container.clone());
@@ -88,12 +47,7 @@ define('js!SBIS3.CONTROLS.MenuLink', ['js!SBIS3.CONTROLS.ButtonBase', 'js!SBIS3.
          $('.controls-MenuLink__header', this._picker._container).bind('click', function(){
             self.hidePicker();
          });
-      },
-
-      _closeHandler: function(){
-         this._container.removeClass('controls-Checked__checked');
       }
-
    });
 
    return MenuLink;
