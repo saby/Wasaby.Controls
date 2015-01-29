e852d63bb2c0cd95d104eb21364d3cb51b746991
iv.cheremushkin <ccherya@gmail.com>
2015-01-29 14:16:17 +0300

    fix Link

--- components/Link/Link.module.js
+++ components/Link/Link.module.js
@@ -1,11 +1,11 @@
-define('js!SBIS3.CORE.Link', ['js!SBIS3.CONTROLS.ButtonBase', 'html!SBIS3.CORE.Link' ], function(ButtonBase, dotTplFn) {
+define('js!SBIS3.CONTROLS.Link', ['js!SBIS3.CONTROLS.ButtonBase', 'html!SBIS3.CONTROLS.Link' ], function(ButtonBase, dotTplFn) {
 
    'use strict';
 
    /**
     * Контрол, отображающий кнопку в виде ссылки. Используется только в онлайне.
     * Сторонние пользователи скорее предпочтут использовать просто <a></a>
-    * @class SBIS3.Engine.Link
+    * @class SBIS3.CONTROLS.Link
     * @extends SBIS3.CONTROLS.ButtonBase
     * @control
     */
@@ -20,6 +20,30 @@ define('js!SBIS3.CORE.Link', ['js!SBIS3.CONTROLS.ButtonBase', 'html!SBIS3.CORE.L
       },
 
       $constructor: function() {
+      },
+
+      setCaption: function(caption){
+         Link.superclass.setCaption.call(this, caption);
+         if (this._options.icon) {
+            $('.controls-Link__field', this._container).html(caption);
+         } else {
+            this._container.html(caption);
+         }
+      },
+
+      setIcon: function(icon){
+         Link.superclass.setIcon.call(this, icon);
+         var content;
+         if (icon) {
+            content = $('<i class="controls-Link__icon ' + this._iconClass + '" ></i><span class="controls-Link__field">' + this._options.caption + '</span>');
+         } else {
+            content = this._options.caption;
+         }
+         this._container.html(content);
+      },
+
+      setHref: function(href){
+         this._options.href = href;
       }
 
    });
