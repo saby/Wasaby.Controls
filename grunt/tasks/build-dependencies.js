d121eaaba813566d2a097cfda99943bbd8596ed9
Dmitriy Pushkov <ezze@ezze.org>
2015-03-03 15:01:40 +0300

    Build dependencies is implemented in Grunt task.

--- grunt/tasks/build-dependencies.js
+++ grunt/tasks/build-dependencies.js
@@ -1,6 +1,9 @@
-/* global module: false */
+/* global global: false, module: false */
 module.exports = function(grunt) {
    'use strict';
+
+   var fs = require('fs'),
+       path = require('path');
 
    grunt.registerTask('build-dependencies', function() {
       if ([
@@ -12,17 +15,37 @@ module.exports = function(grunt) {
          // All files are built, no need to rebuild them
          return;
       }
+      
+      var
+         newPath,
+         jsModules = {},
+         gruntFilePath = path.resolve(),
+         resourcesBaseDir = path.join(gruntFilePath, 'components'),
+         dirWalker = function(dir) {
+            newPath = '';
+            var files = fs.readdirSync(dir);
+            for (var i = 0; i < files.length; i++){
+               newPath = path.join(dir, files[i]);
+               if (fs.statSync(newPath).isDirectory()){
+                  dirWalker(newPath);
+               }
+               else {
+                  if (/\.module\.js$/.test(files[i])){
+                     require(newPath);
+                  }
+               }
+            }
+         };
 
-      // TODO: implement dependencyCollector.js as Grunt task here
-      var done = this.async();
-      grunt.util.spawn({
-         cmd: 'node',
-         args: ['depencyCollector.js']
-      }, function(error) {
-         if (error) {
-            console.log(error);
-         }
-         done();
-      });
+      var restoreDefine = global.define;
+      global.define = function(name){
+         jsModules[name.replace(/js!/,'')] = '/' + path.relative(resourcesBaseDir, newPath).replace(/\\/g,'/');
+      };
+      dirWalker(resourcesBaseDir);
+      global.define = restoreDefine;
+
+      var jsModulesJsonString = JSON.stringify({jsModules: jsModules}, null, 3);
+      fs.writeFileSync(path.join(gruntFilePath, 'components/contents.json'), jsModulesJsonString);
+      fs.writeFileSync(path.join(gruntFilePath, 'components/contents.js'), 'contents = ' + jsModulesJsonString + ';');
    });
 };
