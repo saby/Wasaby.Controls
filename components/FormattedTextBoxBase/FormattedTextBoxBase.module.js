808b462808be6c3c87bc9eaa29b8db16276a8157
iv.cheremushkin <ccherya@gmail.com>
2015-03-05 16:10:23 +0300

    готовый NumberTextBox

--- components/FormattedTextBoxBase/FormattedTextBoxBase.module.js
+++ components/FormattedTextBoxBase/FormattedTextBoxBase.module.js
@@ -140,7 +140,7 @@ define(
             this.setText(this._options.text);
          }
 
-         this._inputField.focus(function () {
+         this._container.bind('focusin', function () {
             self._focusHandler();
          });
          this._inputField.keypress(function (event) {
@@ -182,15 +182,11 @@ define(
        * @private
        */
       _focusHandler: function(){
-         // Если в поле еще не введено ни единого символа -- установить курсор в начало поля
-         if ( $(this._inputField.get(0)).text() == this._clearMask ){
-            var
-               child = this._isSeparatorContainerFirst ? 1 : 0,
-               startContainer = this._inputField.get(0).childNodes[child].childNodes[0],
-               startPosition = 0;
-
-            this._moveCursor(startContainer, startPosition);
-         }
+         var
+            child = this._isSeparatorContainerFirst ? 1 : 0,
+            startContainer = this._inputField.get(0).childNodes[child].childNodes[0],
+            startPosition = 0;
+         this._moveCursor(startContainer, startPosition);
       },
 
       /**
