03a859c7d6faba07e30acc59c30bebed7095775a
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-03-25 15:23:35 +0300

    добавила блок игнорируемых опций

--- components/SwitcherBase/SwitcherBase.module.js
+++ components/SwitcherBase/SwitcherBase.module.js
@@ -14,6 +14,9 @@ define('js!SBIS3.CONTROLS.SwitcherBase', ['js!SBIS3.CORE.Control', 'js!SBIS3.CON
     * Данный класс поведенческий.
     * @class SBIS3.CONTROLS.SwitcherBase
     * @extends $ws.proto.Control
+    * @ignoreOptions validators independentContext contextRestriction extendedTooltip element linkedContext handlers
+    * @ignoreOptions autoHeight autoWidth context horizontalAlignment isContainerInsideParent modal owner stateKey
+    * @ignoreOptions subcontrol verticalAlignment parent
     */
 
    var SwitcherBase = Control.Control.extend( [Clickable, FormWidgetMixin], /** @lends SBIS3.CONTROLS.SwitcherBase.prototype */ {
