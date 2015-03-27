eeb476e9f832773c516aa34022ba228fdd5892e6
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-03-27 17:05:42 +0300

    поправила описание

--- components/NumberTextBox/NumberTextBox.module.js
+++ components/NumberTextBox/NumberTextBox.module.js
@@ -15,6 +15,7 @@ define('js!SBIS3.CONTROLS.NumberTextBox', ['js!SBIS3.CONTROLS.TextBox', 'html!SB
     *    <li>{@link onlyPositive запрещение ввода отрицательных чисел};</li>
     *    <li>{@link onlyInteger запрещение ввода дробных чисел};</li>
     *    <li>{@link enableArrows отображать ли стрелки для увеличения/уменьшения числа};</li>
+    *    <li>{@link text начальное значение}.</li>
     * </ol>
     * @class SBIS3.CONTROLS.NumberTextBox
     * @extends SBIS3.CONTROLS.TextBox
