76a3258925075d97360bec73d3972c54aa929dc7
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2015-04-09 17:16:31 +0300

    FilterView start

--- components/FilterButton/FilterButton.module.js
+++ components/FilterButton/FilterButton.module.js
@@ -2,27 +2,45 @@
  * Created by as.suhoruchkin on 07.04.2015.
  */
 define('js!SBIS3.CONTROLS.FilterButton', [
-   'js!SBIS3.CORE.CompoundControl'
-], function(CompoundControl) {
+   'js!SBIS3.CORE.CompoundControl',
+   'html!SBIS3.CONTROLS.FilterButton',
+   'js!SBIS3.CONTROLS.PickerMixin'
+], function(CompoundControl, dotTplFn, PickerMixin) {
 
-   var OperationDelete = CompoundControl.extend({
+   var FilterButton = CompoundControl.extend([PickerMixin],{
+      _dotTplFn: dotTplFn,
 
       $protected: {
          _options: {
-            icon: 'sprite:icon-24 action-hover icon-Erase icon-error'
-         }
+         },
+         _blocks: undefined
+
       },
 
       $constructor: function() {
+         this._blocks = {
+            closedButton: this._container.find('.controls__filter-button__closed'),
+            openedButton: this._container.find('.controls__filter-button__opened'),
+            wrapper: this._container.find('.controls__filter-button__wrapper')
+         };
+         this._blocks.closedButton.bind('click', this.showPicker.bind(this));
+         this._blocks.openedButton.bind('click', this.hidePicker.bind(this));
       },
-      _clickHandler: function() {
-         var view = this.getParent().getLinkedView(),
-            selectedItems = view.getSelectedItems(),
-            records = selectedItems.length ? selectedItems : view._dataSet._indexId;
-         view.deleteRecords(records);
+      _setPickerContent: function() {
+         this._picker.getContainer().append(this._blocks.wrapper);
+         this._blocks.wrapper.removeClass('ws-hidden');
+      },
+      _setPickerConfig: function () {
+         return {
+            corner: 'tr',
+            target: this,
+            horizontalAlign: {
+               side: 'right'
+            }
+         };
       }
    });
 
-   return OperationDelete;
+   return FilterButton;
 
 });
---
+++ components/FilterView/FilterView.module.js
@@ -0,0 +1,62 @@
+/**
+ * Created by as.suhoruchkin on 07.04.2015.
+ */
+define('js!SBIS3.CONTROLS.FilterView', [
+   'js!SBIS3.CONTROLS.ListViewDS',
+   'js!SBIS3.CONTROLS.PickerMixin',
+   'html!SBIS3.CONTROLS.FilterView',
+   'html!SBIS3.CONTROLS.FilterView/resources/FilterViewItemTemplate',
+   'js!SBIS3.CONTROLS.Link',
+   'js!SBIS3.CONTROLS.MenuLink',
+   'js!SBIS3.CONTROLS.ComboBox'
+], function(ListView, PickerMixin, dotTplFn, itemDotTpl) {
+   var userTpl = '<div class="userClass"><span style="margin-right: 15px;">{{=it.get("cfg").textValue}}</span><span style="color: red;">Это прикладной шаблон</span></div>';
+   var userItems = [ {name: '1993', title: '1993'}, {name: '1994', title: '1994'}, {name: '1995', title: '1995'}, {name: '1996', title: '1996'}, {name: '1997', title: '1997'} ];
+   var items = [
+      { field: 'Наличие', cfg: { textValue: 'В наличии' }},
+      { field: 'Тип', cfg: { textValue: 'ТипТип' }, tpl: userTpl },
+      { field: 'Год', componentType: 'SBIS3.CONTROLS.MenuLink', cfg: { items: userItems, caption: 'Год' } },
+      { field: 'ГодComboBox', componentType: 'SBIS3.CONTROLS.ComboBox', cfg: { items: userItems, caption: 'ГодComboBox' } }
+   ];
+
+   var FilterButton = ListView.extend([PickerMixin], {
+      $protected: {
+         _dotTplFn: dotTplFn,
+         _itemDotTpl: itemDotTpl,
+
+         _options: {
+            linkText: 'Другие фильтры',
+            items: items
+         },
+         _linkButton: undefined
+      },
+      $constructor: function() {
+         this._publish('onClickLink');
+         this._bindLinkButton();
+      },
+      _bindLinkButton: function() {
+         var self = this;
+         this.waitChildControlByName('FilterViewLink').addCallback(function(instanse){
+            self._linkButton = instanse;
+            self._linkButton.subscribe('onActivated', self._onLinkActivated.bind(self));
+         });
+      },
+      _onLinkActivated: function() {
+         this._notify('onClickLink');
+      },
+      _getItemTemplate: function () {
+         return this._itemDotTpl;
+      },
+      _drawItemsCallback: function() {
+
+      },
+      deleteRecords: function(idArray) {
+         /*TODO тут будет не удаление а скрытие видимости, когда появится перерисовка только одной строки*/
+         this._dataSet.removeRecord(idArray);
+         this._dataSource.sync(this._dataSet);
+      }
+   });
+
+   return FilterButton;
+
+});
