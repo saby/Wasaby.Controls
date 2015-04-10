0567af172eb17eb28b3ab33bf8a73a5c3e5a554b
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-04-10 09:07:43 +0300

    прописала блок при инициализации

--- components/TreeDataGrid/TreeDataGrid.module.js
+++ components/TreeDataGrid/TreeDataGrid.module.js
@@ -7,6 +7,18 @@ define('js!SBIS3.CONTROLS.TreeDataGrid', ['js!SBIS3.CONTROLS.DataGrid', 'js!SBIS
     * @mixes SBIS3.CONTROLS.TreeMixin
     * @public
     * @control
+    * @initial
+    * <component data-component='SBIS3.CONTROLS.TreeDataGrid'>
+    *    <options name="columns" type="array">
+    *       <options>
+    *          <option name="title">Поле 1</option>
+    *          <option name="width">100</option>
+    *       </options>
+    *       <options>
+    *          <option name="title">Поле 2</option>
+    *       </options>
+    *    </options>
+    * </component>
     */
 
    var TreeDataGrid = DataGrid.extend([TreeMixin], /** @lends SBIS3.CONTROLS.TreeDataGrid.prototype*/ {
