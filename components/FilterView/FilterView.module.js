877a028e4d90da471fad208759e736b53742402a
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2015-04-10 14:21:05 +0300

    стили

--- components/FilterView/FilterView.module.js
+++ components/FilterView/FilterView.module.js
@@ -8,15 +8,17 @@ define('js!SBIS3.CONTROLS.FilterView', [
    'html!SBIS3.CONTROLS.FilterView/resources/FilterViewItemTemplate',
    'js!SBIS3.CONTROLS.Link',
    'js!SBIS3.CONTROLS.MenuLink',
-   'js!SBIS3.CONTROLS.ComboBox'
+   'js!SBIS3.CONTROLS.ComboBox',
+   'js!SBIS3.CONTROLS.MenuButton'
 ], function(ListView, PickerMixin, dotTplFn, itemDotTpl) {
    var userTpl = '<div class="userClass"><span style="margin-right: 15px;">{{=it.get("cfg").textValue}}</span><span style="color: red;">Это прикладной шаблон</span></div>';
    var userItems = [ {name: '1993', title: '1993'}, {name: '1994', title: '1994'}, {name: '1995', title: '1995'}, {name: '1996', title: '1996'}, {name: '1997', title: '1997'} ];
    var items = [
       { field: 'Наличие', cfg: { textValue: 'В наличии' }},
       { field: 'Тип', cfg: { textValue: 'ТипТип' }, tpl: userTpl },
-      { field: 'Год', componentType: 'SBIS3.CONTROLS.MenuLink', cfg: { items: userItems, caption: 'Год' } },
-      { field: 'ГодComboBox', componentType: 'SBIS3.CONTROLS.ComboBox', cfg: { items: userItems, caption: 'ГодComboBox' } }
+      { field: 'ГодMenuLink', componentType: 'SBIS3.CONTROLS.MenuLink', cfg: { items: userItems, caption: 'ГодMenuLink' } },
+      { field: 'ГодComboBox', componentType: 'SBIS3.CONTROLS.ComboBox', cfg: { items: userItems, caption: 'ГодComboBox' } },
+      { field: 'ГодMenuButton', componentType: 'SBIS3.CONTROLS.MenuButton', cfg: { items: userItems, caption: 'ГодMenuButton' } }
    ];
 
    var FilterButton = ListView.extend([PickerMixin], {
