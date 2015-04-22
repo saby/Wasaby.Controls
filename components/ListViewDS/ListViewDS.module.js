f2900ccaec94fcf958ed1875ba869af759d4ddec
Черёмушкин И.В <iv.cheremushkin@tensor.ru>
2015-04-22 19:10:10 +0300

    убрал что то лишнее

--- components/ListViewDS/ListViewDS.module.js
+++ components/ListViewDS/ListViewDS.module.js
@@ -474,24 +474,7 @@ define('js!SBIS3.CONTROLS.ListViewDS',
           * @param {String} items.isMainOption На строке ли кнопка (или в меню).
           * @example
           * <pre>
-          *     dataGrid.setItemsActions([{
-          *        name: 'delete',
-          *        icon: 'sprite:icon-16 icon-Erase icon-error',
-          *        title: 'Удалить',
-          *        isMainAction: true,
-          *        onActivated: function(item) {
-          *           this.deleteRecords(item.data('id'));
-          *        }
-          *     },
-          *     {
-          *        name: 'addRecord',
-          *        icon: 'sprite:icon-16 icon-Add icon-error',
-          *        title: 'Добавить',
-          *        isMainAction: true,
-          *        onActivated: function(item) {
-          *           this.showRecordDialog();
-          *        }
-          *     }]
+          *     dataGrid.setItemsActions();
           * <pre>
           * @see itemsActions
           * @see getItemsActions
