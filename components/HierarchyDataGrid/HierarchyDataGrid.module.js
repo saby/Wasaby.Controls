c31be5a43184b5d7d8c7ef51911713979fea5bf1
kraynovdo <do.kraynov@tensor.ru>
2015-04-23 14:24:17 +0300

    Иерархическая таблица

---
+++ components/HierarchyDataGrid/HierarchyDataGrid.module.js
@@ -0,0 +1,39 @@
+define('js!SBIS3.CONTROLS.HierarchyDataGrid', [
+   'js!SBIS3.CONTROLS.DataGrid',
+   'js!SBIS3.CONTROLS.hierarchyMixin',
+   'html!SBIS3.CONTROLS.HierarchyDataGrid/resources/rowTpl'
+], function(DataGrid, hierarchyMixin, rowTpl) {
+   'use strict';
+   /**
+    * Контрол отображающий набор данных, имеющих иерархическую структуру, в виде в таблицы с несколькими колонками.
+    * @class SBIS3.CONTROLS.TreeDataGrid
+    * @extends SBIS3.CONTROLS.DataGrid
+    * @mixes SBIS3.CONTROLS.TreeMixin
+    * @public
+    * @control
+    * @initial
+    * <component data-component='SBIS3.CONTROLS.TreeDataGrid'>
+    *    <options name="columns" type="array">
+    *       <options>
+    *          <option name="title">Поле 1</option>
+    *          <option name="width">100</option>
+    *       </options>
+    *       <options>
+    *          <option name="title">Поле 2</option>
+    *       </options>
+    *    </options>
+    * </component>
+    */
+
+   var TreeDataGrid = DataGrid.extend([hierarchyMixin], /** @lends SBIS3.CONTROLS.TreeDataGrid.prototype*/ {
+      $protected: {
+         _rowTpl : rowTpl
+      },
+
+      $constructor: function() {
+      }
+   });
+
+   return TreeDataGrid;
+
+});
