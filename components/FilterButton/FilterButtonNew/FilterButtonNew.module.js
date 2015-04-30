d4ac5f430813f8cd1d482ecf62437f11b5c6762d
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2015-04-30 13:06:13 +0300

    ds

--- components/FilterButton/FilterButtonNew/FilterButtonNew.module.js
+++ components/FilterButton/FilterButtonNew/FilterButtonNew.module.js
@@ -19,10 +19,15 @@ define('js!SBIS3.CONTROLS.FilterButtonNew', [
             linkText: 'Нужно отобрать?',
             filterAlign: 'right',
             template: '',
-            pickerClassName: 'controls__filterButton__picker'
+            pickerClassName: 'controls__filterButton__picker',
+            items: [
+               {field: 'field1', textValue: 'filter1'},
+               {field: 'field2', textValue: 'filter2'},
+               {field: 'field3', textValue: 'filter3'},
+               {field: 'field4', textValue: 'filter4'}
+            ]
          },
          _filterLineButton: undefined
-
       },
 
       $constructor: function() {
@@ -36,6 +41,7 @@ define('js!SBIS3.CONTROLS.FilterButtonNew', [
       init: function() {
          FilterButtonNew.superclass.init.apply(this, arguments);
          (this._filterLineButton = this.getChildControlByName('filterLine')).subscribe('onActivated', this.showPicker.bind(this));
+         this.reload();
       },
 
       resetFilter: function() {
@@ -46,7 +52,7 @@ define('js!SBIS3.CONTROLS.FilterButtonNew', [
          console.log('applyFilter');
       },
 
-      _redraw: function() {/*Переопределяем, чтобы не было отрисовки*/},
+      //_redraw: function() {/*Переопределяем, чтобы не было отрисовки*/},
 
       _setPickerContent: function() {
          var self = this;
@@ -56,7 +62,27 @@ define('js!SBIS3.CONTROLS.FilterButtonNew', [
             self._picker.getChildControlByName('applyFilterButton').subscribe('onActivated', self.applyFilter.bind(self));
          });
       },
+      _getItemTemplate: function(item) {
+         var textValue = item.getRaw().textValue;
+         if (textValue) {
+            return '<span>' + textValue + ', </span>';
+         }
+      },
+      _drawItemsCallback: function() {
+         var lastSpan = this._filterLineButton._container.find('.controls-Link__field').find('span:last');
+         if (lastSpan.length) {
+            lastSpan.text(lastSpan.text().slice(0, -2));
+         } else {
+            this._filterLineButton.setCaption(this._options.linkText);
+         }
+      },
+      _getItemsContainer: function() {
+         return this._filterLineButton._container.find('.controls-Link__field');
+      },
+      _clearItems: function() {
 
+         this._getItemsContainer().empty();
+      },
       _setPickerConfig: function () {
          return {
             corner: this._options.filterAlign === 'right' ? 'tr' : 'tl',
