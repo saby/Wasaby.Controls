c9a10961ac26d9ea19631c8d699d57a5fec2b97d
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-05-04 15:09:24 +0300

    Добавила примеры в описания опций, перекрёстные ссылки, указала путь до папки с примером

--- components/SearchString/SearchString.module.js
+++ components/SearchString/SearchString.module.js
@@ -3,10 +3,13 @@ define('js!SBIS3.CONTROLS.SearchString', ['js!SBIS3.CORE.Control'], function (Co
    'use strict';
 
    /**
-    * Cтрока поиска, поле ввода + кнопка поиска
+    * Cтрока поиска, поле ввода + кнопка поиска.
     * @class SBIS3.CONTROLS.SearchString
     * @extends $ws.proto.Control
     * @public
+    * @control
+    * @demo SBIS3.Demo.Control.MySearchString
+    * @author Крайнов Дмитрий Олегович
     */
 
    var SearchString = Control.Control.extend(/** @lends SBIS3.CONTROLS.SearchString.prototype */{
@@ -14,14 +17,28 @@ define('js!SBIS3.CONTROLS.SearchString', ['js!SBIS3.CORE.Control'], function (Co
          _options: {
             /**
              * @cfg {String} Текст на кнопке поиска
+             * @example
+             * <pre>
+             *     <option name="btnCaption">Найти</option>
+             * </pre>
              */
             btnCaption: '',
             /**
              * @cfg {String} Подсказка в поле ввода
+             * @example
+             * <pre>
+             *     <option name="placeholder">Введите ФИО полностью</option>
+             * </pre>
              */
             placeholder: '',
             /**
              * @cfg {Number} При каком колчистве символов начинать поиск
+             * @example
+             * <pre>
+             *     <option name="countStart">5</option>
+             * </pre>
+             * @see apply
+             * @see reset
              */
             countStart: null
          }
@@ -33,6 +50,8 @@ define('js!SBIS3.CONTROLS.SearchString', ['js!SBIS3.CORE.Control'], function (Co
 
       /**
        * Начать поиск с тем текстом, что введен
+       * @see reset
+       * @see countStart
        */
       apply: function() {
 
@@ -40,6 +59,7 @@ define('js!SBIS3.CONTROLS.SearchString', ['js!SBIS3.CORE.Control'], function (Co
 
       /**
        * Сбросить поиск
+       * @see apply
        */
       reset: function(){
 
