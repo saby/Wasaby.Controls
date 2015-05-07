bf7c98fc20d4221caff08ff165404b2b28ece43f
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-05-07 17:11:19 +0300

    переименовала пространство имён в CONTROLS

--- components/TreeView/Demo/MyTreeView/MyTreeView.js
+++ components/TreeView/Demo/MyTreeView/MyTreeView.js
@@ -1,18 +1,18 @@
-define('js!SBIS3.CONTROLSs.Demo.MyTreeView',
+define('js!SBIS3.CONTROLS.Demo.MyTreeView',
     [
         'js!SBIS3.CORE.CompoundControl',
-        'html!SBIS3.CONTROLSs.Demo.MyTreeView',
-        'css!SBIS3.CONTROLSs.Demo.MyTreeView',
+        'html!SBIS3.CONTROLS.Demo.MyTreeView',
+        'css!SBIS3.CONTROLS.Demo.MyTreeView',
         'js!SBIS3.CONTROLS.TreeView',
         'js!SBIS3.CONTROLS.TreeDataGrid'
     ], function(CompoundControl, dotTplFn) {
     /**
-     * SBIS3.CONTROLSs.Demo.MyTreeView
-     * @class SBIS3.CONTROLSs.Demo.MyTreeView
+     * SBIS3.CONTROLS.Demo.MyTreeView
+     * @class SBIS3.CONTROLS.Demo.MyTreeView
      * @extends $ws.proto.CompoundControl
      * @control
      */
-    var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLSs.Demo.MyTreeView.prototype */{
+    var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLS.Demo.MyTreeView.prototype */{
         _dotTplFn: dotTplFn,
         $protected: {
             _options: {
