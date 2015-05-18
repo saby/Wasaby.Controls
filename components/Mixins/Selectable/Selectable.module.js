98b41e64800b15d367eb0656c3ad65809db09142
is.protasov <is.protasov@tensor.ru>
2015-05-18 11:27:34 +0300

    rename property selectedIndex to selectedKey, selectedIndexes to selectedKeys

--- components/Mixins/Selectable/Selectable.module.js
+++ components/Mixins/Selectable/Selectable.module.js
@@ -17,14 +17,14 @@ define('js!SBIS3.CONTROLS.Selectable', [], function() {
              * @cfg {String} Идентификатор выбранного элемента
              * @example
              * <pre>
-             *     <option name="selectedIndex">3</option>
+             *     <option name="selectedKey">3</option>
              * </pre>
              * @see setSelectedKey
              * @see getSelectedKey
              * @see setSelectedItem
              * @see getSelectedItem
              */
-            selectedIndex: null,
+            selectedKey: null,
             /**
              * TODO Выбранный элемент
              */
@@ -36,8 +36,8 @@ define('js!SBIS3.CONTROLS.Selectable', [], function() {
       $constructor: function() {
          this._publish('onSelectedItemChange');
          if (this._options.selectedItem) {
-            console.log('c 3.7.3 свойство selectedItem перестанет работать. Используйте свойство selectedIndex');
-            this._options.selectedIndex = this._options.selectedItem;
+            console.log('c 3.7.3 свойство selectedItem перестанет работать. Используйте свойство selectedKey');
+            this._options.selectedKey = this._options.selectedItem;
          }
          else {
             if (this._options.allowEmptySelection == false) {
@@ -76,13 +76,13 @@ define('js!SBIS3.CONTROLS.Selectable', [], function() {
        * @see getSelectedKey
        */
       setSelectedKey : function(id) {
-         this._options.selectedIndex = id;
-         if (!this._options.selectedIndex && this._options.allowEmptySelection == false) {
+         this._options.selectedKey = id;
+         if (!this._options.selectedKey && this._options.allowEmptySelection == false) {
             this._setFirstItemAsSelected();
          }
-         this.saveToContext('SelectedItem', this._options.selectedIndex); //TODO: Перенести отсюда
-         this._drawSelectedItem(this._options.selectedIndex);
-         this._notifySelectedItem(this._options.selectedIndex);
+         this.saveToContext('SelectedItem', this._options.selectedKey); //TODO: Перенести отсюда
+         this._drawSelectedItem(this._options.selectedKey);
+         this._notifySelectedItem(this._options.selectedKey);
       },
 
       /**
@@ -95,7 +95,7 @@ define('js!SBIS3.CONTROLS.Selectable', [], function() {
        * @see setSelectedKey
        */
       getSelectedKey : function() {
-         return this._options.selectedIndex;
+         return this._options.selectedKey;
       },
 
       _drawSelectedItem : function() {
@@ -108,14 +108,14 @@ define('js!SBIS3.CONTROLS.Selectable', [], function() {
       },
 
       _dataLoadedCallback : function(){
-         if (!this._options.selectedIndex && this._options.allowEmptySelection == false) {
+         if (!this._options.selectedKey && this._options.allowEmptySelection == false) {
             this._setFirstItemAsSelected();
          }
       },
 
       _setFirstItemAsSelected : function() {
          if (this._dataSet) {
-            this._options.selectedIndex = this._dataSet.at(0).getKey();
+            this._options.selectedKey = this._dataSet.at(0).getKey();
          }
       }
    };
