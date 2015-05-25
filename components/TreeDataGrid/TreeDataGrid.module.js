39a56ac929530409fe8969ee27e1f085c29a6b52
iv.cheremushkin <ccherya@gmail.com>
2015-05-25 16:46:21 +0300

    Отступы в дереве через дивы Папки в первой колонке Починил SetColumns - больше не надо отдавать пустую первую колонку 15рх шрифт для папок

--- components/TreeDataGrid/TreeDataGrid.module.js
+++ components/TreeDataGrid/TreeDataGrid.module.js
@@ -93,8 +93,11 @@ define('js!SBIS3.CONTROLS.TreeDataGrid', [
          if (parentKey) {
             var
                parentCont = $('.controls-ListView__item[data-id="' + parentKey + '"]', this._container.get(0)).get(0),
-               parentMargin = parseInt($('.controls-TreeView__expand', parentCont).css('margin-left'), 10) || 0;
-            $('.controls-TreeView__expand', container).css('margin-left', parentMargin + 16);
+               parentWrappersCount = $('.controls-TreeView__hierWrapper', parentCont).length;
+               //parentMargin = parseInt($('.controls-TreeView__expand', parentCont).css('margin-left'), 10) || 0;
+               for (var i = 0; i <= parentWrappersCount; i++) {
+                 $('.controls-TreeView__expand', container).before('<div class="controls-TreeView__hierWrapper"></div>');
+               };
          }
       }
    });
