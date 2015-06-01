d8b257d06af1e5f8ec61acde9cd2480b9981e407
iv.cheremushkin <ccherya@gmail.com>
2015-06-01 12:37:10 +0300

    draw-selected-items DataGrid

--- components/DataGrid/DataGrid.module.js
+++ components/DataGrid/DataGrid.module.js
@@ -237,6 +237,7 @@ define('js!SBIS3.CONTROLS.DataGrid',
 
             this._movableElems = this._findMovableCells();
          }
+         this._drawSelectedItems(this._options.selectedKeys);
       },
       /************************/
       /*   Частичный скролл   */
