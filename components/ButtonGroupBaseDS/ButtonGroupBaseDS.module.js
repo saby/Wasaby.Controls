1e134cba07df654c6c0b96723e838934a4ae8b7b
Черёмушкин И.В <iv.cheremushkin@tensor.ru>
2015-06-02 15:22:41 +0300

    getItemsInstances

--- components/ButtonGroupBaseDS/ButtonGroupBaseDS.module.js
+++ components/ButtonGroupBaseDS/ButtonGroupBaseDS.module.js
@@ -38,7 +38,7 @@ define('js!SBIS3.CONTROLS.ButtonGroupBaseDS', ['js!SBIS3.CORE.CompoundControl', 
 
       setEnabled: function(enabled) {
         ButtonGroupBase.superclass.setEnabled.call(this, enabled);
-        var itemsInstances = this.getChildControls();
+        var itemsInstances = this.getItemsInstances();
         for (var i in itemsInstances){
           itemsInstances[i].setEnabled(enabled);
         }
