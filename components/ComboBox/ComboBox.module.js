d2b78dd5c7c3db14b2795f1b304c1984f4d2da69
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-06-06 12:43:32 +0300

    убрала игнорирование, т.к. уже спрятано в textBox, от которого наследуемся

--- components/ComboBox/ComboBox.module.js
+++ components/ComboBox/ComboBox.module.js
@@ -36,7 +36,6 @@ define('js!SBIS3.CONTROLS.ComboBox', [
     * @mixes SBIS3.CONTROLS.FormWidgetMixin
     * @mixes SBIS3.CONTROLS.DSMixin
     * @mixes SBIS3.CONTROLS.Selectable
-    * @ignoreOptions independentContext contextRestriction extendedTooltip
     */
 
    var ComboBox = TextBox.extend([PickerMixin, DSMixin, Selectable, DataBindMixin], /** @lends SBIS3.CONTROLS.ComboBox.prototype */{
