0edd5d53c31907701cd32194515c9e819621f67a
ea.korobeynikova <ea.korobeynikova@tensor.ru>
2015-06-16 12:10:20 +0300

    переименовала

--- components/CheckBoxGroup/CheckBoxGroup.module.js
+++ components/CheckBoxGroup/CheckBoxGroup.module.js
@@ -20,17 +20,17 @@ define('js!SBIS3.CONTROLS.CheckBoxGroup', ['js!SBIS3.CONTROLS.CheckBoxGroupBase'
     *    <options name="items" type="array">
     *       <options>
     *          <option name="id">1</option>
-    *          <option name="title">CheckBox 1</option>
+    *          <option name="caption">CheckBox 1</option>
     *       </options>
     *       <options>
     *          <option name="id">2</option>
-    *          <option name="title">CheckBox 2</option>
+    *          <option name="caption">CheckBox 2</option>
     *       </options>
     *    </options>
     *    <option name="keyField">id</option>
-    *    <option name="displayField">title</option>
+    *    <option name="displayField">caption</option>
     * </component>
-    * @cssModifier controls-ButtonGroup__vertical Для вертикального расположения элементов в группе.
+    * @cssModifier controls-ButtonGroup__vertical Задаёт вертикальное расположение элементов в группе.
     */
 
    var CheckBoxGroup = CheckBoxGroupBase.extend( /** @lends SBIS3.CONTROLS.CheckBoxGroup.prototype */ {
@@ -38,7 +38,7 @@ define('js!SBIS3.CONTROLS.CheckBoxGroup', ['js!SBIS3.CONTROLS.CheckBoxGroupBase'
        /**
         * @typedef {Object} CheckBoxGroupItems
         * @property {String} id Идентификатор элемента группы.
-        * @property {String} title Подпись у элемента группы.
+        * @property {String} caption Заголовок элемента группы.
         */
        /**
         * @cfg {CheckBoxGroupItems[]} Набор исходных данных, по которому строится отображение группы
@@ -48,15 +48,15 @@ define('js!SBIS3.CONTROLS.CheckBoxGroup', ['js!SBIS3.CONTROLS.CheckBoxGroupBase'
         *     <options name="items" type="array">
         *        <options>
         *            <option name="id">1</option>
-        *            <option name="title">Флажок_1</option>
+        *            <option name="caption">Флажок_1</option>
         *         </options>
         *         <options>
         *            <option name="id">2</option>
-        *            <option name="title">Флажок_2</option>
+        *            <option name="caption">Флажок_2</option>
         *         </options>
         *         <options>
         *            <option name="id">3</option>
-        *            <option name="title">Флажок_3</option>
+        *            <option name="caption">Флажок_3</option>
         *         </options>
         *      </options>
         * </pre>
