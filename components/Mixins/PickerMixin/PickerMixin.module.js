9df33a47ade6f851bdf05e7f745bb03c20a7c47a
turinv <turinv@tensor.ru>
2015-06-29 17:22:37 +0300

    Исправляю багу в PickerMixin - затирался контекст, игнорировалась переданная опция context у объекта pickerConfig

--- components/Mixins/PickerMixin/PickerMixin.module.js
+++ components/Mixins/PickerMixin/PickerMixin.module.js
@@ -58,7 +58,11 @@ define('js!SBIS3.CONTROLS.PickerMixin', ['js!SBIS3.CONTROLS.FloatArea'], functio
          var pickerConfig = this._setPickerConfig();
          pickerConfig.parent = this.getParent();
          pickerConfig.opener = this;
-         pickerConfig.context = this.getParent() ? this.getParent().getLinkedContext() : {};
+
+         if (!pickerConfig.context) {
+            pickerConfig.context = this.getParent() ? this.getParent().getLinkedContext() : {};
+         }
+
          pickerConfig.target = pickerConfig.target || this._container;
          pickerConfig.element = pickerContainer;
          return new FloatArea(pickerConfig);
