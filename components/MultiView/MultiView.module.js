1fd56a0d33a9c9fa16a8c9e084aa0778cd9aadc8
kraynovdo <do.kraynov@tensor.ru>
2015-06-30 13:41:37 +0300

    Первый коммит

--- components/MultiView/MultiView.module.js
+++ components/MultiView/MultiView.module.js
@@ -1,4 +1,4 @@
-define('js!SBIS3.CONTROLS.MultiView', ['js!SBIS3.CONTROLS.DataGrid', 'js!SBIS3.CONTROLS.MultiViewMixin'], function(DataGrid, MultiViewMixin) {
+define('js!SBIS3.CONTROLS.MultiView', ['js!SBIS3.CONTROLS.DataGrid', 'js!SBIS3.CONTROLS.CompositeViewMixin'], function(DataGrid, MultiViewMixin) {
    'use strict';
 
    var MultiView = DataGrid.extend([MultiViewMixin],/** @lends SBIS3.CONTROLS.DataGrid.prototype*/ {
