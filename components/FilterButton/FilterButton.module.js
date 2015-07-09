2e13e6fae3743eb2eeb35ad9183213632255c795
turinv <turinv@tensor.ru>
2015-07-09 19:30:06 +0300

    Доработка новой кнопки фильтров по замечаниям

--- components/FilterButton/FilterButton.module.js
+++ components/FilterButton/FilterButton.module.js
@@ -16,6 +16,8 @@ define('js!SBIS3.CONTROLS.FilterButton',
 
    var linkTextDef = 'Нужно отобрать?';
 
+   var contextRootName = 'sbis3-controls-filter-button';
+
    var FilterButton = CompoundControl.extend([PickerMixin],{
       _dotTplFn: dotTplFn,
       _dotTplPicker: dotTplForPicker,
@@ -32,29 +34,48 @@ define('js!SBIS3.CONTROLS.FilterButton',
             resetLinkText: linkTextDef,
 
             filterFields: [],
-            linkTextFields: []
+            linkTextFields: [],
+
+            independentContext: true
          },
          _pickerContext: null,
          _linkTextFields: null,
          _filterFields: null,
-         _linkTextEl: null,
-         _linkCrossEl: null
+         _textToFilterFields: null
       },
 
       $constructor: function() {
-         function splitFields(fields) {
-            return $ws.helpers.isPlainArray(fields) ? fields : ('' + (fields || '')).split(',');
+         var
+            linkCrossEl = this._container.find('.controls__filterButton__filterLine-cross'),
+            showButtonEl = this._container.find('.controls__filterButton__filterLine-hoverContainer, .controls__filterButton-button'),
+            split;
+
+         function splitFields(fields, sep) {
+            return $ws.helpers.isPlainArray(fields) ? fields : ('' + (fields || '')).split(sep);
          }
-         this._linkTextFields = splitFields(this._options.linkTextFields);
-         this._filterFields = splitFields(this._options.filterFields);
+
+         this._filterFields = splitFields(this._options.filterFields, ',');
+
+         this._linkTextFields = [];
+         this._textToFilterFields = {};
+         split = splitFields(this._options.linkTextFields, ',');
+
+         $ws.helpers.forEach(split, function(textField) {
+            var
+               splitField = splitFields(textField, ':'),
+               textField = splitField[0];;
+            if (splitField.length > 1) {
+               this._textToFilterFields[textField] = splitField[1];
+            }
+            this._linkTextFields.push(textField);
+         }, this);
 
          this._container.removeClass('ws-area');
-         this._linkTextEl = this._container.find('.controls__filterButton__filterLine-items');
-         this._linkCrossEl = this._container.find('.controls__filterButton__filterLine-cross');
 
-         var showButtonEl = this._container.find('.controls__filterButton__filterLine-hoverContainer, .controls__filterButton-button');
+         linkCrossEl.click(function() {
+            this._resetFilter(false);
+         }.bind(this));
 
-         this._linkCrossEl.click(this.resetFilter.bind(this));
          showButtonEl.click(this.showPicker.bind(this));
 
          this.subscribe('onDestroy' , function() {
@@ -63,67 +84,69 @@ define('js!SBIS3.CONTROLS.FilterButton',
             });
          });
 
-         this._updateLink();
+         this._recalcInternalContext();
       },
 
-      _isFilterChanged: function() {
-         return !$ws.helpers.isEqualObject(this._options.filter, this._options.resetFilter);
-      },
-
-      _updateLink: function() {
+      _recalcInternalContext: function() {
          var
-            changed = this._isFilterChanged(),
-            text = changed ? this._options.linkText : this._options.resetLinkText;
-         this._linkTextEl.text(text);
-         this._linkCrossEl.toggleClass('ws-hidden', !changed);
+            changed = !$ws.helpers.isEqualObject(this._options.filter, this._options.resetFilter),
+            linkText = changed ? this._options.linkText : this._options.resetLinkText;
+
+         this.getLinkedContext().setValueSelf(contextRootName, {
+            changed: changed,
+            linkText: linkText
+         });
       },
 
-      _syncContext: function(fromContext, syncText) {
+      _syncContext: function(fromContext) {
          var
             context = this._pickerContext,
-            descrValues, descr;
+            descrValues, linkText, filter, description;
 
          if (context) {
             if (fromContext) {
                context.requestUpdate();
-               this._options.filter = context.getValue('filter');
-            } else if (context) {
-               context.setValueSelf('filter', this._options.filter);
+               this._options.filter = context.getValue(contextRootName + '/filter');
+            } else {
+               context.setValueSelf(contextRootName + '/filter', this._options.filter);
             }
 
-            if (syncText) {
-               descrValues = $ws.helpers.map(this._linkTextFields, function (fieldName) {
-                  var
-                     ctx = this._pickerContext,
-                     filter = ctx.getValue('filter'),
-                     description = ctx.getValue('description'),
-                     result;
+            filter = context.getValue(contextRootName + '/filter');
+            description = context.getValue(contextRootName + '/description');
 
+            descrValues = $ws.helpers.map(this._linkTextFields, function (textFieldName) {
+               var
+                  result, fieldName;
+
+               if (textFieldName in filter) {
+                  result = filter[textFieldName];
+               } else if (textFieldName in this._textToFilterFields) {
+                  fieldName = this._textToFilterFields[textFieldName];
                   if (fieldName in filter) {
-                     result = filter[fieldName];
-                  } else {
-                     result = description[fieldName] || '';
+                     result = description[textFieldName];
                   }
-                  return result;
-               }, this);
+               } else {
+                  result = description[textFieldName];
+               }
 
-               descr = $ws.helpers.filter(descrValues, function (v) {
-                  return v !== '';
-               }).join(', ');
+               return result;
+            }, this);
 
-               this._options.linkText = descr;
-               this._updateLink();
-            }
+            linkText = $ws.helpers.filter(descrValues, function (v) {
+               return v !== undefined && v !== '';
+            }).join(', ');
+
+            this._options.linkText = linkText;
          }
+         this._recalcInternalContext();
       },
 
-      resetFilter: function() {
-         this._options.filter = this._options.resetFilter;
-         this._options.linkText = this._options.resetLinkText;
-
-         this._syncContext(false, false);
-         this._updateLink();
-         this._notify('onPropertyChanged');
+      _resetFilter: function(internalOnly) {
+         if (internalOnly) {
+            this._pickerContext.setValueSelf(contextRootName + '/filter', this._options.resetFilter);
+         } else {
+            this.setFilter(this._options.resetFilter);
+         }
       },
 
       applyFilter: function() {
@@ -138,12 +161,21 @@ define('js!SBIS3.CONTROLS.FilterButton',
       setFilter: function(filter) {
          this._options.filter = filter;
          this._syncContext(false, true);
+
          this._notify('onPropertyChanged');
       },
 
       setLinkText: function(text) {
          this._options.linkText = text;
-         this._updateLink();
+
+         this._recalcInternalContext();
+         this._notify('onPropertyChanged');
+      },
+
+      setResetLinkText: function(text) {
+         this._options.resetLinkText = text;
+
+         this._recalcInternalContext();
          this._notify('onPropertyChanged');
       },
 
@@ -160,7 +192,7 @@ define('js!SBIS3.CONTROLS.FilterButton',
          this._picker.getContainer().addClass('controls__filterButton-' + this._options.filterAlign);
 
          $ws.single.CommandDispatcher.declareCommand(this._picker, 'apply-filter', this.applyFilter.bind(this));
-         $ws.single.CommandDispatcher.declareCommand(this._picker, 'reset-filter', this.resetFilter.bind(this));
+         $ws.single.CommandDispatcher.declareCommand(this._picker, 'reset-filter', this._resetFilter.bind(this, true));
 
          this._forEachFieldLinks(function(fieldLink) {
             ControlHierarchyManager.addNode(fieldLink.getSuggest());
@@ -168,24 +200,40 @@ define('js!SBIS3.CONTROLS.FilterButton',
       },
 
       _setPickerConfig: function () {
-         this._pickerContext = new $ws.proto.Context({restriction: 'set'});
-         this._pickerContext.setValueSelf('filter', this._options.filter);
-         this._pickerContext.setValueSelf('description', {});
+         var
+            ctx = new $ws.proto.Context({restriction: 'set'}),
+            firstTime = true,
+            updatePickerContext = function () {
+               ctx.setValue(contextRootName, this.getLinkedContext().getValueSelf(contextRootName));
+               ctx.setValue(contextRootName + '/filter', this._options.filter);
+               ctx.setValue(contextRootName + '/description', {});
+            }.bind(this);
 
-         this._pickerContext.subscribe('onFieldNameResolution', function(event, fieldName) {
+         this._pickerContext = ctx;
+
+         updatePickerContext();
+
+         ctx.subscribe('onFieldNameResolution', function(event, fieldName) {
             var
                finder = function(field) {
                   return field === fieldName;
                },
                inFilterFields = $ws.helpers.findIdx(this._filterFields, finder) !== -1,
-               inDescrFields = $ws.helpers.findIdx(this._linkTextFields, finder) !== -1,
-               SEP = $ws.proto.Context.STRUCTURE_SEPARATOR;
+               inDescrFields = $ws.helpers.findIdx(this._linkTextFields, finder) !== -1;
 
             if (inFilterFields) {
-               event.setResult('filter' + SEP + fieldName);
+               event.setResult(contextRootName + '/filter/' + fieldName);
             } else if (inDescrFields) {
-               event.setResult('description' + SEP + fieldName);
+               event.setResult(contextRootName + '/description/' + fieldName);
             }
+         }.bind(this));
+
+         ctx.subscribe('onFieldsChanged', function() {
+            var
+               filter = ctx.getValue(contextRootName + '/filter'),
+               changed = !$ws.helpers.isEqualObject(filter, this._options.resetFilter);
+
+            ctx.setValueSelf(contextRootName + '/changed', changed);
          }.bind(this));
 
          return {
@@ -199,13 +247,20 @@ define('js!SBIS3.CONTROLS.FilterButton',
       },
             closeButton: true,
             closeByExternalClick: true,
-            context: this._pickerContext,
+            context: ctx,
             template: dotTplForPicker.call(this, {template: this._options.template}),
             handlers: {
                onClose: function() {
                   this._forEachFieldLinks(function(fieldLink) {
                      fieldLink.getSuggest()._hideMenu();
                   });
+               }.bind(this),
+
+               onShow: function() {
+                  if (!firstTime) {
+                     updatePickerContext();
+                  }
+                  firstTime = false;
                }.bind(this)
             }
          };
