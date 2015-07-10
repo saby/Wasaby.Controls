5ba244dfff169c855314b7148ec3634eb0847e87
aa.malcev2 <aleksey.a.maltsev@gmail.com>
2015-07-10 09:04:29 +0300

    не рассчитывалась минимальная ширина picker-а переформатировал отступы

--- components/SuggestTextBox/Demo/MySuggestTextBox/MySuggestTextBox.js
+++ components/SuggestTextBox/Demo/MySuggestTextBox/MySuggestTextBox.js
@@ -1,28 +1,26 @@
 define('js!SBIS3.CONTROLS.Demo.MySuggestTextBox', [
-    'js!SBIS3.CORE.CompoundControl',
-    'html!SBIS3.CONTROLS.Demo.MySuggestTextBox',
-    'css!SBIS3.CONTROLS.Demo.MySuggestTextBox',
-    'js!SBIS3.CONTROLS.SuggestTextBox'
-], function(CompoundControl, dotTplFn) {
-    /**
-     * SBIS3.CONTROLS.Demo.MySuggestTextBox
-     * @class SBIS3.CONTROLS.Demo.MySuggestTextBox
-     * @extends $ws.proto.CompoundControl
-     * @control
-     */
-    var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLS.Demo.MySuggestTextBox.prototype */{
-        _dotTplFn: dotTplFn,
-        $protected: {
-            _options: {
+   'js!SBIS3.CORE.CompoundControl',
+   'html!SBIS3.CONTROLS.Demo.MySuggestTextBox',
+   'css!SBIS3.CONTROLS.Demo.MySuggestTextBox',
+   'js!SBIS3.CONTROLS.SuggestTextBox'
+], function (CompoundControl, dotTplFn) {
+   /**
+    * SBIS3.CONTROLS.Demo.MySuggestTextBox
+    * @class SBIS3.CONTROLS.Demo.MySuggestTextBox
+    * @extends $ws.proto.CompoundControl
+    * @control
+    */
+   var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLS.Demo.MySuggestTextBox.prototype */{
+      _dotTplFn: dotTplFn,
+      $protected: {
+         _options: {}
+      },
+      $constructor: function () {
+      },
 
-            }
-        },
-        $constructor: function() {
-        },
-
-        init: function() {
-            moduleClass.superclass.init.call(this);
-        }
-    });
-    return moduleClass;
+      init: function () {
+         moduleClass.superclass.init.call(this);
+      }
+   });
+   return moduleClass;
 });
