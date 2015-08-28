98d543be22265b75da83ff5c91c95f9363e31b50
kraynovdo <do.kraynov@tensor.ru>
2015-08-28 17:38:27 +0300

    Пример

--- components/MenuButton/Demo/MyMenuButton/MyMenuButton.module.js
+++ components/MenuButton/Demo/MyMenuButton/MyMenuButton.module.js
@@ -1,10 +1,11 @@
 define('js!SBIS3.CONTROLS.Demo.MyMenuButton',
     ['js!SBIS3.CORE.CompoundControl',
-     'html!SBIS3.CONTROLS.Demo.MyMenuButton',
+     'html!SBIS3.CONTROLS.Demo.MyMenuButton','js!SBIS3.CONTROLS.ArrayStrategy', 'js!SBIS3.CONTROLS.StaticSource',
      'css!SBIS3.CONTROLS.Demo.MyMenuButton',
      'js!SBIS3.CONTROLS.MenuButton',
-     'js!SBIS3.CONTROLS.MenuLink'],
-    function(CompoundControl, dotTplFn) {
+     'js!SBIS3.CONTROLS.MenuLink',
+      'js!SBIS3.CONTROLS.Button'],
+    function(CompoundControl, dotTplFn, ArrayStrategy, StaticSource) {
    /**
     * SBIS3.CONTROLS.Demo.MyMenuButton
     * @class SBIS3.CONTROLS.Demo.MyMenuButton
@@ -13,6 +14,7 @@ define('js!SBIS3.CONTROLS.Demo.MyMenuButton',
     */
    var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLS.Demo.MyMenuButton.prototype */{
       _dotTplFn: dotTplFn,
+      _myMenu: null,
       $protected: {
          _options: {
             
@@ -23,6 +25,28 @@ define('js!SBIS3.CONTROLS.Demo.MyMenuButton',
 
       init: function() {
          moduleClass.superclass.init.call(this);
+         this._myMenu = this.getChildControlByName('MenuButton');
+         var self = this;
+         this.getChildControlByName('setData').subscribe('onActivated', function(){
+            var ds = new StaticSource({
+               data: [{
+                  'id' : 1,
+                  'title' : 'Первый'
+               },
+               {
+                  'id' : 2,
+                  'title' : 'Второй'
+               },
+               {
+                  'id': 3,
+                  'title' : 'Третий'
+
+               }],
+               strategy: new ArrayStrategy(),
+               keyField : 'id'
+            });
+            self._myMenu.setDataSource(ds);
+         });
       }
    });
    return moduleClass;
