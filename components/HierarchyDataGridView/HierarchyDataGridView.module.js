7bf4e8c2cf8c4c1d907c2814bee83fa0bac50531
ad.Chistyakova <a.d.chistyakova@gmail.com>
2015-09-04 10:48:04 +0300

    Даем возможность прокинуть пользовательский шаблон в хлебные крошки в поиске Ошибка в разработку от 06.08.2015 №981866 Новый каталог. Режим поиска. - Не отмечаются цветом удаленные разделы - Не показываются отметки '... https://inside.tensor.ru/opendoc.html?guid=8be0e50d-8282-47ea-ae27-2b6c0c43ba84

--- components/HierarchyDataGridView/HierarchyDataGridView.module.js
+++ components/HierarchyDataGridView/HierarchyDataGridView.module.js
@@ -174,10 +174,11 @@ define('js!SBIS3.CONTROLS.HierarchyDataGridView', [
       _drawBreadCrumbs:function(path, record, container){
          if (path.length) {
             var self = this,
-                  elem;
+                  elem,
+                  groupBy = this._options.groupBy,
+                  cfg;
             container.find('td').append(elem = $('<div/>'));
-
-            var ps = new BreadCrumbs({
+            cfg = {
                element : elem,
                items: this._createPathItemsDS(path),
                highlightEnabled: this._options.highlightEnabled,
@@ -185,7 +186,11 @@ define('js!SBIS3.CONTROLS.HierarchyDataGridView', [
                colorMarkEnabled: this._options.colorMarkEnabled,
                colorField: this._options.colorField,
                className : 'controls-BreadCrumbs__smallItems'
-            });
+            };
+            if (groupBy.hasOwnProperty('breadCrumbsTpl')){
+               cfg.itemTemplate = groupBy.breadCrumbsTpl
+            }
+            var ps = new BreadCrumbs(cfg);
             ps.once('onItemClick', function(event, id){
                //Таблицу нужно связывать только с тем PS, в который кликнули. Хорошо, что сначала идет _notify('onBreadCrumbClick'), а вотом выполняется setCurrentRoot
                event.setResult(false);
@@ -221,7 +226,8 @@ define('js!SBIS3.CONTROLS.HierarchyDataGridView', [
             dsItems.push({
                id: pathRecords[i].getKey(),
                title: pathRecords[i].get(this._options.displayField),
-               parentId: pathRecords[i].get(this._options.hierField)[0]
+               parentId: pathRecords[i].get(this._options.hierField)[0],
+               data: pathRecords[i]
             });
          }
          return dsItems;
