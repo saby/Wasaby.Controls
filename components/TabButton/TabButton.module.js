4f00108235429364aed9b9094dd91eddd86def55
as.krasilnikov <as.krasilnikov@tensor.ru>
2015-10-02 11:32:53 +0300

    editAtPlace + itemTemplate

--- components/TabButton/TabButton.module.js
+++ components/TabButton/TabButton.module.js
@@ -3,7 +3,8 @@
  *
  * @description
  */
-define('js!SBIS3.CONTROLS.TabButton', ['js!SBIS3.CONTROLS.RadioButtonBase', 'html!SBIS3.CONTROLS.TabButton'], function (RadioButtonBase, dotTplFn) {
+define('js!SBIS3.CONTROLS.TabButton',
+   ['js!SBIS3.CONTROLS.RadioButtonBase', 'html!SBIS3.CONTROLS.TabButton', 'js!SBIS3.CONTROLS.EditAtPlace'], function (RadioButtonBase, dotTplFn, EditAtPlace) {
 
    'use strict';
    /**
@@ -16,13 +17,43 @@ define('js!SBIS3.CONTROLS.TabButton', ['js!SBIS3.CONTROLS.RadioButtonBase', 'htm
       $protected: {
          _options: {
             align: 'right',
-            additionalText: ''
+            additionalText: '',
+            editable: false,
+            template: undefined
          }
       },
       _dotTplFn: dotTplFn,
 
       $constructor: function () {
-      }
+         this._createEditAtPlace();
+         this._setTabButtonTemplate();
+      },
+      _createEditAtPlace: function(){
+         if (!this._options.editable){
+            return;
+         }
+         var element = this.getContainer().find('.controls-TabButton__caption');
+         var self = this;
+         element.attr('data-bind', '{Text: feature}');
+         new EditAtPlace({
+            element: element,
+            name: $ws.helpers.randomId('tabButton-'),
+            text: element.text(),
+            enableControlPanel: true,
+            editorTpl: '<component data-component="SBIS3.CONTROLS.TextBox" data-bind="{Text: feature}">'+
+               '<option name="maxLength">13</option>'+
+               '</component>'
+         });
+         this.getParent().getLinkedContext().setValue('feature', element.text())
+      },
+       _setTabButtonTemplate: function(){
+          if (!this._options.template){
+             return;
+          }
+          var element = this.getContainer().find('.controls-TabButton__inner'),
+              tpl = this.getParent()._buildTplItem({}, this._options.template);
+          element.html(tpl);
+       }
 
    });
 
