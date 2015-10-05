6e04820e1731fbe828ee17d2d0d79d985b5ffb12
is.protasov <is.protasov@tensor.ru>
2015-10-05 19:09:56 +0300

    Разработка нового SwitchableArea и TabControl. Промежуточный коммит 3 - Для SwitchableArea за основу взят CORE.SwitchableArea и его шаблоны. Для связывания добавил метод setItems

--- components/TabControl/TabControl.module.js
+++ components/TabControl/TabControl.module.js
@@ -1,28 +1,47 @@
-/**
- * Created by iv.cheremushkin on 14.08.2014.
- */
-
-define('js!SBIS3.CONTROLS.TabControl', ['js!SBIS3.CONTROLS.SwitchableArea', 'html!SBIS3.CONTROLS.TabControl','js!SBIS3.CONTROLS.TabButtons'], function(SwitchableArea, dotTplFn) {
+define('js!SBIS3.CONTROLS.TabControl', [
+   'js!SBIS3.CORE.CompoundControl',
+   'html!SBIS3.CONTROLS.TabControl',
+   'js!SBIS3.CONTROLS.SwitchableArea',
+   'js!SBIS3.CONTROLS.TabButtons',
+   'js!SBIS3.CONTROLS.Link'
+], function(CompoundControl, dotTplFn) {
 
    'use strict';
+
+   var contextName = 'sbis3-controls-tab-control';
 
    /**
     * Контрол, содержащий несколько областей содержащих контент.
     * В каждый момент времени отображается только одна область. Отображаемая область может переключаться при клике на корешки закладок.
     * @class SBIS3.CONTROLS.TabControl
-    * @extends SBIS3.CONTROLS.SwitchableArea
+    * @extends SBIS3.CORE.CompoundControl
     * @author Крайнов Дмитрий Олегович
     */
 
-   var TabControl = SwitchableArea.extend( /** @lends SBIS3.CONTROLS.TabControl.prototype */ {
+   var TabControl = CompoundControl.extend( /** @lends SBIS3.CONTROLS.TabControl.prototype */ {
       _dotTplFn : dotTplFn,
       $protected: {
          _options: {
-
+            selectedKey: null,
+            displayField: null,
+            keyField: null,
+            loadType: 'cached'
          }
       },
 
       $constructor: function() {
+         this._context.setValueSelf(contextName+'/items',this._options.items);
+      },
+
+      init: function() {
+         TabControl.superclass.init.call(this);
+         this._tabButtons = this.getChildControlByName('TabButtons');
+         this._tabButtons.subscribe('onSelectedItemChange', this._onSelectedItemChange);
+         this._switchableArea = this.getChildControlByName('SwitchableArea');
+      },
+
+      _onSelectedItemChange: function() {
+         //console.log('onSelectedItemChange 2');
 
       }
    });
