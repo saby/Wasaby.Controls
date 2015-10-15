b269e15b8dd63a522d18a66fd096ac010791039f
aa.malcev2 <aleksey.a.maltsev@gmail.com>
2015-10-15 11:01:43 +0300

    в обычном дереве не приходили уведомления об изменениях

--- components/Data/Collection/LoadableTree.module.js
+++ components/Data/Collection/LoadableTree.module.js
@@ -2,18 +2,16 @@
 define('js!SBIS3.CONTROLS.Data.Collection.LoadableTree', [
    'js!SBIS3.CONTROLS.Data.Collection.Tree',
    'js!SBIS3.CONTROLS.Data.Collection.LoadableTreeChildren',
-   'js!SBIS3.CONTROLS.Data.Bind.ICollection',
    'js!SBIS3.CONTROLS.Data.Query.IQueryable',
    'js!SBIS3.CONTROLS.Data.Collection.ISourceLoadable',
    'js!SBIS3.CONTROLS.Data.Collection.LoadableTreeItemMixin'
-], function (Tree, LoadableTreeChildren, IBindCollection, IQueryable, ISourceLoadable, LoadableTreeItemMixin) {
+], function (Tree, LoadableTreeChildren, IQueryable, ISourceLoadable, LoadableTreeItemMixin) {
    'use strict';
 
    /**
     * Дерево, загружаемое через источник данных
     * @class SBIS3.CONTROLS.Data.Collection.LoadableTree
     * @extends SBIS3.CONTROLS.Data.Collection.Tree
-    * @mixes SBIS3.CONTROLS.Data.Bind.ICollection
     * @mixes SBIS3.CONTROLS.Data.Query.IQueryable
     * @mixes SBIS3.CONTROLS.Data.Collection.ISourceLoadable
     * @mixes SBIS3.CONTROLS.Data.Collection.LoadableTreeItemMixin
@@ -21,7 +19,7 @@ define('js!SBIS3.CONTROLS.Data.Collection.LoadableTree', [
     * @author Мальцев Алексей
     */
 
-   var LoadableTree = Tree.extend([IBindCollection, IQueryable, ISourceLoadable, LoadableTreeItemMixin], /** @lends SBIS3.CONTROLS.Data.Collection.LoadableTree.prototype */{
+   var LoadableTree = Tree.extend([IQueryable, ISourceLoadable, LoadableTreeItemMixin], /** @lends SBIS3.CONTROLS.Data.Collection.LoadableTree.prototype */{
       _moduleName: 'SBIS3.CONTROLS.Data.Collection.LoadableTree',
        $protected: {
           _options: {
--- components/Data/Collection/Tree.module.js
+++ components/Data/Collection/Tree.module.js
@@ -1,18 +1,20 @@
 /* global define, $ws */
 define('js!SBIS3.CONTROLS.Data.Collection.Tree', [
-   'js!SBIS3.CONTROLS.Data.Collection.TreeItem'
-], function (TreeItem) {
+   'js!SBIS3.CONTROLS.Data.Collection.TreeItem',
+   'js!SBIS3.CONTROLS.Data.Bind.ICollection'
+], function (TreeItem, IBindCollection) {
    'use strict';
 
    /**
     * Дерево.
     * @class SBIS3.CONTROLS.Data.Collection.Tree
     * @extends SBIS3.CONTROLS.Data.Collection.TreeItem
+    * @mixes SBIS3.CONTROLS.Data.Bind.ICollection
     * @public
     * @author Мальцев Алексей
     */
 
-   return TreeItem.extend(/** @lends SBIS3.CONTROLS.Data.Collection.Tree.prototype */{
+   return TreeItem.extend([IBindCollection], /** @lends SBIS3.CONTROLS.Data.Collection.Tree.prototype */{
       _moduleName: 'SBIS3.CONTROLS.Data.Collection.Tree',
 
       $constructor: function (cfg) {
