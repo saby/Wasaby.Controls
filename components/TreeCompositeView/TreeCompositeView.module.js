7fb2b22524d51be1dd92ecdde19c060c2c0e35fc
as.avramenko <as.avramenko@tensor.ru>
2015-11-02 16:01:25 +0300

    Зовём правильный метод (Илья каким то чудом затащил вызов неправильного метода) Ошибка в разработку от 02.11.2015 №1120910 Не отображается прайсовая цена в каталоге, если она была создана или изменена через карточку номе... https://inside.tensor.ru/opendoc.html?guid=c331f96d-5e9e-4b59-8a7c-54dfb9bd2304

--- components/TreeCompositeView/TreeCompositeView.module.js
+++ components/TreeCompositeView/TreeCompositeView.module.js
@@ -208,7 +208,7 @@ define('js!SBIS3.CONTROLS.TreeCompositeView', ['js!SBIS3.CONTROLS.TreeDataGridVi
                //Если запись найдена в обновленном DataSet, то перерисовываем её
                if (record) {
                   currentDataSet.getRecordByKey(row.key).merge(record);
-                  self.redrawItem(record);
+                  self.redrawRow(record);
                } else { //Иначе - удаляем запись
                   currentDataSet.removeRecord(row.key);
                   self.destroyFolderToolbar(row.key);
