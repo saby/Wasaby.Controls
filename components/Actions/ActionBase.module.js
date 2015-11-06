3069f406ce4923db18b751858de993fafb0a8088
kraynovdo <do.kraynov@tensor.ru>
2015-11-06 11:09:10 +0300

    базовый экшн

---
+++ components/Actions/ActionBase.module.js
@@ -0,0 +1,35 @@
+define('js!SBIS3.CONTROLS.ActionBase', ['js!SBIS3.CONTROLS.Control'], function(Control){
+   'use strict';
+
+   /**
+    * Класс базовый для всех стандартных действий, которые можно использовать в интерфейсе
+    * @class SBIS3.CONTROLS.ActionBase
+    * @public
+    * @extends $ws.proto.Control
+    * @author Крайнов Дмитрий Олегович
+    *
+    * @ignoreOptions validators independentContext contextRestriction extendedTooltip
+    *
+    * @ignoreMethods activateFirstControl activateLastControl addPendingOperation applyEmptyState applyState clearMark
+    * @ignoreMethods changeControlTabIndex destroyChild detectNextActiveChildControl disableActiveCtrl findParent
+    * @ignoreMethods focusCatch getActiveChildControl getChildControlById getChildControlByName getChildControls
+    * @ignoreMethods getClassName getContext getEventBusOf getEventHandlers getEvents getExtendedTooltip getOpener
+    * @ignoreMethods getImmediateChildControls getLinkedContext getNearestChildControlByName getOwner getOwnerId
+    * @ignoreMethods getReadyDeferred getStateKey getTabindex getUserData getValue hasActiveChildControl hasChildControlByName
+    * @ignoreMethods hasEventHandlers isActive isAllReady isDestroyed isMarked isReady makeOwnerName setOwner setSize
+    * @ignoreMethods markControl moveFocus moveToTop once registerChildControl registerDefaultButton saveToContext
+    * @ignoreMethods sendCommand setActive setChildActive setClassName setExtendedTooltip setOpener setStateKey activate
+    * @ignoreMethods setTabindex setTooltip setUserData setValidators setValue storeActiveChild subscribe unregisterChildControl
+    * @ignoreMethods unregisterDefaultButton unsubscribe validate waitAllPendingOperations waitChildControlById waitChildControlByName
+    *
+    * @ignoreEvents onActivate onAfterLoad onAfterShow onBeforeControlsLoad onBeforeLoad onBeforeShow onChange onClick
+    * @ignoreEvents onFocusIn onFocusOut onKeyPressed onReady onResize onStateChanged onTooltipContentRequest
+    */
+   //TODO наследуемся от контрола, чтоб можно было размещать в xhtml
+   var ActionBase = Control.Control.extend({
+      execute: function() {
+
+      }
+   });
+   return ActionBase;
+});
