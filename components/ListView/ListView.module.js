bcbc136c3a6deadf072952e183fd62b8128953b5
ganshinyao <gansin-yaroslav@yandex.ru>
2015-11-10 12:01:23 +0300

    Задача в разработку от 15.10.2015 №1088506 В новых DataGrid'ах не нашел возможность включить стандартные опции записи: редактировать, удалит... https://inside.tensor.ru/opendoc.html?guid=69718e51-2d23-48fc-94ad-7e6b6742ea4c

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -281,7 +281,7 @@ define('js!SBIS3.CONTROLS.ListView',
                   hoverMode: false, //todo EIP Сухоручкин: Переделать на mode: String, т.к. могут быть и другие виды
                   template: undefined,
                   onFieldChange: undefined
-               },
+               }
             }
          },
 
@@ -324,21 +324,6 @@ define('js!SBIS3.CONTROLS.ListView',
                this._container.bind('taphold', this._longTapHandler.bind(this));
                this._container.bind('tap', this._tapHandler.bind(this));
             }
-            this._options.itemsActions = [{
-               name: 'moveUp',
-                  icon: 'sprite:icon-16 icon-ArrowUp icon-error',
-                  tooltip: 'Удалить',
-                  caption: 'Удалить',
-                  isMainAction: true,
-                  onActivated: this.moveRecordUp
-               },{
-               name: 'moveDown',
-               icon: 'sprite:icon-16 icon-ArrowDown icon-error',
-               tooltip: 'Удалить',
-               caption: 'Удалить',
-               isMainAction: true,
-               onActivated:  this.moveRecordDown
-            }]
          },
          _keyboardHover: function (e) {
             var selectedKey = this.getSelectedKey();
