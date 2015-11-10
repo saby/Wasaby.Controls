ebe8e99cd8212f1a74cf174c845e9c0085f4b6e7
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2015-11-10 14:44:41 +0300

    рефакторинг
    Conflicts:
    	components/ListView/ListView.module.js
    	components/ListView/resources/EditInPlaceBaseController/EditInPlaceBaseController.module.js
    
    1. Перевел добавление по месту на команду
    2. Порефакторил демку (чтобы не прыгала)
    
    _

--- components/FieldLink/Demo/FieldLinkWithEditInPlace/FieldLinkWithEditInPlace.module.js
+++ components/FieldLink/Demo/FieldLinkWithEditInPlace/FieldLinkWithEditInPlace.module.js
@@ -23,15 +23,22 @@ define('js!SBIS3.CONTROLS.Demo.FieldLinkWithEditInPlace', [
    var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLS.Demo.FieldLinkWithEditInPlace.prototype */{
       _dotTplFn: dotTplFn,
       $protected: {
-         _options: {}
+         _dataGrid: null
+      },
+
+      $constructor: function() {
+         $ws.single.CommandDispatcher.declareCommand(this, 'newItem', this._newItem.bind(this));
       },
 
       init: function () {
-         var
-            dataGrid;
          moduleClass.superclass.init.call(this);
-         dataGrid = this.getChildControlByName('DemoDataGrid');
-         dataGrid.setDataSource(this._createDataGridSource());
+         this._dataGrid = this.getChildControlByName('DemoDataGrid');
+         this._dataGrid.setDataSource(this._createDataGridSource());
+         this._dataGrid.sendCommand('newItem');
+      },
+
+      _newItem: function() {
+         return this._dataGrid.sendCommand('newItem');
       },
 
       _createDataGridSource: function () {
