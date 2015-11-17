a71d604ed94111a9c20712011da9434cce1db3f1
aa.malcev2 <aleksey.a.maltsev@gmail.com>
2015-11-17 15:58:15 +0300

    Не убрал SBIS3.CONTROLS.Data.Bind.IProperty

--- components/Data/Collection/ObservableList.module.js
+++ components/Data/Collection/ObservableList.module.js
@@ -11,13 +11,12 @@ define('js!SBIS3.CONTROLS.Data.Collection.ObservableList', [
     * @class SBIS3.CONTROLS.Data.Collection.ObservableList
     * @extends SBIS3.CONTROLS.Data.Collection.List
     * @mixes SBIS3.CONTROLS.Data.Bind.ICollection
-    * @mixes SBIS3.CONTROLS.Data.Bind.IProperty
     * @mixes SBIS3.CONTROLS.Data.Collection.ObservableListMixin
     * @public
     * @author Мальцев Алексей
     */
 
-   var ObservableList = List.extend([IBindCollection, IBindProperty, ObservableListMixin], /** @lends SBIS3.CONTROLS.Data.Collection.ObservableList.prototype */{
+   var ObservableList = List.extend([IBindCollection, ObservableListMixin], /** @lends SBIS3.CONTROLS.Data.Collection.ObservableList.prototype */{
       _moduleName: 'SBIS3.CONTROLS.Data.Collection.ObservableList'
    });
 
