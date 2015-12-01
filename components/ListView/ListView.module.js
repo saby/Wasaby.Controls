f7d747f2dc431c5fede7e54a3ebf0814f85db5e1
Ilya <iv.cheremushkin@tensor.ru>
2015-12-01 18:08:52 +0300

    fix

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -329,8 +329,8 @@ define('js!SBIS3.CONTROLS.ListView',
          _keyboardHover: function (e) {
             var selectedKey = this.getSelectedKey(),
                selectedItem = $('[data-id="' + selectedKey + '"]', this._getItemsContainer()),
-               nextItem = this._getSiblingItem(selectedItem, 'next'),
-               previousItem = this._getSiblingItem(selectedItem, 'previous');
+               nextItem = this._getSiblingItem(selectedItem, true),
+               previousItem = this._getSiblingItem(selectedItem, false);
 
             switch (e.which) {
                case $ws._const.key.up:
@@ -351,12 +351,12 @@ define('js!SBIS3.CONTROLS.ListView',
          },
          // TODO Подумать, как решить данную проблему. Не надёжно хранить информацию в доме
          // Поиск следующего или предыдущего элемента коллекции с учётом вложенных контролов
-         _getSiblingItem: function(item, order){
+         _getSiblingItem: function(item, isNext){
             if (!item.length) return;
-            var sq = order == 'next' ? 1 : -1;
+            var order = isNext ? 1 : -1;
             var items = $('.controls-ListView__item', this._getItemsContainer()).not('.ws-hidden'),
-               siblingItem = items.eq(items.index(item) + sq);
-            return this._dataSet.getRecordByKey(siblingItem.data('id')) ? siblingItem : this._getSiblingItem(siblingItem, order);
+               siblingItem = items.eq(items.index(item) + order);
+            return this._dataSet.getRecordByKey(siblingItem.data('id')) ? siblingItem : this._getSiblingItem(siblingItem, isNext);
          },
          _checkTargetContainer: function (target) {
             return this._options.showPaging && this._pager && $.contains(this._pager.getContainer()[0], target[0]);
