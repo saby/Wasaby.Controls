1a99a4919e6d1a4a96c23b09eab530dff60d9ef5
aa.malcev2 <aleksey.a.maltsev@gmail.com>
2015-12-11 10:54:54 +0300

    вылечил демку с анимацией, переместил в отдельную папку

--- components/ListControl/Demo/AnimatedListView/AnimatedListView.module.js
+++ components/ListControl/Demo/AnimatedListView/AnimatedListView.module.js
@@ -1,8 +1,9 @@
 /* global define, console, doT, $ws, $ */
-define('js!SBIS3.CONTROLS.ListControlMixin.AnimatedListView', [
+define('js!SBIS3.CONTROLS.ListControl.Demo.AnimatedListView', [
    'js!SBIS3.CONTROLS.ListControl.ListView',
-   'css!SBIS3.CONTROLS.ListControl.AnimatedListView'
-], function (ListView) {
+   'html!SBIS3.CONTROLS.ListControl.Demo.AnimatedListView/AnimatedListItem',
+   'css!SBIS3.CONTROLS.ListControl.Demo.AnimatedListView'
+], function (ListView, ListItemContainerTemplate) {
    'use strict';
 
    /**
@@ -11,12 +12,14 @@ define('js!SBIS3.CONTROLS.ListControlMixin.AnimatedListView', [
     * @extends SBIS3.CONTROLS.ListControl.ListView
     * @author Крайнов Дмитрий Олегович
     */
-   var AnimatedListView = ListView.extend(/** @lends SBIS3.CONTROLS.ListControl.AnimatedListView.prototype */{
-      _moduleName: 'SBIS3.CONTROLS.ListControl.AnimatedListView',
+   var AnimatedListView = ListView.extend(/** @lends SBIS3.CONTROLS.ListControl.Demo.AnimatedListView.prototype */{
+      _moduleName: 'SBIS3.CONTROLS.ListControl.Demo.AnimatedListView',
       $protected: {
+         _itemContainerTemplate: ListItemContainerTemplate,
          _rootNodeСssClass: 'controls-ListView controls-ListView__animated',
          _rowHeight: 28,
-         _rowsCount: 0
+         _rowsCount: 0,
+         _removeTimeout: 500
       },
 
       //region SBIS3.CONTROLS.ListControl.IListView
@@ -26,35 +29,38 @@ define('js!SBIS3.CONTROLS.ListControlMixin.AnimatedListView', [
          var target = this._getTargetNode(item),
             template = this._getItemTemplate(item);
          this._shiftItemsPositions(target, at, 1);
-
-         var container = this._buildItemContainer(item, template, at)
+         var container = this._buildItemContainer(item, template)
             .addClass(this._сssPrefix + 'item__beforeInsert')
             .appendTo(target);
+         this._updateTargetNodeHeight(target);
          setTimeout((function() {
             container.removeClass(this._сssPrefix + 'item__beforeInsert');
          }).bind(this), 0);
-
-         this._updateTargetNodeHeight(target);
       },
 
-      removeItem: function (item, at) {
+      removeItem: function (item) {
          this._rowsCount--;
          var target = this._getTargetNode(item),
-            old = this._getItemContainer(target, at);
+            old = this._getItemContainer(target, item);
          if (old.length) {
             old.addClass(this._сssPrefix + 'item__afterDelete');
+            this._shiftItemsPositions(
+               target,
+               1 + this._getItemContainerIndex(old),
+               -1
+            );
+            this._updateTargetNodeHeight(target);
             setTimeout((function() {
                this._removeСomponents(old);
                old.remove();
-            }).bind(this), 500);
-            this._shiftItemsPositions(target, 1 + at, -1);
-            this._updateTargetNodeHeight(target);
+            }).bind(this), this._removeTimeout);
          }
       },
 
-      moveItem: function (item, from, to) {
+      moveItem: function (item, to) {
          var targetNode = this._getTargetNode(item),
-            container = this._getItemContainer(targetNode, from);
+            container = this._getItemContainer(targetNode, item),
+            from = this._getItemContainerIndex(container);
          if (from > to) {
             this._shiftItemsPositions(targetNode, to, 1, from - 1);
          } else if (from < to) {
@@ -74,15 +80,16 @@ define('js!SBIS3.CONTROLS.ListControlMixin.AnimatedListView', [
          var data = AnimatedListView.superclass._getRenderData.call(this, items);
 
          this._rowsCount = data.items.length;
-         data.rowHeight = this._rowHeight;
          data.height = this._rowsCount * this._rowHeight;
          return data;
       },
 
-      _getItemRenderData: function(item, index) {
-         var data = AnimatedListView.superclass._getItemRenderData.call(this, item, index);
-         data.index = index;
-         data.top = this._getItemContainerTop(index);
+      _getItemRenderData: function(item) {
+         var data = AnimatedListView.superclass._getItemRenderData.call(this, item);
+
+         data.index = item.getOwner().getIndex(item);
+         data.top = this._getItemContainerTop(data.index);
+         data.height = this._rowHeight;
          return data;
       },
 
@@ -90,34 +97,17 @@ define('js!SBIS3.CONTROLS.ListControlMixin.AnimatedListView', [
 
       //region DOM
 
-      _buildItemContainer: function (item, template, at) {
-         var container = AnimatedListView.superclass._buildItemContainer.call(this, item, template, at);
-         this._setItemContainerIndex(container, at);
-         this._updateItemContainerPosition(container);
-         return container;
-      },
-
-      _getItemContainerIndex: function(parent, domNode) {
-         return parseInt($(domNode).attr('data-index')) || 0;
-      },
-
-      _getItemContainer: function(parent, at) {
-         return parent.find('> .' + this._сssPrefix + this._itemContainerCssClass + '[data-index=' + at + ']');
-      },
-
       _updateTargetNodeHeight: function(container) {
          container.css({
             height: this._rowsCount * this._rowHeight
          });
       },
 
-      _setItemContainerIndex: function(container, index) {
-         container.attr('data-index', index);
-      },
-
       _updateItemContainerPosition: function(container) {
          container.css({
-            top: this._getItemContainerTop(container.attr('data-index'))
+            top: this._getItemContainerTop(
+               this._getItemContainerIndex(container)
+            )
          });
       },
 
@@ -125,15 +115,23 @@ define('js!SBIS3.CONTROLS.ListControlMixin.AnimatedListView', [
          var items = this._getItemsContainers(parent);
          for (var i = 0; i < items.length; i++) {
             var container = $(items[i]),
-               index = parseInt(container.attr('data-index')) || 0;
+               index = this._getItemContainerIndex(container);
             if (index >= fromIndex && (toIndex === undefined || index <= toIndex)) {
-               this._setItemContainerIndex(container, index+ offset);
+               this._setItemContainerIndex(container, index + offset);
                this._updateItemContainerPosition(container);
             }
          }
       },
 
       //endregion DOM
+
+      _getItemContainerIndex: function(container) {
+         return parseInt(container.attr('data-index'), 10) || 0;
+      },
+
+      _setItemContainerIndex: function(container, index) {
+         container.attr('data-index', index);
+      },
 
       _getItemContainerTop: function (index) {
          return parseInt(index) * this._rowHeight;
