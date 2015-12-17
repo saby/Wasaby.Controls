30f291447f585f46896a97754ec817f909054f2e
is.protasov <is.protasov@tensor.ru>
2015-12-17 23:05:48 +0300

    + Ошибка в разработку от 14.12.2015 №1204593 Задачи\На мне. В правой стороне реестра лишняя синяя полоса(скрин1) https://inside.tensor.ru/opendoc.html?guid=d01cb9d2-0d6f-482a-9ac9-08e6622c5fb0

--- components/TabControl/TabControl.module.js
+++ components/TabControl/TabControl.module.js
@@ -14,6 +14,7 @@ define('js!SBIS3.CONTROLS.TabControl', [
     * В каждый момент времени отображается только одна область. Отображаемая область может переключаться при клике на корешки закладок.
     * @class SBIS3.CONTROLS.TabControl
     * @extends SBIS3.CORE.CompoundControl
+    * @control
     * @author Крайнов Дмитрий Олегович
     */
 
@@ -23,9 +24,29 @@ define('js!SBIS3.CONTROLS.TabControl', [
          _tabButtons: null,
          _switchableArea: null,
          _options: {
+            /**
+             * @cfg {String} Ключ выбранной записи.
+             * @see keyField
+             */
             selectedKey: null,
-            displayField: null,
+            /**
+             * @cfg {String} Наименование поля из которого будет браться содержимое вкладки (корешка)
+             */
+            tabsDisplayField: null,
+            /**
+             * @cfg {String} Название поля-идентификатора записи. Например 'id'
+             * @see selectedKey
+             */
             keyField: null,
+            /**
+             * @cfg {String} Режим загрузки дочерних контролов в области под вкладками
+             * @example
+             * <pre>
+             *     <option name="loadType">all</option>
+             * </pre>
+             * @variant all инстанцировать все области сразу;
+             * @variant cached инстанцировать только 1 область, при смене предыдущую не уничтожать (кэширование областей).
+             */
             loadType: 'cached'
          }
       },
@@ -38,6 +59,7 @@ define('js!SBIS3.CONTROLS.TabControl', [
       init: function() {
          TabControl.superclass.init.call(this);
          this._switchableArea = this.getChildControlByName('SwitchableArea');
+         this._switchableArea.setActiveArea(this._options.selectedKey);
          this._tabButtons = this.getChildControlByName('TabButtons');
          this._tabButtons.subscribe('onSelectedItemChange', this._onSelectedItemChange.bind(this));
       },
