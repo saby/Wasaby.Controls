bec713115c90a6efd6f9c72e40d67274aac9302d
ganshinyao <gansin-yaroslav@yandex.ru>
2015-12-17 11:42:47 +0300

    Ошибка в define dataset is undefined

--- components/Data/Collection/RecordSet.module.js
+++ components/Data/Collection/RecordSet.module.js
@@ -1,7 +1,7 @@
 /* global define, $ws */
 define('js!SBIS3.CONTROLS.Data.Collection.RecordSet', [
    'js!SBIS3.CONTROLS.Data.Collection.ObservableList',
-   'js!SBIS3.CONTROLS.Data.Collection.RecordSet',
+   'js!SBIS3.CONTROLS.DataSet',
    'js!SBIS3.CONTROLS.Data.Model'
 ], function (ObservableList, DataSet) {
    'use strict';
