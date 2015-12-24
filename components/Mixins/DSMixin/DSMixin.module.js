e80abaab03b1fe0463fcfe928dd6ebf91e8bd9af
ad.Chistyakova <a.d.chistyakova@gmail.com>
2015-12-24 11:45:48 +0300

    Сразу строим шаблон footer'a в верстке Задача в разработку от 08.12.2015 №1195904 Доработать SBIS3.CONTROLS.DropdownList. Нужно добавить возможность отрисовки области под списком ... https://inside.tensor.ru/opendoc.html?guid=c63bf2f4-b0d7-4fed-9619-17fae5bb2aa2
    Conflicts:
    	components/DataGridControl/DataGridView.xhtml

--- components/Mixins/DSMixin/DSMixin.module.js
+++ components/Mixins/DSMixin/DSMixin.module.js
@@ -366,6 +366,14 @@ define('js!SBIS3.CONTROLS.DSMixin', [
          this.unsubscribeFrom(this._itemsProjection, 'onCollectionChange', this._onCollectionChange);
          this.unsubscribeFrom(this._itemsProjection, 'onCollectionItemChange', this._onCollectionItemChange);
       },
+      _modifyOptions: function(opts){
+         var tpl = opts.footerTpl;
+         //Если нам передали шаблон как строку вида !html, то нужно из нее сделать функцию
+         if (tpl && typeof tpl === 'string' && tpl.match(/^html!/)){
+            opts.footerTpl = require(tpl);
+         }
+         return opts;
+      },
        /**
         * Метод установки источника данных.
         * @remark
@@ -688,19 +696,12 @@ define('js!SBIS3.CONTROLS.DSMixin', [
                   curAt.at++;
                }
             }
-            if (this._options.footerTpl) {
-               targetContainer = this._getFooterContainer();
-               this._appendItemTemplate(undefined, targetContainer,  this._buildTplItem({}, this._options.footerTpl));
-            }
             this.reviveComponents().addCallback(this._notifyOnDrawItems.bind(this)).addErrback(function(e){
                throw e;
             });
          } else {
             this._notifyOnDrawItems();
          }
-      },
-      _getFooterContainer: function(){
-         return this._getTargetContainer();
       },
 
       /**
