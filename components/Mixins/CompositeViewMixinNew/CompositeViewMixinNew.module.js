ef653c09c00fd5923a407ea4ced1c72c8c843331
cheremushkin ilya <iv.cheremushkin@tensor.ru>
2015-12-24 17:41:13 +0300

    CompositeViewMixin

---
+++ components/Mixins/CompositeViewMixinNew/CompositeViewMixinNew.module.js
@@ -0,0 +1,82 @@
+define('js!SBIS3.CONTROLS.CompositeViewMixinNew', ['html!SBIS3.CONTROLS.DataGridControl.DataGridView', 'html!SBIS3.CONTROLS.ListControl.View'], function(DataGridViewTemplate, ListViewTemplate) {
+   'use strict';
+   /**
+    * Позволяет контролу отображать данные в виде плитки/списка/таблицы
+    * @mixin SBIS3.CONTROLS.CompositeViewMixin
+    * @public
+    */
+   var CompositeViewMixin = {
+      $protected: {
+         _tileWidth: null,
+         _folderWidth: null,
+         _options: {
+            /**
+             * @cfg {Object} Режим отображения
+             * @variant table таблица
+             * @variant list список
+             */
+            viewMode: 'table',
+
+            imageField: null,
+            listTemplate: null
+         }
+      },
+
+      $constructor: function() {
+
+      },
+
+      setViewMode: function(mode) {
+         this._options.viewMode = mode;
+         this._drawViewMode(mode);
+      },
+
+      getViewMode: function() {
+         return this._options.viewMode;
+      },
+
+      _getItemTemplateSelector: function(){
+         var self = this;
+         return function(){
+            var template;
+            switch(self.getViewMode()) {
+               case 'list': 
+                  template = self._options.listTemplate;
+                  break;
+               case 'table':
+                  template = self._options.itemTemplate;
+                  break;
+               case 'tile':
+                  template = self._options.tileTemplate;
+            } 
+            return template;
+         };
+      },
+
+      _drawViewMode: function(mode){
+         this._container.toggleClass('controls-CompositeView-table', mode == 'table')
+                        .toggleClass('controls-CompositeView-list', mode == 'list')
+                        .toggleClass('controls-CompositeView-tile', mode == 'tile');
+         
+         if (mode == 'table'){
+            this._getView().setTemplate(DataGridViewTemplate);
+         } else {
+            this._getView().setTemplate(ListViewTemplate);
+         }
+      },
+
+      around: {
+         _getViewTemplate: function (parentFunc) {
+            if (this._options.viewMode == 'table'){
+               return parentFunc.call(this);
+            } else {
+               return ListViewTemplate;
+            }
+         }
+      }
+
+   };
+
+   return CompositeViewMixin;
+
+});
