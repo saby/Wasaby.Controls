06f1721e828c3b8cde1a50e196f133d04390ffe5
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2015-12-25 10:05:01 +0300

    Ошибка в разработку от 22.12.2015 №1221641 Интеграционное тестирование CONTROLS - TreeDataGridView: после отбора записи через фильтр в диало... https://inside.tensor.ru/opendoc.html?guid=520615fe-0049-4887-8129-dac516a67da9

--- components/OperationsPanel/resources/OperationMove/resources/MoveDialog.module.js
+++ components/OperationsPanel/resources/OperationMove/resources/MoveDialog.module.js
@@ -27,7 +27,7 @@ define('js!SBIS3.CONTROLS.MoveDialog', [
             self = this,
             linkedView = this._options.linkedView,
             selectedCount = this._options.records.length,
-            filter;
+            filter = {};
          this.setTitle('Перенести ' + selectedCount + ' запис' + $ws.helpers.wordCaseByNumber(selectedCount, 'ей', 'ь', 'и') + ' в');
          this.getChildControlByName('MoveDialogTemplate-moveButton')
             .subscribe('onActivated', this._onMoveButtonActivated.bind(this));
@@ -38,14 +38,9 @@ define('js!SBIS3.CONTROLS.MoveDialog', [
          this._treeView.subscribe('onDrawItems', function() {
             self._createRoot();
          });
-         filter = $ws.core.clone(linkedView.getFilter());
-         //Чтобы получить всю выборку папок, проставим в поле иерархии null
-         filter[linkedView._options.hierField] = null;
-         if ($ws.helpers.instanceOfModule(linkedView._dataSource, 'SBIS3.CONTROLS.SbisServiceSource')) {
-            filter['ВидДерева'] = "Только узлы";
-            //TODO: костыль написан специально для нуменклатуры, чтобы не возвращалась выборка всех элементов при заходе в пустую папку
-            filter['folderChanged'] = true;
-         }
+         filter['ВидДерева'] = "Только узлы";
+         //TODO: костыль написан специально для нуменклатуры, чтобы не возвращалась выборка всех элементов при заходе в пустую папку
+         filter['folderChanged'] = true;
          this._treeView.setFilter(filter, true);
          this._treeView.setDataSource(linkedView._dataSource);
       },
