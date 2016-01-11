a2e8ca158fb0c64f9b49a3c37b5e9b4118fa77df
ad.Chistyakova <a.d.chistyakova@gmail.com>
2016-01-11 13:47:45 +0300

    Если бесконечный скролл был установлен в опции - вернем его при сбрасывании из поиска Ошибка в разработку от 23.12.2015 №1223574 Не происходит переход в пустой раздел каталога, не скроллируется каталог после выполнения такого ... https://inside.tensor.ru/opendoc.html?guid=90d41743-7558-4e94-9ceb-60681f817c4b

--- components/HierarchyDataGridView/HierarchyDataGridView.module.js
+++ components/HierarchyDataGridView/HierarchyDataGridView.module.js
@@ -206,6 +206,8 @@ define('js!SBIS3.CONTROLS.HierarchyDataGridView', [
                   if (self._options.groupBy.field) {
                      filter[self._options.groupBy.field] = undefined;
                   }
+                  //Если бесконечный скролл был установлен в опции - вернем его
+                  self.setInfiniteScroll(self._options.infiniteScroll, true);
                   self.setGroupBy({});
                   self.setHighlightText('', false);
                   self.setFilter(filter, true);
