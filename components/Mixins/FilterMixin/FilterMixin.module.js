f753c0205d4739d8d8af31b673918d3d7b192e72
aa.shipin <aa.shipin@tensor.ru>
2016-01-19 19:10:23 +0300

    правки после code review
    Ошибка в разработку от 07.12.2015 №1191351
    SBIS3.CONTROLS.FilterMixin: неправильное документирование - filterStructure - опция не попадает в...
    https://inside.tensor.ru/opendoc.html?guid=59dd0d2b-52c4-4c9f-96db-38491a2b90ec

--- components/Mixins/FilterMixin/FilterMixin.module.js
+++ components/Mixins/FilterMixin/FilterMixin.module.js
@@ -41,16 +41,17 @@ define('js!SBIS3.CONTROLS.FilterMixin', [
          _options: {
             /**
              * @typedef {Array} filterStructure
-             * @property {String} internalValueField Индекс поля, содержащего значение элемента. По-умолчанию null.
-             * @property {String} internalCaptionField Индекс поля, содержащего подпись элемента. По-умолчанию null.
-             * @property {Object} caption Текущая подпись элемента. Может быть не определено.
+             * @property {String} internalValueField Название поля, которое хранит значение элемента. По-умолчанию null.
+             * @property {String} internalCaptionField Название поля, которое хранит текстовое отображение значения. По-умолчанию null.
+             * @property {String} caption Текущее текстовое отображение значения. Может быть не определено.
              * @property {Object} value Текущее значение элемента. Может быть не определено.
-             * @property {Object} resetValue Значение элемента по-умолчанию, используемое для сброса текущего значения. Может быть не определено.
-             * @property {Object} resetCaption Подпись элемента по-умолчанию, использаемая для сброса текущей подписи. Может быть не определено.
+             * @property {Object} resetValue Значение поля при сбрасывании фильтра, или при пустом значении в value. Может быть не определено.
+             * @property {String} resetCaption Текст по умолчанию. Если задали, то при пустом (или заданном в resetValue) значении будет
+             * отображаться заданный здесь текст. Может быть не определено.
              */
             /**
              * @cfg {filterStructure[]} Структура элемента фильтра
-             * @remark при установке структуры меняется значение св-ва filter (строится по полям value у структуры)
+             * @remark Важно! все, что задано в filterStructure влияет на объекты в контексе - filter и filterDescr(строится по полям value у структуры)
              */
             filterStructure: [ /*filterStructureElementDef*/ ],
             /**
