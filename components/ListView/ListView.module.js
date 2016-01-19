97c0eab875f58290181c714b9bb2e2d781f62ab0
as.krasilnikov <as.krasilnikov@tensor.ru>
2016-01-19 13:15:47 +0300

    Строка Итогов Задача в разработку от 22.12.2015 №1221697 В новом контроле TreeCompositeView сделать отображение строки итогов и сделать возможность отренд... https://inside.tensor.ru/opendoc.html?guid=2ca75e92-70c3-4625-b1b1-26fb96822693

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -13,7 +13,9 @@ define('js!SBIS3.CONTROLS.ListView',
       'js!SBIS3.CONTROLS.DecorableMixin',
       'js!SBIS3.CONTROLS.DragNDropMixin',
       'js!SBIS3.CONTROLS.ItemActionsGroup',
+      'js!SBIS3.CORE.MarkupTransformer',
       'html!SBIS3.CONTROLS.ListView',
+      'js!SBIS3.CONTROLS.Utils.TemplateUtil',
       'js!SBIS3.CONTROLS.CommonHandlers',
       'js!SBIS3.CONTROLS.MoveHandlers',
       'js!SBIS3.CONTROLS.Pager',
@@ -24,7 +26,7 @@ define('js!SBIS3.CONTROLS.ListView',
       'is!browser?html!SBIS3.CONTROLS.ListView/resources/emptyData',
       'is!browser?js!SBIS3.CONTROLS.ListView/resources/SwipeHandlers'
    ],
-   function (CompoundControl, CompoundActiveFixMixin, DSMixin, MultiSelectable, Selectable, DataBindMixin, DecorableMixin, DragNDropMixin, ItemActionsGroup, dotTplFn, CommonHandlers, MoveHandlers, Pager, EditInPlaceHoverController, EditInPlaceClickController, Link, groupByTpl, emptyDataTpl) {
+   function (CompoundControl, CompoundActiveFixMixin, DSMixin, MultiSelectable, Selectable, DataBindMixin, DecorableMixin, DragNDropMixin, ItemActionsGroup, MarkupTransformer, dotTplFn, TemplateUtil, CommonHandlers, MoveHandlers, Pager, EditInPlaceHoverController, EditInPlaceClickController, Link, groupByTpl, emptyDataTpl) {
 
       'use strict';
 
@@ -196,6 +198,7 @@ define('js!SBIS3.CONTROLS.ListView',
                model: null
             },
             _emptyData: undefined,
+            _isDrawResults: false,
             _options: {
                /**
                 * @cfg {Boolean} Разрешить отсутствие выбранного элемента
@@ -377,7 +380,23 @@ define('js!SBIS3.CONTROLS.ListView',
                 *     </opt>
                 * </pre>
                 */
-               editingTemplate: undefined
+               editingTemplate: undefined,
+               /**
+                * @cfg {String} Позиция отображения строки итогов
+                * Данная опция позволяет отображать строку итогов в случае отсутствия записей.
+                * Возможные значения:
+                * <ol>
+                *    <li>'none' - Не отображать строку итогов</li>
+                *    <li>'top' - вверху</li>
+                *    <li>'bottom' - внизу</li>
+                * </ol>
+                */
+               resultsPosition: 'none',
+               /**
+                * @cfg {String} Заголовок строки итогов
+                */
+               resultsText : 'Итого',
+               resultsTpl: undefined
             }
          },
 
@@ -413,6 +432,7 @@ define('js!SBIS3.CONTROLS.ListView',
             $ws.single.CommandDispatcher.declareCommand(this, 'beginEdit', this._beginEdit);
             $ws.single.CommandDispatcher.declareCommand(this, 'cancelEdit', this._cancelEdit);
             $ws.single.CommandDispatcher.declareCommand(this, 'commitEdit', this._commitEdit);
+            this.subscribe('onDrawItems', this._setResults);
          },
 
          init: function () {
@@ -1678,6 +1698,33 @@ define('js!SBIS3.CONTROLS.ListView',
             if (this.getItemsActions() && hoveredItem.container) {
                this._showItemActions(hoveredItem);
             }
+         },
+         _setResults: function(){
+            if (!this._checkResults()){
+               return;
+            }
+            var resultsDS = this.getDataSet().getMetaData().results,
+               resultRow = this._getResultsContainer(resultsDS);
+            this._drawResults(this._getItemsContainer(), resultRow);
+         },
+         _checkResults: function(){
+            return this._options.resultsPosition !== 'none' && this.getDataSet().getCount();
+         },
+         _getResultsContainer: function(resultsData){
+            return MarkupTransformer(TemplateUtil.prepareTemplate(this._options.resultsTpl)({
+               results: resultsData
+            }));
+         },
+         _drawResults: function(container, resultRow){
+            if (!resultRow){
+               return;
+            }
+            var position = this._options.resultsPosition == 'top' ? 'prepend' : 'append';
+            if (this._isDrawResults){
+               $('.controls-DataGridView__results', container).remove();
+            }
+            this._isDrawResults = true;
+            $(container)[position](resultRow);
          }
          /*DRAG_AND_DROP END*/
       });
