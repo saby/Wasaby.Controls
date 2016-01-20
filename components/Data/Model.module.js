4eb7f30881e9f8f937eb43dcf41baefd4900db18
aa.malcev2 <aleksey.a.maltsev@gmail.com>
2016-01-20 18:29:59 +0300

    при использовании Adapter.Sbis Model::merge() падал с ошибкой, если донор содержал больше полей, чем принимающая модель.

--- components/Data/Model.module.js
+++ components/Data/Model.module.js
@@ -211,17 +211,19 @@ define('js!SBIS3.CONTROLS.Data.Model', [
             $ws.single.ioc.resolve('ILogger').error('SBIS3.CONTROLS.Data.Model::set()', 'Property name is empty');
          }
 
-         if (!this.has(name)) {
-            this._addProperty(name);
-         }
-
          var property = this._options.properties[name];
          if (property && property.set) {
             value = this._getCalculatedValue(name, value, property, false);
+            if (value === undefined) {
+               return;
+            }
          }
 
          if (this._getOriginalPropertyValue(name) !== value) {
             this._setOriginalPropertyValue(name, value);
+            if (!this.has(name)) {
+               this._addProperty(name);
+            }
             this._setChanged(true);
             if (name in this._propertiesCache &&
                value !== this._propertiesCache[name]
@@ -356,7 +358,13 @@ define('js!SBIS3.CONTROLS.Data.Model', [
        */
       merge: function (model) {
          model.each(function(field, value) {
-            this.set(field, value);
+            try {
+               this.set(field, value);
+            } catch (e) {
+               if (!(e instanceof ReferenceError)) {
+                  throw e;
+               }
+            }
          }, this);
          this._isStored = this._isStored || model._isStored;
          this._isDeleted = this._isDeleted || model._isDeleted;
