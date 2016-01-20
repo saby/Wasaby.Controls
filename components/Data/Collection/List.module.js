d0d3c02dd85924382ffa03eb711853952d120bba
ganshinyao <gansin-yaroslav@yandex.ru>
2016-01-20 19:54:28 +0300

    Ошибка в разработку от 19.01.2016 №1172540810 ФЛ: не работает листинг вложений 'Влево\вправо' в фулскрине. Так же, если мы смотрим первое\после... https://inside.tensor.ru/opendoc.html?guid=42a1eda4-cab6-4395-85f1-616f189ab18d

--- components/Data/Collection/List.module.js
+++ components/Data/Collection/List.module.js
@@ -39,7 +39,12 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
          /**
           * @var {SBIS3.CONTROLS.Data.Collection.ArrayEnumerator} Служебный энумератор
           */
-         _serviceEnumerator: undefined
+         _serviceEnumerator: undefined,
+         /**
+          * @var {SBIS3.CONTROLS.Data.Collection._hashIndex} Индекс хешей элементов
+          */
+         _hashIndex: undefined
+
       },
 
       $constructor: function (cfg) {
@@ -108,7 +113,7 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
             Array.prototype.splice.apply(this._items, [this._items.length, 0].concat(items));
          }
 
-         this._getServiceEnumerator().reIndex();
+         this._reindex();
       },
 
       toArray: function () {
@@ -130,7 +135,7 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
             Array.prototype.splice.apply(this._items, [0, 0].concat(isArray ? instead : instead.toArray()));
          }
 
-         this._getServiceEnumerator().reIndex();
+         this._reindex();
       },
 
       add: function (item, at) {
@@ -144,7 +149,7 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
             this._items.splice(at, 0, item);
          }
 
-         this._getServiceEnumerator().reIndex();
+         this._reindex();
       },
 
       at: function (index) {
@@ -161,7 +166,7 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
          }
          this._items.splice(index, 1);
 
-         this._getServiceEnumerator().reIndex();
+         this._reindex();
       },
 
       replace: function (item, at) {
@@ -170,12 +175,12 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
          }
          this._items[at] = item;
 
-         this._getServiceEnumerator().reIndex();
+         this._reindex();
       },
 
       getIndex: function (item) {
          if ($ws.helpers.instanceOfMixin(item, 'SBIS3.CONTROLS.Data.IHashable')) {
-            return this.getItemIndexByPropertyValue('hash', item.getHash());
+            return this._getItemIndexByHash(item.getHash());
          }
 
          return Array.indexOf(this._items, item);
@@ -248,6 +253,28 @@ define('js!SBIS3.CONTROLS.Data.Collection.List', [
        */
       _isValidIndex: function (index) {
          return index >= 0 && index < this.getCount();
+      },
+
+      _getItemIndexByHash: function (hash) {
+         if (typeof this._hashIndex === 'undefined') {
+            this._createHashIndex();
+         }
+         return this._hashIndex.hasOwnProperty(hash) ? this._hashIndex[hash] : -1;
+      },
+
+      _createHashIndex: function () {
+         var self = this;
+         self._hashIndex = {};
+         this.each(function (item, position) {
+            if ($ws.helpers.instanceOfMixin(item, 'SBIS3.CONTROLS.Data.IHashable')) {
+               self._hashIndex[item.getHash()] = position;
+            }
+         });
+      },
+
+      _reindex: function () {
+         this._hashIndex = {};
+         this._getServiceEnumerator().reIndex();
       }
 
       //endregion Protected methods
