7825feeda96760f942c855b08e6223bc5e7fee2d
kraynovdo <do.kraynov@tensor.ru>
2016-01-22 15:19:41 +0300

    Не звался родительский метод

--- components/Mixins/DSMixin/DSMixin.module.js
+++ components/Mixins/DSMixin/DSMixin.module.js
@@ -362,13 +362,15 @@ define('js!SBIS3.CONTROLS.DSMixin', [
          this.unsubscribeFrom(this._itemsProjection, 'onCollectionChange', this._onCollectionChange);
          this.unsubscribeFrom(this._itemsProjection, 'onCollectionItemChange', this._onCollectionItemChange);
       },
-      _modifyOptions: function(opts){
-         var tpl = opts.footerTpl;
-         //Если нам передали шаблон как строку вида !html, то нужно из нее сделать функцию
-         if (tpl && typeof tpl === 'string' && tpl.match(/^html!/)){
-            opts.footerTpl = require(tpl);
+      after : {
+         _modifyOptions: function (opts) {
+            var tpl = opts.footerTpl;
+            //Если нам передали шаблон как строку вида !html, то нужно из нее сделать функцию
+            if (tpl && typeof tpl === 'string' && tpl.match(/^html!/)) {
+               opts.footerTpl = require(tpl);
+            }
+            return opts;
          }
-         return opts;
       },
        /**
         * Метод установки источника данных.
