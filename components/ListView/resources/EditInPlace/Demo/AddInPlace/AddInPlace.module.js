96ccc671278c20eeb55a460dc59f24f194632460
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2016-01-26 16:37:33 +0300

    Задача в разработку от 10.01.2016 №1240245 Сделать на новом датагриде кнопку +Наименование. По нажатию должна создаваться новая запись и дол... https://inside.tensor.ru/opendoc.html?guid=252050ff-2f31-4428-9e1e-da851044b130

---
+++ components/ListView/resources/EditInPlace/Demo/AddInPlace/AddInPlace.module.js
@@ -0,0 +1,49 @@
+define('js!SBIS3.CONTROLS.DEMO.AddInPlace',
+   [
+      'js!SBIS3.CORE.CompoundControl', 
+      'html!SBIS3.CONTROLS.DEMO.AddInPlace',
+      'js!SBIS3.CONTROLS.Data.Source.SbisService',
+      'js!SBIS3.CONTROLS.DataGridView', 
+      'js!SBIS3.CONTROLS.Button',
+      'js!SBIS3.CONTROLS.TextBox',
+      'js!SBIS3.CONTROLS.NumberTextBox'
+   ], 
+   function(CompoundControl, dotTplFn, SbisService) {
+   /**
+    * SBIS3.DOCS.AddInPlace
+    * @class SBIS3.DOCS.AddInPlace
+    * @extends $ws.proto.CompoundControl
+    * @control
+    * @public
+    */
+   var moduleClass = CompoundControl.extend(/** @lends SBIS3.DOCS.AddInPlace.prototype */{
+      _dotTplFn: dotTplFn,
+      $protected: {
+         _options: {
+            
+         }
+      },
+      $constructor: function() {
+      },
+
+      init: function() {
+         moduleClass.superclass.init.call(this);
+         
+         var view = this.getChildControlByName('ДобавлениеПоМесту'),
+             // инициализируем источник данных БЛ
+             dataSource = new SbisService({
+                 service: 'Ноутбуки',
+                 formatMethodName: 'Создать'
+             });
+         
+         view.setDataSource(dataSource); // устанавливаем источник данных для таблицы
+         
+         // подписываем кнопку на добавление новой записи представлению данных
+         this.getChildControlByName('СоздатьЗапись').subscribe('onActivated', function(){
+            view.sendCommand('beginAdd');
+         }.bind(this));
+      }     
+   });
+
+   return moduleClass;
+});
