55434a1ba50c84fb1d706fa41763cf8efb160ed0
as.krasilnikov <as.krasilnikov@tensor.ru>
2016-01-28 17:15:50 +0300

    дестроим контролы внутри строки результатов при перерисовке

--- components/Mixins/DSMixin/DSMixin.module.js
+++ components/Mixins/DSMixin/DSMixin.module.js
@@ -649,18 +649,21 @@ define('js!SBIS3.CONTROLS.DSMixin', [
          if (container.length){
             var itemsContainers = $('.controls-ListView__item, .controls-GroupBy', container.get(0));
             /*Удаляем вложенные компоненты*/
-            $('[data-component]', itemsContainers).each(function (i, item) {
-               var inst = $(item).wsControl();
-               if (inst) {
-                  inst.destroy();
-               }
-            });
+            this._destroyControls(itemsContainers);
 
             /*Удаляем сами items*/
             itemsContainers.remove();
          }
       },
 
+      _destroyControls: function(container){
+         $('[data-component]', container).each(function (i, item) {
+            var inst = $(item).wsControl();
+            if (inst) {
+               inst.destroy();
+            }
+         });
+      },
       //метод определяющий в какой контейнер разместить определенный элемент
       _getTargetContainer: function (item) {
          //по стандарту все строки рисуются в itemsContainer
