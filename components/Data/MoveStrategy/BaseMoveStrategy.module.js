1f5ed43f2079576ce8cde9bd670d218be5fc85a7
ganshinyao <gansin-yaroslav@yandex.ru>
2016-01-28 11:38:35 +0300

    Ошибка в разработку от 28.01.2016 №1172560857 Стандартной сигнатурой метода удаления является метод, который принимает на вход массив идентифик... https://inside.tensor.ru/opendoc.html?guid=c69d7362-3e7a-4c84-9313-fb81abc10c61

--- components/Data/MoveStrategy/BaseMoveStrategy.module.js
+++ components/Data/MoveStrategy/BaseMoveStrategy.module.js
@@ -2,6 +2,6 @@ define('js!SBIS3.CONTROLS.Data.BaseMoveStrategy', [
    'js!SBIS3.CONTROLS.Data.MoveStrategy.Base'
 ], function (Base) {
    'use strict';
-   $ws.single.ioc.resolve('ILogger').error('DataSet', 'Module SBIS3.CONTROLS.Data.BaseMoveStrategy was renaim in 3.7.3.100. Use SBIS3.CONTROLS.Data.MoveStrategy.Base instead');
+   $ws.single.ioc.resolve('ILogger').error('BaseMoveStrategy', 'Module SBIS3.CONTROLS.Data.SbisMoveStrategy was renaim in 3.7.3.100. Please use SBIS3.CONTROLS.Data.MoveStrategy.Sbis.');
    return Base;
 });
--- components/Data/MoveStrategy/SbisMoveStrategy.module.js
+++ components/Data/MoveStrategy/SbisMoveStrategy.module.js
@@ -2,6 +2,6 @@ define('js!SBIS3.CONTROLS.Data.SbisMoveStrategy', [
    'js!SBIS3.CONTROLS.Data.MoveStrategy.Sbis'
 ], function (Sbis) {
    'use strict';
-   $ws.single.ioc.resolve('ILogger').error('DataSet', 'Module SBIS3.CONTROLS.Data.SbisMoveStrategy was renaim in 3.7.3.100. Use SBIS3.CONTROLS.Data.MoveStrategy.Sbis instead');
+   $ws.single.ioc.resolve('ILogger').log('SbisMoveStrategy', 'Module SBIS3.CONTROLS.Data.SbisMoveStrategy was renaim in 3.7.3.100. Please use SBIS3.CONTROLS.Data.MoveStrategy.Sbis.');
    return Sbis;
 });
