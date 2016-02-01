4484dc323925e270b202e483fe00605e5ee80b92
kraynovdo <do.kraynov@tensor.ru>
2016-02-01 12:49:18 +0300

    Ошибка в разработку от 01.02.2016 №1172568678 Не работает смена шаблона у TreeViewDS
    test-online.sbis.ru
    devyatov / devyatov123
    
    Документы -...
    https://inside.tensor.ru/opendoc.html?guid=5bd3d338-b705-427d-8c6c-174c9d1e7798

--- components/Mixins/DSMixin/DSMixin.module.js
+++ components/Mixins/DSMixin/DSMixin.module.js
@@ -609,7 +609,12 @@ define('js!SBIS3.CONTROLS.DSMixin', [
       _drawItemsCallback: function () {
          /*Method must be implemented*/
       },
-
+      /**
+       * Метод перерисвоки списка без повторного получения данных
+       */
+      redraw: function() {
+         this._redraw();
+      },
       _redraw: function () {
          var records;
 
