b15ad312742a2f1508f317eff7c21619ae405047
Cheremushkin Ilya <ccherya@gmail.com>
2016-02-01 15:40:05 +0300

    если поле пустое, не должна валиться ошибка

--- components/NumberTextBox/NumberTextBox.module.js
+++ components/NumberTextBox/NumberTextBox.module.js
@@ -237,7 +237,7 @@ define('js!SBIS3.CONTROLS.NumberTextBox', ['js!SBIS3.CONTROLS.TextBox', 'html!SB
             this._options.onlyPositive,
             this._options.maxLength
          );
-         if (this._options.hideEmptyDecimals && !(value.indexOf('.') == -1) && fromFocusOut ){
+         if (value && this._options.hideEmptyDecimals && !(value.indexOf('.') == -1) && fromFocusOut ){
             while (value[value.length - 1] == '0'){
                value = value.substr(0, value.length - 1);
             }
