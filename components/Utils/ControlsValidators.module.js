9d518a6eb214f62246fc3764b4e4d2a441ddcc1a
am.gerasimov <gersasha@Yandex.ru>
2016-02-01 16:50:58 +0300

    Поправил отображение операций над записью по перевода DSmixin'a на проекции
    Ошибка в разработку от 01.02.2016 №1172569438
    Platforma_3.7.3.100_integration_test_CONTROLS_ff - Не открывается меню действий над записью по хо...
    https://inside.tensor.ru/opendoc.html?guid=99474ab7-c666-4911-bcf4-8a27c7c1f65c

---
+++ components/Utils/ControlsValidators.module.js
@@ -0,0 +1,43 @@
+/**
+ * Created by am.gerasimov on 01.02.2016.
+ */
+define('js!SBIS3.CONTROLS.ControlsValidators', [],function() {
+
+   'use strict';
+
+   return  {
+      required: function(option) {
+         var isEmpty;
+
+         switch (typeof option) {
+            case 'string' :
+               isEmpty = Boolean(option);
+               break;
+            case 'number' :
+               isEmpty = false;
+               break;
+            case 'object' :
+               if(option instanceof $ws.proto.Enum) {
+                  isEmpty = option.getCurrentValue() === null;
+               } else if(option instanceof Array) {
+                  isEmpty = Boolean(option.length);
+               } else if(option instanceof Object) {
+                  isEmpty = Object.isEmpty(option)
+               } else if(option === null) {
+                  isEmpty = true;
+               }
+               break;
+            case 'boolean' :
+               isEmpty = option;
+               break;
+            case 'undefined' :
+               isEmpty = true;
+               break;
+         }
+
+         return isEmpty ?
+             'Поле обязательно для заполнения' :
+             true;
+      }
+   };
+});
