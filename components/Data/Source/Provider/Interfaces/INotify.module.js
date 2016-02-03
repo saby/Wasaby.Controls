8ad4aef8a3567124cbd7a3d076674c0f20f6ba07
aa.malcev2 <aleksey.a.maltsev@gmail.com>
2016-02-03 13:30:12 +0300

    сделал интерфейс более похожим на ServerEventBus - можно подписываться на любой канал

--- components/Data/Source/Provider/Interfaces/INotify.module.js
+++ components/Data/Source/Provider/Interfaces/INotify.module.js
@@ -8,41 +8,35 @@ define('js!SBIS3.CONTROLS.Data.Source.Provider.INotify', [
     * @mixin SBIS3.CONTROLS.Data.Source.Provider.INotify
     * @public
     * @author Мальцев Алексей
+    * @example
+    * <pre>
+    *    if ($ws.helpers.instanceOfModule(dataSource, 'SBIS3.CONTROLS.Data.Source.Remote') {
+    *       var provider = dataSource.getProvider();
+    *       if ($ws.helpers.instanceOfMixin(provider, 'SBIS3.CONTROLS.Data.Source.Provider.INotify') {
+    *          provider.getChannel().subscribe('onMessage', function(event, message) {
+    *             $ws.helpers.alert('A message from the server: ' + message);
+    *          });
+    *       }
+    *    }
+    * </pre>
+    * @example
+    * <pre>
+    *    var provider = dataSource.getProvider();
+    *    if ($ws.helpers.instanceOfMixin(provider, 'SBIS3.CONTROLS.Data.Source.Provider.INotify') {
+       *       provider.getChannel('ErrorLog').subscribe('onMessage', function(event, message) {
+       *          $ws.helpers.alert('Something went wrong: ' + message);
+       *       });
+       *    }
+    * </pre>
     */
 
    return /** @lends SBIS3.CONTROLS.Data.Source.Provider.INotify.prototype */{
-
       /**
-       * @event onMessage При получении уведомления о серверном событии
-       * @param {$ws.proto.EventObject} event Дескриптор события.
-       * @param {String|Object} message Сообщение события.
-       * @example
-       * <pre>
-       *    var provider = dataSource.getProvider();
-       *    if ($ws.helpers.instanceOfMixin(provider, 'SBIS3.CONTROLS.Data.Source.Provider.INotify') {
-       *       provider.subscribe('onMessage', function(event, message) {
-       *          $ws.helpers.alert('A message from the server: ' + message);
-       *       });
-       *    }
-       * </pre>
+       * Возвращает канал серверных событий
+       * @param {String} [name] Имя канала событий
+       * @returns {SBIS3.CONTROLS.Data.Source.Provider.IChannel}
        */
-
-      $protected: {
-         _options: {
-            /**
-             * @cfg {String} Название канала событий
-             * @see getEventsChannel
-             */
-            eventsChannel: ''
-         }
-      },
-
-      /**
-       * Возвращает название канала событий
-       * @returns {String}
-       * @see eventsChannel
-       */
-      getEventsChannel: function () {
+      getEventsChannel: function (name) {
          throw new Error('Method must be implemented');
       }
    };
