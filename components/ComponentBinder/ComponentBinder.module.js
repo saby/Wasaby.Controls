610b5f6acc3fbd7d7576a0ff37e9ce495b4d0196
cheremushkin ilya <iv.cheremushkin@tensor.ru>
2016-02-04 11:30:11 +0300

    корень может быть не только null, это нужно учитывать

--- components/ComponentBinder/ComponentBinder.module.js
+++ components/ComponentBinder/ComponentBinder.module.js
@@ -291,7 +291,7 @@ define('js!SBIS3.CONTROLS.ComponentBinder', [], function () {
                self._currentRoot = hier[0];
                self._path = hier.reverse();
             } else {
-               if (id === null){
+               if (id === view._options.root){
                    self._currentRoot = null;
                    self._path = [];
                }
@@ -316,7 +316,6 @@ define('js!SBIS3.CONTROLS.ComponentBinder', [], function () {
                }
             }
 
-            breadCrumbs._toggleHomeIcon(!self._currentRoot);
             breadCrumbs.setItems(self._path);
             backButton.setCaption(self._currentRoot ? $ws.helpers.escapeHtml(self._currentRoot.title) : '');
          });
