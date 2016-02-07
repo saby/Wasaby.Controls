05067cbe940357cfd4376f5d69c91a9222b082da
Gersasha <gersasha@yandex.ru>
2016-02-07 23:29:02 +0300

    Доработал отображение подсказки у строки кнопки фильтров Написал немного документации
    Ошибка в разработку от 28.01.2016 №1172562271
    ЕГАИС. Реестр. Нет тултипа у фильтра реестра. http://prntscr.com/9vv908
    1. кабинет123/кабинет123
    ...
    https://inside.tensor.ru/opendoc.html?guid=5116a2c8-c036-4e9c-8986-407c82b9e46b

--- components/FilterButton/FilterLine/FilterLine.module.js
+++ components/FilterButton/FilterLine/FilterLine.module.js
@@ -6,52 +6,63 @@ define('js!SBIS3.CONTROLS.FilterButton.FilterLine',
    ],
    function(CompoundControl, TemplateUtil, dotTplFn) {
 
+      /**
+       * Контрол, отображающий строку из применённых фильтров рядом с кнопкой фильтров.
+       * Умеет отображать строку по определенному шаблону. Работает исключительно через контекст.
+       * @class SBIS3.CONTROLS.FilterButton.FilterLine
+       * @extends $ws.proto.CompoundControl
+       * @control
+       * @public
+       */
+
       var FilterLine = CompoundControl.extend({
+
          _dotTplFn: dotTplFn,
+
          $constructor: function() {
-            var
-               ctx = this.getLinkedContext(),
-               self = this;
+            var context = this.getLinkedContext(),
+               self = this,
+               updateContext = function() {
+                  var linkText, textArr, template, templateRes;
 
-            function updateContext() {
-               var changed = ctx.getValue('filterChanged'),
-                   filterStructure = ctx.getValue('filterStructure'),
-                   resetLinkText = ctx.getValue('filterResetLinkText'),
-                   linkText, textArr, template, templateRes;
+                  /* Проверяем, изменился ли фильтр */
+                  if (context.getValue('filterChanged')) {
+                     /* Пробежимся по структуре фильтров и склеим строку */
+                     textArr = $ws.helpers.reduce(context.getValue('filterStructure'), function(result, element) {
+                        template = TemplateUtil.prepareTemplate(element.itemTemplate);
 
-               if (changed) {
-                  textArr = $ws.helpers.reduce(filterStructure, function(result, element) {
-                     template = TemplateUtil.prepareTemplate(element.itemTemplate);
+                        /* Если есть шаблон, строим строку по шаблону */
+                        if(template) {
+                           templateRes = template(element);
+                           if(templateRes) {
+                              result.push(template(element));
+                           }
+                           return result;
+                        } else if(template === null) {
+                           return result;
+                        }
 
-                     if(template) {
-                        templateRes = template(element);
-                        if(templateRes) {
-                           result.push(template(element));
+                        if (element.caption && !$ws.helpers.isEqualObject(element.value, element.resetValue)) {
+                           result.push(element.caption);
                         }
                         return result;
-                     } else if(template === null) {
-                        return result;
-                     }
+                     }, []);
+                     linkText = textArr.join(', ');
+                  } else {
+                     linkText = context.getValue('filterResetLinkText');
+                  }
 
-                     if (element.caption && !$ws.helpers.isEqualObject(element.value, element.resetValue)) {
-                        result.push(element.caption);
-                     }
-                     return result;
-                  }, []);
-                  linkText = textArr.join(', ');
-               } else {
-                  linkText = resetLinkText;
-               }
-
-               ctx.setValueSelf('linkText', linkText);
-               self.toggle(!!linkText);
-            }
+                  context.setValueSelf({
+                     linkText: linkText,
+                     titleText: $ws.helpers.escapeTagsFromStr(linkText, '')
+                  });
+                  self.toggle(!!linkText);
+               };
 
             updateContext();
-            ctx.subscribe('onFieldsChanged', updateContext);
+            context.subscribe('onFieldsChanged', updateContext);
 
-            var resetBtn = this._container.find('.controls__filterButton__filterLine-cross');
-            resetBtn.bind('click', function() {
+            this._container.on('click', '.controls__filterButton__filterLine-cross', function() {
                this.sendCommand('reset-filter');
             }.bind(this));
          }
