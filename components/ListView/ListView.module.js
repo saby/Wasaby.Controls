f8fe8c0f894abe51684df1349421262e68262879
as.suhoruchkin <as.suhoruchkin@tenzor.ru>
2016-02-10 15:22:55 +0300

    Ошибка в разработку от 10.02.2016 №1172591012 SBIS3.CONTROLS.DataGridView. Метод sendCommand('beginAdd') перестал возвращать Deferred https://inside.tensor.ru/opendoc.html?guid=e23a7fe1-0788-4f98-a2d1-67f8bdfdfee3

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -875,7 +875,9 @@ define('js!SBIS3.CONTROLS.ListView',
 
          showEip: function(target, model, options) {
             if (this.isEnabled()) {
-               this._getEditInPlace().showEip(target, model, options);
+               return this._getEditInPlace().showEip(target, model, options);
+            } else {
+               return $ws.proto.Deferred.fail();
             }
          },
 
