9cdd293c76ae554be023507ee3450e638bb9efad
cheremushkin ilya <iv.cheremushkin@tensor.ru>
2016-02-20 14:45:46 +0300

    вынес общее в EditAtPlaceMixin, добавил стили по новым стандартам

--- components/EditAtPlace/EditAtPlace.module.js
+++ components/EditAtPlace/EditAtPlace.module.js
@@ -4,13 +4,11 @@
 define('js!SBIS3.CONTROLS.EditAtPlace',
    ['js!SBIS3.CORE.CompoundControl',
       'js!SBIS3.CONTROLS.TextBox',
-      'js!SBIS3.CONTROLS.IconButton',
       'js!SBIS3.CONTROLS.PickerMixin',
-      'js!SBIS3.CONTROLS.DataBindMixin',
       'js!SBIS3.CORE.Dialog',
-      'js!SBIS3.CONTROLS.ControlHierarchyManager',
+      'js!SBIS3.CONTROLS.EditAtPlaceMixin',
       'html!SBIS3.CONTROLS.EditAtPlace'],
-   function (CompoundControl, TextBox, IconButton, _PickerMixin, _DataBindMixin, Dialog, ControlHierarchyManager, dotTplFn) {
+   function (CompoundControl, TextBox, PickerMixin, Dialog, EditAtPlaceMixin, dotTplFn) {
       'use strict';
       /**
        * @noShow
@@ -23,26 +21,24 @@ define('js!SBIS3.CONTROLS.EditAtPlace',
        * @author Крайнов Дмитрий Олегович
        */
 
-      var EditAtPlace = CompoundControl.extend([_PickerMixin, _DataBindMixin], /** @lends SBIS3.CONTROLS.EditAtPlace.prototype */{
+      var EditAtPlace = CompoundControl.extend([PickerMixin, EditAtPlaceMixin], /** @lends SBIS3.CONTROLS.EditAtPlace.prototype */{
          _dotTplFn: dotTplFn,
          _aliasForContent: 'editorTpl',
          $protected: {
             _textField: null,
-            _cancelCross: null,
             _okButton: null,
             _oldText: '',
             _requireDialog: false,
             _options: {
                text: '',
+               placeholder: '',
                editorTpl: '<component data-component="SBIS3.CONTROLS.TextBox"></component>',
-               multiline: false,
-               displayAsEditor: false,
-               editInPopup: false,
-               enableControlPanel: false
+               multiline: false
             }
          },
 
          $constructor: function () {
+            this._publish('onCancel', 'onApply', 'onShowEditor');
             var self = this;
             this._textField = $('.controls-EditAtPlace__textField', this._container.get(0));
             this._textField.bind('click', function () {
@@ -50,9 +46,7 @@ define('js!SBIS3.CONTROLS.EditAtPlace',
             });
             /*FixMe: придрот, выпилить когда будет номральный CompoundControl*/
             this._container.removeClass('ws-area');
-            if (this._options.displayAsEditor || !this._options.editInPopup) {
-               $('[data-component]', this._container).width(this._container.width());
-            }
+            
             this.subscribe('onTextChange', function(event, text){
                self._requireDialog = text != self._oldText;
             });
@@ -61,55 +55,15 @@ define('js!SBIS3.CONTROLS.EditAtPlace',
                $(this._container.children()[0]).addClass('controls-EditAtPlace__textAreaWrapper');
             }
 
-            this.reviveComponents();
-
-            this.getContext().subscribe('onFieldChange', function(e, f, v, o){
-               self.getParent().getLinkedContext().setValue(f, v, false, o);
-            });
-
             $('.js-controls-EditAtPlace__editor', this._container.get(0)).bind('keydown', function (e) {
                self._keyPressHandler(e);
             });
          },
 
-         showPicker: function(){
-            EditAtPlace.superclass.showPicker.call(this);
-            this._requireDialog = false;
-            this._resizeTextArea();
-         },
+         init: function(){
+            EditAtPlace.superclass.init.call(this);
+            if (this._options.text){
 
-         //FixMe костыль для авторесайза TextArea
-         _resizeTextArea: function(){
-            if (this._options.editInPopup) {
-               $('.controls-TextArea__inputField', this._picker._container).each(function () {
-                  if ($(this).data('autosize')) {
-                     $(this).data('autosize', false).autosize();
-                  }
-               });
-               this._picker._container.height('');
-               this._picker.recalcPosition(true);
-            } else {
-               $('.controls-TextArea__inputField', this._container).each(function () {
-                  $(this).data('autosize', false).autosize();
-               });
-            }
-         },
-
-         //FixMe Придрот для менеджера окон. Выпилить когда будет свой
-         _moveToTop: function(adjust){
-            if (this._options.editInPopup) {
-               if (this._picker.isVisible()) {
-                  var pos = Array.indexOf($ws.single.WindowManager._modalIndexes, this._picker._zIndex);
-                  $ws.single.WindowManager._modalIndexes.splice(pos, 1);
-                  pos = Array.indexOf($ws.single.WindowManager._visibleIndexes, this._picker._zIndex);
-                  $ws.single.WindowManager._visibleIndexes.splice(pos, 1);
-                  if (adjust) {
-                     this._picker._zIndex = $ws.single.WindowManager.acquireZIndex(true);
-                     this._picker._container.css('z-index', this._picker._zIndex);
-                     $ws.single.WindowManager.setVisible(this._picker._zIndex);
-                     $ws.single.ModalOverlay.adjust();
-                  }
-               }
             }
          },
 
@@ -131,8 +85,6 @@ define('js!SBIS3.CONTROLS.EditAtPlace',
                   this.setInPlaceEditMode(true);
                   this._addControlPanel(this._container.parent());
                }
-               this._resizeTextArea();
-
                if (this._options.editInPopup) {
                   if ($('.js-controls-TextBox__field', this._picker._container).get(0)) {
                      $('.js-controls-TextBox__field', this._picker._container).focus();
@@ -157,46 +109,30 @@ define('js!SBIS3.CONTROLS.EditAtPlace',
             }
          },
 
+         _setKeyPressHandler: function(newHandler){
+            if (typeof newHandler == 'function') {
+               var editors = $('.js-controls-EditAtPlace__editor', this._container.get(0));
+               editors.unbind('keydown', this._keyPressHandler);
+               editors.bind('keydown', newHandler);
+               this._keyPressHandler = newHandler;
+            }
+         },
+
          _setPickerContent: function () {
             var self = this;
             this._picker.getContainer().addClass('controls-EditAtPlace__editorOverlay');
-            $('[data-component]', this._picker.getContainer()).width(this._container.width());
             this._picker.reviveComponents();
             this._addControlPanel(this._picker._container);
             this._picker._container.bind('keydown', function (e) {
                self._keyPressHandler(e);
-            });
-            this._picker._container.bind('mousedown', function(){
-               self._moveToTop(true);
             });
          },
 
          setInPlaceEditMode: function (inPlace) {
             this._options.displayAsEditor = inPlace;
             this._container.toggleClass('controls-EditAtPlace__editorHidden', !inPlace).toggleClass('controls-EditAtPlace__fieldWrapperHidden', inPlace);
-         },
-
-         // Добавляем кнопки
-         _addControlPanel: function (container) {
-            if (this._options.enableControlPanel) {
-               this._cancelCross = $('<span class="controls-EditAtPlace__cancel"></span>');
-               var self = this,
-                  $ok = $('<span class="controls-EditAtPlace__okButton"></span>'),
-                  $cntrlPanel = $('<span class="controls-EditAtPlace__controlPanel"></span>').append($ok).append(this._cancelCross);
-
-               // Добавляем кнопки
-               this._okButton = new IconButton({
-                  parent: (self._options.editInPopup) ? self._picker : self,
-                  element: $ok,
-                  icon: 'sprite:icon-24 icon-Successful icon-done action-hover'
-               });
-               container.append($cntrlPanel);
-               this._okButton.subscribe('onActivated', function () {
-                  self._applyEdit();
-               });
-               this._cancelCross.bind('click', function () {
-                  self._cancelEdit();
-               });
+            if (inPlace) {
+               this._notify('onShowEditor');
             }
          },
 
@@ -212,148 +148,14 @@ define('js!SBIS3.CONTROLS.EditAtPlace',
             this._notify('onCancel');
          },
 
-         _applyEdit: function () {
-            if (this.validate()) {
-               var values = {};
-               if (this._options.editInPopup) {
-                  this._requireDialog = false;
-                  this._picker.hide();
-               } else {
-                  this.setInPlaceEditMode(false);
-                  this._removeControlPanel();
-               }
-               this._saveContextTo(values);
-               this._notify('onApply', values);
-               this._moveToTop(true);
-            }
-         },
-
-         _removeControlPanel: function(){
-            if (this._options.enableControlPanel) {
-               this._cancelCross.parent().remove();
-            }
-         },
-
-         _setPickerConfig: function () {
-            return {
-               corner: 'tl',
-               verticalAlign: {
-                  side: 'top'
-               },
-               horizontalAlign: {
-                  side: 'left'
-               },
-               template: this._options.editorTpl,
-               closeByExternalClick: true,
-               isModal: true
-            };
-         },
-
-         /**
-          * Открывает диалог подтверждения при отмене радактирования.
-          * @returns {$ws.proto.Deferred} deferred на закрытие модального диалога подтверждения.
-          * @private
-          */
-         _openConfirmDialog: function () {
-            var result,
-               deferred = new $ws.proto.Deferred();
-            this._dialogConfirm = new Dialog({
-               parent: this,
-               opener: this,
-               template: 'confirmRecordActionsDialog',
-               resizable: false,
-               handlers: {
-                  onReady: function () {
-                     var children = this.getChildControls(),
-                        dialog = this,
-                        onActivatedHandler = function () {
-                           dialog.getLinkedContext().setValue('result', this.getName());
-                           dialog.close();
-                        };
-                     for (var i = 0, len = children.length; i < len; i++) {
-                        if (children[i].hasEvent('onActivated')) {
-                           children[i].subscribe('onActivated', onActivatedHandler);
-                        }
-                     }
-                  },
-                  onKeyPressed: function (event, result) {
-                     if (result.keyCode === $ws._const.key.esc) {
-                        this.getLinkedContext().setValue('result', 'cancelButton');
-                     }
-                  },
-                  onAfterClose: function () {
-                     result = this.getLinkedContext().getValue('result');
-                  },
-                  onDestroy: function () {
-                     deferred.callback(result);
-                  }
-               }
-            });
-            ControlHierarchyManager.setTopWindow(this._dialogConfirm);
-            return deferred;
-         },
-
-         /**
-          * Сохранение текущих значений полей ввода (контекста) в объект
-          * @param obj {Object} объект, куда сохранить контекст
-          * @private
-          */
-         _saveContextTo: function (obj) {
-            var cnt = this.getLinkedContext()._context._contextObject;
-            for (var i in cnt){
-               if (cnt.hasOwnProperty(i)){
-                  obj[i] = cnt[i].value;
-               }
-            }
-            return obj;
-         },
-
-         _initializePicker: function(){
-            var self = this;
-            EditAtPlace.superclass._initializePicker.call(this);
-            this._picker.subscribe('onClose', function(event){
-               event.setResult(!self._requireDialog);
-               if (self._requireDialog) {
-                  self._moveToTop(true);
-                  self._openConfirmDialog().addCallback(function (result) {
-                     switch (result) {
-                        case 'yesButton':
-                           self._applyEdit();
-                           break;
-                        case 'noButton':
-                           self._cancelEdit();
-                           break;
-                        default:
-                           self._moveToTop(true);
-                     }
-                  });
-               }
-            });
-         },
-
-
-
-         _keyPressHandler: function (e) {
-            switch (e.which) {
-               /*case 13: {
-                  this._applyEdit();
-               }
-                  break;*/
-               case 27: {
-                  this._cancelEdit();
-                  e.stopPropagation();
-               }
-                  break;
-               default:
-                  break;
-            }
+         _getEditTemplate: function(){
+            return this._options.editorTpl;
          },
 
          setText: function (text) {
             var oldText = this._options.text;
             this._options.text = text || '';
             if (oldText !== this._options.text) {
-               this.saveToContext('Text', text);
                this._notify('onTextChange', this._options.text);
             }
             if (text) {
--- components/EditAtPlaceGroup/EditAtPlaceGroup.module.js
+++ components/EditAtPlaceGroup/EditAtPlaceGroup.module.js
@@ -3,13 +3,13 @@
  */
 define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
    ['js!SBIS3.CORE.CompoundControl',
-      'js!SBIS3.CONTROLS.IconButton',
       'js!SBIS3.CONTROLS.PickerMixin',
       'js!SBIS3.CONTROLS.EditAtPlace',
       'js!SBIS3.CORE.Dialog',
-      'js!SBIS3.CONTROLS.ControlHierarchyManager',
+      'js!SBIS3.CONTROLS.EditAtPlaceMixin',
+      'js!SBIS3.CORE.CompoundActiveFixMixin',
       'html!SBIS3.CONTROLS.EditAtPlaceGroup'],
-   function (CompoundControl, IconButton, PickerMixin, EditAtPlace, Dialog, ControlHierarchyManager, dotTplFn) {
+   function (CompoundControl, PickerMixin, EditAtPlace, Dialog, EditAtPlaceMixin, CompoundActiveFixMixin, dotTplFn) {
       'use strict';
       /**
        * @noShow
@@ -22,44 +22,37 @@ define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
        * @author Крайнов Дмитрий Олегович
        */
 
-      var EditAtPlaceGroup = CompoundControl.extend([PickerMixin], /** @lends SBIS3.CONTROLS.EditAtPlaceGroup.prototype */{
+      var EditAtPlaceGroup = CompoundControl.extend([PickerMixin, EditAtPlaceMixin, CompoundActiveFixMixin], /** @lends SBIS3.CONTROLS.EditAtPlaceGroup.prototype */{
          $protected: {
             _dotTplFn: dotTplFn,
             _textField: null,
             _cancelButton: null,
             _okButton: null,
-            _cancelCross: null,
             _editorTpl: null,
             _requireDialog: false,
             _options: {
                /**
                 * @cfg {String} шаблон
                 */
-               template: null,
-               /**
-                * @cfg {Boolean} отображать как редактор
-                */
-               displayAsEditor: false,
-               /**
-                * @cfg {Boolean} редактировать во всплывашке или в собственном контейнере
-                */
-               editInPopup: false
+               template: null
             }
          },
 
          $constructor: function () {
-            var self = this;
-            this._publish('onCancel', 'onApply');
-            this._loadChildControls();
+            this._publish('onCancel', 'onApply', 'onShowEditor');
+         },
 
+         init: function(){
+            EditAtPlaceGroup.superclass.init.call(this);
+            var self = this;
+            this.reviveComponents();
             if (this._options.displayAsEditor) {
                this.setInPlaceEditMode();
             } else {
                // всем EditAtPlace задаем свой обработчик клика
                this._iterateChildEditAtPlaces(function(child){
-                  child._setClickHandler(function () {
-                     self._clickHandler();
-                  });
+                  child._setClickHandler(self._clickHandler.bind(self));
+                  child._setKeyPressHandler(self._keyPressHandler.bind(self));
                   if ($(child._options.editorTpl).attr('data-component') == 'SBIS3.CONTROLS.TextArea'){
                      $(child._container.children()[0]).addClass('controls-EditAtPlace__textAreaWrapper');
                   }
@@ -68,69 +61,10 @@ define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
                   });
                });
             }
-         },
-
-         _initializePicker: function(){
-            var self = this;
-            EditAtPlaceGroup.superclass._initializePicker.call(this);
-            this._picker.subscribe('onClose', function(event){
-               event.setResult(!self._requireDialog);
-               if (self._requireDialog) {
-                  self._moveToTop(true);
-                  self._openConfirmDialog().addCallback(function (result) {
-                     switch (result) {
-                        case 'yesButton':
-                           self._applyEdit();
-                           break;
-                        case 'noButton':
-                           self._cancelEdit();
-                           break;
-                        default:
-                           self._moveToTop(true);
-                     }
-                  });
-               }
-            });
-         },
-
-         showPicker: function(){
-            EditAtPlaceGroup.superclass.showPicker.call(this);
-            this._requireDialog = false;
-            this._resizeTextArea();
-         },
-
-         //FixMe костыль для авторесайза TextArea
-         _resizeTextArea: function(){
-            if (this._options.editInPopup) {
-               $('.controls-TextArea__inputField', this._picker._container).each(function () {
-                  if ($(this).data('autosize')) {
-                     $(this).data('autosize', false).autosize();
-                  }
+            if (!this._options.editInPopup){
+               this.subscribe('onChildFocusOut', function(){
+                  self._applyEdit();
                });
-               this._picker._container.height('');
-               this._picker.recalcPosition(true);
-            } else {
-               $('.controls-TextArea__inputField', this._container).each(function () {
-                  $(this).data('autosize', false).autosize();
-               });
-            }
-         },
-
-         //FixMe Придрот для менеджера окон. Выпилить когда будет свой
-         _moveToTop: function(adjust){
-            if (this._options.editInPopup) {
-               if (this._picker.isVisible()) {
-                  var pos = Array.indexOf($ws.single.WindowManager._modalIndexes, this._picker._zIndex);
-                  $ws.single.WindowManager._modalIndexes.splice(pos, 1);
-                  pos = Array.indexOf($ws.single.WindowManager._visibleIndexes, this._picker._zIndex);
-                  $ws.single.WindowManager._visibleIndexes.splice(pos, 1);
-                  if (adjust) {
-                     this._picker._zIndex = $ws.single.WindowManager.acquireZIndex(true);
-                     this._picker._container.css('z-index', this._picker._zIndex);
-                     $ws.single.WindowManager.setVisible(this._picker._zIndex);
-                     $ws.single.ModalOverlay.adjust();
-                  }
-               }
             }
          },
 
@@ -145,9 +79,6 @@ define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
                child.setInPlaceEditMode(true);
             }, this._picker);
             this._addControlPanel(this._picker._container);
-            this._picker._container.bind('mousedown', function(){
-               self._moveToTop(true);
-            });
          },
 
          _iterateChildEditAtPlaces: function(func, parent){
@@ -172,28 +103,9 @@ define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
             this._iterateChildEditAtPlaces(function(child){
                child.setInPlaceEditMode(inPlace);
             });
-         },
-
-         // Добавляем кнопки
-         _addControlPanel: function (container) {
-            this._cancelCross = $('<span class="controls-EditAtPlace__cancel"></span>');
-            var self = this,
-               $ok = $('<span class="controls-EditAtPlace__okButton"></span>'),
-               $cntrlPanel = $('<span class="controls-EditAtPlaceGroup__controlPanel"></span>').append($ok).append(this._cancelCross);
-
-            // Добавляем кнопки
-            this._okButton = new IconButton({
-               parent: self._picker,
-               element: $ok,
-               icon: 'sprite:icon-24 icon-Successful icon-done action-hover'
-            });
-            container.append($cntrlPanel);
-            this._okButton.subscribe('onActivated', function () {
-               self._applyEdit();
-            });
-            this._cancelCross.bind('mousedown', function () {
-               self._cancelEdit();
-            });
+            if (inPlace) {
+               this._notify('onShowEditor');
+            }
          },
 
          _cancelEdit: function () {
@@ -210,46 +122,14 @@ define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
             this._notify('onCancel');
          },
 
-         _applyEdit: function () {
-            if (this.validate()) {
-               var value = {};
-               if (this._options.editInPopup) {
-                  this._requireDialog = false;
-                  this._picker.hide();
-               } else {
-                  this.setInPlaceEditMode(false);
-                  this._removeControlPanel();
-               }
-
-               this._notify('onApply', value);
-            }
-            this._moveToTop(true);
-         },
-
-         _removeControlPanel: function(){
-            this._cancelCross.parent().remove();
-         },
-
-         _setPickerConfig: function () {
-            return {
-               corner: 'tl',
-               verticalAlign: {
-                  side: 'top'
-               },
-               horizontalAlign: {
-                  side: 'left'
-               },
-               closeByExternalClick: true,
-               isModal: true,
-               template: this._options.template
-            };
+         _getEditTemplate: function(){
+            return this._options.template;
          },
 
          _clickHandler: function () {
             if (this.isEnabled()) {
                this._iterateChildEditAtPlaces(function (child) {
                   child._saveOldText();
-
                });
                if (this._options.editInPopup) {
                   this.showPicker();
@@ -273,102 +153,11 @@ define('js!SBIS3.CONTROLS.EditAtPlaceGroup',
             }
          },
 
-         _keyPressHandler: function (e) {
-            switch (e.which) {
-               case 13: {
-                  this._applyEdit();
-               }
-                  break;
-               case 27: {
-                  this._cancelEdit();
-                  e.stopPropagation();
-               }
-                  break;
-               default:
-                  break;
-            }
-         },
-
          setEnabled: function(enabled){
             EditAtPlaceGroup.superclass.setEnabled.call(this, enabled);
             this._iterateChildEditAtPlaces(function(child){
                child.setEnabled(enabled);
             });
-         },
-
-         /**
-          * Открывает диалог подтверждения при отмене радактирования.
-          * @returns {$ws.proto.Deferred} deferred на закрытие модального диалога подтверждения.
-          * @private
-          */
-         _openConfirmDialog: function () {
-            var result,
-               deferred = new $ws.proto.Deferred();
-            this._dialogConfirm = new Dialog({
-               parent: this,
-               opener: this,
-               template: 'confirmRecordActionsDialog',
-               resizable: false,
-               handlers: {
-                  onReady: function () {
-                     var children = this.getChildControls(),
-                        dialog = this,
-                        onActivatedHandler = function () {
-                           dialog.getLinkedContext().setValue('result', this.getName());
-                           dialog.close();
-                        };
-                     for (var i = 0, len = children.length; i < len; i++) {
-                        if (children[i].hasEvent('onActivated')) {
-                           children[i].subscribe('onActivated', onActivatedHandler);
-                        }
-                     }
-                  },
-                  onKeyPressed: function (event, result) {
-                     if (result.keyCode === $ws._const.key.esc) {
-                        this.getLinkedContext().setValue('result', 'cancelButton');
-                     }
-                  },
-                  onAfterClose: function () {
-                     result = this.getLinkedContext().getValue('result');
-                  },
-                  onDestroy: function () {
-                     deferred.callback(result);
-                  }
-               }
-            });
-            ControlHierarchyManager.setTopWindow(this._dialogConfirm);
-            return deferred;
-         },
-
-         /**
-          * Сохранение текущих значений полей ввода (контекста) в объект
-          * @param obj {Object} объект, куда сохранить контекст
-          * @private
-          */
-         _saveContextTo: function (obj) {
-            var cnt = this.getLinkedContext()._context._contextObject;
-            for (var i in cnt){
-               if (cnt.hasOwnProperty(i)){
-                  obj[i] = cnt[i].value;
-               }
-            }
-            return obj;
-         },
-
-         //*TODO переопределяем метод compoundControl - костыль*//*
-         _loadControls: function (pdResult) {
-            return pdResult.done([]);
-         },
-
-         /*TODO свой механизм загрузки дочерних контролов - костыль*/
-         _loadChildControls: function () {
-            var def = new $ws.proto.Deferred();
-            var self = this;
-            self._loadControlsBySelector(new $ws.proto.ParallelDeferred(), undefined, '[data-component]')
-               .getResult().addCallback(function () {
-                  def.callback();
-               });
-            return def;
          }
       });
 
