579ccb3ccd0edf3fe66da63dc61c2871a359c811
Shilovda <sceletron@gmail.com>
2016-02-22 11:39:18 +0300

    Ошибка в разработку от 21.02.2016 №1172616390 фикс инсайд, не загружается реестр Контакты https://inside.tensor.ru/opendoc.html?guid=cfcdc5d3-8be5-44f3-bece-b2ac80adff69

--- tests/unit/list.js
+++ tests/unit/list.js
@@ -8,10 +8,8 @@ define([
 '../../components/Data/resources/test/Serializer.test',
 '../../components/Data/Source/resources/test/DataSet.test',
 '../../components/Data/Source/resources/test/Memory.test',
-'../../components/Data/Source/resources/test/SbisService.test',
 '../../components/__DataSet/DataFactory/resources/DataFactory.test',
 '../../components/__DataSet/DataSet/resources/test/DataSet.test',
 '../../components/__DataSet/DataSet/resources/test/Record.test',
-'../../components/__DataSet/DataSource/resources/test/SbisServiceSource.test',
 '../../components/__DataSet/DataSource/resources/test/StaticSource.test'
 ]);
