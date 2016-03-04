aa7232ea4469ee5dd17f755c1cf61ad5bc90ccba
am.gerasimov <am.gerasimov@tensor.ru>
2016-03-04 13:29:52 +0300

    Поправил синхронизацию выбранных элементов При отсуцтвии проекции всё равно должен вызываться метод drawSelectedItem
    Ошибка в разработку от 03.03.2016 №1172641332
    При клике в строку удобного ввода сбрасывается наименование http://prntscr.com/aam4po
    adonis/adon...
    https://inside.tensor.ru/opendoc.html?guid=252e92ee-fb2b-4a54-8492-52aa86c67d66

--- components/Mixins/SyncSelectionMixin/SyncSelectionMixin.module.js
+++ components/Mixins/SyncSelectionMixin/SyncSelectionMixin.module.js
@@ -4,35 +4,52 @@
 define('js!SBIS3.CONTROLS.SyncSelectionMixin', ['js!SBIS3.CONTROLS.Data.Model'], function(Model) {
 
    /**
-    * Миксин, добавляющий синхронизацию выбранных элементов по следующему правилу:
+    * Миксин, добавляющий синхронизацию выбранных элементов
+    * @remark
     * selectedItem всегда смотрит на первый элемент из selectedItems
+    * selectedKey всегда смотрит на перывй элемент из selectedKeys
     * @mixin SBIS3.CONTROLS.SyncSelectionMixin
     * @public
     * @author Крайнов Дмитрий Олегович
     */
+
+   var PROPS_TO_SYNC = {
+      selectedItem: true,
+      selectedItems: true,
+      selectedKeys: true,
+      selectedKey: true
+   };
 
    var SyncSelectionMixin = /**@lends SBIS3.CONTROLS.SyncSelectionMixin.prototype  */{
       $constructor: function() {
          this.subscribe('onPropertyChanged', function(e, propName) {
             var propValue, item;
 
-            if(propName === 'selectedItem' || propName === 'selectedItems') {
-               propValue =  this.getProperty(propName);
+            if (PROPS_TO_SYNC[propName]) {
+               propValue = this.getProperty(propName);
 
-               if(propName === 'selectedItem') {
-                  $ws.helpers.instanceOfModule(propValue, 'SBIS3.CONTROLS.Data.Model') ?
-                      this.setSelectedItems([propValue]) :
-                      this.clearSelectedItems();
-               }
-
-               if(propName === 'selectedItems') {
-                  item = propValue.at(0);
-                  this._options.selectedItem = item ? item : null;
-                  this._options.selectedKey = item ? item.getId() : null;
+               switch (propName) {
+                  case 'selectedItem':
+                     $ws.helpers.instanceOfModule(propValue, 'SBIS3.CONTROLS.Data.Model') ?
+                         this.setSelectedItems([propValue]) :
+                         this.clearSelectedItems();
+                     break;
+                  case 'selectedItems':
+                     item = propValue.at(0);
+                     this._options.selectedItem = item ? item : null;
+                     this._options.selectedKey = item ? item.getId() : null;
+                     break;
+                  case 'selectedKeys':
+                     this._options.selectedKey = propValue.length ? propValue[0] : null;
+                     break;
+                  case 'selectedKey':
+                     this._options.selectedKeys = propValue === null ? [] : [propValue];
+                     break;
                }
             }
          });
       },
+
       around: {
          _drawSelectedItem: function(parentFunc, key) {
             this._options.selectedKeys = key === null ? [] : [key];
