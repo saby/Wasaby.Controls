eefa1968d3ef4424ce7655c93dc6ebb6b76961f4
am.gerasimov <am.gerasimov@tensor.ru>
2016-04-18 11:47:22 +0300

    Добавил возможность задать массив фильтров, которые не должны запоминаться в историю
    Ошибка в разработку от 18.04.2016 №1172748952
    У всех фильтр В обработке стал показывать все документы
    Горит
    https://inside.tensor.ru/opendoc.html?guid=76d3415d-b93e-49b4-8bf4-784edac377ef

--- components/Browser/Browser.module.js
+++ components/Browser/Browser.module.js
@@ -76,9 +76,9 @@ define('js!SBIS3.CONTROLS.Browser', [
              */
             historyId : '',
             /**
-             * @cfg {Array} ignoreFilterList Массив ключей фильтров, которые не надо запоминать в историю.
+             * @cfg {Array} ignoreFiltersList Массив ключей фильтров, которые не надо запоминать в историю.
              */
-            ignoreFilterKeys: [],
+            ignoreFiltersList: [],
             contentTpl : contentTpl
          }
       },
@@ -143,7 +143,7 @@ define('js!SBIS3.CONTROLS.Browser', [
                    this._fastDataFilter,
                    this._options.searchParam,
                    this._options.historyId,
-                   this._options.ignoreFilterKeys,
+                   this._options.ignoreFiltersList,
                    HistoryController,
                    this);
             } else {
