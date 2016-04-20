d9b2035fb35edcb7671bdeeef3d63edac5d92429
as.krasilnikov <as.krasilnikov@tensor.ru>
2016-04-20 15:17:47 +0300

    После установки новых данных, не все ключи останутся актуальными Ошибка в разработку от 12.04.2016 №1172737537 Используем  компонент SBIS3.CONTROLS.DropdownList Данные набиваем вручную на клиенте При различны... https://inside.tensor.ru/opendoc.html?guid=fb610ec3-8ebc-4f6d-be2a-4e8e3a2631b3

--- components/DropdownList/DropdownList.module.js
+++ components/DropdownList/DropdownList.module.js
@@ -405,6 +405,7 @@ define('js!SBIS3.CONTROLS.DropdownList',
             var textValues = [],
                 len = id.length,
                 self = this,
+                record,
                 pickerContainer,
                 def;
 
@@ -413,7 +414,10 @@ define('js!SBIS3.CONTROLS.DropdownList',
 
                if(this._dataSet) {
                   for(var i = 0; i < len; i++) {
-                     textValues.push(this._dataSet.getRecordByKey(id[i]).get(this._options.displayField));
+                     record = this._dataSet.getRecordByKey(id[i]);
+                     if (record){ //После установки новых данных, не все ключи останутся актуальными
+                        textValues.push(record.get(this._options.displayField));
+                     }
                   }
                   def.callback(textValues);
                } else {
