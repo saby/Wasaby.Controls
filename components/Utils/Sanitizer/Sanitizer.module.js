71dda7a030de8d1aafd2830757c99d384fcb5ef6
ps.borisov <ps.borisov@tensor.ru>
2016-05-05 17:52:35 +0300

    Написал небольшую доку. как сейччас можно использовать санитизатор Merge request от 26.04.2016 №1172773298 Регламент: Ошибка в разработку. Автор: Авраменко А.С. Описание: XSS уязвимость в текстовом редакт... https://inside.tensor.ru/opendoc.html?guid=837462d6-4acd-4aec-9e35-4907edb15bf2

--- components/Utils/Sanitizer/Sanitizer.module.js
+++ components/Utils/Sanitizer/Sanitizer.module.js
@@ -8,18 +8,40 @@ define('js!SBIS3.CONTROLS.Utils.Sanitizer',
       'js!SBIS3.CONTROLS.Utils.Sanitizer/TinyyMCEClasses/DomParser',
       'js!SBIS3.CONTROLS.Utils.Sanitizer/TinyyMCEClasses/Tools'
    ], function(Serializer,Schema, DomParser, Tools) {
-   var trim = Tools.trim;
-   return Sanitizer = function(settings) {
+      'use strict';
+      /**
+       * Утилита для очистки содержимого (string) от скриптов
+       * @class SBIS3.CONTROLS.Utils.Sanitizer
+       * @author Авраменко А.С.
+       * @public
+       * */
+   var Sanitizer = function(settings) {
       this.schema = new Schema(settings);
       this.parser = new DomParser(settings, this.schema);
       this.serializer = new Serializer(this.schema);
+      /**
+       * Очищает контент от вредоносных скриптов
+       * @param {string} content который необходимо очистить от вредоносных скриптов
+       * @returns {string}
+       * @example
+       * <pre>
+       *    require([js!SBIS3.CONTROLS.Utils.Sanitizer'], function(Sanitizer) {
+       *       var
+       *          dirtyHtml = '<div>content</div><script>alert(1)</script>'
+       *          sanitizer = new Sanitizer(),
+       *          clearContent = sanitizer.clear(dirtyHtml);
+       *          //clearContent = '<div>content</div>'
+       *    })
+       * <pre>
+       * */
       this.clear = function(content) {
          if (!content) {
             return "";
          }
          content = this.serializer.serialize(this.parser.parse(content));
-         content = trim(content);
+         content = Tools.trim(content);
          return content;
       };
-   }
+   };
+   return Sanitizer;
 });
