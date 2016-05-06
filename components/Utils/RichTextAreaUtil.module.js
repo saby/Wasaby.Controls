ac7b162042d20720f3d51e64f26f7cd9a80276ca
ps.borisov <ps.borisov@tensor.ru>
2016-05-06 13:40:41 +0300

    поправил доку

--- components/Utils/RichTextAreaUtil.module.js
+++ components/Utils/RichTextAreaUtil.module.js
@@ -30,6 +30,7 @@ define('js!SBIS3.CONTROLS.Utils.RichTextAreaUtil',[], function () {
       unmarkRichContentOnCopy: function(target){
          target = target.get(0);
          if (!$ws._const.browser.isMobileIOS) {
+            //в webkit в бтре идёт подписка на cut и удаляется лишний символ, поэтому нужно подписываться на capture фазе
             if (target.removeEventListener) {
                target.removeEventListener('copy', this._markingRichContent, true);
                target.removeEventListener('cut', this._markingRichContent, true);
