6b217a57bea205ce4f3d9faea298268be5f9ca7a
ps.borisov <ps.borisov@tensor.ru>
2016-05-08 16:36:31 +0300

    clearContent правильное название метьода Ошибка в разработку от 08.05.2016 №1172794371 Ошибка прохождения unit-тестов Controls  SBIS3.CONTROLS.Utils.Sanitizer .clear().should return em... https://inside.tensor.ru/opendoc.html?guid=d5f62e3c-3e4d-49fe-a0ad-923ecccdee68

--- components/Utils/Sanitizer/resources/tests/sanitizer.test.js
+++ components/Utils/Sanitizer/resources/tests/sanitizer.test.js
@@ -19,47 +19,47 @@ define([
             content = undefined;
          });
 
-         describe('.clear()', function() {
+         describe('.clearContent()', function() {
             it('should return empty string if content have just script', function() {
                content = '<script>alert(1)</script>';
-               assert.equal(htmlClear.clear(content), '');
+               assert.equal(htmlClear.clearContent(content), '');
             });
             it('not remove special symbols', function() {
                content = '&nbsp; &iexcl; &cent; &pound; &curren; &yen; $brvbar; &sect; &uml; &copy;';
-               assert.equal(htmlClear.clear(content), content);
+               assert.equal(htmlClear.clearContent(content), content);
             });
             it('not remove line break symbols', function() {
                content = 'test' + String.fromCharCode(10) + 'test';
-               assert.equal(htmlClear.clear(content), content);
+               assert.equal(htmlClear.clearContent(content), content);
                content = 'test\ntest';
-               assert.equal(htmlClear.clear(content), content);
+               assert.equal(htmlClear.clearContent(content), content);
             });
             it('not save onclick', function() {
                content = '<div onclick="alert(1);">123</div>';
-               assert.equal(htmlClear.clear(content), '<div>123</div>');
+               assert.equal(htmlClear.clearContent(content), '<div>123</div>');
             });
             it('save onclick', function() {
                content = '<div onclick="alert(1);">123</div>';
                htmlClear = new Sanitizer({extended_valid_elements: 'div[onclick]'});
-               assert.equal(htmlClear.clear(content), content);
+               assert.equal(htmlClear.clearContent(content), content);
             });
             it('save style', function() {
                content = '<div style="margin:20px">123</div>';
-               assert.equal(htmlClear.clear(content), content);
+               assert.equal(htmlClear.clearContent(content), content);
             });
             it('remove all attributes in div', function() {
                content = '<div style="margin:20px" onclick="alert(1)" width="100" class="myclass" >123</div>';
                htmlClear = new Sanitizer({extended_valid_elements: 'div'});
-               assert.equal(htmlClear.clear(content), '<div>123</div>');
+               assert.equal(htmlClear.clearContent(content), '<div>123</div>');
             });
             it('save script', function() {
                content = '<script>alert(1)</script>';
                htmlClear = new Sanitizer({extended_valid_elements: 'script'});
-               assert.equal(htmlClear.clear(content), content);
+               assert.equal(htmlClear.clearContent(content), content);
             });
             it('remove not valid attribute', function() {
                content = '<div stylesss="margin:20px">123</div>';
-               assert.equal(htmlClear.clear(content), '<div>123</div>');
+               assert.equal(htmlClear.clearContent(content), '<div>123</div>');
             });
          });
       });
