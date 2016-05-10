48b94fb875605e46f23e03317565aa3a996916bc
vs.romanov <vs.romanov@tensor.ru>
2016-05-10 12:37:55 +0300

    Добавил возможность передавать фильтр в выпадающий список Написал пример использования фильтра Ошибка в разработку от 25.04.2016 №1172769424 Добавить возможность указывать filter к dataSource при задании настроек данных для выпадашек к Fa... https://inside.tensor.ru/opendoc.html?guid=22bfe467-21f7-4ed4-88d2-93b30909af7c

--- components/FastDataFilter/FastDataFilter.module.js
+++ components/FastDataFilter/FastDataFilter.module.js
@@ -103,7 +103,8 @@ define('js!SBIS3.CONTROLS.FastDataFilter',
                displayField: item.get('displayField'),
                className: item.get('className') || 'controls-DropdownList__linkStyle',
                pickerClassName: (item.get('pickerClassName') + ' controls-DropdownList__picker') || 'controls-DropdownList__picker',
-               dataSource: item.get('dataSource')
+               dataSource: item.get('dataSource'),
+               filter: item.get('filter')
             };
             return '<component data-component="SBIS3.CONTROLS.DropdownList" config="' + $ws.helpers.encodeCfgAttr(cfg) + '">' +
                         //'<opts name="selectedKeys" type="array" bind="' + cfg.filterName +'" ></opts>' + //direction="fromProperty" oneWay="true"
