fadde6d40249254c83037e66dfbf7eb4de0823cb
as.krasilnikov <as.krasilnikov@tensor.ru>
2016-05-13 15:54:42 +0300

    кривой ребейз

--- components/FormController/FormController.module.js
+++ components/FormController/FormController.module.js
@@ -320,7 +320,7 @@ define('js!SBIS3.CONTROLS.FormController', ['js!SBIS3.CORE.CompoundControl', 'js
                isNewModel = self._options.newModel;
                self._options.newModel = false;
                self._notify('onUpdateModel', self._options.record, isNewModel);
-               if (closePanelAfterSubmit) {
+               if (config.closePanelAfterSubmit) {
                   self._panel.ok();
                }
                else {
