2f1ae283e28789415810529f92fb064b5e75e1d8
Gersasha <gersasha@yandex.ru>
2016-05-22 19:11:23 +0300

    Ошибка в разработку от 20.05.2016 №1172823119 Интеграционное тестирование CONTROLS - если быстро ввести текст в поле поиска и нажать ENTER, то ... https://inside.tensor.ru/opendoc.html?guid=df02dc54-9d1a-4b19-8da4-3ca6f87ef9cd

--- components/Mixins/SearchMixin/SearchMixin.module.js
+++ components/Mixins/SearchMixin/SearchMixin.module.js
@@ -21,9 +21,9 @@ define('js!SBIS3.CONTROLS.SearchMixin', [], function() {
        * @param {$ws.proto.EventObject} eventObject Дескриптор события.
        */
       $protected: {
-         _curText: '',
          //Чтобы событие onReset не отправлялось непрерывно
          _onResetIsFired: true,
+         _searchDelay: null,
          _options: {
             /**
              * @cfg {Number|null} количество символов, которые нужно ввести, чтоб начать поиск.
@@ -49,18 +49,23 @@ define('js!SBIS3.CONTROLS.SearchMixin', [], function() {
       },
 
       _startSearch: function(text) {
-         var self = this;
-         this._curText = text;
-         window.setTimeout(function(){
-            if (text == self._curText) {
-               self._applySearch(text, false);
-            }
-         }, this._options.searchDelay);
+         this._searchDelay = setTimeout($ws.helpers.forAliveOnly(function () {
+            this._applySearch(text);
+         }, this), this._options.searchDelay);
+      },
+
+      _clearSearchDelay: function () {
+        if(this._searchDelay) {
+           clearTimeout(this._searchDelay);
+           this._searchDelay = null;
+        }
       },
 
       _applySearch : function(text, force) {
          var hasStartCharacter = this._options.startCharacter !== null;
 
+         /* Если поиск запущен, то надо отменить поиск с задержкой */
+         this._clearSearchDelay();
          if (text) {
             text = text.replace(/[<>]/g, '');
             if ( (hasStartCharacter && String.trim(text).length >= this._options.startCharacter) || force ) {
