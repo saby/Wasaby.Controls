063626bb0a73691f41d6d63fb5afbf3fe7659b5b
Gersasha <gersasha@yandex.ru>
2016-05-22 20:38:12 +0300

    Ошибка в разработку от 20.05.2016 №1172822622 Ipad После отправки быстрого ответа появляются опции строки как повторить: контакты-диалоги. Свай... https://inside.tensor.ru/opendoc.html?guid=912cf7bd-6639-40bb-aef7-6a6ccab2a258

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -1432,6 +1432,9 @@ define('js!SBIS3.CONTROLS.ListView',
                      },
                      onItemActionActivated: function(e, key) {
                         self.setSelectedKey(key);
+                        if(self._touchSupport) {
+                           self._clearHoveredItem();
+                        }
                      }
 
                   }
