e8ed9718b5a557db680de4d325319a5612b3a8ed
ganshinyao <yo.ganshin@tensor.ru>
2016-05-26 19:47:46 +0300

    Ошибка в разработку от 25.05.2016 №1172831791 Задачи. Умирает страница, если сбросить поиск greencoast\qwerty123456 -> Задачи\От меня ->... https://inside.tensor.ru/opendoc.html?guid=6205b28f-629b-4545-a229-d68850e9ceab

--- components/Data/Projection/Tree.module.js
+++ components/Data/Projection/Tree.module.js
@@ -423,7 +423,7 @@ define('js!SBIS3.CONTROLS.Data.Projection.Tree', [
                }
                hierIndex[parentId].push(index);
             }
-            //todo с бл могут прийти дублирующие узлы мы их убираем
+            //todo с бл могут прийти дублирующиеся узлы мы их убираем
             var sortmap = buildHierarchy(options.root), uniq = [];
             for (index = 0, count= sortmap.length; index < count; index++) {
                if (Array.indexOf(uniq, sortmap[index]) === -1) {
