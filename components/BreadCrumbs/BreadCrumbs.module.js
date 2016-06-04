8e112ddcec866133903618ae00246b8ad16d3b20
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2016-06-04 12:42:49 +0300

    Поправил расчеты для крайнего случая с двумя оставшимися элементами https://inside.tensor.ru/opendoc.html?guid=ce1d113a-4455-4fe1-8393-9eb38f8524a1&description= Ошибка в разработку: С одной стороны места не хватает, а с другой дырка

--- components/BreadCrumbs/BreadCrumbs.module.js
+++ components/BreadCrumbs/BreadCrumbs.module.js
@@ -220,24 +220,24 @@ define('js!SBIS3.CONTROLS.BreadCrumbs', [
 
          var targetContainer = this._getTargetContainer(),
             containerWidth = this._container.width(),
-            points = $('.controls-BreadCrumbs__crumb', targetContainer),
-            i = points.length - 1;
+            crumbs = $('.controls-BreadCrumbs__crumb', targetContainer),
+            i = crumbs.length - 1;
 
-         if (points.length){
+         if (crumbs.length){
             //Добавляем троеточие если пункты не убираются в контейнер
-            if ((targetContainer.width() + this._homeIconWidth >= containerWidth) && points.length > 2) {
-               $(points[i - 1]).before(dots);
+            if ((targetContainer.width() + this._homeIconWidth >= containerWidth) && crumbs.length > 2) {
+               $(crumbs[i - 1]).before(dots);
                //скрываем пункты левее троеточия пока не уберемся в контейнер
                for (i; i > 1; i--) {
                   if (targetContainer.width() + this._homeIconWidth < containerWidth || i == 1) {
                      break;
                   }
-                  points[i - 1].className += ' ws-hidden';
+                  crumbs[i - 1].className += ' ws-hidden';
                }
             }
             
             //Если после всех манипуляций все еще не убираемся в контейнер, будем обрезать текст
-            points = $('.controls-BreadCrumbs__crumb:not(.ws-hidden)', targetContainer);
+            crumbs = $('.controls-BreadCrumbs__crumb', targetContainer).not('.ws-hidden').not('.controls-BreadCrumbs__dots');
 
             //Минимум остается первая и последняя хлебная крошка
             if (targetContainer.width() + this._homeIconWidth >= containerWidth) {
@@ -245,10 +245,22 @@ define('js!SBIS3.CONTROLS.BreadCrumbs', [
                var dotsWidth = $('.controls-BreadCrumbs__dots', this._container).outerWidth(true) || 0;
                var width = this._homeIconWidth + dotsWidth + this._arrowWidth * 2;
                var halfWidth = Math.floor((containerWidth - width) / 2);
-               if (points.length >= 2){
-                  $('.controls-BreadCrumbs__title', points).css('max-width', halfWidth);
+               //Если два элемента
+               if (crumbs.length == 2){
+                  var first = crumbs.eq(0), firstWidth = first.width(),
+                     last = crumbs.eq(1), lastWidth = last.width();
+
+                  if (firstWidth > halfWidth && lastWidth > halfWidth){
+                     $('.controls-BreadCrumbs__title', crumbs).css('max-width', halfWidth);
+                  } else {
+                     if (firstWidth > halfWidth) {
+                        $('.controls-BreadCrumbs__title', first).css('max-width', containerWidth - width - lastWidth);
+                     } else {
+                        $('.controls-BreadCrumbs__title', last).css('max-width', containerWidth - width - firstWidth);
+                     }
+                  }
                } else {
-                  $('.controls-BreadCrumbs__title', points).css('max-width', containerWidth - width);
+                  $('.controls-BreadCrumbs__title', crumbs).css('max-width', containerWidth - width);
                }
             }
          }
@@ -266,7 +278,7 @@ define('js!SBIS3.CONTROLS.BreadCrumbs', [
       _initNonTextElementSizes: function(){
          if (!this._homeIconWidth || !this._arrowWidth){
             this._homeIconWidth = $('.controls-BreadCrumbs__crumb-home', this._container).outerWidth(true);
-            this._arrowWidth = $('.controls-BreadCrumbs__arrow', this._container).outerWidth(true);
+            this._arrowWidth = $('.controls-BreadCrumbs__arrow', this._container).last(0).outerWidth(true);
          }
       },
 
