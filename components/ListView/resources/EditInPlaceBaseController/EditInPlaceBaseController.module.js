7e43bbae57f5225fcee3bf98beb72492ec6f9085
am.gerasimov <am.gerasimov@tensor.ru>
2016-06-06 13:00:55 +0300

    https://inside.tensor.ru/opendoc.html?guid=cf12c993-e7b2-496f-9286-0102af58152f&description= Ошибка в разработку: Интеграционное тестирование CONTROLS - ListView вложенные друг в друга: при наведении мыши на опц...

--- components/ListView/resources/EditInPlaceBaseController/EditInPlaceBaseController.module.js
+++ components/ListView/resources/EditInPlaceBaseController/EditInPlaceBaseController.module.js
@@ -203,9 +203,10 @@ define('js!SBIS3.CONTROLS.EditInPlaceBaseController',
                return this.endEdit(true).addCallback(function() {
                   return self._prepareEdit(record).addCallback(function(preparedRecord) {
                      if (preparedRecord) {
-                        self._lastTargetAdding = self._options.itemsProjection.getItemBySourceItem(preparedRecord);
-                        self._eip.edit(target, preparedRecord, self._lastTargetAdding);
+                        var itemProjItem = self._options.itemsProjection.getItemBySourceItem(preparedRecord);
+                        self._eip.edit(target, preparedRecord, itemProjItem);
                         self._notify('onAfterBeginEdit', preparedRecord);
+                        self._lastTargetAdding = itemProjItem;
                         return preparedRecord;
                      }
                      return preparedRecord;
