dae89cb9fd8a0c0d85a0af8bb6e6f9a20ba2c076
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2016-06-07 17:22:44 +0300

    Revert "Рендерим дробную часть без округления"
    This reverts commit fa38330fd3103c981638c80b059069952075b4fa.

--- components/NumberTextBox/NumberTextBox.module.js
+++ components/NumberTextBox/NumberTextBox.module.js
@@ -279,8 +279,7 @@ define('js!SBIS3.CONTROLS.NumberTextBox', ['js!SBIS3.CONTROLS.TextBox', 'html!SB
             this._options.delimiters,
             decimals,
             this._options.onlyPositive,
-            this._options.maxLength,
-            true
+            this._options.maxLength
          );
          return value || '';
       },
