741ee3b68fa083f644e821d646c5bd5df35888e2
am.gerasimov <am.gerasimov@tensor.ru>
2016-06-15 11:26:19 +0300

    https://inside.tensor.ru/opendoc.html?guid=e5b5a27d-f152-4587-9974-5da8ad14c7b7&description= Ошибка в разработку: Список сотрудников. Не видно верх страницы. Сотрудники, проскролить страницу вниз, открыть подр...

--- components/Mixins/ItemsControlMixin/ItemsControlMixin.module.js
+++ components/Mixins/ItemsControlMixin/ItemsControlMixin.module.js
@@ -1517,6 +1517,10 @@ define('js!SBIS3.CONTROLS.ItemsControlMixin', [
       _scrollTo: function scrollTo(target, container) {
          var scrollContainer = container || this._getScrollContainer(),
              scrollContainerOffset = scrollContainer.offset(),
+             channel = $ws.single.EventBus.globalChannel(),
+         //FIXME решение для 3.7.3.200, чтобы правильно работал скролл при scrollIntoView
+             /* Оповестим аккордион, о том что контент проскролен, иначе он не заметит и не сместит свой скролл */
+             scrollNotify = channel.notify.bind(channel, 'ContentScrolling', null),
              targetOffset;
 
          if (typeof target === 'string') {
@@ -1527,8 +1531,10 @@ define('js!SBIS3.CONTROLS.ItemsControlMixin', [
 
          if( (targetOffset.top - scrollContainerOffset.top - scrollContainer.scrollTop()) < 0) {
             target[0].scrollIntoView(true);
+            scrollNotify();
          } else if ( (targetOffset.top + target.height() - scrollContainerOffset.top - scrollContainer.scrollTop()) > scrollContainer[0].clientHeight) {
             target[0].scrollIntoView(false);
+            scrollNotify();
          }
       },
       _scrollToItem: function(itemId) {
