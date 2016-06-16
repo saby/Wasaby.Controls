fb7e27fb010f5fca480edd0b202afc634dabb15b
aa.malcev2 <aa.malcev2@tensor.ru>
2016-06-16 17:34:59 +0300

    подключил ws-data

--- depencyCollector.js
+++ depencyCollector.js
@@ -5,23 +5,25 @@ var fs = require('fs'),
    path = require('path'),
    jsModules = {},
    baseResources = path.join(__dirname, 'components'),
+   dataResources = path.join(__dirname, 'ws-data/lib'),
    newPath = '',
-dirWalker = function(dir){
+dirWalker = function(dir, pattern){
    var files = fs.readdirSync(dir);
    for (var i = 0; i < files.length; i++){
       newPath = path.join(dir, files[i]);
       if (fs.statSync(newPath).isDirectory()){
-         dirWalker(newPath);
+         dirWalker(newPath, pattern);
       }
       else {
-         if (/\.module\.js$/.test(files[i])){
+         if (new RegExp(pattern + '$').test(files[i])){
             require(newPath);
          }
       }
    }
 };
 
-dirWalker(baseResources);
+dirWalker(baseResources, '\\.module\\.js');
+dirWalker(dataResources, '\\.js');
 
 fs.writeFileSync(path.join(__dirname, 'components/contents.json'), JSON.stringify({jsModules: jsModules}, null, 3));
 fs.writeFileSync(path.join(__dirname, 'components/contents.js'), 'contents = ' + JSON.stringify({jsModules: jsModules}, null, 3) + ';');
