8dc8b751775df4d3e04b49d9d92415d97390288d
aa.malcev2 <aa.malcev2@tensor.ru>
2016-06-16 11:23:50 +0300

    удалил устарвшие данные

--- components/__DataSet/DataStrategy/ArrayStrategy.module.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by as.manuylov on 10.11.14.
- */
-define('js!SBIS3.CONTROLS.ArrayStrategy', ['js!SBIS3.CONTROLS.Data.Adapter.Json'], function (AdapterJson) {
-   'use strict';
-   $ws.single.ioc.resolve('ILogger').error('ArrayStrategy', 'Module SBIS3.CONTROLS.ArrayStrategy has been removed in 3.7.3.100. Use SBIS3.CONTROLS.Data.Adapter.Json instead');
-   return AdapterJson;
-});
