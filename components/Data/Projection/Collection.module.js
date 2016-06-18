e5a7398f224809803c15719359f59ed08f8d6e27
Aleksey Maltsev <aleksey.a.maltsev@gmail.com>
2016-06-18 10:53:40 +0300

    возвращаем, что ничего не поменялось вместо исключения.

--- components/Data/Projection/Collection.module.js
+++ components/Data/Projection/Collection.module.js
@@ -1101,9 +1101,18 @@ define('js!SBIS3.CONTROLS.Data.Projection.Collection', [
                
                case 'moved':
                   //собираем перемещенные элементы
-                  /*if (before.length !== after.length) {
-                     throw new Error('The "before" and "after" arrays are not synchronized by the length - "move" can\'t be applied.');
-                  }*/
+                  if (before.length !== after.length) {
+                     //TODO: вернуть в 3.7.4.
+                     //throw new Error('The "before" and "after" arrays are not synchronized by the length - "move" can\'t be applied.');
+                     return {
+                        newItems: [],
+                        newItemsIndex: 0,
+                        oldItems: [],
+                        oldItemsIndex: 0,
+                        endAt: -1,
+                        offset: 0
+                     };
+                  }
                   if (!beforeItem || beforeItem === afterItem) {
                      continue;
                   }
