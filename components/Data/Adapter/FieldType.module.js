9c2b2e7f6bbcc493c8305d7968f22d2b13867970
aa.malcev2 <aa.malcev2@tensor.ru>
2016-06-20 17:20:15 +0300

    FieldType -> SbisFieldType

--- components/Data/Adapter/FieldType.module.js
+++ components/Data/Adapter/FieldType.module.js
@@ -1,5 +1,5 @@
-define('js!SBIS3.CONTROLS.Data.Adapter.FieldType', ['js!WS.Data.Adapter.FieldType'], function(FieldType){
+define('js!SBIS3.CONTROLS.Data.Adapter.FieldType', ['js!WS.Data.Adapter.SbisFieldType'], function(FieldType){
    'use strict';
-   $ws.single.ioc.resolve('ILogger').error('SBIS3.CONTROLS.Data.Adapter.FieldType', 'Module is no longer available since version 3.7.4.100. Use WS.Data.Adapter.FieldType instead.');
+   $ws.single.ioc.resolve('ILogger').error('SBIS3.CONTROLS.Data.Adapter.FieldType', 'Module is no longer available since version 3.7.4.100. Use WS.Data.Adapter.SbisFieldType instead.');
    return FieldType;
 });
