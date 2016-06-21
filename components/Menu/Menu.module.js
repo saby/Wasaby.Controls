dca162519327dcfa45b2e4fe4866583a4b402598
am.gerasimov <am.gerasimov@tensor.ru>
2016-06-21 16:18:41 +0300

    https://inside.tensor.ru/opendoc.html?guid=623c7548-36f6-4a76-9067-d9ba8bebb9dd&description= Ошибка в разработку 17.06.2016 При повторном вводе пользователя в диалоге 'Права' в поле кому отсутствует подсказка реестр Мои/О...

--- components/Menu/Menu.module.js
+++ components/Menu/Menu.module.js
@@ -309,6 +309,16 @@ define('js!SBIS3.CONTROLS.Menu', [
          return config;
       },
 
+      /* Само меню не должно вызывать перерасчёта у соседних элементов,
+         т.к. создаётся абсолютом в body, однако, в меню могу лежать контролы,
+         которым требуется расчитывать ширину, поэтому запускаем расчёты только для дочерних */
+      _setVisibility: function(show) {
+         Menu.superclass._setVisibility.apply(this, arguments);
+         if(show) {
+            this._resizeChilds();
+         }
+      },
+
       destroy : function(){
          Menu.superclass.destroy.call(this);
          this.destroySubObjects();
