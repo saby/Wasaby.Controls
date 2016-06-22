c7f67579870113be16681f7b5be02370d30b6f57
as.avramenko <as.avramenko@tensor.ru>
2016-06-22 14:04:32 +0300

    При перезагрузке разблокируем тулбар https://inside.tensor.ru/opendoc.html?guid=22d3a1f4-5c40-4f9e-9548-b69a13a3203c&description= Ошибка в разработку 16.06.2016 Невозможно закрыть исходящий счет, появляется лишний элемент интерфейса, если была изменена наша ...

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -1128,6 +1128,7 @@ define('js!SBIS3.CONTROLS.ListView',
             this._reloadInfiniteScrollParams();
             this._previousGroupBy = undefined;
             this._firstScrollTop = true;
+            this._unlockItemsToolbar();
             this._hideItemsToolbar();
             this._destroyEditInPlace();
             return ListView.superclass.reload.apply(this, arguments);
@@ -1519,6 +1520,11 @@ define('js!SBIS3.CONTROLS.ListView',
           */
          _showItemsToolbar: function(target) {
             this._getItemsToolbar().show(target, this._touchSupport);
+         },
+         _unlockItemsToolbar: function() {
+            if (this._itemsToolbar) {
+               this._itemsToolbar.unlockToolbar();
+            }
          },
          _hideItemsToolbar: function (animate) {
             if (this._itemsToolbar) {
