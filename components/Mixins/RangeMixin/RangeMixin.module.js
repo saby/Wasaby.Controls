31fb57dd565b75d6b62b0c2807aeaaa3f63476d7
au.mironov <au.mironov@tensor.ru>
2016-06-23 17:31:48 +0600

    Миксин, добавляющий поведение хранения начального и конечного значений диапазона.
    https://inside.tensor.ru/opendoc.html?guid=4fb71d50-8fd7-4e8d-8702-7fe5ee0baff3&description=
    Задача в разработку 15.04.2016 Прошу реализовать новый вид Большого выбора периода в соответствии со спецификацией http://axure...

---
+++ components/Mixins/RangeMixin/RangeMixin.module.js
@@ -0,0 +1,154 @@
+define('js!SBIS3.CONTROLS.RangeMixin', [], function() {
+
+   /**
+    * Миксин, добавляющий поведение хранения начального и конечного значений диапазона.
+    * @mixin SBIS3.CONTROLS.RangeMixin
+    * @author Миронов Александр Юрьевич
+    * @public
+    */
+
+   var RangeMixin = /**@lends SBIS3.CONTROLS.RangeMixin.prototype  */{
+      $protected: {
+         _options: {
+            /**
+             * @cfg {Number|Date} Начальное значение диапазона
+             */
+            startValue: null,
+            /**
+             * @cfg {Number|Date} Конечное значение диапазона
+             */
+            endValue: null
+         }
+      },
+
+      $constructor: function() {
+         this._publish('onRangeChange', 'onStartValueChange', 'onEndValueChange');
+      },
+
+      /**
+       * Установить начальное значение диапазона.
+       * @param {Object} value Начальное значение.
+       * @param {Boolean} silent Если True, то события изменения свойства не генерируются.
+       * @see startValue
+       */
+      setStartValue: propertySetter('startValue', undefined, '_notifyOnRangeChangedAfterSetters'),
+
+      /**
+       * Получить начальное значение диапазона.
+       * @return {Object} Начальная дата диапазона.
+       * @see startValue
+       */
+      getStartValue: function() {
+         return this._options.startValue;
+      },
+
+      /**
+       * Установить конечное значение диапазона.
+       * @param {Object} value Конечное значение диапазона.
+       * @param {Boolean} silent Если True, то события изменения свойства не генерируются.
+       * @see endValue
+       */
+      setEndValue: propertySetter('endValue', undefined, '_notifyOnRangeChangedAfterSetters'),
+
+      /**
+       * Получить конечное значение диапазона.
+       * @return {Object} Конечная дата диапазона.
+       * @see endValue
+       */
+      getEndValue: function() {
+         return this._options.endValue;
+      },
+
+      /**
+       * Установить начальное и конечное значение.
+       * Необходимо использовать, если надо одновлеменно установить начальное и конечное значение. Если вместо этого
+       * использовать setStartValue и setEndValue, то событие onRangeChange будет сгенерировано 2 раза.
+       * @param startValue Начальное значение диапазона.
+       * @param endValue Конечное значение диапазона.
+       */
+      setRange: function(startValue, endValue) {
+         var changed = false;
+
+         if (this.setStartValue(startValue, true)) {
+            this._notifyOnPropertyChanged('startValue');
+            this._notify('onStartValueChange', this._options.startValue);
+            changed = true;
+         }
+
+         if (this.setEndValue(endValue, true)) {
+            this._notifyOnPropertyChanged('endValue');
+            this._notify('onEndValueChange', this._options.endValue);
+            changed = true;
+         }
+
+         if (changed) {
+            this._notifyOnRangeChanged();
+         }
+      },
+
+      _notifyOnRangeChangedAfterSetters: function (value, oldValue, silent) {
+         if (!silent) {
+            this._notifyOnRangeChanged();
+         }
+      },
+
+      _notifyOnRangeChanged: function () {
+         this._notify('onRangeChange', this._options.startValue, this._options.endValue);
+      }
+   };
+
+
+   /**
+    * Фабрика, генерирующая сеттеры свойства для модулей.
+    * Для хранения значения свойства использует переменную в поле _options у объекта.
+    * @param optionName {String} Название переменной в поле _options в объекте.
+    * @param validationFunction {Function} Функция проверяющая граничные условия для устанавливаемого значения.
+    * На вход принимает утанавливаемое значение. Должна вернуть скоректированное устанавливаемое значение,
+    * которое будет установлено для свойства. Или сгенерировать исключение.
+    * быть перерисовано. Для работы этого параметра необходимо что бы к классу был подключен ViewValidateMixin.
+    * @param afterFunction {Function} Функция которая вызывается после того как значение свойства было установлено.
+    * Вместо этой функции лучше использовать подписку на события изменения свойства. В отличии от подписки на события,
+    * эта функция вызывается для silent вызовов сеттера которые не генерируют события.
+    * @return {Function}
+    */
+   function propertySetter (optionName, validationFunction, afterFunction) {
+      var propertyWrap =  function (value, silent) {
+         var oldValue = this._options[optionName],
+            eventName;
+
+         if (oldValue === value) {
+            return false;
+         }
+
+         if (validationFunction) {
+            value = callFunction(this, validationFunction, [value, oldValue]);
+         }
+
+         this._options[optionName] = value;
+
+         if (!silent) {
+            this._notifyOnPropertyChanged(optionName);
+            eventName = 'on' + optionName.charAt(0).toUpperCase() + optionName.slice(1) + 'Change';
+            this._notify(eventName, value);
+         }
+         if (afterFunction) {
+            callFunction(this, afterFunction, [value, oldValue, silent]);
+         }
+
+         return true;
+      };
+      return propertyWrap;
+   }
+
+   function callFunction (obj, func, args) {
+      switch(typeof func) {
+         case 'function':
+            return func.apply(obj, args);
+         case 'string':
+            return obj[func].apply(obj, args);
+      }
+   }
+
+   return RangeMixin;
+
+});
