de55d70485d2979727104e95417434194ad4be5f
pa.filippov <pa.filippov@tensor.ru>
2016-06-28 15:43:14 +0300

    убираю зависимость, будем сами подписываться на событие

--- components/DataGridView/DataGridView.module.js
+++ components/DataGridView/DataGridView.module.js
@@ -14,10 +14,9 @@ define('js!SBIS3.CONTROLS.DataGridView',
       'browser!html!SBIS3.CONTROLS.DataGridView/resources/ItemTemplate',
       'browser!html!SBIS3.CONTROLS.DataGridView/resources/ItemContentTemplate',
       'browser!html!SBIS3.CONTROLS.DataGridView/resources/cellTemplate',
-      'browser!html!SBIS3.CONTROLS.DataGridView/resources/GroupTemplate',
-      'js!SBIS3.FixedHeaderManager'
+      'browser!html!SBIS3.CONTROLS.DataGridView/resources/GroupTemplate'
    ],
-   function(ListView, dotTplFn, rowTpl, colgroupTpl, headTpl, resultsTpl, MarkupTransformer, DragAndDropMixin, groupByTpl, LadderDecorator, TemplateUtil, ItemTemplate, ItemContentTemplate, cellTemplate, GroupTemplate, FixedHeaderManager) {
+   function(ListView, dotTplFn, rowTpl, colgroupTpl, headTpl, resultsTpl, MarkupTransformer, DragAndDropMixin, groupByTpl, LadderDecorator, TemplateUtil, ItemTemplate, ItemContentTemplate, cellTemplate, GroupTemplate) {
    'use strict';
 
       var ANIMATION_DURATION = 500; //Продолжительность анимации скролла заголовков
@@ -471,9 +470,6 @@ define('js!SBIS3.CONTROLS.DataGridView',
              В этом случае плавающая ширина скукоживаться не будет.
              Пример можно посмотреть в реестре номенклатур. */
             this._setColumnWidthForPartScroll();
-         }
-         if (this._options.stickyHeader){
-            FixedHeaderManager.tableHeadRedrawn(this.getContainer().find('table:first'));
          }
          this._notify('onDrawHead');
       },
