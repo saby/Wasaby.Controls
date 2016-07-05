229f9054e7dca49e45a58bbbe67b7af7f42b0a1b
ganshinyao <yo.ganshin@tensor.ru>
2016-07-05 16:05:41 +0300

    https://inside.tensor.ru/opendoc.html?guid=e4d88577-22b7-49c3-8d21-2663f6a7ffb3&description= Задача в разработку 08.06.2016 Реализовать возможность связать 2 списочных контрола через перетаскивание элементов одного на дру...

--- components/Mixins/DragAndDropMixin/DragCurrentElement.module.js
+++ components/Mixins/DragAndDropMixin/DragCurrentElement.module.js
@@ -1,7 +1,7 @@
 define('js!SBIS3.CONTROLS.DragCurrentElement', [], function() {
    'use strict';
    /**
-    * Синглтон в котором хранится элемент который сейчас перетаскивают
+    * Синглтон который обеспечивает работу с элементом который сейчас перетаскивают
     *
     * @class SBIS3.CONTROLS.DragTarget
     * @control
@@ -12,34 +12,56 @@ define('js!SBIS3.CONTROLS.DragCurrentElement', [], function() {
 
       $protected: {
          _owner: undefined,
-         _currentElementConfig:undefined,
+         _currentElement:undefined,
          _avatar: undefined,
          _dragging: undefined
       },
-
+      /**
+       * Возвращает перетаскиваемый элемент
+       * @returns {Object}
+       */
       get: function() {
-         return this._currentElementConfig;
+         return this._currentElement;
       },
-
-      set: function(config, owner) {
-         this._currentElementConfig = config;
+      /**
+       * Устанавливает текущий элемент
+       * @param {Object} element - элемент который перетаскивают
+       * @param {SBIS3.CONTROLS.Control} owner - контрол который устанавливает текущий элемент
+       */
+      set: function(element, owner) {
+         this._currentElement = element;
          this._owner = owner;
       },
-
+      /**
+       * Возвращает контрол которому принадлежит элемент
+       * @returns {SBIS3.CONTROLS.Control}
+       */
       getOwner: function() {
          return this._owner;
       },
-
+      /**
+       * Очищает текущий элемент
+       */
       reset: function () {
          this._currentElementConfig = undefined;
          this._owner = undefined;
          this.setAvatar(null);
       },
-
+      /**
+       * Возвращает аватар, иконка которая отображается около курсора
+       * @see setAvatar
+       * @see removeAvatar
+       * @returns {JQuery}
+       */
       getAvatar: function() {
          return this._avatar;
       },
-
+      /**
+       * Устанавливает аватар
+       * @param {JQuery} avatar
+       * @see getAvatar
+       * @see removeAvatar
+       */
       setAvatar: function(avatar) {
          this.removeAvatar();
          if (avatar) {
@@ -47,19 +69,28 @@ define('js!SBIS3.CONTROLS.DragCurrentElement', [], function() {
          }
          this._avatar = avatar;
       },
-
+      /**
+       * удаляет аватар
+       */
       removeAvatar: function() {
          if (this._avatar) {
             this._avatar.remove();
          }
       },
-
+      /**
+       * Вернет true если сейчас тащат элемент
+       * @returns {Boolean}
+       */
       isDragging: function() {
          return this._dragging;
       },
-
+      /**
+       * @protected
+       * Устанавливает признак перемещения вызывается только из SBIS3.CONTROLS.DragNDropMixin
+       * @param {Boolean} dragging
+       */
       setDragging: function(dragging) {
-         this._dragging = dragging;
+         this._dragging = !!dragging;
       }
    });
 
