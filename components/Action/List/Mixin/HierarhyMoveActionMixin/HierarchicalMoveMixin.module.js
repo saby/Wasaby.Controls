f941d613c35582ec581312073b655071d099de6b
ganshinyao <yo.ganshin@tensor.ru>
2016-07-13 18:27:45 +0300

    https://inside.tensor.ru/opendoc.html?guid=cc1e2a8d-f149-4694-a47a-0d565757ebfd&description= Ошибка в разработку 17.05.2016 Совсем не BestPractices - каждую выбранную запись перетаскивать в папку отдельным обращением в БЛ...

--- components/Action/List/Mixin/HierarhyMoveActionMixin/HierarchicalMoveMixin.module.js
+++ components/Action/List/Mixin/HierarhyMoveActionMixin/HierarchicalMoveMixin.module.js
@@ -1,7 +1,8 @@
 /*global define, $ws*/
 define('js!SBIS3.CONTROLS.Action.List.HierarchicalMoveMixin',[
+      'js!WS.Data/Collection/RecordSet'
    ],
-   function () {
+   function (RecordSet) {
       'use strict';
       /**
        * Действие перемещения
@@ -26,17 +27,38 @@ define('js!SBIS3.CONTROLS.Action.List.HierarchicalMoveMixin',[
           */
          hierarhyMove: function (from, to) {
             var def = new $ws.proto.ParallelDeferred(),
-               newParent = to ? to.getId() : null,
-               dataSource = this._options.linkedObject.getDataSource(),
-               self = this;
-            if ($ws.helpers.type(from) !== 'array') {
-               from = [from];
+               self = this,
+               updateItems;
+
+            this._setParent(from, to);
+
+            if ($ws.helpers.type(from) === 'array') {
+               if (from.length > 1) {
+                  updateItems = new RecordSet({
+                     adapter: from[0].getAdapter()
+                  });
+                  updateItems.append(from);
+               } else {
+                  updateItems = from[0];
+               }
+            } else {
+               updateItems = from;
             }
-            $ws.helpers.forEach(from, function (record) {
-               record.set(self._options.parentProperty, newParent);
-               def.push(dataSource.update(record));
-            });
-            return def.done().getResult();
+
+            return self._options.dataSource.update(updateItems);
+         },
+
+         _setParent: function(from, to) {
+            var newParent = to ? to.getId() : null,
+               records = $ws.helpers.type(from) !== 'array' ? [from] : from;
+
+            for (var i = 0, len = records.length; i < len; i++) {
+               var record = records[i];
+               record.set(this._options.hierField, newParent);
+               if (record.getOwner()) {
+                  record.getOwner()._reindexTree(this._options.hierField);
+               }
+            }
          }
       };
    }
