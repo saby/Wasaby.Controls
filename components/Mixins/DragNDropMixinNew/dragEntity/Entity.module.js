ca4019e3df40a35aa3593e0545eb82660d05db64
ganshinyao <yo.ganshin@tensor.ru>
2016-07-15 15:06:43 +0300

    https://inside.tensor.ru/opendoc.html?guid=e4d88577-22b7-49c3-8d21-2663f6a7ffb3&description= Задача в разработку 08.06.2016 Реализовать возможность связать 2 списочных контрола через перетаскивание элементов одного на дру...

---
+++ components/Mixins/DragNDropMixinNew/dragEntity/Entity.module.js
@@ -0,0 +1,17 @@
+/*global define, $ws, $*/
+define('js!SBIS3.CONTROLS.DragEntity.Entity', [], function() {
+   'use strict';
+   /**
+    * Абстрактная перемещаемая сущность
+    *
+    * @class SBIS3.CONTROLS.DragEntity.Entity
+    * @control
+    * @public
+    * @author Крайнов Дмитрий Олегович
+    */
+   var Entity = $ws.proto.Abstract.extend(/**@lends SBIS3.CONTROLS.DragEntity.Entity*/{
+
+   });
+
+   return Entity;
+});
