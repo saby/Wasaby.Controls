4761fab00c1411008eb3aff28cf5489cf8f863b8
as.krasilnikov <as.krasilnikov@tensor.ru>
2016-07-22 14:54:02 +0300

    делаем unsibscribe событий, т.к. можем задестроить шаблон, но не задестроить саму панель
    https://inside.tensor.ru/opendoc.html?guid=78d02953-6650-4c0d-a234-2e727b4be394&description=
    Ошибка в разработку 21.07.2016 Задачи. Появилась возможность создать задачу с незаполненными обязательными полями. kuda/a1s1d1...

--- components/FormController/FormController.module.js
+++ components/FormController/FormController.module.js
@@ -185,30 +185,35 @@ define('js!SBIS3.CONTROLS.FormController', ['js!SBIS3.CORE.CompoundControl', 'js
          });
          //Выписал задачу, чтобы при событии onBeforeClose стрелял метод у floatArea, который мы бы переопределили здесь,
          //чтобы не дергать getTopParent
-         this._panel.subscribe('onBeforeClose', function(event, result){
-            var record = this._options.record;
-            //Если попали сюда из метода _saveRecord, то this._saving = true и мы просто закрываем панель
-            if (this._saving || !(record && record.isChanged())){
-               //Дестроим запись, когда выполнены три условия
-               //1. если это было создание
-               //2. если есть ключ (метод создать его вернул)
-               //3. ничего не поменяли в рекорде, но закрывают либо поменяли, но нажали нет
-               if (this._newRecord && record.getId() && (!this._saving && !record.isChanged() || result === false)){
-                  this._destroyModel();
-               }
-               this._saving = false;
-               this._resetTitle();
-               return;
-            }
-            event.setResult(false);
-            this._saveRecord({});
-         }.bind(this));
-         var self = this;
+         this._panel.subscribe('onBeforeClose', this._onBeforeCloseHandler);
          this._panelReadyDeferred = new $ws.proto.Deferred();
-         this._panel.once('onAfterShow', function() {
-            self._updateIndicatorZIndex();
-            self._panelReadyDeferred.callback();
-         });
+         this._panel.subscribe('onAfterShow', this._onAfterShowHandler);
+      },
+
+      _onAfterShowHandler: function(){
+         var self = this._getTemplateComponent();
+         self._updateIndicatorZIndex();
+         self._panelReadyDeferred.callback();
+      },
+
+      _onBeforeCloseHandler: function(event, result){
+         var self = this._getTemplateComponent(),
+             record = self._options.record;
+         //Если попали сюда из метода _saveRecord, то this._saving = true и мы просто закрываем панель
+         if (self._saving || !(record && record.isChanged())){
+            //Дестроим запись, когда выполнены три условия
+            //1. если это было создание
+            //2. если есть ключ (метод создать его вернул)
+            //3. ничего не поменяли в рекорде, но закрывают либо поменяли, но нажали нет
+            if (self._newRecord && record.getId() && (!self._saving && !record.isChanged() || result === false)){
+               self._destroyModel();
+            }
+            self._saving = false;
+            self._resetTitle();
+            return;
+         }
+         event.setResult(false);
+         self._saveRecord({});
       },
 
       _onBeforeNavigate: function(event, activeElement, isIconClick){
@@ -733,6 +738,12 @@ define('js!SBIS3.CONTROLS.FormController', ['js!SBIS3.CORE.CompoundControl', 'js
          else{
             this.activateFirstControl();
          }
+      },
+
+      destroy: function(){
+         this._panel.unsubscribe('onAfterShow', this._onAfterShowHandler);
+         this._panel.unsubscribe('onBeforeClose', this._onBeforeCloseHandler);
+         FormController.superclass.destroy.apply(this, arguments);
       }
    });
       //todo Костыль, позволяющий с прототипа компонента вычитать запись до инициализации компонента и прокинуть ее в опции. Сделано в рамках ускорения
