c4d97e09942489e801b69a899f840b645c212a45
pv.stepin <pv.stepin@tensor.ru>
2016-07-26 15:38:51 +0300

    https://inside.tensor.ru/opendoc.html?guid=e4ab771b-27ea-4498-8594-7dd53eae5ef1&description= Ошибка в разработку 18.07.2016 Контакты - Лента новостей Скроллим ленту вниз и вверх, видно что она 'дергается'

--- components/Utils/ScrollWatcher.module.js
+++ components/Utils/ScrollWatcher.module.js
@@ -48,7 +48,8 @@ define('js!SBIS3.CONTROLS.ScrollWatcher', [], function() {
          _scrollingContainer: undefined,
          _onWindowScrollHandler : undefined,
          _floatAreaScrollHandler : undefined,
-         _onContainerScrollHandler: undefined
+         _onContainerScrollHandler: undefined,
+         _isWSBodyScrollingContent: false
       },
 
       $constructor: function() {
@@ -67,6 +68,7 @@ define('js!SBIS3.CONTROLS.ScrollWatcher', [], function() {
             var scrollingContent = $('.ws-body-scrolling-content');
             if (scrollingContent && scrollingContent.length){
                this._scrollingContainer = scrollingContent;
+               this._isWSBodyScrollingContent = true;
                this._type = 'container';
             }
          }
@@ -75,10 +77,13 @@ define('js!SBIS3.CONTROLS.ScrollWatcher', [], function() {
          if (this._inContainer()) {
             this._onContainerScrollHandler =  this._onContainerScroll.bind(this);
             this._scrollingContainer.bind('scroll.wsScrollWatcher', this._onContainerScrollHandler);
-            //Нужно чтобы вызвать скролл у контейнеров без видимого скролла.
-            $ws.helpers.wheel(this._scrollingContainer, function(event){
-               $(self._scrollingContainer).scrollTop($(self._scrollingContainer).scrollTop() - event.wheelDelta/2);
-            });
+
+            if(!this._isWSBodyScrollingContent){
+               //Нужно чтобы вызвать скролл у контейнеров без видимого скролла.
+               $ws.helpers.wheel(this._scrollingContainer, function(event){
+                  $(self._scrollingContainer).scrollTop($(self._scrollingContainer).scrollTop() - event.wheelDelta/2);
+               });
+            }
 
          } else if (this._inWindow()) {
             this._onWindowScrollHandler = this._onWindowScroll.bind(this);
