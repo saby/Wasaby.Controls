9b02436741fc9efbe92de698d6f6dbafa2cf4cc7
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2016-08-04 14:58:19 +0300

    новый класс

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -59,11 +59,12 @@ define('js!SBIS3.CONTROLS.ListView',
        * @cssModifier controls-ListView__orangeMarker Показывать маркер активной строки у элементов ListView. Актуально только для ListView.
        * @cssModifier controls-ListView__showCheckBoxes Чекбоксы показываются не по ховеру, а сразу все.
        * @cssModifier controls-ListView__hideCheckBoxes Скрыть все чекбоксы.
-       * @cssModifier controls-DragNDropMixin__notDraggable За помеченные данным селектором элементы Drag&Drop производиться не будет.
        * @cssModifier controls-ListView__bottomStyle Оформляет операции строки под строкой
        * @cssModifier controls-ListView__pagerNoSizePicker Скрыть выбор размера страницы в пейджинге.
        * @cssModifier controls-ListView__pagerNoAmount Скрыть отображение количества записей на странице в пейджинге.
        * Т.е. текст "1-10" при отображении 10 записей на 1-ой странице
+       *
+       * @css controls-DragNDropMixin__notDraggable За помеченные данным селектором элементы Drag&Drop производиться не будет.
        */
 
       /*TODO CommonHandlers MoveHandlers тут в наследовании не нужны*/
