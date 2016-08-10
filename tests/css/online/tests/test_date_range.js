19db0ddb1d0071ff8edd18798af20e17aaa40503
da.dubenec <da.dubenec@tensor.ru>
2016-08-10 13:11:06 +0300

    refactor test

--- tests/css/online/tests/test_date_range.js
+++ tests/css/online/tests/test_date_range.js
@@ -8,8 +8,8 @@ gemini.suite('SBIS3.CONTROLS.DateRange Online', function () {
 
             .before(function (actions, find) {
                 actions.waitForElementToShow('[name="DateRange 1"]', 40000);
-				actions.waitForElementToShow('[name="DateRange__Button"] .controls-Button__text', 40000);
-				this.open_chooser = find('[name="DateRange__Button"] .controls-Button__text');
+				actions.waitForElementToShow('[name="DateRange__Button"]', 40000);
+				this.open_chooser = find('[name="DateRange__Button"]');
 				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
                 this.input = find('[sbisname="TextBox 1"] input');
             })
@@ -20,9 +20,12 @@ gemini.suite('SBIS3.CONTROLS.DateRange Online', function () {
 
             .capture('opened_chooser', function (actions) {
                 actions.click(this.open_chooser);
-				actions.waitForElementToShow('table.ws-date-range-choose-table', 1000);
-				actions.waitForElementToShow('[sbisname="apply"] .ws-button-caption', 1000);
-				actions.waitForElementToShow('.ws-window-titlebar-action.close', 1000);
+				actions.waitForElementToShow('.controls-RangeSelectable__item.controls-DateRangeBigChoose-MonthView:nth-child(3)', 1000);
+				actions.waitForElementToShow('.controls-RangeSelectable__item.controls-DateRangeBigChoose-MonthView:nth-child(6)', 1000);
+				actions.waitForElementToShow('.controls-RangeSelectable__item.controls-DateRangeBigChoose-MonthView:nth-child(9)', 1000);
+				actions.waitForElementToShow('.controls-RangeSelectable__item.controls-DateRangeBigChoose-MonthView:nth-child(12)', 1000);
+				actions.waitForElementToShow('[sbisname="ApplyButton"]', 1000);
+				actions.waitForElementToShow('[sbisname="CloseButton"]', 1000);
             })
 			/*
 			.capture('selected_week', function (actions, find) {
@@ -31,17 +34,15 @@ gemini.suite('SBIS3.CONTROLS.DateRange Online', function () {
 				actions.mouseMove(this.april);
 				this.day = find('table tr[week="4"] td:nth-child(3)');
 				actions.click(this.day);
-            })*/
-			
+            })
 			.capture('selected_range', function (actions, find) {
-				/*this.check_range = find('[sbisname="date-range-choose-TypeSelection"] div.box');
-				actions.click(this.check_range);*/
+				this.check_range = find('[sbisname="date-range-choose-TypeSelection"] div.box');
+				actions.click(this.check_range);
 				this.day1 = find('table tr[week="2"] td:nth-child(2)');
 				actions.click(this.day1);
 				this.day2 = find('table tr[week="4"] td:nth-child(5)');
 				actions.click(this.day2);
-            })
-			
+            })			
 			.capture('hovered_to_current_date', function (actions, find) {
 				this.to_current_date = find('[sbisname="ws-navigationButton"] a');
 				actions.mouseMove(this.to_current_date);
@@ -55,6 +56,6 @@ gemini.suite('SBIS3.CONTROLS.DateRange Online', function () {
 			.capture('hovered_close', function (actions, find) {
 				this.close = find('.ws-window-titlebar-action.close');
 				actions.mouseMove(this.close);
-            })
+            })*/
     });
 });
