8d5d38011145addf5f1b35266f8b7142949b5ef0
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2016-08-10 13:46:35 +0300

    https://inside.tensor.ru/opendoc.html?guid=c70703b2-b48c-420b-afbd-1b9f75a96f99&description= Задача в разработку 17.05.2016 Необходимо реализовать новый вид Разделителей(заголовков) в соответствии со спецификацией http:/...

--- components/LinkHeader/LinkHeader.module.js
+++ components/LinkHeader/LinkHeader.module.js
@@ -12,10 +12,22 @@ define('js!SBIS3.CONTROLS.LinkHeader',
       var LinkHeader = Header.extend({
          _dotTplFn: dotTplFn,
 
+         hrefTemplate: hrefTemplate,
+
+         _link: null,
+
          $protected: {
             _options: {
-               href: ''
+               href: '',
+
+               separator: false
             }
+         },
+
+         init: function(){
+            LinkHeader.superclass.init.call(this);
+
+            this._link = this.getChildControlByName('LinkHeader-caption');
          },
 
          getHref: function(){
@@ -24,6 +36,16 @@ define('js!SBIS3.CONTROLS.LinkHeader',
 
          setHref: function(link){
             this._options.href = link;
+            this._link.setHref(link);
+         },
+
+         getSeparator: function(){
+            return this._options.separator;
+         },
+
+         setSeparator: function(separator){
+            this._options.separator = separator;
+            this._container.html(this._options.hrefTemplate(this._options));
          }
       });
 
