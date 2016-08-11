16a5cfffbb908d2b87aadf6d406454c54c756cdd
da.dubenec <da.dubenec@tensor.ru>
2016-08-11 17:54:15 +0300

    new test

--- tests/css/online/tests/test_image.js
+++ tests/css/online/tests/test_image.js
@@ -26,4 +26,32 @@ gemini.suite('SBIS3.CONTROLS.Image Online', function () {
 				actions.wait(1000);
 			})
     });
+	
+	gemini.suite('with_edit_dialog', function (test) {
+
+        test.setUrl('/regression_image_online_2.html').setCaptureElements('html')
+
+            .before(function (actions, find) {
+                actions.waitForElementToShow('[name="Image 1"]', 40000);
+                this.image = find('[name="Image 1"] .controls-image__image');
+            })
+									
+            .capture('plain', function (actions) {
+				actions.mouseMove(this.image);
+				actions.wait(2000);				
+				actions.executeJS(function (window) {
+                    window.$('[sbisname="ButtonEdit"]').click();					
+                });				
+				actions.wait(2000);
+				actions.waitForElementToShow('[sbisname="applyButton"]', 5000);
+				actions.waitForElementToShow('.ws-window-title.controls-EditDialog__title', 5000);
+				actions.waitForElementToHide('[sbisname="ButtonEdit"]', 2000);
+				actions.executeJS(function (window) {
+					window.$('.jcrop-hline').removeClass('jcrop-hline');
+					window.$('.jcrop-vline').removeClass('jcrop-vline');
+                    window.$('.ws-browser-ajax-loader').addClass('ws-hidden');					
+                });				
+				actions.wait(1000);
+			})
+    });
 });
