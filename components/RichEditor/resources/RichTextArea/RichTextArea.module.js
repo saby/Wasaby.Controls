46684f3e378eddb3379c35a4f142d0b4eda70691
ps.borisov <ps.borisov@tensor.ru>
2016-08-15 16:08:33 +0300

    https://inside.tensor.ru/opendoc.html?guid=997af543-3b23-467d-b9aa-e074bdfca43d&description= Merge request 04.08.2016 Регламент: Ошибка в разработку. Автор: Писарева Н.А. Описание: Mozilla FF. БТР - после нажатия кн...

--- components/RichEditor/resources/RichTextArea/RichTextArea.module.js
+++ components/RichEditor/resources/RichTextArea/RichTextArea.module.js
@@ -619,6 +619,10 @@ define('js!SBIS3.CONTROLS.RichTextArea',
           */
          execCommand: function(command) {
             this._tinyEditor.execCommand(command);
+            //TODO:https://github.com/tinymce/tinymce/issues/3104, восстанавливаю выделение тк оно теряется если после нжатия кнопки назад редактор стал пустым
+            if ($ws._const.browser.firefox && command == 'undo' && this._getTinyEditorValue() == '') {
+               this._tinyEditor.selection.select(this._tinyEditor.getBody(), true);
+            }
          },
 
          /**
