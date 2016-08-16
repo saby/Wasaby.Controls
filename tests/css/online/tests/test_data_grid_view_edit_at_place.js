2c70a6c4b24a8bee254c0e2f01371d4e2a2b1d9a
da.dubenec <da.dubenec@tensor.ru>
2016-08-16 18:11:17 +0300

    Добавил новые тесты

--- tests/css/online/tests/test_data_grid_view_edit_at_place.js
+++ tests/css/online/tests/test_data_grid_view_edit_at_place.js
@@ -519,4 +519,29 @@ gemini.suite('SBIS3.CONTROLS.DataGridEditAtPlace Online', function () {
 				actions.wait(350);
             })
     });
+	
+	gemini.suite('items_actions_on_column_template_2', function (test) {
+
+        test.setUrl('/regression_data_grid_view_edit_at_place_online_14.html').setCaptureElements('html')
+
+            .before(function (actions, find) {
+                actions.waitForElementToShow('[sbisname="ТипНоменклатуры"]', 40000);
+                this.item3 = find('[sbisname="ТипНоменклатуры"] [data-id="3"]');
+				this.item7 = find('[sbisname="ТипНоменклатуры"] [data-id="7"]');
+				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
+                this.input = find('[name="TextBox 1"] .controls-TextBox__field');
+				this.cancell = find('[title="Отмена"]');
+				this.save = find('[title="Сохранить"]');
+				this.editor_input = find('[sbisname="Содержимоеbind"] input')
+            })
+
+            .capture('opened_editor', function (actions) {
+                actions.click(this.item3);
+				actions.waitForElementToShow('[title="Отмена"]', 2000);
+				actions.waitForElementToShow('[title="Сохранить"]', 2000);
+				actions.click(this.editor_input);
+				actions.sendKeys(this.editor_input, gemini.ARROW_RIGHT);
+				actions.sendKeys(this.editor_input, gemini.SHIFT+gemini.HOME);
+            })
+    });
 });
