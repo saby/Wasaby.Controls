93fb04fa2b68de68ffae2bd6fc94fe8394934cd2
au.mironov <au.mironov@tensor.ru>
2016-08-22 10:37:56 +0700

    DateRange ошибка валидации
    https://inside.tensor.ru/opendoc.html?guid=4fb71d50-8fd7-4e8d-8702-7fe5ee0baff3&description=
    Задача в разработку 15.04.2016 Прошу реализовать новый вид Большого выбора периода в соответствии со спецификацией http://axure...

--- components/DateRange/DateRange.module.js
+++ components/DateRange/DateRange.module.js
@@ -98,8 +98,8 @@ define('js!SBIS3.CONTROLS.DateRange', [
          this._options.startValue = this._options.startValue || this._options.startDate;
          this._options.endValue = this._options.endValue || this._options.endDate;
          // приводим даты к Date-типу и устанавливаем их в DatePicker-ах
-         this.setStartValue(this._options.startDate);
-         this.setEndValue(this._options.endDate);
+         this.setStartValue(this._options.startValue, false);
+         this.setEndValue(this._options.endValue, false);
 
          this._addDefaultValidator();
       },
@@ -108,7 +108,7 @@ define('js!SBIS3.CONTROLS.DateRange', [
          //Добавляем к прикладным валидаторам стандартный, который проверяет что дата начала периода меньше даты конца.
          this._options.validators.push({
             validator: function() {
-               return !(this._options.startDate && this._options.endDate && this._options.endDate < this._options.startDate);
+               return !(this._options.startValue && this._options.endValue && this._options.endValue < this._options.startValue);
             }.bind(this),
             errorMessage: rk('Дата начала периода не может быть больше даты окончания')
          });
