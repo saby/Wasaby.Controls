1309fa5b28dea8a881e270e3d08685523efc142d
da.dubenec <da.dubenec@tensor.ru>
2016-08-22 15:15:25 +0300

    new tests

--- tests/css/online/tests/test_data_grid_view.js
+++ tests/css/online/tests/test_data_grid_view.js
@@ -265,4 +265,31 @@ gemini.suite('SBIS3.CONTROLS.DataGridView Online', function () {
 				actions.wait(500);
             })
     });
+	
+	gemini.suite('marker_right', function (test) {
+
+        test.setUrl('/regression_data_grid_view_online_11.html').setCaptureElements('.capture')
+
+            .before(function (actions, find) {
+                actions.waitForElementToShow('[name="DataGridView 1"]', 40000);
+                this.data2 = find('[data-id="2"]');
+				this.data6 = find('[data-id="6"]');
+				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
+				this.input = find('[sbisname="TextBox 1"] input');
+            })
+
+            .capture('plain', function (actions) {
+                actions.click(this.input);
+            })
+
+            .capture('hovered_row', function (actions) {
+                actions.mouseMove(this.data2);
+				actions.wait(500);
+            })
+
+            .capture('selected_row', function (actions) {
+                actions.click(this.data6);
+				actions.wait(500);
+            })
+    });
 });
--- tests/css/online/tests/test_image.js
+++ tests/css/online/tests/test_image.js
@@ -61,4 +61,29 @@ gemini.suite('SBIS3.CONTROLS.Image Online', function () {
 				actions.waitForElementToHide('.controls-image__image-bar .icon-Edit', 1000);
 			})
     });
+	
+	gemini.suite('small_image', function (test) {
+
+        test.setUrl('/regression_image_online_3.html').setCaptureElements('[sbisname="Image 1"]')
+
+            .before(function (actions, find) {
+                actions.waitForElementToShow('[name="Image 1"]', 40000);
+                this.image = find('[name="Image 1"]');
+				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
+                this.input = find('[sbisname="TextBox 1"] input');
+            })
+
+            .capture('plain', function (actions) {
+				actions.click(this.input);
+			})
+			
+			.capture('hovered', function (actions) {
+				actions.executeJS(function (window) {
+                    window.$('[sbisname="ButtonEdit"]').removeClass('ws-hidden');
+					window.$('[sbisname="ButtonReset"]').removeClass('ws-hidden');
+                });
+				actions.mouseMove(this.image);
+				actions.wait(1000);
+			})
+    });
 });
