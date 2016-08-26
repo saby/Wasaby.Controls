59c9067c1bf77de5d03e455e38d1624fbe456d05
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2016-08-26 15:53:32 +0300

    Поправил случай когда скроллится window https://inside.tensor.ru/opendoc.html?guid=92e5eede-8e72-4ef9-8605-1b19eeaeb3ee&description= Merge request 25.08.2016 Регламент: Задача в разработку. Автор: Крайнов Д.О. Описание: Сконцентрировать весь код по подгру...

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -2577,12 +2577,14 @@ define('js!SBIS3.CONTROLS.ListView',
          },
          destroy: function () {
             this._destroyEditInPlace();
-            this._scrollWatcher.unsubscribe('onTotalScroll', this._onTotalScrollHandler);
-            if (this._options.scrollPaging){
-               this._scrollWatcher.unsubscribe('onScroll', this._onScrollHandler);
+            if (this._scrollWatcher) {
+               this._scrollWatcher.unsubscribe('onTotalScroll', this._onTotalScrollHandler);
+               this._scrollWatcher.destroy();
+               this._scrollWatcher = undefined;
+               if (this._options.scrollPaging){
+                  this._scrollWatcher.unsubscribe('onScroll', this._onScrollHandler);
+               }
             }
-            this._scrollWatcher.destroy();
-            this._scrollWatcher = undefined;
             if (this._pager) {
                this._pager.destroy();
                this._pager = undefined;
