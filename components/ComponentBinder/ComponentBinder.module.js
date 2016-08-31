e3b37da0f9e6c1ef007c9febdf7463745f976fc7
am.gerasimov <am.gerasimov@tensor.ru>
2016-08-31 09:41:40 +0300

    https://inside.tensor.ru/opendoc.html?guid=0c4aaeb1-9f84-4632-88ae-3ed3bb9e2294&description= Ошибка в разработку 26.08.2016 Список сотрудников. Зависают хлебные крошки при смене структуры когда заполнена строка поиска. ...

--- components/ComponentBinder/ComponentBinder.module.js
+++ components/ComponentBinder/ComponentBinder.module.js
@@ -448,7 +448,8 @@ define('js!SBIS3.CONTROLS.ComponentBinder', ['js!SBIS3.CONTROLS.Utils.KbLayoutRe
                   self._currentRoot = hier[0];
                   self._path = hier.reverse();
                } else {
-                  if (id === view._options.root){
+                  /* Если root не установлен, и переданный id === null, то считаем, что мы в корне */
+                  if ( (id === view._options.root) || (!view._options.root && id === null) ){
                       self._currentRoot = null;
                       self._path = [];
                   }
@@ -775,10 +776,10 @@ define('js!SBIS3.CONTROLS.ComponentBinder', ['js!SBIS3.CONTROLS.Utils.KbLayoutRe
             this._options.view.getContainer().css('padding-bottom', '32px');
          }
          if (this._options.paging.getSelectedKey() > pagesCount){
-            this._options.paging._options.selectedKey = pagesCount;   
+            this._options.paging._options.selectedKey = pagesCount;
          }
          this._options.paging.setPagesCount(pagesCount);
-         
+
          //Если есть страницы - покажем paging
          this._options.paging.setVisible(pagesCount > 1);
       },
