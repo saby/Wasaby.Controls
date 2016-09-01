c2e662a56ce04e1a99f8ba7102bd6b4d212bc35d
au.mironov <au.mironov@tensor.ru>
2016-09-01 16:23:57 +0700

    https://inside.tensor.ru/opendoc.html?guid=88a89616-a033-465a-993e-fd03c2f0bdc4&description= Ошибка в разработку 01.09.2016 Platforma_3.7.4.120_regr_test_CONTROLS - При раскрытии DatePicker отображается не тот месяц 1. Ра...

--- components/DatePicker/DatePicker.module.js
+++ components/DatePicker/DatePicker.module.js
@@ -338,7 +338,9 @@ define(
          self._chooserControl = new DateRangeBigChoose({
             parent: this._picker,
             element: element,
-            rangeselect: false
+            rangeselect: false,
+            startValue: this.getDate(),
+            endValue: this.getDate()
          });
 
          // Добавляем в пикер
