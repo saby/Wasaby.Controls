50dd738b8770c3a9c686413ad55cb36e29c5db85
ps.borisov <ps.borisov@tensor.ru>
2016-09-06 17:07:27 +0300

    https://inside.tensor.ru/opendoc.html?guid=88981858-8459-4380-ad89-732686ae0f01&description= Ошибка в разработку 06.09.2016 Выбор периода. На ipad убрать возможность выбора недели в 120 версию.

--- components/ListView/resources/SwipeHandlers.js
+++ components/ListView/resources/SwipeHandlers.js
@@ -37,31 +37,27 @@
 
       handleSwipe: function(start, stop, self, target) {
          if (stop.time - start.time < $.event.special.swipe.durTreshold){
+            var
+               direction,
+               eventType;
             if (Math.abs(start.coords[0] - stop.coords[0]) > $.event.special.swipe.xTreshold &&
                Math.abs(start.coords[1] - stop.coords[1]) < $.event.special.swipe.yTreshold) {
-               var
-                  direction = start.coords[0] > stop.coords[0] ? 'left' : 'right';
-               $.event.trigger($.Event('swipe', {
-                  target: target,
-                  swipestart: start,
-                  swipestop: stop,
-                  direction: direction
-               }), undefined, self);
-               return true;
+               direction = start.coords[0] > stop.coords[0] ? 'left' : 'right';
+               eventType = 'swipe';
             }
-
             if (Math.abs(start.coords[1] - stop.coords[1]) > $.event.special.swipe.xTreshold &&
                Math.abs(start.coords[0] - stop.coords[0]) < $.event.special.swipe.yTreshold) {
-               var
-                  direction = start.coords[1] > stop.coords[1] ? 'top' : 'bottom';
-               $.event.trigger($.Event('swipeVertical', {
-                  target: target,
-                  swipestart: start,
-                  swipestop: stop,
-                  direction: direction
-               }), undefined, self);
-               return true;
+               direction = start.coords[1] > stop.coords[1] ? 'top' : 'bottom';
+               eventType = 'swipeVertical';
             }
+
+            $.event.trigger($.Event(eventType, {
+               target: target,
+               swipestart: start,
+               swipestop: stop,
+               direction: direction
+            }), undefined, self);
+            return true;
          }
          return false;
       },
