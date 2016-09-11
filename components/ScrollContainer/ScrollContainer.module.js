8d30abd88ffba255085ccefbea3cd373a7ff1b6e
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2016-09-11 14:18:22 +0300

    Исправил подгрузку данных https://inside.tensor.ru/opendoc.html?guid=2677e20c-4c86-4ee0-8115-234acbdf422f&description= Ошибка в разработку 09.09.2016 При использовании кастомного скрола DataGridView с включенной опцией бесконечной подгрузки сам по...

--- components/ScrollContainer/ScrollContainer.module.js
+++ components/ScrollContainer/ScrollContainer.module.js
@@ -2,11 +2,11 @@ define('js!SBIS3.CONTROLS.ScrollContainer',
    [
       'js!SBIS3.CORE.CompoundControl',
       'html!SBIS3.CONTROLS.ScrollContainer',
-      'is!browser?js!SBIS3.CONTROLS.ScrollContainer/resources/custom-scrollbar-plugin/jquery.mCustomScrollbar.min',
+      'is!browser?js!SBIS3.CONTROLS.ScrollContainer/resources/custom-scrollbar-plugin/jquery.mCustomScrollbar',
       'is!browser?css!SBIS3.CONTROLS.ScrollContainer/resources/custom-scrollbar-plugin/jquery.mCustomScrollbar.min',
-      'is!browser?js!SBIS3.CONTROLS.ScrollContainer/resources/custom-scrollbar-plugin/jquery.mousewheel-3.0.6.min'
+      'is!browser?js!SBIS3.CONTROLS.ScrollContainer/resources/custom-scrollbar-plugin/jquery.mousewheel-3.0.6'
    ],
-   function(CompoundControl, dotTplFn){
+   function(CompoundControl, dotTplFn) {
 
       'use strict';
 
@@ -24,20 +24,20 @@ define('js!SBIS3.CONTROLS.ScrollContainer',
             _hasScroll: false
          },
 
-         $constructor: function(){
+         $constructor: function() {
             this._publish('onScroll');
          },
 
-         init: function(){
+         init: function() {
             Scroll.superclass.init.call(this);
 
             this._container.bind('mousemove touchstart', this._create.bind(this));
          },
 
-         _create: function(){
+         _create: function() {
             var self = this;
 
-            $('.controls-Scroll__container').mCustomScrollbar({
+            this.getContainer().mCustomScrollbar({
                theme: 'minimal-dark',
                scrollInertia: 0,
                updateOnContentResize: false,
@@ -65,36 +65,43 @@ define('js!SBIS3.CONTROLS.ScrollContainer',
             this._container.unbind('mousemove touchstart');
          },
 
-         isScrollOnTop: function(){
-           return this.hasScroll() && this._scroll.mcs.topPct === 0;
+         isScrollOnTop: function() {
+            return this.hasScroll() && this._scroll.mcs.topPct === 0;
          },
 
-         isScrollOnBottom: function(){
-            var scroll_cotainer = $('.mCSB_draggerContainer'),
-                scroll = $('#mCSB_1_dragger_vertical');
+         isScrollOnBottom: function() {
+            var
+               container = this.getContainer(),
+               scroll_cotainer = container.find('.mCSB_draggerContainer'),
+               scroll = container.find('#mCSB_1_dragger_vertical');
             return this.hasScroll() && scroll_cotainer.height() === (scroll.height() + this.getScrollTop());
          },
 
-         scrollTo: function(option){
-            var $scroll = $('.controls-Scroll__container');
-            $scroll.mCustomScrollbar("scrollTo", option, {scrollInertia: 0});
+         scrollTo: function(option) {
+            this.getContainer().mCustomScrollbar('scrollTo', option, {scrollInertia: 0});
          },
 
-         hasScroll: function(){
+         hasScroll: function() {
             this.updateScroll();
             return this._hasScroll;
          },
 
-         getScrollTop: function(){
+         getScrollTop: function() {
             if (this._scroll){
                return this._scroll.mcs.draggerTop;
             }
          },
 
-         updateScroll: function(){
-            $('.controls-Scroll__container').mCustomScrollbar('update');
+         updateScroll: function() {
+            this.getContainer().mCustomScrollbar('update');
+         },
+
+         destroy: function() {
+            this.getContainer().mCustomScrollbar('destroy');
+            this._scroll = undefined;
+            this.superclass.destroy.call(this);
          }
       });
 
-   return Scroll;
-});
+      return Scroll;
+   });
