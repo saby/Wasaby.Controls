0b87c90291281210ed73c48611938df82ce61e30
ps.borisov <ps.borisov@tensor.ru>
2016-09-12 17:44:27 +0300

    При подскроле к предыдущему он не обязательно должен быть под клавиатурой под ней должен быть сам таргет если таргет под клавиатурой тогда ддоскроливаем у предыдущему, потому запускаем скролл до таргета, там снова пойдет проверка что таргет под клавиатурой
    https://inside.tensor.ru/opendoc.html?guid=34274f82-4af5-4888-852b-965c6c9a2522&description=
    Ошибка в разработку 15.08.2016 Написать сообщения на iPad практически не возможно. Клавиатура прыгает туда и обратно. У меня вче...

--- components/RichEditor/RichEditor.module.js
+++ components/RichEditor/RichEditor.module.js
@@ -1823,7 +1823,7 @@ define('js!SBIS3.CONTROLS.RichEditor',
             if (this.isVisible()) {
                if (this._tinyEditor && this._tinyEditor.initialized && this._tinyEditor.selection && this._textChanged && (this._inputControl[0] === document.activeElement)) {
                   closestParagraph = $(this._tinyEditor.selection.getNode()).closest('p')[0];
-                  if (closestParagraph) {
+                  if (closestParagraph && $ws._const.browser.isMobileIOS) {
                      this._scrollToPrev(closestParagraph);//необходимо передавать абзац
                   }
 
@@ -1835,26 +1835,33 @@ define('js!SBIS3.CONTROLS.RichEditor',
                }
             }
          },
-         _scrollTo: function(target, side){
+
+         _elementIsUnderKeyboard: function(target, side){
             var
                targetOffset = target.getBoundingClientRect(),
                keyboardCoef = (window.innerHeight > window.innerWidth) ? constants.ipadCoefficient[side].vertical : constants.ipadCoefficient[side].horizontal; //Для альбома и портрета коэффициенты разные.
+            return $ws._const.browser.isMobileIOS && this.isEnabled() && targetOffset[side] > window.innerHeight * keyboardCoef;
+         },
 
-            if ( $ws._const.browser.isMobileIOS && this.isEnabled() && targetOffset[side] > window.innerHeight * keyboardCoef) { //
+         _scrollTo: function(target, side){
+            if (this._elementIsUnderKeyboard(target, side)) {
                target.scrollIntoView(true);
             }
          },
+
          _scrollToPrev: function(target){
-            if (target.previousSibling) {
-               this._scrollTo(target.previousSibling, 'bottom');
+            if (target.previousSibling && this._elementIsUnderKeyboard(target, 'bottom')) {
+               target.previousSibling.scrollIntoView(true);
             }
             this._scrollTo(target, 'bottom');
          },
+
          _updateDataReview: function(value) {
             if (this._dataReview) {
                this._dataReview.html(this._prepareReviewContent(value));
             }
          },
+
          _prepareReviewContent: function(value, it) {
             if (value && value[0] !== '<') {
                value = '<p>' + value.replace(/\n/gi, '<br/>') + '</p>';
