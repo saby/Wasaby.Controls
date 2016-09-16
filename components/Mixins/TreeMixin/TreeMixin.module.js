efb2d36150ba49970ff1bfa65b0e8102ab802145
kraynovdo <do.kraynov@tensor.ru>
2016-09-16 15:24:02 +0300

    https://inside.tensor.ru/opendoc.html?guid=2f32386b-5bf0-49bf-b749-f2dbfaa32b8b&description= Ошибка в разработку 16.09.2016 Во время тестирования препроцессора на тестовом юните перестали отдаваться страницы, в логах преп...

--- components/Mixins/TreeMixin/TreeMixin.module.js
+++ components/Mixins/TreeMixin/TreeMixin.module.js
@@ -1,5 +1,5 @@
 define('js!SBIS3.CONTROLS.TreeMixin', ['js!SBIS3.CONTROLS.BreadCrumbs',
-   'browser!html!SBIS3.CONTROLS.DataGridView/resources/DataGridViewGroupBy', 'js!WS.Data/Display/Tree'], function (BreadCrumbs, groupByTpl, TreeProjection) {
+   'html!SBIS3.CONTROLS.DataGridView/resources/DataGridViewGroupBy', 'js!WS.Data/Display/Tree'], function (BreadCrumbs, groupByTpl, TreeProjection) {
 
    var createDefaultProjection = function(items, cfg) {
       var
