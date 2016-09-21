439ef26ab1f6c110281deaff4aa9b70459d2497a
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2016-09-21 16:33:17 +0300

    Написал демку для скролла https://inside.tensor.ru/opendoc.html?guid=a1263a73-5e0b-4cb4-b486-2f1cbb3497f6&description= Задача в разработку 08.08.2016 R! Сделать ленивую инициализацию контрола 'Область с прокруткой'

---
+++ components/ScrollContainer/Demo/MyScrollContainer.module.js
@@ -0,0 +1,31 @@
+define('js!SBIS3.CONTROLS.Demo.MyScrollContainer',
+   [
+      'js!SBIS3.CONTROLS.CompoundControl',
+      'html!SBIS3.CONTROLS.MyScrollContainer',
+      'js!SBIS3.CONTROLS.ScrollContainer',
+      'js!SBIS3.CONTROLS.ListView',
+      'js!WS.Data/Source/Memory',
+      'css!SBIS3.CONTROLS.MyScrollContainer'
+   ],
+   function(CompoundControl, dotTplFn) {
+
+      'use strict';
+
+      var MyScrollContainer = CompoundControl.extend({
+
+         _dotTplFn: dotTplFn,
+
+         $protected: {
+            _options: {
+               listConfig: {
+                  infiniteScroll: 'down',
+                  pageSize: 10
+               }
+            }
+         }
+      });
+
+      return MyScrollContainer;
+   }
+
+);
