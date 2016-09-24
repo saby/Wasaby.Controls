af6c3238f853af01e68ced90072abd5c44e63299
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2016-09-24 20:03:39 +0300

    Доделал демку https://inside.tensor.ru/opendoc.html?guid=a37b8e70-7877-4190-ad3d-2a1604e8e3f7&description= Merge request 11.09.2016 Регламент: Ошибка в разработку. Автор: Вострилов И.С. Описание: Задачи. При наведении на левую ча...

--- components/ScrollContainer/Demo/MyScrollContainer.module.js
+++ components/ScrollContainer/Demo/MyScrollContainer.module.js
@@ -2,15 +2,21 @@ define('js!SBIS3.CONTROLS.Demo.MyScrollContainer',
    [
       'js!SBIS3.CONTROLS.CompoundControl',
       'html!SBIS3.CONTROLS.Demo.MyScrollContainer',
+      'js!WS.Data/Source/Memory',
       'js!SBIS3.CONTROLS.ScrollContainer',
       'js!SBIS3.CONTROLS.ListView',
-      'js!WS.Data/Source/Memory',
       'css!SBIS3.CONTROLS.Demo.MyScrollContainer'
    ],
-   function(CompoundControl, dotTplFn) {
+   function(CompoundControl, dotTplFn, MemorySource) {
 
       'use strict';
 
+      /**
+       * SBIS3.CONTROLS.Demo.MyScrollContainer
+       * @class SBIS3.CONTROLS.Demo.MyScrollContainer
+       * @extends SBIS3.CONTROLS.CompoundControl
+       * @control
+       */
       var MyScrollContainer = CompoundControl.extend({
 
          _dotTplFn: dotTplFn,
@@ -22,6 +28,28 @@ define('js!SBIS3.CONTROLS.Demo.MyScrollContainer',
                   pageSize: 10
                }
             }
+         },
+
+         init: function() {
+            MyScrollContainer.superclass.init.call(this);
+
+            var
+               items = [],
+               source;
+
+            for (var i = 1; i < 51; i++) {
+               items.push({
+                  id: i,
+                  title: 'Элемент листа №' + i
+               });
+            }
+
+            source = new MemorySource({
+               data: items,
+               idProperty: 'id'
+            });
+
+            this.getChildControlByName('listView').setDataSource(source);
          }
       });
 
