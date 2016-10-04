b155ca24cbe06fa67663012ba0648466e460fe37
da.dubenec <da.dubenec@tensor.ru>
2016-10-04 17:39:59 +0300

    Седьмой шаг

--- tests/css/online/tests/test_icon_button.js
+++ tests/css/online/tests/test_icon_button.js
@@ -5,26 +5,16 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
         test.setUrl('/regression_icon_button_online.html').setCaptureElements('.capture')
 
             .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
+				
+				this.icon_button = '[name="IconButton 1"]';
+                this.input = '[sbisname="TextBox 1"] input';
+				
+                actions.waitForElementToShow(this.icon_button, 40000);
+				actions.waitForElementToShow(this.input, 5000);
             })
 
             .capture('plain', function (actions) {
                 actions.click(this.input);
-            })
-
-            .capture('hovered', function (actions) {
-                actions.mouseMove(this.icon_button);
-            })
-
-            .capture('actived', function (actions) {
-                actions.mouseDown(this.icon_button);
-            })
-
-            .after(function (actions) {
-                actions.mouseUp(this.icon_button);
             })
     });
 
@@ -33,26 +23,16 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
         test.setUrl('/regression_icon_button_online_7.html').setCaptureElements('.capture')
 
             .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
+				
+				this.icon_button = '[name="IconButton 1"]';
+                this.input = '[sbisname="TextBox 1"] input';
+				
+                actions.waitForElementToShow(this.icon_button, 40000);
+				actions.waitForElementToShow(this.input, 5000);
             })
 
             .capture('plain', function (actions) {
                 actions.click(this.input);
-            })
-
-            .capture('hovered', function (actions) {
-                actions.mouseMove(this.icon_button);
-            })
-
-            .capture('actived', function (actions) {
-                actions.mouseDown(this.icon_button);
-            })
-
-            .after(function (actions) {
-                actions.mouseUp(this.icon_button);
             })
     });
 
@@ -61,49 +41,16 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
         test.setUrl('/regression_icon_button_online_3.html').setCaptureElements('.capture')
 
             .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
+				
+				this.icon_button = '[name="IconButton 1"]';
+                this.input = '[sbisname="TextBox 1"] input';
+				
+                actions.waitForElementToShow(this.icon_button, 40000);
+				actions.waitForElementToShow(this.input, 5000);
             })
 
             .capture('plain', function (actions) {
                 actions.click(this.input);
-            })
-
-            .capture('hovered', function (actions) {
-                actions.mouseMove(this.icon_button);
-            })
-
-            .capture('actived', function (actions) {
-                actions.mouseDown(this.icon_button);
-            })
-
-            .after(function (actions) {
-                actions.mouseUp(this.icon_button);
-            })
-    });
-
-    gemini.suite('disabled_round_border', function (test) {
-
-        test.setUrl('/regression_icon_button_online_3.html').setCaptureElements('.capture')
-
-            .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
-                actions.executeJS(function (window) {
-                    window.$ws.single.ControlStorage.getByName('IconButton 1').setEnabled(false);
-                });
-            })
-
-            .capture('plain', function (actions) {
-                actions.click(this.input);
-            })
-
-            .capture('hovered', function (actions) {
-                actions.mouseMove(this.icon_button);
             })
     });
 
@@ -112,10 +59,12 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
         test.setUrl('/regression_icon_button_online_4.html').setCaptureElements('.capture')
 
             .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
+				
+				this.icon_button = '[name="IconButton 1"]';
+                this.input = '[sbisname="TextBox 1"] input';
+				
+                actions.waitForElementToShow(this.icon_button, 40000);
+				actions.waitForElementToShow(this.input, 5000);
             })
 
             .capture('plain', function (actions) {
@@ -130,30 +79,14 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
                 actions.mouseDown(this.icon_button);
             })
 
-            .after(function (actions) {
+            .capture('disabled', function (actions) {
                 actions.mouseUp(this.icon_button);
-            })
-    });
-
-    gemini.suite('disabled_with_icon24', function (test) {
-
-        test.setUrl('/regression_icon_button_online_4.html').setCaptureElements('.capture')
-
-            .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
-                actions.executeJS(function (window) {
+				actions.executeJS(function (window) {
                     window.$ws.single.ControlStorage.getByName('IconButton 1').setEnabled(false);
                 });
             })
-
-            .capture('plain', function (actions) {
-                actions.click(this.input);
-            })
-
-            .capture('hovered', function (actions) {
+			
+			.capture('disabled_and_hovered', function (actions) {
                 actions.mouseMove(this.icon_button);
             })
     });
@@ -163,10 +96,12 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
         test.setUrl('/regression_icon_button_online_6.html').setCaptureElements('.capture')
 
             .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
+				
+				this.icon_button = '[name="IconButton 1"]';
+                this.input = '[sbisname="TextBox 1"] input';
+				
+                actions.waitForElementToShow(this.icon_button, 40000);
+				actions.waitForElementToShow(this.input, 5000);
             })
 
             .capture('plain', function (actions) {
@@ -181,30 +116,14 @@ gemini.suite('SBIS3.CONTROLS.IconButton Online', function () {
                 actions.mouseDown(this.icon_button);
             })
 
-            .after(function (actions) {
+            .capture('disabled', function (actions) {
                 actions.mouseUp(this.icon_button);
-            })
-    });
-
-    gemini.suite('disabled_round_border_with_icon24', function (test) {
-
-        test.setUrl('/regression_icon_button_online_6.html').setCaptureElements('.capture')
-
-            .before(function (actions) {
-                actions.waitForElementToShow('[name="IconButton 1"]', 40000);
-                this.icon_button = find('[name="IconButton 1"]');
-				actions.waitForElementToShow('[sbisname="TextBox 1"]', 40000);
-                this.input = find('[sbisname="TextBox 1"] input');
-                actions.executeJS(function (window) {
+				actions.executeJS(function (window) {
                     window.$ws.single.ControlStorage.getByName('IconButton 1').setEnabled(false);
                 });
             })
-
-            .capture('plain', function (actions) {
-                actions.click(this.input);
-            })
-
-            .capture('hovered', function (actions) {
+			
+			.capture('disabled_and_hovered', function (actions) {
                 actions.mouseMove(this.icon_button);
             })
     });
