2d63b7268f774fbc332416f6e8a7c938a4a75430
ps.borisov <ps.borisov@tensor.ru>
2016-10-12 17:19:15 +0300

    вырезание картинок при форматной вставке
    https://inside.tensor.ru/opendoc.html?guid=12984b5d-c80f-4143-88fe-4d873dc2a846&description=
    Ошибка в разработку 11.10.2016 Картинку скопированную в буфер с сайта, можно вставить в поле бтр по вставке с учетом стилей. К...

--- components/RichEditor/resources/RichTextArea/RichTextArea.module.js
+++ components/RichEditor/resources/RichTextArea/RichTextArea.module.js
@@ -398,8 +398,8 @@ define('js!SBIS3.CONTROLS.RichTextArea',
                   content = content.replace(new RegExp('<!--StartFragment-->|<!--EndFragment-->|<html>|<body>|</html>|</body>', 'img'), '').trim();
                   //получение результата из события  BeforePastePreProcess тини потому что оно возвращает контент чистым от тегов Ворда,
                   //withStyles: true нужно чтобы в нашем обработчике BeforePastePreProcess мы не обрабатывали а прокинули результат в обработчик тини
-                  eventResult = self.getTinyEditor().fire('BeforePastePreProcess', {content: content, withStyles: true});
-                  self.insertHtml(eventResult.content);
+                  content = Sanitize(eventResult.content, {validNodes: {img: false}});
+                  self.insertHtml(content);
                   self._setTrimmedText(self._getTinyEditorValue());
                },
                onPaste = function(event) {
