5d70525b66d1165cc76c3823ccd659c68dc6d107
ps.borisov <ps.borisov@tensor.ru>
2016-10-27 15:54:00 +0300

    https://inside.tensor.ru/opendoc.html?guid=d68e0364-2cdc-4da1-9525-33b04137dab8&description= Ошибка в разработку 21.10.2016 Mac(safari). При копировании текста с помощью command+a и command+c, весь текст в бтр удаляется ...

--- components/RichEditor/resources/RichEditorToolbar/RichEditorToolbar.module.js
+++ components/RichEditor/resources/RichEditorToolbar/RichEditorToolbar.module.js
@@ -190,9 +190,7 @@ define('js!SBIS3.CONTROLS.RichEditorToolbar', [
          },
 
          _buttonSetEnabled: function(buttonName,enabled ) {
-            if (this.getItems().getRecordById(buttonName)) {
-               this.getItemInstance(buttonName).setEnabled(enabled);
-            }
+            this.getItemInstance(buttonName).setEnabled(enabled);
          },
 
          _toggleContentSourceHandler: function(event, state) {
@@ -231,6 +229,34 @@ define('js!SBIS3.CONTROLS.RichEditorToolbar', [
                }
             }
             return items;
+         },
+
+         _unbindEditor: function() {
+            var
+               editor = this._options.linkedEditor;
+            RichEditorToolbar.superclass._unbindEditor.apply(this, arguments);
+            if (editor) {
+               editor.unsubscribe('onUndoRedoChange', this._handlersInstances.undoRedo);
+               editor.unsubscribe('onNodeChange', this._handlersInstances.node);
+               editor.unsubscribe('onToggleContentSource', this._handlersInstances.source);
+            }
+         },
+
+         _bindEditor: function() {
+            var
+               editor = this._options.linkedEditor;
+            RichEditorToolbar.superclass._bindEditor.apply(this, arguments);
+
+            this._handlersInstances.undoRedo = this._undoRedoChangeHandler.bind(this);
+            this._handlersInstances.node = this._nodeChangeHandler.bind(this);
+            this._handlersInstances.source = this._toggleContentSourceHandler.bind(this);
+            if (this.getItems().getRecordById('undo') && this.getItems().getRecordById('redo')) {
+               editor.subscribe('onUndoRedoChange', this._handlersInstances.undoRedo);
+            }
+            if (this.getItems().getRecordById('unlink')) {
+               editor.subscribe('onNodeChange', this._handlersInstances.node);
+            }
+            editor.subscribe('onToggleContentSource', this._handlersInstances.source);
          },
 
          /*БЛОК ФУНКЦИЙ ОБЁРТОК ДЛЯ ОТПРАВКИ КОМАНД РЕДАКТОРУ*/
