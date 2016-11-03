6b0b75a00b851ce18ef8ffc19fa11ca3cc005154
aa.bykanov <aa.bykanov@tensor.ru>
2016-11-03 21:26:33 +0300

    Доработка описания про информационные окна

--- components/Utils/InformationPopupManager.module.js
+++ components/Utils/InformationPopupManager.module.js
@@ -42,12 +42,24 @@ define('js!SBIS3.CONTROLS.Utils.InformationPopupManager',
 
       return /** @lends SBIS3.CONTROLS.Utils.InformationPopupManager.prototype */{
          /**
-          * Показать диалог с кнопками "Да", "Нет" и (опционально) "Отмена"
-          * @param {Configuration} Устанавливает конфигурацию для окна - {@link SBIS3.CONTROLS.SubmitPopup}
-          * @param {Function} positiveHandler Обработчик нажатия на кнопку "Да"
-          * @param {Function} negativeHandler Обработчик нажатия на кнопку "Нет"
-          * @param {Function} [cancelHandler] Обработчик нажатия на кнопку "Отмена"
-          * @returns {SBIS3.CONTROLS.SubmitPopup} экземпляр диалога
+          * Открывает диалог с кнопками "Да", "Нет" и "Отмена" (опционально).
+          * @param {Object} Объект конфигурацией открываемого диалога - {@link SBIS3.CONTROLS.SubmitPopup}.
+          * @param {Function} positiveHandler Обработчик нажатия на кнопку "Да".
+          * @param {Function} negativeHandler Обработчик нажатия на кнопку "Нет".
+          * @param {Function} [cancelHandler] Обработчик нажатия на кнопку "Отмена".
+          * @returns {SBIS3.CONTROLS.SubmitPopup} Экземпляр класса диалога.
+          * @example
+          * <pre>
+          * InformationPopupManager.showConfirmDialog(
+          *    {
+          *       message: 'Сохранить изменения?',
+          *       details: 'Чтобы продолжить редактирование нажмите, «Отмена».',
+          *       opener: self
+          *    },
+          *    myPositiveHandler, myNegativeHandler
+          * );
+          * </pre>
+          * @see showMessageDialog
           */
          showConfirmDialog: function(config, positiveHandler, negativeHandler, cancelHandler){
             return showSubmitDialog(cMerge(config, {
@@ -56,20 +68,44 @@ define('js!SBIS3.CONTROLS.Utils.InformationPopupManager',
          },
 
          /**
-          * Показать диалог с сообщением и 1 кнопкой. Диалог может иметь одно из состояний: "Ошибка" / "Успешно" / "Предупреждение"
-          * @param {Configuration} Устанавливает конфигурацию для окна - {@link SBIS3.CONTROLS.SubmitPopup}
-          * @param {Function} handler Обработчик нажатия на кнопку "Ок"
-          * @returns {SBIS3.CONTROLS.SubmitPopup} экземпляр диалога
+          * Открывает диалог с сообщением и одной кнопкой "Ок". Диалог может находиться в одном из трёх состояний: "Ошибка" , "Успешно" или "Предупреждение".
+          * @param {Object} Объект конфигурацией открываемого диалога - {@link SBIS3.CONTROLS.SubmitPopup}.
+          * @param {Function} handler Обработчик нажатия на кнопку "Ок".
+          * @returns {SBIS3.CONTROLS.SubmitPopup} Экземпляр класса диалога.
+          * @example
+          * <pre>
+          * InformationPopupManager.showMessageDialog(
+          *    {
+          *       message: 'Изменения были сохранены',
+          *       opener: self
+          *    },
+          *    myOkHandler
+          * );
+          * </pre>
+          * @see showConfirmDialog
           */
          showMessageDialog: function(config, handler){
             return showSubmitDialog(config, null, null, handler);
          },
 
          /**
-          * Показать нотификационное сообщение
-          * @param {Configuration} Устанавливает конфигурацию для окна - {@link SBIS3.CONTROLS.NotificationPopup}
-          * @param {Boolean} notHide Не прятать окно по истичению времени жизни
-          * @returns {SBIS3.CONTROLS.NotificationPopup} экземпляр нотификационного сообщения
+          * Показывает нотификационное сообщение.
+          * @param {Object} Объект конфигурацией открываемого окна - {@link SBIS3.CONTROLS.NotificationPopup}.
+          * @param {Boolean} notHide true - не скрывать окно по истичению времени жизни.
+          * @returns {SBIS3.CONTROLS.NotificationPopup} Экземпляр класса окна нотификационного сообщения.
+          * @example
+          * <pre>
+          * InformationPopupManager.showNotification(
+          *    {
+          *       icon: 'icon-24 icon-Chat icon-primary',
+          *       caption: 'Новое уведомление',
+          *       bodyTemplate: myTpl,
+          *       opener: self
+          *    },
+          *    true
+          * );
+          * </pre>
+          * @see showCustomNotification
           */
          showNotification: function(config, notHide){
             var popup = new NotificationPopup(cMerge({
@@ -82,9 +118,10 @@ define('js!SBIS3.CONTROLS.Utils.InformationPopupManager',
          },
 
          /**
-          * Показать произвольное нотификационное сообщение
-          * @param inst Экземпляр окна
-          * @param {Boolean} notHide Не прятать окно по истичению времени жизни
+          * Показывает произвольное нотификационное сообщение.
+          * @param {SBIS3.CONTROLS.PopupMixin|*} inst Экземпляр класса окна. Это может быть любое окно, созданное на основе указанного миксина.
+          * @param {Boolean} notHide true - не скрывать окно по истичению времени жизни.
+          * @see showNotification
           */
          showCustomNotification: function(inst, notHide){
             NotificationManager.showNotification(inst, notHide);
