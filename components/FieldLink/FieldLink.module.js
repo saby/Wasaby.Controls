9e3c22858647ab61642b44420597f70ce613849f
am.gerasimov <am.gerasimov@tensor.ru>
2016-11-22 13:01:19 +0300

    Написал комментарий, поправил по замечанию
    https://inside.tensor.ru/opendoc.html?guid=39d09e54-43b3-48a4-b5b9-5a59d07b537a&des=
    Merge request 22.11.2016
    Регламент: Ошибка в разработку. Автор: Писарева Н.А.
    
    Описание: iPad. Диалог задачи - при кл...

--- components/FieldLink/FieldLink.module.js
+++ components/FieldLink/FieldLink.module.js
@@ -368,8 +368,14 @@ define('js!SBIS3.CONTROLS.FieldLink',
              FieldLink.superclass.init.apply(this, arguments);
 
              if(this._options.useSelectorAction) {
-                this.subscribeTo((this._selectorAction = this.getChildControlByName('FieldLinkSelectorAction')), 'onExecuted', function(event, meta, result) {
+                this.subscribeTo(this._getSelectorAction(), 'onExecuted', function(event, meta, result) {
+                   /* После выбора из панели выбора, надо фокус возвращать в поле связи:
+                      после закрытия панели фокус будет проставляться на компонент, который был активным до этого (механизм WindowManager),
+                      последним активным компонентом была кнопка открытия справочника/ссылка открывающая справочник,
+                      но по стандарту курсор должен проставиться в поле ввода поля связи после выбора из панели,
+                      поэтому руками устанавливаем фокус в поле связи  */
                    this.setActive(true);
+
                    if(result) {
                       this.setSelectedItems(result);
                    }
@@ -387,6 +393,13 @@ define('js!SBIS3.CONTROLS.FieldLink',
              }
 
              this.getChildControlByName('fieldLinkMenu').setItems(this._options.dictionaries);
+          },
+
+          _getSelectorAction: function() {
+             if(!this._selectorAction) {
+                this._selectorAction = this.getChildControlByName('FieldLinkSelectorAction')
+             }
+             return this._selectorAction;
           },
 
            _getShowAllConfig: function(){
@@ -489,7 +502,7 @@ define('js!SBIS3.CONTROLS.FieldLink',
              }
 
              if(this._options.useSelectorAction) {
-                this._selectorAction.execute({
+                this._getSelectorAction().execute({
                    template: template,
                    componentOptions: componentOptions,
                    multiselect: this.getMultiselect(),
