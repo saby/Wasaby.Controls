e292e3d290caddd60dbb8c1d76f4717a2a08a95b
kraynovdo <do.kraynov@tensor.ru>
2016-11-22 09:46:25 +0300

    вот

--- components/NumberTextBox/NumberTextBox.module.js
+++ components/NumberTextBox/NumberTextBox.module.js
@@ -182,7 +182,7 @@ define('js!SBIS3.CONTROLS.NumberTextBox', [
       },
 
       _modifyOptions: function(options){
-         options = NumberTextBox.superclass._modifyOptions.call(this);
+         options = NumberTextBox.superclass._modifyOptions.apply(this, arguments);
          if (options.numericValue){
             options.text = formatText(
                options.numericValue, 
