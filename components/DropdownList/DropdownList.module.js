3dda49bf209d21bd029f9708e202e4f34115eeb2
as.krasilnikov <as.krasilnikov@tensor.ru>
2017-01-10 16:03:42 +0300

    Берем первую запись из enum, она под индексом 0
    https://inside.tensor.ru/opendoc.html?guid=032f4bec-cf2e-47b7-9ea1-8c25f567c283&des=
    Ошибка в разработку 09.01.2017 При биндинге к Enum падает JS ошибка во время рендеринга. pre-test-online.sbis.ru Бизнес &…

--- components/DropdownList/DropdownList.module.js
+++ components/DropdownList/DropdownList.module.js
@@ -653,7 +653,7 @@ define('js!SBIS3.CONTROLS.DropdownList',
                 id;
 
             if (this._isEnumTypeData()){
-               id = items.get();
+               id = 0; //Берем первую запись из enum, она под индексом 0
             }
             else if (this._options.emptyValue){ //Записи "Не выбрано" нет в наборе данных
                id = null;
