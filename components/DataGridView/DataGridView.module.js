4fc282d65c50ee8eb26cbfadc8c86ea2242cc2f1
vs.romanov <vs.romanov@tensor.ru>
2017-01-16 15:41:05 +0300

    Фиксированный заголовок вырезается только при скролле, поэтому пока нет скролла раздетилель рисуем от таблицы, а при скролле рисуется от фиксированного заголовка https://inside.tensor.ru/opendoc.html?guid=3a40acb2-5264-4838-8506-87203ee76b63&des= Ошибка в разработку 12.01.2017 1. Диалоги: Выписка, Кассовая книга, Приказ на оплату - В реестре потерялась первая линия таблицы (…

--- components/DataGridView/DataGridView.module.js
+++ components/DataGridView/DataGridView.module.js
@@ -1249,6 +1249,7 @@ define('js!SBIS3.CONTROLS.DataGridView',
 
       _toggleEmptyData: function(show) {
          DataGridView.superclass._toggleEmptyData.apply(this, arguments);
+         this.getContainer().toggleClass('controls-DataGridView__emptyTable', !!show);
          if (this._options.emptyHTML && this._options.allowToggleHead) {
             if (this._thead) {
                this._thead.toggleClass('ws-hidden', !!show);
