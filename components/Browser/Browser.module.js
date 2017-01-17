ecbfb8c13a56fe53b75acf5d6e9eafce04c54440
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2017-01-17 17:47:54 +0300

    https://inside.tensor.ru/opendoc.html?guid=18468d65-ec58-4e2d-a0f0-fc35af4dfde5&des= Ошибка в разработку 17.01.2017 Скрывается пейджинг в списке сотрудников, когда разворачиваем ПМО. Сотрудники/ ПМО свернута/ Скрин…

--- components/Browser/Browser.module.js
+++ components/Browser/Browser.module.js
@@ -159,6 +159,12 @@ define('js!SBIS3.CONTROLS.Browser', [
          this._operationsPanel = this._getOperationsPanel();
          if (this._operationsPanel) {
             this._componentBinder.bindOperationPanel(!this._options.showCheckBoxes, this._operationsPanel);
+            //Временное решение. Необходимо для решения ошибки: https://inside.tensor.ru/opendoc.html?guid=18468d65-ec58-4e2d-a0f0-fc35af4dfde5
+            //Если Browser переделать на flex-box то такие придроты не понадобятся.
+            //Выписана задача https://inside.tensor.ru/opendoc.html?guid=04d2ea0c-133c-4ee1-bf9b-4b222921b7d3
+            this._operationsPanel.subscribe('onToggle', function() {
+               self._container.toggleClass('controls-Browser__operationPanel-opened', this.isVisible());
+            });
          }
 
          this._filterButton = this._getFilterButton();
