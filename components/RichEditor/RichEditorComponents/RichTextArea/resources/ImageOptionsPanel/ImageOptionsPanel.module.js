542f2bab6c3e00e88ce2f8f4a24f951323c5c034
ps.borisov <ps.borisov@tensor.ru>
2017-01-26 10:27:57 +0300

    https://inside.tensor.ru/opendoc.html?guid=645ac964-fce5-4668-8fe4-ab2c488a35cd&des= Ошибка в разработку 26.01.2017 Platforma_3.7.5_regr_test_CONTROLS - Появляется непонятный тублар если кликнуть на картинку в БТР ht…

--- components/RichEditor/RichEditorComponents/RichTextArea/resources/ImageOptionsPanel/ImageOptionsPanel.module.js
+++ components/RichEditor/RichEditorComponents/RichTextArea/resources/ImageOptionsPanel/ImageOptionsPanel.module.js
@@ -7,7 +7,8 @@ define('js!SBIS3.CONTROLS.RichEditor.ImageOptionsPanel',
       'js!WS.Data/Di',
       'Core/helpers/fast-control-helpers',
       'html!SBIS3.CONTROLS.RichEditor.ImageOptionsPanel',
-      'js!SBIS3.CONTROLS.CommandsButton'
+      'js!SBIS3.CONTROLS.CommandsButton',
+      'css!SBIS3.CONTROLS.RichEditor.ImageOptionsPanel'
    ], function(CompoundControl, PopupMixin, FileStorageLoader, Di, fcHelpers, dotTplFn) {
       'use strict';
 
