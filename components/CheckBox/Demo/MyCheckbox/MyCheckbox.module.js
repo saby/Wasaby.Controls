ce8cdf09707c767b45c54a711f0beb295d4fc018
Антон Быканов <aa.bykanov@usd-bykanov1.corp.tensor.ru>
2017-01-31 15:31:28 +0300

    Исправления только в DEMO: ошибки с _dotTplFn + codestyle

--- components/CheckBox/Demo/MyCheckbox/MyCheckbox.module.js
+++ components/CheckBox/Demo/MyCheckbox/MyCheckbox.module.js
@@ -1,11 +1,5 @@
 define('js!SBIS3.CONTROLS.Demo.MyCheckbox', ['js!SBIS3.CORE.CompoundControl', 'html!SBIS3.CONTROLS.Demo.MyCheckbox', 'css!SBIS3.CONTROLS.Demo.MyCheckbox', 'js!SBIS3.CONTROLS.CheckBox'], function(CompoundControl, dotTplFn) {
-   /**
-    * SBIS3.CONTROLS.Demo.MyCheckbox
-    * @class SBIS3.CONTROLS.Demo.MyCheckbox
-    * @extends $ws.proto.CompoundControl
-    * @control
-    */
-   var moduleClass = CompoundControl.extend(/** @lends SBIS3.CONTROLS.Demo.MyCheckbox.prototype */{
+   var moduleClass = CompoundControl.extend({
       _dotTplFn: dotTplFn,
       $protected: {
          _options: {
