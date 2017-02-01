9d7fdae670ccade5238493b18fbfbc6869090061
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2017-02-01 12:57:47 +0300

    уменьшил отступ для загрузки следующей страницы https://inside.tensor.ru/opendoc.html?guid=39b75459-0386-4160-9dc8-c92f0baf6ef0&des= Ошибка в разработку 29.01.2017 Вот эта вертикальная линия отпрыгивает при прорисовке …

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -79,7 +79,7 @@ define('js!SBIS3.CONTROLS.ListView',
             return records;
          };
       var
-         START_NEXT_LOAD_OFFSET = 800,
+         START_NEXT_LOAD_OFFSET = 400,
          DRAG_META_INSERT = {
             on: 'on',
             after: 'after',
