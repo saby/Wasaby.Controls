94c0ad1321b45c83b7d99561d658604b8b014f83
Nikita <kochnevns@icloud.com>
2017-02-01 12:29:31 +0300

    https://inside.tensor.ru/opendoc.html?guid=23df015d-6752-4c92-b682-be88df6023b3&des= Ошибка в разработку 01.02.2017 Отсутствует кнопка разворота истории фильтров, Controls 3.7.5 http://test-autotest68:37535/int_eb_…

--- buildThemes.py
+++ buildThemes.py
@@ -15,10 +15,11 @@ def process_file(name):
    subprocess.call([util, name, css_name])
 
 def main():
-   for root, dirs, files in os.walk(r'themes'):
-      for dirname in dirs:
-         fname = os.path.join(root, dirname, os.path.basename(dirname) + ".less")
-         if os.path.exists(fname):
-            process_file(fname)
+   # for root, dirs, files in os.walk(r'themes'):
+   #    for dirname in dirs:
+   #       fname = os.path.join(root, dirname, os.path.basename(dirname) + ".less")
+   #       if os.path.exists(fname):
+   #          process_file(fname)
+   subprocess.call(['grunt', 'css'])
 if __name__ == "__main__":
    main()
