fffdbf679c3d0808f03ab7078b72a1ce3d05c811
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2017-02-03 17:20:11 +0300

    правильно учитвыаем высоту фиксированой шапки немного порефакторил https://inside.tensor.ru/opendoc.html?guid=a77e2cbd-7b13-4beb-95f9-f6765f73c9bf&des= Ошибка в разработку 01.02.2017 Кадровый учет. Некорректно работает навигация, начина я с перехода на третью страницу. Сотрудники\…

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -943,13 +943,11 @@ define('js!SBIS3.CONTROLS.ListView',
          },
 
          _onScrollHandler: function(event, scrollTop){
-            var scrollPage = this._scrollBinder._getScrollPage(scrollTop),
-                itemActions = this.getItemsActions();
+            var itemActions = this.getItemsActions();
 
-            if(itemActions && itemActions.isItemActionsMenuVisible()){
+            if (itemActions && itemActions.isItemActionsMenuVisible()){
                itemActions.hide();
             }
-            this._notify('onScrollPageChange', scrollPage);
          },
          _setScrollPagerPosition: function(){
             var right;
