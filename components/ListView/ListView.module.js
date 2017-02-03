ee9063807610dd0eae291dae61c3d4ff75a76942
kraynovdo <do.kraynov@tensor.ru>
2017-02-03 15:33:20 +0300

    еще правки для поиска по курсорам

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -49,6 +49,7 @@ define('js!SBIS3.CONTROLS.ListView',
    "Core/helpers/functional-helpers",
    "Core/helpers/dom&controls-helpers",
    'js!SBIS3.CONTROLS.CursorListNavigation',
+   "js!WS.Data/Source/SbisService",
    "browser!js!SBIS3.CONTROLS.ListView/resources/SwipeHandlers",
    "js!SBIS3.CONTROLS.DragEntity.Row",
    "js!WS.Data/Collection/RecordSet",
@@ -61,7 +62,7 @@ define('js!SBIS3.CONTROLS.ListView',
              Selectable, DataBindMixin, DecorableMixin, DragNDropMixin, FormWidgetMixin, BreakClickBySelectMixin, ItemsToolbar, MarkupTransformer, dotTplFn,
              TemplateUtil, CommonHandlers, Pager, EditInPlaceHoverController, EditInPlaceClickController, ImitateEvents,
              Link, ScrollWatcher, IBindCollection, List, groupByTpl, emptyDataTpl, ItemTemplate, ItemContentTemplate, GroupTemplate, InformationPopupManager,
-             Paging, ComponentBinder, Di, ArraySimpleValuesUtil, fcHelpers, colHelpers, cInstance, fHelpers, dcHelpers, CursorNavigation) {
+             Paging, ComponentBinder, Di, ArraySimpleValuesUtil, fcHelpers, colHelpers, cInstance, fHelpers, dcHelpers, CursorNavigation, SbisService) {
 
      'use strict';
 
@@ -2908,13 +2909,16 @@ define('js!SBIS3.CONTROLS.ListView',
                query = new Query(),
                queryFilter = filter;
             if (this._options.navigation && this._options.navigation.type == 'cursor') {
+               var options = this._dataSource.getOptions();
+               options.navigationType = SbisService.prototype.NAVIGATION_TYPE.POSITION;
+               this._dataSource.setOptions(options);
                if (this._getItemsProjection()) {
                   queryFilter = $ws.core.clone(filter);
                   var addParams = this._listNavigation.prepareQueryParams(this._getItemsProjection(), this._infiniteScrollState.mode);
                   $ws.core.merge(queryFilter, addParams);
                }
             }
-            query.where(filter)
+            query.where(queryFilter)
                .offset(offset)
                .limit(limit)
                .orderBy(sorting)
