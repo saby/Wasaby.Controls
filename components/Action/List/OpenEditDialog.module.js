f6fb5ed26b2764e2418a2a16e7a74bb11bb83a5d
as.krasilnikov <as.krasilnikov@tensor.ru>
2017-02-09 16:26:06 +0300

    Убираю ограничение по опциям для диалога редактирования, этот функционал был сделан в рамках единообразности интерфейса. Эту задачу нужно решать на самих компонентах (floatArea || Dialog), а не в action'e.
    Правлю последствия рефактора
    
    https://inside.tensor.ru/opendoc.html?guid=8f48c639-5d17-4b55-9bbc-77c5f5309a62&des=
    Ошибка в разработку 08.02.2017 (smoke) pre-test-inside (автотест_админ/Админ321) Не открывается окно создания папки проблем 1.…s=

--- components/Action/List/OpenEditDialog.module.js
+++ components/Action/List/OpenEditDialog.module.js
@@ -135,7 +135,7 @@ define('js!SBIS3.CONTROLS.Action.OpenEditDialog', [
          return this._openInNewTab;
       },
 
-      _openComponent:function(meta, dialogComponent, mode) {
+      _openComponent:function(meta, mode) {
          var openUrl = meta.item && meta.item.get(this._options.urlProperty);
          if (this._needOpenInNewTab() && openUrl) {
             window.open(openUrl);
@@ -143,11 +143,11 @@ define('js!SBIS3.CONTROLS.Action.OpenEditDialog', [
          }
          if (this._isNeedToRedrawDialog()) {
             this._saveRecord().addCallback(function () {
-               OpenEditDialog.superclass._openComponent.call(this, meta, dialogComponent, mode);
+               OpenEditDialog.superclass._openComponent.call(this, meta, mode);
             }.bind(this));
          }
          else {
-            OpenEditDialog.superclass._openComponent.call(this, meta, dialogComponent, mode);
+            OpenEditDialog.superclass._openComponent.call(this, meta, mode);
          }
       },
 
