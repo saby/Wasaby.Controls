7b32419ce3a6183bc4df82074eafebb24947b1f0
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2017-02-17 16:27:54 +0300

    Поправил отступы https://inside.tensor.ru/opendoc.html?guid=d8fe4478-546c-42db-829d-69181518822c&des= Merge request 10.02.2017 Регламент: Ошибка в разработку. Автор: Хакиев М.Р. Описание: При прогрузке длинных диалогов/реестро…

--- components/Scrollbar/Scrollbar.module.js
+++ components/Scrollbar/Scrollbar.module.js
@@ -2,9 +2,9 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
       'js!SBIS3.CONTROLS.CompoundControl',
       'tmpl!SBIS3.CONTROLS.Scrollbar',
       'js!SBIS3.CONTROLS.DragNDropMixin',
-       'css!SBIS3.CONTROLS.Scrollbar'
+      'css!SBIS3.CONTROLS.Scrollbar'
    ],
-   function(CompoundControl, dotTplFn, DragNDropMixinNew) {
+   function (CompoundControl, dotTplFn, DragNDropMixinNew) {
 
       'use strict';
 
@@ -34,20 +34,20 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
             _beginClient: undefined,
             _thumbPosition: 0,
             _viewportRatio: undefined,
-	         //Отношение видимой части трека к невидимой части контента
+            //Отношение видимой части трека к невидимой части контента
             _scrollRatio: undefined,
             _thumbHeight: undefined,
             _containerHeight: undefined,
             _containerOuterHeight: undefined,
-	         //Является ли высота ползунка константой
-	         _isConstThumb: undefined
+            //Является ли высота ползунка константой
+            _isConstThumb: undefined
          },
 
-         $constructor: function() {
+         $constructor: function () {
             this._publish('onScrollbarDrag');
          },
 
-         init: function() {
+         init: function () {
             Scrollbar.superclass.init.call(this);
 
             this._thumb = this._container.find('.js-controls-Scrollbar__thumb');
@@ -57,14 +57,14 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
             this._containerOuterHeight = this._container.outerHeight(true);
             this._setViewportRatio();
             this._setThumbHeight();
-	         this._setScrollRatio();
+            this._setScrollRatio();
          },
 
-         getPosition: function() {
+         getPosition: function () {
             return this._options.position;
          },
 
-         setPosition: function(position) {
+         setPosition: function (position) {
             var maxPosition = this.getContentHeight() - this._containerOuterHeight;
 
             position = this._calcPosition(position, 0, maxPosition);
@@ -72,17 +72,17 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
             this._setThumbPosition();
          },
 
-         getContentHeight: function() {
+         getContentHeight: function () {
             return this._options.contentHeight;
          },
 
-         setContentHeight: function(contentHeight) {
+         setContentHeight: function (contentHeight) {
             this._containerHeight = this._container.height();
             this._containerOuterHeight = this._container.outerHeight(true);
             this._options.contentHeight = contentHeight;
             this._setViewportRatio();
             this._setThumbHeight();
-	         this._setScrollRatio();
+            this._setScrollRatio();
          },
 
          /**
@@ -90,7 +90,7 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
           * если да то вернуть позицию как границу за которую мы вышли. bottom - нижняя граница,
           * top -верхняя граница.
           */
-         _calcPosition: function(position, bottom, top) {
+         _calcPosition: function (position, bottom, top) {
             if (position < bottom) {
                position = bottom;
             }
@@ -101,7 +101,7 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
             return position;
          },
 
-         _onClickDragHandler: function(e) {
+         _onClickDragHandler: function (e) {
             if (!$(e.target).hasClass('js-controls-Scrollbar__thumb')) {
                this._beginClient = this._thumb.get(0).getBoundingClientRect().top + this._thumbHeight / 2;
                this._onDragHandler(null, e);
@@ -109,63 +109,63 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
          },
 
          //Вернуть метод который инициализирует DragNDrop
-         _getDragInitHandler: function() {
-            return (function(e) {
+         _getDragInitHandler: function () {
+            return (function (e) {
                this._initDrag.call(this, e);
                e.preventDefault();
             }).bind(this);
          },
 
          //Сдвигаем ползунок на нужную позицию
-         _setThumbPosition: function() {
+         _setThumbPosition: function () {
             this._thumbPosition = this._calcProjectionSize(this.getPosition(), this._scrollRatio);
             this._thumb.get(0).style.top = this._thumbPosition + 'px';
          },
 
          //Высчитываем и задаём высоту ползунка
-         _setThumbHeight: function(){
+         _setThumbHeight: function () {
             this.getContainer().toggleClass('ws-invisible', this._viewportRatio >= 1);
             this._thumbHeight = this._calcProjectionSize(this._containerHeight, this._viewportRatio);
-	         //Проверим не является ли высота ползунка меньше минимальной.
-	         if (this._thumbHeight < BROWSER_SCROLLBAR_MIN_HEIGHT) {
-		         this._thumbHeight = BROWSER_SCROLLBAR_MIN_HEIGHT;
-		         this._isConstThumb = true;
-	         } else {
-		         this._isConstThumb = false;
-	         }
+            //Проверим не является ли высота ползунка меньше минимальной.
+            if (this._thumbHeight < BROWSER_SCROLLBAR_MIN_HEIGHT) {
+               this._thumbHeight = BROWSER_SCROLLBAR_MIN_HEIGHT;
+               this._isConstThumb = true;
+            } else {
+               this._isConstThumb = false;
+            }
             this._thumb.height(this._thumbHeight);
          },
 
-         _calcProjectionSize: function(size, ratio) {
+         _calcProjectionSize: function (size, ratio) {
             return size * ratio;
          },
 
-         _onResizeHandler: function() {
+         _onResizeHandler: function () {
             this._containerHeight = this._container.height();
             this._containerOuterHeight = this._container.outerHeight(true);
             this._setThumbHeight();
          },
 
          //Изменить отношение видимой части к размеру контента
-         _setViewportRatio: function(){
+         _setViewportRatio: function () {
             this._viewportRatio = this._containerOuterHeight / this.getContentHeight();
          },
 
-	      _setScrollRatio: function() {
-		      //Если длинна ползунка константа, то мы расчитываем её по определению, иначе расчитываем по короткой формуле.
-		      if (this._isConstThumb) {
-			      this._scrollRatio = (this.getContainer().height() - this._thumbHeight) / (this.getContentHeight() - this._containerOuterHeight);
-		      } else {
-			      this._scrollRatio = this.getContainer().height() / this.getContentHeight();
-		      }
-	      },
+         _setScrollRatio: function () {
+            //Если длинна ползунка константа, то мы расчитываем её по определению, иначе расчитываем по короткой формуле.
+            if (this._isConstThumb) {
+               this._scrollRatio = (this.getContainer().height() - this._thumbHeight) / (this.getContentHeight() - this._containerOuterHeight);
+            } else {
+               this._scrollRatio = this.getContainer().height() / this.getContentHeight();
+            }
+         },
 
-         _beginDragHandler: function(dragObject, e) {
+         _beginDragHandler: function (dragObject, e) {
             this._container.addClass('controls-Scrollbar__dragging');
             this._beginClient = e.clientY;
          },
 
-         _onDragHandler: function(dragObject, e) {
+         _onDragHandler: function (dragObject, e) {
             var newThumbPosition = this._thumbPosition + e.clientY - this._beginClient;
 
             this.setPosition(newThumbPosition / this._scrollRatio);
@@ -174,11 +174,11 @@ define('js!SBIS3.CONTROLS.Scrollbar', [
             this._beginClient = e.clientY - newThumbPosition + this._thumbPosition;
          },
 
-         _endDragHandler: function(dragObject, droppable, e) {
+         _endDragHandler: function (dragObject, droppable, e) {
             this._container.removeClass('controls-Scrollbar__dragging');
          },
 
-         destroy: function() {
+         destroy: function () {
             Scrollbar.superclass.destroy.call(this);
 
             this.getContainer().off('mousedown touchstart');
