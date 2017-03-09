bc420fa244e850b322a904d8440c92ee2a7f0455
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2017-03-09 17:30:03 +0300

    правильный родитель для кнопки, если не в пикере https://inside.tensor.ru/opendoc.html?guid=d5110963-0166-4900-843d-f389db242153&des= Ошибка в разработку 08.03.2017 При сохранении имени файла-основания по галочке выходит диалог сохранения Как повторить:логин/пароль…

--- components/Mixins/EditAtPlaceMixin/EditAtPlaceMixin.module.js
+++ components/Mixins/EditAtPlaceMixin/EditAtPlaceMixin.module.js
@@ -184,7 +184,7 @@ define('js!SBIS3.CONTROLS.EditAtPlaceMixin',
 
                // Добавляем кнопки
                this._okButton = new IconButton({
-                  parent: self._picker,
+                  parent: self._picker || self,
                   element: $ok,
                   primary: true, //Нужно, чтобы кнопка была дефолтной в своей области. иначе ctrl+enter вызовет обработчик дефолтной кнопки, расположенной выше.
                   icon: 'sprite:icon-16 icon-Yes icon-done action-hover'
