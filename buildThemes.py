ce497d757835e07d1f3b19dccd2aa2248ce22833
ma.rozov <ma.rozov@tensor.ru>
2017-03-16 13:59:56 +0300

    УБран запуск buildThemes так как данную процедуру делает builder

--- buildThemes.py
+++ buildThemes.py
@@ -3,27 +3,39 @@ import os.path
 import subprocess
 import platform
 
+
 def process_file(name):
-   css_name = name[:-4] + "css"
-   # https://docs.python.org/3/library/subprocess.html#subprocess.call
-   print(name)
-   util = "lessc"
-   if platform.system() == "Windows":
-      util = os.path.join("build","duncansmart-less","lessc.cmd")
-   else:
-      util = "lessc"
-   subprocess.call([util, name, css_name])
+    css_name = name[:-4] + "css"
+    # https://docs.python.org/3/library/subprocess.html#subprocess.call
+    print(name)
+    util = "lessc"
+    if platform.system() == "Windows":
+        util = os.path.join("build","duncansmart-less","lessc.cmd")
+    else:
+        util = "lessc"
+    subprocess.call([util, name, css_name])
+
 
 def main():
-   # for root, dirs, files in os.walk(r'themes'):
-   #    for dirname in dirs:
-   #       fname = os.path.join(root, dirname, os.path.basename(dirname) + ".less")
-   #       if os.path.exists(fname):
-   #          process_file(fname)
+    # for root, dirs, files in os.walk(r'themes'):
+    #    for dirname in dirs:
+    #       fname = os.path.join(root, dirname, os.path.basename(dirname) + \
+    #                            ".less")
+    #       if os.path.exists(fname):
+    #          process_file(fname)
 
-   # INSTALLING GRUNT LOCALY
-   subprocess.call(['npm' ,'install', 'grunt'], shell=True)
-   subprocess.call(['npm' ,'install', 'grunt-cli'], shell=True)
-   subprocess.call(['node', 'node_modules/grunt-cli/bin/grunt', 'css'], shell=True)
+    # INSTALLING GRUNT LOCALY
+    if platform.system() == "Windows":
+        shell = True
+    else:
+        shell = False
+    subprocess.call(["npm", "config", "set", "registry", "http://npmregistry.sbis.ru/"], shell=shell)
+    subprocess.call(['npm', 'install'], shell=shell)
+    subprocess.call(['npm', 'install', 'grunt'], shell=shell)
+    subprocess.call(['npm', 'install', 'grunt-cli'], shell=shell)
+    subprocess.call(['npm', 'install', 'async'], shell=shell)
+    subprocess.call(['node', 'node_modules/grunt-cli/bin/grunt', 'css'], shell=shell)
+
+
 if __name__ == "__main__":
-   main()
+    main()
