3c4e7e69672f1d48fababc989a3884403e04471e
kraynovdo <do.kraynov@tensor.ru>
2017-03-16 12:12:09 +0300

    чтоб не вырезать компоненты

--- components/FilterButton/FilterLine/FilterLine.module.js
+++ components/FilterButton/FilterLine/FilterLine.module.js
@@ -36,7 +36,7 @@ define('js!SBIS3.CONTROLS.FilterButton.FilterLine',
                      linkText = context.getValue('filterResetLinkText');
                   }
 
-                  linkText = Sanitize(linkText);
+                  linkText = Sanitize(linkText, {validNodes: {component: true}});
 
                   context.setValueSelf({
                      linkText: linkText,
