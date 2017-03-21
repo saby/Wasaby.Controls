8248810436d3fc7de67d0fc38601ed8ca42ce3ff
ps.borisov <ps.borisov@tensor.ru>
2017-03-21 16:13:07 +0300

    https://inside.tensor.ru/opendoc.html?guid=2f680f2e-c014-45a2-a8e4-986e2e725b97&des= Задача в разработку 10.02.2017 Вынести depricated модули в отдельный модуль. …

--- components/Utils/DialogOpener.module.js
+++ components/Utils/DialogOpener.module.js
@@ -3,24 +3,21 @@
  */
 define('js!SBIS3.CONTROLS.Utils.DialogOpener',[
    'js!SBIS3.CORE.DialogRecord',
-   'Transport/Record',
-   'Transport/ParserSBIS'
-], function (DialogRecord, Record, ParserSBIS) {
+   'Transport/Record'
+], function (DialogRecord, Record) {
    'use strict';
    var convertRecord = function(record) {
       var rec, raw = record.getRawData();
       if (raw.s && raw.d) {
          //TODO очень нужный метод
          var
-            parser = new ParserSBIS(),
-            cfg = parser.readRecord(raw),
             pkValue;
          if (raw.d[0]) {
             pkValue = raw.d[0] instanceof Array ? raw.d[0][0] : raw.d[0];
          }
          rec = new Record({
-            colDef : cfg.columns,
-            row : cfg.row,
+            colDef : raw.s,
+            row : raw.d,
             pkValue : pkValue
          })
       }
