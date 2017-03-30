269dfc8966ddf472be3fb074e9e88fcd8fb8ef8b
as.krasilnikov <as.krasilnikov@tensor.ru>
2017-03-30 17:59:39 +0300

    TODO временное решение. Если бинд был сделан на опцию visible у итема, то не перерисовываю все вкладки. подробности https://inside.tensor.ru/opendoc.html?guid=52beaec0-1f23-4e10-a9ff-b2902e18707a&des= Сделал через доп.опцию observeVisibleProperty, чтобы поведение включили там, где это нужно Подобное решение существует на уровне TabControl'a. Нужен механизм на уровне контекстов, который сможет распознавать изменения в сложных объектах.
     https://inside.tensor.ru/opendoc.html?guid=e0c63872-4768-46ec-883a-e5e628774cbf&des=
     Ошибка в разработку 29.03.2017 Подбор персонала. На отдельной вкладке не прогружаются похожие вакансии и превью организации Открыть…

--- components/SwitchableArea/SwitchableArea.module.js
+++ components/SwitchableArea/SwitchableArea.module.js
@@ -1,6 +1,7 @@
 /* На основе SBIS3.CORE.SwitchableArea */
 define('js!SBIS3.CONTROLS.SwitchableArea', [
-   'js!SBIS3.CORE.SwitchableArea'], function (CoreSwitchableArea) {
+   'js!SBIS3.CORE.SwitchableArea',
+   'Core/core-instance'], function (CoreSwitchableArea, cInstance) {
 
    'use strict';
 
@@ -24,18 +25,31 @@ define('js!SBIS3.CONTROLS.SwitchableArea', [
        * </ul>
        */
       setItems: function (items) {
-         var tabControl = this.getParent();
-         this._currentAreaId = undefined; //Сбрасываю перед установкой новых данных. Заполнится в setActiveArea, когда будут доступны все итемы
-
-         for (var id in this._areaHashMap){
-            if (this._areaHashMap.hasOwnProperty(id)){
-               this.removeArea(id);
+         var tabControl = this.getParent(),
+             newItems = cInstance.instanceOfModule(items, 'WS.Data/Collection/RecordSet') ? items.getRawData() : items,
+             oldItems = cInstance.instanceOfModule(this.getItems(), 'WS.Data/Collection/RecordSet') ? this.getItems().getRawData() : this.getItems();
+         //TODO временное решение. Если бинд был сделан на опцию visible у итема, то не перерисовываю все вкладки. подробности https://inside.tensor.ru/opendoc.html?guid=52beaec0-1f23-4e10-a9ff-b2902e18707a&des=
+         //Сделал через доп.опцию observeVisibleProperty, чтобы поведение включили там, где это нужно
+         //Подобное решение существует на уровне TabControl'a. Нужен механизм на уровне контекстов, который сможет распознавать изменения в сложных объектах.
+         if (tabControl._options.observeVisibleProperty && oldItems.length == newItems.length) {
+            for (var i = 0, l = newItems.length; i < l; i++) {
+               if (newItems[i]['visible'] !== undefined && oldItems[i].isVisible() !== newItems[i]['visible']) {
+                  oldItems[i].setVisible(newItems[i]['visible']);
+               }
             }
          }
-         for (var i = 0, l = items.length; i < l; i++){
-            this.addArea(items[i].id, items[i].content);
+         else {
+            this._currentAreaId = undefined; //Сбрасываю перед установкой новых данных. Заполнится в setActiveArea, когда будут доступны все итемы
+            for (var areaId in this._areaHashMap){
+               if (this._areaHashMap.hasOwnProperty(areaId)){
+                  this.removeArea(areaId);
+               }
+            }
+            for (i = 0, l = items.length; i < l; i++){
+               this.addArea(items[i].id, items[i].content);
+            }
+            tabControl._setActiveArea(tabControl.getSelectedKey()); //После новой установки итемов - делаем активной уже выбранную вкладку
          }
-         tabControl._setActiveArea(tabControl.getSelectedKey()); //После новой установки итемов - делаем активной уже выбранную вкладку
       }
    });
    return SwitchableAreaOld;
