453f4f80cfbeed5ac740f04b9a4f6c160fb7bddf
Nikita <kochnevns@icloud.com>
2017-04-10 14:52:05 +0300

    Правки в таске гранта

--- grunt/tasks/less1by1.js
+++ grunt/tasks/less1by1.js
@@ -37,7 +37,6 @@ function itIsControl(path) {
 module.exports = function less1by1Task(grunt) {
   let root = grunt.option('root') || '',
       app = grunt.option('application') || '',
-      changedComponent = grunt.config.get('changed') || '',
       rootPath = path.join(root, app),
       themesPath = path.join(rootPath, './themes/');
 
@@ -61,17 +60,19 @@ module.exports = function less1by1Task(grunt) {
     }, function writeCSS(compileLessError, output) {
 
         if (compileLessError) {
-            console.error(compileLessError);
+            grunt.log.error(`${compileLessError.message} in file: ${compileLessError.filename} on line: ${compileLessError.line}`);
         }
         let suffix = '';
 
         if (itIsControl) {
-          suffix = ( theme === DEFAULT_THEME ) ? '' : `__${theme}`
+          suffix = ( theme === DEFAULT_THEME ) ? '' : `__${theme}`;
         }
         let newName = `${path.dirname(filePath)}/${path.basename(filePath, '.less')}${suffix}.css`;
         if (output) {
             fs.writeFile(newName, output.css, function writeFileCb(writeFileError) {
-                if (writeFileError) grunt.log.error(`Не могу записать файл. Ошибка: ${writeFileError.message}.`);
+                if (writeFileError) {
+                  grunt.log.error(`Не могу записать файл. Ошибка: ${writeFileError.message}.`);
+                }
                // grunt.log.ok(`file ${filePath} successfuly compiled. Theme: ${theme}`);
             });
         }
@@ -89,14 +90,14 @@ module.exports = function less1by1Task(grunt) {
 
           if (helpers.validateFile(filepath, [grunt.config.get('changed') || `components/**/*.less`]) || helpers.validateFile(filepath, [grunt.config.get('changed') || 'themes/**/*.less'])) {
                 fs.readFile(filepath, function readFileCb(readFileError, data) {
-                  let theme = resolveThemeName(filepath)
+                  let theme = resolveThemeName(filepath);
                     if (itIsControl(filepath)) {
                       for (let themeName of themes) {
-                        processLessFile(data, filepath, readFileError, themeName, true)
+                        processLessFile(data, filepath, readFileError, themeName, true);
                       }
                     }
                     else {
-                      processLessFile(data, filepath, readFileError, theme, false)
+                      processLessFile(data, filepath, readFileError, theme, false);
                     }
 
                 });
