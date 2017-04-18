ea735562eba1b10ac561c82f56cca4667160af1d
dv.zuev <dv.zuev@tensor.ru>
2017-04-18 19:47:33 +0300

    Кнопки + слой совместимости

--- components/Button/Button.module.js
+++ components/Button/Button.module.js
@@ -1,58 +1,45 @@
 define('js!SBIS3.CONTROLS.Button',
    [
       'Core/core-extend',
+      "Core/Abstract.compatible",
+      'js!SBIS3.CORE.Control/Control.compatible',
+      "js!SBIS3.CORE.AreaAbstract/AreaAbstract.compatible",
+      'js!SBIS3.CONTROLS.BaseCompatible',
       'js!SBIS3.CONTROLS.Button/Button.compatible',
       'tmpl!SBIS3.CONTROLS.Button',
+      'Core/core-functions',
       'css!SBIS3.CONTROLS.Button',
       'css!WS.Controls.Button/resources/ButtonCommonStyles'
    ],
 
    function (extend,
-             Compatible,
-             template) {
+             AbstractCompatible,
+             ControlCompatible,
+             AreaAbstractCompatible,
+             BaseCompatible,
+             ButtonCompatible,
+             template,
+             functions) {
 
       'use strict';
 
-      var Button = extend.extend(
+      var Button = extend.extend([AbstractCompatible, ControlCompatible, AreaAbstractCompatible, BaseCompatible, ButtonCompatible],
          {
             _controlName: 'SBIS3.CONTROLS.Button',
             _template: template,
 
             _useNativeAsMain: true,
 
-            __conatiner: null,
-            get _container() {
-               return this.__conatiner;
-            },
-            set _container(val) {
-               if (!val)
-                  return;
-
-               if (this.__conatiner && !this.__conatiner.startTag && typeof(this.__conatiner.unbind) === 'function')
-                  this.__conatiner.unbind();
-
-               this.__conatiner = val;
-
-               try{
-                  this.__conatiner[0].wsControl = this;
-               }catch (e){}
-
-               this._initInnerAction(val);
-            },
-
             constructor: function(cfg) {
-
-               this._options = cfg;
-
                this.deprecatedContr(cfg);
-
-               this._thisIsInstance = true;
-
                this._publish('onActivated');
+            },
+
+            _onClick: function(){
+               this._notify("onActivated");
             }
 
          });
 
-      Object.assign(Button.prototype, Compatible);
       return Button;
    });
