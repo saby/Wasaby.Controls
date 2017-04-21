1fbd6af5239e211e21181a63e9198db99789cf61
da.dubenec <da.dubenec@tensor.ru>
2017-04-21 17:46:46 +0300

    Поправил тесты под новое поведение

--- tests/css/online/tests/test_fast_data_filter.js
+++ tests/css/online/tests/test_fast_data_filter.js
@@ -104,13 +104,13 @@ gemini.suite('SBIS3.CONTROLS.FastDataFilter Online', function () {
             })
 
             .capture('opened_one', function (actions) {
-                actions.mouseMove(this.first_title)
+                actions.click(this.first_title)
 				actions.waitForElementToShow('.controls-DropdownList__item[data-id="2"]', 4000);
             })
 
             .capture('opened_two', function (actions) {
                 actions.click(this.box);
-				actions.mouseMove(this.second_title)
+				actions.click(this.second_title)
 				actions.waitForElementToShow('.controls-DropdownList__item[data-id="2"]', 4000);
             })
     });
