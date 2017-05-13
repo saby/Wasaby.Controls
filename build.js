1305bc3e5e4b5ff30da586f52833857c235172d4
dv.zuev <dv.zuev@tensor.ru>
2017-05-13 18:04:12 +0300

    https://online.sbis.ru/opendoc.html?guid=f136a572-3bdf-4938-9170-2d59d7d56dcc&des= Ошибка в разработку 13.05.2017 Platforma_3.7.5.100_unit_tests_controls Не отвечает npm сервер provider: starting interval checkerpr…

---
+++ build.js
@@ -0,0 +1,63 @@
+/**
+ * Собирает файлы contents.json и contents.js
+ */
+
+var fs = require('fs'),
+   path = require('path'),
+   baseDir = process.cwd(),
+   baseResources = path.join(baseDir, 'components'),//а может быть SBIS3.CONTROLS
+   testResources = path.join(baseDir, 'test'),
+   getModules = (function() {
+      var modules = {},
+         modulePattern = /\.module\.js$/,
+         currentItem = '';
+      
+      //Dirty hack
+      global.define = function(name) {
+         modules[name.replace(/^js!/, '')] = '/' + path.relative(baseDir, currentItem).replace(/\\/g, '/');
+      };
+
+      return function (dir) {
+         fs.readdirSync(dir).forEach(function (file) {
+            currentItem = path.join(dir, file);
+            if (fs.statSync(currentItem).isDirectory()) {
+               getModules(currentItem);
+            } else {
+               if (modulePattern.test(file)) {
+                  require(currentItem);
+               }
+            }
+         });
+
+         return modules;
+      };
+   })();
+
+var modules = getModules(baseResources),
+   testModules = getModules(testResources);
+for (var key in testModules) {
+   if (testModules.hasOwnProperty(key)) {
+      modules[key] = testModules[key];
+   }
+}
+
+[{
+   key: 'jsModules',
+   name: path.join(baseDir, 'contents.json')
+}, {
+   key: 'jsModules',
+   name: path.join(baseDir, 'contents.js'),
+   prefix: 'contents = ',
+   suffix: ';'
+}].forEach(function(file) {
+   var item = {};
+   item[file.key] = modules;
+
+   fs.writeFileSync(
+      file.name,
+      (file.prefix || '') +
+      JSON.stringify(item, null, 3) +
+      (file.suffix || '')
+   );
+   console.log('File "' + file.name + '" created successfully.');
+});
