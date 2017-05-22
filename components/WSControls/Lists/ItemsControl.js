91a2ab8b5267798d13670e5e32710a82bd32eb6c
dv.zuev <dv.zuev@tensor.ru>
2017-05-22 22:37:49 +0300

    <ws:WSControls.Lists.ItemsControl> 	<ws:itemTemplate type="function">tmpl!WSControls/Lists/one</ws:itemTemplate> 	<ws:items> 		<ws:Array> 			<ws:Object> 				<ws:data>string1</ws:data> 			</ws:Object> 			<ws:Object> 				<ws:data>string2</ws:data> 			</ws:Object> 		</ws:Array> 	</ws:items> </ws:WSControls.Lists.ItemsControl>

--- components/WSControls/Lists/ItemsControl.js
+++ components/WSControls/Lists/ItemsControl.js
@@ -1,6 +1,48 @@
 define('js!WSControls/Lists/ItemsControl', [
    'Core/core-extend',
-   'Core/Abstract.compatible'
-], function(){
+   'Core/Abstract.compatible',
+   'js!SBIS3.CORE.Control/Control.compatible',
+   'js!SBIS3.CORE.AreaAbstract/AreaAbstract.compatible',
+   'js!SBIS3.CORE.BaseCompatible',
+   'js!WS.Data/Entity/InstantiableMixin',
+   'tmpl!WSControls/Lists/ItemsControl',
+   'tmpl!WSControls/Lists/one'
+], function (extend,
+             AbstractCompatible,
+             ControlCompatible,
+             AreaAbstractCompatible,
+             BaseCompatible,
+             InstantiableMixin,
+             template,
+             one) {
 
+   'use strict';
+
+   var ItemsControl = extend.extend([AbstractCompatible, ControlCompatible, AreaAbstractCompatible, BaseCompatible, InstantiableMixin],
+      {
+         _controlName: 'WSControls/Lists/ItemsControl',
+         _template: template,
+         iWantVDOM: false,
+         _isActiveByClick: false,
+
+         items: null,
+         itemTemplate: null,
+
+         constructor: function (cfg) {
+
+            this.items = cfg.items || [];
+            this.itemTemplate = cfg.itemTemplate || one;
+
+            this.deprecatedContr(cfg);
+         },
+
+         setItems: function(items){
+            this.items = items;
+            this._setDirty();
+         }
+
+
+      });
+
+   return ItemsControl;
 });
