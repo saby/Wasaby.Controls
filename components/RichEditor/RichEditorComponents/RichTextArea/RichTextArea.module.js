ba0aff2e8fe8a4bf0a0f8ca9dd550703c7970bd8
ps.borisov <ps.borisov@tensor.ru>
2017-06-06 15:19:19 +0300

    https://online.sbis.ru/opendoc.html?guid=eb9ee973-7038-4852-827e-de1db751af6b&des= Ошибка в разработку 25.05.2017 Розница / Настройки / магазины и рабочие места Кнопки скролла не соответствуют стандарту 1) Раздели…

--- components/RichEditor/RichEditorComponents/RichTextArea/RichTextArea.module.js
+++ components/RichEditor/RichEditorComponents/RichTextArea/RichTextArea.module.js
@@ -836,6 +836,12 @@ define('js!SBIS3.CONTROLS.RichTextArea',
                            element: this._fieldHref,
                            linkedContext: context,
                            name: 'fre_link_href'
+                        }).getContainer().on('keydown', function(e) {
+                           if (e.which == cConstants.key.enter) {
+                              e.preventDefault();
+                              e.stopPropagation();
+                              return false;
+                           }
                         });
                         this._titleBar
                            .prepend($('<a href="javascript:void(0)"></a>')
@@ -845,7 +851,6 @@ define('js!SBIS3.CONTROLS.RichTextArea',
                                  return false;
                               }))
                            .append(okButton);
-                        this._fieldHref._keysWeHandle= [cConstants.key.enter];
                         new Button({
                            caption: 'ОК',
                            primary: true,
