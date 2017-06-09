f5bd9ce52b5d844307fa691ace3a935d8a6127d1
dv.zuev <dv.zuev@tensor.ru>
2017-06-10 01:54:42 +0300

    https://online.sbis.ru/opendoc.html?guid=346b84e3-3c95-43b6-8d2a-c6e20ca928da&des= Задача в разработку 01.06.2017 Подготовить TodoMVC для автотестов…

--- components/WSControls/Control/Base.js
+++ components/WSControls/Control/Base.js
@@ -30,9 +30,97 @@ define('js!WSControls/Control/Base',
             _controlName: 'WSControls/Control/Base',
             iWantVDOM: false,
 
+            _sendCommandFn: null,
+            parentSendCommand: null,
+            logicParent: null,
+            _eventsPile: {},
+            _transientProperties: ['rawData', 'data', 'parentSendCommand', 'focusing'],
+            _overrideTemplateOptsFn: null,
+            _commandHandlers: [],
+
+            _sendCommand: function(command) {
+               var
+                  handlers = this._commandHandlers,
+                  parentSend = this.get('parentSendCommand'),
+                  result;
+
+               if (handlers[command]) {
+                  result = handlers[command].apply(this, functionalHelpers.argumentsToArray(arguments).slice(1));
+               } else if (parentSend) {
+                  result = parentSend.apply(this, arguments);
+               }
+               return result;
+            },
+
+            _overrideChildrenOptions: function(childOptions) {
+               var
+                  visible = this.visible!==undefined?this.visible:true,
+                  enabled = this.enabled!==undefined?this.enabled:true;
+
+               if (!visible || !enabled) {
+                  if (!visible) {
+                     childOptions.visible = false;
+                  }
+
+                  if (!enabled) {
+                     childOptions.enabled = false;
+                  }
+               }
+
+               childOptions.parentSendCommand = this._sendCommandFn;
+
+               return childOptions;
+            },
+
+            _addObservableListenters: function (newOptions) {},
+
+            getDefaultOptions: function () {
+               return {};
+            },
+
+            _afterApplyOptions: function(fromConstructor, oldOptions, newOptions) {
+            },
+
+            _initializeCommandHandlers: function initializeCommandHandlers() {
+               if (this.commands) {
+                  for (var command in this.commands) {
+                     this._declareCommand(command, this.commands[command]);
+                  }
+               }
+            },
+
+            _declareCommand: function(command, handler) {
+               this._commandHandlers[command] = handler.bind(this);
+            },
+
+            _overrideTemplateOpts: function(model) {
+               return model;
+            },
+
+            _validateOptions: function(newOptions) {
+               return newOptions;
+            },
+
+            _getMarkup: function(rootKey) {
+               var decOpts = this._prepareDecOptions();
+               return this._template(this, decOpts, rootKey, true)[0];
+
+
+            },
+
+            _applyChangedOptions: function(newOptions) {
+               Object.getOwnPropertyNames(newOptions).forEach(function (prop) {
+                  this._options[prop] = newOptions[prop];
+
+               }, this);
+            },
+
             constructor: function (cfg) {
+               this.logicParent = cfg.logicParent;
                this.deprecatedContr(cfg);
             }
+
+
 
 
          });
