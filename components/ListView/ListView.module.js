623eea8be8f2c9758777ae6220bc7f5808f390c3
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2017-06-12 11:38:43 +0300

    В папке может не быть элементов вообще, тогда нужно сохранить как нулевую страницу https://online.sbis.ru/opendoc.html?guid=88cd4090-048c-436a-8d06-399f265f4d72&des= Ошибка в разработку 01.06.2017 Не происходит выход на уровень вверх при помощи хлебных крошек в реестре номенклатуры, ошибка в конс…

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -1968,7 +1968,7 @@ define('js!SBIS3.CONTROLS.ListView',
          _getCurrentPage: function() {
             var page = 0;
             if (this._scrollBinder) {
-               var scrollPage = this._scrollBinder._getScrollPage();
+               var scrollPage = this._scrollBinder._getScrollPage() || 0;
                page = Math.floor(scrollPage.element.index() / this._limit);
             }
             // прибавим к полученой странице количество еще не загруженных страниц
