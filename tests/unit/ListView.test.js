f4bd5f3804ed6ec33b86d24be61de9d6fa9ad2be
ganshinyao <yo.ganshin@tensor.ru>
2017-06-30 16:01:12 +0300

    https://online.sbis.ru/opendoc.html?guid=96e56bce-3c65-4025-a66e-6ce9f4990f6b&des= Ошибка в разработку 30.06.2017 Ошибка Platforma_3.17.10_unit_tests_controls SBIS3.CONTROLS.ListView _checkHorisontalDragndrop.shoul…

--- tests/unit/ListView.test.js
+++ tests/unit/ListView.test.js
@@ -10,32 +10,6 @@ define([
             this.skip();
          }
       });
-      describe('_checkHorisontalDragndrop', function () {
-         it('should return true if target has display inline-block', function () {
-            var target = $('<div style="display:inline-block; float:none">');
-            assert.isTrue(ListView.prototype._checkHorisontalDragndrop(target));
-         });
-         it('should return true if target is a float', function () {
-            var target = $('<div style="float:left">');
-            assert.isTrue(ListView.prototype._checkHorisontalDragndrop(target));
-         });
-         it('should return true if parent targets has display flex', function () {
-            var target = $('<div style="float:none">');
-            parent = $('<div style="display: flex; flex-direction: row">');
-            parent.html(target);
-            assert.isTrue(ListView.prototype._checkHorisontalDragndrop(target));
-         });
-         it('should return flase if target has display block', function () {
-            var target = $('<div style="float:none; display: block">');
-            assert.isFalse(ListView.prototype._checkHorisontalDragndrop(target));
-         });
-         it('should return false if parent targets has display flex and flex-direction column', function () {
-            var target = $('<div style="float:none">');
-            parent = $('<div style="display: flex; flex-direction: column">');
-            parent.html(target);
-            assert.isFalse(ListView.prototype._checkHorisontalDragndrop(target));
-         });
-      });
       describe('move', function () {
          it('should trigger onBeginMove', function (done) {
             var target = $('<div><div></div></div>'),
