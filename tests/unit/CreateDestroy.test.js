7274dc22907c1b75914405024596fa3389014e70
nv.belotelov <nv.belotelov@tensor.ru>
2017-07-07 14:57:33 +0300

    https://online.sbis.ru/opendoc.html?guid=273cfe39-e00c-4dbf-89f7-6505c8dea636&des= Задача в разработку 07.07.2017 Сделать тесты на логического родителя с этой веткой WS. (сделать отдельную ветку в контролы). …

--- tests/unit/CreateDestroy.test.js
+++ tests/unit/CreateDestroy.test.js
@@ -2,9 +2,10 @@
  * Created by dv.zuev on 18.05.2017.
  */
 define([
-   'js!WSDemo/TestSubControls/TestSubControlParent'
+   'js!WSDemo/TestSubControls/TestSubControlParent',
+   'js!WSDemo/TestSubControls/LogicParentTestControl'
 ], function (
-   SyntheticEvent, ParentControl
+   ParentControl, LogicParentTestControl
 ) {
    'use strict';
 
@@ -16,6 +17,21 @@ define([
       });
       describe('SubControls', function(){
          it('SubControls', function(done) {
+            var parentControl = new ParentControl({
+               element: $('<div></div>')
+            });
+            setTimeout(function () {
+               assert.isTrue(!!parentControl.children.child);
+               var a = parentControl.children.child;
+
+               parentControl.destroy();
+               assert.isTrue(a.isDestroyed());
+               done();
+            }, 0)
+
+         });
+
+         it('Parents', function(done) {
             var parentControl = new ParentControl({
                element: $('<div></div>')
             });
