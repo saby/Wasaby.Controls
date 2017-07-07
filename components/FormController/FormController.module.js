b403817c29b6ffbb35e949ec3c689246ea3fc743
Nikolay Maksimov <n.maksimov@inlearno.com>
2017-07-07 18:14:03 +0300

    no message

--- components/FormController/FormController.module.js
+++ components/FormController/FormController.module.js
@@ -164,7 +164,7 @@ define('js!SBIS3.CONTROLS.FormController', [
             /**
              * @cfg {Boolean} Устанавливает сохранение только изменённых полей.
              */
-            diffOnly: false,
+            diffOnly: true,
             /**
              * @cfg {Object} Устанавливает ассоциативный массив, который используют только при создании новой записи для инициализации её начальными значениями.
              */
@@ -205,6 +205,8 @@ define('js!SBIS3.CONTROLS.FormController', [
       },
 
       $constructor: function() {
+         var dataSource = this._options.dataSource;
+
          this._publish('onFail', 'onReadModel', 'onBeforeUpdateModel', 'onUpdateModel', 'onDestroyModel', 'onCreateModel', 'onAfterFormLoad');
          this._declareCommands();
          this._panel = this.getTopParent();
@@ -228,7 +230,14 @@ define('js!SBIS3.CONTROLS.FormController', [
 
          //TODO в рамках совместимости
          this._dataSource = this._options.source;
-         if (this._options.dataSource && this._options.dataSource.endpoint) {
+         if (dataSource && dataSource.endpoint) {
+            
+            // Установка режима dataSource.updateOnlyChanged происходит, только если он не задан напрямую в опциях
+            dataSource.options = dataSource.options || {};
+            if (typeof dataSource.options.updateOnlyChanged === 'undefined') {
+               dataSource.options.updateOnlyChanged = !!this._options.diffOnly;
+            }
+            
             this._dataSource = this._dataSource || FormController.prototype.createDataSource(this._options);
             if (!this._options.record && !cInstance.instanceOfModule(this._options._receiptRecordDeferred, 'Core/Deferred')) {
                this._getRecordFromSource({});
@@ -408,36 +417,6 @@ define('js!SBIS3.CONTROLS.FormController', [
          if (fields.title || Object.isEmpty(fields)) {
             this._updateDocumentTitle();
          }
-      },
-
-      _getRecordForUpdate: function () {
-         if (!this._options.diffOnly){
-            return this._options.record;
-         }
-
-         var record = this._options.record,
-            changedRec = new Model({
-               idProperty: record.getIdProperty(),
-               adapter: record.getAdapter()
-            }),
-            changedFields = record.getChanged();
-         if (changedFields.indexOf(record.getIdProperty()) === -1){
-            changedFields.push(record.getIdProperty());
-         }
-
-         $.each(changedFields, function(i, key){
-            var formatIndex = record.getFormat().getFieldIndex(key);
-            if (formatIndex > -1) {
-               changedRec.addField(record.getFormat().at(formatIndex), undefined, record.get(key));
-               if (cInstance.instanceOfModule(record.getAdapter(), 'WS.Data/Adapter/Sbis')) {
-                  var newFormatIndex = changedRec.getFormat().getFieldIndex(key);
-                  //todo сделать нормальную сериализацию формата, щас не сериализуется поле связь и при копировании уходит как строка
-                  changedRec.getRawData().s[newFormatIndex] = cFunctions.clone(record.getRawData().s[formatIndex]);
-               }
-            }
-         });
-
-         return changedRec;
       },
 
       _setContextRecord: function(record){
@@ -815,8 +794,7 @@ define('js!SBIS3.CONTROLS.FormController', [
             self = this;
 
          if (this._options.record.isChanged() || self._newRecord || this._needUpdateAlways) {
-            this._updateDeferred = this._dataSource.update(this._getRecordForUpdate()).addCallback(function (key) {
-               self.getRecord().acceptChanges(); //Выпилить вообще весь функционал _getRecordForUpdate, задача с отправкой только измененных полей решается через опцию sbisService
+            this._updateDeferred = this._dataSource.update(this._options.record).addCallback(function (key) {
                updateConfig.additionalData.key = key;
                self._newRecord = false;
                return key;
