5e4cf052df62a18921985f2d1dc9f1f12b78c7a2
nv.belotelov <nv.belotelov@tensor.ru>
2017-07-07 10:52:45 +0300

    https://online.sbis.ru/opendoc.html?guid=3ff5d966-b690-4a89-b21d-a8840570445a&des= Ошибка в разработку 15.06.2017 Написать юнит тест на создание контрола с подконтролом…

--- tests/unit/CreateDestroy.test.js
+++ tests/unit/CreateDestroy.test.js
@@ -3,18 +3,32 @@
  */
 define([
    'Core/vdom/Synchronizer/resources/SyntheticEvent',
-   'js!WSDemo/MasterDetail/MasterControl/MasterControl'
+   'js!WSDemo/TestSubControls/TestSubControlParent'
 ], function (
-   SyntheticEvent, MasterControl
+   SyntheticEvent, ParentControl
 ) {
    'use strict';
 
    describe('CreateDestroy', function () {
-
+      beforeEach(function () {
+         if (typeof $ === 'undefined') {
+            this.skip();
+         }
+      });
       describe('SubControls', function(){
-         it('SubControls', function() {
-            var parentControl = new MasterControl({});
-            assert.isTrue(parentControl.children.length > 0);
+         it('SubControls', function(done) {
+            var parentControl = new ParentControl({
+               element: $('<div></div>')
+            });
+            setTimeout(function () {
+               assert.isTrue(!!parentControl.children.child);
+               var a = parentControl.children.child;
+
+               parentControl.destroy();
+               assert.isTrue(a.isDestroyed());
+               done();
+            }, 0)
+
          });
 
       });
