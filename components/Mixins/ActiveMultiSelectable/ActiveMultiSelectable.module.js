588c4f02c76adf6bb8ed8cbcaa3d6bac39866593
am.gerasimov <am.gerasimov@tensor.ru>
2017-07-10 09:50:01 +0300

    https://online.sbis.ru/opendoc.html?guid=92375b6b-ba02-4c14-af49-62e17af800e5&des= Ошибка в разработку 08.07.2017 Platforma_3.17.10_int_test_CONTROLS_chrome - тест TestDataGridDB2.test_03_fieldlink_on_formcontrolle…

--- components/Mixins/ActiveMultiSelectable/ActiveMultiSelectable.module.js
+++ components/Mixins/ActiveMultiSelectable/ActiveMultiSelectable.module.js
@@ -195,10 +195,15 @@ define('js!SBIS3.CONTROLS.ActiveMultiSelectable', ['js!WS.Data/Entity/Model', 'C
                асинхронный getSelectedItems, который либо сразу вернёт набор, либо когда загрузятся все записи */
             this.getSelectedItems(true).addCallback(function (selectedItems) {
                if(selectedItems) {
+                  /* Необходимо делать clone, иначе по ссылке изменения произойдут и в оригинальном рекордсете,
+                     что может вызвать преждевременное срабатывание биндингов */
+                  selectedItems = selectedItems.clone();
+                  
                   if(!self._options.multiselect) {
                      selectedItems.clear();
                   }
                   selectedItems.append(newItems);
+                  self._options.selectedItems = selectedItems;
                } else {
                   self._options.selectedItems = self._makeList(newItems);
                }
