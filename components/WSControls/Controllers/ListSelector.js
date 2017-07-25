d0c634d1d98ca8bfe2d822d40eb986595587faa9
kraynovdo <do.kraynov@tensor.ru>
2017-07-25 08:49:38 +0300

    небольшие правки селектора

--- components/WSControls/Controllers/ListSelector.js
+++ components/WSControls/Controllers/ListSelector.js
@@ -2,118 +2,115 @@ define('js!WSControls/Controllers/ListSelector', [
    'Core/Abstract',
    'js!WSControls/Lists/resources/utils/ItemsUtil'
 ], function(Abstract, ItemsUtil) {
-   var ListSelector = Abstract.extend({
+   var ListSelector;
+   ListSelector = Abstract.extend({
       _useNativeAsMain: true,
-      constructor: function(cfg) {
+      constructor: function (cfg) {
          ListSelector.superclass.constructor.apply(this, arguments);
          this._prepareData(cfg);
-         this._allowEmptyApply();
+         this._applyAllowEmpty();
          this._publish('onSelectedItemChange');
       },
 
-      _prepareData: function(cfg) {
+      _prepareData: function (cfg) {
          this._options = {
-            selectedIndex : cfg.selectedIndex,
-            allowEmptySelection : cfg.allowEmptySelection,
+            selectedIndex: cfg.selectedIndex,
+            allowEmptySelection: cfg.allowEmptySelection,
             selectedKey: cfg.selectedKey,
             projection: cfg.projection,
             idProperty: cfg.idProperty
 
          };
-         this.selectedIndex = (this._options.selectedIndex !== undefined && this._options.selectedIndex !== null) ? this._options.selectedIndex : -1;
-         this.allowEmptySelection = (this._options.allowEmptySelection !== undefined && this._options.allowEmptySelection !== null) ? this._options.allowEmptySelection: true;
-         this.selectedKey = this._options.selectedKey || null;
+         this._selectedIndex = (this._options.selectedIndex !== undefined && this._options.selectedIndex !== null) ? this._options.selectedIndex : -1;
+         this.allowEmptySelection = this._options.allowEmptySelection !== false;
+         this._selectedKey = this._options.selectedKey || null;
 
-         if (this._isEmptyIndex(this.selectedIndex)) {
+         if (this._isEmptyIndex(this._selectedIndex)) {
             //Если передали пустой индекс и ключ, определяем индекс по ключу
-            this._prepareSelectedIndexByKey(this.selectedKey)
+            this._prepareSelectedIndexByKey(this._selectedKey)
          }
          else {
             //если индекс передали - вычисляем ключ
-            this._prepareSelectedKeyByIndex(this.selectedIndex)
+            this._prepareSelectedKeyByIndex(this._selectedIndex)
          }
       },
 
-      _allowEmptyApply: function() {
-         if (!this.allowEmptySelection && this._isEmptyIndex(this.selectedIndex)) {
+      _applyAllowEmpty: function () {
+         if (!this.allowEmptySelection && this._isEmptyIndex(this._selectedIndex)) {
             if (this._options.projection.getCount()) {
                this.setSelectedIndex(0)
             }
          }
       },
 
-      _prepareSelectedIndexByKey: function(key) {
+      _prepareSelectedIndexByKey: function (key) {
          //Вычисляем индекс по известному ключу
-         if (typeof key === 'undefined') {
-            this.selectedIndex = -1;
-         }
-         else {
-            this.selectedIndex = this._getItemIndexByKey(key);
-         }
-         this._allowEmptyApply();
+         this._selectedIndex = this._getItemIndexByKey(key);
+         this._applyAllowEmpty();
       },
 
-      _prepareSelectedKeyByIndex: function(index) {
+      _prepareSelectedKeyByIndex: function (index) {
          //Вычисляем ключ по известному индексу
-         if (this._isEmptyIndex(index)) {
-            this.selectedKey = null;
-         }
-         else {
-            this.selectedKey = this._getKeyByIndex(this.selectedIndex);
-         }
-         this._allowEmptyApply();
+         this._selectedKey = this._getKeyByIndex(this._selectedIndex);
+         this._applyAllowEmpty();
       },
 
-      _getKeyByIndex: function(index) {
-         if(this._hasItemByIndex(index)) {
+      _getKeyByIndex: function (index) {
+         if (this._hasItemByIndex(index) && !this._isEmptyIndex(index)) {
             var itemContents = this._options.projection.at(index).getContents();
             return ItemsUtil.getPropertyValue(itemContents, this._options.idProperty);
          }
+         else {
+            return null
+         }
       },
 
-      _getItemIndexByKey: function(id) {
+      _getItemIndexByKey: function (id) {
+         if (id === undefined) {
+            return -1;
+         }
          var projItem = ItemsUtil.getItemById(this._options.projection, id, this._options.idProperty);
          return this._options.projection.getIndex(projItem);
       },
 
-      _hasItemByIndex: function(index) {
+      _hasItemByIndex: function (index) {
          return (typeof index != 'undefined') && (index !== null) && (typeof this._options.projection.at(index) != 'undefined');
       },
 
-      setSelectedKey: function(id) {
-         if (this.selectedKey != id) {
-            this.selectedKey = id;
+      setSelectedKey: function (id) {
+         if (this._selectedKey != id) {
+            this._selectedKey = id;
             this._prepareSelectedIndexByKey(id);
-            this._notifySelectedItem(this.selectedIndex, this.selectedKey);
+            this._notifySelectedItem(this._selectedIndex, this._selectedKey);
          }
       },
 
-      getSelectedKey: function(id) {
-         return this.selectedKey;
+      getSelectedKey: function (id) {
+         return this._selectedKey;
       },
 
-      getSelectedIndex : function() {
-         return this.selectedIndex;
+      getSelectedIndex: function () {
+         return this._selectedIndex;
       },
 
-      setSelectedByHash: function(hash) {
+      setSelectedByHash: function (hash) {
          var elem = this._options.projection.getByHash(hash);
          this.setSelectedIndex(this._options.projection.getIndex(elem));
       },
 
-      _isEmptyIndex: function(index) {
+      _isEmptyIndex: function (index) {
          return index === null || typeof index == 'undefined' || index == -1;
       },
 
-      setSelectedIndex: function(index) {
-         if (this.selectedIndex != index) {
-            this.selectedIndex = index;
+      setSelectedIndex: function (index) {
+         if (this._selectedIndex != index) {
+            this._selectedIndex = index;
             this._prepareSelectedKeyByIndex(index);
-            this._notifySelectedItem(this.selectedIndex, this.selectedKey);
+            this._notifySelectedItem(this._selectedIndex, this._selectedKey);
          }
       },
 
-      _notifySelectedItem : function(index, key) {
+      _notifySelectedItem: function (index, key) {
          this._notify('onSelectedItemChange', index, key);
       }
    });
