b6b9e46afc06d7776462f032d61048cc1ded4f08
kraynovdo <do.kraynov@tensor.ru>
2017-08-01 14:44:30 +0300

    забыл выпилить дублянку

--- components/WSControls/Lists/resources/utils/TreeItemsUtil.js
+++ components/WSControls/Lists/resources/utils/TreeItemsUtil.js
@@ -73,26 +73,6 @@ define('js!WSControls/Lists/resources/utils/TreeItemsUtil', [
          }
 
          return new TreeDisplay(displayProperties);
-      },
-
-      getTemplateData: function(data) {
-         var
-            templateData = {},
-            collection = data.projItem.getOwner().getCollection(),
-            idPropertyValue = data.getPropertyValue(data.item, data.idProperty),
-            nodePropertyValue = data.getPropertyValue(data.item, data.nodeProperty),
-            collectionItem = collection.at(collection.getIndexByValue(data.idProperty, idPropertyValue));
-
-         templateData.children = data.hierarchyRelation.getChildren(collectionItem, collection);
-         templateData.isLoaded = data.projItem.isLoaded();
-         templateData.itemLevel = data.projItem.getLevel() - 1;
-         templateData.hasLoadedChild = templateData.children.length > 0;
-         templateData.classIsLoaded = templateData.isLoaded ? ' controls-ListView__item-loaded' : '';
-         templateData.classHasLoadedChild = templateData.hasLoadedChild ? ' controls-ListView__item-with-child' : ' controls-ListView__item-without-child';
-         templateData.classNodeType = ' controls-ListView__item-type-' + (nodePropertyValue === null ? 'leaf' : nodePropertyValue === true ? 'node' : 'hidden');
-         templateData.classNodeState = nodePropertyValue !== null ? (' controls-TreeView__item-' + (data.projItem.isExpanded() ? 'expanded' : 'collapsed')) : '';
-         templateData.addClasses = templateData.classNodeType + templateData.classNodeState + templateData.classIsLoaded + templateData.classHasLoadedChild;
-         return templateData;
       }
    };
 });
