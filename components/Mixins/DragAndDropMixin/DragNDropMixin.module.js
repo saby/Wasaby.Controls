b8deb5754082822a08b12984d868cd528bd1e784
ganshinyao <yo.ganshin@tensor.ru>
2017-08-02 18:23:42 +0300

    https://online.sbis.ru/opendoc.html?guid=0aca73a6-5064-4299-bb28-a74c79117273&des= Ошибка в разработку 31.07.2017 Отображается ненужная иконка аватарки драгропа при скроллинге в реестре заметок за полоску скролла.…

--- components/Mixins/DragAndDropMixin/DragNDropMixin.module.js
+++ components/Mixins/DragAndDropMixin/DragNDropMixin.module.js
@@ -522,6 +522,7 @@ define('js!SBIS3.CONTROLS.DragNDropMixin', [
                 sel.empty();
              }
           }
+          //останавливаем распрастранение, что бы его не поймал дргой контрол с DragNDropMixin
           e.stopPropagation();
        }
     };
