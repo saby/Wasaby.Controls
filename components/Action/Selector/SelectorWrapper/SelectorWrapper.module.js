c705c672600e9d12433c178f7226a225cffa5141
Gersasha <gersasha@yandex.ru>
2017-08-05 19:29:14 +0300

    https://online.sbis.ru/opendoc.html?guid=7ef78408-a59c-4d1b-b0f6-1e5a79689dc7&des= Ошибка в разработку 05.08.2017 Platforma_3.17.100_int_test_CONTROLS_ff - При клике на запись не открывается диалог. Только FIREFOX …

--- components/Action/Selector/SelectorWrapper/SelectorWrapper.module.js
+++ components/Action/Selector/SelectorWrapper/SelectorWrapper.module.js
@@ -171,6 +171,7 @@ define('js!SBIS3.CONTROLS.SelectorWrapper', [
                   linkedObject.toggleItemsSelection([id]);
                }
             } else if(!isBranch) {
+               clickResult = false;
                this._applyItemSelect(item);
             }
          } else {
@@ -182,8 +183,8 @@ define('js!SBIS3.CONTROLS.SelectorWrapper', [
                if(this.getSelectionType() === 'allBySelectAction') {
                   return;
                }
-               clickResult = false;
             }
+            clickResult = false;
             this._applyItemSelect(item);
          }
          return clickResult;
