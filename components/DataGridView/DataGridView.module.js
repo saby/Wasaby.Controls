b0d049a633ec0f68c3ae94bed796d1152e0a2234
iv.cheremushkin <iv.cheremushkin@tensor.ru>
2017-08-11 15:31:04 +0300

    redrawThead и redrawTfoot вызывают reviveItems, который вызывает цепочку ресайзов, которые вызывают подгрузку по скроллу раньше чем отрисовываются начальные данные https://online.sbis.ru/opendoc.html?guid=bc2ff123-ad2f-4e1e-8872-15616fff00c7&des= Ошибка в разработку 11.08.2017 при открытии новой картчоки аккаунта несколько раз зовется метод Контрагент.СписокКонтрагентовКлиент…

--- components/DataGridView/DataGridView.module.js
+++ components/DataGridView/DataGridView.module.js
@@ -1005,9 +1005,9 @@ define('js!SBIS3.CONTROLS.DataGridView',
       },
 
       _redrawItems: function() {
+         DataGridView.superclass._redrawItems.apply(this, arguments);
          //FIXME в 3.7.4 поправить, не всегда надо перерисовывать, а только когда изменились колонки
          this._redrawTheadAndTfoot();
-         DataGridView.superclass._redrawItems.apply(this, arguments);
       },
       _startEditOnItemClick: function(event, id, record, target, originalEvent) {
          var
