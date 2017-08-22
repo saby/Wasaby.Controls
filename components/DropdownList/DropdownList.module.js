3379eb198b87a05dffe5ba6fd31d8a5b301bf58e
as.krasilnikov <as.krasilnikov@tensor.ru>
2017-08-22 13:54:46 +0300

    https://online.sbis.ru/opendoc.html?guid=6310bc7f-63ab-4725-8fda-9e15475a2a4e

--- components/DropdownList/DropdownList.module.js
+++ components/DropdownList/DropdownList.module.js
@@ -332,18 +332,15 @@ define('js!SBIS3.CONTROLS.DropdownList',
          $constructor: function() {
             this._publish('onClickMore');
             var self = this;
-            this._container.bind(this._isHoverMode() ? 'mouseenter' : 'click', function(){
+            this._container.bind(this._isHoverMode() ? 'mouseenter' : 'click', function(event){
                if (self._getItemsProjection()) {
-                  self.showPicker();
+                  self.showPicker(event);
                }
             });
             if (this._container.hasClass('controls-DropdownList__withoutCross')){
                this._options.pickerClassName += ' controls-DropdownList__withoutCross';
             }
             this._setHeadVariables();
-         },
-         init: function(){
-            DropdownList.superclass.init.apply(this, arguments);
          },
          _modifyOptions: function() {
             var cfg = DropdownList.superclass._modifyOptions.apply(this, arguments);
