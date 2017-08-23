1ee9ddb95599d916081b810ae6e15ccb70140b83
kraynovdo <do.kraynov@tensor.ru>
2017-08-23 13:53:53 +0300

    навигация

---
+++ components/WSControls/Lists/Controllers/Navigation.js
@@ -0,0 +1,27 @@
+define('js!WSControls/Lists/Controllers/Navigation',
+   [],
+   function () {
+      /**
+       *
+       * @author Крайнов Дмитрий
+       * @public
+       */
+      var Navigation = {
+
+
+         constructor: function() {
+
+         },
+
+         prepareQueryParams: function(projection, direction) {
+
+         },
+
+         analyzeResponseParams: function(dataset) {
+
+         }
+
+      };
+
+      return Navigation;
+   });
