e05b5bce8d4807adde2225e2562469d1f7623ceb
as.krasilnikov <as.krasilnikov@tensor.ru>
2017-08-31 18:12:58 +0300

    Нужен фильтр с текущей вкладки (см предыдущий коммит)

--- components/Mixins/SuggestTextBoxMixin/SuggestTextBoxMixin.module.js
+++ components/Mixins/SuggestTextBoxMixin/SuggestTextBoxMixin.module.js
@@ -189,7 +189,7 @@ define('js!SBIS3.CONTROLS.SuggestTextBoxMixin', [
             recordsId.push(this._getHistoryRecordId(this._historyController.at(i).get('data')));
          }
          filter[this._getListIdProperty()] = recordsId;
-         filter = cMerge(filter, this._options.listFilter || {}); //Отдаем в запрос на историю фильтр с листа
+         filter = cMerge(filter, this.getList().getFilter() || {}); //Отдаем в запрос на историю фильтр с листа
          query.where(filter).limit(12);
          return query;
       },
