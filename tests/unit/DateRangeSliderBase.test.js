beb37594727f03f39452d11544d7ed435411fa2d
au.mironov <au.mironov@tensor.ru>
2017-09-05 12:25:08 +0700

    ПОправил тесты
    https://online.sbis.ru/opendoc.html?guid=ab605e14-3c22-4d3f-a7fd-390b03f112b6

--- tests/unit/DateRangeSliderBase.test.js
+++ tests/unit/DateRangeSliderBase.test.js
@@ -6,11 +6,17 @@ define([
    describe('SBIS3.CONTROLS.DateRangeSliderBase', function () {
       describe('._getCaption', function () {
          it('should return emptyCaption option value if range dont specified.', function () {
+            if (typeof $ === 'undefined') {
+               this.skip();
+            }
             let emptyStr = '...',
                control = new DateRangeSliderBase({emptyCaption: emptyStr, showUndefined: true});
             assert.strictEqual(control._getCaption(), emptyStr);
          });
          it('should return range string value if emptyCaption option and range dont specified.', function () {
+            if (typeof $ === 'undefined') {
+               this.skip();
+            }
             let control = new DateRangeSliderBase({
                   emptyCaption: '...',
                   showUndefined: true,
@@ -20,6 +26,19 @@ define([
             assert.strictEqual(control._getCaption(), "1 января'17");
          });
 
+         it('should return emptyCaption option value if options passed as parameter and range dont specified.', function () {
+            let emptyStr = '...';
+            assert.strictEqual(DateRangeSliderBase.prototype._getCaption({emptyCaption: emptyStr, showUndefined: true}), emptyStr);
+         });
+         it('should return range string value if options passed as parameter and emptyCaption option and range dont specified.', function () {
+            let opts =  {
+                  emptyCaption: '...',
+                  showUndefined: true,
+                  startValue: new Date(2017, 0, 1),
+                  endValue: new Date(2017, 0, 1)
+               };
+            assert.strictEqual(DateRangeSliderBase.prototype._getCaption(opts), "1 января'17");
+         });
       });
    });
 });
