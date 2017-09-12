6e16bd1b08c9c3868254b6db632f0bbc42bdc24f
am.gerasimov <am.gerasimov@tensor.ru>
2017-09-12 15:56:55 +0300

    https://online.sbis.ru/opendoc.html?guid=1269c2f6-fea1-403d-a15f-afdd26f8cc7c

--- components/ComponentBinder/controllers/ScrollPagingController.module.js
+++ components/ComponentBinder/controllers/ScrollPagingController.module.js
@@ -233,8 +233,10 @@ define('js!SBIS3.CONTROLS.ScrollPagingController',
             текущее кол-во загруженных страниц + 1, если в метаинформации рекордсета есть данные о том, что на бл есть ещё записи.
             Необходимо для того, чтобы в пэйджинге не моргала кнопка перехода к следующей странице, пока грузятся данные. */
          var pagesCount = this._scrollPages.length + (view._hasNextPage(view.getItems().getMetaData().more) ? 1 : 0);
+         var pagingVisibility = pagesCount > 1 && view._getOption('infiniteScroll') !== 'up';
    
-         view.getContainer().toggleClass('controls-ScrollPaging__pagesPadding', pagesCount > 1);
+         /* Если пэйджинг скрыт - паддинг не нужен */
+         view.getContainer().toggleClass('controls-ScrollPaging__pagesPadding', pagingVisibility);
          if (this._options.paging.getSelectedKey() > pagesCount){
             this._options.paging.setSelectedKey(pagesCount);
          }
@@ -242,7 +244,7 @@ define('js!SBIS3.CONTROLS.ScrollPagingController',
 
          //Если есть страницы - покажем paging
          
-         this._options.paging.setVisible((pagesCount > 1) && !this._options.hiddenPager && view._getOption('infiniteScroll') !== 'up');
+         this._options.paging.setVisible(pagingVisibility && !this._options.hiddenPager);
       },
 
       destroy: function(){
