52f299ad5dfa24e30e32f022c2c53a63864b3657
pv.stepin <pv.stepin@tensor.ru>
2017-09-20 18:18:45 +0300

    https://online.sbis.ru/opendoc.html?guid=7a244e83-5142-4bc1-9150-c2c88b988e94

--- components/OperationsPanel/OperationsPanel.module.js
+++ components/OperationsPanel/OperationsPanel.module.js
@@ -330,25 +330,39 @@ define('js!SBIS3.CONTROLS.OperationsPanel', [
          /* Доступная под операции ширина = Ширина контейнера - ширина кнопки с меню*/
          var allowedWidth = container.width() - ITEMS_MENU_WIDTH;
 
-         /* Проверяем на вместимость только видимые операции. Операции линейно лежат в контейнере, поэтому для оптимизации выборки проверим только детей первого уровня. */
-         var operations = this._getItemsContainer().find('> .js-controls-operationsPanel__action:visible');
+         var operations = this._getItemsContainer().find('> .js-controls-operationsPanel__action');
+         operations.css('display', '');
+
+         //Отберем только видимые элементы.
+         operations = operations.filter(function(index, elem){
+            return !!$(elem).is(':visible');
+         });
 
          var width = 0;
          var isMenuNecessary = false;
          this._getItemsContainer().css('width', '');
 
          for(var i = 0, l = operations.length; i < l; i++){
-            var elemWidth = $(operations[i]).outerWidth(true);
 
-            /* Если текущая ширина привышает доступную, то ограничеваем ее, таким образом, кнопка с меню прижмется справа */
-            if(width + elemWidth > allowedWidth){
-               isMenuNecessary = true;
-               this._getItemsContainer().css('width', width);
-               break;
+            var operation = $(operations[i]);
+
+            if(!isMenuNecessary){
+               var elemWidth = operation.outerWidth(true);
+
+               /* Если текущая ширина привышает доступную, то ограничеваем ее, таким образом, кнопка с меню прижмется справа */
+               if(width + elemWidth > allowedWidth){
+                  isMenuNecessary = true;
+                  this._getItemsContainer().css('width', width);
+               }
+               else {
+                  width += elemWidth;
+               }
             }
             else {
-               width += elemWidth;
+               //Скроем оставшиеся операции. Иначе при их активации до них будет прокручиваться скролл.
+               operation.css('display', 'none');
             }
+
          }
 
          if (isMenuNecessary) {
