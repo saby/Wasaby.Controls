3126261a0b477e7ccb1fe4eba473847fb3c47c85
as.avramenko <as.avramenko@tensor.ru>
2017-09-27 17:40:46 +0300

    https://online.sbis.ru/opendoc.html?guid=223b03eb-361b-4868-b46c-57fcf08a0812

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -2074,7 +2074,7 @@ define('js!SBIS3.CONTROLS.ListView',
             if (this._scrollBinder) {
                var scrollPage, pagesCount, average, commonItemsCount, firstPageElemIndex;
                scrollPage = this._scrollBinder._getScrollPage();
-               pagesCount = this._scrollBinder.getPagesCount();
+               pagesCount = this._scrollPager.getPagesCount();
                commonItemsCount = this._getItemsProjection() ? this._getItemsProjection().getCount() : 0;
                average = commonItemsCount / pagesCount;
                firstPageElemIndex = (scrollPage - 1) * average;
