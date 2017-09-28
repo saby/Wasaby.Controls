4fe386115a5910e7a24e707f03ce945548b6e2bb
nv.belotelov <nv.belotelov@tensor.ru>
2017-09-28 18:44:42 +0300

    https://online.sbis.ru/opendoc.html?guid=37e22197-aa9b-42c2-99a8-0a331d69b61b

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -78,56 +78,8 @@ define('js!SBIS3.CONTROLS.ListView',
      'use strict';
 
       var
-         _addOptionsFromClass = function _addOptionsFromClass(opts) {
-            opts.pagerConfig = opts.pagerConfig || {};
-            var classes = [
-               { class: 'controls-small-ListView', optionName: 'isSmall', value: true, defaultValue: false },
-               { class: 'controls-ListView__disableHover', optionName: 'noHover', value: true, defaultValue: false },
-               { class: 'controls-ListView__pagerNoSizePicker', optionName: 'noSizePicker', value: true, defaultValue: false },
-               { class: 'controls-ListView__pagerNoAmount', optionName: 'noPagerAmount', value: true, defaultValue: false },
-               { class: 'controls-ListView__pagerHideEndButton', optionName: 'hideEndButton', value: true, defaultValue: false },
-               { class: 'controls-ListView__orangeMarker', optionName: 'showSelectedMarker', value: true, defaultValue: false },
-               { class: 'controls-ListView__outside-scroll-loader', optionName: 'outsideScroll', value: true, defaultValue: false }
-            ]
-            function hasClass(fullClass, className) {
-               if(typeof fullClass === 'string') {
-                  var index = fullClass.split(' ')
-                     .filter(function (el) {
-                        return el !== ''
-                     })
-                     .indexOf(className);
-                  return !!~index;
-               } else {
-                  return undefined;
-               }
-            }
-            var el;
-            for(var i = 0; i < classes.length; i++) {
-               el = classes[i];
-               ////Прокинуть правильные опции
-               // if(el.isPagerConfig) {
-               //    if(!opts.pagerConfig[el.optionName]) {
-               //       hasClass(opts.className, el.class) ? opts.pagerConfig[el.optionName] = el.value :
-               //          opts.pagerConfig[el.optionName] = el.defaultValue;
-               //    }
-               // } else {
-               //    if(!opts[el.optionName]) {
-               //       hasClass(opts.className, el.class) ? opts[el.optionName] = el.value :
-               //          opts[el.optionName] = el.defaultValue;
-               //    }
-               // }
-               if (!opts[el.optionName]) {
-                  if (hasClass(opts.className, el.class)) {
-                     opts[el.optionName] = el.value;
-                  } else {
-                     opts[el.optionName] = el.defaultValue;
-                  }
-               }
-            }
-         },
          buildTplArgsLV = function(cfg) {
             var tplOptions = cfg._buildTplArgsSt.call(this, cfg);
-            _addOptionsFromClass(cfg);
             tplOptions.multiselect = cfg.multiselect;
             tplOptions.decorators = cfg._decorators;
             tplOptions.highlightText = cfg.highlightText;
@@ -1105,8 +1057,57 @@ define('js!SBIS3.CONTROLS.ListView',
             container[bind ? 'on' : 'off']('swipe tap mousemove mouseleave touchend taphold touchstart contextmenu mousedown mouseup', this._eventProxyHdl);
          },
 
+         _addOptionsFromClass: function(opts) {
+            opts.pagerConfig = opts.pagerConfig || {};
+            var classes = [
+               { class: 'controls-small-ListView', optionName: 'isSmall', value: true, defaultValue: false },
+               { class: 'controls-ListView__disableHover', optionName: 'noHover', value: true, defaultValue: false },
+               { class: 'controls-ListView__pagerNoSizePicker', optionName: 'noSizePicker', value: true, defaultValue: false },
+               { class: 'controls-ListView__pagerNoAmount', optionName: 'noPagerAmount', value: true, defaultValue: false },
+               { class: 'controls-ListView__pagerHideEndButton', optionName: 'hideEndButton', value: true, defaultValue: false },
+               { class: 'controls-ListView__orangeMarker', optionName: 'showSelectedMarker', value: true, defaultValue: false },
+               { class: 'controls-ListView__outside-scroll-loader', optionName: 'outsideScroll', value: true, defaultValue: false }
+            ]
+            function hasClass(fullClass, className) {
+               if(typeof fullClass === 'string') {
+                  var index = fullClass.split(' ')
+                     .filter(function (el) {
+                        return el !== ''
+                     })
+                     .indexOf(className);
+                  return !!~index;
+               } else {
+                  return undefined;
+               }
+            }
+            var el;
+            for(var i = 0; i < classes.length; i++) {
+               el = classes[i];
+               ////Прокинуть правильные опции
+               // if(el.isPagerConfig) {
+               //    if(!opts.pagerConfig[el.optionName]) {
+               //       hasClass(opts.className, el.class) ? opts.pagerConfig[el.optionName] = el.value :
+               //          opts.pagerConfig[el.optionName] = el.defaultValue;
+               //    }
+               // } else {
+               //    if(!opts[el.optionName]) {
+               //       hasClass(opts.className, el.class) ? opts[el.optionName] = el.value :
+               //          opts[el.optionName] = el.defaultValue;
+               //    }
+               // }
+               if (!opts[el.optionName]) {
+                  if (hasClass(opts.className, el.class)) {
+                     opts[el.optionName] = el.value;
+                  } else {
+                     opts[el.optionName] = el.defaultValue;
+                  }
+               }
+            }
+         },
+
          _modifyOptions : function(opts){
             var lvOpts = ListView.superclass._modifyOptions.apply(this, arguments);
+            this._addOptionsFromClass(lvOpts);
             //Если нам задали бесконечный скролл в виде Bool, то если true, то 'down' иначе null
             if (lvOpts.hasOwnProperty('infiniteScroll')){
                lvOpts.infiniteScroll = typeof lvOpts.infiniteScroll === 'boolean' ?
@@ -1549,7 +1550,9 @@ define('js!SBIS3.CONTROLS.ListView',
                var oldHoveredItem = this.getHoveredItem().container;
                oldHoveredItem.removeClass('controls-ListView__hoveredItem');
             }
-            target.addClass('controls-ListView__hoveredItem');
+            if(!this._options.noHover && !target.hasClass('controls-EditAtPlace')) {
+               target.addClass('controls-ListView__hoveredItem');
+            }
 
             this._hoveredItem = this._getElementData(target);
             this._notifyOnChangeHoveredItem();
@@ -3699,7 +3702,9 @@ define('js!SBIS3.CONTROLS.ListView',
          _setHoveredItem: function(hoveredItem) {
             //
             if (hoveredItem.container) {
-               hoveredItem.container.addClass('controls-ListView__hoveredItem');
+               if(!this._options.noHover && !hoveredItem.container.hasClass('controls-EditAtPlace')) {
+                  hoveredItem.container.addClass('controls-ListView__hoveredItem');
+               }
                this._hoveredItem = hoveredItem;
             }
          },
