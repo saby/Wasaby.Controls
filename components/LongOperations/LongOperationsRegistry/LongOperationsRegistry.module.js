fe18306c972787f07036c698e737a60613c957e2
as.avramenko <as.avramenko@tensor.ru>
2017-10-05 15:12:13 +0300

    https://online.sbis.ru/opendoc.html?guid=12e0b96e-881b-479a-b708-8814c9b2e742

--- components/LongOperations/LongOperationsRegistry/LongOperationsRegistry.module.js
+++ components/LongOperations/LongOperationsRegistry/LongOperationsRegistry.module.js
@@ -105,18 +105,7 @@ define('js!SBIS3.CONTROLS.LongOperationsRegistry',
 
             view.setGroupBy({
                field: 'status',
-               method: function (record, at, isLast) {
-                  var STATUSES = LongOperationEntry.STATUSES;
-                  var status = record.get('status');
-                  var prev = self._previousGroupBy;
-                  if (prev === null
-                        || (status === STATUSES.suspended && prev === STATUSES.running)
-                        || (status === STATUSES.ended && (prev === STATUSES.running || prev === STATUSES.suspended))) {
-                     self._previousGroupBy = status;
-                     return true;
-                  }
-               },
-               /*contentTemplate*/template: groupTpl
+               contentTemplate: groupTpl
             });
 
             this._bindEvents();
