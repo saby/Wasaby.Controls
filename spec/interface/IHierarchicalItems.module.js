f4c1f4d822a6963c22a30c9e560d67c28b658019
begunovav <av.begunov2@tensor.ru>
2017-10-12 13:22:14 +0300

    интерфейс работы с иерархией

---
+++ spec/interface/IHierarchicalItems.module.js
@@ -0,0 +1,19 @@
+define('js!SBIS3.SPEC.interface.IHierarchicalItems', [
+], function() {
+
+   /**
+    * Интерфейс работы списков.
+    * @mixin SBIS3.SPEC.interface.IHierarchicalItems
+    * @public
+    */
+
+   /**
+    * @name SBIS3.SPEC.interface.IItems#parentProperty
+    * @cfg {String} Имя поля в объекте из набора items, которое используется для создания отношения иерархии.
+    */
+   /**
+    * @name SBIS3.SPEC.interface.IItems#hasChildrenProperty
+    * @cfg {String} Имя поля в объекте из набора items, которое используется для создания определения наличия детей.
+    */
+
+});
