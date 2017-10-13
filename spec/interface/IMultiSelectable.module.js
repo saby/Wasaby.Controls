1e114deda889b832c659932a9b21ae514a3f0a02
begunovav <av.begunov2@tensor.ru>
2017-10-13 12:36:06 +0300

    мультиселект

--- spec/interface/IMultiSelectable.module.js
+++ spec/interface/IMultiSelectable.module.js
@@ -9,12 +9,24 @@ define('js!SBIS3.SPEC.interface.IMultiSelectable', [
 
    /**
     * @name SBIS3.SPEC.interface.IMultiSelectable#selectedKeys
-    * @cfg {Array} Ключевое поле выбранного элемента коллекции.
+    * @cfg {Array} Массив ключевых полей выбранных элементов коллекции.
     */
 
    /**
     * @event SBIS3.SPEC.interface.IInputText#onChangeSelectedKeys Происходит при изменении массива выделенных записей.
-    * @param {Array} keys Текст в поле ввода.
+    * @param {Array} keys массив ключей выбранных элементов коллекции.
+    * @param {Array} added массив добавленных ключей в списке выбранных элементов коллекции.
+    * @param {Array} deleted массив удаленных ключей из списка выбранных элементов коллекции.
+    */
+
+   /**
+    * Выделить все
+    * @function SBIS3.SPEC.Control#selectAll
+    */
+
+   /**
+    * Снять выделение
+    * @function SBIS3.SPEC.Control#UnselectAll
     */
 
 });
