955d5216ec59a18d9173447b0dbb39843fc67063
begunovav <av.begunov2@tensor.ru>
2017-10-13 16:33:52 +0300

    popup

---
+++ spec/popup/Manager.module.js
@@ -0,0 +1,45 @@
+define('js!SBIS3.SPEC.popup.Manager', [
+], function() {
+
+   /**
+    * Однострочное текстовое поле ввода.
+    * @class SBIS3.SPEC.popup.Manager
+    * @control
+    * @public
+    */
+
+
+
+   /**
+    * Показать всплывающее окно
+    * @function SBIS3.SPEC.popup.Manager#show
+    * @param template компонент, который будет показан в окне
+    * @param componentOptions опции комонента
+    * @param strategy стратегия позиционирования всплывающего окна
+    * @param opener компонент, который инициировал открытие окна
+    */
+});
+
+/*
+
+         Popup = extends({
+            constructor(){
+               this.getContantainer().on('mousedown', function(e){
+                  e.preventDefault();
+               })
+
+            },
+            close(){
+            }
+         })
+ */
+
+/*
+   show: function(template, componentOptions, strategy, opener){
+
+      window.addEventListener('onMouseDown', function(){
+
+      }
+   }
+
+ */
