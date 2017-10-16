d4b0dfda1f24baa33d561c0256c1accd4dd4c841
sa.bogdanov <sa.bogdanov@tensor.ru>
2017-10-16 18:38:37 +0300

    Доработка описания

--- spec/input/interface/IInputTag.module.js
+++ spec/input/interface/IInputTag.module.js
@@ -2,14 +2,14 @@ define('js!SBIS3.SPEC.input.interface.IInputTag', [
 ], function() {
 
    /**
-    * Интерфейс работы c тегами (цветными информационными уголками) в полях ввода.
+    * Интерфейс работы c тегами (цветными информационными иконками) в полях ввода.
     * @mixin SBIS3.SPEC.input.interface.IInputTag
     * @public
     */
 
    /**
     * @name SBIS3.SPEC.input.interface.IInputTag#tagStyle
-    * @cfg {String | undefined} Цвет тега
+    * @cfg {String | undefined} Цвет тега (цветная информационная иконка).
     * @variant primary
     * @variant done
     * @variant attention
@@ -18,10 +18,10 @@ define('js!SBIS3.SPEC.input.interface.IInputTag', [
     */
 
    /**
-    * @event SBIS3.SPEC.input.interface.IInputTag#onTagClick Происходит при клике по тегу (информационному уголку).
+    * @event SBIS3.SPEC.input.interface.IInputTag#onTagClick Происходит при клике по тегу (информационной иконке).
     */
 
    /**
-    * @event SBIS3.SPEC.input.interface.IInputTag#onTagHover Происходит, когда курсор мыши входит в область тега (информационного уголка).
+    * @event SBIS3.SPEC.input.interface.IInputTag#onTagHover Происходит, когда курсор мыши входит в область тега (информационной иконки).
     */
 });
