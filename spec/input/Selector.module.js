234e42f0206579eb4e01215932a23d0a4801ff79
pv.stepin <pv.stepin@tensor.ru>
2017-10-16 16:04:58 +0300

    Добавил Selector

---
+++ spec/input/Selector.module.js
@@ -0,0 +1,20 @@
+define('js!SBIS3.SPEC.input.Selector', [
+], function() {
+
+   /**
+    * Поле выбора.
+    * @class SBIS3.SPEC.input.Selector
+    * @extends SBIS3.SPEC.Control
+    * @mixes SBIS3.SPEC.input.interface.IInputText
+    * @mixes SBIS3.SPEC.input.interface.IPlaceholder
+    * @mixes SBIS3.SPEC.interface.IDataSource
+    * @mixes SBIS3.SPEC.interface.IItems
+    * @mixes SBIS3.SPEC.input.interface.IValidation
+    * @mixes SBIS3.SPEC.input.interface.IInputTag
+    * @mixes SBIS3.SPEC.interface.ISingleSelectable
+    * @control
+    * @public
+    * @category Inputs
+    */
+
+});
