b77d8a8754b7ad46d07b21f7619040fc52957b6a
sa.bogdanov <sa.bogdanov@tensor.ru>
2017-10-23 14:39:15 +0300

    Revert "Merge branch '3.17.200/feature/spec_text_box' of git.sbis.ru:sbis/controls into 3.17.200/feature/spec_text_box"
    This reverts commit 6be75b473c3471d9c0ded4cab99684caea1339e6, reversing
    changes made to 7798d9b70bb67d0b3b6267498a4ce3641c38a527.

--- components/WSControls/Windows/Strategy/IPositioningStrategy.js
+++
@@ -1,9 +0,0 @@
-define('js!WSControls/Windows/Strategy/IPositioningStrategy', [],
-   function () {
-      return {
-         getPosition: function(){
-            throw new Error('Method getPosition must be implemented');
-         }
-      };
-   }
-);
