198a7eb8e40212d907d833573767f91b40fe1306
nv.belotelov <nv.belotelov@tensor.ru>
2017-10-25 08:02:07 +0300

    https://online.sbis.ru/opendoc.html?guid=fc3a823d-562e-4046-9c91-ee12d15d9de4

--- components/ListView/ListView.module.js
+++ components/ListView/ListView.module.js
@@ -2607,11 +2607,11 @@ define('js!SBIS3.CONTROLS.ListView',
                         this._toggleEmptyData(false);
                         this._showToolbar(model);
                         this.setSelectedKey(model.getId());
+                        var curSelected = $('.controls-ListView__item[data-id="' +  (model.getId() === undefined ? '' : model.getId()) + '"]', this._container);
                         if (model.getState() === Record.RecordState.DETACHED) {
                            $(".controls-ListView__item", this._getItemsContainer())
                               .removeClass('controls-ListView__item__selected')
                               .removeClass('controls-ListView__item__selected__withMarker');
-                           var curSelected = $('.controls-ListView__item[data-id="' +  (model.getId() === undefined ? '' : model.getId()) + '"]', this._container);
                            curSelected.addClass('controls-ListView__item__selected');
                            if(this._options.showSelectedMarker) {
                               curSelected.addClass('controls-ListView__item__selected__withMarker');
@@ -2620,7 +2620,7 @@ define('js!SBIS3.CONTROLS.ListView',
                         else {
                            this.setSelectedKey(model.getId());
                         }
-                        curSelected.find('controls-ListView__itemCheckBox').addClass('controls-ListView__itemCheckBox__showAlways');
+                        curSelected.find('.controls-ListView__itemCheckBox').addClass('controls-ListView__itemCheckBox__showAlways');
                         // Могут быть операции над записью с тулбаром под записью. В таком случае на ListView вешается класс с padding-bottom.
                         // Этот отступ при скроле тоже должен учитываться.
                         // Поэтому вначале подскролливаем к тулбару и затем скролим к элементу.
