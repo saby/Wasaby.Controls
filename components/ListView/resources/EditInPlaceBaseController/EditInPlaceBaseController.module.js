6b1f21bcb0841527f644e8ae2060e177b74f607f
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2017-10-27 17:37:20 +0300

    https://online.sbis.ru/opendoc.html?guid=3e397119-703a-41b5-870c-1b50187929ad

--- components/ListView/resources/EditInPlaceBaseController/EditInPlaceBaseController.module.js
+++ components/ListView/resources/EditInPlaceBaseController/EditInPlaceBaseController.module.js
@@ -284,6 +284,7 @@ define('js!SBIS3.CONTROLS.EditInPlaceBaseController',
                             itemProjItem = self._options.itemsProjection.getItemBySourceItem(model);
                         self._getEip().edit(preparedRecord, itemProjItem, withoutActivateFirstControl);
                         editingRecord = self._getEip().getEditingRecord();
+                        self._notify('onAfterBeginEdit', editingRecord);
                         //TODO: необходимо разбивать контроллер редактирования по месту, для плоских и иерархических представлений
                         if (self._options.parentProperty) {
                            parentProjItem = itemProjItem.getParent();
@@ -292,7 +293,6 @@ define('js!SBIS3.CONTROLS.EditInPlaceBaseController',
                         if (!self._pendingOperation) {
                            self._subscribeToAddPendingOperation(editingRecord);
                         }
-                        self._notify('onAfterBeginEdit', editingRecord);
                      }
                      return editingRecord;
                   })
