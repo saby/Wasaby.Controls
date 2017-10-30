46de433e1c97d2b8a5490f5c4d31562e4c589092
va.spirin <va.spirin@tensor.ru>
2017-10-30 12:59:04 +0700

    RichEditor: Поправлена функция reduceInlineTextElements в TinyMCE - добавлена проверка на промежуточные узлы, ломающие стиль
    1174471769 https://online.sbis.ru/opendoc.html?guid=8baf7b51-a733-4b3a-accf-74c75c182e8c

--- components/RichEditor/RichEditorComponents/RichTextArea/resources/tinymce/tinymce.js
+++ components/RichEditor/RichEditorComponents/RichTextArea/resources/tinymce/tinymce.js
@@ -24208,10 +24208,29 @@
             if (merge) {
                var root = editor.getBody(), elementUtils = new ElementUtils(dom);
 
-               Tools.each(dom.select('*[data-mce-fragment]'), function(node) {
-                  for (var testNode = node.parentNode; testNode && testNode != root; testNode = testNode.parentNode) {
-                     if (textInlineElements[node.nodeName.toLowerCase()] && elementUtils.compare(testNode, node)) {
-                        dom.remove(node, true);
+               Tools.each(dom.select('*[data-mce-fragment]'), function (node) {
+                  if (textInlineElements[node.nodeName.toLowerCase()]) {
+                     for (var ansestor = node.parentNode; ansestor && ansestor != root; ansestor = ansestor.parentNode) {
+                        if (elementUtils.compare(ansestor, node)) {
+                           // Найден выщестоящий элемент, имеющий такой же тип, атрибуты и инлайн-стили, что и проверяемый node. Значит, можно
+                           // удалить node, пересадив его содержимое на его место. Но ТОЛЬКО ЕСЛИ между ansestor и node нет элементов,
+                           // переопределяющих их общий стиль иначе
+                           var hasMiddle;
+                           var style = dom.parseStyle(dom.getAttrib(node, 'style'));
+                           for (var n = node.parentNode; n && n != ansestor; n = n.parentNode) {
+                              var s = dom.parseStyle(dom.getAttrib(n, 'style'));
+                              for (var p in style) {
+                                 if (p in s && s[p] !== style[p]) {
+                                    hasMiddle = true;
+                                    break;
+                                 }
+                              }
+                           }
+                           if (!hasMiddle) {
+                              dom.remove(node, true);
+                              break;
+                           }
+                        }
                      }
                   }
                });
