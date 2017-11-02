05fad762bea09a7f6aff392042b2b2c9f43ec6c2
pv.stepin <pv.stepin@tensor.ru>
2017-11-02 14:04:21 +0300

    https://online.sbis.ru/opendoc.html?guid=6616326e-777a-460d-ba7c-34da729932c1

---
+++ Controls/input/Suggest.js
@@ -0,0 +1,45 @@
+define('js!Controls/Input/Suggest', [
+], function() {
+
+   /**
+    * Поле ввода с автодополнением
+    * @class Controls/Input/Suggest
+    * @extends Controls/Input/Text
+    * @control
+    * @public
+    * @category Input
+    */
+
+   //В базовый???Под вопросом.
+   /**
+    * @name Controls/Input/Suggest#withoutCross
+    * @cfg {Boolean} Скрыть крестик удаления значения
+    */
+
+   //Обсуждаемо
+   /**
+    * @event Controls/Input/Suggest#onChooserClick Происходит при клике на кнопку открытия диалога выбора
+    */
+
+   /**
+    * @name Controls/Input/Suggest#startCharacter
+    * @cfg {Number} Минимальное количество символов для отображения автодополнения
+    */
+
+   //Обсуждаемо
+   /**
+    * @name Controls/Input/Suggest#list
+    * @cfg {Object} Устанавливает конфигурацию выпадающего блока, отображающего список значений для автодополнения
+    */
+
+   /**
+    * @name Controls/Input/Suggest#showEmptySuggest
+    * @cfg {Boolean} Показывать ли выпадающий блок, при пустом списке
+    */
+
+   /**
+    * @name Controls/Input/Suggest#searchParam
+    * @cfg {string} Устанавливает имя параметра, который будет передан при вызове метода БЛ
+    */
+
+});
