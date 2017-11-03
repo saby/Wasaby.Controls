0eeabf2221d3d99ff492b1f2a2bffe2e15086ec9
kraynovdo <do.kraynov@tensor.ru>
2017-11-03 09:39:18 +0300

    доделал алгоритм заполнения пэйджинга

--- Controls/List/Paging/DigitButtons.js
+++ Controls/List/Paging/DigitButtons.js
@@ -10,6 +10,7 @@ define('js!Controls/List/Paging/DigitButtons', [
    var
       ModuleClass = BaseControl.extend({
          _template: template,
+         //получаем граничные цифры, окружающие выбранный элемент, по условия +-3 в обе стороны (4 5 6 [7] 8 9 10)
          __calcSurroundElemens: function(digitsCount, currentDigit) {
             var first, last;
             first = currentDigit - SUR_ELEMENTS_STEP;
@@ -27,46 +28,45 @@ define('js!Controls/List/Paging/DigitButtons', [
             }
          },
 
-         __initDrawedDigits: function(digitsCount, currentDigit, full) {
+         __initDrawnDigits: function(digitsCount, currentDigit, full) {
 
             var
                surElements,
-               needLeftDot = false,
-               needRightDot = false,
-               drawedDigits = [];
+               drawnDigits = [];
 
             if (digitsCount) {
 
                surElements = this.__calcSurroundElemens(digitsCount, currentDigit);
 
-               if (full) {
-                  if (surElements.first - 2 > 1) {
-                     needLeftDot = true;
+               if (surElements.first > 1) {
+                  //если левая граничная цифра больше единицы, то единицу точно рисуем
+                  drawnDigits.push(1);
+                  //если левая граничная цифра больше 3, надо рисовать многоточие (1 ... 4 5 6 [7])
+                  if (surElements.first > 3) {
+                     drawnDigits.push('...');
                   }
-                  if (surElements.last + 2 < digitsCount) {
-                     needRightDot = true;
+                  else if (surElements.first == 3) {//а если 3, то надо рисовать двойку по правилу исключения, что многоточием не может заменяться одна цифра
+                     drawnDigits.push(2);
                   }
                }
 
-               for (var i = 1; i <= digitsCount; i++) {
-                  if (i == 1) {
-                     drawedDigits.push(i);
-                     if (needLeftDot) {
-                        drawedDigits.push('...');
-                     }
+               //рисуем все граничные цифры
+               for (var i = surElements.first; i <= surElements.last; i++) {
+                  drawnDigits.push(i);
+               }
+
+               //и рисуем правый блок аналогично левому, но в противоположную строну
+               if (surElements.last < digitsCount) {
+                  if (surElements.last < digitsCount - 2) {
+                     drawnDigits.push('...');
                   }
-                  else if (i == digitsCount) {
-                     if (needRightDot) {
-                        drawedDigits.push('...');
-                     }
-                     drawedDigits.push(i);
+                  else if (surElements.last < digitsCount - 1) {
+                     drawnDigits.push(digitsCount - 1);
                   }
-                  else if (((i >= surElements.first) || !needLeftDot) && ((i <= surElements.last) || !needRightDot)) {
-                     drawedDigits.push(i);
-                  }
+                  drawnDigits.push(digitsCount);
                }
             }
-            return drawedDigits;
+            return drawnDigits;
          }
 
       });
