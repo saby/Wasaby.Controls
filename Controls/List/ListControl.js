36dcd0fcac74787f0ab9f25b2fc62556f50afac8
pv.stepin <pv.stepin@tensor.ru>
2017-11-08 16:28:52 +0300

    Добавил описание

--- Controls/List/ListControl.js
+++ Controls/List/ListControl.js
@@ -78,30 +78,44 @@ define('js!Controls/List/ListControl', [
    /**
     * @typedef {String} ListNavigationSource
     * @variant position Описание
+    * @variant offset Описание
+    * @variant page Описание
     */
 
    /**
     * @typedef {String} ListNavigationView
     * @variant infinity Описание
+    * @variant pages Описание
+    * @variant demand Описание
     */
 
    /**
-    * @typedef {Object} ListNavigationSourceConfig
-    * @property {Number} offset Описание
+    * @typedef {Object} ListNavigationPositionSourceConfig
+    * @property {String} field Описание
+    * @property {String} direction Описание
+    */
+
+   /**
+    * @typedef {Object} ListNavigationOffsetSourceConfig
     * @property {Number} limit Описание
     */
 
    /**
-    * @typedef {Object} ListNavigationViewConfig
-    * @property {Boolean} lastPage Описание
+    * @typedef {Object} ListNavigationInfinityViewConfig
+    * @property {String} pagingMode Описание
+    */
+
+   /**
+    * @typedef {Object} ListNavigationPagesViewConfig
+    * @property {Boolean} pagesCountSelector Описание
     */
 
    /**
     * @typedef {Object} ListNavigation
     * @property {ListNavigationSource} source Описание
     * @property {ListNavigationView} view Описание
-    * @property {ListNavigationSourceConfig} sourceConfig Описание
-    * @property {ListNavigationViewConfig} viewConfig Описание
+    * @property {ListNavigationPositionSourceConfig|ListNavigationOffsetSourceConfig} sourceConfig Описание
+    * @property {ListNavigationInfinityViewConfig|ListNavigationPagesViewConfig} viewConfig Описание
     */
 
    /**
