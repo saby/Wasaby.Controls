be40d3a23785cfad9c1f948eb324a1d2fc33e935
am.gerasimov <am.gerasimov@tensor.ru>
2017-11-17 10:51:37 +0300

    https://online.sbis.ru/opendoc.html?guid=1bfefae0-0bd8-4d4d-a07b-fc1fcf6ac17b

--- components/FilterButton/FilterButton.module.js
+++ components/FilterButton/FilterButton.module.js
@@ -404,7 +404,7 @@ define('js!SBIS3.CONTROLS.FilterButton',
                    side: 'top'
                 },
                 closeButton: true,
-                locationStrategy: 'bodyBounds',
+                locationStrategy: detection.isMobilePlatform ? 'bodyBounds' : null,
                 closeByExternalClick: true,
                 closeOnTargetMove: !detection.isMobilePlatform,
                 context: context,
