9c078d7592070f28d40bf569fce67e883f05873d
kraynovdo <do.kraynov@tensor.ru>
2017-12-01 10:03:50 +0300

    поправил замечания по пэйджингу

--- Controls/List/Paging.js
+++ Controls/List/Paging.js
@@ -9,85 +9,91 @@ define('js!Controls/List/Paging', [
              template
    ) {
    'use strict';
-   var ModuleClass = BaseControl.extend(
-      {
-         _template: template,
-         _stateBegin: 'normal',
-         _stateEnd: 'normal',
-         _stateNext: 'normal',
-         _statePrev: 'normal',
+   var _private, ModuleClass;
 
-         constructor: function(cfg) {
-            ModuleClass.superclass.constructor.apply(this, arguments);
-            this._stateBegin = cfg.stateBegin || 'disabled';
-            this._stateEnd = cfg.stateEnd || 'disabled';
-            this._stateNext = cfg.stateNext || 'disabled';
-            this._statePrev = cfg.statePrev || 'disabled';
-         },
+   _private = {
+      initArrowDefaultStates: function(self, config) {
+         self._stateBegin = config.stateBegin || 'disabled';
+         self._stateEnd = config.stateEnd || 'disabled';
+         self._stateNext = config.stateNext || 'disabled';
+         self._statePrev = config.statePrev || 'disabled';
+      },
 
-         _beforeMount: function(newOptions) {
-            if (newOptions.showDigits) {
-               this.__calcBtnStates(newOptions.selectedKey);
-            }
-         },
-
-         _beforeUpdate: function(newOptions) {
-            if (newOptions.showDigits) {
-               this.__calcBtnStates(newOptions.selectedKey);
-            }
-            else {
-               this._stateBegin = newOptions.stateBegin || 'disabled';
-               this._stateEnd = newOptions.stateEnd || 'disabled';
-               this._stateNext = newOptions.stateNext || 'disabled';
-               this._statePrev = newOptions.statePrev || 'disabled';
-            }
-         },
-
-         __calcBtnStates: function(selKey) {
-            if (selKey <= 1) {
-               this._stateBegin = 'disabled';
-               this._statePrev = 'disabled';
-            }
-            else {
-               this._stateBegin = 'normal';
-               this._statePrev = 'normal';
-            }
-
-            if (selKey >= this._options.pagesCount) {
-               this._stateEnd = 'disabled';
-               this._stateNext = 'disabled';
-            }
-            else {
-               this._stateEnd = 'normal';
-               this._stateNext = 'normal';
-            }
-         },
-
-         __digitClick: function(e, digit) {
-            this.__changePage(digit);
-         },
-
-         __changePage: function(page) {
-            if (this._options.selectedKey != page) {
-               this._notify('onChangeSelectedKey', page);
-            }
-         },
-
-         __arrowClick: function(e, btnName) {
-            if(this['_state' + btnName] == 'normal') {
-               if (this._options.showDigits) {
-                  var targetPage;
-                  switch (btnName) {
-                     case 'Begin': targetPage = 1; break;
-                     case 'End': targetPage = this._options.pagesCount; break;
-                     case 'Prev': targetPage = this._options.selectedKey - 1; break;
-                     case 'Next': targetPage = this._options.selectedKey + 1; break;
-                  }
-                  this.__changePage(targetPage);
-               }
-               this._notify('onArrowClick', btnName);
-            }
+      initArrowStateBySelectedPage: function(self, page, config) {
+         if (page <= 1) {
+            self._stateBegin = 'disabled';
+            self._statePrev = 'disabled';
          }
-      });
+         else {
+            self._stateBegin = 'normal';
+            self._statePrev = 'normal';
+         }
+
+         if (page >= config.pagesCount) {
+            self._stateEnd = 'disabled';
+            self._stateNext = 'disabled';
+         }
+         else {
+            self._stateEnd = 'normal';
+            self._stateNext = 'normal';
+         }
+      }
+   };
+
+   ModuleClass = BaseControl.extend({
+      _template: template,
+      _stateBegin: 'normal',
+      _stateEnd: 'normal',
+      _stateNext: 'normal',
+      _statePrev: 'normal',
+
+      _beforeMount: function(newOptions) {
+         if (newOptions.showDigits) {
+            _private.__calcBtnStates(this, newOptions.selectedPage, newOptions);
+         }
+         else {
+            _private.initArrowDefaultStates(this, newOptions);
+         }
+      },
+
+      _beforeUpdate: function(newOptions) {
+         if (newOptions.showDigits) {
+            _private.__calcBtnStates(this, newOptions.selectedPage, newOptions);
+         }
+         else {
+            _private.initArrowDefaultStates(this, newOptions);
+         }
+      },
+
+      __calcBtnStates: function(selKey) {
+
+      },
+
+      __digitClick: function(e, digit) {
+         this.__changePage(digit);
+      },
+
+      __changePage: function(page) {
+         if (this._options.selectedPage != page) {
+            this._notify('onChangeSelectedPage', page);
+         }
+      },
+
+      __arrowClick: function(e, btnName) {
+         if(this['_state' + btnName] == 'normal') {
+            if (this._options.showDigits) {
+               var targetPage;
+               switch (btnName) {
+                  case 'Begin': targetPage = 1; break;
+                  case 'End': targetPage = this._options.pagesCount; break;
+                  case 'Prev': targetPage = this._options.selectedPage - 1; break;
+                  case 'Next': targetPage = this._options.selectedPage + 1; break;
+               }
+               this.__changePage(targetPage);
+            }
+            this._notify('onArrowClick', btnName);
+         }
+      }
+   });
    return ModuleClass;
 });
