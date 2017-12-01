338cb660ecf8171832255df84b9160ee1f1837e7
kraynovdo <do.kraynov@tensor.ru>
2017-12-01 09:43:59 +0300

    исправил замечания по DigitButtons

--- Controls/List/Paging/DigitButtons.js
+++ Controls/List/Paging/DigitButtons.js
@@ -7,90 +7,97 @@ define('js!Controls/List/Paging/DigitButtons', [
    'css!Controls/List/Paging/DigitButtons'
 ], function(BaseControl, template) {
    'use strict';
-   var SUR_ELEMENTS_STEP = 3;
-   var
-      ModuleClass = BaseControl.extend({
-         _selectedKey: null,
-         _template: template,
-         _digits: null,
+   var SUR_ELEMENTS_STEP = 3, _private, ModuleClass;
 
-         constructor: function(cfg) {
-            ModuleClass.superclass.constructor.apply(this, arguments);
-            this._digits = [];
-         },
 
-         _beforeMount: function(newOptions) {
-            this._digits = this.__getDrawnDigits(newOptions.count, newOptions.selectedKey);
-         },
+   _private = {
+      //получаем граничные цифры, окружающие выбранный элемент, по условия +-3 в обе стороны (4 5 6 [7] 8 9 10)
+      getSurroundElemens: function(digitsCount, currentDigit) {
+         var first, last;
+         first = currentDigit - SUR_ELEMENTS_STEP;
+         last = currentDigit + SUR_ELEMENTS_STEP;
 
-         _beforeUpdate: function(newOptions) {
-            if (newOptions.count != this._options.count || newOptions.selectedKey != this._options.selectedKey ) {
-               this._digits = this.__getDrawnDigits(newOptions.count, newOptions.selectedKey);
-            }
-         },
-
-         //получаем граничные цифры, окружающие выбранный элемент, по условия +-3 в обе стороны (4 5 6 [7] 8 9 10)
-         __getSurroundElemens: function(digitsCount, currentDigit) {
-            var first, last;
-            first = currentDigit - SUR_ELEMENTS_STEP;
-            last = currentDigit + SUR_ELEMENTS_STEP;
-
-            if (first < 1) {
-               first = 1;
-            }
-            if (last > digitsCount) {
-               last = digitsCount;
-            }
-            return {
-               first : first,
-               last: last
-            }
-         },
-
-         __getDrawnDigits: function(digitsCount, currentDigit, full) {
-            var
-               surElements,
-               drawnDigits = [];
-
-            if (digitsCount) {
-
-               surElements = this.__getSurroundElemens(digitsCount, currentDigit);
-
-               if (surElements.first > 1) {
-                  //если левая граничная цифра больше единицы, то единицу точно рисуем
-                  drawnDigits.push(1);
-                  //если левая граничная цифра больше 3, надо рисовать многоточие (1 ... 4 5 6 [7])
-                  if (surElements.first > 3) {
-                     drawnDigits.push('...');
-                  }
-                  else if (surElements.first == 3) {//а если 3, то надо рисовать двойку по правилу исключения, что многоточием не может заменяться одна цифра
-                     drawnDigits.push(2);
-                  }
-               }
-
-               //рисуем все граничные цифры
-               for (var i = surElements.first; i <= surElements.last; i++) {
-                  drawnDigits.push(i);
-               }
-
-               //и рисуем правый блок аналогично левому, но в противоположную строну
-               if (surElements.last < digitsCount) {
-                  if (surElements.last < digitsCount - 2) {
-                     drawnDigits.push('...');
-                  }
-                  else if (surElements.last < digitsCount - 1) {
-                     drawnDigits.push(digitsCount - 1);
-                  }
-                  drawnDigits.push(digitsCount);
-               }
-            }
-            return drawnDigits;
-         },
-         __digitClick: function(e, digit) {
-            this._notify('onDigitClick', digit);
+         if (first < 1) {
+            first = 1;
          }
+         if (last > digitsCount) {
+            last = digitsCount;
+         }
+         return {
+            first : first,
+            last: last
+         }
+      },
+      getDrawnDigits: function(digitsCount, currentDigit) {
+         var
+            surElements,
+            drawnDigits = [];
 
-      });
+         if (digitsCount) {
+
+            surElements = _private.getSurroundElemens(digitsCount, currentDigit);
+
+            if (surElements.first > 1) {
+               //если левая граничная цифра больше единицы, то единицу точно рисуем
+               drawnDigits.push(1);
+               //если левая граничная цифра больше 3, надо рисовать многоточие (1 ... 4 5 6 [7])
+               if (surElements.first > 3) {
+                  drawnDigits.push('...');
+               }
+               else if (surElements.first == 3) {//а если 3, то надо рисовать двойку по правилу исключения, что многоточием не может заменяться одна цифра
+                  drawnDigits.push(2);
+               }
+            }
+
+            //рисуем все граничные цифры
+            for (var i = surElements.first; i <= surElements.last; i++) {
+               drawnDigits.push(i);
+            }
+
+            //и рисуем правый блок аналогично левому, но в противоположную строну
+            if (surElements.last < digitsCount) {
+               if (surElements.last < digitsCount - 2) {
+                  drawnDigits.push('...');
+               }
+               else if (surElements.last < digitsCount - 1) {
+                  drawnDigits.push(digitsCount - 1);
+               }
+               drawnDigits.push(digitsCount);
+            }
+         }
+         return drawnDigits;
+      }
+   };
+
+   ModuleClass = BaseControl.extend({
+      _selectedKey: null,
+      _template: template,
+      _digits: null,
+
+      constructor: function(cfg) {
+         ModuleClass.superclass.constructor.apply(this, arguments);
+         this._digits = [];
+      },
+
+      _beforeMount: function(newOptions) {
+         this._digits = _private.getDrawnDigits(newOptions.count, newOptions.selectedKey);
+      },
+
+      _beforeUpdate: function(newOptions) {
+         if (newOptions.count != this._options.count || newOptions.selectedKey != this._options.selectedKey ) {
+            this._digits = _private.getDrawnDigits(newOptions.count, newOptions.selectedKey);
+         }
+      },
+
+      __digitClick: function(e, digit) {
+         this._notify('onDigitClick', digit);
+      }
+
+   });
+
+   //for unit test
+   ModuleClass._private = _private;
+   //
 
    return ModuleClass;
 });
--- tests/unit/Lists/Paging/DigitButtons.test.js
+++ tests/unit/Lists/Paging/DigitButtons.test.js
@@ -5,66 +5,66 @@ define([
    'js!Controls/List/Paging/DigitButtons'
 ], function(DigitButtons){
    describe('Controls.List.Paging.DigitButtons', function () {
-      it('__initDrawedDigits 10 pages', function () {
+      it('getDrawnDigits 10 pages', function () {
          var db, digits;
          db = new DigitButtons({});
-         digits = db.__getDrawnDigits (10, 1, true);
-         assert.deepEqual([1, 2, 3, 4, '...', 10], digits, '__getDrawnDigits10 test case 1: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 1, true);
+         assert.deepEqual([1, 2, 3, 4, '...', 10], digits, 'getDrawnDigits10 test case 1: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 2, true);
-         assert.deepEqual([1, 2, 3, 4, 5, '...', 10], digits, '__getDrawnDigits10 test case 2: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 2, true);
+         assert.deepEqual([1, 2, 3, 4, 5, '...', 10], digits, 'getDrawnDigits10 test case 2: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 3, true);
-         assert.deepEqual([1, 2, 3, 4, 5, 6, '...', 10], digits, '__getDrawnDigits10 test case 3: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 3, true);
+         assert.deepEqual([1, 2, 3, 4, 5, 6, '...', 10], digits, 'getDrawnDigits10 test case 3: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 4, true);
-         assert.deepEqual([1, 2, 3, 4, 5, 6, 7, '...', 10], digits, '__getDrawnDigits10 test case 4: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 4, true);
+         assert.deepEqual([1, 2, 3, 4, 5, 6, 7, '...', 10], digits, 'getDrawnDigits10 test case 4: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 5, true);
-         assert.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], digits, '__getDrawnDigits10 test case 5: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 5, true);
+         assert.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 5: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 6, true);
-         assert.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], digits, '__getDrawnDigits10 test case 6: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 6, true);
+         assert.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 6: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 7, true);
-         assert.deepEqual([1, '...', 4, 5, 6, 7, 8, 9, 10], digits, '__getDrawnDigits10 test case 7: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 7, true);
+         assert.deepEqual([1, '...', 4, 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 7: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 8, true);
-         assert.deepEqual([1, '...', 5, 6, 7, 8, 9, 10], digits, '__getDrawnDigits10 test case 8: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 8, true);
+         assert.deepEqual([1, '...', 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 8: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 9, true);
-         assert.deepEqual([1, '...', 6, 7, 8, 9, 10], digits, '__getDrawnDigits10 test case 9: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 9, true);
+         assert.deepEqual([1, '...', 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 9: WrongResult');
 
-         digits = db.__getDrawnDigits (10, 10, true);
-         assert.deepEqual([1, '...', 7, 8, 9, 10], digits, '__getDrawnDigits10 test case 10: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (10, 10, true);
+         assert.deepEqual([1, '...', 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 10: WrongResult');
       });
 
-      it('__getDrawnDigits 15 pages', function () {
+      it('getDrawnDigits 15 pages', function () {
          var db, digits;
          db = new DigitButtons({});
 
-         digits = db.__getDrawnDigits (15, 7, true);
-         assert.deepEqual([1, '...', 4, 5, 6, 7, 8, 9, 10, '...', 15], digits, '__getDrawnDigits15 test case 7: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (15, 7, true);
+         assert.deepEqual([1, '...', 4, 5, 6, 7, 8, 9, 10, '...', 15], digits, 'getDrawnDigits15 test case 7: WrongResult');
       });
 
-      it('__getDrawnDigits 5 pages', function () {
+      it('getDrawnDigits 5 pages', function () {
          var db, digits;
          db = new DigitButtons({});
 
-         digits = db.__getDrawnDigits (5, 1, true);
-         assert.deepEqual([1, 2, 3, 4, 5], digits, '__getDrawnDigits5 test case 1: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (5, 1, true);
+         assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 1: WrongResult');
 
-         digits = db.__getDrawnDigits (5, 2, true);
-         assert.deepEqual([1, 2, 3, 4, 5], digits, '__getDrawnDigits5 test case 2: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (5, 2, true);
+         assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 2: WrongResult');
 
-         digits = db.__getDrawnDigits (5, 3, true);
-         assert.deepEqual([1, 2, 3, 4, 5], digits, '__getDrawnDigits5 test case 3: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (5, 3, true);
+         assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 3: WrongResult');
 
-         digits = db.__getDrawnDigits (5, 4, true);
-         assert.deepEqual([1, 2, 3, 4, 5], digits, '__getDrawnDigits5 test case 4: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (5, 4, true);
+         assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 4: WrongResult');
 
-         digits = db.__getDrawnDigits (5, 5, true);
-         assert.deepEqual([1, 2, 3, 4, 5], digits, '__getDrawnDigits5 test case 5: WrongResult');
+         digits = DigitButtons._private.getDrawnDigits (5, 5, true);
+         assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 5: WrongResult');
       });
    })
 });
