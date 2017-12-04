ab816952f21256cb7f43c4085c626e67b487bda1
devyatovis <is.devyatov@tensor.ru>
2017-12-04 17:18:11 +0300

    поправил обновление индексов

--- Controls/List/ListControl/ListView.js
+++ Controls/List/ListControl/ListView.js
@@ -37,7 +37,9 @@ define('js!Controls/List/ListControl/ListView', [
             if (newOptions.listModel) {
                this._listModel = newOptions.listModel;
                this._listModel.subscribe('onListChange', this._onListChangeFnc);
-               this._listModel.subscribe('onUpdateIndexes', function() {this._forceUpdate();}.bind(this));
+               this._listModel.subscribe('onUpdateIndexes', function() {
+                  this._forceUpdate();
+               }.bind(this));
             }
             this._itemTemplate = newOptions.itemTemplate || this._defaultItemTemplate;
          },
@@ -46,6 +48,9 @@ define('js!Controls/List/ListControl/ListView', [
             if (newOptions.listModel && (this._listModel != newOptions.listModel)) {
                this._listModel = newOptions.listModel;
                this._listModel.subscribe('onListChange', this._onListChangeFnc);
+               this._listModel.subscribe('onUpdateIndexes', function() {
+                  this._forceUpdate();
+               }.bind(this));
             }
             this._itemTemplate = newOptions.itemTemplate || this._defaultItemTemplate;
          },
