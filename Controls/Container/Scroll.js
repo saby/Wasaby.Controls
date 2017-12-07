fcd97e2d914c7f397a278dafbf66549a7d966207
Изыгин Н.Р <nr.izygin@corp.tensor.ru>
2017-12-07 17:34:22 +0300

    https://online.sbis.ru/opendoc.html?guid=64f95c10-a492-440b-8257-0212ced86e1c

--- Controls/Container/Scroll.js
+++ Controls/Container/Scroll.js
@@ -2,14 +2,54 @@ define('js!Controls/Container/Scroll',
    [
       'Core/Control',
       'tmpl!Controls/Container/Scroll/Scroll',
-      'css!Controls/Container/Scroll/Scroll'
+      'Core/detection',
+      'js!Controls/Container/Scrollbar/Scrollbar',
+      'css!Controls/Container/Scroll/Scroll',
    ],
-   function(Control, template) {
+   function(Control, template, detection) {
 
       'use strict';
 
       var ScrollContainer = Control.extend({
-         _template: template
+         _template: template,
+         contentHeight: 1,
+         scrollBarPosition: 0,
+         showScrollbar: false,
+         _getScrollHeight: function(){
+            return this._children.content.scrollHeight;
+         },
+         _getContainerHeight: function() {
+            return this._container.get(0).offsetHeight;
+         },
+         _toggleGradient: function() {
+            // maxScrollTop > 1 - погрешность округления на различных браузерах.
+            var maxScrollTop = this._getScrollHeight() - this._getContainerHeight();
+            this.topGradient = this._getScrollTop() > 0;
+            this.bottomGradient = maxScrollTop > 1 && this._getScrollTop() < maxScrollTop;
+         },
+         _getScrollTop: function(){
+            return this._children.content.scrollTop;
+         },
+         initScrollbar: function initScrollbar() {
+            this.showScrollbar = !(detection.isMobileIOS || detection.isMobileAndroid);
+            this.contentHeight = this._getScrollHeight();
+            this.scrollBarPosition = this._getScrollTop();
+         },
+         hideScrollbar: function hideScrollbar() {
+            this.showScrollbar = false;
+         },
+         onScroll: function onScroll() {
+            var scrollTop = this._getScrollTop();
+            if (this.showScrollbar) {
+               this.scrollBarPosition = scrollTop;
+            }
+            this._toggleGradient();
+         },
+         _afterMount: function afterMountScrollContainer() {
+            if (this.showScrollbar) {
+               this._toggleGradient();
+            }
+         }
       });
 
       return ScrollContainer;
