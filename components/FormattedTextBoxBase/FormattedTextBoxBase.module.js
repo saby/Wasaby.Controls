f5408c768544c9e2ffb59ca9f2865093e7ce848e
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2017-12-12 11:01:22 +0300

    https://online.sbis.ru/opendoc.html?guid=b7474cec-bddf-45ba-bfe8-8617d8db233e

--- components/FormattedTextBoxBase/FormattedTextBoxBase.module.js
+++ components/FormattedTextBoxBase/FormattedTextBoxBase.module.js
@@ -234,6 +234,11 @@ define(
             innerChar;
 
          this.model = [];
+
+         //Явно преобразуем маску к строке, т.к. при построении на сервере, в ней лежит объект rk.
+         strMask = '' + strMask;
+
+
          for (var i = 0; i < strMask.length; i++) {
             maskChar = strMask.charAt(i);
             //заменяем символы маски на внутренние, например HH:MM на dd:dd
