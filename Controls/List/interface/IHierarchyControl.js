82612726ea125e477a6c1e00b796e655ba57984c
kraynovdo <do.kraynov@tensor.ru>
2017-12-14 14:52:28 +0300

    https://online.sbis.ru/opendoc.html?guid=41f550d1-e989-42db-8131-b57d8ae872c7

---
+++ Controls/List/interface/IHierarchyControl.js
@@ -0,0 +1,7 @@
+
+/**
+ * @event Controls/List/interface/ITreeControl#nodeEnter (Название не согласовано) Перед проваливанием в папку
+ */
+/**
+ * @event Controls/List/interface/ITreeControl#nodeEntered (Название не согласовано) После проваливания в папку
+ */
