cf74dfb33a7ecafc1a4db7f79b0cf626e5ba1f4b
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2017-12-15 16:02:49 +0300

    Поменял styleShadow на shadowStyle.

--- components/ScrollContainer/ScrollContainer.module.js
+++ components/ScrollContainer/ScrollContainer.module.js
@@ -131,7 +131,7 @@ define('js!SBIS3.CONTROLS.ScrollContainer', [
                 * @variant none тень отсутствует.
                 * @variant standart.
                 */
-               styleShadow: 'standart',
+               shadowStyle: 'standart',
                /**
                 * @cfg {Object}
                 */
@@ -324,7 +324,7 @@ define('js!SBIS3.CONTROLS.ScrollContainer', [
          	var maxScrollTop;
 
          	// Не устанавливаем тень у контейнера с 0 высотой.
-         	if (this._getContainerHeight() && this._options.styleShadow !== 'none') {
+         	if (this._getContainerHeight() && this._options.shadowStyle !== 'none') {
                // $elem[0].scrollHeight - integer, $elem.height() - float
                maxScrollTop = this._getScrollHeight() - this._getContainerHeight();
                // maxScrollTop > 1 - погрешность округления на различных браузерах.
