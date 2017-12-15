7810a18c0bc369232ffdc84d21c0233a997e8b85
pv.stepin <pv.stepin@tensor.ru>
2017-12-15 17:22:24 +0300

    .

--- Controls/Popup/DialogHelper.js
+++ Controls/Popup/DialogHelper.js
@@ -8,40 +8,96 @@ define('js!Controls/Popup/DialogHelper',
    function (Control, merge, template) {
       'use strict';
 
-      return Control.extend({
-         _controlName: 'Controls/Popup/DialogHelper',
-         _template: template,
+      /**
+       *
+       * Хелпер открытия диалоговых окон
+       * @class Controls/Popup/DialogHelper
+       * @extends Core/Control
+       * @control
+       * @public
+       * @category Popup
+       * @author Степин Павел Владимирович
+       */
 
-         _afterMount: function () {
-            var self = this;
-            //TODO Пока через subscribe, уточнить у Лощинина
-            self._children.opener.subscribe('onResult', function (e, res) {
-               self._notify('onResult', res);
-            });
-         },
+      /**
+       * @function Controls/Popup/DialogHelper#confirm
+       * Открыть диалоговое окно подтверждения
+       */
 
-         _openDialog: function(cfg, status){
+      /**
+       * @function Controls/Popup/DialogHelper#info
+       * Открыть окно со статусом "информация"
+       */
+
+      /**
+       * @function Controls/Popup/DialogHelper#error
+       * Открыть окно со статусом "ошибка"
+       */
+
+      /**
+       * @function Controls/Popup/DialogHelper#success
+       * Открыть окно со статусом "успех"
+       */
+
+      var _private = {
+         openDialog: function(cfg, status, positiveHandler, negativeHandler, cancelHandler){
             this._children.opener.open({
                componentOptions: merge(merge({
                   status: status
                }, this._options.dialogOptions), cfg)
             });
+
+            this._positiveHandler = positiveHandler;
+            this._negativeHandler = negativeHandler;
+            this._cancelHandler = cancelHandler;
          },
 
-         confirm: function(cfg){
-            this._openDialog(cfg, 'confirm');
+         resultHandler: function(res){
+            var handler;
+
+            switch(res){
+               case true: handler = this._positiveHandler; break;
+               case false: handler = this._negativeHandler; break;
+               default: handler = this._cancelHandler; break;
+            }
+
+            if(handler && typeof handler === 'function'){
+               handler(res);
+            }
+            this._notify('onResult', res);
+         }
+      };
+
+      return Control.extend({
+         _controlName: 'Controls/Popup/DialogHelper',
+         _template: template,
+
+         _positiveHandler: null,
+         _negativeHandler: null,
+         _cancelHandler: null,
+
+         _afterMount: function () {
+            var self = this;
+
+            self._children.opener.subscribe('onResult', function (e, res) {
+               _private.resultHandler.call(self, res);
+            });
          },
 
-         info: function(cfg){
-            this._openDialog(cfg, 'default');
+         confirm: function(cfg, positiveHandler, negativeHandler, cancelHandler){
+            _private.openDialog.call(this, cfg, 'confirm', positiveHandler, negativeHandler, cancelHandler);
          },
 
-         error: function(cfg){
-            this._openDialog(cfg, 'error');
+         info: function(cfg, handler){
+            _private.openDialog.call(this, cfg, 'default', handler);
          },
 
-         success: function(cfg){
-            this._openDialog(cfg, 'success');
+         error: function(cfg, handler){
+            _private.openDialog.call(this, cfg, 'error', handler);
+         },
+
+         success: function(cfg, handler){
+            _private.openDialog.call(this, cfg, 'success', handler);
          }
 
       });
