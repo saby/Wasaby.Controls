852638d768e60dfccebc8ad8f46bbff93b05471b
vs.romanov <vs.romanov@tensor.ru>
2017-12-20 16:21:08 +0300

    Упрощение стилей Кнопки-ссылки https://online.sbis.ru/opendoc.html?guid=03ffff15-153e-4757-bff5-6c057b06329e

--- components/Utils/LinkUtil.js
+++ components/Utils/LinkUtil.js
@@ -5,11 +5,11 @@ define('SBIS3.CONTROLS/Utils/LinkUtil', [],
      */
     function () /** @lends SBIS3.CONTROLS/Utils/LinkUtil.prototype */{
         return {
-            preparedClassFromOptions: function (opts, attrToMerge) {
+            getStyleByConfig: function(opts, attrToMerge){
                 var classes = ['mainLink', 'mainLink__2', 'mainLink__3', 'additionalLink', 'additionalLink__2', 'additionalLink__3', 'additionalLink__4', 'additionalLink__5'],
                     className = (attrToMerge && attrToMerge.class) || (opts.element && opts.element.className) || opts.className || '',
                     color = 'mainLink',
-                    colorClass = ' controls-Link_';
+                    state = 'mainLink';
 
                 if (className) {
                     classes.forEach(function (type) {
@@ -19,16 +19,16 @@ define('SBIS3.CONTROLS/Utils/LinkUtil', [],
                     });
                 }
                 switch(color){
-                    case 'mainLink': colorClass += 'main'; break;
-                    case 'mainLink__2': colorClass += 'main-2'; break;
-                    case 'mainLink__3': colorClass += 'main-3'; break;
-                    case 'additionalLink': colorClass += 'additional'; break;
-                    case 'additionalLink__2': colorClass += 'additional-2'; break;
-                    case 'additionalLink__3': colorClass += 'additional-3'; break;
-                    case 'additionalLink__4': colorClass += 'additional-4'; break;
-                    case 'additionalLink__5': colorClass += 'additional-5'; break;
+                    case 'mainLink': state = 'main'; break;
+                    case 'mainLink__2': state = 'main-2'; break;
+                    case 'mainLink__3': state = 'main-3'; break;
+                    case 'additionalLink': state = 'additional'; break;
+                    case 'additionalLink__2': state = 'additional-2'; break;
+                    case 'additionalLink__3': state = 'additional-3'; break;
+                    case 'additionalLink__4': state = 'additional-4'; break;
+                    case 'additionalLink__5': state = 'additional-5'; break;
                 }
-                opts.cssClassName += colorClass;
+                return state;
             }
         };
     });
