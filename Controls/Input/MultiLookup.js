1c69a39f6cb2438a87abee8256358ae296948172
kraynovdo <do.kraynov@tensor.ru>
2018-01-09 15:58:53 +0300

    https://online.sbis.ru/opendoc.html?guid=ec0457d3-4a8c-40e2-8d57-e5dc7b95dc2f

---
+++ Controls/Input/MultiLookup.js
@@ -0,0 +1,25 @@
+/**
+ * Created by kraynovdo on 09.01.2018.
+ */
+define('js!Controls/Input/Lookup', [
+], function() {
+
+   /**
+    * Поле выбора из справочника с множественным выбором
+    * @class Controls/Input/Lookup
+    * @extends Controls/Control
+    * @mixes Controls/Input/interface/ISearch
+    * @mixes Controls/interface/ISource
+    * @mixes Controls/interface/IFilter
+    * @mixes Controls/Input/interface/ISuggest
+    * @mixes Controls/Input/interface/ILookup
+    * @mixes Controls/interface/INavigation
+    * @mixes Controls/interface/IMultiSelectable
+    * @mixes Controls/Input/interface/IInputPlaceholder
+    * @mixes Controls/Input/interface/IInputText
+    * @mixes Controls/Input/interface/IValidation
+    * @control
+    * @public
+    * @category Input
+    */
+});
---
+++ Controls/Input/interface/ILookup.js
@@ -0,0 +1,18 @@
+/**
+ * Created by kraynovdo on 09.01.2018.
+ */
+define('js!Controls/Input/interface/ILookup', [
+], function() {
+
+   /**
+    * Интерфейс поиска внутри поля
+    *
+    * @mixin Controls/Input/interface/ILookup
+    * @public
+    */
+
+   /**
+    * @name Controls/Input/interface/ILookup#lookupTemplate
+    * @cfg {Function} Шаблон панели выбора
+    */
+});
