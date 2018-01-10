e11d92c29d416e6c197d2c3164787099ccd04904
ee.zolotova <ee.zolotova@tensor.ru>
2018-01-10 16:07:42 +0300

    https://online.sbis.ru/opendoc.html?guid=e50e2371-ea6d-47a9-b7ef-dc87f4577898

---
+++ tests/unit/Input/Search.test.js
@@ -0,0 +1,94 @@
+define(
+   [
+      'Core/Control',
+      'js!Controls/Input/Search',
+      'Core/vdom/Synchronizer/resources/SyntheticEvent'
+   ],
+   function (Control, Search, SyntheticEvent) {
+      'use strict'
+
+      var
+         isSearched = false,
+         isReseted = false,
+         search;
+
+      describe('Controls/Input/Search', function () {
+
+         before(function () {
+            if (typeof $ === 'undefined') {
+               this.skip();
+            }
+            var container = $('<div></div>');
+            $('#mocha').append(container);
+
+            if (typeof $ !== 'undefined') {
+               container = $('<div></div>');
+               $('#mocha').append(container);
+            }
+            search = Control.createControl(Search, {element: container}, container);
+            search.subscribe('search', function () {
+               isSearched = true;
+            });
+            search.subscribe('reset', function () {
+               isReseted = true;
+            });
+         });
+
+         beforeEach(function () {
+            if(typeof $==='undefined') {
+               this.skip();
+            }
+         });
+
+         after(function () {
+            if (typeof $ !== 'undefined') {
+               search.destroy();
+            }
+         });
+
+         describe('search', function () {
+            it('1 or 2 character', function () {
+               isSearched = false;
+               search._changeValueHandler(new SyntheticEvent('onchange', {
+                  target: {}
+               }), 'n');
+               assert.isFalse(isSearched);
+
+               search._changeValueHandler(new SyntheticEvent('onchange', {
+                  target: {}
+               }), 'no');
+               assert.isFalse(isSearched);
+            });
+
+            it('3 character', function () {
+               isSearched = false;
+               search._changeValueHandler(new SyntheticEvent('onchange', {
+                  target: {}
+               }), 'noo');
+               assert.isTrue(isSearched);
+            });
+
+            it('2 character', function () {
+               isSearched = false;
+               search._changeValueHandler(new SyntheticEvent('onchange', {
+                  target: {}
+               }), 'no');
+               assert.isFalse(isSearched);
+            });
+
+            it('Click on reset', function () {
+               isSearched = false;
+               search._onResetClick();
+               assert.isTrue(isReseted);
+               assert.isFalse(isSearched);
+            });
+
+            it('Click on search', function () {
+               isSearched = false;
+               search._onSearchClick();
+               assert.isTrue(isSearched);
+            });
+
+         });
+      });
+   });
