dfd5f6bb4312095ab1715f9f16c568040db18981
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-01-11 15:51:07 +0300

    https://online.sbis.ru/opendoc.html?guid=758a04a2-866c-4926-8d28-d2489b5e71e9

--- components/Button/ListAddButton.js
+++ components/Button/ListAddButton.js
@@ -7,16 +7,15 @@ define('SBIS3.CONTROLS/Button/ListAddButton', [
    var ListAddButton = WSButton.extend([], /** @lends SBIS3.CONTROLS/Button/ToggleButton.prototype */ {
       $protected: {
          _options: {
-            icon: 'icon-16 icon-Add'
+            //icon: 'icon-16 icon-Add'
          }
       },
 
-      _modifyOptions: function(){
-         var cfg = ListAddButton.superclass._modifyOptions.apply(this, arguments);
-         //cfg.icon += 'icon-16 icon-Add';
+      _modifyOptions: function(cfg){
+         cfg.icon += 'icon-16 icon-Add';
          cfg.className += ' controls-ListAddButton ' + (cfg.enabled ? 'controls-ListAddButton__enable' : 'controls-ListAddButton__disable');
          cfg._textClass += ' controls-ListAddButtonText';
-         return cfg;
+         return ListAddButton.superclass._modifyOptions.call(this, cfg);
       }
    });
 
