a8710deae468bb6d0c32d3b5bb86d1f57a4e0226
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-01-11 13:30:15 +0300

    https://online.sbis.ru/opendoc.html?guid=758a04a2-866c-4926-8d28-d2489b5e71e9

---
+++ components/Button/ListAddButton.js
@@ -0,0 +1,24 @@
+
+define('SBIS3.CONTROLS/Button/ListAddButton', [
+   'js!WSControls/Buttons/Button',
+   'css!SBIS3.CONTROLS/Button/ListAddButton/ListAddButton'
+], function(WSButton) {
+
+   var ListAddButton = WSButton.extend([], /** @lends SBIS3.CONTROLS/Button/ToggleButton.prototype */ {
+      $protected: {
+         _options: {
+            icon: 'icon-16 icon-Add'
+         }
+      },
+
+      _modifyOptions: function(){
+         var cfg = ListAddButton.superclass._modifyOptions.apply(this, arguments);
+         cfg.className += ' controls-ListAddButton ' + (cfg.enabled ? 'controls-ListAddButton__enable' : 'controls-ListAddButton__disable');
+         cfg._textClass += ' controls-ListAddButtonText';
+         return cfg;
+      }
+   });
+
+   return ListAddButton;
+
+});
