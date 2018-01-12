249d152eba9d74b4d74a898103880725366dcaa4
mitin <mitin@tensor.ru>
2018-01-12 16:59:30 +0300

    правка конфига и скрипта удаления лишних файлов

--- tests/delete_test_files.py
+++ tests/delete_test_files.py
@@ -4,12 +4,10 @@ import shutil
 
 # удаление лишних папок со страницами тестов
 root = os.getcwd()
-if os.path.exists('reg'):
-    shutil.rmtree('reg')
-if os.path.exists('unit'):
-    shutil.rmtree('unit')
-if os.path.exists(r'stand/intest/regression'):
-    shutil.rmtree(r'stand/intest/regression')
+dirs_for_del = ['reg', 'unit', r'stand/intest/regression']
+for tmp_dir in dirs_for_del:
+    if os.path.exists(tmp_dir):
+        shutil.rmtree(tmp_dir)
 
 os.chdir(r'stand/intest/integration')
 root_1 = os.getcwd()
@@ -17,15 +15,3 @@ exclude = ['_VDOM']
 tmp_list_dirs = [d for d in os.listdir(root_1) if os.path.isdir(os.path.join(root_1, d)) and d not in exclude]
 for tmp_dir in tmp_list_dirs:
     shutil.rmtree(tmp_dir)
-
-# удаление лишних файлов тестов
-os.chdir(root)
-os.chdir('int')
-root_1 = os.getcwd()
-exclude = ['start_tests.py', 'test_todomvc.py', 'common.py',
-           'todomvc.py', 'tabbuttons.py', 'browser.py', 'index.py', 'wisbis.py']
-for top, folders, files in os.walk(root_1):
-    if top == root_1 or top == os.path.join(root_1, 'pages'):
-        for file in files:
-            if file not in exclude:
-                os.remove(os.path.join(top, file))
