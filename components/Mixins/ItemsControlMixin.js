2c2b134e1a9a3566eb9d19d9efc165210d9cb97e
as.avramenko <as.avramenko@tensor.ru>
2018-01-15 10:45:40 +0300

    Поправил коммент

--- components/Mixins/ItemsControlMixin.js
+++ components/Mixins/ItemsControlMixin.js
@@ -1098,7 +1098,7 @@ define('SBIS3.CONTROLS/Mixins/ItemsControlMixin', [
             projection = this._getItemsProjection(),
             data;
 
-         //TODO в 3.7.5 избавиться от проверки на _path
+         //todo В VDOM логика будет правильной и сделаем без подобных проверок.
          if (targetElement.length) {
             if (targetElement.hasClass('controls-HierarchyDataGridView__path')) {
                this._redrawHierarchyPathItem(item);
