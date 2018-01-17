19e44cff89d2330d8367feb88da6f7ed2f29d85f
va.spirin <va.spirin@tensor.ru>
2018-01-17 13:42:02 +0700

    RichEditor: Вывернуто условие
    32963 https://online.sbis.ru/opendoc.html?guid=853f36e3-4a3a-4e12-988c-53af66d78094

--- components/RichEditor/Components/RichTextArea/resources/tinymce/tinymce.js
+++ components/RichEditor/Components/RichTextArea/resources/tinymce/tinymce.js
@@ -33783,7 +33783,7 @@
                   var text = sel.getContent({format:'text'});
                   // В хроме и эксплорере нужно заменить переводы строк на windows-ные
                   // 32963 https://online.sbis.ru/opendoc.html?guid=853f36e3-4a3a-4e12-988c-53af66d78094
-                  e.clipboardData.setData('text/plain', !Env.gecko && navigator.userAgent.search(/\bwindows\b/i) !== -1 ? text.replace(/\n/gi, '\r\n') : text);
+                  e.clipboardData.setData('text/plain', (Env.webkit || Env.ie) && navigator.userAgent.search(/\bwindows\b/i) !== -1 ? text.replace(/\n/gi, '\r\n') : text);
 
                   // Needed delay for https://code.google.com/p/chromium/issues/detail?id=363288#c3
                   // Nested delete/forwardDelete not allowed on execCommand("cut")
