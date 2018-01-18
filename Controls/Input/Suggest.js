1afed23e4cd0a4fa6b15cf45fa435098e830d3e7
am.gerasimov <am.gerasimov@tensor.ru>
2018-01-18 11:38:36 +0300

    Возвращаю правки, который пропали при удалении папки input
    https://online.sbis.ru/opendoc.html?guid=ef0c70e8-4608-4d6e-992b-c3fc9a00d602

--- Controls/Input/Suggest.js
+++ Controls/Input/Suggest.js
@@ -4,12 +4,13 @@ define('js!Controls/Input/Suggest',
       'tmpl!Controls/Input/Suggest/Suggest',
       'js!WS.Data/Type/descriptor',
       'js!Controls/Input/resources/SuggestController',
+      'Controls/Input/resources/InputRender/SimpleViewModel',
       'js!Controls/Popup/Opener/Sticky',
       'js!Controls/Popup/Opener/Stack',
       'css!Controls/Input/Suggest/Suggest'
    ],
-   function(Control, template, types, SuggestController) {
-   
+   function(Control, template, types, SuggestController, SimpleViewModel) {
+      
       /**
        * Поле ввода с автодополнением
        * @class Controls/Input/Suggest
@@ -23,21 +24,34 @@ define('js!Controls/Input/Suggest',
        * @public
        * @category Input
        */
-   
+      
       'use strict';
       
+      var _private = {
+         onSearchStart: function(self) {
+            self._searching = true;
+            self._forceUpdate();
+         },
+         
+         onSearchEnd: function(self) {
+            self._searching = false;
+            self._forceUpdate();
+         }
+      };
+      
       var Suggest = Control.extend({
-   
+         
          _template: template,
          _controlName: 'Controls/Input/Suggest',
-   
+         
          // <editor-fold desc="LifeCycle">
          
          constructor: function(options) {
             Suggest.superclass.constructor.call(this, options);
             this._selectHandler = this._selectHandler.bind(this);
+            this._simpleViewModel = new SimpleViewModel();
          },
-   
+         
          _afterMount: function() {
             this._suggestController = new SuggestController({
                suggestTemplate: this._options.suggestTemplate,
@@ -50,7 +64,9 @@ define('js!Controls/Input/Suggest',
                searchParam: this._options.searchParam,
                navigation: this._options.navigation,
                textComponent: this._children.suggestText,
-               selectCallback: this._selectHandler
+               selectCallback: this._selectHandler,
+               searchStartCallback: _private.onSearchStart.bind(this, this),
+               searchEndCallback: _private.onSearchEnd.bind(this, this)
             });
          },
          
@@ -63,28 +79,32 @@ define('js!Controls/Input/Suggest',
          },
          
          // </editor-fold>
-   
-   
+         
+         
          // <editor-fold desc="handlers">
          
          _changeValueHandler: function(event, value) {
             this._suggestController.setValue(value);
          },
-   
+         
          _selectHandler: function(item) {
             this._notify('select', item);
             this._notify('valueChanged', item.get(this._options.displayProperty));
          },
-   
+         
+         _clearClick: function() {
+            this._notify('valueChanged', '');
+         },
+         
          _keyDownHandler: function(event) {
             this._suggestController.keyDown(event);
          }
-   
+         
          // </editor-fold>
          
       });
-   
-   
+      
+      
       // <editor-fold desc="OptionsDesc">
       Suggest.getOptionTypes = function() {
          return {
@@ -95,7 +115,7 @@ define('js!Controls/Input/Suggest',
             displayProperty: types(String).required()
          };
       };
-   
+      
       Suggest.getDefaultOptions = function() {
          return {
             searchDelay: 500,
