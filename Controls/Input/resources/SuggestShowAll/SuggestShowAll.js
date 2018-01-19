87ed7ac3f4a0bd073ce5dd2cfc71cdd273120761
kraynovdo <do.kraynov@tensor.ru>
2018-01-19 11:36:53 +0300

    https://online.sbis.ru/opendoc.html?guid=dc0fd3e0-522b-4740-8203-5fc3f37d73f2

--- Controls/Input/resources/SuggestShowAll/SuggestShowAll.js
+++ Controls/Input/resources/SuggestShowAll/SuggestShowAll.js
@@ -16,8 +16,12 @@ define('js!Controls/Input/resources/SuggestShowAll/SuggestShowAll',
          constructor: function(options) {
             SuggestShowAll.superclass.constructor.call(this, options);
             //TODO переписать, как список переведут на актуальное апи навигации
+            //так задаем дефолтные настройки для навигации в списке
             if (options.navigation && options.navigation.sourceConfig) {
                options.navigation.sourceConfig.pageSize = 20;
+               options.navigation.view = 'infinity';
+               options.navigation.viewConfig = options.navigation.viewConfig || {};
+               options.navigation.viewConfig.pagingMode = true;
             }
          },
          
