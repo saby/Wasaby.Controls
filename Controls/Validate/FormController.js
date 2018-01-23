560684ae5e2a526a6612053141586ae0ca57a7f4
ma.baranov1 <ma.baranov1@tensor.ru>
2018-01-23 11:54:41 +0300

    Добавил валидацию по ctrl + enter ___ https://online.sbis.ru/opendoc.html?guid=df5e7ae9-213e-425f-9c7b-d38f9041c180

--- Controls/Validate/FormController.js
+++ Controls/Validate/FormController.js
@@ -31,8 +31,12 @@ define('Controls/Validate/FormController',
             e.stopPropagation();
          },
          onKeyPressed: function(e) {
+            var
+               self = this;
             if(!(e.nativeEvent.altKey || e.nativeEvent.shiftKey) && e.nativeEvent.ctrlKey) { // Ctrl+Enter, Cmd+Enter
-               this._notify('onSubmit');
+               this.validate().addCallback(function(results) {
+                  self._notify('onSubmit', [results]);
+               });
             }
          },
          validate: function() {
