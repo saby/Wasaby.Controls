4bafac2f21dc9e9428b5824391010894d384dcb3
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-01-23 15:33:32 +0300

    https://online.sbis.ru/opendoc.html?guid=9e27b4dc-8cea-4d79-923d-8f5d0c81cecd

---
+++ Controls/List/AddButton.js
@@ -0,0 +1,19 @@
+define('js!Controls/List/AddButton', [
+   'Core/Control',
+   'tmpl!Controls/List/AddButton/AddButton',
+   'WS.Data/Type/descriptor',
+   'css!Controls/List/AddButton/AddButton'
+], function (Control, template, types) {
+
+   var AddButton = Control.extend({
+      _template: template
+   });
+
+   AddButton.getOptionTypes = function getOptionTypes() {
+      return {
+         caption: types(String)
+      };
+   };
+
+   return AddButton;
+});
