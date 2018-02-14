f90881f5f5b99098db9134a6452dccc9d9053109
mitin <mitin@tensor.ru>
2018-02-14 17:02:42 +0300

    добавление тестов для запуска в сборке билдера

--- tests/delete_test_files.py
+++ tests/delete_test_files.py
@@ -11,7 +11,21 @@ for tmp_dir in dirs_for_del:
 
 os.chdir(r'stand/intest/integration')
 root_1 = os.getcwd()
-exclude = ['_VDOM']
+exclude = ['_VDOM', '_Grids']
+tmp_list_dirs = [d for d in os.listdir(root_1) if os.path.isdir(os.path.join(root_1, d)) and d not in exclude]
+for tmp_dir in tmp_list_dirs:
+    shutil.rmtree(tmp_dir)
+
+os.chdir(r'_Grids')
+root_1 = os.getcwd()
+exclude = ['IntTreeDataGrid']
+tmp_list_dirs = [d for d in os.listdir(root_1) if os.path.isdir(os.path.join(root_1, d)) and d not in exclude]
+for tmp_dir in tmp_list_dirs:
+    shutil.rmtree(tmp_dir)
+
+os.chdir(r'IntTreeDataGrid')
+root_1 = os.getcwd()
+exclude = ['TDG4']
 tmp_list_dirs = [d for d in os.listdir(root_1) if os.path.isdir(os.path.join(root_1, d)) and d not in exclude]
 for tmp_dir in tmp_list_dirs:
     shutil.rmtree(tmp_dir)
