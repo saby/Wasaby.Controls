303b4327db72cc2c5a6387fdf2f05b024ea2568e
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-02-19 17:17:10 +0300

    https://online.sbis.ru/opendoc.html?guid=8745d030-ada8-4acf-9a38-4f2cc9bdcf54

--- Controls/Button.js
+++ Controls/Button.js
@@ -37,15 +37,24 @@ define('Controls/Button', [
        _controlName: 'Controls/Button',
        _template: template,
 
+       constructor: function (options) {
+          Button.superclass.constructor.apply(this, arguments);
+          this._style = options.style + (options.enabled ? '':'-disabled');
+          this._type = options.style.split('-')[0] + (options.enabled ? '':'-disabled');
+          this._typeWithSize = options.style.split('-')[0] + '__size-' + options.size;
+       },
+
+       _beforeUpdate: function (newOptions) {
+          this._style = newOptions.style + (newOptions.enabled ? '':'-disabled');
+          this._type = newOptions.style.split('-')[0] + (newOptions.enabled ? '':'-disabled');
+          this._typeWithSize = newOptions.style.split('-')[0] + '__size-' + newOptions.size;
+       },
+
        _clickHandler: function (e) {
            if(!this.isEnabled()){
                e.stopPropagation();
            }
-       },
-
-      buttonType: function (style) {
-         return style.split('-')[0];
-      }
+       }
    });
 
    Button.getDefaultOptions = function() {
