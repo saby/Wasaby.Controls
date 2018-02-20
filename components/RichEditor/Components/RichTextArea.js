88acec271bac193108dd18b78020ad0cfabc0656
va.spirin <va.spirin@tensor.ru>
2018-02-20 14:48:26 +0700

    RichEditor: Для MSIE принудительно смещаем курсор ввода
    1174853380 https://online.sbis.ru/opendoc.html?guid=77405679-2b2b-42d3-8bc0-d2eee745ea23

--- components/RichEditor/Components/RichTextArea.js
+++ components/RichEditor/Components/RichTextArea.js
@@ -1105,7 +1105,9 @@ define('SBIS3.CONTROLS/RichEditor/Components/RichTextArea',
                                     if (selection.getContent() === '' || (fre._isOnlyTextSelected() && cConstants.browser.firefox)) {
                                        var linkText = selection.getContent({format:'text'}) || href;
                                        var linkHtml = dom.createHTML('a', linkAttrs, dom.encode(linkText));
-                                       editor.insertContent(linkHtml);
+                                       // Для MSIE принудительно смещаем курсор ввода после вставленной ссылки
+                                       // 1174853380 https://online.sbis.ru/opendoc.html?guid=77405679-2b2b-42d3-8bc0-d2eee745ea23
+                                       editor.insertContent(cConstants.browser.isIE ? linkHtml + '&#65279;&#8203;' : linkHtml);
                                     } else {
                                        editor.execCommand('mceInsertLink', false, linkAttrs);
                                        if (cConstants.browser.firefox) {
