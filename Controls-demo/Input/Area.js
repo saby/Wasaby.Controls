51e738d05b9d5a77189af1bb99da9f9650f86144
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-02-27 20:14:05 +0300

    https://online.sbis.ru/opendoc.html?guid=20847eb8-e3e3-4fe5-84fe-fb535c453a74

---
+++ Controls-demo/Input/Area.js
@@ -0,0 +1,32 @@
+define('Controls-demo/Input/Area', [
+   'Core/Control',
+   'tmpl!Controls-demo/Input/Area',
+   'WS.Data/Source/Memory'
+], function(Control, template) {
+
+   'use strict';
+
+   var VdomDemoArea = Control.extend({
+      _template: template,
+      _text1: '',
+      _placeholder: '',
+      _constraint: '',
+      _validationErrors: '',
+      _validationErrorsValue: false,
+      _trim: false,
+      _maxLength: '',
+      _minLines: '',
+      _maxLines: '',
+      _enabled: true,
+
+      valueChangedHandler: function () {
+         if (this._validationErrorsValue){
+            this._validationErrors = ['Some error'];
+         } else{
+            this._validationErrors = null;
+         }
+      }
+   });
+
+   return VdomDemoArea;
+});
