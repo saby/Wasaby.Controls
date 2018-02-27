f6c4a0f0fd96a497ce21890b9ee0c8887c8eab01
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-02-27 21:28:57 +0300

    https://online.sbis.ru/opendoc.html?guid=20847eb8-e3e3-4fe5-84fe-fb535c453a74

--- Controls-demo/Input/VDomInputs.js
+++ Controls-demo/Input/VDomInputs.js
@@ -1,9 +1,6 @@
 define('Controls-demo/Input/VDomInputs', [
    'Core/Control',
    'tmpl!Controls-demo/Input/VDomInputs',
-   'Controls-demo/Input/Number',
-   'Controls-demo/Input/Text',
-   'Controls-demo/Input/Area',
    'WS.Data/Source/Memory'
 ], function(Control, template) {
 
