c6055edf871d9b5497820225ee37a9ca89bdc0fa
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-02-27 21:23:28 +0300

    https://online.sbis.ru/opendoc.html?guid=20847eb8-e3e3-4fe5-84fe-fb535c453a74

--- Controls-demo/Input/VDomInputs.js
+++ Controls-demo/Input/VDomInputs.js
@@ -1,38 +1,17 @@
 define('Controls-demo/Input/VDomInputs', [
    'Core/Control',
    'tmpl!Controls-demo/Input/VDomInputs',
+   'Controls-demo/Input/Number',
+   'Controls-demo/Input/Text',
+   'Controls-demo/Input/Area',
    'WS.Data/Source/Memory'
 ], function(Control, template) {
 
    'use strict';
 
    var VDomListView = Control.extend({
-      _template: template,
+      _template: template
 
-      //стейты, на которые биндим значения инпутов
-      _text1: '',
-      _text2: '',
-      _text3: '',
-      _text4: '',
-
-      _area1: '',
-      _area2: '',
-      _area3: '',
-      _area4: '',
-      _area5: '',
-      _area6: '',
-      _area7: '123',
-      _area8: '456',
-
-      _number1: '',
-      _number2: '',
-      _number3: '',
-      _number4: '',
-      _number5: '',
-      _number6: '789',
-
-      _pass1: '',
-      _pass2: 'test'
    });
 
    return VDomListView;
