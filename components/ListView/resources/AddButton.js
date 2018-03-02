d9766e0011b93d4e7e58b338a9ac746b463e5f10
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-03-02 16:34:05 +0300

    https://online.sbis.ru/opendoc.html?guid=49d9ea58-677b-4ca5-a82f-6c165d95f6b9

--- components/ListView/resources/AddButton.js
+++ components/ListView/resources/AddButton.js
@@ -16,6 +16,14 @@ define('SBIS3.CONTROLS/ListView/resources/AddButton', [
          cfg.className += 'controls-AddButton-wrapper controls-AddButton ' + (cfg.enabled ? 'controls-AddButton__enable' : 'controls-AddButton__disable');
          cfg._textClass += ' controls-AddButtonText';
          return AddButton.superclass._modifyOptions.call(this, cfg);
+      },
+
+      _toggleState: function() {
+         var  container = this._container;
+         container[0].className = container[0].className.replace(/(^|\s)controls-Link_state-\S+/g, '');
+
+         container.addClass('controls-Link_state-' + (this._options.enabled ? this._options.style : 'disabled'));
+         Link.superclass._toggleState.apply(this, arguments);
       }
    });
 
