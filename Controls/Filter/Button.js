e31d8986a965693e6af87b66b44c9b8f6f046b52
am.gerasimov <am.gerasimov@tensor.ru>
2018-03-07 15:07:37 +0300

    Выношу код в compatible слой

--- Controls/Filter/Button.js
+++ Controls/Filter/Button.js
@@ -21,44 +21,17 @@ define('Controls/Filter/Button',
       'use strict';
    
       var _private = {
-         
-         notifyOnFilterChange: function(self, filter) {
-            self._notify('filterChanged', filter, {bubbling: true});
-         },
-         
-         oldPanelChangeHandler: function(self, stringTransformer) {
-            var panelOpener = self._oldPanelOpener;
-            self._text = stringTransformer.string(panelOpener.getFilterStructure(), 'itemTemplate');
-            _private.notifyOnFilterChange(self, panelOpener.getFilter());
-            self._forceUpdate();
-         },
-         
-         getOldPanelConfig: function(self) {
-           return {
-              element: self._container.querySelector('.controls-FilterButton__oldTemplate'),
-              template: self._options.template,
-              filterStructure: self._options.items,
-              filterAlign: self._options.filterAlign === 'left' ? 'right' : 'left'
-           };
-         },
-         
-         getOldPanelOpener: function(self) {
-            return moduleStubs.require(['Controls/Filter/Button/OldPanelOpener', 'SBIS3.CONTROLS/Filter/Button/Utils/FilterToStringUtil', 'Controls/Filter/Button/converterFilterStructure']).addCallback(function(result) {
+         getFilterButtonCompatible: function(self) {
+            return moduleStubs.require('Controls/Filter/Button/_FilterCompatible').addCallback(function(_FilterCompatible) {
                if (!self._oldPanelOpener) {
-                  self._oldPanelOpener = new result[0](_private.getOldPanelConfig(self));
-                  self._oldPanelOpener.subscribe('onApplyFilter', function() {
-                     _private.oldPanelChangeHandler(self, result[1]);
-                  });
-                  self._oldPanelOpener.subscribe('onResetFilter', function(event, internal) {
-                     if (!internal) {
-                        _private.oldPanelChangeHandler(self, result[1]);
-                     }
+                  self._oldPanelOpener = new _FilterCompatible[0]({
+                     filterButton: self,
+                     filterButtonOptions: self._options
                   });
                }
                return self._oldPanelOpener;
             });
          }
-      
       };
       
       var FilterButton = Control.extend({
@@ -72,17 +45,17 @@ define('Controls/Filter/Button',
          },
    
          _clearClick: function() {
-            _private.getOldPanelOpener(this).addCallback(function(panelOpener) {
-               panelOpener.sendCommand('reset-filter');
+            _private.getFilterButtonCompatible(this).addCallback(function(panelOpener) {
+               panelOpener.clearFilter();
             });
          },
    
          _openFilterPanel: function() {
             if (this.isEnabled()) {
                /* if template - show old component */
-               if (this._options.template) {
-                  _private.getOldPanelOpener(this).addCallback(function (panelOpener) {
-                     panelOpener.showPicker();
+               if (this._options.filterTemplate) {
+                  _private.getFilterButtonCompatible(this).addCallback(function (panelOpener) {
+                     panelOpener.showFilterPanel();
                   });
                } else {
                   //...soon
