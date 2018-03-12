c345e7eb54a50e211e33878e3a0d0b9d1a988150
ganshinyao <yo.ganshin@tensor.ru>
2018-03-12 18:16:13 +0300

    https://online.sbis.ru/opendoc.html?guid=66f323a2-0e30-4d6b-9b94-ef8289fc20f9

--- components/Mixins/DragAndDropMixin/DragObject.js
+++ components/Mixins/DragAndDropMixin/DragObject.js
@@ -213,6 +213,7 @@ define('SBIS3.CONTROLS/Mixins/DragAndDropMixin/DragObject', [
                //на ipad'e нет метода elementsFromPoint
                return $(document.elementFromPoint(this._jsEvent.pageX, this._jsEvent.pageY));
             } else if ($(this._jsEvent.target).closest('.controls-editInPlace').length > 0) {
+               //у редактирования по месту нет родителя поэтому его надо игнорить, что бы драгндроп не затухал
                return $(this._jsEvent.target).closest('.controls-editInPlace').parent();
             }
             return $(this._jsEvent.target);
