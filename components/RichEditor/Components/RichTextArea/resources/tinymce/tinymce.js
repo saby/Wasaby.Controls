816918b4481774a7e5a6c80651e0743e864c2a20
va.spirin <va.spirin@tensor.ru>
2018-03-13 17:12:57 +0700

    Revert "RichEditor: Добавлена проверка"
    This reverts commit 02a58d9324813b267dc6fa72a0ecf8864cc0c207.

--- components/RichEditor/Components/RichTextArea/resources/tinymce/tinymce.js
+++ components/RichEditor/Components/RichTextArea/resources/tinymce/tinymce.js
@@ -54906,9 +54906,7 @@ tinymce.PluginManager.add('noneditable', function(editor) {
             Delay.setEditorTimeout(editor, function() {
                var match;
 
-               // Иногда попадаем сюда при неготовности editor.selection
-               // 1174986716 https://online.sbis.ru/opendoc.html?guid=456e3c31-5511-43c5-ad71-77e344a1356e
-               match = editor.selection ? findFrontMostMatch(editor.selection.getNode()) : null;
+               match = findFrontMostMatch(editor.selection.getNode());
                if (match) {
                   hideAllContextToolbars();
                   showContextToolbar(match);
