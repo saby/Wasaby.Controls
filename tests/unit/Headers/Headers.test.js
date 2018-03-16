7e37d7f38678ad93399f62abb1ea40ea173ac8dc
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-03-16 18:43:15 +0300

    https://online.sbis.ru/opendoc.html?guid=72bd3da3-9a1a-43c7-9faf-fa168f7f135e

---
+++ tests/unit/Headers/Headers.test.js
@@ -0,0 +1,54 @@
+define(['Controls/Header'], function (Header) {
+   'use strict';
+   var header, successClick, successCountClick;
+   describe('Controls.Header', function () {
+      beforeEach(function(){
+         header = new Header({
+            captions: 'capt1',
+            style: 'default'
+         });
+         successClick = false;
+         successCountClick = false;
+         //subscribe на vdom компонентах не работает, поэтому мы тут переопределяем _notify
+         //(дефолтный метод для vdom компонент который стреляет событием).
+         //он будет вызван вместо того что стрельнет событием, тем самым мы проверяем что отправили
+         //событие и оно полетит с корректными параметрами.
+         header._notify = function(event){
+            if(event==='countClick'){
+               successCountClick = true;
+            }
+            if(event==='click'){
+               successClick = true;
+            }
+         };
+      });
+
+      afterEach(function () {
+         header.destroy();
+         header = undefined;
+      });
+
+      it('click with common click', function () {
+         var opt = {
+           countClickable: false
+         };
+         header.saveOptions(opt);
+         header.countClickHandler();
+         assert(!successCountClick);
+      });
+
+      it('click with count click', function () {
+         var opt = {
+           countClickable: true
+         };
+         var event = {
+            stopPropagation: function(){
+               return true;
+            }
+         };
+         header.saveOptions(opt);
+         header.countClickHandler(event);
+         assert(successCountClick);
+      });
+   });
+});
