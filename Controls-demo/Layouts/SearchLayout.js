799bb2d84f327e6f267020b982b97ee2f6e0a14b
ee.zolotova <ee.zolotova@tensor.ru>
2018-03-22 17:42:40 +0300

    https://online.sbis.ru/opendoc.html?guid=0621e853-a70d-43ca-8f25-ddd35ebe8cf1

--- Controls-demo/Layouts/SearchLayout.js
+++ Controls-demo/Layouts/SearchLayout.js
@@ -21,28 +21,28 @@ define('Controls-demo/Layouts/SearchLayout', [
    'use strict';
 
    var sourceData = [
-      { id: 1, title: 'Sasha' },
-      { id: 2, title: 'Dmitry' },
-      { id: 3, title: 'Andrey' },
-      { id: 4, title: 'Aleksey' },
-      { id: 5, title: 'Sasha' },
-      { id: 6, title: 'Ivan' },
-      { id: 7, title: 'Petr' },
-      { id: 8, title: 'Roman' },
-      { id: 9, title: 'Maxim' },
-      { id: 10, title: 'Andrey' },
-      { id: 12, title: 'Sasha' },
-      { id: 13, title: 'Sasha' },
-      { id: 14, title: 'Sasha' },
-      { id: 15, title: 'Sasha' },
-      { id: 16, title: 'Sasha' },
-      { id: 17, title: 'Sasha' },
-      { id: 18, title: 'Dmitry' },
-      { id: 19, title: 'Andrey' },
-      { id: 20, title: 'Aleksey' },
-      { id: 21, title: 'Sasha' },
-      { id: 22, title: 'Ivan' },
-      { id: 23, title: 'Petr' }
+      { id: 1, title: 'Sasha', lastName: 'aaaa' },
+      { id: 2, title: 'Dmitry', lastName: 'aaaa' },
+      { id: 3, title: 'Andrey', lastName: 'aaaa' },
+      { id: 4, title: 'Aleksey', lastName: 'aaaa' },
+      { id: 5, title: 'Sasha', lastName: 'aaaa' },
+      { id: 6, title: 'Ivan', lastName: 'dfsf'},
+      { id: 7, title: 'Petr', lastName: 'dfsf'},
+      { id: 8, title: 'Roman', lastName: 'dfsf'},
+      { id: 9, title: 'Maxim', lastName: 'dfsf'},
+      { id: 10, title: 'Andrey', lastName: 'dfsf'},
+      { id: 12, title: 'Sasha', lastName: 'dfsf'},
+      { id: 13, title: 'Sasha', lastName: 'dfsf'},
+      { id: 14, title: 'Sasha', lastName: 'dfsf'},
+      { id: 15, title: 'Sasha', lastName: 'dfsf'},
+      { id: 16, title: 'Sasha', lastName: 'dfsf'},
+      { id: 17, title: 'Sasha', lastName: 'dfsf'},
+      { id: 18, title: 'Dmitry', lastName: 'dfsf'},
+      { id: 19, title: 'Andrey', lastName: 'dfsf'},
+      { id: 20, title: 'Aleksey', lastName: 'dfsf'},
+      { id: 21, title: 'Sasha', lastName: 'dfsf'},
+      { id: 22, title: 'Ivan', lastName: 'dfsf'},
+      { id: 23, title: 'Petr', lastName: 'dfgdfg' }
    ];
 
    var filterData = [
@@ -76,6 +76,23 @@ define('Controls-demo/Layouts/SearchLayout', [
                   {id: 2, title: '1'},
                   {id: 3, title: '2'},
                   {id: 3, title: '3'}
+               ]
+            }
+         }
+      },
+      {
+         id: 'name',
+         idProperty: 'lastName',
+         displayProperty: 'title',
+         source: {
+            module: 'WS.Data/Source/Memory',
+            options: {
+               data: [
+                  {id: 0, title: 'All', lastName: 'aaaa'},
+                  {id: 1, title: '0', lastName: 'aaaa'},
+                  {id: 2, title: '1', lastName: 'aaaa'},
+                  {id: 3, title: '2', lastName: 'aaaa'},
+                  {id: 3, title: '3', lastName: 'aaaa'}
                ]
             }
          }
