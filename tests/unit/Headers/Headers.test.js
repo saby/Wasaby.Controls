a68c68cd6af9029f6dd48613e63faacae07e62c5
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-03-23 10:26:24 +0300

    https://online.sbis.ru/opendoc.html?guid=72bd3da3-9a1a-43c7-9faf-fa168f7f135e

--- tests/unit/Headers/Headers.test.js
+++ tests/unit/Headers/Headers.test.js
@@ -1,6 +1,6 @@
 define(['Controls/Header'], function (Header) {
    'use strict';
-   var header, successCountClick;
+   var header, successCountClick, successIconClick;
    describe('Controls.Header', function () {
       beforeEach(function(){
          header = new Header({
@@ -8,6 +8,7 @@ define(['Controls/Header'], function (Header) {
             style: 'default'
          });
          successCountClick = false;
+         successIconClick = false;
          //subscribe на vdom компонентах не работает, поэтому мы тут переопределяем _notify
          //(дефолтный метод для vdom компонент который стреляет событием).
          //он будет вызван вместо того что стрельнет событием, тем самым мы проверяем что отправили
@@ -15,6 +16,9 @@ define(['Controls/Header'], function (Header) {
          header._notify = function(event){
             if(event==='countClick'){
                successCountClick = true;
+            }
+            if(event==='iconClick'){
+               successIconClick = true;
             }
          };
       });
@@ -24,7 +28,7 @@ define(['Controls/Header'], function (Header) {
          header = undefined;
       });
 
-      it('click with common click', function () {
+      it('click counter without countClick', function () {
          var opt = {
            countClickable: false
          };
@@ -33,7 +37,30 @@ define(['Controls/Header'], function (Header) {
          assert(!successCountClick);
       });
 
-      it('click with count click', function () {
+      it('click icon without iconClick', function () {
+         var opt = {
+            iconClickable: false
+         };
+         header.saveOptions(opt);
+         header.iconClickHandler();
+         assert(!successIconClick);
+      });
+
+      it('click counter with iconClick', function () {
+         var opt = {
+            iconClickable: true
+         };
+         var event = {
+            stopPropagation: function(){
+               return true;
+            }
+         };
+         header.saveOptions(opt);
+         header.iconClickHandler(event);
+         assert(successIconClick);
+      });
+
+      it('click counter with countClick', function () {
          var opt = {
            countClickable: true
          };
