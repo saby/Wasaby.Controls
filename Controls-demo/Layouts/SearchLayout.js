89e5e6a93e2affd06b1b5d874b532f0e3281c666
ee.zolotova <ee.zolotova@tensor.ru>
2018-03-23 17:43:40 +0300

    Дополнено демо https://online.sbis.ru/opendoc.html?guid=0621e853-a70d-43ca-8f25-ddd35ebe8cf1

--- Controls-demo/Layouts/SearchLayout.js
+++ Controls-demo/Layouts/SearchLayout.js
@@ -25,40 +25,41 @@ define('Controls-demo/Layouts/SearchLayout', [
    'use strict';
    
    var sourceData = [
-      { id: 1, title: 'Sasha', lastName: 'aaaa' },
-      { id: 2, title: 'Dmitry', lastName: 'aaaa' },
-      { id: 3, title: 'Andrey', lastName: 'aaaa' },
-      { id: 4, title: 'Aleksey', lastName: 'aaaa' },
-      { id: 5, title: 'Sasha', lastName: 'aaaa' },
-      { id: 6, title: 'Ivan', lastName: 'dfsf'},
-      { id: 7, title: 'Petr', lastName: 'dfsf'},
-      { id: 8, title: 'Roman', lastName: 'dfsf'},
-      { id: 9, title: 'Maxim', lastName: 'dfsf'},
-      { id: 10, title: 'Andrey', lastName: 'dfsf'},
-      { id: 12, title: 'Sasha', lastName: 'dfsf'},
-      { id: 13, title: 'Sasha', lastName: 'dfsf'},
-      { id: 14, title: 'Sasha', lastName: 'dfsf'},
-      { id: 15, title: 'Sasha', lastName: 'dfsf'},
-      { id: 16, title: 'Sasha', lastName: 'dfsf'},
-      { id: 17, title: 'Sasha', lastName: 'dfsf'},
-      { id: 18, title: 'Dmitry', lastName: 'dfsf'},
-      { id: 19, title: 'Andrey', lastName: 'dfsf'},
-      { id: 20, title: 'Aleksey', lastName: 'dfsf'},
-      { id: 21, title: 'Sasha', lastName: 'dfsf'},
-      { id: 22, title: 'Ivan', lastName: 'dfsf'},
-      { id: 23, title: 'Petr', lastName: 'dfgdfg' }
+      { id: 1, firstName: 'Sasha', lastName: 'aaaa' },
+      { id: 2, firstName: 'Dmitry', lastName: 'aaaa' },
+      { id: 3, firstName: 'Andrey', lastName: 'aaaa' },
+      { id: 4, firstName: 'Aleksey', lastName: 'aaaa' },
+      { id: 5, firstName: 'Sasha', lastName: 'aaaa' },
+      { id: 6, firstName: 'Ivan', lastName: 'Lalala'},
+      { id: 7, firstName: 'Petr', lastName: 'dfsf'},
+      { id: 8, firstName: 'Roman', lastName: 'dfsf'},
+      { id: 9, firstName: 'Maxim', lastName: 'dfsf'},
+      { id: 10, firstName: 'Andrey', lastName: 'Lalala'},
+      { id: 12, firstName: 'Sasha', lastName: 'dfsf'},
+      { id: 13, firstName: 'Sasha', lastName: 'dfsf'},
+      { id: 14, firstName: 'Sasha', lastName: 'dfsf'},
+      { id: 15, firstName: 'Sasha', lastName: 'dfsf'},
+      { id: 16, firstName: 'Sasha', lastName: 'Lalala'},
+      { id: 17, firstName: 'Sasha', lastName: 'dfsf'},
+      { id: 18, firstName: 'Dmitry', lastName: 'Lalala'},
+      { id: 19, firstName: 'Andrey', lastName: 'dfsf'},
+      { id: 20, firstName: 'Aleksey', lastName: 'dfsf'},
+      { id: 21, firstName: 'Sasha', lastName: 'dfsf'},
+      { id: 22, firstName: 'Ivan', lastName: 'dfsf'},
+      { id: 23, firstName: 'Petr', lastName: 'dfgdfg' }
    ];
 
    var filterData = [
       {
-         id: 'title',
+         id: 'firstName',
          idProperty: 'title',
          displayProperty: 'title',
+         resetValue: 'По имени',
          source: {
             module: 'WS.Data/Source/Memory',
             options: {
                data: [
-                  {id: 0, title: 'All'},
+                  {id: 0, title: 'По имени'},
                   {id: 1, title: 'Sasha'},
                   {id: 2, title: 'Petr'},
                   {id: 3, title: 'Ivan'},
@@ -71,32 +72,35 @@ define('Controls-demo/Layouts/SearchLayout', [
          id: 'id',
          idProperty: 'id',
          displayProperty: 'title',
+         resetValue: '0',
          source: {
             module: 'WS.Data/Source/Memory',
             options: {
                data: [
-                  {id: 0, title: 'All'},
-                  {id: 1, title: '0'},
-                  {id: 2, title: '1'},
-                  {id: 3, title: '2'},
-                  {id: 3, title: '3'}
+                  {id: 0, title: 'По id'},
+                  {id: 1, title: '1'},
+                  {id: 2, title: '2'},
+                  {id: 3, title: '3'},
+                  {id: 4, title: '4'}
                ]
             }
          }
       },
       {
-         id: 'name',
+         id: 'lastName',
          idProperty: 'lastName',
          displayProperty: 'title',
+         value: 'aaaa',
+         resetValue: '0',
          source: {
             module: 'WS.Data/Source/Memory',
             options: {
                data: [
-                  {id: 0, title: 'All', lastName: 'aaaa'},
-                  {id: 1, title: '0', lastName: 'aaaa'},
-                  {id: 2, title: '1', lastName: 'aaaa'},
-                  {id: 3, title: '2', lastName: 'aaaa'},
-                  {id: 3, title: '3', lastName: 'aaaa'}
+                  {id: 1, title: 'aaaa', lastName: 'aaaa'},
+                  {id: 2, title: 'dfsf', lastName: 'dfsf'},
+                  {id: 3, title: 'Такой нет', lastName: 'aaaaa'},
+                  {id: 4, title: 'Lalala', lastName: 'Lalala'},
+                  {id: 0, title: 'По фамилии', lastName: '0'}
                ]
             }
          }
@@ -122,7 +126,8 @@ define('Controls-demo/Layouts/SearchLayout', [
          _fastFilterSource: new MemorySource({
             idProperty: 'id',
             data: filterData
-         })
+         }),
+         _filterData: filterData
       });
    return ModuleClass;
 });
