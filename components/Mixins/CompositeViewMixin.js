9d5a74eb745b919a87d63dbe25330e656edb7a3b
ai.konovalova <ai.konovalova@tensor.ru>
2018-03-26 17:16:43 +0300

    доработка описания опции tileMode

--- components/Mixins/CompositeViewMixin.js
+++ components/Mixins/CompositeViewMixin.js
@@ -137,9 +137,13 @@ define('SBIS3.CONTROLS/Mixins/CompositeViewMixin', [
              */
             viewMode : 'table',
             /**
-             * @cfg {String} Устанавливает режим отображения в режиме плитки
-             * @variant static
-             * @variant dynamic
+             * @cfg {String} Устанавливает отображение плитки по ховеру
+             * @remark
+             * Использование опции актуально для режима отображения {@link /doc/platform/developmentapl/interface-development/components/list/list-settings/list-visual-display/tiles/ "Плитка"}.
+             * В зависимости от значения опции ширина плитки может меняться или оставаться неизменной при наведении на нее курсора.
+             * @variant static ширина плитки остается неизменной.
+             * @variant dynamic ширина плитки меняется.
+             * @see viewNode
              */
             tileMode: '',
             /**
