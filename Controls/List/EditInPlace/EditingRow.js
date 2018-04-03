d8bafe8680d8648e6d7cfeb9cdc27ec30f367623
as.zaycev <as.zaycev@tensor.ru>
2018-04-03 09:30:51 +0300

    https://online.sbis.ru/opendoc.html?guid=18781e78-93a0-4335-b5e4-aeef900a8c6e

--- Controls/List/EditInPlace/EditingRow.js
+++ Controls/List/EditInPlace/EditingRow.js
@@ -8,11 +8,6 @@ define('Controls/List/EditInPlace/EditingRow', [
       _afterMount: function() {
          //TODO: надо подумать как ставить фокус в нужный редактор в гриде
          this.activate();
-      },
-
-      _onDeactivated: function(e, options) {
-         //TODO: шифт-таб
-         this._notify('rowDeactivated', [options.isTabPressed], { bubbling: true });
       }
    });
 
