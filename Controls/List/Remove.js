8af6d4a0db6b94bfe75a097a72c0583362f1511d
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2018-04-04 13:08:22 +0300

    https://online.sbis.ru/opendoc.html?guid=348abd43-5096-4a9d-96e5-915dd1211613

--- Controls/List/Remove.js
+++ Controls/List/Remove.js
@@ -3,17 +3,61 @@ define('Controls/List/Remove', [
    'tmpl!Controls/List/Remove/Remove',
    'Core/Deferred'
 ], function (Control, template, Deferred) {
+   var _private = {
+      removeFromSource: function(self, items) {
+         return self._sourceController.remove(items);
+      },
 
-   var Remove = Control.extend( {
-      _template: template,
+      removeFromModel: function(self, items) {
+         self._listModel.removeItems(items);
+      },
 
-      beforeItemsRemove: function(items) {
-         var beforeItemsRemoveResult = this._notify('beforeItemsRemove', [items]);
+      beforeItemsRemove: function(self, items) {
+         var beforeItemsRemoveResult = self._notify('beforeItemsRemove', [items]);
          return beforeItemsRemoveResult instanceof Deferred ? beforeItemsRemoveResult : Deferred.success(beforeItemsRemoveResult);
       },
 
-      afterItemsRemove: function(items, result) {
-         this._notify('afterItemsRemove', [items, result]);
+      afterItemsRemove: function(self, items, result) {
+         self._notify('afterItemsRemove', [items, result]);
+      }
+   };
+
+   var Remove = Control.extend( {
+      _template: template,
+      _listModel: undefined,
+      _sourceController: undefined,
+
+      _beforeMount: function(newOptions){
+         this._updateOptions(newOptions);
+      },
+
+      _beforeUpdate: function(newOptions){
+         this._updateOptions(newOptions);
+      },
+
+      _updateOptions: function(newOptions) {
+         if (newOptions.listModel && (this._listModel !== newOptions.listModel)) {
+            this._listModel = newOptions.listModel;
+         }
+         if (newOptions.sourceController && (this._sourceController !== newOptions.sourceController)) {
+            this._sourceController = newOptions.sourceController;
+         }
+      },
+
+      removeItems: function(items) {
+         var self = this;
+         _private.beforeItemsRemove(this, items).addCallback(function(result) {
+            if (result !== false) {
+               self._notify('showIndicator', [], { bubbling: true });
+               _private.removeFromSource(self, items).addCallback(function(result) {
+                  _private.removeFromModel(self, items);
+                  return result;
+               }).addBoth(function(result) {
+                  self._notify('hideIndicator', [], { bubbling: true });
+                  _private.afterItemsRemove(self, items, result);
+               });
+            }
+         });
       }
    });
 
