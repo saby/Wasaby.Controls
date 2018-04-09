0b657673d58111d0a69bc0c7efe73f651706bb6e
vd.volockoy <vd.volockoy@tensor.ru>
2018-04-09 17:48:09 +0300

    Фикс для хука

--- Controls-demo/HighCharts/HighCharts.js
+++ Controls-demo/HighCharts/HighCharts.js
@@ -3,7 +3,7 @@ define('Controls-demo/HighCharts/HighCharts',
       'Core/Control',
       'tmpl!Controls-demo/HighCharts/HighCharts'
    ],
-   function (Control, template) {
+   function(Control, template) {
       return Control.extend({
          _template: template
       });
