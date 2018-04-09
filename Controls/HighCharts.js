adccee94fd0517d0cd95b3e36748ce362a6ae945
vd.volockoy <vd.volockoy@tensor.ru>
2018-04-09 17:38:28 +0300

    Добавлен новый компонент VDOM HighCharts Задача: https://online.sbis.ru/opendoc.html?guid=18c0337f-6289-41d3-8137-9a38dfec9074

---
+++ Controls/HighCharts.js
@@ -0,0 +1,11 @@
+define('Controls/HighCharts',
+   [
+      'Core/Control',
+      'tmpl!Controls/HighCharts/HighCharts',
+      'css!Controls/HighCharts/HighCharts'
+   ],
+   function (Control, template) {
+      return Control.extend({
+         _template: template
+      });
+   });
