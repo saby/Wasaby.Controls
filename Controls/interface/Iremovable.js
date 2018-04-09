a5ca65f8c782ac034c5cdb8cf267699d0f836601
vd.volockoy <vd.volockoy@tensor.ru>
2018-04-09 16:30:11 +0300

    Добавил интерфейс для удаления элементов из коллекций

---
+++ Controls/interface/Iremovable.js
@@ -0,0 +1,29 @@
+define('Controls/interface/IRemovable', [], function () {
+
+    /**
+     * Interface for remove instances from collection
+     *
+     * @mixin Controls/interface/IRemovable
+     * @public
+     */
+
+    /**
+     * @event Controls/interface/IRemovable#beforeItemsRemove Fires before items are removed.
+     * @param {Core/EventObject} eventObject The event descriptor.
+     * @param {Array.<String>|Array.<Number>} idArray Array of identifiers of removable items.
+     * @returns {*} result If result=false then canceled the logic of removing the items by default.
+     */
+
+    /**
+     * @event Controls/interface/IRemovable#afterItemsRemove Fires after items are removed.
+     * @param {Core/EventObject} eventObject The event descriptor.
+     * @param {Array.<String>|Array.<Number>} idArray Array of identifiers of removable items.
+     * @param {*} result The result of removing items from the data source.
+     */
+
+    /**
+     * @function Controls/interface/IRemovable#removeItems Removes items from the data source by identifiers of the items in the collection.
+     * @param {Array} items Array with the identifiers of the items in the collection.
+     */
+
+});
