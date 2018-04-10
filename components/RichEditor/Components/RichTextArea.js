d46eacb85f47624010b3b4d46137b174a31a5c03
va.spirin <va.spirin@tensor.ru>
2018-04-10 14:36:16 +0700

    RichEditor: Испрвлено неправильное положение рэнжа при вставке ссылки
    1175114814 https://online.sbis.ru/opendoc.html?guid=4cef3009-ccbc-4751-b755-dea3d69b82f1

--- components/RichEditor/Components/RichTextArea.js
+++ components/RichEditor/Components/RichTextArea.js
@@ -1248,13 +1248,14 @@ define('SBIS3.CONTROLS/RichEditor/Components/RichTextArea',
                                  if (href) {
                                     linkAttrs.href = href;
                                     selection.setRng(range);
-                                    if (selection.getContent() === '' || (fre._isOnlyTextSelected() && cConstants.browser.firefox)) {
-                                       var linkText = caption;
-                                       var linkHtml = dom.createHTML('a', linkAttrs, dom.encode(linkText));
-                                       // Для MSIE принудительно смещаем курсор ввода после вставленной ссылки
+                                    var content = selection.getContent();
+                                    if (content === '' || ((content.indexOf('<') === -1 || (content.indexOf('href=') !== -1 && /^<a [^>]+>[^<]+<\/a>$/.test(content))) && cConstants.browser.firefox)) {
+                                       var linkHtml = dom.createHTML('a', linkAttrs, dom.encode(caption));
+                                       // Для MSIE и FF принудительно смещаем курсор ввода после вставленной ссылки
                                        // 1174853380 https://online.sbis.ru/opendoc.html?guid=77405679-2b2b-42d3-8bc0-d2eee745ea23
-                                       editor.insertContent(cConstants.browser.isIE ? linkHtml + '&#65279;&#8203;' : linkHtml);
-                                       selection.select(selection.getNode().querySelector('a'), true);
+                                       // 1175114814 https://online.sbis.ru/opendoc.html?guid=4cef3009-ccbc-4751-b755-dea3d69b82f1
+                                       editor.insertContent(cConstants.browser.isIE ? linkHtml + '&#65279;&#8203;' : (cConstants.browser.firefox ? linkHtml + '&#65279;' : linkHtml));
+                                       //selection.select(selection.getNode().querySelector('a'), true);
                                        selection.collapse(false);
                                     }
                                     else {
