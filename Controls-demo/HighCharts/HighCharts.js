a3982467df1d706c39a1e785c3599fbf373abe82
vd.volockoy <vd.volockoy@tensor.ru>
2018-04-10 15:33:58 +0300

    Компонент диаграмма(плагин Highcharts). Обновление: 1)Добавлено построение диаграммы(настраиваемое на клиенте) 2)Добавлено обновление диаграммы(при обновлении объекта настроек на клиенте)

--- Controls-demo/HighCharts/HighCharts.js
+++ Controls-demo/HighCharts/HighCharts.js
@@ -5,6 +5,36 @@ define('Controls-demo/HighCharts/HighCharts',
    ],
    function(Control, template) {
       return Control.extend({
-         _template: template
+         _template: template,
+         highChartOptions1: {
+            chart: {
+               type: 'line'
+            },
+            series: [{
+               name: 'USD to EUR',
+               data: [10, 20]
+            }]
+         },
+         highChartOptions2: {
+            chart: {
+               type: 'pie'
+            },
+            series: [{
+               name: 'USD to EUR',
+               data: [10, 20]
+            }]
+         },
+         highChartOptions3: {
+            chart: {
+               type: 'line'
+            },
+            series: [{
+               name: 'USD to EUR',
+               data: [10, 20]
+            }]
+         },
+         changeHighChartOptions: function () {
+            this.highChartOptions1 = this.highChartOptions3;
+         }
       });
    });
--- Controls/HighCharts.js
+++ Controls/HighCharts.js
@@ -2,10 +2,53 @@ define('Controls/HighCharts',
    [
       'Core/Control',
       'tmpl!Controls/HighCharts/HighCharts',
-      'css!Controls/HighCharts/HighCharts'
+      'Core/constants',
+      'Core/Date',
+      'css!Controls/HighCharts/HighCharts',
+      'browser!/cdn/highcharts/4.2.7/highcharts-more.js'
    ],
-   function(Control, template) {
-      return Control.extend({
-         _template: template
+   function (Control, template, constants) {
+      'use strict';
+
+      var HighChart = Control.extend({
+         _template: template,
+
+         _shouldUpdate: function () {
+            return false;
+         },
+
+         _beforeMount: function () {
+            Highcharts.setOptions({
+               lang: {
+                  numericSymbols: ['', '', '', '', '', ''],
+                  months : constants.Date.longMonths,
+                  shortMonths : constants.Date.months,
+                  weekdays: constants.Date.longDays,
+                  thousandsSep : ' '
+               },
+               plotOptions: {
+                  series: {
+                     animation: !constants.browser.isIE10
+                  }
+               }
+            });
+         },
+
+         _afterMount: function(config) {
+            this._drawChart(config);
+         },
+
+         _beforeUpdate: function (config) {
+            if (this._options.highChartOptions !== config.highChartOptions) {
+               this._drawChart(config);
+            }
+         },
+
+         _drawChart: function (config) {
+            config.highChartOptions.chart.renderTo = this._children[this._options.highChartContainer];
+            new Highcharts.Chart(config.highChartOptions);
+         }
       });
+
+      return HighChart;
    });
