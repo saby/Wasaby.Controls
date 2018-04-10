22c500bfc7bb4a1343831078bb10cceac814fa0d
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-10 19:48:40 +0300

    https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

---
+++ Controls-demo/FilterPanel/FilterPanel.js
@@ -0,0 +1,80 @@
+define('Controls-demo/FilterPanel/FilterPanel',
+   [
+      'Core/Control',
+      'WS.Data/Source/Memory',
+      'tmpl!Controls-demo/FilterPanel/FilterPanel',
+      'Controls/Input/Number',
+      'Controls/Input/Password',
+      'Controls/Input/Area',
+      'Controls/Input/Dropdown',
+      'Controls/Filter/Panel'
+   ],
+
+   function (Control, Memory, template) {
+      'use strict';
+
+      var FilterPanel = Control.extend({
+
+         _template: template,
+         areaText: '',
+         sKey: 1,
+
+         dataSource: {
+            module: 'WS.Data/Source/Memory',
+            options: {
+               data: [
+                  {key: 1, title: 'все страны'},
+                  {key: 2, title: 'Россия'},
+                  {key: 3, title: 'США'},
+                  {key: 4, title: 'Великобритания'}
+               ],
+               idProperty: 'key'
+            }
+         },
+
+         itemsFilterPanel: [
+            {
+               id: 'number',
+               value: '',
+               editor: 'text',
+               template: 'Controls/Input/Number'
+            },
+            {
+               id: 'password',
+               value: '',
+               editor: 'text',
+               template: 'Controls/Input/Password'
+            },
+            {
+               id: 'area',
+               value: 'Long long',
+               editor: 'text',
+               template: 'Controls/Input/Area'
+            },
+            {
+               id: 'button',
+               style: 'linkMain',
+               caption: 'click me',
+               editor: 'boolean',
+               template: 'Controls/Button'
+            },
+            {
+               id: 'list',
+               value: 1,
+
+               keyProperty: 'key',
+               editor: 'keys',
+               template: 'Controls/Input/Dropdown'
+            }],
+
+         changeHandler:function (event, value) {
+            this.areaText = value['0'];
+            this.areaText += '\n' + value['1'] + '\n' + value['2'];
+         }
+
+
+
+      });
+
+      return FilterPanel;
+   });
---
+++ Controls/Filter/Panel/PropertyGrid.js
@@ -0,0 +1,32 @@
+define('Controls/Filter/Panel/PropertyGrid', [
+   'Core/Control',
+   'WS.Data/Chain',
+   'Core/helpers/Object/isEqual',
+   'tmpl!Controls/Filter/Panel/PropertyGrid/PropertyGrid',
+   'Controls/Filter/Panel/Editor/Boolean',
+   'Controls/Filter/Panel/Editor/Text',
+   'Controls/Filter/Panel/Editor/Keys'
+   // 'css!Controls/Filter/Panel/Panel'
+], function (Control, Chain, isEqual, template) {
+
+   'use strict';
+
+   var PropertyGrid = Control.extend({
+      _template: template,
+
+      _beforeMount: function (opts) {
+         this.items = opts.items;
+      },
+
+      _beforeUpdate: function (newOptions) {
+         if (!isEqual(this.items, newOptions.items)) {
+            this.items = newOptions.items;
+            this._forceUpdate();
+         }
+      }
+
+   });
+
+   return PropertyGrid;
+
+});
