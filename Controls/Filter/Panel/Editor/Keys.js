c420fe200a95782ee1a8ef98c2e00e90a418bbf8
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-10 19:48:40 +0300

    https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

---
+++ Controls/Filter/Panel/Editor/Keys.js
@@ -0,0 +1,23 @@
+define('Controls/Filter/Panel/Editor/Keys', [
+   'Core/Control',
+   'tmpl!Controls/Filter/Panel/Editor/Keys/KeysEditor'
+   // 'css!Controls/Filter/Panel/Panel'
+], function (Control, template) {
+
+   'use strict';
+
+   var KeysEditor = Control.extend({
+      _template: template,
+
+      constructor: function (cfg) {
+         KeysEditor.superclass.constructor.apply(this, arguments);
+      },
+
+      _beforeMount: function (options) {
+         this._item = options.item;
+      }
+   });
+
+   return KeysEditor;
+
+});
---
+++ Controls/Filter/Panel/Editor/Text.js
@@ -0,0 +1,27 @@
+define('Controls/Filter/Panel/Editor/Text', [
+   'Core/Control',
+   'tmpl!Controls/Filter/Panel/Editor/Text/TextEditor'
+   // 'css!Controls/Filter/Panel/Panel'
+], function (Control, template) {
+
+   'use strict';
+
+   var TextEditor = Control.extend({
+      _template: template,
+
+      constructor: function (cfg) {
+         TextEditor.superclass.constructor.apply(this, arguments);
+      },
+
+      _beforeMount: function (options) {
+         this._item = options.item;
+      },
+
+      _beforeUpdate: function (newOptions) {
+         return newOptions.item;
+      }
+   });
+
+   return TextEditor;
+
+});
