3e4f045093bebf4faf761ac20c14e9cca6526634
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-04-10 12:15:21 +0300

    https://online.sbis.ru/opendoc.html?guid=9eb5b9a9-2355-4840-a4ea-14d5214dd659

--- Controls-demo/Buttons/demoButtons.js
+++ Controls-demo/Buttons/demoButtons.js
@@ -12,8 +12,17 @@ define('Controls-demo/Buttons/demoButtons', [
       {
          _template: template,
          count:0,
+         _addButtonIcon: '',
          clickHandler: function (e) {
             this.count++;
+         },
+
+         mouseoverAddButtonHandler: function (e) {
+            this._addButtonIcon = "primary";
+         },
+
+         mouseoutAddButtonHandler: function (e) {
+            this._addButtonIcon = "";
          }
       });
    return ModuleClass;
