716926e90d5d01027c5e3aa37f8ace039511ae39
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-11 18:48:50 +0300

    https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

--- Controls/Filter/Panel/PropertyGrid.js
+++ Controls/Filter/Panel/PropertyGrid.js
@@ -6,19 +6,20 @@ define('Controls/Filter/Panel/PropertyGrid', [
    'Controls/Filter/Panel/Editor/Boolean',
    'Controls/Filter/Panel/Editor/Text',
    'Controls/Filter/Panel/Editor/Keys'
+
    // 'css!Controls/Filter/Panel/Panel'
-], function (Control, Chain, isEqual, template) {
+], function(Control, Chain, isEqual, template) {
 
    'use strict';
 
    var PropertyGrid = Control.extend({
       _template: template,
 
-      _beforeMount: function (opts) {
+      _beforeMount: function(opts) {
          this.items = opts.items;
       },
 
-      _beforeUpdate: function (newOptions) {
+      _beforeUpdate: function(newOptions) {
          if (!isEqual(this.items, newOptions.items)) {
             this.items = newOptions.items;
             this._forceUpdate();
