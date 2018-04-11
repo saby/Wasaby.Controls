b918d4cb7075d7f7709ab6531f9aa6a22d5fec2b
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-11 18:48:50 +0300

    https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

--- Controls/Filter/Panel/Editor/Boolean.js
+++ Controls/Filter/Panel/Editor/Boolean.js
@@ -1,19 +1,20 @@
 define('Controls/Filter/Panel/Editor/Boolean', [
    'Core/Control',
    'tmpl!Controls/Filter/Panel/Editor/Boolean/BooleanEditor'
+
    // 'css!Controls/Filter/Panel/Panel'
-], function (Control, template) {
+], function(Control, template) {
 
    'use strict';
 
    var BooleanEditor = Control.extend({
       _template: template,
 
-      constructor: function (cfg) {
-        BooleanEditor.superclass.constructor.apply(this, arguments);
+      constructor: function(cfg) {
+         BooleanEditor.superclass.constructor.apply(this, arguments);
       },
 
-      _clickHandler:function () {
+      _clickHandler: function() {
          this._options.item.value = !this._options.item.value;
 
       }
--- Controls/Filter/Panel/Editor/Text.js
+++ Controls/Filter/Panel/Editor/Text.js
@@ -1,23 +1,24 @@
 define('Controls/Filter/Panel/Editor/Text', [
    'Core/Control',
    'tmpl!Controls/Filter/Panel/Editor/Text/TextEditor'
+
    // 'css!Controls/Filter/Panel/Panel'
-], function (Control, template) {
+], function(Control, template) {
 
    'use strict';
 
    var TextEditor = Control.extend({
       _template: template,
 
-      constructor: function (cfg) {
+      constructor: function(cfg) {
          TextEditor.superclass.constructor.apply(this, arguments);
       },
 
-      _beforeMount: function (options) {
+      _beforeMount: function(options) {
          this._item = options.item;
       },
 
-      _beforeUpdate: function (newOptions) {
+      _beforeUpdate: function(newOptions) {
          return newOptions.item;
       }
    });
