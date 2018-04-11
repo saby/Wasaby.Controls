48bf519704e219949ee62417a712a8965b9616f5
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-11 19:20:00 +0300

    https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

--- Controls/Filter/Button.js
+++ Controls/Filter/Button.js
@@ -8,20 +8,23 @@ define('Controls/Filter/Button',
       'Core/moduleStubs',
       'WS.Data/Chain',
       'WS.Data/Utils',
-      'css!Controls/Filter/Button/Button'
+      'Core/helpers/Array/clone',
+      'css!Controls/Filter/Button/Button',
+      'Controls/Input/Text',
+      'Controls/Filter/Panel'
    ],
-   
-   function(Control, template, moduleStubs, Chain, Utils) {
-      
+
+   function(Control, template, moduleStubs, Chain, Utils, clone) {
+
       /**
        * @class Controls/Container/Search
        * @extends Controls/Control
        * @control
        * @public
        */
-      
+
       'use strict';
-   
+
       var _private = {
          getFilterButtonCompatible: function(self) {
             return moduleStubs.require('Controls/Filter/Button/_FilterCompatible').addCallback(function(_FilterCompatible) {
@@ -34,56 +37,56 @@ define('Controls/Filter/Button',
                return self._filterCompatible;
             });
          },
-   
+
          getText: function(items) {
             var textArr = [];
-      
+
             Chain(items).each(function(item) {
                var textValue = Utils.getItemPropertyValue(item, 'textValue');
-         
+
                if (textValue) {
                   textArr.push(textValue);
                }
             });
-      
+
             return textArr.join(', ');
          },
-         
+
          resolveItems: function(self, items) {
             self._items = items;
             self._text = _private.getText(items);
          }
       };
-      
+
       var FilterButton = Control.extend({
-         
+
          _template: template,
          _oldPanelOpener: null,
          _text: '',
-         
+
          _beforeUpdate: function(options) {
             if (this._options.items !== options.items) {
                _private.resolveItems(this, options.items);
             }
          },
-         
+
          _beforeMount: function(options) {
             if (options.items) {
                _private.resolveItems(this, options.items);
             }
          },
-         
+
          _getFilterState: function() {
             return this.isEnabled() ? 'default' : 'disabled';
          },
-   
+
          _clearClick: function() {
             _private.getFilterButtonCompatible(this).addCallback(function(panelOpener) {
                panelOpener.clearFilter();
             });
          },
-   
-         _openFilterPanel: function() {
+
+         _openFilterPanel: function(event) {
             if (this.isEnabled()) {
                /* if template - show old component */
                if (this._options.filterTemplate) {
@@ -91,17 +94,28 @@ define('Controls/Filter/Button',
                      panelOpener.showFilterPanel();
                   });
                } else {
-                  //...soon
+                  this._children.filterStickyOpener.open({
+                     componentOptions: {
+                        items: clone(this._options.items)
+                     },
+                     template: 'Controls/Filter/Panel',
+                     target: event.target
+                  });
                }
             }
+         },
+
+         _notifyFilter: function (event, filter) {
+            this._notify('filterChanged', [filter]);
+            this._children.filterStickyOpener.close();
          }
       });
-      
+
       FilterButton.getDefaultOptions = function() {
          return {
             filterAlign: 'right'
          };
       };
-      
+
       return FilterButton;
    });
