c4b01050519b96f5ae2caca0be85a9d745a9c509
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-13 21:15:40 +0300

    Стили https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

--- Controls/Filter/Panel.js
+++ Controls/Filter/Panel.js
@@ -2,26 +2,34 @@ define('Controls/Filter/Panel', [
    'Core/Control',
    'WS.Data/Chain',
    'WS.Data/Utils',
+   'Core/helpers/Object/isEqual',
    'Core/helpers/Array/clone',
    'tmpl!Controls/Filter/Panel/Panel',
-   'css!Controls/Popup/DialogTemplate',
    'css!Controls/Filter/Panel/Panel'
 
-], function(Control, Chain, Utils, clone, template) {
+], function(Control, Chain, Utils, isEqual, clone, template) {
 
    'use strict';
 
 
    var FilterPanel = Control.extend({
       _template: template,
+      _isEnabled: true,
 
       _beforeMount: function(options) {
          this._items = clone(options.items);
       },
 
+      //?
+      _beforeUpdate: function() {
+         if (isEqual(this._items, this._options.items)) {
+            this._isEnabled = true;
+         }
+      },
+
       applyFilter: function() {
          this._notify('filterChanged', [this.getFilter()]);
-         return this._items;
+         this._notify('close', [], {bubbling: true});
       },
 
       getFilter: function() {
@@ -36,10 +44,11 @@ define('Controls/Filter/Panel', [
          Chain(this._items).each(function(item) {
             item.value = item.resetValue;
          });
+         this._isEnabled = false;
          this._children.PropertyGrid._forceUpdate();
       },
 
-      close: function() {
+      _close: function() {
          this.resetFilter();
          this._notify('close', [], {bubbling: true});
       }
--- Controls/Filter/Panel/PropertyGrid.js
+++ Controls/Filter/Panel/PropertyGrid.js
@@ -1,23 +1,31 @@
 define('Controls/Filter/Panel/PropertyGrid', [
    'Core/Control',
    'WS.Data/Chain',
-   'Core/helpers/Object/isEqual',
    'tmpl!Controls/Filter/Panel/PropertyGrid/PropertyGrid',
    'Controls/Filter/Panel/Editor/Boolean',
    'Controls/Filter/Panel/Editor/Text',
    'Controls/Filter/Panel/Editor/Keys',
    'css!Controls/Filter/Panel/PropertyGrid/PropertyGrid'
-], function(Control, Chain, isEqual, template) {
+], function(Control, Chain, template) {
+   /**
+    * Control PropertyGrid
+    * Provides a user interface for browsing and editing the properties of an object.
+    *
+    * @class Controls/Filter/Panel/PropertyGrid
+    * @extends Controls/Control
+    * @control
+    * @public
+    * @author Золотова Э.Е.
+    */
 
    'use strict';
 
    var PropertyGrid = Control.extend({
       _template: template,
 
-      _beforeMount: function(opts) {
-         this.items = opts.items;
+      _beforeMount: function(options) {
+         this.items = options.items;
       }
-
    });
 
    return PropertyGrid;
