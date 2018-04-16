a2106e5f15ae28d44bb9c6027d3b0c0e52ef3040
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-16 17:09:59 +0300

    Исправления по замечаниям https://online.sbis.ru/opendoc.html?guid=4bc3ba0b-ae25-43ab-9b9a-c1eaefac38bf

--- Controls/Filter/Panel/Editor/Boolean.js
+++ Controls/Filter/Panel/Editor/Boolean.js
@@ -2,17 +2,12 @@ define('Controls/Filter/Panel/Editor/Boolean', [
    'Core/Control',
    'tmpl!Controls/Filter/Panel/Editor/Boolean/BooleanEditor'
 
-   // 'css!Controls/Filter/Panel/Panel'
 ], function(Control, template) {
 
    'use strict';
 
    var BooleanEditor = Control.extend({
       _template: template,
-
-      _clickHandler: function() {
-         this._item.value = !this._options.item.value;
-      },
 
       _beforeMount: function(options) {
          this._item = options.item;
