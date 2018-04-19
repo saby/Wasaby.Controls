28dbba0fe599abdb88be28a80801f3254630d74c
va.spirin <va.spirin@tensor.ru>
2018-04-19 19:06:28 +0700

    ExportCustomizer: Первичная раскладка (4)
    1174986512 https://online.sbis.ru/opendoc.html?guid=36c73e65-003b-4f5b-91ea-10f52fb5410b

---
+++ components/ExportCustomizer/_Formatter/View.js
@@ -0,0 +1,102 @@
+/**
+ * Контрол "Форматирование экспортируемого файла настройщика экспорта"
+ *
+ * @public
+ * @class SBIS3.CONTROLS/ExportCustomizer/_Formatter/View
+ * @extends SBIS3.CONTROLS/CompoundControl
+ */
+define('SBIS3.CONTROLS/ExportCustomizer/_Formatter/View',
+   [
+      'SBIS3.CONTROLS/CompoundControl',
+      'tmpl!SBIS3.CONTROLS/ExportCustomizer/_Formatter/View'/*,
+      'css!SBIS3.CONTROLS/ExportCustomizer/_Formatter/View'*/
+   ],
+
+   function (CompoundControl, dotTplFn) {
+      'use strict';
+
+      var View = CompoundControl.extend(/**@lends SBIS3.CONTROLS/ExportCustomizer/_Formatter/View.prototype*/ {
+
+         /**
+          * @typedef {object} ExportFormatterResult Тип, описывающий возвращаемые настраиваемые значения компонента
+          * @property {string} fileUrl Урл файла с результатом форматирования
+          * @property {string} previewUrl Урл для предпросмотра результата форматирования
+          *
+          * @see fileUrl
+          * @see previewUrl
+          */
+
+         _dotTplFn: dotTplFn,
+         $protected: {
+            _options: {
+               /**
+                * @cfg {string} Урл файла с результатом форматирования
+                */
+               fileUrl: null,
+               /**
+                * @cfg {string} Урл для предпросмотра результата форматирования
+                */
+               previewUrl: null
+            },
+            // Контрол выбора способа форматирования
+            _menu: null
+         },
+
+         _modifyOptions: function () {
+            var options = View.superclass._modifyOptions.apply(this, arguments);
+            options._items = [//^^^'Редактировать'
+               {id:'browser', title:'в браузере'},
+               {id:'app', title:'в приложении'}
+            ];
+            return options;
+         },
+
+         /*$constructor: function () {
+         },*/
+
+         init: function () {
+            View.superclass.init.apply(this, arguments);
+            this._menu = this.getChildControlByName('controls-ExportCustomizer-Formatter-View__head__edit');
+            this._bindEvents();
+         },
+
+         _bindEvents: function () {
+            /*^^^this.subscribeTo(this._grid, 'on^^^', function (evtName) {
+             var ^^^ = ^^^;
+             this._options.^^^ = ^^^;
+             this.sendCommand('subviewChanged');
+             }.bind(this));*/
+         },
+
+         /**
+          * Установить указанные настраиваемые значения компонента
+          *
+          * @public
+          * @param {object} values Набор из нескольких значений, которые необходимо изменить
+          */
+         setValues: function (values) {
+            if (!values || typeof values !== 'object') {
+               throw new Error('Object required');
+            }
+            var options = this._options;
+            //^^^
+         },
+
+         /**
+          * Получить все настраиваемые значения компонента
+          *
+          * @public
+          * @return {ExportFormatterResult}
+          */
+         getValues: function () {
+            var options = this._options;
+            return {
+               fileUrl: options.fileUrl,
+               previewUrl: options.previewUrl
+            };
+         }
+      });
+
+      return View;
+   }
+);
