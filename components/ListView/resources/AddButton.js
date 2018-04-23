fe02752524b9de2def48b8e05701fc2668f976ba
aa.bykanov <aa.bykanov@tensor.ru>
2018-04-23 15:55:31 +0300

    Добавлена заметка про новую кнопку.

--- components/ListView/resources/AddButton.js
+++ components/ListView/resources/AddButton.js
@@ -5,7 +5,15 @@ define('SBIS3.CONTROLS/ListView/resources/AddButton', [
    'css!SBIS3.CONTROLS/ListView/resources/AddButton/AddButton'
 ], function(WSButton) {
 
-   var AddButton = WSButton.extend([], /** @lends SBIS3.CONTROLS/Button/ToggleButton.prototype */ {
+    /**
+     * Класс кнопки, которая используется для добавления записей внизу таблицы.
+     * <a href="http://axure.tensor.ru/standarts/v7/%D0%BA%D0%BD%D0%BE%D0%BF%D0%BA%D0%B8__%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D1%8F_08_.html">Стандарт</a>.
+     * @class SBIS3.CONTROLS/ListView/resources/AddButton
+     * @extends SBIS3.CONTROLS/WSControls/Buttons/Button
+     * @public
+     *
+     */
+   var AddButton = WSButton.extend([], /** @lends SBIS3.CONTROLS/ListView/resources/AddButton.prototype */ {
       $protected: {
          _options: {
          }
