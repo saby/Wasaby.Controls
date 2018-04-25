39349c73dcc9f382aa4b112e1a319cc657a08209
ee.zolotova <ee.zolotova@tensor.ru>
2018-04-25 19:53:03 +0300

    Блок "Еще можно отобрать" https://online.sbis.ru/opendoc.html?guid=6a09665e-2ec3-44eb-b2c1-03183e731104

--- Controls/Filter/Button/Panel/Editor/Boolean.js
+++ Controls/Filter/Button/Panel/Editor/Boolean.js
@@ -1,15 +1,19 @@
 define('Controls/Filter/Button/Panel/Editor/Boolean', [
    'Core/Control',
+   'WS.Data/Utils',
    'tmpl!Controls/Filter/Button/Panel/Editor/Boolean/BooleanEditor'
 
-], function(Control, template) {
+], function(Control, Utils, template) {
 
    'use strict';
 
    var BooleanEditor = Control.extend({
       _template: template,
 
-      _valueChangedHandler: function() {
+      _valueChangedHandler: function(event, value, visibility) {
+         if (visibility !== undefined) {
+            Utils.setItemPropertyValue(this._options.item, 'visibility', visibility);
+         }
          this._notify('valueChanged', [this._options.item.value]);
       }
    });
