870ef611411416bed2f9c6c679a6be526b2ae0ab
kraynovdo <do.kraynov@tensor.ru>
2018-04-26 16:44:08 +0300

    https://online.sbis.ru/opendoc.html?guid=0ef9515a-ad32-4e14-91d5-c8e9113fbca8

---
+++ Controls/Container/Adapter/Enum.js
@@ -0,0 +1,75 @@
+/**
+ * Created by kraynovdo on 26.04.2018.
+ */
+define('Controls/Container/Adapter/Enum',
+   [
+      'Core/Control',
+      'tmpl!Controls/Container/Adapter/Enum/Enum',
+      'WS.Data/Source/Memory'
+   ],
+
+   function(Control, template, Memory) {
+      var _private = {
+         getArrayFromEnum: function(enumInstance) {
+            var arr = [];
+            enumInstance.each(function(item) {
+               arr.push({'title' : item});
+            });
+            return arr;
+         },
+
+         getSourceFromEnum: function(enumInstance) {
+            var data = _private.getArrayFromEnum(enumInstance);
+            return new Memory({
+               data: data,
+               idProperty: 'title'
+            });
+         }
+
+      };
+
+      /**
+       * Container component for Search
+       * Notify bubbling event "search".
+       * Should be located inside Controls/Container/Search.
+       * @class Controls/Container/Input/Search
+       * @extends Controls/Control
+       * @author Герасимов Александр
+       * @control
+       * @public
+       */
+
+      'use strict';
+
+      var SearchContainer = Control.extend({
+
+         _template: template,
+         _source: null,
+
+         _enum: null,
+
+         _beforeMount: function(newOptions) {
+            if (newOptions.enum) {
+               this._enum = newOptions.enum;
+               this._source = _private.getSourceFromEnum(newOptions.enum);
+               this._selectedKey = newOptions.enum.getAsValue();
+            }
+         },
+
+         _beforeUpdate: function(newOptions) {
+            if ((newOptions.enum) || (newOptions.enum !== this._enum)) {
+               this._source = _private.getSourceFromEnum(newOptions.enum);
+               this._selectedKey = newOptions.enum.getAsValue();
+            }
+         },
+
+         _changeKey: function(e, key) {
+            if (this._enum) {
+               this._enum.setByValue(key);
+            }
+         }
+
+      });
+
+      return SearchContainer;
+   });
