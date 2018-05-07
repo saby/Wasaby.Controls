ff04396344beea94c10c4e0f3418d8bc9669370c
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-05-07 08:34:47 +0300

    https://online.sbis.ru/opendoc.html?guid=6e201c19-6106-421e-8b4e-20290d9e9199

--- Controls/Input/Mask.js
+++ Controls/Input/Mask.js
@@ -44,8 +44,10 @@ define('Controls/Input/Mask',
        *
        * @example
        * <pre>
-       *    1. 'dd.dd' - маска ввода времени.
-       *    2. 'dd.dd.dddd' - маска ввода даты.
+       *    1. 'dd.dd' - the input mask time.
+       *    2. 'dd.dd.dddd' - the input mask date.
+       *    3. 'd\{1,3}l\{1,3}'.
+       *    4. 'd\*' - the input mask infinity number of digits.
        * </pre>
        */
 
