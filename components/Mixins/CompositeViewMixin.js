73be7e6a8412b80ba43319629595e6b326e206db
vd.volockoy <vd.volockoy@tensor.ru>
2018-05-09 14:09:52 +0300

    Добавил пэддинги при скролле вниз у ListView, чтобы scrollIndicator не перекрывал элементы Ошибка: https://online.sbis.ru/opendoc.html?guid=1acbc61c-a8cf-4931-a10c-922ced76ab45

--- components/Mixins/CompositeViewMixin.js
+++ components/Mixins/CompositeViewMixin.js
@@ -313,8 +313,37 @@ define('SBIS3.CONTROLS/Mixins/CompositeViewMixin', [
          var
             additionalWidth = Math.floor(item.outerWidth() / 2),
             margin = Math.floor(item.outerWidth(true) / 2 - additionalWidth),
-            additionalHeight = Math.floor(item.outerHeight() / 2);
-         item.css('padding', Math.ceil(additionalHeight / 2) + 'px ' + Math.ceil(additionalWidth / 2) + 'px').css('margin', '' + Math.floor(-(additionalHeight / 2 - margin)) + 'px ' + Math.floor(-(additionalWidth / 2 - margin)) + 'px');
+            additionalHeight = Math.floor(item.outerHeight() / 2),
+            container = this._getItemsContainer(),
+            containerRects = container.get(0).getBoundingClientRect(),
+            itemRects = item.get(0).getBoundingClientRect(),
+            horizontalRightDiffer = containerRects.width - (itemRects.width + item.get(0).offsetLeft + additionalWidth / 2),
+            horizontalLeftDiffer = item.get(0).offsetLeft - additionalWidth / 2,
+            verticalBottomDiffer = containerRects.height - (itemRects.height + item.get(0).offsetTop + additionalHeight / 2),
+            verticalTopDiffer = item.get(0).offsetTop - additionalHeight / 2 ,
+            marginLeft,
+            marginRight,
+            marginTop,
+            marginBottom,
+            padding = Math.ceil(additionalHeight / 2) + 'px ' + Math.ceil(additionalWidth / 2) + 'px';
+
+         marginTop = marginBottom = (Math.floor(-(additionalHeight / 2 - margin)) + 'px ');
+         marginLeft = marginRight = (Math.floor(-(additionalWidth / 2 - margin)) + 'px ');
+         if (horizontalLeftDiffer < 0) {
+            marginRight = (Math.floor(-(additionalWidth - margin)) + 'px ');
+            marginLeft = 0;
+         } else if (horizontalRightDiffer < 0) {
+            marginLeft = (Math.floor(-(additionalWidth - margin)) + 'px');
+            marginRight = 0 + ' ';
+         }
+         if (verticalTopDiffer < 0) {
+            marginBottom = (Math.floor(-(additionalHeight - margin)) + 'px ');
+            marginTop = 0 + ' ';
+         } else if (verticalBottomDiffer < 0) {
+            marginTop = (Math.floor(-(additionalHeight - margin)) + 'px ');
+            marginBottom = 0 + ' ';
+         }
+         item.css('padding', padding).css('margin', marginTop + marginRight + marginBottom + marginLeft);
       },
 
       _setStaticHoveredStyles: function(item) {
