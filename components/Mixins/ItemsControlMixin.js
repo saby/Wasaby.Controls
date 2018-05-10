fdc224b2c99c4fbeccb8eee1e282e752a060e6cc
as.avramenko <as.avramenko@tensor.ru>
2018-05-10 16:00:58 +0300

    https://online.sbis.ru/opendoc.html?guid=0e882813-4c1e-44f3-a80f-e59decca8d94

--- components/Mixins/ItemsControlMixin.js
+++ components/Mixins/ItemsControlMixin.js
@@ -1347,15 +1347,16 @@ define('SBIS3.CONTROLS/Mixins/ItemsControlMixin', [
 
       _getInsertMarkupConfigICM: function(newItemsIndex, newItems, prevDomNode) {
          var
-             inside = true,
-             prepend = false,
-             container = this._getItemsContainer(),
-             projection = this._getItemsProjection(),
-             // При некоторых последовательностях изменений проекции индекс, определенный для вставки может стать устаревшим.
-             // Пересчитываем этот индекс: https://online.sbis.ru/opendoc.html?guid=fa48e9e0-e9ab-40fc-ae41-58e13f652b3a
-             newItemsIndex = projection.getIndex(newItems[0]),
-             prevItem = projection.at(newItemsIndex - 1),
-             lastItemsIndex = projection.getCount() - newItems.length;
+            inside = true,
+            prepend = false,
+            container = this._getItemsContainer(),
+            projection = this._getItemsProjection(),
+            prevItem, lastItemsIndex;
+         // При некоторых последовательностях изменений проекции индекс, определенный для вставки может стать устаревшим.
+         // Пересчитываем этот индекс: https://online.sbis.ru/opendoc.html?guid=fa48e9e0-e9ab-40fc-ae41-58e13f652b3a
+         newItemsIndex = projection.getIndex(newItems[0]);
+         prevItem = projection.at(newItemsIndex - 1);
+         lastItemsIndex = projection.getCount() - newItems.length;
 
          // TODO: тут зависимость от virtualscrolling, которой быть не должно
          if (this._virtualScrollController && this._virtualScrollController._currentWindow[1] > 0) {
