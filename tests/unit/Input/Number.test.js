514e22b52dad79050d2777facfdec417e0d5ce3d
ma.baranov1 <ma.baranov1@tensor.ru>
2018-05-15 14:50:24 +0300

    Ещё один кейс + тесты ___ https://online.sbis.ru/opendoc.html?guid=26d26487-cadb-4152-81ca-6fd7b8e07c22

--- tests/unit/Input/Number.test.js
+++ tests/unit/Input/Number.test.js
@@ -7,7 +7,7 @@ define(
       'use strict';
 
       describe('Controls.Input.Number', function() {
-         it('trimEmptyDecimals', function() {
+         it('trimEmptyDecimals ("000" at the ond of string)', function() {
             var
                res;
             Number._private.trimEmptyDecimals({
@@ -25,6 +25,46 @@ define(
             });
 
             assert.equal(res, '123');
+         });
+
+         it('trimEmptyDecimals ("." at the ond of string)', function() {
+            var
+               res;
+            Number._private.trimEmptyDecimals({
+               _options: {
+                  showEmptyDecimals: false
+               },
+               _numberViewModel: {
+                  getValue: function() {
+                     return '123.';
+                  },
+                  updateValue: function(processedVal) {
+                     res = processedVal;
+                  }
+               }
+            });
+
+            assert.equal(res, '123');
+         });
+
+         it('trimEmptyDecimals ("456" at the ond of string)', function() {
+            var
+               res;
+            Number._private.trimEmptyDecimals({
+               _options: {
+                  showEmptyDecimals: false
+               },
+               _numberViewModel: {
+                  getValue: function() {
+                     return '123.456';
+                  },
+                  updateValue: function(processedVal) {
+                     res = processedVal;
+                  }
+               }
+            });
+
+            assert.equal(res, '123.456');
          });
       });
    }
