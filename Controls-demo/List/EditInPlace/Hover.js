b635517de0aa72e275c1ca7f2d94630ceb1f2a57
as.zaycev <as.zaycev@tensor.ru>
2018-05-16 08:51:36 +0300

    https://online.sbis.ru/opendoc.html?guid=18781e78-93a0-4335-b5e4-aeef900a8c6e

--- Controls-demo/List/EditInPlace/Hover.js
+++ Controls-demo/List/EditInPlace/Hover.js
@@ -2,7 +2,6 @@ define('Controls-demo/List/EditInPlace/Hover', [
    'Core/Control',
    'tmpl!Controls-demo/List/EditInPlace/Hover',
    'WS.Data/Source/Memory',
-   'Controls/Validate/Validators/IsRequired',
    'css!Controls-demo/List/EditInPlace/EditInPlace'
 ], function(Control,
    template,
@@ -21,32 +20,15 @@ define('Controls-demo/List/EditInPlace/Hover', [
       }
    ];
 
-   var srcData2 = [
-      {
-         id: 1,
-         title: 'Record3'
-      },
-      {
-         id: 2,
-         title: 'Record4'
-      }
-   ];
-
    var Hover = Control.extend({
       _template: template,
       editingConfig: null,
-      _editOnClick: true,
       _enabled: true,
 
       _beforeMount: function() {
          this._viewSource = new MemorySource({
             idProperty: 'id',
             data: srcData
-         });
-
-         this._viewSource2 = new MemorySource({
-            idProperty: 'id',
-            data: srcData2
          });
       }
    });
