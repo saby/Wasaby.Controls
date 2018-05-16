2ef368ec14725e26b51709b34b1d07716856addb
av.zalyaev <av.zalyaev@tensor.ru>
2018-05-16 17:55:40 +0300

    https://online.sbis.ru/opendoc.html?guid=be12d127-db6d-455c-8679-0b8809d91108

--- File/LocalFileLink.ts
+++ File/LocalFileLink.ts
@@ -1,4 +1,18 @@
 /// <amd-module name="File/LocalFileLink" />
+type FileInfo = {
+    name: string;
+    size: number;
+    isDirectory: boolean;
+    type: string;
+}
+
+/**
+ * @typedef {Object} FileInfo Информация о файле
+ * @property {String} [name] Имя
+ * @property {String} [type]  Тип
+ * @property {Boolean} [isDirectory] Является ли директорией
+ * @property {Number} [size] Размер
+ */
 /**
  * Класс - обёртка над ссылкой на локальный файл
  * @class
@@ -7,13 +21,23 @@
  * @author Заляев А.В.
  */
 class LocalFileLink {
+    private readonly _fileInfo: Partial<FileInfo>;
     /**
      * @param {String} fileLink Ссылка на файл
      * @param {*} [meta] Дополнительные мета-данные
+     * @param {FileInfo} [fileInfo] Информация о файле
      * @constructor
      * @name File/LocalFileLink
      */
-    constructor(private fileLink: string, private meta?: any) {}
+    constructor(private fileLink: string, private meta?: any, fileInfo?: Partial<FileInfo>) {
+        this._fileInfo = fileInfo || {};
+        if (!this._fileInfo.name) {
+            /*
+             * Для ссылки на локальный файл, именем является часть пути до него после последнего слеша
+             */
+            this._fileInfo.name = fileLink.replace(/.*(\\|\/)/, "");
+        }
+    }
     /**
      * Возвращает ссылку на файл, находящийся на локальном устройстве
      * @return {String}
@@ -31,6 +55,20 @@ class LocalFileLink {
      */
     getMeta(): any {
         return this.meta;
+    }
+    /**
+     * Возвращает информацию о файле, если такая имеется
+     * @return {FileInfo}
+     */
+    getFileInfo(): Partial<FileInfo> {
+        return this._fileInfo || {};
+    }
+    /**
+     * Возвращает имя файла
+     * @return {String}
+     */
+    getName(): string {
+        return this._fileInfo.name || "";
     }
 }
 export  = LocalFileLink;
