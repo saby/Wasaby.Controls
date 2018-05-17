f818b2130aedaa6d046fdd4a90f58ee7c8fa4951
av.zalyaev <av.zalyaev@tensor.ru>
2018-05-17 14:33:28 +0300

    https://online.sbis.ru/opendoc.html?guid=b2811321-0b6e-4a9f-80a7-0a856f022bc5

--- File/ResourceGetter/DropArea.js
+++ File/ResourceGetter/DropArea.js
@@ -1,89 +1,49 @@
-"use strict";
 /// <amd-module name="File/ResourceGetter/DropArea" />
 /// <amd-dependency path="css!File/ResourceGetter/DropArea" />
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var IResourceGetterBase = require("File/ResourceGetter/Base");
-var Deferred = require("Core/Deferred");
-var random = require("Core/helpers/random-helpers");
-var ExtensionsHelper = require("File/utils/ExtensionsHelper");
-var replaceDir = require("File/ResourceGetter/DropArea/replaceDir");
-var OVERLAY_ID_PREFIX = "DropArea-";
-var OVERLAY_CLASS = 'DropArea_overlay';
-var GLOBAL = (function () { return this || (0, eval)('this'); }());
-var DOC = GLOBAL.document;
-var IS_SUPPORT = DOC && DOC.body;
-var areas = Object.create(null);
-var areaCount = 0;
-var dropAreas = [];
-/**
- * Возвращает значение свойства z-index у элемента
- * @param element
- * @return {Number}
- */
-var getZIndex = function (element) {
-    if (!element || element === DOC) {
-        return 0;
-    }
-    var index = parseInt(DOC.defaultView.getComputedStyle(element).getPropertyValue('z-index'));
-    if (isNaN(index)) {
-        return getZIndex(element.parentNode);
-    }
-    return index;
-};
-/**
- * Устанавливает позицию перекрывающего элемента над указанным
- * @param overlay
- * @param element
- */
-var setOverlayPosition = function (overlay, element) {
-    overlay.setAttribute('style', "\n        top: " + element.offsetTop + "px;\n        left: " + element.offsetLeft + "px;\n        width: " + element.offsetWidth + "px;\n        height: " + element.offsetHeight + "px;\n        z-index: " + (getZIndex(element) + 1) + ";\n    ");
-};
-// Создаёт обёртку над указанным элементом
-var createOverlay = function (area, uid) {
-    var overlay = DOC.createElement("div");
-    var element = area.element;
-    overlay.classList.add(OVERLAY_CLASS);
-    overlay.setAttribute('id', "" + OVERLAY_ID_PREFIX + uid);
-    setOverlayPosition(overlay, element);
-    var inner = DOC.createElement("div");
-    if (area.innerClass) {
-        inner.classList.add(area.innerClass || "");
-    }
-    inner.innerText = area.dragText;
-    overlay.appendChild(inner);
-    element.parentNode.insertBefore(overlay, element);
-    return overlay;
-};
-var getArea = function (element) {
-    var uid = element.getAttribute("id").replace(OVERLAY_ID_PREFIX, "");
-    return areas[uid];
-};
-var isDrag;
-// Удаление обёрточных элементов
-var dragEnd = function () {
-    isDrag = false;
-    dropAreas.forEach(function (overlay) {
-        overlay.parentNode.removeChild(overlay);
-    });
-    dropAreas = [];
-};
-/// region event handlers
-// обработчики событий drag&drop на обёртке
-var overlayHandlers = {
-    dragenter: function (event) {
-        if (dropAreas.indexOf(event.target) === -1) {
-            return;
+define("File/ResourceGetter/DropArea", ["require", "exports", "tslib", "File/ResourceGetter/Base", "Core/Deferred", "Core/helpers/random-helpers", "File/utils/ExtensionsHelper", "File/utils/getFilePreparer", "File/ResourceGetter/DropArea/replaceDir", "css!File/ResourceGetter/DropArea"], function (require, exports, tslib_1, IResourceGetterBase, Deferred, random, ExtensionsHelper, getFilePreparer, replaceDir) {
+    "use strict";
+    var OVERLAY_ID_PREFIX = "DropArea-";
+    var OVERLAY_CLASS = 'DropArea_overlay';
+    var GLOBAL = (function () { return this || (0, eval)('this'); }());
+    var DOC = GLOBAL.document;
+    var IS_SUPPORT = DOC && DOC.body;
+    var areas = Object.create(null);
+    var areaCount = 0;
+    var dropAreas = [];
+    /**
+     * Возвращает значение свойства z-index у элемента
+     * @param element
+     * @return {Number}
+     */
+    var getZIndex = function (element) {
+        if (!element || element === DOC) {
+            return 0;
         }
-<<<<<<<<< Temporary merge branch 1
+        var index = parseInt(DOC.defaultView.getComputedStyle(element).getPropertyValue('z-index'));
+        if (isNaN(index)) {
+            return getZIndex(element.parentNode);
+        }
+        return index;
+    };
+    /**
+     * Устанавливает позицию перекрывающего элемента над указанным
+     * @param overlay
+     * @param element
+     */
+    var setOverlayPosition = function (overlay, element) {
+        overlay.setAttribute('style', "\n        top: " + element.offsetTop + "px;\n        left: " + element.offsetLeft + "px;\n        width: " + element.offsetWidth + "px;\n        height: " + element.offsetHeight + "px;\n        z-index: " + (getZIndex(element) + 1) + ";\n    ");
+    };
+    // Создаёт обёртку над указанным элементом
+    var createOverlay = function (area, uid) {
+        var overlay = DOC.createElement("div");
+        var element = area.element;
+        overlay.classList.add(OVERLAY_CLASS);
+        overlay.setAttribute('id', "" + OVERLAY_ID_PREFIX + uid);
+        setOverlayPosition(overlay, element);
+        var inner = DOC.createElement("div");
+        if (area.innerClass) {
+            inner.classList.add(area.innerClass || "");
+        }
         inner.innerText = area.dragText;
         overlay.appendChild(inner);
         element.parentNode.insertBefore(overlay, element);
@@ -104,59 +64,41 @@ var overlayHandlers = {
         isDrag = false;
         dropAreas.forEach(function (overlay) {
             overlay.parentNode.removeChild(overlay);
-=========
-        event.target.querySelector('div').innerText = getArea(event.target).dropText;
-    },
-    dragover: function (event) {
-        // без этого нормально не убьётся стандартный обработчик drop
-        event.preventDefault();
-    },
-    dragleave: function (event) {
-        // игнорируем перемещение на внутренний элемент и обратно
-        if (event.relatedTarget && (event.relatedTarget.parentNode == this) ||
-            (dropAreas.indexOf(event.relatedTarget) !== -1)) {
-            return;
-        }
-        var target = dropAreas.indexOf(event.target) === -1 ? event.target.parentNode : event.target;
-        target.querySelector('div').innerText = getArea(target).dragText;
-    },
-    drop: function (event) {
-        event.preventDefault();
-        event.stopPropagation();
-        var target = dropAreas.indexOf(event.target) === -1 ? event.target.parentNode : event.target;
-        var area = getArea(target);
-        replaceDir(event.dataTransfer).addCallback(function (files) {
-            area.handler(files);
->>>>>>>>> Temporary merge branch 2
         });
-        dragEnd();
-    }
-};
-// обработчики событий drag&drop на документе
-var globalHandlers = {
-    dragenter: function (event) {
-        var dataTransfer = event.dataTransfer;
-        var type = (dataTransfer.types[0] || "").toLowerCase();
-        // Если обёртки готовы для всех элементов или событие не содержит файлы, то выходим
-        if (isDrag || !areaCount || type !== "files") {
-            return;
-        }
-        // иначе создаём обёртки и вешаем обработчики
-        isDrag = true;
-        for (var uid in areas) {
-            var overlay = createOverlay(areas[uid], uid);
-            dropAreas.push(overlay);
-            for (var event_1 in overlayHandlers) {
-                overlay.addEventListener(event_1, overlayHandlers[event_1]);
+        dropAreas = [];
+    };
+    /// region event handlers
+    // обработчики событий drag&drop на обёртке
+    var overlayHandlers = {
+        dragenter: function (event) {
+            if (dropAreas.indexOf(event.target) === -1) {
+                return;
             }
-        }
-    },
-    dragleave: function (event) {
-        // Если покинули область окна браузера, то убираем наши обёртки
-        if (!event.relatedTarget) {
+            event.target.querySelector('div').innerText = getArea(event.target).dropText;
+        },
+        dragover: function (event) {
+            // без этого нормально не убьётся стандартный обработчик drop
+            event.preventDefault();
+        },
+        dragleave: function (event) {
+            // игнорируем перемещение на внутренний элемент и обратно
+            if (event.relatedTarget && (event.relatedTarget.parentNode == this) ||
+                (dropAreas.indexOf(event.relatedTarget) !== -1)) {
+                return;
+            }
+            var target = dropAreas.indexOf(event.target) === -1 ? event.target.parentNode : event.target;
+            target.querySelector('div').innerText = getArea(target).dragText;
+        },
+        drop: function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            var target = dropAreas.indexOf(event.target) === -1 ? event.target.parentNode : event.target;
+            var area = getArea(target);
+            replaceDir(event.dataTransfer).addCallback(function (files) {
+                area.handler(files);
+            });
             dragEnd();
         }
-<<<<<<<<< Temporary merge branch 1
     };
     var isNeedOverlay = function (dataTransfer) {
         /**
@@ -184,131 +126,147 @@ var globalHandlers = {
                 dropAreas.push(overlay);
                 for (var event_1 in overlayHandlers) {
                     overlay.addEventListener(event_1, overlayHandlers[event_1]);
-=========
-    },
-    drop: function (event) {
-        // Если бросили файл в произвольную область - просто убераем наши обёртки
-        dragEnd();
-    }
-};
-/// endregion event handlers
-if (IS_SUPPORT) {
-    for (var event_2 in globalHandlers) {
-        DOC.addEventListener(event_2, globalHandlers[event_2]);
-    }
-}
-var OPTION = {
-    /**
-     * @cfg {Array.<String>} Список расширений выбираемых файлов
-     * <wiTag group="Управление">
-     * Помимо перечисления массива конкретных расширений файлов, можно также передать в массив значения:
-     * <ul>
-     *      <li> "image" - доступен выбор всех типов изображений</li>
-     *      <li> "audio" - доступен выбор всех типов аудио файлов</li>
-     *      <li> "video" - доступен выбор всех типов видео файлов</li>
-     * </ul>
-     * @example
-     * <pre>
-     *    extensions: ["image"]
-     *    // extensions: ["jpe","jpg","jpeg","gif","png","bmp","ico","svg","svgz","tif","tiff","pct","psd"]
-     * </pre>
-     * @name File/ResourceGetter/DropArea#extensions
-     */
-    extensions: null,
-    /**
-     * @cfg {HTMLElement} DOM элемент для перетаскивания файлов
-     * @name File/ResourceGetter/DropArea#element
-     */
-    element: null,
-    /**
-     * @cfg {Function} Обработчик события onDrop элемента. Позволяет получать ресурсы не ожидая вызова метода getFiles
-     * @name File/ResourceGetter/DropArea#ondrop
-     */
-    ondrop: function (files) { },
-    /**
-     * @cfg {String} Текст на внутреннем элементе во время перемещения файлов
-     * @name File/ResourceGetter/DropArea#dragText
-     */
-    dragText: rk("Переместите файлы сюда"),
-    /**
-     * @cfg {String} Текст на внутреннем элементе во время перемещения файлов непосредственно над ним
-     * @name File/ResourceGetter/DropArea#dropText
-     */
-    dropText: rk("Отпустите файлы"),
-    /**
-     * @cfg {String} Класс внутреннего элемента обёртки, содержащий текст
-     * @name File/ResourceGetter/DropArea#innerClass
-     */
-    innerClass: null
-};
-/**
- * Класс реализующий интерфейс IResourceGetter, позволяющий получать ресурсы через DragAndDrop
- * @class
- * @name File/ResourceGetter/DropArea
- * @extends File/IResourceGetter
- * @public
- * @author Заляев А.В.
- */
-var DropArea = /** @class */ (function (_super) {
-    __extends(DropArea, _super);
-    function DropArea(cfg) {
-        var _this = _super.call(this) || this;
-        _this.name = "DropArea";
-        _this._options = Object.assign({}, OPTION, cfg);
-        if (!_this._options.element) {
-            throw new Error('arguments "element" is required');
-        }
-        _this._extensions = new ExtensionsHelper(_this._options.extensions);
-        var _a = _this._options, element = _a.element, innerClass = _a.innerClass, dragText = _a.dragText, dropText = _a.dropText;
-        _this._uid = random.createGUID();
-        areas[_this._uid] = {
-            element: element, innerClass: innerClass, dragText: dragText, dropText: dropText,
-            handler: function (files) {
-                var result = _this._extensions.verifyAndReplace(files);
-                _this._options.ondrop.call(_this, result);
-                if (_this._selectDef) {
-                    _this._selectDef.callback(result);
-                    _this._selectDef = null;
->>>>>>>>> Temporary merge branch 2
                 }
             }
-        };
-        areaCount++;
-        return _this;
+        },
+        dragleave: function (event) {
+            // Если покинули область окна браузера, то убираем наши обёртки
+            if (!event.relatedTarget) {
+                dragEnd();
+            }
+        },
+        drop: function (event) {
+            // Если бросили файл в произвольную область - просто убераем наши обёртки
+            dragEnd();
+        }
+    };
+    /// endregion event handlers
+    if (IS_SUPPORT) {
+        for (var event_2 in globalHandlers) {
+            DOC.addEventListener(event_2, globalHandlers[event_2]);
+        }
     }
-    /**
-     * Возвращает Deferred, который стрельнёт, когда на указанный элемент будут перемещены файлы
-     * @return {Core/Deferred.<Array.<File/LocalFile | Error>>}
-     * @method
-     * @name File/ResourceGetter/DropArea#getFiles
-     * @see File/LocalFile
-     */
-    DropArea.prototype.getFiles = function () {
-        if (this.isDestroyed()) {
-            return Deferred.fail("Resource getter is destroyed");
-        }
-        if (this._selectDef) {
-            this._selectDef.cancel();
-        }
-        return this._selectDef = new Deferred();
+    var OPTION = {
+        /**
+         * @cfg {Array.<String>} Список расширений выбираемых файлов
+         * <wiTag group="Управление">
+         * Помимо перечисления массива конкретных расширений файлов, можно также передать в массив значения:
+         * <ul>
+         *      <li> "image" - доступен выбор всех типов изображений</li>
+         *      <li> "audio" - доступен выбор всех типов аудио файлов</li>
+         *      <li> "video" - доступен выбор всех типов видео файлов</li>
+         * </ul>
+         * @example
+         * <pre>
+         *    extensions: ["image"]
+         *    // extensions: ["jpe","jpg","jpeg","gif","png","bmp","ico","svg","svgz","tif","tiff","pct","psd"]
+         * </pre>
+         * @name File/ResourceGetter/DropArea#extensions
+         */
+        extensions: null,
+        /**
+         * @cfg {Number} Максимальный размер файла доступный для выбора (в МБ)
+         * @name File/ResourceGetter/DropArea#maxSize
+         */
+        maxSize: undefined,
+        /**
+         * @cfg {HTMLElement} DOM элемент для перетаскивания файлов
+         * @name File/ResourceGetter/DropArea#element
+         */
+        element: null,
+        /**
+         * @cfg {Function} Обработчик события onDrop элемента. Позволяет получать ресурсы не ожидая вызова метода getFiles
+         * @name File/ResourceGetter/DropArea#ondrop
+         */
+        ondrop: function (files) { },
+        /**
+         * @cfg {String} Текст на внутреннем элементе во время перемещения файлов
+         * @name File/ResourceGetter/DropArea#dragText
+         */
+        dragText: rk("Переместите файлы сюда"),
+        /**
+         * @cfg {String} Текст на внутреннем элементе во время перемещения файлов непосредственно над ним
+         * @name File/ResourceGetter/DropArea#dropText
+         */
+        dropText: rk("Отпустите файлы"),
+        /**
+         * @cfg {String} Класс внутреннего элемента обёртки, содержащий текст
+         * @name File/ResourceGetter/DropArea#innerClass
+         */
+        innerClass: null
     };
     /**
-     * Возможен ли выбор файлов
-     * @return {Core/Deferred.<Boolean>}
-     * @method
-     * @name File/ResourceGetter/DropArea#canExec
+     * Класс реализующий интерфейс IResourceGetter, позволяющий получать ресурсы через DragAndDrop
+     * @class
+     * @name File/ResourceGetter/DropArea
+     * @extends File/IResourceGetter
+     * @public
+     * @author Заляев А.В.
      */
-    DropArea.prototype.canExec = function () {
-        return Deferred.success(IS_SUPPORT && !this.isDestroyed());
-    };
-    DropArea.prototype.destroy = function () {
-        if (this._selectDef) {
-            this._selectDef.cancel();
+    var DropArea = /** @class */ (function (_super) {
+        tslib_1.__extends(DropArea, _super);
+        function DropArea(cfg) {
+            var _this = _super.call(this) || this;
+            _this.name = "DropArea";
+            _this._options = Object.assign({}, OPTION, cfg);
+            if (!_this._options.element) {
+                throw new Error('arguments "element" is required');
+            }
+            _this._extensions = new ExtensionsHelper(_this._options.extensions);
+            var _a = _this._options, element = _a.element, innerClass = _a.innerClass, dragText = _a.dragText, dropText = _a.dropText;
+            _this._uid = random.createGUID();
+            areas[_this._uid] = {
+                element: element, innerClass: innerClass, dragText: dragText, dropText: dropText,
+                handler: function (files) {
+                    var filePreparer = getFilePreparer({
+                        extensions: _this._extensions,
+                        maxSize: _this._options.maxSize
+                    });
+                    var result = filePreparer(files);
+                    _this._options.ondrop.call(_this, result);
+                    if (_this._selectDef) {
+                        _this._selectDef.callback(result);
+                        _this._selectDef = null;
+                    }
+                }
+            };
+            areaCount++;
+            return _this;
         }
-        delete areas[this._uid];
-        areaCount--;
-        _super.prototype.destroy.call(this);
-    };
+        /**
+         * Возвращает Deferred, который стрельнёт, когда на указанный элемент будут перемещены файлы
+         * @return {Core/Deferred.<Array.<File/LocalFile | Error>>}
+         * @method
+         * @name File/ResourceGetter/DropArea#getFiles
+         * @see File/LocalFile
+         */
+        DropArea.prototype.getFiles = function () {
+            if (this.isDestroyed()) {
+                return Deferred.fail("Resource getter is destroyed");
+            }
+            if (this._selectDef) {
+                this._selectDef.cancel();
+            }
+            return this._selectDef = new Deferred();
+        };
+        /**
+         * Возможен ли выбор файлов
+         * @return {Core/Deferred.<Boolean>}
+         * @method
+         * @name File/ResourceGetter/DropArea#canExec
+         */
+        DropArea.prototype.canExec = function () {
+            return Deferred.success(IS_SUPPORT && !this.isDestroyed());
+        };
+        DropArea.prototype.destroy = function () {
+            if (this._selectDef) {
+                this._selectDef.cancel();
+            }
+            delete areas[this._uid];
+            areaCount--;
+            _super.prototype.destroy.call(this);
+        };
+        return DropArea;
+    }(IResourceGetterBase));
     return DropArea;
-}(IResourceGetterBase));
-module.exports = DropArea;
+});
