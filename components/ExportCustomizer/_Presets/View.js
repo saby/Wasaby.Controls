8d8fe2aa64b01dd4ee937a9db360f4e85bf5dc6d
va.spirin <va.spirin@tensor.ru>
2018-05-17 17:50:14 +0700

    ExportCustomizer: Добавлены пользовательские пресеты
    1174986512 https://online.sbis.ru/opendoc.html?guid=36c73e65-003b-4f5b-91ea-10f52fb5410b

--- components/ExportCustomizer/_Presets/View.js
+++ components/ExportCustomizer/_Presets/View.js
@@ -9,17 +9,18 @@ define('SBIS3.CONTROLS/ExportCustomizer/_Presets/View',
    [
       'SBIS3.CONTROLS/CompoundControl',
       'WS.Data/Collection/RecordSet',
+      'WS.Data/Di',
       'tmpl!SBIS3.CONTROLS/ExportCustomizer/_Presets/View',
       'tmpl!SBIS3.CONTROLS/ExportCustomizer/_Presets/tmpl/item',
       'tmpl!SBIS3.CONTROLS/ExportCustomizer/_Presets/tmpl/footer',
       'css!SBIS3.CONTROLS/ExportCustomizer/_Presets/View'
    ],
 
-   function (CompoundControl, RecordSet, dotTplFn) {
+   function (CompoundControl, RecordSet, Di, dotTplFn) {
       'use strict';
 
       /**
-       * @typedef {object} ExportPreset Тип, содержащий информацию о преустановленных настройках экспорта
+       * @typedef {object} ExportPreset Тип, содержащий информацию о предустановленных настройках экспорта
        * @property {string|number} id Идентификатор пресета
        * @property {string} title Отображаемое название пресета
        * @property {Array<string>} fieldIds Список привязки колонок в экспортируемом файле к полям данных
@@ -31,6 +32,15 @@ define('SBIS3.CONTROLS/ExportCustomizer/_Presets/View',
        * @property {Array<string>} fieldIds Список привязки колонок в экспортируемом файле к полям данных
        * @property {string} fileUuid Uuid шаблона форматирования эксель-файла
        */
+
+
+
+      /**
+       * Имя регистрации объекта, предоставляющего методы загрузки и сохранения пользовательских пресетов, в инжекторе зависимостей
+       * @private
+       * @type {string}
+       */
+      var EXPORT_PRESETS_LOADER_NAME = 'ExportPresets.Loader';
 
 
 
@@ -55,19 +65,18 @@ define('SBIS3.CONTROLS/ExportCustomizer/_Presets/View',
                 */
                selectedId: null
             },
+            // Объект, предоставляющий методы загрузки и сохранения пользовательских пресетов
+            _storage: null,
+            // Список пользовательских пресетов
+            _customs: null,
             // Контрол выбора пресета
             _selector: null
          },
 
          _modifyOptions: function () {
             var options = View.superclass._modifyOptions.apply(this, arguments);
-            var list = options.statics;
-            if (list && list.length) {
-               options._items = new RecordSet({
-                  rawData: list,
-                  idProperty: 'id'
-               });
-            }
+            options._items = this._makeItems(options);
+            options._selectedId = this._makeSelectedId(options);
             return options;
          },
 
@@ -76,17 +85,82 @@ define('SBIS3.CONTROLS/ExportCustomizer/_Presets/View',
 
          init: function () {
             View.superclass.init.apply(this, arguments);
+            if (Di.isRegistered(EXPORT_PRESETS_LOADER_NAME)) {
+               this._storage = Di.resolve(EXPORT_PRESETS_LOADER_NAME);
+            }
             this._selector = this.getChildControlByName('controls-ExportCustomizer-Presets-View__button');
             this._bindEvents();
+            if (this._storage) {
+               this._storage.load(this._options.namespace).addCallback(function (presets) {
+                  this._customs = presets;
+                  var options = this._options;
+                  var selector = this._selector;
+                  var items = options._items = this._makeItems(options);
+                  selector.setItems(items);
+                  this._updateSelectorListOptions('items', items);
+                  var selectedId = this._makeSelectedId(options);
+                  selector.setSelectedKeys([selectedId]);
+                  this._updateSelectorListOptions('selectedKey', selectedId);
+               }.bind(this));
+            }
          },
 
          _bindEvents: function () {
             this.subscribeTo(this._selector, 'onSelectedItemsChange', function (evtName, ids, changes) {
                var selectedId = ids[0];
                this._options.selectedId = selectedId;
-               this._selector.getProperty('dictionaries')[0].componentOptions.selectedKey = selectedId;
+               this._updateSelectorListOptions('selectedKey', selectedId);
                this.sendCommand('subviewChanged');
             }.bind(this));
+         },
+
+         /**
+          * Приготовить список элементов для списочного контрола
+          *
+          * @protected
+          * @param {object} options Опции компонента
+          * @return {Array<object>}
+          */
+         _makeItems: function (options) {
+            var list = [];
+            var statics = options.statics;
+            if (statics && statics.length) {
+               list.push.apply(list, statics);
+            }
+            var customs = this._customs;
+            if (customs && customs.length) {
+               list.push.apply(list, customs);
+            }
+            return !list.length ? null : new RecordSet({
+               rawData: list,
+               idProperty: 'id'
+            });
+         },
+
+         /**
+          * Приготовить идентификатор выбранного элемента для списочного контрола
+          *
+          * @protected
+          * @param {object} options Опции компонента
+          * @return {string|number}
+          */
+         _makeSelectedId: function (options) {
+            var items = options._items;
+            if (items && items.getCount()) {
+               var selectedId = options.selectedId;
+               return selectedId && items.getRecordById(selectedId) ? selectedId : items.at(0).getId();
+            }
+         },
+
+         /**
+          * Обновить опцию списочного контрола внутри селектора
+          *
+          * @protected
+          * @param {string} name Имя опции
+          * @param {*} value Значение опции
+          */
+         _updateSelectorListOptions: function (name, value) {
+            this._selector.getProperty('dictionaries')[0].componentOptions[name] = value;
          },
 
          /**
