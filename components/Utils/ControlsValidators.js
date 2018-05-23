31a55fa3b17a45d5a64e1b4f5e671a9b0b903bea
kraynovdo <do.kraynov@tensor.ru>
2018-05-23 09:15:56 +0300

    https://online.sbis.ru/opendoc.html?guid=e23b84b3-fb2c-4665-b602-b174a94bca72

--- components/Utils/ControlsValidators.js
+++ components/Utils/ControlsValidators.js
@@ -2,13 +2,14 @@
  * Created by am.gerasimov on 01.02.2016.
  */
 define('SBIS3.CONTROLS/Utils/ControlsValidators', [
+   'SBIS3.CONTROLS/Utils/InnUtils',
    'Core/core-instance',
    'Core/IoC',
    'Controls/Validate/Validators/IsEmail',
    'Controls/Validate/Validators/IsRequired',
    'SBIS3.CONTROLS/Utils/DateUtil',
    'i18n!SBIS3.CONTROLS/Utils/ControlsValidators'
-],function(cInstance, IoC, IsEmail, IsRequired, DateUtil) {
+],function(InnUtils, cInstance, IoC, IsEmail, IsRequired, DateUtil) {
 
    'use strict';
 
@@ -19,36 +20,7 @@ define('SBIS3.CONTROLS/Utils/ControlsValidators', [
          }
       };
 
-      return validateInn.call(obj, innLen);
-   }
-
-   /**
-    * Проверяет строку на соответствие формату идентификационного номера налогоплательщика (ИНН).
-    * @param {Number|String} [innLen] Устанавливает длину строки, которой должен соответствовать введённый ИНН.
-    * @returns {Boolean|String}
-    * <ol>
-    *    <li>В случае прохождения валидации возвращает true.</li>
-    *    <li>В случае не прохождения валидации возвращает сообщение "ИНН должен состоять из 10 или 12 цифр".</li>
-    * </ol>
-    * @see innCheckValue
-    * @see innCheckSum
-    */
-   function validateInn(innLen) {
-      innLen = parseInt(innLen, 10) || 0;
-      var value = getValueForValidation(this.getValue());
-      if(value === ''){
-         return true;
-      }
-
-      var
-         valLen = ('' + value).length,
-         isNumbers = (/^([0-9]+)$/).test(value),
-         isCorrectLength = (innLen > 0) ?  (('' + value).length == innLen) : (valLen === 10 || valLen === 12);
-      if(!isNumbers || !isCorrectLength){
-         return rk('ИНН должен состоять из') + ' ' + (innLen > 0 ? innLen : rk('10 или 12')) + ' ' + rk('цифр');
-      }
-
-      return obj.innCheckSum.apply( this );
+      return InnUtils.inn.call(obj, innLen);
    }
 
 
