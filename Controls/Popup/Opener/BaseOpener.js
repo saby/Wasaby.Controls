1f189bfccf00bec62470a1e913eb9f8d73788a54
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-05-23 20:19:47 +0300

    https://online.sbis.ru/opendoc.html?guid=f55d0380-87f0-48c3-9e79-230dc05ce1a0

--- Controls/Popup/Opener/BaseOpener.js
+++ Controls/Popup/Opener/BaseOpener.js
@@ -60,7 +60,7 @@ define('Controls/Popup/Opener/BaseOpener',
                return (new Deferred()).callback(config.template);
             } else if (requirejs.defined(config.template)) {
                return (new Deferred()).callback(requirejs(config.template));
-            } else if (!this._openerListDeferred) {
+            } else if (!this._openerListDeferred || this._openerListDeferred.isReady()) {
                this._openerListDeferred = new Deferred();
                requirejs([config.template], function(template) {
                   this._openerListDeferred.callback(template);
