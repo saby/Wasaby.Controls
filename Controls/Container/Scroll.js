b15a66edfb23d457fda031761caf74378bca2c01
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-05-23 12:28:38 +0300

    https://online.sbis.ru/opendoc.html?guid=71961123-faa4-4f9a-a676-f4590e38c6a1

--- Controls/Container/Scroll.js
+++ Controls/Container/Scroll.js
@@ -25,16 +25,30 @@ define('Controls/Container/Scroll',
        * @control
        * @public
        * @category Container
-       *
+       */
+
+      /**
+       * @event scroll Scrolling content.
+       * @param {SyntheticEvent} eventObject.
+       * @param {Number} scrollTop Top position of content relative to container.
+       */
+
+      /**
        * @name Controls/Container/Scroll#content
        * @cfg {Content} Container contents.
-       *
+       */
+
+      /**
        * @name Controls/Container/Scroll#shadowVisible
        * @cfg {Boolean} Whether shadow should be shown (when content doesn't fit).
-       *
+       */
+
+      /**
        * @name Controls/Container/Scroll#scrollbarVisible
        * @cfg {Boolean} Whether scrollbar should be shown.
-       *
+       */
+
+      /**
        * @name Controls/Container/Scroll#style
        * @cfg {String} Color scheme (colors of the shadow and scrollbar).
        * @variant normal Default theme (for bright backgrounds).
