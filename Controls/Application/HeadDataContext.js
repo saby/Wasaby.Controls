0e6d19caaebbfd34631a90b05dc571e6f9c30ab9
nv.belotelov <nv.belotelov@tensor.ru>
2018-05-25 09:45:01 +0300

    https://online.sbis.ru/opendoc.html?guid=b9b6ba1e-3746-4ad7-9349-307dd0e506e8

--- Controls/Application/HeadDataContext.js
+++ Controls/Application/HeadDataContext.js
@@ -2,7 +2,153 @@ define('Controls/Application/HeadDataContext', [
    'Core/DataContext',
    'Core/Deferred',
    'Core/IoC'
-], function(DataContext, Deferred) {
+], function(DataContext, Deferred, IoC) {
+   var logger = IoC.resolve('ILogger');
+   var bundles;
+   try {
+      bundles = require('json!WS.Core/ext/requirejs/bundles');
+   } catch (e) {
+      bundles = {};
+   }
+
+   var modDeps;
+   try {
+      modDeps = require('json!resources/module-dependencies');
+   } catch(e) {
+      modDeps = { links: {}, nodes: {} };
+   }
+
+   function hasCss(bundle) {
+      if (!Array.isArray(bundle)) {
+         return false;
+      }
+      for (var i = 0; i < bundle.length; i++) {
+         if (~bundle[i].indexOf('css!')) {
+            return true;
+         }
+      }
+      return false;
+   }
+
+   function getDependentCss(jsLinks) {
+      function cropBundleName(s) {
+         if (s.indexOf('.min')) {
+            var cropped = s.split('.min')[0];
+         } else {
+            cropped = s;
+         }
+         if (cropped.indexOf('/') === 0) {
+            cropped = cropped.slice(1);
+         }
+         return cropped;
+      }
+
+      var dependCssLinks = {};
+      var bundleNames = Object.keys(bundles);
+      var croppedBundleNames = [];
+      for (var i = 0; i < bundleNames.length; i++) {
+         croppedBundleNames.push(cropBundleName(bundleNames[i]));
+      }
+      for (var key in jsLinks) {
+         if(jsLinks.hasOwnProperty(key)) {
+            var croppedJsLink = cropBundleName(key);
+            var needLoadCssBundle = false;
+            var bundleIdx = croppedBundleNames.indexOf(croppedJsLink);
+            if (bundleIdx !== -1) {
+               var name = bundleNames[bundleIdx];
+               if (hasCss(bundles[name])) {
+                  needLoadCssBundle = true;
+               }
+            }
+            if (needLoadCssBundle) {
+               var jsLinkNoExtension = key.split('.js')[0];
+               dependCssLinks[jsLinkNoExtension] = true;
+            }
+         }
+      }
+      return dependCssLinks;
+   }
+
+   function checkForBundles(allDeps) {
+      function removeAllFromObject(obj, els) {
+         for (var i = 0; i < els.length; i++) {
+            delete obj[els[i]];
+         }
+      }
+      function findBundle(str) {
+         for (var key in bundles) {
+            if (~bundles[key].indexOf(str)) {
+               return key;
+            }
+         }
+         return null;
+      }
+      var jsBundles = {};
+      for (var key in allDeps) {
+         if (allDeps.hasOwnProperty(key)) {
+            var bundleName = findBundle(key);
+            if (bundleName) {
+               removeAllFromObject(allDeps, bundles[bundleName]);
+               jsBundles[bundleName] = true;
+            }
+         }
+      }
+      return jsBundles;
+   }
+
+   function fixLink(link, type) {
+      if (type === 'css') {
+         if (link.indexOf('css!') === 0) {
+            link = link.split('css!')[1];
+         }
+         link += '.css';
+      } else {
+         link += '.js';
+      }
+      if (link.indexOf('resources/') !== 0) {
+         link = 'resources/' + link;
+      }
+      return link;
+   }
+
+   function collectDependencies(deps, callback) {
+      function recursiveWalker(allDeps, curNodeDeps) {
+         if (curNodeDeps && curNodeDeps.length) {
+            for (var i = 0; i < curNodeDeps.length; i++) {
+               var node = curNodeDeps[i];
+               if (!allDeps[node]) {
+                  var nodeDeps = modDeps.links[node];
+                  allDeps[node] = true;
+                  recursiveWalker(allDeps, nodeDeps);
+               }
+            }
+         }
+      }
+
+      var allDeps = {};
+      recursiveWalker(allDeps, deps);
+      var jsBundles = checkForBundles(allDeps); //Find all bundles, and removes dependencies that are included in bundles
+      var cssBundles = getDependentCss(jsBundles);
+      var files = { js: [], css: [] };
+      for (var key in jsBundles) {
+         if(jsBundles.hasOwnProperty(key)) {
+            files.js.push(fixLink(key, 'js'));
+         }
+      }
+      for (var key in cssBundles) {
+         if(cssBundles.hasOwnProperty(key)) {
+            files.css.push(fixLink(key, 'css'));
+         }
+      }
+      for (var key in allDeps) {
+         if (key.indexOf('css!') === 0) {
+            files.css.push(fixLink(key, 'css'));
+         } else if (key.indexOf('tmpl!') !== 0) {
+            files.js.push(fixLink(key, 'js'));
+         }
+      }
+      return callback(undefined, files);
+   }
 
    return DataContext.extend({
       pushDepComponent: function(componentName) {
@@ -11,10 +157,9 @@ define('Controls/Application/HeadDataContext', [
       pushWaiterDeferred: function(def) {
          var self = this;
          this.waiterDef = def;
-         var modpacker = require('wsmodPacker');
          this.waiterDef.addCallback(function() {
             var components = Object.keys(self.depComponentsMap);
-            modpacker.collectDependencies(components, this.theme || '', false, function(err, files) {
+            collectDependencies(components, function(err, files) {
                if (err) {
                   self.err = err;
                   return;
