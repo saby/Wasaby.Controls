b63c97ae0b7c51e59ecfe148f4ae892f6ff5fe71
dv.zuev <dv.zuev@tensor.ru>
2018-05-25 11:53:54 +0300

    https://online.sbis.ru/opendoc.html?guid=a793cf37-3ba3-48d5-b6b2-c0601c186b5b

--- Controls/Application.js
+++ Controls/Application.js
@@ -97,18 +97,13 @@ define('Controls/Application',
             if (!receivedState) {
                receivedState = {};
             }
-            self.cssLinks = receivedState.cssLinks || (context.AppData ? context.AppData.cssLinks : cfg.cssLinks);
             self.wsRoot = receivedState.wsRoot || (context.AppData ? context.AppData.wsRoot : cfg.wsRoot);
             self.resourceRoot = receivedState.resourceRoot || (context.AppData ? context.AppData.resourceRoot : cfg.resourceRoot);
-            self.jsLinks = receivedState.jsLinks || (context.AppData ? context.AppData.jsLinks : cfg.jsLinks);
-            self.cssBundles = receivedState.cssBundles || (context.AppData ? context.AppData.cssBundles : cfg.cssBundles);
             self.BodyClasses = BodyClasses;
 
             if (receivedState && context.AppData) {
-               context.AppData.cssLinks = self.cssLinks;
                context.AppData.wsRoot = self.wsRoot;
                context.AppData.resourceRoot = self.resourceRoot;
-               context.AppData.jsLinks = self.jsLinks;
                context.AppData.application = self.application;
             }
             
@@ -117,9 +112,6 @@ define('Controls/Application',
              * роутинга и с ним же надо восстанавливаться на клиенте.
              */
             def.callback({
-               jsLinks: self.jsLinks,
-               cssLinks: self.cssLinks,
-               cssBundles: self.cssBundles,
                title: self.title,
                wsRoot: self.wsRoot,
                resourceRoot: self.resourceRoot,
