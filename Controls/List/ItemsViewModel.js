3c886f72181a91c8e948e83f4b446b4cd364d3f0
as.zaycev <as.zaycev@tensor.ru>
2018-05-30 09:04:47 +0300

    замечания https://online.sbis.ru/opendoc.html?guid=18781e78-93a0-4335-b5e4-aeef900a8c6e

--- Controls/List/ItemsViewModel.js
+++ Controls/List/ItemsViewModel.js
@@ -175,10 +175,6 @@ define('Controls/List/ItemsViewModel',
             this._items.setEventRaising(true, true);
          },
 
-         _prepareDisplayItemForAdd: function(item) {
-            return ItemsUtil.getDefaultDisplayItem(this._display, item);
-         },
-
          destroy: function() {
             ItemsViewModel.superclass.destroy.apply(this, arguments);
             this._display = null;
