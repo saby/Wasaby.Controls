17d12cda8fa196a30317bb65053fe080d4197225
va.spirin <va.spirin@tensor.ru>
2018-05-30 12:41:17 +0700

    ExportCustomizer: Добавлена ссылка-меню (2+)
    1175354787 https://online.sbis.ru/opendoc.html?guid=bade0ee6-422d-4b43-9876-280bb414d0b9

--- components/ExportCustomizer/MenuLink.js
+++ components/ExportCustomizer/MenuLink.js
@@ -7,12 +7,18 @@
  */
 define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
    [
-      'SBIS3.CONTROLS/Menu/MenuLink'/*^^^,
+      'Core/core-extend',
+      'Core/Deferred',
+      'SBIS3.CONTROLS/Menu/MenuLink',
+      'WS.Data/Di',
+      'WS.Data/Entity/ObservableMixin',
+      'WS.Data/Source/DataSet',
+      'WS.Data/Source/ISource'/*^^^,
       'tmpl!SBIS3.CONTROLS/ExportCustomizer/MenuLink',
       'css!SBIS3.CONTROLS/ExportCustomizer/MenuLink'*/
    ],
 
-   function (MenuLink/*^^^, tmpl*/) {
+   function (CoreExtend, Deferred, MenuLink, Di, ObservableMixin, DataSet, ISource/*^^^, tmpl*/) {
       'use strict';
 
       /**
@@ -23,8 +29,7 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
        * @property {string} fileUuid Uuid шаблона форматирования эксель-файла
        */
 
-      var Link = MenuLink.extend(/**@lends SBIS3.CONTROLS/ExportCustomizer/MenuLink.prototype*/ {
-
+      var ExportMenuLink = MenuLink.extend(/**@lends SBIS3.CONTROLS/ExportCustomizer/MenuLink.prototype*/ {
          /*^^^_dotTplFn: tmpl,*/
          $protected: {
             _options: {
@@ -40,28 +45,110 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
          },
 
          _modifyOptions: function () {
-            var options = Link.superclass._modifyOptions.apply(this, arguments);
-            options.dataSource = new LinkDataSource({
-               staticPresets: options.staticPresets,
-               presetNamespace: options.presetNamespace
+            var options = ExportMenuLink.superclass._modifyOptions.apply(this, arguments);
+            options.dataSource = new ExportMenuLinkDataSource({
+               statics: options.staticPresets,
+               namespace: options.presetNamespace
             });
             return options;
-         },/*^^^*/
+         }/*^^^,*/
 
          /*^^^$constructor: function () {
          },*/
 
          /*^^^init: function () {
-            Link.superclass.init.apply(this, arguments);
+          ExportMenuLink.superclass.init.apply(this, arguments);
          },*/
 
          /*destroy: function () {
-            Link.superclass.destroy.apply(this, arguments);
+          ExportMenuLink.superclass.destroy.apply(this, arguments);
          }*/
       });
 
 
 
-      return Link;
+      /**
+       * Имя регистрации объекта, предоставляющего методы загрузки и сохранения пользовательских пресетов, в инжекторе зависимостей
+       * @private
+       * @type {string}
+       */
+      var _DI_STORAGE_NAME = 'ExportPresets.Loader';
+
+      /**
+       * Простая оболочка над SBIS3.ENGINE/Controls/ExportPresets/Loader для имплементации интерфейса WS.Data/Source/ISource
+       * @private
+       * @type {WS.Data/Source/ISource}
+       */
+      var ExportMenuLinkDataSource = CoreExtend.extend({}, [ISource, ObservableMixin], /** @lends SBIS3.CONTROLS/ExportCustomizer/ExportMenuLinkDataSource.prototype */{
+         $protected: {
+            _options: {
+               statics: null,
+               namespace: null,
+               navigationType: 'Page'
+            },
+            _storage: null
+         },
+
+         $constructor: function () {
+            this._publish('onBeforeProviderCall');//^^^
+         },
+
+         init: function () {
+            DataSource.superclass.init.apply(this, arguments);
+            if (Di.isRegistered(_DI_STORAGE_NAME)) {
+               this._storage = Di.resolve(_DI_STORAGE_NAME);
+            }
+         },
+
+         /**
+          * Возвращает дополнительные настройки источника данных.
+          * @return {Object}
+          */
+         getOptions: function () {
+            return this._options;//^^^
+         },
+
+         /**
+          * Возвращает дополнительные настройки источника данных.
+          * @param {Object}
+          */
+         setOptions: function (options) {
+            this._options = options;//^^^
+         },
+
+         /**
+          * Выполняет запрос на выборку
+          * @param {WS.Data/Query/Query} [query] Запрос
+          * @return {Core/Deferred} Асинхронный результат выполнения. В колбэке придет {@link WS.Data/Source/DataSet}.
+          */
+         query: function (query) {
+            this._notify('onBeforeProviderCall');//^^^
+            var options = this._options;
+            return (this._storage ? this._storage.load(options.namespace) : Deferred.success(null)).addCallback(function (presets) {
+               var items = [];
+               var statics = options.statics;
+               if (statics && statics.length) {
+                  items.push.apply(items, statics);
+               }
+               if (presets && presets.length) {
+                  items.push.apply(items, presets);
+               }
+               items.push({id:null, title:rk('Создать новый шаблон', 'НастройщикЭкспорта')});
+               return new DataSet({
+                  rawData: {
+                     items: items,
+                     more: false
+                  },
+                  idProperty: 'id',
+                  itemsProperty: 'items',
+                  totalProperty: 'more'
+               });
+            });
+         },
+      });
+
+
+
+      return ExportMenuLink;
    }
 );
