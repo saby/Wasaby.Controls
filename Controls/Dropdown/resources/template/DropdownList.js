4a452ce9a8a4ca0e82091592b09650e01bee747f
Герасимов А.М <am.gerasimov@tensor.ru>
2018-06-05 19:19:18 +0300

    Update DropdownList.js

--- Controls/Dropdown/resources/template/DropdownList.js
+++ Controls/Dropdown/resources/template/DropdownList.js
@@ -116,15 +116,15 @@ define('Controls/Dropdown/resources/template/DropdownList',
                   this._notify('sendResult', [result]);
             }
          },
-         _itemClickHandler: function(event, item, flag) { //todo нужно обсудить
+         _itemClickHandler: function(event, item, pinClicked) { //todo нужно обсудить
             var result = {
                action: 'itemClick',
                event: event,
-               data: [item, flag]
+               data: [item, pinClicked]
             };
             
-            //flag means that pin button was clicked
-            if (flag) {
+            // means that pin button was clicked
+            if (pinClicked) {
                event.stopPropagation();
             }
             this._notify('sendResult', [result]);
