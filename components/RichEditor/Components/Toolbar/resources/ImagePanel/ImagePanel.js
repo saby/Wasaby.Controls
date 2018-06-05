f33ebf3ad5e33d228d0486811ec8e1020fc0bc0e
av.zalyaev <av.zalyaev@tensor.ru>
2018-06-05 16:55:13 +0300

    https://online.sbis.ru/opendoc.html?guid=41c16059-a45f-4df4-ac0a-d0659344b85e

--- components/RichEditor/Components/Toolbar/resources/ImagePanel/ImagePanel.js
+++ components/RichEditor/Components/Toolbar/resources/ImagePanel/ImagePanel.js
@@ -5,7 +5,6 @@ define('SBIS3.CONTROLS/RichEditor/Components/Toolbar/resources/ImagePanel/ImageP
       'Lib/Mixins/LikeWindowMixin',
       'tmpl!SBIS3.CONTROLS/RichEditor/Components/Toolbar/resources/ImagePanel/ImagePanel',
       "Core/EventBus",
-      'SbisFile/DIUploader/RichEditor',
       'css!SBIS3.CONTROLS/RichEditor/Components/Toolbar/resources/ImagePanel/ImagePanel'
    ], function(CompoundControl, PopupMixin, LikeWindowMixin, dotTplFn, EventBus) {
       'use strict';
