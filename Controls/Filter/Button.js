7956efe26c8066034f33e7fc29dacd46f137cb72
am.gerasimov <am.gerasimov@tensor.ru>
2018-06-05 14:18:28 +0300

    https://online.sbis.ru/opendoc.html?guid=338b62d4-dade-4edb-96c2-65caf7f6a8bd

--- Controls/Filter/Button.js
+++ Controls/Filter/Button.js
@@ -11,9 +11,9 @@ define('Controls/Filter/Button',
       'WS.Data/Type/descriptor',
       'css!Controls/Filter/Button/Button'
    ],
-
+   
    function(Control, template, Chain, Utils, FilterContextField, types) {
-
+      
       /**
        * @class Controls/Filter/Button
        * @extends Core/Control
@@ -21,84 +21,84 @@ define('Controls/Filter/Button',
        * @control
        * @public
        */
-
+      
       'use strict';
-   
+      
       var _private = {
          getFilterButtonCompatible: function(self) {
             var result = new Deferred();
             requirejs(['Controls/Popup/Compatible/Layer'], (function(Layer) {
-                  Layer.load().addCallback(function(res) {
-                     requirejs(['Controls/Filter/Button/_FilterCompatible'], function(_FilterCompatible) {
-                        if (!self._filterCompatible) {
-                           self._filterCompatible = new _FilterCompatible({
-                              filterButton: self,
-                              filterButtonOptions: self._options
-                           });
-                        }
-                        result.callback(self._filterCompatible);
-                     });
-                     return res;
+               Layer.load().addCallback(function(res) {
+                  requirejs(['Controls/Filter/Button/_FilterCompatible'], function(_FilterCompatible) {
+                     if (!self._filterCompatible) {
+                        self._filterCompatible = new _FilterCompatible({
+                           filterButton: self,
+                           filterButtonOptions: self._options
+                        });
+                     }
+                     result.callback(self._filterCompatible);
                   });
-               })
+                  return res;
+               });
+            })
             );
             return result;
          },
-
+         
          getText: function(items) {
             var textArr = [];
-
+            
             Chain(items).each(function(item) {
                var textValue = Utils.getItemPropertyValue(item, 'textValue');
-
+               
                if (textValue) {
                   textArr.push(textValue);
                }
             });
-
+            
             return textArr.join(', ');
          },
-
+         
          resolveItems: function(self, items) {
             self._items = items;
             self._text = _private.getText(items);
          }
       };
-
+      
       var FilterButton = Control.extend({
-
+         
          _template: template,
          _oldPanelOpener: null,
          _text: '',
          _historyId: null,
-
-         constructor: function(config) {
+         
+         constructor: function() {
             FilterButton.superclass.constructor.apply(this, arguments);
             this._onFilterChanged = this._onFilterChanged.bind(this);
          },
-
+         
          _beforeUpdate: function(options) {
             if (this._options.items !== options.items) {
                _private.resolveItems(this, options.items);
             }
          },
-
+         
          _beforeMount: function(options) {
             if (options.items) {
                _private.resolveItems(this, options.items);
             }
          },
-
+         
          _getFilterState: function() {
             return this._options.readOnly ? 'disabled' : 'default';
          },
-
+         
          _clearClick: function() {
             _private.getFilterButtonCompatible(this).addCallback(function(panelOpener) {
                panelOpener.clearFilter();
             });
          },
-
+         
          _openFilterPanel: function() {
             if (!this._options.readOnly) {
                /* if template - show old component */
@@ -122,19 +122,19 @@ define('Controls/Filter/Button',
                }
             }
          },
-
+         
          _onFilterChanged: function(data) {
             this._notify('filterChanged', [data.filter]);
             this._notify('itemsChanged', [data.items]);
          }
       });
-
+      
       FilterButton.getDefaultOptions = function() {
          return {
             filterAlign: 'right'
          };
       };
-
+      
       FilterButton.getOptionsTypes = function() {
          return {
             itemTemplate: types(Object),
@@ -143,6 +143,6 @@ define('Controls/Filter/Button',
             additionalTemplateProperty: types(String)
          };
       };
-
+      
       return FilterButton;
    });
