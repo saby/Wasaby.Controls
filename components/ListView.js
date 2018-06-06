513e5f23b432d5bf130f6f404824fa4810340195
as.avramenko <as.avramenko@tensor.ru>
2018-06-06 14:51:56 +0300

    https://online.sbis.ru/opendoc.html?guid=beb3c7a6-9249-4616-98a9-0cc5a650e2cc

--- components/ListView.js
+++ components/ListView.js
@@ -1532,7 +1532,7 @@ define('SBIS3.CONTROLS/ListView',
                      itemsProjection.getRoot().getContents().get(recordItems.getIdProperty()) == id);
                },
                siblingItem;
-            if (index === -1 && id && isRootId(id)) {
+            if (index === -1 && typeof id !== 'undefined' && isRootId(id)) {
                index = 0;
             }
             if (isNext) {
