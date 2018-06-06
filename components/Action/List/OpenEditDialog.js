84c6e491a8bcdca9f8adbf9eec2da6c4808dc334
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-06-06 17:27:50 +0300

    https://online.sbis.ru/opendoc.html?guid=6f9fe91c-3c32-4d3d-9d60-0cfb612e2452

--- components/Action/List/OpenEditDialog.js
+++ components/Action/List/OpenEditDialog.js
@@ -256,11 +256,8 @@ define('SBIS3.CONTROLS/Action/List/OpenEditDialog', [
                def.addErrback(function (error) {
                   self._finishExecuteDeferred(error);
                   return error;
-               }).addBoth(function (record) {
-                  self._hideLoadingIndicator();
-                  return record;
                });
-            })
+            });
          } else {
             OpenEditDialog.superclass._createComponent.call(this, config, meta)
          }
@@ -686,6 +683,10 @@ define('SBIS3.CONTROLS/Action/List/OpenEditDialog', [
                onAfterClose: function (e, meta) {
                   self._notifyOnExecuted(meta);
                   self._clearVariables();
+               },
+               onBeforeShow: function() {
+                  self._hideLoadingIndicator();
+                  self._notify('onBeforeShow', this);
                },
                //При множественном клике панель может начать закрываться раньше, чем откроется, в этом случае
                //onAfterClose не будет, смотрим на destroy
