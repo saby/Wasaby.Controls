8a2be742557aa7234fc6fa86aa27d754583d37cf
ee.zolotova <ee.zolotova@tensor.ru>
2018-06-07 13:43:21 +0300

    https://online.sbis.ru/opendoc.html?guid=2b801ead-7b48-48a8-9b32-21d41b15cb21

--- Controls/Input/Dropdown/Util.js
+++ Controls/Input/Dropdown/Util.js
@@ -3,12 +3,6 @@ define('Controls/Input/Dropdown/Util', [], function() {
    'use strict';
    var DropdownUtil = {
 
-   /**
-     * Открывает всплывашку
-     * @param {Object} self
-     * @param {Object} target
-     */
-
       prepareEmpty: function(emptyText) {
          if (emptyText) {
             return emptyText === true ? 'Не выбрано' : emptyText;
