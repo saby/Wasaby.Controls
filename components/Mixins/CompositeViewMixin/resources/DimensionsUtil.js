2aa6cbde776f94da0358f2a744e5411eaee05c8a
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2018-06-08 16:46:00 +0300

    https://online.sbis.ru/opendoc.html?guid=bf011d04-f24c-4482-921b-16d9f5234d92

--- components/Mixins/CompositeViewMixin/resources/DimensionsUtil.js
+++ components/Mixins/CompositeViewMixin/resources/DimensionsUtil.js
@@ -22,34 +22,35 @@ define('SBIS3.CONTROLS/Mixins/CompositeViewMixin/resources/DimensionsUtil', [
             horizontalRightDiffer = containerRects.width - (itemRects.width + item.get(0).offsetLeft + additionalWidth / 2),
             horizontalLeftDiffer = item.get(0).offsetLeft - additionalWidth / 2,
             verticalBottomDiffer = containerRects.height - (itemRects.height + item.get(0).offsetTop + additionalHeight / 2),
-            verticalTopDiffer = item.get(0).offsetTop - additionalHeight / 2 ,
+            verticalTopDiffer = item.get(0).offsetTop - additionalHeight / 2,
             marginLeft,
             marginRight,
             marginTop,
             marginBottom,
-            padding = Math.ceil(additionalHeight / 2) + 'px ' + Math.ceil(additionalWidth / 2) + 'px';
+            paddingTop = Math.ceil(additionalHeight / 2),
+            paddingBottom = Math.ceil(additionalWidth / 2);
 
-         marginTop = marginBottom = (Math.floor(-(additionalHeight / 2 - margin)) + 'px ');
-         marginLeft = marginRight = (Math.floor(-(additionalWidth / 2 - margin)) + 'px ');
+         marginTop = marginBottom = Math.floor(-(additionalHeight / 2 - margin));
+         marginLeft = marginRight = Math.floor(-(additionalWidth / 2 - margin));
          if (horizontalLeftDiffer < 0) {
-            marginRight = (Math.floor(-(additionalWidth - margin)) + 'px ');
+            marginRight += marginLeft;
             marginLeft = 0;
          } else if (horizontalRightDiffer < 0) {
-            marginLeft = (Math.floor(-(additionalWidth - margin)) + 'px');
-            marginRight = 0 + ' ';
+            marginLeft += marginRight;
+            marginRight = 0;
          }
          if (verticalTopDiffer < 0) {
-            marginBottom = (Math.floor(-(additionalHeight - margin)) + 'px ');
-            marginTop = 0 + ' ';
+            marginBottom += marginTop;
+            marginTop = 0;
          } else if (verticalBottomDiffer < 0) {
-            marginTop = (Math.floor(-(additionalHeight - margin)) + 'px ');
-            marginBottom = 0 + ' ';
+            marginTop += marginBottom;
+            marginBottom = 0;
          }
 
          return {
-            padding: padding,
-            margin: marginTop + marginRight + marginBottom + marginLeft
+            padding: paddingTop + 'px ' + paddingBottom + 'px',
+            margin: marginTop + 'px ' + marginRight + 'px ' + marginBottom + 'px ' + marginLeft + 'px'
          };
       }
-   }
+   };
 });
