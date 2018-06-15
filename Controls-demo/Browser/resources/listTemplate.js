10a826a6380a6873c32ec9bcd33345afbe8fd90d
ee.zolotova <ee.zolotova@tensor.ru>
2018-06-15 19:06:52 +0300

    https://online.sbis.ru/opendoc.html?guid=70d3fbe7-210f-4312-ac4b-b7b89b0a6018

--- Controls-demo/Browser/resources/listTemplate.js
+++ Controls-demo/Browser/resources/listTemplate.js
@@ -1,44 +1,15 @@
 define('Controls-demo/Browser/resources/listTemplate', [
    'Core/Control',
-   'tmpl!Controls-demo/Browser/resources/listTemplate',
-   'WS.Data/Source/Memory'
+   'tmpl!Controls-demo/Browser/resources/listTemplate'
 
-], function(BaseControl, template, Memory) {
+], function(BaseControl, template) {
    'use strict';
 
-   var sourceData = [
-      { id: 1, title: 'Sasha'},
-      { id: 2, title: 'Dmitry'},
-      { id: 3, title: 'Andrey'},
-      { id: 4, title: 'Aleksey'},
-      { id: 5, title: 'Sasha'},
-      { id: 6, title: 'Ivan'},
-      { id: 7, title: 'Petr'},
-      { id: 8, title: 'Roman'},
-      { id: 9, title: 'Maxim'},
-      { id: 10, title: 'Andrey'},
-      { id: 12, title: 'Sasha'},
-      { id: 13, title: 'Sasha'},
-      { id: 14, title: 'Sasha'},
-      { id: 15, title: 'Sasha'},
-      { id: 16, title: 'Sasha'},
-      { id: 17, title: 'Sasha'},
-      { id: 18, title: 'Dmitry'},
-      { id: 19, title: 'Andrey'},
-      { id: 20, title: 'Aleksey'},
-      { id: 21, title: 'Sasha'},
-      { id: 22, title: 'Ivan'},
-      { id: 23, title: 'Petr'}
-   ];
+
 
    var ModuleClass = BaseControl.extend(
       {
-         _template: template,
-
-         _source: new Memory({
-            idProperty: 'id',
-            data: sourceData
-         })
+         _template: template
 
       });
    return ModuleClass;
