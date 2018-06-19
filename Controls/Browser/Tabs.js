4c53932b098b9c4bf67c15f4745e40c3768b61f4
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-19 14:47:30 +0300

    https://online.sbis.ru/opendoc.html?guid=59b72308-2360-42cb-be77-9924c8ad2159

--- Controls/Browser/Tabs.js
+++ Controls/Browser/Tabs.js
@@ -2,33 +2,13 @@
 define('Controls/Browser/Tabs', [
    'Core/Control',
    'tmpl!Controls/Browser/Tabs/Tabs',
-   'WS.Data/Source/Memory',
-   'Core/Deferred'
-], function(Control, template, MemorySource, Deferred) {
+   'WS.Data/Source/Memory'
+], function(Control, template, MemorySource) {
    'use strict';
    var _private = {
-      initContent: function(currentTab, self) {
-         var def = new Deferred();
-         requirejs([currentTab.content], function(tabContent) {
-            self._content = tabContent;
-            def.callback(tabContent);
-         });
-         return def;
-      }
    };
    var browserTabs = Control.extend({
-      _template: template,
-      _beforeMount: function(options, context, receivedState) {
-         if (receivedState) {
-            this._content = receivedState;
-         } else {
-            return _private.initContent(options.tabsSource._getRecordByKey(options.tabsSelectedKey), this);
-         }
-      },
-
-      _beforeUpdate: function(newOptions) {
-         return _private.initContent(newOptions.tabsSource._getRecordByKey(newOptions.tabsSelectedKey), this);
-      }
+      _template: template
    });
    return browserTabs;
 });
