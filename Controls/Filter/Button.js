227d938c99d5b88dbbf05fefc771d2f266f73cca
ee.zolotova <ee.zolotova@tensor.ru>
2018-06-19 12:25:52 +0300

    https://online.sbis.ru/opendoc.html?guid=4b9f0a2f-a5ce-4595-be41-cf68c9d25fad

--- Controls/Filter/Button.js
+++ Controls/Filter/Button.js
@@ -99,10 +99,12 @@ define('Controls/Filter/Button',
          },
 
          _clearClick: function() {
-            _private.getFilterButtonCompatible(this).addCallback(function(panelOpener) {
-               panelOpener.clearFilter();
-               this._text = '';
-            });
+            if (this._options.filterTemplate) {
+               _private.getFilterButtonCompatible(this).addCallback(function (panelOpener) {
+                  panelOpener.clearFilter();
+               });
+            }
+            this._text = '';
          },
 
          _openFilterPanel: function() {
