aa9077393095f1a157b1d7f51f2551913340cb0f
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-20 13:32:37 +0300

    https://online.sbis.ru/opendoc.html?guid=622c1609-37b3-4325-94b0-734264f8793e

--- Controls/Browser/Tabs.js
+++ Controls/Browser/Tabs.js
@@ -4,8 +4,20 @@ define('Controls/Browser/Tabs', [
    'tmpl!Controls/Browser/Tabs/Tabs'
 ], function(Control, template) {
    'use strict';
+   var _private = {
+      setTabContent: function(options, self) {
+         self._tabContent = options.tabsSource._getRecordByKey(options.tabsSelectedKey).content;
+      }
+   };
    var browserTabs = Control.extend({
-      _template: template
+      _template: template,
+
+      _beforeMount: function(options) {
+         _private.setTabContent(options, this);
+      },
+      _beforeUpdate: function(options) {
+         _private.setTabContent(options, this);
+      }
    });
    return browserTabs;
 });
