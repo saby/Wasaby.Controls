23727327ad19a0c22c97730e02ec6e5d58f15c4b
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-22 15:34:31 +0300

    https://online.sbis.ru/opendoc.html?guid=622c1609-37b3-4325-94b0-734264f8793e

--- Controls/PageLayout.js
+++ Controls/PageLayout.js
@@ -2,10 +2,19 @@
 define('Controls/PageLayout', [
    'Core/Control',
    'tmpl!Controls/PageLayout/PageLayout',
+   'Controls/Controllers/SourceController',
    'css!Controls/PageLayout/PageLayout'
-], function(Control, template) {
+], function(Control, template, SourceController) {
    'use strict';
    var _private = {
+      initItems: function(source, self) {
+         self._sourceController = new SourceController({
+            source: source
+         });
+         return self._sourceController.load().addCallback(function(items) {
+            return items;
+         });
+      },
       setCurrentItemState: function(options, self) {
          if (options.tabsSource) {
             options.tabsSource.read(options.tabsSelectedKey).addCallback(function(item) {
@@ -18,10 +27,25 @@ define('Controls/PageLayout', [
    var browserTabs = Control.extend({
       _template: template,
 
-      _beforeMount: function(options) {
+      _beforeMount: function(options, context, receivedState) {
+         if (receivedState) {
+            this._items = receivedState;
+         } else {
+            return _private.initItems(options.tabsSource, this).addCallback(function(items) {
+               this._items = items;
+               return items;
+            }.bind(this));
+         }
          _private.setCurrentItemState(options, this);
       },
       _beforeUpdate: function(newOptions) {
+         var self = this;
+         if (newOptions.tabsSource && newOptions.tabsSource !== this._options.tabsSource) {
+            return _private.initItems(newOptions.tabsSource, this).addCallback(function(items) {
+               this._items = items;
+               self._forceUpdate();
+            }.bind(this));
+         }
          _private.setCurrentItemState(newOptions, this);
       }
    });
