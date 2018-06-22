34ddcbfb007cc5f4b92cc39db33e7c3077e6c578
aa.shipin <aa.shipin@tensor.ru>
2018-06-22 18:39:58 +0300

    https://online.sbis.ru/opendoc.html?guid=1c4581f1-e4e9-4bed-baf4-80f3b5ca1010

--- Controls/Popup/Compatible/Layer.js
+++ Controls/Popup/Compatible/Layer.js
@@ -1,7 +1,18 @@
 /**
  * Created by as.krasilnikov on 14.05.2018.
  */
-define('Controls/Popup/Compatible/Layer', ['Core/Deferred'], function(Deferred) {
+define('Controls/Popup/Compatible/Layer', [
+   'Core/Deferred',
+   'Core/ParallelDeferred',
+   'Core/constants',
+   'Core/RightsManager',
+   'Core/ExtensionsManager',
+   'WS.Data/Chain',
+   'Core/moduleStubs',
+   'Core/IoC',
+   'WS.Data/Source/SbisService',
+   'cdn!jquery/3.3.1/jquery-min.js'
+], function(Deferred, ParallelDeferred, Constants, RightsManager, ExtensionsManager, Chain, moduleStubs, IoC, SbisService) {
    'use strict';
 
    var loadDeferred;
@@ -20,9 +31,142 @@ define('Controls/Popup/Compatible/Layer', ['Core/Deferred'], function(Deferred) 
       'is!browser?WS.Data/ContextField/Enum',
       'is!browser?WS.Data/ContextField/List'
    ];
+   var defaultLicense = {
+      defaultLicense: true
+   };
 
    function isNewEnvironment() {
       return !!document.getElementsByTagName('html')[0].controlNodes;
+   }
+
+   function loadDataProviders() {
+      var def = new Deferred();
+
+      var parallelDef = new ParallelDeferred();
+
+      parallelDef.push(ExtensionsManager.loadExtensions().addErrback(function(err) {
+         IoC.resolve('ILogger').error('Layer', 'Can\'t load system extensions', err);
+         return err;
+      }));
+
+      parallelDef.push(RightsManager.readUserRights().addCallbacks(function(rights) {
+         // возможно можно уже удалить, это совместимость со старым форматом прав. на препроцессоре не удалено, решил скопировать и сюда
+         if (rights) {
+            Object.keys(rights).forEach(function(id) {
+               var right = rights[id];
+               if (right) {
+                  if (typeof right === 'number') {
+                     rights[id] = right;
+                  } else if (right.flags) {
+                     // Копируем, если доступ > 0
+                     if (right.restrictions) {
+                        // Если новый формат, копируем объект целиком
+                        rights[id] = right;
+                     } else {
+                        // Если старый формат, копируем только число-флаг
+                        rights[id] = right.flags;
+                     }
+                  }
+               }
+            });
+         }
+         Constants.rights = true;
+         window.rights = rights || {};
+      }, function(err) {
+         IoC.resolve('ILogger').error('Layer', 'Can\'t load user rights', err);
+      }));
+
+      parallelDef.push(viewSettingsData().addCallbacks(function(viewSettings) {
+         window.viewSettings = viewSettings || {};
+      }, function(err) {
+         IoC.resolve('ILogger').error('Layer', 'Can\'t load view settings', err);
+      }));
+
+      parallelDef.push(new SbisService({
+         endpoint: 'Пользователь'}).call('GetCurrentUserInfo', {}).addCallbacks(function(userInfo) {
+         window.userInfo = Chain(userInfo.getRow()).toObject() || {};
+      }, function(err) {
+         IoC.resolve('ILogger').error('Layer', 'Can\'t load user info', err);
+      }));
+
+      parallelDef.push(getUserLicense().addCallbacks(function(userLicense) {
+         window.userLicense = userLicense || defaultLicense;
+      }, function(err) {
+         IoC.resolve('ILogger').error('Layer', 'Can\'t load user license', err);
+      }));
+
+      //globalClientConfig
+      // параметры пользователськие, клиенстские, глобальные. причем они в session или localStorage могут быть
+      // параметры нужно положить в контекст (белый список)
+      parallelDef.push(readGlobalClientConfig().addCallbacks(function(globalClientConfig) {
+         window.globalClientConfig = globalClientConfig || {};
+      }, function(err) {
+         IoC.resolve('ILogger').error('Layer', 'Can\'t load global client config', err);
+      }));
+
+      //cachedMethods
+      window.cachedMethods = [];
+
+      //product
+      window.product = 'продукт никому не нужен?';
+
+      //активность???
+
+      def.dependOn(parallelDef.done().getResult());
+
+      return def;
+   }
+
+   function viewSettingsData() {
+      var
+         dResult = new Deferred(),
+         viewSettings = {};
+
+      moduleStubs.require(['OnlineSbisRu/ViewSettings/Util/ViewSettingsData']).addCallback(function(mods) {
+         mods[0].getData(null, true).addCallback(function(data) {
+            viewSettings = data;
+            dResult.callback(viewSettings);
+         }).addErrback(function() {
+            dResult.callback(viewSettings);
+         });
+      }).addErrback(function() {
+         dResult.callback(viewSettings);
+      });
+      return dResult;
+   }
+
+   function getUserLicense() {
+      var def = new Deferred();
+
+      new SbisService({endpoint: 'Биллинг'}).call('ДанныеЛицензии', {}).addCallbacks(function(record) {
+         if (record && record.getRow().get('ПараметрыЛицензии')) {
+            var data = record.getRow().get('ПараметрыЛицензии').toObject();
+            def.callback(data);
+         } else {
+            def.callback(defaultLicense);
+         }
+      }, function(err) {
+         def.errback(err);
+      });
+
+      return def;
+   }
+
+   function readGlobalClientConfig() {
+      var def = new Deferred();
+
+      var gcc = {};
+      new SbisService({endpoint: 'ГлобальныеПараметрыКлиента'}).call('ПолучитьПараметры', {}).addCallbacks(function(rs) {
+         rs = rs.getAll();
+         rs.forEach(function(r) {
+            gcc[r.get('Название')] = r.get('Значение');
+         });
+         def.callback(gcc);
+      }, function(err) {
+         def.errback(err);
+      });
+
+      return def;
    }
 
    return {
@@ -32,47 +176,50 @@ define('Controls/Popup/Compatible/Layer', ['Core/Deferred'], function(Deferred) 
          }
          if (!loadDeferred) {
             loadDeferred = new Deferred();
-            requirejs(['Core/moduleStubs', 'Core/constants', 'Core/IoC', 'Core/ExtensionsManager', 'cdn!jquery/3.3.1/jquery-min.js'], function(moduleStubs, Constants, IoC, ExtensionsManager) {
-               if (window && window.$) {
-                  Constants.$win = $(window);
-                  Constants.$doc = $(document);
-                  Constants.$body = $('body');
-               }
 
-               deps = (deps || []).concat(compatibleDeps);
+            if (window && window.$) {
+               Constants.$win = $(window);
+               Constants.$doc = $(document);
+               Constants.$body = $('body');
+            }
 
-               moduleStubs.require(deps).addCallback(function(result) {
-                  // var tempCompatVal = constants.compat;
-                  Constants.compat = true;
-                  Constants.systemExtensions = true;
+            deps = (deps || []).concat(compatibleDeps);
 
-                  ExtensionsManager.loadExtensions().addCallbacks(function() {
+            moduleStubs.require(deps).addCallback(function(result) {
+               // var tempCompatVal = constants.compat;
+               Constants.compat = true;
+               Constants.systemExtensions = true;
+
+               if (typeof window !== 'undefined') {
+                  loadDataProviders().addCallbacks(function() {
                      loadDeferred.callback(result);
                   }, function(e) {
-                     IoC.resolve('ILogger').error('Layer', 'Can\'t load extensions', e);
+                     IoC.resolve('ILogger').error('Layer', 'Can\'t load data providers', e);
                      loadDeferred.callback(result);
                   });
+               } else {
+                  loadDeferred.callback(result);
+               }
 
-                  // constants.compat = tempCompatVal; //TODO выпилить
-                  (function($) {
-                     $.fn.wsControl = function() {
-                        var control = null,
-                           element;
-                        try {
-                           element = this[0];
-                           while (element) {
-                              if (element.wsControl) {
-                                 control = element.wsControl;
-                                 break;
-                              }
-                              element = element.parentNode;
+               // constants.compat = tempCompatVal; //TODO выпилить
+               (function($) {
+                  $.fn.wsControl = function() {
+                     var control = null,
+                        element;
+                     try {
+                        element = this[0];
+                        while (element) {
+                           if (element.wsControl) {
+                              control = element.wsControl;
+                              break;
                            }
-                        } catch (e) {
+                           element = element.parentNode;
                         }
-                        return control;
-                     };
-                  })(jQuery);
-               });
+                     } catch (e) {
+                     }
+                     return control;
+                  };
+               })(jQuery);
             });
          }
          return loadDeferred;
