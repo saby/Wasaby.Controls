90dc5a151b5c31d5d1ae44abd53cda059f72bc26
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-22 20:33:38 +0300

    https://online.sbis.ru/opendoc.html?guid=d1341412-25a5-4602-bf21-dc36f78b29e9

--- Controls/Button/Close.js
+++ Controls/Button/Close.js
@@ -5,7 +5,11 @@ define('Controls/Button/Close', [
 ], function(Control, template) {
 
    var CloseButton = Control.extend({
-      _template: template
+      _template: template,
+
+      clickHandler: function() {
+         this._notify('close', [], {bubbling: true})
+      }
    });
 
    CloseButton.getDefaultOptions = function() {
