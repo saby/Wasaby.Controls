55498f41a7e8fb592b8f6909e593f215b4ef4c8d
aa.bykanov <aa.bykanov@tensor.ru>
2018-06-22 15:17:02 +0300

    Группируем routes.js

--- ExampleRoutes/demo-pages.routes.js
+++ ExampleRoutes/demo-pages.routes.js
@@ -7,8 +7,15 @@ module.exports = function(Component) {
    };
 
    return {
+
+
+      /*
+        ### СТРАНИЧКИ ДЛЯ НОВЫХ КОНТРОЛОВ - WS4 (VDOM). ### 
+        ### Для построения страницы используется компонент Controls/Application. ###
+        ### Пространство имён - Controls/ . ###
+      */
+
       // Демо-пример: Демо-пример: открытие шаблона, совместимого с WS3, в программном окружении WS4.
-      // Для построения страницы используется компонент Controls/Application.
       '/demo-ws4-open-component-from-ws3': function(req, res) {
          requirejs('Examples/ws4open/Module');
          res.render('tmpl!Controls/Application/Route', {
@@ -18,7 +25,6 @@ module.exports = function(Component) {
       },
 
       // Демо-пример: cвайп при работе со списочным компонентом на тач-устройствах.
-      // Для построения страницы используется компонент Controls/Application.
       '/demo-ws4-swipe': function(req, res) {
         requirejs('Examples/Swipe/Module');
         res.render('tmpl!Controls/Application/Route', {
@@ -28,7 +34,6 @@ module.exports = function(Component) {
       },
 
       // Демо-пример: редактирование по месту в списках.
-      // Для построения страницы используется компонент Controls/Application.
       '/edit-in-place': function(req, res) {
          requirejs('Examples/List/EditInPlace/EditInPlace');
          res.render('tmpl!Controls/Application/Route', {
@@ -37,16 +42,7 @@ module.exports = function(Component) {
          }, []);
       },
 
-      // Демо-пример: открытие шаблона, совместимого с WS4, в программном окружении WS3.
-      // Для построения страницы используется шаблон VIEW.
-      '/demo-ws3-open-component-from-ws4': function(req, res) {
-         requirejs(['Examples/ws3open/Module'], function() {
-            render(req, res, 'Examples/ws3open/Module');
-         });
-      },
-
       // Демо-пример: концепция использования прикладных шаблонов.
-      // Для построения страницы используется компонент Controls/Application.
       '/demo-ws4-templates': function(req, res) {
          requirejs('Examples/List/Base');
          res.render('tmpl!Controls/Application/Route', {
@@ -56,7 +52,6 @@ module.exports = function(Component) {
       },
 
       // Демо-пример: фильтрация и поиск.
-      // Для построения страницы используется компонент Controls/Application.
       '/demo-ws4-filter-search': function(req, res) {
          requirejs('Examples/Layouts/SearchLayout');
          res.render('tmpl!Controls/Application/Route', {
@@ -66,7 +61,6 @@ module.exports = function(Component) {
       },
 
       // Демо-пример: компоненты для работы с всплывающими окнами.
-      // Для построения страницы используется компонент Controls/Application.
       '/demo-ws4-popup-opener': function(req, res) {
          requirejs('Examples/Popup/Opener');
          res.render('tmpl!Controls/Application/Route', {
@@ -76,13 +70,25 @@ module.exports = function(Component) {
       },
 
       // Демо-пример: работа с перемещениями элементов интерфейса.
-      // Для построения страницы используется компонент Controls/Application.
       '/demo-ws4-drag-n-drop': function(req, res) {
          requirejs('Examples/DragNDrop/Container');
          res.render('tmpl!Controls/Application/Route', {
             application: 'Examples/DragNDrop/Container',
             initDependencies: false
          }, []);
-      }
+      },
+
+      /*
+        ### СТРАНИЧКИ ДЛЯ СТАРЫХ КОНТРОЛОВ - WS3. ### 
+        ### Для построения страницы используется шаблон VIEW. ###
+        ### Пространство имён - SBIS3.CONTROLS, Lib/Controls, Deprecated/Controls. ###
+      */
+
+      // Демо-пример: открытие шаблона, совместимого с WS4, в программном окружении WS3. 
+      '/demo-ws3-open-component-from-ws4': function(req, res) {
+        requirejs(['Examples/ws3open/Module'], function() {
+           render(req, res, 'Examples/ws3open/Module');
+        });
+     }
    }
 };
