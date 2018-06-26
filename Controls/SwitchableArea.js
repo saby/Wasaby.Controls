7114416a49cb67253d0c6154f02abaf83619031a
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-26 21:42:34 +0300

    https://online.sbis.ru/opendoc.html?guid=9d800e18-1aa7-45ab-82c2-ae47d85ed5f2

--- Controls/SwitchableArea.js
+++ Controls/SwitchableArea.js
@@ -6,23 +6,23 @@ define('Controls/SwitchableArea',
 ],
 function(Control, ViewModel, template) {
 
-      'use strict';
+   'use strict';
 
-      var SwitchableArea = Control.extend({
-         _template: template,
+   var SwitchableArea = Control.extend({
+      _template: template,
 
-         _beforeMount: function (options) {
-            this._viewModel = new ViewModel(options.items, options.selectedKey);
-            this._items = options.items;
-         },
+      _beforeMount: function (options) {
+         this._viewModel = new ViewModel(options.items, options.selectedKey);
+         this._items = options.items;
+      },
 
-         _beforeUpdate: function (newOptions) {
-            this._viewModel.updateViewModel(newOptions.items, newOptions.selectedKey);
-         },
-         isActiveItem: function (item) {
-            return this._options.selectedKey !== item.get(this._options.items.getIdProperty());
-         }
-      });
-      return SwitchableArea;
+      _beforeUpdate: function (newOptions) {
+         this._viewModel.updateViewModel(newOptions.items, newOptions.selectedKey);
+      },
+      isActiveItem: function (item) {
+         return this._options.selectedKey !== item.get(this._options.items.getIdProperty());
+      }
+   });
+   return SwitchableArea;
 });
 
