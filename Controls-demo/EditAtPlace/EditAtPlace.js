de42a5fcd196864d40ed84dba9efa6d8e2cd36f4
ps.borisov <ps.borisov@tensor.ru>
2018-06-28 11:35:36 +0300

    https://online.sbis.ru/opendoc.html?guid=799c3a24-855a-4e7c-b4ec-1d5a9e89ee9a
    Scotty doesn't know
    Scotty doesn't know
    Scotty doesn't know
    Scotty doesn't know
    So don't tell Scotty
    Scotty doesn't know ph
    (Don't tell Scotty!)

--- Controls-demo/EditAtPlace/EditAtPlace.js
+++ Controls-demo/EditAtPlace/EditAtPlace.js
@@ -5,56 +5,112 @@ define('Controls-demo/EditAtPlace/EditAtPlace', [
    'WS.Data/Entity/Record',
    'WS.Data/Source/Memory',
    'tmpl!Controls-demo/EditAtPlace/resources/tabTemplate',
+   'tmpl!Controls-demo/EditAtPlace/resources/tabTemplate2',
    'css!Controls-demo/EditAtPlace/EditAtPlace'
-], function(Control, template, cClone, Record, MemorySource, tabTemplate) {
+], function(
+   Control,
+   template,
+   cClone,
+   Record,
+   MemorySource,
+   tabTemplate,
+   tabTemplate2
+) {
    'use strict';
    var tabsData = [
-      {
-         id: '0',
-         title: 'Go go go',
-         align: 'left',
-         number: '1175501898',
-         date: '26.06.18',
-         itemTemplate: tabTemplate
-      },
-      {
-         id: '1',
-         title: 'Kochnev',
-         align: 'left'
-      },
-      {
-         id: '2',
-         title: 'Cheremushkin',
-         align: 'left'
-      },
-      {
-         id: '3',
-         title: 'Izigin'
-      },
-      {
-         id: '4',
-         align: 'left',
-         title: 'Stepin'
-      },
-      {
-         id: '5',
-         align: 'left',
-         title: 'Romanov'
-      },
-      {
-         id: '6',
-         align: 'left',
-         title: 'Borisov'
-      },
-      {
-         id: '7',
-         title: ' Zaitsev'
-      },
-      {
-         id: '8',
-         title: ' Sukhoruchkin'
-      }
-   ];
+         {
+            id: '0',
+            title: 'Ошибка в разработку',
+            align: 'left',
+            number: '1175501898',
+            date: '26.06.18',
+            itemTemplate: tabTemplate
+         },
+         {
+            id: '1',
+            title: 'Kochnev',
+            align: 'left'
+         },
+         {
+            id: '2',
+            title: 'Cheremushkin',
+            align: 'left'
+         },
+         {
+            id: '3',
+            title: 'Izigin'
+         },
+         {
+            id: '4',
+            align: 'left',
+            title: 'Stepin'
+         },
+         {
+            id: '5',
+            align: 'left',
+            title: 'Romanov'
+         },
+         {
+            id: '6',
+            align: 'left',
+            title: 'Borisov'
+         },
+         {
+            id: '7',
+            title: ' Zaitsev'
+         },
+         {
+            id: '8',
+            title: ' Sukhoruchkin'
+         }
+      ],
+      tabsData2 = [
+         {
+            id: '0',
+            align: 'left',
+            name: 'Имя',
+            surname: 'Фамилия',
+            patronymic: 'Отчество',
+            itemTemplate: tabTemplate2
+         },
+         {
+            id: '1',
+            title: 'Kochnev',
+            align: 'left'
+         },
+         {
+            id: '2',
+            title: 'Cheremushkin',
+            align: 'left'
+         },
+         {
+            id: '3',
+            title: 'Izigin'
+         },
+         {
+            id: '4',
+            align: 'left',
+            title: 'Stepin'
+         },
+         {
+            id: '5',
+            align: 'left',
+            title: 'Romanov'
+         },
+         {
+            id: '6',
+            align: 'left',
+            title: 'Borisov'
+         },
+         {
+            id: '7',
+            title: ' Zaitsev'
+         },
+         {
+            id: '8',
+            title: ' Sukhoruchkin'
+         }
+      ];
 
    var EditAtPlace = Control.extend({
       _template: template,
@@ -62,6 +118,7 @@ define('Controls-demo/EditAtPlace/EditAtPlace', [
       _record2: null,
       _record3: null,
       _selectedTab: '1',
+      _selectedTab2: '2',
       _tabSource: null,
 
       _beforeMount: function() {
@@ -80,6 +137,7 @@ define('Controls-demo/EditAtPlace/EditAtPlace', [
             }
          });
          this._updateTabSource();
+         this._updateTabSource2();
       },
 
       _cancelHandler: function() {
@@ -109,6 +167,27 @@ define('Controls-demo/EditAtPlace/EditAtPlace', [
          this._tabSource = new MemorySource({
             idProperty: 'id',
             data: tabsData
+         });
+      },
+      _cancelHandler4: function() {
+         this._updateTabSource2();
+      },
+      _beforeEndEdit2: function(event, item) {
+         this.updateItem2(item);
+         this._updateTabSource2();
+      },
+
+      updateItem2: function(item) {
+         for (var i = 0; i < tabsData2.length; i++) {
+            if (tabsData2[i].id === item.get('id')) {
+               tabsData2[i] = item.getRawData();
+            }
+         }
+      },
+      _updateTabSource2: function() {
+         this._tabSource2 = new MemorySource({
+            idProperty: 'id',
+            data: tabsData2
          });
       }
    });
