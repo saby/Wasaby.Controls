993f875eb28e59bb49e3556a57a10eb00bd8fcfe
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-28 15:13:06 +0300

    https://online.sbis.ru/opendoc.html?guid=287fedaa-f590-4742-9b25-da6a0351b955

--- Controls/SwitchableArea.js
+++ Controls/SwitchableArea.js
@@ -1,28 +1,29 @@
 define('Controls/SwitchableArea',
-[
-   'Core/Control',
-   'Controls/SwitchableArea/ViewModel',
-   'tmpl!Controls/SwitchableArea/SwitchableArea'
-],
-function(Control, ViewModel, template) {
+   [
+      'Core/Control',
+      'Controls/SwitchableArea/ViewModel',
+      'tmpl!Controls/SwitchableArea/SwitchableArea'
+   ],
+   function(Control, ViewModel, template) {
 
-   'use strict';
+      'use strict';
 
-   var SwitchableArea = Control.extend({
-      _template: template,
+      var SwitchableArea = Control.extend({
+         _template: template,
 
-      _beforeMount: function(options) {
-         this._viewModel = new ViewModel(options.items, options.selectedKey);
-         this._items = options.items;
-      },
+         _beforeMount: function(options) {
+            this._viewModel = new ViewModel(options.items, options.selectedKey);
+            this._items = options.items;
+         },
 
-      _beforeUpdate: function(newOptions) {
-         this._viewModel.updateViewModel(newOptions.items, newOptions.selectedKey);
-      },
-      isActiveItem: function(item) {
-         return this._options.selectedKey !== item.get(this._options.items.getIdProperty());
-      }
-   });
-   return SwitchableArea;
-});
+         _beforeUpdate: function(newOptions) {
+            this._viewModel.updateViewModel(newOptions.items, newOptions.selectedKey);
+         },
+         isActiveItem: function(item) {
+            return this._options.selectedKey !== item.get(this._options.items.getIdProperty());
+         }
+      });
+      return SwitchableArea;
+   }
+);
 
