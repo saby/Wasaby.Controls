0be44f846cc51e1a590aff7e6cb76544c4774286
va.spirin <va.spirin@tensor.ru>
2018-06-28 16:07:47 +0700

    ExportCustomizer: Изменён MenuLink (2)
    1175496701 https://online.sbis.ru/opendoc.html?guid=fa84a270-7792-410f-9f7e-2366fb98b4ab

--- components/ExportCustomizer/MenuLink.js
+++ components/ExportCustomizer/MenuLink.js
@@ -12,6 +12,7 @@
 define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
    [
       'Core/Deferred',
+      'Core/core-merge',
       'SBIS3.CONTROLS/CompoundControl',
       'WS.Data/Di',
       'WS.Data/Collection/RecordSet',
@@ -19,7 +20,7 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
       'css!SBIS3.CONTROLS/ExportCustomizer/MenuLink'
    ],
 
-   function (Deferred, CompoundControl, Di, RecordSet, tmpl) {
+   function (Deferred, cMerge, CompoundControl, Di, RecordSet, tmpl) {
       'use strict';
 
       /**
@@ -28,6 +29,22 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
        * @property {string} title Отображаемое название пресета
        * @property {Array<string>} fieldIds Список привязки колонок в экспортируемом файле к полям данных
        * @property {string} fileUuid Uuid стилевого эксель-файла
+       */
+
+      /**
+       * @event onFirstItemsActive Происходит при выборе пункта меню, задаваемого опцией firstItems
+       * @param {Core/EventObject} evtDescriptor Дескриптор события
+       * @param {string} menuItemId Идентификатор выбранного пункта меню
+       * @see firstItems
+       */
+
+      /**
+       * @event onPresetActive Происходит при выборе пункта меню, задаваемого пресетом
+       * @param {Core/EventObject} evtDescriptor Дескриптор события
+       * @param {string} presetId Идентификатор выбранного пресета
+       * @param {boolean} skipCustomization Применить пресет сразу, без показа настройщика экспорта
+       * @see staticPresets
+       * @see presetNamespace
        */
 
       /**
@@ -49,13 +66,26 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
                /**
                 * @cfg {string} Пространство имён для сохранения пользовательских пресетов (опционально)
                 */
-               presetNamespace: null
+               presetNamespace: null,
+               /**
+                * @cfg {string} Пиктограмка, которая будет показана у первого пресета (опционально)
+                */
+               presetIcon: null,
+               /**
+                * @cfg {string} Название, которым будети заменено название первого статического пресета (опционально)
+                */
+               presetCaption: null,
+               /**
+                * @cfg {Array<object>} Список объектов с опциями для пунктов меню, которые будут показаны над пунктами пресетов (опционально)
+                */
+               firstItems: null
             },
             _storage: null,
             _menuLink: null
          },
 
          init: function () {
+            this._publish('onFirstItemsActive', 'onPresetActive');
             ExportMenuLink.superclass.init.apply(this, arguments);
             var menuLink = this._menuLink = this.getChildControlByName('controls-ExportCustomizer-MenuLink__menuLink');
             if (Di.isRegistered(_DI_STORAGE_NAME)) {
@@ -64,6 +94,15 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
             //this._update();
             this.subscribeTo(menuLink, 'onActivated', function () {
                this._update().addCallback(menuLink.showPicker.bind(menuLink));
+            }.bind(this));
+            this.subscribeTo(menuLink, 'onMenuItemActivate', function (evt, menuItemId, origEvt) {
+               var item = menuLink.getItems().getRecordById(menuItemId).getRawData();
+               if (item.isPreset) {
+                  this._notify('onPresetActive', menuItemId, !$(origEvt.target).hasClass('controls-ExportCustomizer-MenuLink__edit'));
+               }
+               else {
+                  this._notify('onFirstItemsActive', menuItemId);
+               }
             }.bind(this));
          },
 
@@ -77,16 +116,30 @@ define('SBIS3.CONTROLS/ExportCustomizer/MenuLink',
             var storage = this._storage;
             return (storage ? storage.load(options.presetNamespace) : Deferred.success(null)).addCallback(function (presets) {
                var items = [];
-               var firstItems = options.firstItems;
-               if (firstItems && firstItems.length) {
-                  items.push.apply(items, firstItems);
-               }
-               if (presets && presets.length) {
-                  items.push.apply(items, presets);
-               }
                var statics = options.staticPresets;
                if (statics && statics.length) {
-                  items.push.apply(items, statics);
+                  items.push.apply(items, statics.map(function (v) { var o = cMerge({isPreset:true}, v); return o; }));
+                  var presetCaption = options.presetCaption;
+                  if (presetCaption) {
+                     items[0].title = presetCaption;
+                  }
+               }
+               if (presets && presets.length) {
+                  items.push.apply(items, presets.map(function (preset) {
+                     var item = cMerge({isPreset:true, className:'controls-ExportCustomizer-MenuLink__dual'}, preset);
+                     item.title += '<span class="controls-ExportCustomizer-MenuLink__edit icon-16 icon-Edit icon-primary"></span>';
+                     return item;
+                  }));
+               }
+               if (items.length) {
+                  var presetIcon = options.presetIcon;
+                  if (presetIcon) {
+                     items[0].icon = presetIcon;
+                  }
+               }
+               var firstItems = options.firstItems;
+               if (firstItems && firstItems.length) {
+                  items.unshift.apply(items, firstItems);
                }
                items.push({id:'', title:rk('Создать новый шаблон', 'НастройщикЭкспорта'), className:'controls-ExportCustomizer-MenuLink__last'});
                return new RecordSet({
