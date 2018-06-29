047fcd352207a9868a15d0c595113c6df7b2311a
nv.belotelov <nv.belotelov@tensor.ru>
2018-06-29 20:34:09 +0300

    https://online.sbis.ru/opendoc.html?guid=60856780-3497-490d-842a-9f1fd823ab3f

--- Controls/Application.js
+++ Controls/Application.js
@@ -10,8 +10,7 @@ define('Controls/Application',
       'Core/compatibility',
       'Controls/Application/AppData',
       'Controls/Application/HeadDataContext',
-      'Core/ConsoleLogger',
-      'Core/app'
+      'Core/ConsoleLogger'
    ],
 
    /**
