b9fc495d71684c5eef4c1f19a149aa8009ba600f
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-06-30 21:07:19 +0300

    https://online.sbis.ru/opendoc.html?guid=9d800e18-1aa7-45ab-82c2-ae47d85ed5f2

--- tests/unit/SwitchableArea/SwitcheableArea.test.js
+++ tests/unit/SwitchableArea/SwitcheableArea.test.js
@@ -15,7 +15,20 @@ define(['Controls/SwitchableArea', 'WS.Data/Collection/RecordSet'], function (Sw
             items: testItems
          };
          SwitchArea.saveOptions(opt);
-         assert(SwitchArea.getIdPropertyItem(testItems.at(2)) === 3);
+         assert.equal(SwitchArea.getIdPropertyItem(testItems.at(2)), 3, 'getIdProperty is uncorrect');
+      });
+
+      it('life circle', function () {
+         var opt = {
+            items: testItems,
+            selectedKey: '1'
+         };
+         SwitchArea.saveOptions(opt);
+         SwitchArea._beforeMount(opt);
+         opt.selectedKey = '2';
+         SwitchArea._beforeUpdate(opt);
+         assert.equal(SwitchArea._viewModel._items.getRecordById('1').loaded, true, '_beforeMount. Item load status is uncorrect');
+         assert.equal(SwitchArea._viewModel._items.getRecordById('2').loaded, true, '_beforeUpdate. Item load status is uncorrect');
       });
    });
 });
