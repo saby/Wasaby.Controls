44c954dc5c79450dbb4d25c3b648bce89c53a2b2
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-07-01 11:07:06 +0300

    https://online.sbis.ru/opendoc.html?guid=9d4e1ded-4d5b-44b4-a749-ac2e9d6f8825

--- Controls/Popup/Previewer/OpenerTemplate.js
+++ Controls/Popup/Previewer/OpenerTemplate.js
@@ -1,14 +1,32 @@
 define('Controls/Popup/Previewer/OpenerTemplate',
    [
       'Core/Control',
+      'Core/Deferred',
       'tmpl!Controls/Popup/Previewer/OpenerTemplate'
    ],
-   function(Control, template) {
+   function(Control, Deferred, template) {
 
       'use strict';
 
       var OpenerTemplate = Control.extend({
          _template: template,
+
+         _beforeMount: function(options) {
+            var _this = this;
+            var def = new Deferred();
+
+            if (typeof options.content === 'string') {
+               require([options.content], function(content) {
+                  _this._content = content;
+
+                  def.callback();
+               });
+            } else {
+               _this._content = options.content;
+            }
+
+            return def;
+         },
 
          _sendResult: function(event) {
             this._notify('sendResult', [event], {bubbling: true});
