372c344c8bec9974bc3e964ddb2be47a25581780
av.zalyaev <av.zalyaev@tensor.ru>
2018-07-02 19:29:08 +0300

    https://online.sbis.ru/opendoc.html?guid=6ac9cfe7-0c5f-4eee-a5a7-b4ecf7aadf30 Base Attach extends abstract

--- File/Attach/IContainer.d.ts
+++ File/Attach/IContainer.d.ts
@@ -2,6 +2,7 @@
 import Deferred = require("Core/Deferred");
 import {Source} from "File/Attach/Source";
 import {IResourceConstructor} from "File/IResource";
+import {IResourceGetter} from "File/IResourceGetter";
 
 export interface IContainer <T> {
     push(element: T, ...args): void;
@@ -17,4 +18,13 @@ export interface IContainerLazy<T, Key = string> extends IContainer<T>{
 export interface ISourceContainer extends IContainer<Source>{
     getRegisteredResource(): Array<IResourceConstructor>
 }
-export interface ISourceContainerLazy extends IContainerLazy<Source>, ISourceContainer {}
+export interface ISourceContainerLazy<Key = IResourceConstructor> extends
+    IContainerLazy<Source, Key>,
+    ISourceContainer
+{}
+
+export interface IGetterContainer extends IContainer<IResourceGetter> {}
+export interface IGetterContainerLazy extends
+    IGetterContainer,
+    IContainerLazy<IResourceGetter, string>
+{}
