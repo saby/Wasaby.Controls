01ebca95e7dec8f14dbc2a99228f5352edd36a05
nv.belotelov <nv.belotelov@tensor.ru>
2018-07-02 17:38:52 +0300

    https://online.sbis.ru/opendoc.html?guid=9b01d585-104f-4925-a930-a69dcfef5ae6

--- tests/unit/DepsCollector/DepsCollector.test.js
+++ tests/unit/DepsCollector/DepsCollector.test.js
@@ -2,87 +2,86 @@ define([
    'Controls/Application/DepsCollector/DepsCollector'
 ], function(DepsCollector) {
    var modDeps = {
-      "ACS/AddressForm/AddressForm": ["tmpl", "tmpl!ACS/AddressForm/AddressForm"],
-      "AccountCertificates/Obrabotchiki/ReestrHandlers": [],
-      "css!AccountCertificates/components/CarrierSigns/CarrierSigns": [],
-      "tmpl!ActiveSales/Controls/ClientsBlock": [],
-      "css!AuthFramework/OAuth/Abstract": [],
-      "Controls/Button": ["Core/Control", "css"],
-      "Core/Control": ["View/Runner/markupGenerator"],
+      "aaa/aaa": [],
+      "css!aaa/bbb": [],
+      "tmpl!aaa/ccc": [],
+      "css!aaa/ddd": [],
+      "ccc/aaa": ["ccc/ccc", "css"],
+      "ccc/ccc": ["ddd/aaa"],
       "css": [],
-      "Controls/Input": [],
-      "NotificationsCenter/VDOM/Util/Refresher": [],
-      "tmpl!NotificationsCenter/VDOM/Util/Refresher": []
+      "ccc/bbb": [],
+      "xxx/aaa": [],
+      "tmpl!xxx/aaa": []
    };
    var modInfo = {
-      "css!AuthFramework/OAuth/Abstract": {path: "resources/AuthFramework/OAuth/Abstract.min.css"},
-      "NotificationsCenter/VDOM/Util/Refresher": {path: "resources/NotificationsCenter/VDOM/Util/Refresher.min.js"},
-      "tmpl!NotificationsCenter/VDOM/Util/Refresher": {path: "resources/NotificationsCenter/VDOM/Util/Refresher.min.tmpl"}
+      "css!aaa/ddd": {path: "resources/aaa/ddd.min.css"},
+      "xxx/aaa": {path: "resources/xxx/aaa.min.js"},
+      "tmpl!xxx/aaa": {path: "resources/xxx/aaa.min.tmpl"}
    };
    var bundlesRoute = {
-      "AccountCertificates/Obrabotchiki/ReestrHandlers": "resources/AccountCertificates/packages/signs_registry.package.min.js",
-      "css!AccountCertificates/components/CarrierSigns/CarrierSigns": "resources/AccountCertificates/packages/signs_registry.package.min.css",
-      "tmpl!ActiveSales/Controls/ClientsBlock": "resources/ActiveSales/crm_sales.package.min.js",
-      "Global_Catalog/Prices/Dialog/NomenclatureSelector": "resources/Global_Catalog/package/Prices.package.min.js",
-      "Global_Catalog/Prices/Dialog/PriceMiniCard": "resources/Global_Catalog/package/Prices.package.min.js",
-      "Controls/Button": "resources/Controls/controls-button.package.min.js",
-      "Core/Control": "resources/WS.Core/core-min.package.min.js",
-      "css": "resources/WS.Core/requirejs-plugins.package.min.js",
-      "View/Runner/markupGenerator": "resources/View/runner.package.min.js",
-      "NotificationsCenter/VDOM/Util/Refresher": "resources/NotificationsCenter/VDOM/VDOMNotificationCenter.package.min.js"
+      "aaa/aaa": "resources/bdl/aaa.package.min.js",
+      "css!aaa/bbb": "resources/bdl/aaa.package.min.css",
+      "tmpl!aaa/ccc": "resources/bdl/bbb.package.min.js",
+      "vvv/aaa": "resources/bdl/ccc.package.min.js",
+      "vvv/bbb": "resources/bdl/ccc.package.min.js",
+      "ccc/aaa": "resources/bdl/ddd.package.min.js",
+      "ccc/ccc": "resources/bdl/eee.package.min.js",
+      "css": "resources/bdl/ggg.package.min.js",
+      "ddd/aaa": "resources/bdl/hhh.package.min.js",
+      "xxx/aaa": "resources/bdl/jjj.package.min.js"
    }
    var depsCollector = new DepsCollector(modDeps, modInfo, bundlesRoute);
    describe('DepsCollector', function() {
       it('single in bundle', function() {
-         var deps = depsCollector.collectDependencies(["AccountCertificates/Obrabotchiki/ReestrHandlers"]);
+         var deps = depsCollector.collectDependencies(["aaa/aaa"]);
          assert.deepEqual(deps, {
-            "js": ["/resources/AccountCertificates/packages/signs_registry.package.min.js"],
+            "js": ["/resources/bdl/aaa.package.min.js"],
             "css": []
          });
       });
       it('several in bundle', function() {
-         var deps = depsCollector.collectDependencies(["Global_Catalog/Prices/Dialog/NomenclatureSelector", "Global_Catalog/Prices/Dialog/PriceMiniCard"]);
-         assert.deepEqual(deps, {"js": ["/resources/Global_Catalog/package/Prices.package.min.js"], "css": []});
+         var deps = depsCollector.collectDependencies(["vvv/aaa", "vvv/bbb"]);
+         assert.deepEqual(deps, {"js": ["/resources/bdl/ccc.package.min.js"], "css": []});
       });
       it('css-bundle hook js', function() {
-         var deps = depsCollector.collectDependencies(["css!AccountCertificates/components/CarrierSigns/CarrierSigns"]);
+         var deps = depsCollector.collectDependencies(["css!aaa/bbb"]);
          assert.deepEqual(deps, {
-            "js": ["/resources/AccountCertificates/packages/signs_registry.package.min.js"],
-            "css": ["/resources/AccountCertificates/packages/signs_registry.package.min.css"]
+            "js": ["/resources/bdl/aaa.package.min.js"],
+            "css": ["/resources/bdl/aaa.package.min.css"]
          });
       });
       it('single css not hook js', function() {
-         var deps = depsCollector.collectDependencies(["css!AuthFramework/OAuth/Abstract"]);
-         assert.deepEqual(deps, {"js": [], "css": ["/resources/AuthFramework/OAuth/Abstract.min.css"]});
+         var deps = depsCollector.collectDependencies(["css!aaa/ddd"]);
+         assert.deepEqual(deps, {"js": [], "css": ["/resources/aaa/ddd.min.css"]});
       });
       it('recursive', function() {
-         var deps = depsCollector.collectDependencies(["Controls/Button"]);
+         var deps = depsCollector.collectDependencies(["ccc/aaa"]);
          assert.deepEqual(deps, {
-            "js": ["/resources/Controls/controls-button.package.min.js",
-               "/resources/WS.Core/core-min.package.min.js",
-               "/resources/View/runner.package.min.js",
-               "/resources/WS.Core/requirejs-plugins.package.min.js"],
+            "js": ["/resources/bdl/ddd.package.min.js",
+               "/resources/bdl/eee.package.min.js",
+               "/resources/bdl/hhh.package.min.js",
+               "/resources/bdl/ggg.package.min.js"],
             "css": []
          });
       });
       it('optional pre-load', function() {
-         var deps = depsCollector.collectDependencies(["optional!NotificationsCenter/VDOM/Util/Refresher"]);
+         var deps = depsCollector.collectDependencies(["optional!xxx/aaa"]);
          assert.deepEqual(deps, {
-            "js": ["/resources/NotificationsCenter/VDOM/VDOMNotificationCenter.package.min.js"],
+            "js": ["/resources/bdl/jjj.package.min.js"],
             "css": []
          });
       });
       it('optional no pre-load', function() {
-         var deps = depsCollector.collectDependencies(["optional!Controls/Input"]);
+         var deps = depsCollector.collectDependencies(["optional!ccc/bbb"]);
          assert.deepEqual(deps, {
             "js": [],
             "css": []
          });
       });
       it('tmpl', function() {
-         var deps = depsCollector.collectDependencies(["tmpl!NotificationsCenter/VDOM/Util/Refresher"]);
+         var deps = depsCollector.collectDependencies(["tmpl!xxx/aaa"]);
          assert.deepEqual(deps, {
-            "js": ["/resources/NotificationsCenter/VDOM/Util/Refresher.min.tmpl"],
+            "js": ["/resources/xxx/aaa.min.tmpl"],
             "css": []
          });
       })
