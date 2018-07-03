75ee8873f0c633eaab6cd6fe8bb16209f453d53f
am.gerasimov <am.gerasimov@tensor.ru>
2018-07-03 14:55:07 +0300

    https://online.sbis.ru/opendoc.html?guid=13a2d6c0-adcc-49bf-89b7-733923957f5b

--- Controls/Container/Data/ContextOptions.js
+++ Controls/Container/Data/ContextOptions.js
@@ -3,12 +3,10 @@
  * @author Герасимов Александр
  * @class Controls/Container/ContextContainerOptions
  */
-define('Controls/Container/SourceBase/ContextContainerOptions', ['Core/DataContext'], function(DataContext) {
+define('Controls/Container/Data/ContextOptions', ['Core/DataContext'], function(DataContext) {
    'use strict';
    
    return DataContext.extend({
-      containerOptions: null,
-      
       constructor: function(options) {
          for (var i in options) {
             if (options.hasOwnProperty(i)) {
