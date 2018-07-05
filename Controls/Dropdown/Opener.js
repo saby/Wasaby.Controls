7cd91128bb9d45218df85866487286264cff0af0
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-07-05 15:49:37 +0300

    https://online.sbis.ru/opendoc.html?guid=370289e7-79f0-4c3c-b4cc-8c9aa7e95c8c

--- Controls/Dropdown/Opener.js
+++ Controls/Dropdown/Opener.js
@@ -1,8 +1,9 @@
 define('Controls/Dropdown/Opener',
    [
-      'Controls/Popup/Opener/Sticky'
+      'Controls/Popup/Opener/Sticky',
+      'Core/core-merge'
    ],
-   function(Sticky) {
+   function(Sticky, coreMerge) {
       /**
        * Opener for dropdown menu.
        *
@@ -101,9 +102,9 @@ define('Controls/Dropdown/Opener',
       DropdownOpener._private = _private;
 
       DropdownOpener.getDefaultOptions = function() {
-         return {
-            closeOnTargetScroll: true
-         };
+         return coreMerge(
+            {closeOnTargetScroll: true},
+            Sticky.getDefaultOptions());
       };
 
       return DropdownOpener;
