c72b47fafcbe975ac184b185bd116c0f12e1130f
ka.sannikov <ka.sannikov@tensor.ru>
2018-07-09 16:42:15 +0300

    https://online.sbis.ru/opendoc.html?guid=dc0e25c8-64a2-425b-b6dc-91c38be95d61

--- File/IResourceGetter.js
+++ File/IResourceGetter.js
@@ -1,4 +1,4 @@
-define("File/IResourceGetter.ts", ["require", "exports"], function (require, exports) {
+define("File/IResourceGetter", ["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
 });
