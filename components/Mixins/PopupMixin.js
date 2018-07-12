bc3024c0d236fa37f1559a1384bae822aa6abc6e
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-07-12 10:38:18 +0300

    https://online.sbis.ru/opendoc.html?guid=d765cfba-6ec4-4042-ace3-df6fb3281814

--- components/Mixins/PopupMixin.js
+++ components/Mixins/PopupMixin.js
@@ -1255,7 +1255,7 @@ define('SBIS3.CONTROLS/Mixins/PopupMixin', [
             }
          } else {
             var opener = this.getOpener() || this.getParent();
-            var openerPopup = opener && opener._container && opener._container.closest('.controls-Popup, .controls-FloatArea, .ws-window');
+            var openerPopup = opener && opener._container && opener._container.closest('.controls-Popup, .controls-FloatArea, .ws-window:not(".controls-CompoundArea")');
             var openerPopupZIndex = openerPopup && openerPopup.css('z-index');
 
             if (openerPopupZIndex) {
