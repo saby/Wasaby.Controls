6f31c1eb16468a8b67aa005f6997bdabe647a17d
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-07-13 17:12:19 +0300

    https://online.sbis.ru/opendoc.html?guid=2c6d6c36-7786-4622-8ab2-591ba8e15704

--- Controls-demo/Input/Password/Basic.js
+++ Controls-demo/Input/Password/Basic.js
@@ -11,63 +11,8 @@ define('Controls-demo/Input/Password/Basic',
 
       'use strict';
 
-      var confirmationCfg = {
-         type: 'ok',
-         style: 'error',
-         message: 'The password contains non-valid characters.'
-      };
-
-      function getMessage(tagStyle) {
-         var message;
-
-         switch (tagStyle) {
-            case 'done':
-               message = 'The password consists of letters and numbers';
-               break;
-            case 'error':
-               message = 'The password must consist only of letters and numbers';
-               break;
-         }
-
-         return message;
-      }
-
       return Control.extend({
-         _template: template,
-
-         _tagStyle: 'done',
-
-         _beforeMount: function() {
-            var _this = this;
-
-            this._validationHandler = function(result) {
-               if (result) {
-                  _this._tagStyle = 'error';
-                  _this._children.confirmation.open(confirmationCfg);
-               } else {
-                  _this._tagStyle = 'done';
-               }
-            };   
-         },
-
-         _tagClickHandler: function(event) {
-            var infoBox = this._children.infoBox;
-
-            if (infoBox.isOpened()) {
-               infoBox.close();
-            } else {
-               infoBox.open({
-                  target: event.target,
-                  message: getMessage(this._tagStyle)
-               });
-            }
-         },
-
-         _inputCompletedHandler: function() {
-            return this._children.validateController
-               .validate()
-               .addCallback(this._validationHandler);
-         }
+         _template: template
       });
    }
 );
--- Controls-demo/Input/Password/Basic/validator.js
+++
@@ -1,8 +0,0 @@
-define('Controls-demo/Input/Password/Basic/validator', [], function() {
-
-   'use strict';
-
-   return function(args) {
-      return /^([0-9A-Za-z])*$/.test(args.value) ? true : 'Valid characters are a-z and 0-9';
-   };
-});
