c912bbc0cfb63ea5c69741feb743e1401442345f
ls.baranova <ls.baranova@tensor.ru>
2018-07-16 16:58:18 +0300

    правки

--- Controls/Application/Core.js
+++ Controls/Application/Core.js
@@ -6,16 +6,50 @@ define('Controls/Application/Core',
       'Core/Control',
       'tmpl!Controls/Application/Core',
       'Controls/Application/AppData',
-      'Controls/Application/HeadDataContext'
+      'Controls/Application/HeadDataContext',
+      'native-css'
    ],
-   function(Control, template, AppData, HeadDataContext) {
+   function(Control, template, AppData, HeadDataContext, nativeCss) {
 
       'use strict';
+
+      /*COPYPASTE FROM Core/css-resolver
+      * заливаю так
+      * когда WS соберется и попадет в SDK - этот кусок будет удален
+      * */
+
+      var global = (function() {
+         return this || (0, eval)('this');
+      }());
+
+      var buildMode = global.contents ? global.contents.buildMode : 'debug',
+         isDebugMode = function() {
+            return global.document && global.document.cookie && global.document.cookie.indexOf('s3debug=true') > -1;
+         };
+
+      var suffix = '';
+      if (buildMode === 'release' && !isDebugMode()) {
+         suffix = '.min';
+      }
+      if (global.buildnumber && !isDebugMode()) {
+         suffix += '.v' + global.buildnumber;
+      }
+      
+      function getCssP(path) {
+         return (window.wsConfig.resourceRoot + '/' + path + suffix + '.css').replace('//', '/');
+      }
 
       var AppCore = Control.extend({
          _template: template,
          ctxData: null,
          constructor: function(cfg) {
+            var self = this;
+            nativeCss.load = function(path, require, load, conf) {
+               load(null);
+               self.headDataCtx.pushCssLink(getCssP(path));
+               self.headDataCtx.updateConsumers();
+            };
+
             try {
                /*TODO: set to presentation service*/
                process.domain.req.compatible = false;
@@ -24,17 +58,20 @@ define('Controls/Application/Core',
 
             AppCore.superclass.constructor.apply(this, arguments);
             this.ctxData = new AppData(cfg);
-            this.headDataCtx = new HeadDataContext(cfg.theme || '', cfg.buildnumber);
+            this.headDataCtx = new HeadDataContext(cfg.theme || '', cfg.buildnumber, cfg.cssLinks);
          },
          _getChildContext: function() {
             return {
                AppData: this.ctxData,
                headData: this.headDataCtx
             };
+         },
+         coreTheme: '',
+         setTheme: function(ev, theme) {
+            this.coreTheme = theme;
          }
       });
 
       return AppCore;
    }
 );
-
