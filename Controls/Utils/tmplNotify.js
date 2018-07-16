d13fd8d84ad2f07fd6c275e2f31a4ec8bd287d32
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-07-16 15:42:09 +0300

    https://online.sbis.ru/opendoc.html?guid=da665fb8-94e3-4663-a2ee-48a0b8a1508d

--- Controls/Utils/tmplNotify.js
+++ Controls/Utils/tmplNotify.js
@@ -3,7 +3,7 @@ define('Controls/Utils/tmplNotify', [], function() {
    'use strict';
 
    /**
-    * A handler to use in the template to carry out event forwarding up.
+    * A handler to use in templates to proxy events to the logic parent.
     */
    return function(event, eventName) {
       var args = Array.prototype.slice.call(arguments, 2);
