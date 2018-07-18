de7bfe3c066d1fc4b7c5dcd6cfdb2221f2731636
as.avramenko <as.avramenko@tensor.ru>
2018-07-18 15:03:06 +0300

    https://online.sbis.ru/opendoc.html?guid=e4d479a6-a2d1-470c-899a-1baf6028ff21

--- tests/unit/Explorer/Explorer.test.js
+++ tests/unit/Explorer/Explorer.test.js
@@ -2,9 +2,16 @@ define(['Controls/Explorer'], function(Explorer) {
    describe('Controls.Explorer', function() {
       it('_private block', function() {
          var
+            dataLoadCallbackCalled = false,
+            dataLoadCallback = function() {
+               dataLoadCallbackCalled = true;
+            },
             forceUpdate = function() {},
             self = {
-               _forceUpdate: forceUpdate
+               _forceUpdate: forceUpdate,
+               _options: {
+                  dataLoadCallback: dataLoadCallback
+               }
             },
             testRoot = 'testRoot',
             testBreadCrumbs = [
@@ -25,27 +32,43 @@ define(['Controls/Explorer'], function(Explorer) {
                }
             };
          Explorer._private.setRoot(self, testRoot);
-         assert.deepEqual({ _root: 'testRoot', _forceUpdate: forceUpdate }, self, 'Incorrect self data after "setRoot(self, testRoot)".');
+         assert.deepEqual({
+            _root: 'testRoot',
+            _forceUpdate: forceUpdate,
+            _options: {
+               dataLoadCallback: dataLoadCallback
+            }
+         }, self, 'Incorrect self data after "setRoot(self, testRoot)".');
          Explorer._private.dataLoadCallback(self, testData1);
          assert.deepEqual({
             _root: 'testRoot',
             _forceUpdate: forceUpdate,
             _breadCrumbsItems: [],
-            _breadCrumbsVisibility: false
+            _breadCrumbsVisibility: false,
+            _options: {
+               dataLoadCallback: dataLoadCallback
+            }
          }, self, 'Incorrect self data after "dataLoadCallback(self, testData1)".');
+         assert.isTrue(dataLoadCallbackCalled, 'Incorrect value "dataLoadCallbackCalled" after call "dataLoadCallback(...)".');
          Explorer._private.dataLoadCallback(self, testData2);
          assert.deepEqual({
             _root: 'testRoot',
             _forceUpdate: forceUpdate,
             _breadCrumbsItems: testBreadCrumbs,
-            _breadCrumbsVisibility: true
+            _breadCrumbsVisibility: true,
+            _options: {
+               dataLoadCallback: dataLoadCallback
+            }
          }, self, 'Incorrect self data after "dataLoadCallback(self, testData2)".');
          Explorer._private.dataLoadCallback(self, testData1);
          assert.deepEqual({
             _root: 'testRoot',
             _forceUpdate: forceUpdate,
             _breadCrumbsItems: [],
-            _breadCrumbsVisibility: false
+            _breadCrumbsVisibility: false,
+            _options: {
+               dataLoadCallback: dataLoadCallback
+            }
          }, self, 'Incorrect self data after "dataLoadCallback(self, testData1)".');
       });
       
