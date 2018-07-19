de0c0ad62e7b0399b8b6dac908614b2d96c8f98d
ee.zolotova <ee.zolotova@tensor.ru>
2018-07-19 15:43:12 +0300

    https://online.sbis.ru/opendoc.html?guid=1397edc3-59d4-44d4-8564-fe34dc064bed

--- Controls-demo/Layouts/SearchLayout.js
+++ Controls-demo/Layouts/SearchLayout.js
@@ -132,19 +132,32 @@ define('Controls-demo/Layouts/SearchLayout', [
       }
    };
 
+   var sourceId = {
+      module: 'WS.Data/Source/Memory',
+      options: {
+         data: [
+            {key: 0, title: 'все id'},
+            {key: 1, title: '1'},
+            {key: 2, title: '2'},
+            {key: 3, title: '3'}
+         ],
+         idProperty: 'key'
+      }
+   };
+
    var filterSourceData = [
       {id: 'FIO', value: '', resetValue: '', visibility: false},
       {id: 'firstName', value: '', resetValue: '', visibility: false},
-      {id: 'id', value: [0], resetValue: [0], source: sourceDropdown, visibility: false},
-      {id: 'checked', value: true, resetValue: false, textValue: 'checked', visibility: false},
+      {id: 'id', value: [0], resetValue: [0], source: sourceId, textValue: '', visibility: false},
+      {id: 'checked', value: false, resetValue: false, textValue: 'checked', visibility: false},
       {id: 'Test1', value: [2], resetValue: [2], source: sourceDropdown, visibility: false},
-      {id: 'Test2', value: true, resetValue: false, textValue: 'Test2', visibility: false},
-      {id: 'Test3', value: true, resetValue: false, textValue: 'Test3', visibility: false},
-      {id: 'Test4', value: true, resetValue: false, textValue: 'Test4', visibility: false},
-      {id: 'Test5', value: true, resetValue: false, textValue: 'FilterText', visibility: false},
-      {id: 'Test6', value: true, resetValue: false, textValue: 'Test6', visibility: false},
-      {id: 'Test7', value: true, resetValue: false, textValue: 'Test7', visibility: false},
-      {id: 'Test8', value: true, resetValue: false, textValue: 'Test8', visibility: true}
+      {id: 'Test2', value: false, resetValue: false, textValue: 'Test2', visibility: false},
+      {id: 'Test3', value: false, resetValue: false, textValue: 'Test3', visibility: false},
+      {id: 'Test4', value: false, resetValue: false, textValue: 'Test4', visibility: false},
+      {id: 'Test5', value: false, resetValue: false, textValue: 'FilterText', visibility: false},
+      {id: 'Test6', value: false, resetValue: false, textValue: 'Test6', visibility: false},
+      {id: 'Test7', value: false, resetValue: false, textValue: 'Test7', visibility: false},
+      {id: 'Test8', value: false, resetValue: false, textValue: 'Test8', visibility: true}
    ];
 
    var ModuleClass = BaseControl.extend(
