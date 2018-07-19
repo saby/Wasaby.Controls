c841e80e13e2f5babf309bc2bf1a40ac1aaf1966
ee.zolotova <ee.zolotova@tensor.ru>
2018-07-19 14:42:40 +0300

    https://online.sbis.ru/opendoc.html?guid=1397edc3-59d4-44d4-8564-fe34dc064bed

---
+++ Controls/interface/ITextValue.js
@@ -0,0 +1,20 @@
+define('Controls/interface/ITextValue', [], function() {
+
+   /**
+    * Interface for controls using in the filter.
+    *
+    * @mixin Controls/interface/ITextValue
+    * @public
+    */
+
+   /**
+    * @name Controls/interface/ITextValue#textValue
+    * @cfg {String} Field storing textValue.
+    */
+
+   /**
+    * @event Controls/interface/ITextValue#textValueChanged Occurs when field textValue was changed.
+    * @param {String} textValue New field textValue.
+    */
+
+});
