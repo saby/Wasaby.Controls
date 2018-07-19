cd9b2025ed70ced420503318776850e106499a50
am.gerasimov <am.gerasimov@tensor.ru>
2018-07-19 09:48:20 +0300

    Revert "https://online.sbis.ru/opendoc.html?guid=b40ab92b-bcf0-4b0b-8092-3415e2efda19"
    This reverts commit bb52e9caa6cbce6bf4fcd4a9eaa4139ba687bb97.

--- components/Utils/FilterPanelUtils.js
+++ components/Utils/FilterPanelUtils.js
@@ -119,11 +119,13 @@ define('SBIS3.CONTROLS/Utils/FilterPanelUtils',
             },
             handlers: {},
             closeButton: true,
-            locationStrategy: detection.isMobilePlatform && !addConfig.task1175546430 ? 'dontMove' : null,
+            locationStrategy: detection.isMobilePlatform ? 'dontMove' : null,
             closeByExternalClick: true,
             closeOnTargetMove: !detection.isMobilePlatform,
+            recalculateOnKeyboardShow: false,
             cssClassName: 'controls__filterButton__picker',
             _canScroll: true,
+            _resizeOnScroll: false,
             activateAfterShow: true
          };
          Object.assign(config, addConfig);
