71eabaed6bf506a08bebb04449fbb6c3908d9d22
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2018-07-20 17:08:09 +0300

    тесты

--- Controls/Container/Data.js
+++ Controls/Container/Data.js
@@ -66,7 +66,7 @@ define('Controls/Container/Data',
                   //компоненте, при вызове метода query. Поэтому на клиент прилетает неправильное состояние и
                   //первый вызов query возвращает захэшированные данные, а не пытается загрузить актуальные данные.
                   //Выписана задача для удаления данного костыля: https://online.sbis.ru/opendoc.html?guid=fb540e42-278c-436c-928b-92e6f72b3abc
-                  result._prefetchSource._done.query = true;
+                  self._prefetchSource._done.query = true;
                   return result;
                });
             }
