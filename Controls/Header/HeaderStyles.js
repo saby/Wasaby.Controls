0bfa753aba304929707018a8a13bcdaedebb994f
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-07-23 15:56:10 +0300

    https://online.sbis.ru/opendoc.html?guid=2474cc6d-93cd-44db-83d4-d2d0854702d4

---
+++ Controls/Header/HeaderStyles.js
@@ -0,0 +1,23 @@
+define('Controls/Header/HeaderStyles.js',
+   [
+   ],
+   function() {
+   }
+
+   /**
+    *
+    * @mixin Controls/Header/HeaderStyles
+    * @public
+    *
+    * @css @font-size_Header__caption_size-s S size of header text.
+    * @css @font-size_Header__caption_size-default Default size of header text.
+    * @css @font-size_Header__caption_size-l L size of header text.
+    * @css @font-size_Header__caption_size-xl Xl size of header text.
+    *
+    * @css @color_Header__caption_style-primary Text color at primary style.
+    * @css @color_Header__caption_style-default Text color at default style.
+    *
+    * @css @color_Header__caption_style-primary_hovered Text color at primary style in hovered and activated state.
+    * @css @color_Header__caption_style-default_hovered Text color at default style in hovered and activated state.
+    */
+);
