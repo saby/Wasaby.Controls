96ae97b1340f529b700a284864b92eeaba703d73
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-07-25 00:29:26 +0300

    https://online.sbis.ru/opendoc.html?guid=76a6eeea-ebb2-4176-95c8-548e53ca6f00

--- Controls-demo/RadioGroup/RadioGroupDemo.js
+++ Controls-demo/RadioGroup/RadioGroupDemo.js
@@ -8,7 +8,7 @@ define('Controls-demo/RadioGroup/RadioGroupDemo', [
    'tmpl!Controls-demo/RadioGroup/resources/UnionItemTemplate',
    'css!Controls-demo/RadioGroup/RadioGroupDemo',
    'WS.Data/Collection/RecordSet'// Удалить после мержа https://online.sbis.ru/opendoc.html?guid=6989b29a-8e1d-4c3b-bb7d-23b09736ef2c
-], function (Control,
+], function(Control,
              template,
              MemorySource,
              CustomItemTemplate,
@@ -17,51 +17,120 @@ define('Controls-demo/RadioGroup/RadioGroupDemo', [
 ) {
    'use strict';
    var source = new MemorySource({
-         idProperty: 'id',
-         displayProperty: 'caption',
-         data: [
-            {
-               id: "1",
-               title: 'Значение1',
-               caption: 'caption1'
-            },
-            {
-               id: "2",
-               title: 'Значение2',
-               caption: 'caption2'
-            },
-            {
-               id: "3",
-               title: 'Значение3',
-               templateTwo: 'tmpl!Controls-demo/RadioGroup/resources/SingleItemTemplate',
-               caption: 'caption3'
-            },
-            {
-               id: "4",
-               title: 'Значение4',
-               caption: 'caption4'
-            },
-            {
-               id: "5",
-               title: 'Значение5',
-               caption: 'caption5'
-            },
-            {
-               id: "6",
-               title: 'Значение6',
-               caption: 'caption6'
-            }
-         ]
-      });
+      idProperty: 'id',
+      displayProperty: 'caption',
+      data: [
+         {
+            id: '1',
+            title: 'Caption1',
+            caption: 'Additional caption1'
+         },
+         {
+            id: '2',
+            title: 'Caption2',
+            caption: 'Additional caption2'
+         },
+         {
+            id: '3',
+            title: 'Caption3',
+            templateTwo: 'tmpl!Controls-demo/RadioGroup/resources/SingleItemTemplate',
+            caption: 'Additional caption3'
+         },
+         {
+            id: '4',
+            title: 'Caption4',
+            caption: 'Additional caption4'
+         },
+         {
+            id: '5',
+            title: 'Caption5',
+            caption: 'Additional caption5'
+         },
+         {
+            id: '6',
+            title: 'Caption6',
+            caption: 'Additional caption6'
+         }
+      ]
+   });
+
+   var source2 = new MemorySource({
+      idProperty: 'id',
+      data: [
+         {
+            id: '1',
+            title: 'Header1'
+         },
+         {
+            id: '2',
+            title: 'Header2'
+         },
+         {
+            id: '3',
+            title: 'Header3'
+         },
+         {
+            id: '4',
+            title: 'Header4'
+         },
+         {
+            id: '5',
+            title: 'Header5'
+         },
+         {
+            id: '6',
+            title: 'Header6'
+         }
+      ]
+   });
+
+   var sourceOfSource = new MemorySource({
+      idProperty: 'title',
+      data: [
+         {
+            title: 'Source',
+            source: source
+         },
+         {
+            title: 'Source2',
+            source: source2
+         }
+      ]
+   });
+
+   var directionSource = new MemorySource({
+      idProperty: 'title',
+      data: [
+         {
+            title: 'horizontal'
+         },
+         {
+            title: 'vertical'
+         }
+      ]
+   });
 
    var RadioGroupDemo = Control.extend({
       _template: template,
       _customItemTemplate: CustomItemTemplate,
       _unionItemTemplate: UnionItemTemplate,
       _source: source,
+      _sourceOfSource: sourceOfSource,
       _selectKey: null,
-      changeKey: function (e, key) {
-         this._selectKey=key;
+      _selectedSource: 'Source',
+      _selectedDirection: 'vertical',
+      _directionSource: directionSource,
+
+      changeKey: function(e, key) {
+         this._selectKey = key;
+      },
+      changeSource: function(e, key) {
+         sourceOfSource.read(key).addCallback(function(source) {
+            this._source = source;
+         });
+      },
+      changeDirection: function(e, key) {
+         this._selectedDirection = key;
       }
    });
    return RadioGroupDemo;
