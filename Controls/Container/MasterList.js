6734b6587475a1f6929c1a3d6fec87340eec40e9
vd.volockoy <vd.volockoy@tensor.ru>
2018-07-24 21:27:21 +0300

    Правки по less

--- Controls/Container/MasterList.js
+++ Controls/Container/MasterList.js
@@ -1,11 +1,12 @@
 define('Controls/Container/MasterList', [
    'Core/Control',
-   'tmpl!Controls/Container/MasterList/MasterList'
+   'tmpl!Controls/Container/MasterList/MasterList',
+   'css!Controls/Container/MasterList/MasterList'
 ], function(Control, template) {
    return Control.extend({
       _template: template,
-      _itemClick: function(event, item) {
-         this._notify('selectedMasterFieldChanged', [item]);
+      _itemClickHandler: function(event, item) {
+         this._notify('selectedMasterFieldChanged', [item], {bubbling: true});
       }
    });
 });
