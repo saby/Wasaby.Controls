52e09d5badd0dd5dce2edeb51ccf6f32579b7051
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2018-07-25 16:45:24 +0300

    https://online.sbis.ru/opendoc.html?guid=665aa72b-dbad-4f58-8997-13f0752c5ade

--- Controls/Operations/Button.js
+++ Controls/Operations/Button.js
@@ -10,9 +10,16 @@ define('Controls/Operations/Button', [
     *
     * @class Controls/Operations/Button
     * @extends Core/Control
-    * @mixes Controls/List/interface/IExpandable
+    * @mixes Controls/interface/IExpandable
     * @control
     * @public
+    *
+    * @css @width_OperationsButton
+    * @css @height_OperationsButton
+    * @css @color_OperationsButton__icon
+    * @css @color_OperationsButton__icon_hovered
+    * @css @color_OperationsButton__icon_readonly
+    * @css @font-size_OperationsButton__icon
     */
 
    return Control.extend({
