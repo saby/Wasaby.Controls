686696a7b3f182ddb2c049245d6978980e8316c7
va.spirin <va.spirin@tensor.ru>
2018-07-26 20:08:37 +0700

    LongOperations: Локализация (plural)
    1175628926 https://online.sbis.ru/opendoc.html?guid=fb7d189b-e695-474d-b96c-72372b602f6f

--- components/LongOperations/Popup.js
+++ components/LongOperations/Popup.js
@@ -496,7 +496,7 @@ define('SBIS3.CONTROLS/LongOperations/Popup',
             $tasks[needMsg ? 'removeClass' : 'addClass']('ws-hidden');
             if (needMsg) {
                $tasks.text(
-                  Math.floor(current) + ' ' + (100 <= total ? '/' : rk('из')) + ' ' + total + ' ' + (20 < total && total % 10 === 1 ? rk('операции', 'ДлительныеОперации') : rk('операций', 'ДлительныеОперации'))
+                  Math.floor(current) + ' ' + (100 <= total ? '/' : rk('из')) + ' ' + total + ' ' + rk('операции(-й,-й)', 'ДлительныеОперации', total)
                );
             }
             this.getContainer().find('.controls-LongOperationsPopup__footer_progress').text(Math.floor(100*current/total) + '%');
