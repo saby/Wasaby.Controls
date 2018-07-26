58482efd22b3a36993b9f28e367cd1b5917eb9ee
ee.zolotova <ee.zolotova@tensor.ru>
2018-07-26 17:31:28 +0300

    https://online.sbis.ru/opendoc.html?guid=a91a5908-c8e7-4fd8-b3c6-34302c0d95b9

---
+++ Controls/Filter/Button/History/resources/historyUtils.js
@@ -0,0 +1,34 @@
+define('Controls/Filter/Button/History/resources/historyUtils',
+   [
+      'Controls/History/FilterSource',
+      'Controls/History/Service',
+      'WS.Data/Source/Memory',
+      'WS.Data/Di'
+   ],
+   function(HistorySource, HistoryService, MemorySource, Di) {
+
+      'use strict';
+
+      function getHistorySource(hId) {
+         return new HistorySource({
+            originSource: new MemorySource({
+               idProperty: 'id',
+               data: []
+            }),
+            historySource: Di.isRegistered('sourceHistory') ? Di.resolve('sourceHistory', {
+               historyId: hId,
+               pinned: true,
+               dataLoaded: true
+            }) : new HistoryService({
+               historyId: hId,
+               pinned: true,
+               dataLoaded: true
+            })
+         });
+      }
+
+      return {
+         getHistorySource: getHistorySource
+      };
+   }
+);
