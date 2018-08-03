616ff7bec43099d2937cbf560602f1be366e7dd9
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-08-03 10:13:46 +0300

    https://online.sbis.ru/opendoc.html?guid=fc9bf844-ec99-4abe-9a6d-06943f76fa12

--- Controls/Input/resources/InputRender/InputRenderStyles.js
+++ Controls/Input/resources/InputRender/InputRenderStyles.js
@@ -5,21 +5,21 @@ define('Controls/Input/resources/InputRender/InputRenderStyles',
    function() {
       /**
        * @interface Controls/Input/resources/InputRender/InputRenderStyles
-       * @css @height_InputRender_m The middle height of the input field container.
-       * @css @height_InputRender_l The large height of the input field container.
-       * @css @height_InputRender_default The default height of the input field container.
-       * @css @font-size_InputRender_m The middle font size of the input field.
-       * @css @font-size_InputRender_l The large font size of the input field.
-       * @css @font-size_InputRender_default The default font size of the input field.
+       * @css @height_InputRender_m Height of the medium sized input field.
+       * @css @height_InputRender_l Height of the large sized input field.
+       * @css @height_InputRender_default Height of the default sized input field.
+       * @css @font-size_InputRender_m Font size of the medium sized input field.
+       * @css @font-size_InputRender_l Font size of the large sized input field.
+       * @css @font-size_InputRender_default Font size of the default sized input field.
        * @css @font-size_InputRender_style_header The font size of the input field with the style option set to "header".
        * @css @font-size_InputRender_style_accentHeader The font size of the input field with the style option set to "accentHeader".
        * @css @padding_InputRender Input padding.
        * @css @border-width_InputRender The size of the border of the input field.
        * @css @width_InputRender-tagStyle The width of the information icon.
        * @css @height_InputRender-tagStyle The height of the information icon.
-       * @css @line-height_InputRender_m The middle line height of the multi-line input field.
-       * @css @line-height_InputRender_l The large line height of the multi-line input field.
-       * @css @line-height_InputRender_default The default line height of the multi-line input field.
+       * @css @line-height_InputRender_m Line height of the medium sized multi-line input field.
+       * @css @line-height_InputRender_l Line height of the large sized multi-line input field.
+       * @css @line-height_InputRender_default Line height of the default sized multi-line input field.
        * @css @border-color_InputRender The color of the border of the input field.
        * @css @border-color_InputRender_hover The color of the border of the input field at hover.
        * @css @border-color_InputRender_active The color of the border of the input field at active.
