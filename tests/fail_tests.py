acd23f54e28c67b51fd993c51a197fdd0c4e78af
ea.proshin <ea.proshin@tensor.ru>
2018-08-08 10:34:26 +0300

    fail_test.py edit

--- tests/fail_tests.py
+++ tests/fail_tests.py
@@ -1,11 +1,17 @@
 import sqlite3
-result = ''
-conn = sqlite3.connect('result.db')
-c = conn.cursor()
-output = c.execute('''SELECT name FROM sqlite_master WHERE type="table" AND name="FailTest" ''')
-if output.fetchone():
-    tests = c.execute("SELECT * FROM FailTest")
-    if tests.fetchone():
-        result = 'true'
-conn.close()
-print(result)
+
+
+def main():
+    result = ''
+    conn = sqlite3.connect('result.db')
+    c = conn.cursor()
+    output = c.execute('''SELECT name FROM sqlite_master WHERE type="table" AND name="FailTest" ''')
+    if output.fetchone():
+        tests = c.execute("SELECT * FROM FailTest")
+        if tests.fetchone():
+            result = 'true'
+    conn.close()
+    return result
+
+if __name__ == '__main__':
+    main()
