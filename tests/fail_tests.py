785971964bae9184f4d3b3c00a2c5ea30b7d2e6b
ea.proshin <ea.proshin@tensor.ru>
2018-08-08 08:36:11 +0300

    add fail_test.py

---
+++ tests/fail_tests.py
@@ -0,0 +1,7 @@
+import sqlite3
+conn = sqlite3.connect('result.db')
+c = conn.cursor()
+output = c.execute('''SELECT name FROM sqlite_master WHERE type="table" AND name="FailTest" ''')
+result = None if not output.fetchone() else c.execute("SELECT * FROM FailTest").fetchone()
+conn.close()
+print(result)
