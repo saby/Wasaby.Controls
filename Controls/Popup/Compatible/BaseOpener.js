84e071c713790e8a5d20acde51d80f3f00b602f9
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-08-09 18:44:45 +0300

    https://online.sbis.ru/opendoc.html?guid=0f03e2e0-8eef-4ee8-b80e-caae61790911

--- Controls/Popup/Compatible/BaseOpener.js
+++ Controls/Popup/Compatible/BaseOpener.js
@@ -144,28 +144,12 @@ function(cMerge,
 
          cfg.closeByExternalClick = cfg.hasOwnProperty('autoHide') ? cfg.autoHide : true;
 
-         var revertPosition = {
-            top: 'bottom',
-            bottom: 'top',
-            left: 'right',
-            right: 'left',
-            middle: 'center',
-            center: 'center'
-         };
-
-         if (cfg.hasOwnProperty('verticalAlign')) {
-            //Если object - значит api popupMixin'a, которое совпадает с новым api => ничего не меняем
-            if (typeof cfg.verticalAlign !== 'object') {
-               cfg.verticalAlign = {side: revertPosition[cfg.verticalAlign]};
-            }
-         }
-
          if (cfg._type === 'dialog' && !cfg.hasOwnProperty('modal')) {
             cfg.isModal = true;
          }
 
          if (cfg.hasOwnProperty('side')) {
-            cfg.horizontalAlign = { side: revertPosition[cfg.side] };
+            cfg.horizontalAlign = { side: cfg.side };
          }
 
          if (cfg.horizontalAlign) {
@@ -191,9 +175,18 @@ function(cMerge,
          if (!cfg.hasOwnProperty('corner') || typeof cfg.corner !== 'object') {
             cfg.corner = {};
             if (cfg.direction !== 'right' && cfg.direction !== 'left') {
-               cfg.direction = 'right';
+               cfg.direction = 'left';
             }
-            cfg.corner.horizontal = revertPosition[cfg.direction];
+            cfg.corner.horizontal = cfg.direction;
+         }
+
+         if (cfg.hasOwnProperty('verticalAlign')) {
+            cfg.corner = cfg.corner || {};
+            //Если object - значит api popupMixin'a, которое совпадает с новым api => ничего не меняем
+            if (typeof cfg.verticalAlign !== 'object') {
+               cfg.corner.vertical = cfg.verticalAlign;
+               delete cfg.verticalAlign;
+            }
          }
 
          if (cfg.hasOwnProperty('modal')) {
