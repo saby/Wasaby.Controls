ffa35caa2dff39b90af8c5e7b80cdff7fdd98fa6
nv.belotelov <nv.belotelov@tensor.ru>
2018-08-09 11:16:11 +0300

    https://online.sbis.ru/opendoc.html?guid=2b177d55-6abb-46f1-8189-0684ea0c8509

---
+++ Controls-demo/BatchUpdater/resources/c1.js
@@ -0,0 +1,24 @@
+define('Controls-demo/BatchUpdater/resources/c1',
+   [
+      'Core/Control',
+      'Core/Deferred',
+      'tmpl!Controls-demo/BatchUpdater/resources/template'
+   ],
+   function(Base, Deferred, template) {
+      'use strict';
+
+      //Component for <head> html-node
+
+      var Page = Base.extend({
+         _template: template,
+         _afterMount: function(options, context, receivedState) {
+            var def = new Deferred();
+            this._notify('requestBatchUpdate', [def, this._forceUpdate], { bubbling: true, shouldUpdate: false });
+            setTimeout(function() {
+               def.callback();
+            }, 20);
+         }
+      });
+      return Page;
+   }
+);
