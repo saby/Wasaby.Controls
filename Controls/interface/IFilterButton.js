026c2293528b91bcc839200b49a15039b817ec66
ee.zolotova <ee.zolotova@tensor.ru>
2018-08-09 13:17:38 +0300

    https://online.sbis.ru/opendoc.html?guid=9f15a8de-44a7-4d16-8049-ac764be0e225

--- Controls/interface/IFilterButton.js
+++ Controls/interface/IFilterButton.js
@@ -6,14 +6,20 @@ define('Controls/interface/IFilterButton', [
     * @interface Controls/interface/IFilterButton
     * @public
     */
-   
+
+   /**
+    * @typedef {Boolean} Visibility
+    * @variant true The filter editor is located in the "Selected"
+    * @variant false The filter editor is located in the "Possible to select"
+    */
+
    /**
     * @typedef {Object} FilterPanelItems
     * @property {String} id Name of filter field
     * @property {*} value Current filter field value
     * @property {*} resetValue Value for reset
     * @property {String} textValue Text value of filter field.  Used to display a textual representation of the filter
-    * @property {Boolean} visibilityValue Determines whether the filter editor is visible
+    * @property {Visibility} visibility Defines in which block the filter editor is located. For filter editors that are never displayed in the "You can still select" section, you do not need to specify.
     */
    
    /**
