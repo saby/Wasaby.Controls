b8256a81ff30ec288278b5f8f4d394764e38f7bf
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-08-21 13:26:48 +0300

    https://online.sbis.ru/opendoc.html?guid=0e9960fc-82dd-4cdc-93b0-120483ca1645

--- Controls/Popup/Opener/BaseOpener.js
+++ Controls/Popup/Opener/BaseOpener.js
@@ -84,6 +84,7 @@ define('Controls/Popup/Opener/BaseOpener',
                   self._isExecuting = false;
                   if (Base.isNewEnvironment()) {
                      self._popupIds.push(result);
+
                      //Зовем перерисовку, чтобы создался emitter на скролл после открытия окна
                      self._forceUpdate();
                   } else {
