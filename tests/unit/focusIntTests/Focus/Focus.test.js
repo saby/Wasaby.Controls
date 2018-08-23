8d7d383ee75be8c990c3dddca7a1f6bdb0411809
kraynovdo <do.kraynov@tensor.ru>
2018-08-23 08:32:06 +0300

    Revert "Доработал события скролла, дописал тесты"
    This reverts commit 0073c98d1ecda82ae5ab0d353d915743797db736.
    
    # Conflicts:
    #	Controls/Container/Scroll/Watcher.js

--- tests/unit/focusIntTests/Focus/Focus.test.js
+++ tests/unit/focusIntTests/Focus/Focus.test.js
@@ -1,64 +1,64 @@
-// /**
-//  * Created by dv.zuev on 18.05.2017.
-//  */
-// define([
-//
-//    'Core/constants',
-//    'Lib/Control/CompoundControl/CompoundControl',
-//    'WSTest/Focus/TestFocusHelpers',
-//    'css!WSTest/Focus/FocusTests'
-// ], function (cConstants,
-//              focusTestControl,
-//              fHelpers,
-//              iHelpers) {
-//    'use strict';
-//
-//    var testNum = 1;
-//
-//    var skipTests = [24, 29];//Пропустить тест
-//    var skipComponent = [13, 14, 15, 16, 17, 18, 19, 20, 26, 30, 32, 33]; //Для тестов, которые сами создают контролы,
-//    // и для асинхронных тестов.
-//    // Аргументом в функцию проверки передается done
-//
-//    describe('Focus-tests', function () {
-//       var testControl;
-//       beforeEach(function () {
-//          if (typeof $ === 'undefined') {//Проверка того, что тесты выполняются в браузере
-//             this.skip();
-//          }
-//          if (~skipTests.indexOf(testNum)) {
-//             testNum++;
-//             this.skip();
-//          }
-//          else {
-//             testNum++;
-//             $('#mocha').append('<div id="component"></div>');//Для добавления верстки на страницу
-//          }
-//       });
-//       for (var i = 1; i < 34; i++) {
-//          (function (i) {
-//             it('Case' + (i), function (done) {
-//                require(['tmpl!WSTest/Focus/Case' + i, 'WSTest/Focus/Scenario/' + i], function (caseTmpl, func) {
-//                   if (!~skipComponent.indexOf(i)) {
-//                      var comp = focusTestControl.extend({
-//                         _dotTplFn: caseTmpl
-//                      });
-//
-//                      testControl = new comp({
-//                         element: 'component',
-//                         name: 'Case' + i + 'Control'
-//                      });
-//                      func(testControl); //Запускаем функцию проверки
-//                      done();
-//                   } else {
-//                      func(done);
-//                   }
-//                });
-//             });
-//          })(i);
-//       }
-//       afterEach(function () {
-//          testControl && testControl.destroy();
-//       });
-//    });
-// });
+/**
+ * Created by dv.zuev on 18.05.2017.
+ */
+define([
+   
+   'Core/constants',
+   'Lib/Control/CompoundControl/CompoundControl',
+   'WSTest/Focus/TestFocusHelpers',
+   'css!WSTest/Focus/FocusTests'
+], function (cConstants,
+             focusTestControl,
+             fHelpers,
+             iHelpers) {
+   'use strict';
+   
+   var testNum = 1;
+   
+   var skipTests = [24, 29];//Пропустить тест
+   var skipComponent = [13, 14, 15, 16, 17, 18, 19, 20, 26, 30, 32, 33]; //Для тестов, которые сами создают контролы,
+   // и для асинхронных тестов.
+   // Аргументом в функцию проверки передается done
+   
+   describe('Focus-tests', function () {
+      var testControl;
+      beforeEach(function () {
+         if (typeof $ === 'undefined') {//Проверка того, что тесты выполняются в браузере
+            this.skip();
+         }
+         if (~skipTests.indexOf(testNum)) {
+            testNum++;
+            this.skip();
+         }
+         else {
+            testNum++;
+            $('#mocha').append('<div id="component"></div>');//Для добавления верстки на страницу
+         }
+      });
+      for (var i = 1; i < 34; i++) {
+         (function (i) {
+            it('Case' + (i), function (done) {
+               require(['tmpl!WSTest/Focus/Case' + i, 'WSTest/Focus/Scenario/' + i], function (caseTmpl, func) {
+                  if (!~skipComponent.indexOf(i)) {
+                     var comp = focusTestControl.extend({
+                        _dotTplFn: caseTmpl
+                     });
+                     
+                     testControl = new comp({
+                        element: 'component',
+                        name: 'Case' + i + 'Control'
+                     });
+                     func(testControl); //Запускаем функцию проверки
+                     done();
+                  } else {
+                     func(done);
+                  }
+               });
+            });
+         })(i);
+      }
+      afterEach(function () {
+         testControl && testControl.destroy();
+      });
+   });
+});
--- tests/unit/tests/CreateVdom.test.js
+++ tests/unit/tests/CreateVdom.test.js
@@ -1,46 +1,46 @@
-// /**
-//  * Created by dv.zuev on 22.06.2017.
-//  */
-// /**
-//  * Created by dv.zuev on 11.06.2017.
-//  */
-// define([
-//    'WSTest/VdomCompoundVdom/VdomCompoundVdom'
-// ], function (VdomCompoundVdom) {
-//    'use strict';
-//
-//    describe('CreateVdomTest', function () {
-//       var testControl;
-//       beforeEach(function () {
-//          if (typeof $ === 'undefined') {//Проверка того, что тесты выполняются в браузере
-//             this.skip();
-//          }
-//          else {
-//             $(document.body).append('<div id="component"></div>');//Для добавления верстки на страницу
-//          }
-//       });
-//       describe('vdomToCompound', function () {
-//          it('vdomToCompound', function (done) {
-//             var vdomControl = new VdomCompoundVdom({
-//                element: $('#component'),
-//                name: 'upper vdom'
-//             });
-//             setTimeout(function () {
-//                vdomControl.text = 'changed text';
-//                vdomControl._forceUpdate();
-//                setTimeout(function() {
-//                   vdomControl.text = 'double changed text';
-//                   vdomControl._forceUpdate();
-//                   setTimeout(function() {
-//                      assert.isTrue(vdomControl._children['compound']._options.compoundText === 'double changed text');
-//                      vdomControl.destroy();
-//                      done();
-//                   }, 50)
-//                }, 50)
-//             }, 50);
-//          });
-//       });
-//
-//    });
-//
-// });
+/**
+ * Created by dv.zuev on 22.06.2017.
+ */
+/**
+ * Created by dv.zuev on 11.06.2017.
+ */
+define([
+   'WSTest/VdomCompoundVdom/VdomCompoundVdom'
+], function (VdomCompoundVdom) {
+   'use strict';
+
+   describe('CreateVdomTest', function () {
+      var testControl;
+      beforeEach(function () {
+         if (typeof $ === 'undefined') {//Проверка того, что тесты выполняются в браузере
+            this.skip();
+         }
+         else {
+            $(document.body).append('<div id="component"></div>');//Для добавления верстки на страницу
+         }
+      });
+      describe('vdomToCompound', function () {
+         it('vdomToCompound', function (done) {
+            var vdomControl = new VdomCompoundVdom({
+               element: $('#component'),
+               name: 'upper vdom'
+            });
+            setTimeout(function () {
+               vdomControl.text = 'changed text';
+               vdomControl._forceUpdate();
+               setTimeout(function() {
+                  vdomControl.text = 'double changed text';
+                  vdomControl._forceUpdate();
+                  setTimeout(function() {
+                     assert.isTrue(vdomControl._children['compound']._options.compoundText === 'double changed text');
+                     vdomControl.destroy();
+                     done();
+                  }, 50)
+               }, 50)
+            }, 50);
+         });
+      });
+
+   });
+
+});
