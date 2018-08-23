7f4e57a8b7645a5be5c4a5a5b681b92f810b01c5
ia.kapustin <ia.kapustin@tensor.ru>
2018-08-23 12:20:39 +0300

    (1175809415) Реализованы демки для класса "Controls/Container/Scroll" https://online.sbis.ru/opendoc.html?guid=a6a2c5b9-f166-4989-863a-1e03604ae857

--- Controls-demo/Container/Scroll.js
+++ Controls-demo/Container/Scroll.js
@@ -3,21 +3,27 @@ define('Controls-demo/Container/Scroll',
       'Core/Control',
       'WS.Data/Source/Memory',
       'Controls/Container/Scroll/Context',
-      'tmpl!Controls-demo/Container/Scroll'
+      'tmpl!Controls-demo/Container/Scroll',
+      'css!Controls-demo/Container/Scroll'
    ],
    function(Control, MemorySource, ScrollData, template) {
+      var scrollStyleSource = new MemorySource({
+         idProperty: 'title',
+         data: [{
+            title: 'normal'
+         }, {
+            title: 'inverted'
+         }]
+      });
+
       return Control.extend({
          _template: template,
-
-         _pagingVisible: true,
-
+         _pagingVisible: false,
          _scrollbarVisible: true,
-
          _shadowVisible: true,
-
-         _numberOfRecords: 100,
-
-         _innerScroll: false,
+         _numberOfRecords: 50,
+         _selectedStyle: 'normal',
+         _scrollStyleSource: scrollStyleSource,
 
          _getChildContext: function() {
             return {
