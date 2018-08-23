f3ccefa767c93c248870c5ebff6777ba8989ff1a
aa.ibragimov <aa.ibragimov@tensor.ru>
2018-08-23 14:54:42 +0300

    Тест ServiceUpdateNotifierVDOM https://online.sbis.ru/opendoc.html?guid=f309c49f-dac0-49e7-b224-69e3712331e1

--- Controls-demo/ServiceUpdateNotifierTest/ServiceUpdateNotifierTest.js
+++ Controls-demo/ServiceUpdateNotifierTest/ServiceUpdateNotifierTest.js
@@ -5,14 +5,13 @@ define('Controls-demo/ServiceUpdateNotifierTest/ServiceUpdateNotifierTest', [
       'Core/EventBus'
    ],
    function (Control, template, SEBus, EB) {
-      window.product = 'inside';
 
       var module = Control.extend({
          _template: template,
          checkVersion: function () {
             SEBus.serverChannel('versionmanager.updatestaticcomplete').notify('onMessage', {
-               distr: 'inside',
-               product: 'inside',
+               distr: 'specifications', //specifications, inside, cloud-ctrl
+               product: 'inside', // cloud-ctrl, inside
                get: function (name) {
                   return (name === 'Distribution') ? this.distr : this.product;
                }
