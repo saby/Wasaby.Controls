d4f3efc9cf7c2086100de806b2a5787760265a7a
ps.kolesova <ps.kolesova@tensor.ru>
2018-08-28 19:36:35 +0300

    https://online.sbis.ru/opendoc.html?guid=89fd5ceb-4410-460d-a54b-043e95a5308b

--- Controls-demo/Input/Text/TextPG.js
+++ Controls-demo/Input/Text/TextPG.js
@@ -15,93 +15,48 @@ define('Controls-demo/Input/Text/TextPG',
 
    function(Control, MemorySource, Chain, cMerge, template, myTmpl, config) {
       'use strict';
-      var sourcePeriod = [
-         { key: 1, title: 'done' },
-         { key: 2, title: 'error' },
-         { key: 3, title: 'primary' },
-         { key: 4, title: 'info' },
-         { key: 5, title: 'attention' }
-      ];
-      var constraintSource = [
-         { id: 1, title: '[0-9]', example: 'You can use only digits' },
-         { id: 2, title: '[a-zA-Z]', example: 'You can use only letters' },
-         { id: 3, title: '[a-z]', example: 'You can use only lowercase letters' },
-         { id: 4, title: '[A-Z]', example: 'You can use only uppercase letters' }
-      ];
-
-      var textOptions = {
-         placeholder: 'Input text',
-         tagStyle: 'primary',
-         constraint: '',
-         trim: false,
-         maxLength: 100,
-         selectOnClick: false,
-         readOnly: false,
-         tooltip: 'myTooltip',
-      }; // опции компонента Text
-
-      var dataObject = {
-         value: {
-            readOnly: true,
-            placeholder: 'Input',
-            tooltip: 'fjfff'
-         },
-         tagStyle: {
-            emptyText: 'none',
-            placeholder: 'select'
-         },
-         constraint: {
-            source: constraintSource,
-            config: {
-               template: 'custom',
-               value: 'title',
-               comment: 'example'
-            }
-         }
-      }; // настройки для редакторов
-
       var TextPG = Control.extend({
          _template: template,
          _metaData: null,
-         _events: '',
-         _textOptions: null,
+         _content: 'Controls/Input/Text',
          _my: myTmpl,
-         someScope: null,
+         _dataObject: null,
+         _textOptions: null,
          _beforeMount: function() {
-            this._textOptions = textOptions;
-            this._metaData = config['Controls/Input/Text'].properties['ws-config'].options;
-            this._metaData = cMerge(this._metaData, dataObject);
-
-            console.log(this._metaData);
-         },
-         _valueChanged: function(event, value) {
-            this._events += 'valueChanged : ' + value + '\n';
-            this._notify('textValueChanged', [this._options.caption + ': ' + value]);
-            this._notify('valueChanged', [value]);
-            this._forceUpdate();
-
-            // console.log(result);
-         },
-         _tagStyleHandler: function() {
-            this._events += 'tagHover\r\n';
-            this._children.infoBox.open({
-               target: this._children.textBox._container,
-               message: 'Hover'
-            });
-
-            // console.log(this._config['Controls/Input/Text'].properties['ws-config'].options);
-         },
-         _itemsChangedHandler: function() {
-            // this._options.items[index].value = value;
-            this._forceUpdate();
-         },
-
-         _tagStyleClickHandler: function() {
-            this._events += 'tagClick\r\n';
-            this._children.infoBox.open({
-               target: this._children.textBox._container,
-               message: 'Click'
-            });
+            this._dataObject = {
+               value: {
+                  readOnly: true
+               },
+               tagStyle: {
+                  emptyText: 'none',
+                  placeholder: 'select'
+               },
+               constraint: {
+                  source: [
+                     { id: 1, title: '[0-9]', example: 'You can use only digits' },
+                     { id: 2, title: '[a-zA-Z]', example: 'You can use only letters' },
+                     { id: 3, title: '[a-z]', example: 'You can use only lowercase letters' },
+                     { id: 4, title: '[A-Z]', example: 'You can use only uppercase letters' }
+                  ],
+                  config: {
+                     template: 'custom',
+                     value: 'title',
+                     comment: 'example'
+                  }
+               }
+            };
+            this._textOptions = {
+               name: 'Switch',
+               placeholder: 'Input text',
+               tagStyle: 'primary',
+               constraint: '',
+               trim: false,
+               maxLength: 100,
+               selectOnClick: true,
+               readOnly: false,
+               tooltip: 'myTooltip',
+            };
+            this._metaData = config[this._content].properties['ws-config'].options;
          }
       });
       return TextPG;
