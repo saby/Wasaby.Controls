033b6b2b2cde07c79ec7c308660bf7a2eb3f2b86
mi.marachev <mi.marachev@tensor.ru>
2018-08-29 10:26:11 +0300

    https://online.sbis.ru/opendoc.html?guid=44d2b6a5-9c33-40a7-bbe4-1803add367e4

---
+++ tests/unit/Popup/Compatible/Layer.test.js
@@ -0,0 +1,33 @@
+define(
+   [
+      'Controls/Popup/Compatible/Layer',
+      'Core/Deferred',
+      'require'
+   ],
+
+   function(Layer, Deferred, require) {
+      'use strict';
+
+      describe('Controls/Popup/Compatible/Layer', function() {
+         it('check deps loaded and deferred', function(done) {
+            var deferred = new Deferred();
+            Layer.load(null, true).addCallback(function() {
+               assert.isTrue(require.defined('Core/Control'));
+               assert.isTrue(require.defined('Lib/Control/Control.compatible'));
+               assert.isTrue(require.defined('Lib/Control/AreaAbstract/AreaAbstract.compatible'));
+               assert.isTrue(require.defined('Lib/Control/BaseCompatible/BaseCompatible'));
+               assert.isTrue(require.defined('Core/vdom/Synchronizer/resources/DirtyCheckingCompatible'));
+               assert.isTrue(require.defined('Lib/StickyHeader/StickyHeaderMediator/StickyHeaderMediator'));
+               assert.isTrue(require.defined('View/Runner/Text/markupGeneratorCompatible'));
+               assert.isTrue(require.defined('Core/nativeExtensions'));
+               done();
+               return deferred;
+            });
+            Layer.load().addCallback(function() {
+               assert.equal(deferred.isReady(), false);
+            });
+
+         });
+      })
+   }
+);
