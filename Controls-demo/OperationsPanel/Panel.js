a12465cd79f21946604a5877037466d2c80094a6
ps.kolesova <ps.kolesova@tensor.ru>
2018-08-30 16:05:04 +0300

    OperationPanel

--- Controls-demo/OperationsPanel/Panel.js
+++ Controls-demo/OperationsPanel/Panel.js
@@ -6,6 +6,79 @@ define('Controls-demo/OperationsPanel/Panel', [
    'css!Controls-demo/OperationsPanel/Panel/Panel'
 ], function(Control, template, RightTemplate, MemorySource) {
    'use strict';
+   var PANEL_ITEMS_FIRST = [{
+      id: 'remove',
+      icon: 'icon-Erase icon-error',
+      '@parent': false,
+      title: 'Удалить',
+      parent: null
+   }, {
+      id: 'save',
+      icon: 'icon-Save',
+      '@parent': true,
+      title: 'Выгрузить',
+      parent: null
+   }, {
+      id: 'savePDF',
+      '@parent': false,
+      title: 'PDF',
+      parent: 'save'
+   }, {
+      id: 'saveExcel',
+      '@parent': false,
+      title: 'Excel',
+      parent: 'save'
+   }, {
+      id: 'move',
+      icon: 'icon-Move',
+      '@parent': false,
+      title: 'Перенести',
+      parent: null
+   }];
+   var PANEL_ITEMS_SECOND = [{
+      id: 'merge',
+      icon: 'icon-Unite',
+      '@parent': false,
+      title: 'Объединить',
+      parent: null
+   }, {
+      id: 'read',
+      icon: 'icon-WorkRead',
+      '@parent': false,
+      title: 'Прочитать',
+      parent: null
+   }, {
+      id: 'show',
+      icon: 'icon-Show',
+      '@parent': false,
+      title: 'Поставить на контроль',
+      parent: null
+   }];
+   var _private = {
+      getPanelSource: function(key) {
+         var data = [];
+         if (key) {
+            data = data.concat(PANEL_ITEMS_FIRST);
+         }
+         if (key === 2) {
+            data = data.concat(PANEL_ITEMS_SECOND);
+         }
+         return new MemorySource({
+            idProperty: 'id',
+            data: data
+         });
+      }
+   };
+   var DEMO_ITEMS = [{
+      id: 0,
+      title: '0 items'
+   }, {
+      id: 1,
+      title: '3 items'
+   }, {
+      id: 2,
+      title: '6 items'
+   }];
    return Control.extend({
       _template: template,
       _expanded: false,
@@ -22,93 +95,20 @@ define('Controls-demo/OperationsPanel/Panel', [
       PANEL_ITEMS_SECOND: null,
       DEMO_ITEMS: null,
       _beforeMount: function() {
-         this._private = {
-            getPanelSource: function(key) {
-               var data = [];
-               if (key) {
-                  data = data.concat(this.PANEL_ITEMS_FIRST);
-               }
-               if (key === 2) {
-                  data = data.concat(this.PANEL_ITEMS_SECOND);
-               }
-               return new MemorySource({
-                  idProperty: 'id',
-                  data: data
-               });
-            }
-         };
-         this.PANEL_ITEMS_FIRST = [{
-            id: 'remove',
-            icon: 'icon-Erase icon-error',
-            '@parent': false,
-            title: 'Удалить',
-            parent: null
-         }, {
-            id: 'save',
-            icon: 'icon-Save',
-            '@parent': true,
-            title: 'Выгрузить',
-            parent: null
-         }, {
-            id: 'savePDF',
-            '@parent': false,
-            title: 'PDF',
-            parent: 'save'
-         }, {
-            id: 'saveExcel',
-            '@parent': false,
-            title: 'Excel',
-            parent: 'save'
-         }, {
-            id: 'move',
-            icon: 'icon-Move',
-            '@parent': false,
-            title: 'Перенести',
-            parent: null
-         }];
-         this.PANEL_ITEMS_SECOND = [{
-            id: 'merge',
-            icon: 'icon-Unite',
-            '@parent': false,
-            title: 'Объединить',
-            parent: null
-         }, {
-            id: 'read',
-            icon: 'icon-WorkRead',
-            '@parent': false,
-            title: 'Прочитать',
-            parent: null
-         }, {
-            id: 'show',
-            icon: 'icon-Show',
-            '@parent': false,
-            title: 'Поставить на контроль',
-            parent: null
-         }];
-         this.DEMO_ITEMS = [{
-            id: 0,
-            title: '0 items'
-         }, {
-            id: 1,
-            title: '3 items'
-         }, {
-            id: 2,
-            title: '6 items'
-         }];
          this._rightTemplateTpl = RightTemplate;
          this._viewSource = new MemorySource({
             idProperty: 'id',
-            data: this.DEMO_ITEMS
+            data: DEMO_ITEMS
          });
          this._sourceConfig = new MemorySource({
             idProperty: 'id',
-            data: this.DEMO_ITEMS
+            data: DEMO_ITEMS
          });
-         this._source = this._private.getPanelSource(1);
+         this._source = _private.getPanelSource(1);
       },
       sourceChange: function(e, key) {
          this._sourceNumber = key;
-         this._source = this._private.getPanelSource(key);
+         this._source = _private.getPanelSource(key);
       },
       _reset: function() {
          this._eventName = '';
