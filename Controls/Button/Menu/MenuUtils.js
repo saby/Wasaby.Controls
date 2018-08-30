d81fb60a94c15a9c8f2747820b9dcc12b4963df3
ee.zolotova <ee.zolotova@tensor.ru>
2018-08-30 18:10:47 +0300

    https://online.sbis.ru/opendoc.html?guid=70c85fbf-401e-41df-8379-55ace443b28a

--- Controls/Button/Menu/MenuUtils.js
+++ Controls/Button/Menu/MenuUtils.js
@@ -12,7 +12,7 @@ define('Controls/Button/Menu/MenuUtils', [
       // для каждого размера вызывающего элемента создаем класс, который выравнивает popup через margin.
       var offsetClassName = 'controls-MenuButton controls-MenuButton_' + currentButtonClass.type;
 
-      if ((!options.icon || menuStyle === 'titleHead') && currentButtonClass.type !== 'button') {
+      if ((!options.icon || currentButtonClass.type === 'iconButtonBordered') && currentButtonClass.type !== 'button') {
          offsetClassName += ('__' + options.size);
       }
 
