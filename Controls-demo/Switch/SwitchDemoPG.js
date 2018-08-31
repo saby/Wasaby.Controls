bf23c86106090475539ed630134ae216027a2e9d
ps.kolesova <ps.kolesova@tensor.ru>
2018-08-31 16:51:38 +0300

    https://online.sbis.ru/opendoc.html?guid=5cb89294-90e4-46c9-8710-a0d45f6a05e0

---
+++ Controls-demo/Switch/SwitchDemoPG.js
@@ -0,0 +1,43 @@
+define('Controls-demo/Switch/SwitchDemoPG',
+   [
+      'Core/Control',
+      'tmpl!Controls-demo/Input/Text/TextPG',
+      'tmpl!Controls-demo/PropertyGrid/PropertyGridTemplate',
+      'json!Controls-demo/PropertyGrid/pgtext',
+
+      'css!Controls-demo/Filter/Button/PanelVDom',
+      'css!Controls-demo/Input/resources/VdomInputs',
+      'css!Controls-demo/Wrapper/Wrapper'
+   ],
+
+   function(Control, template, myTmpl, config) {
+      'use strict';
+      var TextPG = Control.extend({
+         _template: template,
+         _metaData: null,
+         _content: 'Controls/Toggle/Switch',
+         _my: myTmpl,
+         _dataObject: null,
+         _textOptions: null,
+         _beforeMount: function() {
+            this._dataObject = {
+               value: {
+                  readOnly: true
+               },
+               captionPosition: {
+                  selectedKey: 0
+               }
+            };
+            this._textOptions = {
+               name: 'Switch',
+               readOnly: false,
+               tooltip: 'myTooltip',
+               caption: 'State1',
+               captionPosition: 'left'
+
+            };
+            this._metaData = config[this._content].properties['ws-config'].options;
+         }
+      });
+      return TextPG;
+   });
