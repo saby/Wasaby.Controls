098f84a6ace99025387347e90ef7c85654594ccf
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-09-03 10:01:54 +0300

    https://online.sbis.ru/opendoc.html?guid=6c8cc808-8a6d-4d07-b4b1-b78da42898a4

--- Controls/Decorator/Number.js
+++ Controls/Decorator/Number.js
@@ -30,12 +30,29 @@ define('Controls/Decorator/Number',
        * @cfg {Number} Number of decimal places. Range from 0 to 20.
        */
 
+      /**
+       * @name Controls/Decorator/Number#mode
+       * @cfg {String} Mode of operation with numbers.
+       * @variant round The number is rounded if necessary, and the fractional part is padded with zeros if necessary
+       * so that it has the specified length.
+       * @variant trunc Truncates (cuts off) the digits to the right of dot so that number has the specified length,
+       * no matter whether the argument is a positive or negative number.
+       * @default round
+       */
+
       var _private = {
-         formatNumber: function(number, fractionSize) {
+
+         /**
+          * Casting a number to a format with divide on triads.
+          * @param number {@link number}
+          * @param mode {@link mode}
+          * @param [fractionSize] {@link fractionSize}
+          */
+         formatNumber: function(number, mode, fractionSize) {
             if (typeof fractionSize === 'number') {
-               number = number.toFixed(fractionSize);
+               number = _private[mode](number, fractionSize);
             } else {
-               number = String(number);
+               number = number.toString();
             }
 
             /**
@@ -46,6 +63,34 @@ define('Controls/Decorator/Number',
             number[0] = number[0].replace(/(\d)(?=(\d{3})+$)/g, '$& ');
 
             return number.join('.');
+         },
+
+         /**
+          * {@link mode} round
+          * @param number {@link number}
+          * @param fractionSize {@link fractionSize}
+          * @returns {String}
+          */
+         round: function(number, fractionSize) {
+            return number.toFixed(fractionSize);
+         },
+
+         /**
+          * {@link mode} trunc
+          * @param number {@link number}
+          * @param fractionSize {@link fractionSize}
+          * @returns {String}
+          */
+         trunc: function(number, fractionSize) {
+            if (fractionSize) {
+               var regExp = new RegExp('-?\\d+.?\\d{0,' + fractionSize + '}');
+
+               number = String.prototype.match.call(number, regExp)[0];
+            } else {
+               number = Math.trunc(number).toString();
+            }
+
+            return number;
          }
       };
 
@@ -55,20 +100,34 @@ define('Controls/Decorator/Number',
          _formattedNumber: null,
 
          _beforeMount: function(options) {
-            this._formattedNumber = _private.formatNumber(options.number, options.fractionSize);
+            this._formattedNumber = _private.formatNumber(options.number, options.mode, options.fractionSize);
          },
 
          _beforeUpdate: function(newOptions) {
-            if (newOptions.number !== this._options.number || newOptions.fractionSize !== this._options.fractionSize) {
-               this._formattedNumber = _private.formatNumber(newOptions.number, newOptions.fractionSize);
+            if (
+               newOptions.number !== this._options.number ||
+               newOptions.fractionSize !== this._options.fractionSize ||
+               newOptions.mode !== this._options.mode
+            ) {
+               this._formattedNumber = _private.formatNumber(newOptions.number, newOptions.mode, newOptions.fractionSize);
             }
          }
       });
 
+      NumberDecorator.getDefaultOptions = function() {
+         return {
+            mode: 'round'
+         };
+      };
+
       NumberDecorator.getOptionTypes = function() {
          return {
             number: descriptor(Number).required(),
-            fractionSize: descriptor(Number)
+            fractionSize: descriptor(Number),
+            mode: descriptor(String).oneOf([
+               'trunc',
+               'round'
+            ])
          };
       };
 
--- tests/unit/Decorator/Number.test.js
+++ tests/unit/Decorator/Number.test.js
@@ -10,48 +10,56 @@ define(
          var result;
 
          it('Zero number', function() {
-            result = Number._private.formatNumber(0);
+            result = Number._private.formatNumber(0, 'round');
             assert.equal(result, '0');
          });
 
          it('No fractionSize', function() {
-            result = Number._private.formatNumber(10);
+            result = Number._private.formatNumber(10, 'round');
             assert.equal(result, '10');
 
-            result = Number._private.formatNumber(10.01);
+            result = Number._private.formatNumber(10.01, 'round');
             assert.equal(result, '10.01');
          });
 
          it('Add fractional path', function() {
-            result = Number._private.formatNumber(10, 2);
+            result = Number._private.formatNumber(10, 'round' , 2);
             assert.equal(result, '10.00');
 
-            result = Number._private.formatNumber(10.0001, 0);
+            result = Number._private.formatNumber(10.0001, 'round' , 0);
             assert.equal(result, '10');
          });
 
          it('Remove fractional path', function() {
-            result = Number._private.formatNumber(10.123, 2);
+            result = Number._private.formatNumber(10.123, 'round' , 2);
             assert.equal(result, '10.12');
          });
 
          it('Divide into triads', function() {
-            result = Number._private.formatNumber(123456.01);
+            result = Number._private.formatNumber(123456.01, 'round');
             assert.equal(result, '123 456.01');
 
-            result = Number._private.formatNumber(123456);
+            result = Number._private.formatNumber(123456, 'round');
             assert.equal(result, '123 456');
 
-            result = Number._private.formatNumber(123456.000001);
+            result = Number._private.formatNumber(123456.000001, 'round');
             assert.equal(result, '123 456.000001');
 
-            result = Number._private.formatNumber(12345);
+            result = Number._private.formatNumber(12345, 'round');
             assert.equal(result, '12 345');
          });
 
          it('Negative number', function() {
-            result = Number._private.formatNumber(-123456.000001);
+            result = Number._private.formatNumber(-123456.000001, 'round');
             assert.equal(result, '-123 456.000001');
+         });
+
+         it('Mode trunc', function() {
+            result = Number._private.formatNumber(1.234567890, 'trunc', 4);
+            assert.equal(result, '1.2345');
+
+            result = Number._private.formatNumber(1.234567890, 'trunc', 0);
+            assert.equal(result, '1');
          });
       });
    }
