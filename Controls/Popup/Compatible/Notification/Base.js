71010dc8cc571377021891cfec7ea33ee2ec2463
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-09-06 18:21:06 +0300

    https://online.sbis.ru/opendoc.html?guid=d1ad9a48-17d2-4555-97fe-a223718bfff7

--- Controls/Popup/Compatible/Notification/Base.js
+++ Controls/Popup/Compatible/Notification/Base.js
@@ -1,53 +1,13 @@
 define('Controls/Popup/Compatible/Notification/Base',
    [
-      'Core/Control',
-      'tmpl!Controls/Popup/Compatible/Notification/Base',
-      'css!Controls/Popup/Templates/Notification/Base'
+      'Controls/Popup/Templates/Notification/Base',
+      'tmpl!Controls/Popup/Compatible/Notification/Base'
    ],
-   function(Control, template) {
+   function(NotificationBase, template) {
 
-      var timeAutoClose = 5000;
-
-      var Notification = Control.extend({
-         _template: template,
-
-         _timerId: null,
-
-         _beforeMount: function(options) {
-            if (options.autoClose) {
-               this._autoClose();
-            }
-         },
-
-         _closeClick: function() {
-            this._notify('close', []);
-         },
-
-         _mouseenterHandler: function() {
-            clearTimeout(this._timerId);
-         },
-
-         _mouseleaveHandler: function() {
-            if (this._options.autoClose) {
-               this._autoClose();
-            }
-         },
-
-         _autoClose: function() {
-            var self = this;
-
-            this._timerId = setTimeout(function() {
-               self._notify('close', []);
-            }, timeAutoClose);
-         }
+      var Notification = NotificationBase.extend({
+         _template: template
       });
-
-      Notification.getDefaultOptions = function() {
-         return {
-            style: 'primary',
-            autoClose: true
-         };
-      };
 
       return Notification;
    }
