b5a96648fcf019f44f04624b735d26a24bafa66d
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-09-11 14:15:12 +0300

    https://online.sbis.ru/opendoc.html?guid=6297fa44-2c3d-4152-b2cb-eab9f1408553

--- components/Action/Compatible/Open.js
+++ components/Action/Compatible/Open.js
@@ -1,7 +1,7 @@
 /**
  * Created by as.krasilnikov on 19.06.2018.
  */
-define('SBIS3.CONTROLS/Action/Compatible/Open', ['Core/Abstract', 'Core/Deferred', 'Controls/Popup/Compatible/Layer'], function(Abstract, Deferred, Layer) {
+define('SBIS3.CONTROLS/Action/Compatible/Open', ['Core/Abstract', 'Core/Deferred'], function(Abstract, Deferred) {
    'use strict';
 
    /**
@@ -10,18 +10,24 @@ define('SBIS3.CONTROLS/Action/Compatible/Open', ['Core/Abstract', 'Core/Deferred
     */
    var CompatibleOpen = Abstract.extend(/** @lends SBIS3.CONTROLS/Action/Compatible/Open.prototype */{
       constructor: function(config) {
-         var self = this;
-         var actionModule = 'SBIS3.CONTROLS/Action/OpenDialog';
-
-         this._layerDeferred =  new Deferred();
-         Layer.load().addCallback(function() {
-            requirejs([actionModule], function(OpenDialog) {
-               self._action = new OpenDialog(config);
-               self._layerDeferred.callback();
-            });
-         });
+         CompatibleOpen.superclass.constructor.apply(this, arguments);
+         this._config = config;
       },
       execute: function(meta) {
+         if (!this._layerDeferred) {
+            var self = this;
+            var actionModule = 'SBIS3.CONTROLS/Action/OpenDialog';
+
+            this._layerDeferred =  new Deferred();
+            requirejs(['Controls/Popup/Compatible/Layer'], function(Layer) {
+               Layer.load().addCallback(function() {
+                  requirejs([actionModule], function(OpenDialog) {
+                     self._action = new OpenDialog(self._config);
+                     self._layerDeferred.callback();
+                  });
+               });
+            });
+         }
          this._layerDeferred.addCallback(this._execute.bind(this, meta));
       },
       _execute: function(meta) {
