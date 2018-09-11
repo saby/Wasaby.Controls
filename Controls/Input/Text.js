2da31725f8c2043d3017ae31bb1c64924975f34b
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-09-11 15:19:24 +0300

    https://online.sbis.ru/opendoc.html?guid=bcf6ebec-62b4-420b-ba25-bf8707f8dc83

--- Controls/Input/Text.js
+++ Controls/Input/Text.js
@@ -127,7 +127,8 @@ define('Controls/Input/Text',
          return {
             trim: types(Boolean),
             selectOnClick: types(Boolean),
-            placeholder: types(String),
+
+            /*placeholder: types(String), вернуть проверку типов, когда будет поддержка проверки на 2 типа https://online.sbis.ru/opendoc.html?guid=00ca0ce3-d18f-4ceb-b98a-20a5dae21421*/
             constraint: types(String),
             value: types(String),
             maxLength: types(Number)
