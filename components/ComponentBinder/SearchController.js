bc7372f6c4d7d31596ac56c1eecabed158a715bf
am.gerasimov <am.gerasimov@tensor.ru>
2018-09-11 09:33:00 +0300

    https://online.sbis.ru/opendoc.html?guid=fc99b214-e6c3-45c4-adef-b89ec0ab6b79

--- components/ComponentBinder/SearchController.js
+++ components/ComponentBinder/SearchController.js
@@ -396,7 +396,8 @@ define('SBIS3.CONTROLS/ComponentBinder/SearchController',
             // переводим фокус на view и устанавливаем активным первый элемент, если поле пустое, либо курсор стоит в конце поля ввода
             if (event.which == constants.key.down && (!this.getText() || this.getText().length === this._inputField[0].selectionStart)) {
                var selectedIndex = null,
-                   itemsProjection = view._getItemsProjection();
+                   itemsProjection = view._getItemsProjection(),
+                  currentSelectedIndex;
                /* При поиске по дереву папки отображаются, как Хлебные крошки
                   поэтому маркер нужно установить на первый элемент проекции, который не является папкой */
                if(self._searchMode && !!itemsProjection.getCount() && itemsProjection.at(0).isNode) {
@@ -408,16 +409,19 @@ define('SBIS3.CONTROLS/ComponentBinder/SearchController',
 
                   view.setSelectedIndex(selectedIndex);
                }else {
-                  selectedIndex = view.getSelectedIndex();
+                  currentSelectedIndex = view.getSelectedIndex();
                   /* При нажатии кнопки вниз в строке поиска ожидается :
                    что появится маркер на view (если его не было) / перейдёт на следующую строку (если был).
                    Поэтому обрабатываем две ситуации, когда маркера нет, то устанавливаем selectedIndex на первый элемент,
-                   если он есть, то просто увеличиваем selectedIndex на единицу, чтобы маркер перескочил на следующий элемент. */
-                  if(selectedIndex === null) {
-                     view.setSelectedIndex(0);
-                  } else {
-                     view.setSelectedIndex(selectedIndex + 1);
-                  }
+                   если он есть, то просто увеличиваем selectedIndex на единицу, чтобы маркер перескочил на следующий элемент.
+                   При этом проверяем, чтобы индекс не установился на группуировку, т.к. на неё маркер не ставится. */
+                  itemsProjection.each(function(item, index) {
+                     if (!selectedIndex && index > currentSelectedIndex && !cInstance.instanceOfModule(item, "WS.Data/Display/GroupItem")) {
+                        selectedIndex = index;
+                     }
+                  });
+   
+                  view.setSelectedIndex(selectedIndex);
                }
 
                view.setActive(true);
