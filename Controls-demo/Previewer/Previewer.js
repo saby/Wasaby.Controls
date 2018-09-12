463d6afb63241fb501bd3ff7fae9f602b27cefcc
mitin <mitin@tensor.ru>
2018-09-12 12:28:03 +0300

    новый тест

--- Controls-demo/Previewer/Previewer.js
+++ Controls-demo/Previewer/Previewer.js
@@ -1,14 +1,36 @@
-define('Controls-demo/Previewer/Previewer',
-   [
-      'Core/Control',
-      'wml!Controls-demo/Previewer/Previewer'
-   ],
-   function(Control, template) {
+define('Controls-demo/Previewer/Previewer', [
+	'Core/Control',
+	'wml!Controls-demo/Previewer/Previewer',
+	'WS.Data/Source/Memory',
+	'css!Controls-demo/Previewer/Previewer',
+],
+function(Control, template, MemorySource) {
+	'use strcit';
 
-      'use strcit';
+	var Previewer = Control.extend({
+		_template: template,
+		_triggerSource: null,
+		_caption1: 'hover',
+		_caption2: 'click',
+		_trigger: 'hover',
+		_value: true,	
+		_selectedTrigger: 'hover',
 
-      var Previewer = Control.extend({
-         _template: template
+		_beforeMount: function() {
+			this._triggerSource = new MemorySource({
+			   idProperty: 'title',
+			   data: [
+				  {title: 'hover'},
+				  {title: 'click'}
+			   ]
+			});
+        },
+		
+		changeTrigger: function(e, key) {
+			this._selectedTrigger = key;
+			this._trigger = key;
+		}
+		
       });
 
       Previewer.getDefaultOptions = function() {
