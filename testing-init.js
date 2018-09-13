9df38e6b4abf26ff672abc8e6232670024cf65f9
aa.malcev2 <aa.malcev2@tensor.ru>
2018-09-13 17:08:02 +0300

    https://online.sbis.ru/opendoc.html?guid=2408aec1-3d58-4a52-8c59-1e4494a27888 Пофиксил ошибки про "global leak detected"

--- testing-init.js
+++ testing-init.js
@@ -1,4 +1,12 @@
 testing.configure = function() {
-   mocha.globals(['doT', 'encodeHTML', 'wrapUndefined', 'encodeEval', 'def']);
+   mocha.globals([
+      'doT',
+      'encodeHTML',
+      'wrapUndefined',
+      'encodeEval',
+      'def',
+      'isPlainObject',//global.isPlainObject in Core/nativeExtensions/Object
+      'isPlainArray'//global.isPlainArray in Core/nativeExtensions/Array
+   ]);
    mocha.checkLeaks();
 };
