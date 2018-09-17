5140249fd51ed9f95f0d0aaa70fd78215e64c3ff
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-09-17 17:42:30 +0300

    https://online.sbis.ru/opendoc.html?guid=d51ce07c-440c-4ece-9ce3-52ffb915b862

--- Controls/StickyHeader/Context.js
+++ Controls/StickyHeader/Context.js
@@ -4,6 +4,21 @@ define('Controls/StickyHeader/Context',
    ],
    function(DataContext) {
       return DataContext.extend({
+
+         /**
+          * The position of the fixed header content relative to the top of the container.
+          * @public
+          * @type {Number}
+          */
+         position: null,
+
+         /**
+          * Determines whether shadow should be shown.
+          * @public
+          * @type {Boolean}
+          */
+         shadowVisible: null,
+
          constructor: function(config) {
             this.shadowVisible = config.shadowVisible;
          }
