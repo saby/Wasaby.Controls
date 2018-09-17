273f28fa649e00015e7d347a1e482ebdb56cc9de
as.avramenko <as.avramenko@tensor.ru>
2018-09-17 15:52:37 +0300

    https://online.sbis.ru/opendoc.html?guid=126a96e0-c89d-4391-9965-651aa062376c

--- Controls/List/BaseControl.js
+++ Controls/List/BaseControl.js
@@ -87,10 +87,12 @@ define('Controls/List/BaseControl', [
 
                if (direction === 'down') {
                   self._listViewModel.appendItems(addedItems);
+
                   // Virtual scroll: https://online.sbis.ru/opendoc.html?guid=cb6361c4-8eda-4894-b484-5c6ebfa6085a
                   // self._virtualScroll.appendItems(addedItems.getCount());
                } else if (direction === 'up') {
                   self._listViewModel.prependItems(addedItems);
+
                   // Virtual scroll: https://online.sbis.ru/opendoc.html?guid=cb6361c4-8eda-4894-b484-5c6ebfa6085a
                   // self._virtualScroll.prependItems(addedItems.getCount());
                }
