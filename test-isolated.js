4fc0e91d79b62185cd12d3ead581a89ec8e23a6b
aa.malcev2 <aa.malcev2@tensor.ru>
2018-09-18 13:06:29 +0300

    https://online.sbis.ru/opendoc.html?guid=080da32e-fee3-497a-959c-d3bc6e94d241 Switch to testing in any folder

--- test-isolated.js
+++ test-isolated.js
@@ -6,13 +6,14 @@
  * node node_modules/ws-unit-testing/scripts/mocha -t 10000 test-isolated
  */
 
-var app = require('ws-unit-testing/isolated'),
-   config = require('./package.json').config;
+let app = require('ws-unit-testing/isolated');
+const config = require('./package.json').config;
+
 require('./test-fix-view.js').fix(config);
+
 app.run({
    moduleType: 'amd',
-   root: './',
+   root: '.',
    ws: config.ws,
-   resources: config.resources,
    tests: config.tests
 });
