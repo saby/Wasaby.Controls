5d8f4a822d912aa33063f9d4aa4b7b7d1f044957
aa.shipin <aa.shipin@tensor.ru>
2018-09-18 17:58:00 +0300

    https://online.sbis.ru/opendoc.html?guid=e0f4d58b-7b42-4a3b-affa-a1f00e037b9d

--- Controls/Popup/Compatible/CompoundAreaForNewTpl/ComponentWrapper.js
+++ Controls/Popup/Compatible/CompoundAreaForNewTpl/ComponentWrapper.js
@@ -4,6 +4,18 @@ define('Controls/Popup/Compatible/CompoundAreaForNewTpl/ComponentWrapper', [
 ], function(Control, template) {
 
    return Control.extend({
-      _template: template
+      _template: template,
+      _fillCallbacks: function(cfg) {
+         this._onCloseHandler = cfg._onCloseHandler;
+         this._onResizeHandler = cfg._onResizeHandler;
+         this._onResultHandler = cfg._onResultHandler;
+         this._onRegisterHandler = cfg._onRegisterHandler;
+      },
+      _beforeMount: function (cfg) {
+         this._fillCallbacks(cfg);
+      },
+      _beforeUpdate: function (cfg) {
+         this._fillCallbacks(cfg);
+      }
    });
 });
