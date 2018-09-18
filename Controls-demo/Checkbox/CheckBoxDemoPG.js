eeaab35676f8fc653141c90b9421e58d6b0743f2
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-09-18 12:46:22 +0300

    https://online.sbis.ru/opendoc.html?guid=d4a326fe-8f38-41b7-aa64-8bb67d0f8f3c

---
+++ Controls-demo/Checkbox/CheckBoxDemoPG.js
@@ -0,0 +1,40 @@
+define('Controls-demo/Checkbox/CheckBoxDemoPG',
+   [
+      'Core/Control',
+      'tmpl!Controls-demo/PropertyGrid/DemoPG',
+      'tmpl!Controls-demo/PropertyGrid/PropertyGridTemplate',
+      'json!Controls-demo/PropertyGrid/pgtext',
+
+      'css!Controls-demo/Filter/Button/PanelVDom',
+      'css!Controls-demo/Input/resources/VdomInputs',
+      'css!Controls-demo/Wrapper/Wrapper'
+   ],
+
+   function(Control, template, myTmpl, config) {
+      'use strict';
+      var CheckBoxDemoPG = Control.extend({
+         _template: template,
+         _metaData: null,
+         _content: 'Controls/Toggle/Checkbox',
+         _my: myTmpl,
+         _dataObject: null,
+         _componentOptions: null,
+         _beforeMount: function() {
+            this._dataObject = {
+               value: {
+                  readOnly: true
+               }
+            };
+            this._componentOptions = {
+               name: 'CheckBox',
+               readOnly: false,
+               tooltip: 'myTooltip',
+               caption: 'State1',
+               triState: false
+
+            };
+            this._metaData = config[this._content].properties['ws-config'].options;
+         }
+      });
+      return CheckBoxDemoPG;
+   });
