c1619cc22649e90aedb99be099ee6de19e0bff11
nv.belotelov <nv.belotelov@tensor.ru>
2018-09-18 17:05:21 +0300

    https://online.sbis.ru/opendoc.html?guid=daf67bb7-1075-427c-babd-7bc4d31e99f9

--- Controls/Container/Data.js
+++ Controls/Container/Data.js
@@ -6,9 +6,8 @@ define('Controls/Container/Data',
       'Controls/Container/Data/getPrefetchSource',
       'Controls/Container/Data/ContextOptions'
    ],
-   
-   function(Control, cInstance, template, getPrefetchSource, ContextOptions) {
 
+   function(Control, cInstance, template, getPrefetchSource, ContextOptions) {
       /**
        * Container component that provides a context field "dataOptions" with necessary data for child containers.
        *
@@ -22,7 +21,7 @@ define('Controls/Container/Data',
        * @public
        * @author A.M. Gerasimov
        */
-      
+
       /**
        * @name Controls/Container/Data#source
        * @cfg Object that implements ISource interface for data access.
@@ -34,9 +33,9 @@ define('Controls/Container/Data',
        */
 
       'use strict';
-      
+
       var CONTEXT_OPTIONS = ['filter', 'navigation', 'keyProperty', 'sorting', 'source', 'prefetchSource', 'items'];
-      
+
       var _private = {
          isEqualItems: function(oldList, newList) {
             return oldList && cInstance.instanceOfModule(oldList, 'WS.Data/Collection/RecordSet') &&
@@ -44,7 +43,7 @@ define('Controls/Container/Data',
                (Object.getPrototypeOf(newList).constructor == Object.getPrototypeOf(newList).constructor) &&
                (Object.getPrototypeOf(newList.getAdapter()).constructor == Object.getPrototypeOf(oldList.getAdapter()).constructor);
          },
-         createOptionsObject: function(self) {
+         updateDataOptions: function(self, dataOptions) {
             function reducer(result, optName) {
                if (optName === 'source' && self._source) {
                   //TODO: При построении на сервере в сериализованом состоянии в компонент приходят prefetchSource и
@@ -61,10 +60,10 @@ define('Controls/Container/Data',
                }
                return result;
             }
-            
-            return CONTEXT_OPTIONS.reduce(reducer, {});
+
+            return CONTEXT_OPTIONS.reduce(reducer, dataOptions);
          },
-   
+
          createPrefetchSource: function(self, data) {
             return getPrefetchSource({
                source: self._source,
@@ -74,7 +73,7 @@ define('Controls/Container/Data',
                keyProperty: self._keyProperty
             }, data);
          },
-         
+
          resolveOptions: function(self, options) {
             self._filter = options.filter;
             self._navigation = options.navigation;
@@ -91,19 +90,21 @@ define('Controls/Container/Data',
             self._prefetchSource = result.source;
          }
       };
-      
+
       var Data =  Control.extend(/** @lends Controls/Container/Data.prototype */{
-         
+
          _template: template,
-         
+
          _beforeMount: function(options, context, receivedState) {
             var self = this;
             _private.resolveOptions(this, options);
             if (receivedState) {
                _private.resolvePrefetchSourceResult(this, receivedState);
+               self._dataOptionsContext = new ContextOptions(_private.updateDataOptions(self, {}));
             } else if (self._source) {
                return _private.createPrefetchSource(this).addCallback(function(result) {
                   _private.resolvePrefetchSourceResult(self, result);
+                  self._dataOptionsContext = new ContextOptions(_private.updateDataOptions(self, {}));
                   return result;
                });
             }
@@ -125,9 +126,11 @@ define('Controls/Container/Data',
 
          _filterChanged: function(event, filter) {
             this._filter = filter;
+            _private.updateDataOptions(this, this._dataOptionsContext);
+            this._dataOptionsContext.updateConsumers();
             this._notify('filterChanged', [filter]);
          },
-         
+
          _itemsChanged: function(event, items) {
             var self = this;
             _private.createPrefetchSource(this, items).addCallback(function(result) {
@@ -136,10 +139,10 @@ define('Controls/Container/Data',
                return result;
             });
          },
-         
+
          _getChildContext: function() {
             return {
-               dataOptions: new ContextOptions(_private.createOptionsObject(this))
+               dataOptions: this._dataOptionsContext
             };
          }
       });
