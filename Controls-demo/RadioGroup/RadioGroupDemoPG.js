098ac1d679ef9ad8a37ea1245f8f3dccb35f67af
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-09-19 23:37:10 +0300

    https://online.sbis.ru/opendoc.html?guid=b36c1ed1-20da-45e6-958a-61399dee1877

---
+++ Controls-demo/RadioGroup/RadioGroupDemoPG.js
@@ -0,0 +1,45 @@
+define('Controls-demo/RadioGroup/RadioGroupDemoPG',
+   [
+      'Core/Control',
+      'tmpl!Controls-demo/PropertyGrid/DemoPG',
+      'tmpl!Controls-demo/PropertyGrid/PropertyGridTemplate',
+      'json!Controls-demo/PropertyGrid/pgtext',
+
+      'css!Controls-demo/Filter/Button/PanelVDom',
+      'css!Controls-demo/Input/resources/VdomInputs',
+      'css!Controls-demo/Wrapper/Wrapper'
+   ],
+
+   function(Control, template, myTmpl, config) {
+      'use strict';
+      var SwitchDemoPG = Control.extend({
+         _template: template,
+         _metaData: null,
+         _content: 'Controls/Toggle/RadioGroup',
+         _my: myTmpl,
+         _dataObject: null,
+         _componentOptions: null,
+         _beforeMount: function() {
+            this._dataObject = {
+               value: {
+                  readOnly: true
+               },
+               captionPosition: {
+                  keyProperty: 'id',
+                  displayProperty: 'title',
+                  selectedKey: 0
+               }
+            };
+            this._componentOptions = {
+               name: 'Switch',
+               readOnly: false,
+               tooltip: 'myTooltip',
+               caption: 'State1',
+               captionPosition: 'left'
+
+            };
+            this._metaData = config[this._content].properties['ws-config'].options;
+         }
+      });
+      return SwitchDemoPG;
+   });
