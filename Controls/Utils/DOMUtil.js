1d98b8e561c59c4a3be89273cb82aeb31c6bc6f1
Смирнов А.А. (таб номер 84580226) <aa.smirnov2@tensor.ru>
2018-09-20 07:47:59 +0300

    Фикс отступов в DOMUtil

--- Controls/Utils/DOMUtil.js
+++ Controls/Utils/DOMUtil.js
@@ -2,25 +2,25 @@
  * Created by ia.kapustin on 19.09.2018.
  */
 define('Controls/Utils/DOMUtil', function() {
-    'use strict';
+   'use strict';
 
-    /**
-     * Возвращяет внутреннюю ширину элемента(т.е за вычетом бордеров, падингов и полосы прокрутки)
-     * @param {window.Node} container
-     * @return {window.Number}
-     **/
-    return {
-        width: function(container) {
-            var
-                computedStyle,
-                containerWidth = container.clientWidth;
+   /**
+    * Возвращяет внутреннюю ширину элемента(т.е за вычетом бордеров, падингов и полосы прокрутки)
+    * @param {window.Node} container
+    * @return {window.Number}
+    **/
+   return {
+      width: function(container) {
+         var
+            computedStyle,
+            containerWidth = container.clientWidth;
 
-            if (window.getComputedStyle) {
-                computedStyle = window.getComputedStyle(container);
-                containerWidth -= parseInt(computedStyle.paddingLeft, 10) + parseInt(computedStyle.paddingRight, 10);
-            }
+         if (window.getComputedStyle) {
+            computedStyle = window.getComputedStyle(container);
+            containerWidth -= parseInt(computedStyle.paddingLeft, 10) + parseInt(computedStyle.paddingRight, 10);
+         }
 
-            return containerWidth;
-        }
-    };
+         return containerWidth;
+      }
+   };
 });
