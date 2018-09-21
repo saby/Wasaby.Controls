7916c945d6031f22855535885ee63ed323470bd5
ps.kolesova <ps.kolesova@tensor.ru>
2018-09-21 17:32:22 +0300

    https://online.sbis.ru/opendoc.html?guid=d4a326fe-8f38-41b7-aa64-8bb67d0f8f3c

--- Controls/Toggle/Checkbox/CheckboxStyles.js
+++ Controls/Toggle/Checkbox/CheckboxStyles.js
@@ -8,28 +8,28 @@ define('Controls/Toggle/Checkbox/CheckboxStyles',
     * @mixin Controls/Toggle/Checkbox/CheckboxStyles
     * @public
     *
-    * @css @font-size__checkBox Checkbox font-size.
+    * @css @font-size_CheckBox Checkbox font-size.
     * @css @height_CheckBox Checkbox height.
     *
     * @css @color_CheckBox__iconBox Enabled icon box color.
-    * @css @color_CheckBox__iconBox_disabled Disabled icon box color.
+    * @css @color_CheckBox__iconBox_disabled Disabled icon box color.lf
     *
     * @css @color_CheckBox__caption_enabled Enabled text color.
     * @css @color_CheckBox__caption_disabled Disabled text color.
     *
     * @css @spacing_CheckBox-between-icon-caption Spacing between icon and caption.
-    * @css @line-height_Checkbox__caption Caption line height.
+    * @css @line-height_CheckBox__caption Caption line height.
     *
     * @css @color_CheckBox__icon_disabled Disabled icon color of selected or tristate checkbox.
     * @css @color_CheckBox__icon_enabled Enabled icon color of selected or tristate checkbox.
     *
-    * @css @border-width_CheckBox__iconBox Border thickness in iconBox.
-    * @css @size-CheckBox__iconBox Size of checkbox iconBox.
+    * @css @width_CheckBox__border Border thickness in iconBox.
+    * @css @size-CheckBox__icon-box Size of checkbox iconBox.
     *
-    * @css @size-CheckBox__iconSuccessful Size of selected checkbox icon.
-    * @css @top-CheckBox__iconSuccessful Top coordinate of selected checkbox icon.
+    * @css @size_CheckBox__icon-successful Size of selected checkbox icon.
+    * @css @size_CheckBox__icon-successful-top Top coordinate of selected checkbox icon.
     *
-    * @css @left-CheckBox__iconSuccessful Left coordinate of selected checkbox icon.
+    * @css @size_CheckBox__icon-successful-left Left coordinate of selected checkbox icon.
     * @css @size-CheckBox__iconTristate Size of tristate checkbox icon.
     */
 );
