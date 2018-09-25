673651faf52e1bd05d6653f9562d4e7e1e702df5
as.zaycev <as.zaycev@tensor.ru>
2018-09-25 11:32:51 +0300

    https://online.sbis.ru/opendoc.html?guid=448c5cf5-b572-41dc-bf16-38cce8e59e8c

--- Controls/Operations/Panel/View.js
+++ Controls/Operations/Panel/View.js
@@ -10,16 +10,11 @@ define('Controls/Operations/Panel/View', [
    'use strict';
 
    var _private = {
-      recalculateToolbarItems: function(self, items) {
-         var
-            toolbarWidth,
-            toolbarBlock = self._children.toolbarBlock;
-         if (items && toolbarBlock) {
-            //In ie clientWidth is 0. But in fact, the element already exists in the DOM and it is visible.
-            toolbarWidth = parseInt(getComputedStyle(toolbarBlock).width, 10);
+      recalculateToolbarItems: function(self, items, toolbarWidth) {
+         if (items) {
             self._toolbarSource = new Memory({
-               idProperty: 'id',
-               data: WidthUtils.fillItemsType(self, items, toolbarWidth).getRawData()
+               idProperty: self._options.keyProperty,
+               data: WidthUtils.fillItemsType(self._options.keyProperty, self._options.parentProperty, items, toolbarWidth).getRawData()
             });
             self._forceUpdate();
          }
@@ -28,7 +23,7 @@ define('Controls/Operations/Panel/View', [
          var newWidth = self._children.toolbarBlock.clientWidth;
          if (self._oldToolbarWidth !== newWidth) {
             self._oldToolbarWidth = newWidth;
-            _private.recalculateToolbarItems(self, self._items);
+            _private.recalculateToolbarItems(self, self._items, newWidth);
          }
       },
       loadData: function(self, source) {
@@ -46,10 +41,6 @@ define('Controls/Operations/Panel/View', [
    return Control.extend({
       _template: template,
       _itemTemplate: ItemTemplate,
-      _toolbarSource: undefined,
-      _loadItemsDeferred: undefined,
-      _lastWidth: undefined,
-      _items: undefined,
       _oldToolbarWidth: 0,
 
       _beforeMount: function(options) {
@@ -57,15 +48,14 @@ define('Controls/Operations/Panel/View', [
       },
 
       _afterMount: function() {
-         this._oldToolbarWidth = this._children.toolbarBlock.clientWidth;
-         _private.recalculateToolbarItems(this, this._items);
+         _private.checkToolbarWidth(this);
       },
 
       _beforeUpdate: function(newOptions) {
          var self = this;
          if (newOptions.source !== this._options.source) {
             _private.loadData(this, newOptions.source).addCallback(function() {
-               _private.recalculateToolbarItems(self, self._items);
+               _private.recalculateToolbarItems(self, self._items, self._children.toolbarBlock.clientWidth);
             });
          }
       },
@@ -75,7 +65,7 @@ define('Controls/Operations/Panel/View', [
       },
 
       _onResize: function() {
-         _private.recalculateToolbarItems(this, this._items);
+         _private.checkToolbarWidth(this);
       },
 
       _toolbarItemClick: function(event, item) {
---
+++ tests/unit/OperationsPanel/View.test.js
@@ -0,0 +1,276 @@
+define([
+   'Controls/Operations/Panel/View',
+   'WS.Data/Source/Memory',
+   'Controls/Operations/Panel/Utils'
+], function(
+   View,
+   Memory,
+   WidthUtils
+) {
+   'use strict';
+
+   function mockFillItemsType(itemsSizes) {
+      return function fillItemsType(keyProperty, parentProperty, items, availableWidth) {
+         var
+            visibleItemsKeys = [],
+            currentWidth = itemsSizes.reduce(function(acc, size) {
+               return acc + size;
+            }, 0);
+
+         items.each(function(item) {
+            if (!item.get(parentProperty)) {
+               visibleItemsKeys.push(item.get(keyProperty));
+            }
+         });
+
+         if (currentWidth > availableWidth) {
+            for (var i = visibleItemsKeys.length - 1; i >= 0; i--) {
+               items.getRecordById(visibleItemsKeys[i]).set('showType', currentWidth > availableWidth ? 0 : 1);
+               currentWidth -= itemsSizes[i];
+            }
+         } else {
+            items.each(function(item) {
+               item.set('showType', 2);
+            });
+         }
+
+         return items;
+      };
+   }
+
+   describe('Controls.Operations.Panel.View', function() {
+      var
+         instance,
+         oldFillItemsType,
+         data = [{
+            id: 0,
+            title: 'print'
+         }, {
+            id: 1,
+            title: 'unload'
+         }],
+         cfg = {
+            source: new Memory({
+               idProperty: 'id',
+               data: data
+            }),
+            keyProperty: 'id'
+         };
+
+      beforeEach(function() {
+         instance = new View();
+         instance.saveOptions(cfg);
+         oldFillItemsType = WidthUtils.fillItemsType;
+      });
+
+      afterEach(function() {
+         WidthUtils.fillItemsType = oldFillItemsType;
+         instance = null;
+      });
+
+
+      it('_beforeMount', function(done) {
+         instance._beforeMount(cfg).addCallback(function(items) {
+            assert.deepEqual(items.getRawData(), data);
+            assert.deepEqual(instance._items.getRawData(), data);
+            done();
+         });
+      });
+
+      describe('_afterMount', function() {
+         it('enough space', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 360
+               }
+            };
+            instance._forceUpdate = function() {
+               forceUpdateCalled = true;
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._afterMount();
+               instance._toolbarSource.query().addCallback(function(result) {
+                  assert.equal(result.getAll().getRecordById(0).get('showType'), 2);
+                  assert.equal(result.getAll().getRecordById(1).get('showType'), 2);
+                  assert.isTrue(forceUpdateCalled);
+                  done();
+               });
+            });
+         });
+
+         it('not enough space', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 100
+               }
+            };
+            instance._forceUpdate = function() {
+               forceUpdateCalled = true;
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._afterMount();
+               instance._toolbarSource.query().addCallback(function(result) {
+                  assert.equal(result.getAll().getRecordById(0).get('showType'), 1);
+                  assert.equal(result.getAll().getRecordById(1).get('showType'), 0);
+                  assert.isTrue(forceUpdateCalled);
+                  done();
+               });
+            });
+         });
+      });
+
+      describe('_beforeUpdate', function() {
+         it('old source', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 100
+               }
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._afterMount();
+               instance._forceUpdate = function() {
+                  forceUpdateCalled = true;
+               };
+               instance._beforeUpdate(cfg);
+               assert.isFalse(forceUpdateCalled);
+               done();
+            });
+         });
+         it('new source', function(done) {
+            var
+               forceUpdateCalled = false,
+               newCfg = {
+                  source: new Memory({
+                     idProperty: 'id',
+                     data: data
+                  }),
+                  keyProperty: 'id'
+               };
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 100
+               }
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._afterMount();
+               instance._forceUpdate = function() {
+                  forceUpdateCalled = true;
+               };
+               instance._beforeUpdate(newCfg);
+               assert.isTrue(forceUpdateCalled);
+               done();
+            });
+         });
+      });
+
+      describe('_afterUpdate', function() {
+         it('enough space', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 360
+               }
+            };
+            instance._forceUpdate = function() {
+               forceUpdateCalled = true;
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._afterUpdate();
+               instance._toolbarSource.query().addCallback(function(result) {
+                  assert.equal(result.getAll().getRecordById(0).get('showType'), 2);
+                  assert.equal(result.getAll().getRecordById(1).get('showType'), 2);
+                  assert.isTrue(forceUpdateCalled);
+                  done();
+               });
+            });
+         });
+
+         it('not enough space', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 100
+               }
+            };
+            instance._forceUpdate = function() {
+               forceUpdateCalled = true;
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._afterUpdate();
+               instance._toolbarSource.query().addCallback(function(result) {
+                  assert.equal(result.getAll().getRecordById(0).get('showType'), 1);
+                  assert.equal(result.getAll().getRecordById(1).get('showType'), 0);
+                  assert.isTrue(forceUpdateCalled);
+                  done();
+               });
+            });
+         });
+      });
+
+      describe('_onResize', function() {
+         it('enough space', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 360
+               }
+            };
+            instance._forceUpdate = function() {
+               forceUpdateCalled = true;
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._onResize();
+               instance._toolbarSource.query().addCallback(function(result) {
+                  assert.equal(result.getAll().getRecordById(0).get('showType'), 2);
+                  assert.equal(result.getAll().getRecordById(1).get('showType'), 2);
+                  assert.isTrue(forceUpdateCalled);
+                  done();
+               });
+            });
+         });
+
+         it('not enough space', function(done) {
+            var forceUpdateCalled = false;
+            instance._children = {
+               toolbarBlock: {
+                  clientWidth: 100
+               }
+            };
+            instance._forceUpdate = function() {
+               forceUpdateCalled = true;
+            };
+            WidthUtils.fillItemsType = mockFillItemsType([80, 90]);
+            instance._beforeMount(cfg).addCallback(function() {
+               instance._onResize();
+               instance._toolbarSource.query().addCallback(function(result) {
+                  assert.equal(result.getAll().getRecordById(0).get('showType'), 1);
+                  assert.equal(result.getAll().getRecordById(1).get('showType'), 0);
+                  assert.isTrue(forceUpdateCalled);
+                  done();
+               });
+            });
+         });
+      });
+
+      it('_toolbarItemClick', function() {
+         var item = {
+            test: 123
+         };
+         instance._notify = function(e, eventArgs) {
+            assert.equal(e, 'itemClick');
+            assert.equal(item, eventArgs[0]);
+         };
+         instance._toolbarItemClick({}, item);
+      });
+   });
+});
