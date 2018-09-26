aab00740f904ea2d302beee8fbf976d95813ebca
va.spirin <va.spirin@tensor.ru>
2018-09-26 18:07:40 +0700

    RichEditor: Устранены утечки памяти
    1175843041 https://online.sbis.ru/opendoc.html?guid=bd45dc47-aa4f-40be-af8c-1c7bd56e511a

--- components/RichEditor/Components/Toolbar.js
+++ components/RichEditor/Components/Toolbar.js
@@ -112,7 +112,7 @@ define('SBIS3.CONTROLS/RichEditor/Components/Toolbar', [
             if (this.getItems().getRecordById('style')){
                this._styleBox = this.getItemInstance('style');
                this._pickerOpenHandler = this._pickerOpenHandler.bind(this);
-               this._styleBox.once('onPickerOpen', this._pickerOpenHandler);
+               this.subscribeOnceTo(this._styleBox, 'onPickerOpen', this._pickerOpenHandler);
             }
          },
 
@@ -402,8 +402,12 @@ define('SBIS3.CONTROLS/RichEditor/Components/Toolbar', [
             this._toggleToolbarButton.unbind('click');
             this._toggleToolbarButton.off('mousedown focus', this._blockFocusEvents);
             this._toggleToolbarButton = null;
+            if (this._styleBox._picker) {
+               this._styleBox._picker._container.off('mousedown focus', this._blockFocusEvents);
+            }
+            this.unsubscribeFrom(this._imagePanel, 'onImageChange', this._onImageChange);
+            this.unsubscribeFrom(this._styleBox, 'onPickerOpen', this._pickerOpenHandler);
             if (this.getItems().getRecordById('style') && this._pickerOpenHandler) {
-               this._styleBox.unsubscribe('onPickerOpen', this._pickerOpenHandler);
                this._pickerOpenHandler = null;
             }
             RichEditorToolbar.superclass.destroy.apply(this, arguments);
