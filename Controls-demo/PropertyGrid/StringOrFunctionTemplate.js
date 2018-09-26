e59650a5c19f718c77ad3471f98b2aada79fc0e0
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-09-26 20:33:20 +0300

    https://online.sbis.ru/opendoc.html?guid=a674dc6f-a84b-45d0-8231-4f5167daa6e5

--- Controls-demo/PropertyGrid/StringOrFunctionTemplate.js
+++ Controls-demo/PropertyGrid/StringOrFunctionTemplate.js
@@ -27,6 +27,9 @@ define('Controls-demo/PropertyGrid/StringOrFunctionTemplate',
                this._notify('valueChanged', [this._value]);
             }
          },
+         _checkBoxValueChanged: function() {
+            this._valueChangedNotify();
+         },
          _choseHandler: function(e, selectedItem) {
             this._notify('valueChanged', [selectedItem.get('template')]);
          }
