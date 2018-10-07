d939cebab208854103cdbda20d2962a6eec6f65a
ps.kolesova <ps.kolesova@tensor.ru>
2018-10-07 17:28:56 +0300

    https://online.sbis.ru/opendoc.html?guid=dfb58d83-97f7-4549-94a5-5cac7cb428f7

--- Controls/Input/interface/IInputBase.js
+++ Controls/Input/interface/IInputBase.js
@@ -54,6 +54,20 @@ define('Controls/Input/interface/IInputBase', [], function() {
     *    <Controls.Input.Text tooltip="Enter your name"/>
     * </pre>
     */
+   /**
+    * @name Controls/Input/interface/IInputBase#selectOnClick
+    * @cfg {Boolean} Determines whether text is selected when input is clicked.
+    * @default false
+    * @remark
+    * This option cab be used if you know that user clicking the field to enter a new value is a more frequent scenario
+    * than user wanting to edit the current value. In that case, they will click on the field, text will get selected, a
+    * nd they will be able to start entering new value immediately.
+    * @example
+    * In this example, when the field is clicked, all text in it will be selected.
+    * <pre>
+    *    <Controls.Input.Text selectOnClick={{true}}/>
+    * </pre>
+    */
 
    /**
     * @name Controls/Input/interface/IInputBase#style
