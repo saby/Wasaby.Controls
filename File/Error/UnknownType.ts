585f965deb32fc458b6a3becdd9fc65abccfbb30
av.zalyaev <av.zalyaev@tensor.ru>
2018-10-10 16:36:07 +0300

    https://online.sbis.ru/opendoc.html?guid=e27f9307-8362-4d9f-a33a-55c5a2d7d5ea

--- File/Error/UnknownType.ts
+++ File/Error/UnknownType.ts
@@ -21,10 +21,6 @@ class UnknownTypeError extends FileError {
             message: MESSAGE,
             fileName: params.fileName
         });
-        /*
-         * https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
-         */
-        Object.setPrototypeOf(this, UnknownTypeError.prototype);
     }
 }
 
