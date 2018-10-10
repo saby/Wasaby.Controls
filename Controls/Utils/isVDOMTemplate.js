36bb64d9fc60e2acc28026a396ef3ac7899402f0
aa.artemeva1 <aa.artemeva1@tensor.ru>
2018-10-10 11:49:03 +0300

    Описание owner + оформление описания класса

--- Controls/Utils/isVDOMTemplate.js
+++ Controls/Utils/isVDOMTemplate.js
@@ -7,16 +7,36 @@ define('Controls/Utils/isVDOMTemplate', [], function() {
 
    /**
     * Модуль возвращает функцию, которая проверяет, совместим ли класс компонента с веб-фреймворком WaSaby.
-    * Аргумент функции - прототип класса компонента.
-    * Критерий проверки: класс наследуется от класса Core/Control.
-    * Возвращает: true - класс совместим с Wasaby (наследуется от Core/Control), иначе - false.
-    * @example
+    *
+    * <h2>Аргумент функции</h2>
+    *
+    * Прототип класса компонента.
+    *
+    * <h2>Критерий проверки</h2>
+    *
+    * Класс наследуется от класса Core/Control.
+    *
+    * <h2>Возвращает</h2>
+    *
+    * <ul>
+    *     <li><b>true</b> - класс совместим с Wasaby (наследуется от Core/Control)</li>
+    *     <li><b>false</b> {Function} -  класс не совместим с Wasaby</li>
+    * </ul>
+    *
+    * <h2>Пример использования</h2>
     * <pre>
-    *    var myClass = require('myClass');
-    *    if (Controls / Utils / isVDOMTemplate(myClass)) {
-    *       console.log('myClass is vdom component')
-    *    }
+    * require(
+    * ['Controls/Button', 'SBIS3.CONTROLS/Button', 'Controls/Utils/isVDOMTemplate'],
+    * function(VDOMButton, WS3Button, isVDOMTemplate) {
+    *
+    *   // true
+    *   isVDOMTemplate(VDOMButton);
+    *
+    *   // false.
+    *   isVDOMTemplate(WS3Button);
+    *  });
     * </pre>
+    *
     * @class Controls/Utils/isVDOMTemplate
     * @public
     * @author Красильников А.С.
