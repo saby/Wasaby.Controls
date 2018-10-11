35d91c625916187787252b646711972b39bd495a
am.gerasimov <am.gerasimov@tensor.ru>
2018-10-11 16:29:41 +0300

    https://online.sbis.ru/opendoc.html?guid=8262a614-60e9-4168-a86b-927e6891c937

--- Controls/Filter/Controller.js
+++ Controls/Filter/Controller.js
@@ -2,7 +2,6 @@ define('Controls/Filter/Controller',
    [
       'Core/Control',
       'wml!Controls/Filter/Controller',
-      'Controls/Container/Filter/FilterContextField',
       'Core/Deferred',
       'WS.Data/Chain',
       'WS.Data/Utils',
@@ -17,7 +16,7 @@ define('Controls/Filter/Controller',
       'Controls/Container/Data/ContextOptions'
    ],
    
-   function(Control, template, FilterContextField, Deferred, Chain, Utils, isEqual, HistorySource, HistoryService, Memory, SourceController, isEmptyObject, merge, clone) {
+   function(Control, template, Deferred, Chain, Utils, isEqual, HistorySource, HistoryService, Memory, SourceController, isEmptyObject, merge, clone) {
       
       'use strict';
       
@@ -259,12 +258,6 @@ define('Controls/Filter/Controller',
             itemsDef.addCallback(function() {
                _private.applyItemsToFilter(self, options.filter, self._filterButtonItems, self._fastFilterItems);
             });
-
-            this._filterContext = new FilterContextField({
-               filterButtonItems: this._filterButtonItems,
-               fastFilterItems: this._fastFilterItems,
-               historyId: this._options.historyId
-            });
             
             return itemsDef;
          },
@@ -292,13 +285,7 @@ define('Controls/Filter/Controller',
                };
                _private.getHistorySource(this).update(isEmptyObject(filter) ? filter : items, meta);
             }
-            if (this._filterButtonItems) {
-               this._filterContext.filterButtonItems = _private.cloneItems(this._filterButtonItems);
-            }
-            if (this._fastFilterItems) {
-               this._filterContext.fastFilterItems = _private.cloneItems(this._fastFilterItems);
-            }
-            this._filterContext.updateConsumers();
+            
             _private.applyItemsToFilter(this, this._options.filter, items);
             _private.notifyFilterChanged(this);
          },
@@ -306,13 +293,8 @@ define('Controls/Filter/Controller',
          _filterChanged: function(event, filter) {
             _private.setFilter(this, filter);
             _private.notifyFilterChanged(this);
-         },
-         
-         _getChildContext: function() {
-            return {
-               filterLayoutField: this._filterContext
-            };
          }
+         
       });
    
       Container._private = _private;
