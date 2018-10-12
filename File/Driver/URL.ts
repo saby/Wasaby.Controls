fc8d71031626acacd9a81850abdd288d7bed4e95
aa.ibragimov <aa.ibragimov@tensor.ru>
2018-10-12 17:08:25 +0300

    File/Downloader возвращает Promise <void | Response | Error>
    https://online.sbis.ru/opendoc.html?guid=67a631f2-43e0-430a-af6a-1cdb7e08adb4

--- File/Driver/URL.ts
+++ File/Driver/URL.ts
@@ -1,7 +1,28 @@
 /// <amd-module name='File/Driver/URL' />
-import DriverInterface = require('File/Driver/Interface');
 import detection = require('Core/detection');
+import FetchAPI = require('Transport/fetch');
+import BlobDriver = require('File/Driver/Blob');
+import { FileDriver, FileParams } from 'File/Driver/Interface';
 
+class URLDriver implements FileDriver /** @lends File/Driver/URL */ {
+
+    /** 
+     * @param {Strign} url URL файла  
+     */
+    constructor(private url: string) { }
+
+    public download(fileParams?: FileParams): Promise<Response | Error> {
+        if (detection.isMobilePlatform) {
+            window.open(this.url, '_self');
+            return;
+        }
+        return FetchAPI({ url: this.url }).then((res: Response) => {
+            new BlobDriver(res.blob()).download(fileParams);
+            return res;
+        });
+    }
+}
+export = URLDriver;
 /**
  * @public
  * @class File/Driver/URL
@@ -11,46 +32,6 @@ import detection = require('Core/detection');
  * require(['File/Driver/URL'], function(URLDriver) {
  *    var url_document = "/file-transfer/file.pdf"
  *    new URLDriver(url_document).download();
- * });
+ * }).catch(console.error);      // обработчик ошибки скачивания
  * </pre>
  */
-class URL implements DriverInterface {
-   
-   /**
-    * @constructor
-    * @param {Strign} url URL файла
-    */
-   constructor(private url: string) { }
-
-   /**
-    * @public
-    * @method
-    * @description Начинает загрузку файла
-    */
-   public download() {
-      if (detection.isMobilePlatform) {
-         window.open(this.url, '_self');
-         return;
-      }
-      if (detection.firefox || detection.safari) {
-         this.downloadByIframe(this.url);
-         return;
-      }
-      this.downloadByA(this.url);
-   }
-
-   private downloadByA(url: string) {
-      var link = document.createElement('a');
-      link.href = url;
-      link.download = url;
-      document.body.appendChild(link).click();
-   }
-
-   private downloadByIframe(url: string) {
-      var iframe = document.createElement('iframe');
-      iframe.src = url;
-      iframe.setAttribute('style', 'width:0px; height:0px; display:none; border:0px;');
-      document.body.appendChild(iframe);
-   }
-}
-export = URL;
