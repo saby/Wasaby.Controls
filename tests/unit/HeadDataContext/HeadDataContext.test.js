b5fd1b3b08c1bcbf2d086d80386198331e8d0a93
nv.belotelov <nv.belotelov@tensor.ru>
2018-10-16 18:17:12 +0300

    https://online.sbis.ru/opendoc.html?guid=6391be62-b989-4439-80eb-3c766782421c

--- tests/unit/HeadDataContext/HeadDataContext.test.js
+++ tests/unit/HeadDataContext/HeadDataContext.test.js
@@ -7,23 +7,28 @@ define(
 
    function(HeadDataContext, Deferred, RecordSet) {
       'use strict';
-      describe('Controls/Popup/Manager/ManagerController', function() {
-            it('ReceivedState dependencies', function(done) {
-               var hdc = new HeadDataContext();
-               var def = new Deferred();
-               hdc.pushWaiterDeferred(def);
-               hdc.addReceivedState('123', new RecordSet({
-                  rawData: []
-               }));
-               var resDef = hdc.waitAppContent();
-               resDef.addCallback(function(res) {
-                  assert.isTrue(!!~res.additionalDeps.indexOf('WS.Data/Collection/RecordSet'));
-                  done();
-               })
-               def.callback();
+      describe('DepsCollector', function() {
+         it('ReceivedState dependencies', function(done) {
+            var hdc = new HeadDataContext();
+            var def = new Deferred();
+            hdc.pushWaiterDeferred(def);
+            hdc.addReceivedState('123', new RecordSet({
+               rawData: []
+            }));
+            var resDef = hdc.waitAppContent();
+            resDef.addCallback(function(res) {
+               assert.isTrue(!!~res.additionalDeps.indexOf('WS.Data/Collection/RecordSet'));
+               if (typeof window !== 'undefined') {
+                  window._$ThemesController = null;
+               } else if (typeof process !== 'undefined') {
+                  if (process && process.domain && process.domain.req) {
+                     process.domain.req._$ThemesController = null;
+                  }
+               }
+               done();
             });
-         }
-      );
-
+            def.callback();
+         });
+      });
    }
-)
+);
