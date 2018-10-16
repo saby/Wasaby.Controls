51f82117b6cad8e78d2eb9f573bdb12555eda1cb
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2018-10-16 11:20:29 +0300

    https://online.sbis.ru/opendoc.html?guid=6520fabe-21bc-4946-94dd-3aa20a0ef13b

--- components/Mixins/CompositeViewMixin.js
+++ components/Mixins/CompositeViewMixin.js
@@ -747,10 +747,11 @@ define('SBIS3.CONTROLS/Mixins/CompositeViewMixin', [
                //TODO когда идет догрузка по скроллу, все перерисовывать слишком дорого - возникли тормоза в контактах, до VDOM вставляем такую проверку
                //1. Если это добавление в конец и на второй странице нет папок
                //2. Если это удаление
-               //3. Если это добавление элементов в плоском списоке
+               //3. Если это добавление элементов в плоском списоке, не в начало, т.к. при добавлении в начало элементы
+               //вставятся перед контейнером для папок.
                // тогда можно отрисовать как обычно
                // в остальных случаях полная перерисовка
-               if (action === 'a' && !cInstance.instanceOfMixin(this, 'SBIS3.CONTROLS/Mixins/TreeMixin') || ((lastItemsIndex == newItemsIndex) && !(cInstance.instanceOfModule(newItems[0], 'WS.Data/Display/TreeItem') && newItems[0].isNode()) && !this._redrawOnCollectionChange) || action === 'rm') {
+               if (action === 'a' && !cInstance.instanceOfMixin(this, 'SBIS3.CONTROLS/Mixins/TreeMixin') && newItemsIndex !== 0 || ((lastItemsIndex == newItemsIndex) && !(cInstance.instanceOfModule(newItems[0], 'WS.Data/Display/TreeItem') && newItems[0].isNode()) && !this._redrawOnCollectionChange) || action === 'rm') {
                   parentFnc.apply(this, args);
                }
                else {
