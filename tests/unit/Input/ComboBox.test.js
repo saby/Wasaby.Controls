ddd0b7c2b9b055349aa0a875a3c52d7e812cbe18
ee.zolotova <ee.zolotova@tensor.ru>
2018-10-17 18:55:08 +0300

    https://online.sbis.ru/opendoc.html?guid=a22b1ee2-da7a-4989-9e90-83ef4fbef4f8

--- tests/unit/Input/ComboBox.test.js
+++ tests/unit/Input/ComboBox.test.js
@@ -6,7 +6,7 @@ define(
       'WS.Data/Collection/RecordSet'
    ],
    (Combobox, Clone, Memory, RecordSet) => {
-      describe('Combobox', () => {
+      describe('Input.Combobox', () => {
          let items = [
             {
                id: '1',
@@ -19,27 +19,6 @@ define(
             {
                id: '3',
                title: 'Запись 3'
-            },
-            {
-               id: '4',
-               title: 'Запись 4'
-            },
-            {
-               id: '5',
-               title: 'Запись 5'
-            },
-            {
-               id: '6',
-               title: 'Запись 6',
-               icon: 'icon-16 icon-Admin icon-primary'
-            },
-            {
-               id: '7',
-               title: 'Запись 7'
-            },
-            {
-               id: '8',
-               title: 'Запись 8'
             }
          ];
 
@@ -49,9 +28,11 @@ define(
          });
 
          let config = {
-            selectedKeys: ['2'],
+            selectedKey: '2',
             displayProperty: 'title',
             keyProperty: 'id',
+            value: 'New text',
+            placeholder: 'This is placeholder',
             source: new Memory({
                idProperty: 'id',
                data: items
@@ -60,37 +41,79 @@ define(
 
 
          let getCombobox = function(config) {
-            let Combobox = new Combobox(config);
-            Combobox.saveOptions(config);
-            return Combobox;
+            let combobox = new Combobox(config);
+            combobox.saveOptions(config);
+            combobox._simpleViewModel = { updateOptions: function() {} };
+            return combobox;
          };
-         let Combobox = new Combobox(config);
-
-         it('_afterUpdate', () => {
-            let ddl = getCombobox(config);
-            ddl._notify = (e, data) => {
-               assert.deepEqual(data[0], 'Запись 8');
-            };
-            let newConfig = Clone(config);
-            newConfig.selectedKeys = ['8'];
-            ddl._beforeUpdate(newConfig);
+         it('_setText', function() {
+            let combobox = getCombobox(config);
+            combobox._setText([itemsRecords.at(1)]);
+            assert.equal('Запись 2', combobox._value);
          });
 
-         it('data load callback', () => {
-            let ddl = getCombobox(config);
-            ddl._setText([itemsRecords.at(5)]);
-            assert.equal(ddl._text, 'Запись 6');
-            assert.equal(ddl._icon, 'icon-16 icon-Admin icon-primary');
+         it('_setText empty item', function() {
+            let emptyConfig = Clone(config),
+               emptyItems = new RecordSet({
+                  idProperty: 'id',
+                  rawData: [{ id: null, title: 'Не выбрано' }]
+               });
+            emptyConfig.emptyText = 'Не выбрано';
+            let combobox = getCombobox(emptyConfig);
+            combobox._setText([emptyItems.at(0)]);
+            assert.equal('', combobox._value);
+            assert.equal('Не выбрано', combobox._placeholder);
          });
 
-         it('check selectedItemsChanged event', () => {
-            let ddl = getCombobox(config);
-            ddl._notify = (e, data) => {
-               if (e === 'selectedKeysChanged') {
-                  assert.deepEqual(data[0], ['6']);
+         it('_selectedItemsChangedHandler', function() {
+            let combobox = getCombobox(config);
+            combobox._notify = function(event, data) {
+               if (event === 'valueChanged') {
+                  assert.equal(data[0], 'Запись 2');
+               } else if (event === 'selectedKeyChanged') {
+                  assert.equal(data[0], '2');
                }
             };
-            ddl._selectedItemsChangedHandler('itemClick', [itemsRecords.at(5)]);
+            combobox._selectedItemsChangedHandler('itemClick', [itemsRecords.at(1)]);
+            assert.isFalse(combobox._isOpen);
+         });
+
+         it('_mouseDownHandler', function() {
+            let combobox = getCombobox(config);
+            assert.isFalse(combobox._isOpen);
+            combobox._mouseDownHandler();
+            assert.isTrue(combobox._isOpen);
+         });
+
+         it('close', function() {
+            let combobox = getCombobox(config);
+            let close = Combobox._private.close.bind(combobox);
+            close();
+            assert.isFalse(combobox._isOpen);
+         });
+
+         it('_beforeMount', function() {
+            let combobox = getCombobox(config);
+            combobox._beforeMount(config);
+            assert.equal(combobox._value, 'New text');
+            assert.equal(combobox._placeholder, 'This is placeholder');
+         });
+
+         it('_beforeUpdate', function() {
+            let combobox = getCombobox(config);
+            combobox._container = {offsetWidth: 250};
+            assert.equal(combobox._width, undefined);
+            combobox._beforeUpdate();
+            assert.equal(combobox._width, 250);
+         });
+         it('_afterMount', function() {
+            let combobox = getCombobox(config);
+            combobox._container = {offsetWidth: 250};
+            assert.equal(combobox._width, undefined);
+            assert.equal(combobox._corner, undefined);
+            combobox._afterMount();
+            assert.equal(combobox._width, 250);
+            assert.deepEqual(combobox._corner, {vertical: 'bottom'});
          });
       });
    }
