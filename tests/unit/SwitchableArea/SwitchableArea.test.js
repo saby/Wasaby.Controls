b4632443ed3c135124c786d407a28bb9964a169e
as.zaycev <as.zaycev@tensor.ru>
2018-10-24 14:02:45 +0300

    https://online.sbis.ru/opendoc.html?guid=08553238-a7c7-42ad-8162-108f8d55128d

---
+++ tests/unit/SwitchableArea/SwitchableArea.test.js
@@ -0,0 +1,35 @@
+define([
+   'Controls/SwitchableArea'
+], function(
+   SwitchableArea
+) {
+   'use strict';
+   var SwitchArea;
+   var testItems = [
+      {
+         id: 1
+      }, {
+         id: 2
+      }, {
+         id: 3
+      }
+   ];
+   describe('Controls.SwitchableArea', function() {
+      beforeEach(function() {
+         SwitchArea = new SwitchableArea();
+      });
+
+      it('life cycle', function() {
+         var opt = {
+            items: testItems,
+            selectedKey: '1'
+         };
+         SwitchArea.saveOptions(opt);
+         SwitchArea._beforeMount(opt);
+         opt.selectedKey = '2';
+         SwitchArea._beforeUpdate(opt);
+         assert.equal(SwitchArea._viewModel._items[0].loaded, true, '_beforeMount. Item load status is uncorrect');
+         assert.equal(SwitchArea._viewModel._items[1].loaded, true, '_beforeUpdate. Item load status is uncorrect');
+      });
+   });
+});
