b78590ed9e8a307d8156bc78da5c1ba8df6cefb6
ee.zolotova <ee.zolotova@tensor.ru>
2018-10-29 15:18:01 +0300

    https://online.sbis.ru/opendoc.html?guid=2a9affa8-972e-4bd1-b1b8-8fee28c05b5a

--- Controls-demo/Layouts/SearchLayout.js
+++ Controls-demo/Layouts/SearchLayout.js
@@ -8,6 +8,7 @@ define('Controls-demo/Layouts/SearchLayout', [
    'Core/Control',
    'wml!Controls-demo/Layouts/SearchLayout/SearchLayout',
    'WS.Data/Source/Memory',
+   'Controls-demo/Utils/MemorySourceFilter',
    'Controls/List',
    'css!Controls-demo/Layouts/SearchLayout/SearchLayout',
    'Controls/Input/Text',
@@ -21,7 +22,8 @@ define('Controls-demo/Layouts/SearchLayout', [
 
 ], function(BaseControl,
    template,
-   MemorySource) {
+   MemorySource,
+   Filter) {
    'use strict';
    var ModuleClass = BaseControl.extend(
       {
@@ -35,19 +37,19 @@ define('Controls-demo/Layouts/SearchLayout', [
          _beforeMount: function() {
             this.sourceDropdown = new MemorySource({
                data: [
-                  {key: 1, title: 'все страны'},
-                  {key: 2, title: 'Россия'},
-                  {key: 3, title: 'США'},
-                  {key: 4, title: 'Великобритания'}
+                  { key: 1, title: 'все страны' },
+                  { key: 2, title: 'Россия' },
+                  { key: 3, title: 'США' },
+                  { key: 4, title: 'Великобритания' }
                ],
                idProperty: 'key'
             });
             this.sourceId = new MemorySource({
                data: [
-                  {key: 0, title: 'все id'},
-                  {key: 1, title: '1'},
-                  {key: 2, title: '2'},
-                  {key: 3, title: '3'}
+                  { key: 0, title: 'все id' },
+                  { key: 1, title: '1' },
+                  { key: 2, title: '2' },
+                  { key: 3, title: '3' }
                ],
                idProperty: 'key'
             });
@@ -59,38 +61,77 @@ define('Controls-demo/Layouts/SearchLayout', [
                   { id: 3, firstName: 'Andrey', lastName: 'aaaa' },
                   { id: 4, firstName: 'Aleksey', lastName: 'aaaa' },
                   { id: 5, firstName: 'Sasha', lastName: 'aaaa' },
-                  { id: 6, firstName: 'Ivan', lastName: 'Lalala'},
-                  { id: 7, firstName: 'Petr', lastName: 'dfsf'},
-                  { id: 8, firstName: 'Roman', lastName: 'dfsf'},
-                  { id: 9, firstName: 'Maxim', lastName: 'dfsf'},
-                  { id: 10, firstName: 'Andrey', lastName: 'Lalala'},
-                  { id: 12, firstName: 'Sasha', lastName: 'dfsf'},
-                  { id: 13, firstName: 'Sasha', lastName: 'dfsf'},
-                  { id: 14, firstName: 'Sasha', lastName: 'dfsf'},
-                  { id: 15, firstName: 'Sasha', lastName: 'dfsf'},
-                  { id: 16, firstName: 'Sasha', lastName: 'Lalala'},
-                  { id: 17, firstName: 'Sasha', lastName: 'dfsf'},
-                  { id: 18, firstName: 'Dmitry', lastName: 'Lalala'},
-                  { id: 19, firstName: 'Andrey', lastName: 'dfsf'},
-                  { id: 20, firstName: 'Aleksey', lastName: 'dfsf'},
-                  { id: 21, firstName: 'Sasha', lastName: 'dfsf'},
-                  { id: 22, firstName: 'Ivan', lastName: 'dfsf'},
+                  { id: 6, firstName: 'Ivan', lastName: 'Lalala' },
+                  { id: 7, firstName: 'Petr', lastName: 'dfsf' },
+                  { id: 8, firstName: 'Roman', lastName: 'dfsf' },
+                  { id: 9, firstName: 'Maxim', lastName: 'dfsf' },
+                  { id: 10, firstName: 'Andrey', lastName: 'Lalala' },
+                  { id: 12, firstName: 'Sasha', lastName: 'dfsf' },
+                  { id: 13, firstName: 'Sasha', lastName: 'dfsf' },
+                  { id: 14, firstName: 'Sasha', lastName: 'dfsf' },
+                  { id: 15, firstName: 'Sasha', lastName: 'dfsf' },
+                  { id: 16, firstName: 'Sasha', lastName: 'Lalala' },
+                  { id: 17, firstName: 'Sasha', lastName: 'dfsf' },
+                  { id: 18, firstName: 'Dmitry', lastName: 'Lalala' },
+                  { id: 19, firstName: 'Andrey', lastName: 'dfsf' },
+                  { id: 20, firstName: 'Aleksey', lastName: 'dfsf' },
+                  { id: 21, firstName: 'Sasha', lastName: 'dfsf' },
+                  { id: 22, firstName: 'Ivan', lastName: 'dfsf' },
                   { id: 23, firstName: 'Petr', lastName: 'dfgdfg' }
-               ]
+               ],
+               filter: Filter({
+                  firstName: 'По имени',
+                  id: [0],
+                  lastName: '0',
+                  Test2: false,
+                  Test3: false,
+                  Test4: false,
+                  Test5: false,
+                  Test6: false,
+                  Test7: false,
+                  Test8: false,
+                  checked: false,
+                  Test1: [0],
+                  FIO: ''
+               })
             });
             this._filterSource = [
-               {id: 'FIO', value: '', resetValue: '', visibility: false},
-               {id: 'firstName', value: 'По имени', resetValue: 'По имени', visibility: false},
-               {id: 'Test1', value: [0], resetValue: [0], source: this.sourceDropdown, textValue: '', visibility: false},
-               {id: 'checked', value: false, resetValue: false, textValue: 'checked', visibility: false},
-               {id: 'id', value: [0], resetValue: [0], source: this.sourceId, visibility: false},
-               {id: 'Test2', value: false, resetValue: false, textValue: 'Test2', visibility: false},
-               {id: 'Test3', value: false, resetValue: false, textValue: 'Test3', visibility: false},
-               {id: 'Test4', value: false, resetValue: false, textValue: 'Test4', visibility: false},
-               {id: 'Test5', value: false, resetValue: false, textValue: 'FilterText', visibility: false},
-               {id: 'Test6', value: false, resetValue: false, textValue: 'Test6', visibility: false},
-               {id: 'Test7', value: false, resetValue: false, textValue: 'Test7', visibility: false},
-               {id: 'Test8', value: false, resetValue: false, textValue: 'Test8'}
+               {
+                  id: 'FIO', value: '', resetValue: '', visibility: false
+               },
+               {
+                  id: 'firstName', value: 'По имени', resetValue: 'По имени', visibility: false
+               },
+               {
+                  id: 'Test1', value: [0], resetValue: [0], source: this.sourceDropdown, textValue: '', visibility: false
+               },
+               {
+                  id: 'checked', value: false, resetValue: false, textValue: 'checked', visibility: false
+               },
+               {
+                  id: 'id', value: [0], resetValue: [0], source: this.sourceId, visibility: false
+               },
+               {
+                  id: 'Test2', value: false, resetValue: false, textValue: 'Test2', visibility: false
+               },
+               {
+                  id: 'Test3', value: false, resetValue: false, textValue: 'Test3', visibility: false
+               },
+               {
+                  id: 'Test4', value: false, resetValue: false, textValue: 'Test4', visibility: false
+               },
+               {
+                  id: 'Test5', value: false, resetValue: false, textValue: 'FilterText', visibility: false
+               },
+               {
+                  id: 'Test6', value: false, resetValue: false, textValue: 'Test6', visibility: false
+               },
+               {
+                  id: 'Test7', value: false, resetValue: false, textValue: 'Test7', visibility: false
+               },
+               {
+                  id: 'Test8', value: false, resetValue: false, textValue: 'Test8'
+               }
             ];
             this._fastFilterData = [
                {
@@ -102,11 +143,11 @@ define('Controls-demo/Layouts/SearchLayout', [
                      displayProperty: 'title',
                      source: new MemorySource({
                         data: [
-                           {id: 0, title: 'По имени'},
-                           {id: 1, title: 'Sasha'},
-                           {id: 2, title: 'Petr'},
-                           {id: 3, title: 'Ivan'},
-                           {id: 3, title: 'Andrey'}
+                           { id: 0, title: 'По имени' },
+                           { id: 1, title: 'Sasha' },
+                           { id: 2, title: 'Petr' },
+                           { id: 3, title: 'Ivan' },
+                           { id: 3, title: 'Andrey' }
                         ],
                         idProperty: 'id'
                      })
@@ -121,11 +162,11 @@ define('Controls-demo/Layouts/SearchLayout', [
                      displayProperty: 'title',
                      source: new MemorySource({
                         data: [
-                           {id: 0, title: 'По id'},
-                           {id: 1, title: '1'},
-                           {id: 2, title: '2'},
-                           {id: 3, title: '3'},
-                           {id: 4, title: '4'}
+                           { id: 0, title: 'По id' },
+                           { id: 1, title: '1' },
+                           { id: 2, title: '2' },
+                           { id: 3, title: '3' },
+                           { id: 4, title: '4' }
                         ],
                         idProperty: 'id'
                      })
@@ -140,11 +181,11 @@ define('Controls-demo/Layouts/SearchLayout', [
                      displayProperty: 'title',
                      source: new MemorySource({
                         data: [
-                           {id: 1, title: 'aaaa', lastName: 'aaaa'},
-                           {id: 2, title: 'dfsf', lastName: 'dfsf'},
-                           {id: 3, title: 'Такой нет', lastName: 'aaaaa'},
-                           {id: 4, title: 'Lalala', lastName: 'Lalala'},
-                           {id: 0, title: 'По фамилии', lastName: '0'}
+                           { id: 1, title: 'aaaa', lastName: 'aaaa' },
+                           { id: 2, title: 'dfsf', lastName: 'dfsf' },
+                           { id: 3, title: 'Такой нет', lastName: 'aaaaa' },
+                           { id: 4, title: 'Lalala', lastName: 'Lalala' },
+                           { id: 0, title: 'По фамилии', lastName: '0' }
                         ],
                         idProperty: 'id'
                      })
@@ -165,6 +206,7 @@ define('Controls-demo/Layouts/SearchLayout', [
                }
             };
          }
-      });
+      }
+   );
    return ModuleClass;
 });
