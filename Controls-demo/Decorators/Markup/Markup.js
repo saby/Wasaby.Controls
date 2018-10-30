385c43b24b63feba44b582a64d3e673acc84514a
rn.kondakov <rn.kondakov@tensor.ru>
2018-10-30 17:16:02 +0300

    https://online.sbis.ru/opendoc.html?guid=1945b94b-d972-47f7-aa83-051d047932f0

--- Controls-demo/Decorators/Markup/Markup.js
+++ Controls-demo/Decorators/Markup/Markup.js
@@ -27,10 +27,11 @@ define('Controls-demo/Decorators/Markup/Markup', [
             return;
          }
          var testValues = this.testValues,
-            globalStatus = 1;
+            globalStatus = 1,
+            element;
          for (var i = 0; i < testValues.length; ++i) {
-            if (document.getElementById('ControlsDemo-Markup__case' + i)) {
-               var checkHtml = document.getElementById('ControlsDemo-Markup__case' + i).outerHTML.replace(/ class=""/g, '');
+            if (element = document.getElementById('ControlsDemo-Markup__case' + i)) {
+               var checkHtml = element.outerHTML.replace(/( class="")|( id="[^"]*")/g, '');
                globalStatus &= testValues[i].status = checkHtml === testValues[i].goodHtml;
             } else {
                globalStatus = 0;
@@ -44,7 +45,7 @@ define('Controls-demo/Decorators/Markup/Markup', [
             value: [],
             tagResolver: undefined,
             className: '',
-            goodHtml: '<span id="ControlsDemo-Markup__case0" tabindex="0"></span>',
+            goodHtml: '<span tabindex="0"></span>',
             status: -1
          },
          {
@@ -53,35 +54,35 @@ define('Controls-demo/Decorators/Markup/Markup', [
                return ['strong', ['em', '456']];
             },
             className: '',
-            goodHtml: '<strong id="ControlsDemo-Markup__case1" tabindex="0"><em>456</em></strong>',
+            goodHtml: '<strong tabindex="0"><em>456</em></strong>',
             status: -1
          },
          {
             value: [['p', '123']],
             tagResolver: undefined,
             className: '',
-            goodHtml: '<div id="ControlsDemo-Markup__case2" tabindex="0"><p>123</p></div>',
+            goodHtml: '<div tabindex="0"><p>123</p></div>',
             status: -1
          },
          {
             value: [['p']],
             tagResolver: undefined,
             className: 'myClassName',
-            goodHtml: '<div id="ControlsDemo-Markup__case3" tabindex="0" class="myClassName"><p></p></div>',
+            goodHtml: '<div tabindex="0" class="myClassName"><p></p></div>',
             status: -1
          },
          {
             value: [['p', '123'], ['p', '456']],
             tagResolver: undefined,
             className: '',
-            goodHtml: '<div id="ControlsDemo-Markup__case4" tabindex="0"><p>123</p><p>456</p></div>',
+            goodHtml: '<div tabindex="0"><p>123</p><p>456</p></div>',
             status: -1
          },
          {
-            value: [['p', ['a', { rel: 'noreferrer', 'href': 'https://ya.ru', target: '_blank' }, 'https://ya.ru']]],
+            value: [['p', ['a', { 'href': 'https://ya.ru', target: '_blank', rel: 'noreferrer' }, 'https://ya.ru']]],
             tagResolver: undefined,
             className: '',
-            goodHtml: '<div id="ControlsDemo-Markup__case5" tabindex="0"><p><a rel="noreferrer" href="https://ya.ru" target="_blank">https://ya.ru</a></p></div>',
+            goodHtml: '<div tabindex="0"><p><a href="https://ya.ru" target="_blank" rel="noreferrer">https://ya.ru</a></p></div>',
             status: -1
          },
          {
@@ -100,7 +101,7 @@ define('Controls-demo/Decorators/Markup/Markup', [
             ],
             tagResolver: linkDecorateResolver,
             className: '',
-            goodHtml: '<div id="ControlsDemo-Markup__case6" tabindex="0"><p><span class="LinkDecorator__wrap"><a class="myLink LinkDecorator__linkWrap" href="https://ya.ru" target="_blank" rel="noreferrer"><img class="LinkDecorator__image" alt="https://ya.ru" src="TODO"></a></span></p></div>',
+            goodHtml: '<div tabindex="0"><p><span class="LinkDecorator__wrap"><a class="myLink LinkDecorator__linkWrap" href="https://ya.ru" target="_blank" rel="noreferrer"><img class="LinkDecorator__image" alt="https://ya.ru" src="TODO"></a></span></p></div>',
             status: -1
          }
       ],
