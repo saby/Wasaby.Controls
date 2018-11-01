0032c33ab4df9095ac675ecef70e3bb407c7cecf
am.gerasimov <am.gerasimov@tensor.ru>
2018-11-01 10:28:38 +0300

    https://online.sbis.ru/opendoc.html?guid=b16cb16b-182b-4f59-8606-cbcff535e5e2

--- Controls/Filter/Fast.js
+++ Controls/Filter/Fast.js
@@ -171,6 +171,7 @@ define('Controls/Filter/Fast',
                   footerTemplate: getPropValue(item, 'footerTemplate'),
                   selectedKeys: getPropValue(this._items.at(index), 'value')
                },
+               
                //FIXME: this._container - jQuery element in old controls envirmoment https://online.sbis.ru/opendoc.html?guid=d7b89438-00b0-404f-b3d9-cc7e02e61bb3
                target: (this._container[0] || this._container).children[index]
             };
