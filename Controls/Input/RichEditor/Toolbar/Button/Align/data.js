32a8c7dc3b1fed47ea1eed1804920738434b9b13
vd.volockoy <vd.volockoy@tensor.ru>
2018-11-02 13:57:04 +0300

    Добавлены кнопки для тулбара

---
+++ Controls/Input/RichEditor/Toolbar/Button/Align/data.js
@@ -0,0 +1,15 @@
+define('Controls/Input/RichEditor/Toolbar/Button/Align/data', [], function() {
+   return [{
+      id: 'JustifyLeft',
+      icon: 'icon-AlignmentLeft icon-medium',
+   }, {
+      id: 'JustifyRight',
+      icon: 'icon-AlignmentRight icon-medium',
+   }, {
+      id: 'JustifyCenter',
+      icon: 'icon-AlignmentCenter icon-medium',
+   }, {
+      id: 'JustifyFull',
+      icon: 'icon-AlignmentWidth icon-medium',
+   }];
+});
