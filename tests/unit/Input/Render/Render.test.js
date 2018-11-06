b370d3bfca02247115961295fa82d6c79f57c0fa
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-11-06 10:29:08 +0300

    unit-тесты https://online.sbis.ru/opendoc.html?guid=63327eaa-4071-4a21-bef5-0a81991a9881

--- tests/unit/Input/Render/Render.test.js
+++ tests/unit/Input/Render/Render.test.js
@@ -30,7 +30,7 @@ define(
             render._children = {
                input: {
                   querySelector: function() {
-                     return {};
+                     return this;
                   }
                }
             };
@@ -72,6 +72,30 @@ define(
                event.target.selectionEnd = 0;
                render._inputHandler(event);
                assert.equal(result, undefined);
+            });
+         });
+         describe('initSelection', function() {
+            it('test1', function() {
+               viewModel.updateOptions({value: '123'});
+               render._viewModel = viewModel;
+               Render._private.initSelection(render);
+
+               assert.equal(render._children.input.selectionStart, 3);
+               assert.equal(render._children.input.selectionEnd, 3);
+            });
+            it('test2', function() {
+               viewModel.updateOptions({value: '123'});
+               render._viewModel = viewModel;
+               render._selection = {
+                  selectionStart: 0,
+                  selectionEnd: 0
+               };
+               render._children.input.selectionStart = 0;
+               render._children.input.selectionEnd = 0;
+               Render._private.initSelection(render);
+
+               assert.equal(render._children.input.selectionStart, 0);
+               assert.equal(render._children.input.selectionEnd, 0);
             });
          });
       });
