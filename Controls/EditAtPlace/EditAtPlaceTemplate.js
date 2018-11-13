184520363f9bbab1229adaa55605b86e2c4553fe
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-11-13 10:50:51 +0300

    https://online.sbis.ru/opendoc.html?guid=9767d7b7-7a66-416b-8112-e23967a25c22

--- Controls/EditAtPlace/EditAtPlaceTemplate.js
+++ Controls/EditAtPlace/EditAtPlaceTemplate.js
@@ -1,7 +1,15 @@
 define('Controls/EditAtPlace/EditAtPlaceTemplate', [
+   'Core/IoC',
    'Controls/Templates/Editors/Base'
-], function(Control) {
+], function(IoC, Control) {
    'use strict';
 
-   return Control;
+   var EditAtPlaceTemplate = Control.extend({
+      constructor: function() {
+         IoC.resolve('ILogger').warn('Controls/EditAtPlace/EditAtPlaceTemplate', 'Контрол еренесен, используйте Controls/Templates/Editors/Base.');
+         EditAtPlaceTemplate.superclass.constructor.apply(this, arguments);
+      }
+   });
+
+   return EditAtPlaceTemplate;
 });
