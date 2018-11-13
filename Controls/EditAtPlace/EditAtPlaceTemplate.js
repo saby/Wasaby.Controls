ca758de5264d19434b2e7156ac19fdf76ef92d2d
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2018-11-13 18:03:17 +0300

    https://online.sbis.ru/opendoc.html?guid=9767d7b7-7a66-416b-8112-e23967a25c22

--- Controls/EditAtPlace/EditAtPlaceTemplate.js
+++ Controls/EditAtPlace/EditAtPlaceTemplate.js
@@ -1,4 +1,4 @@
-define('Controls/EditAtPlace/EditAtPlaceTemplate', [
+define('Controls/EditableArea/Templates/Editors/Base', [
    'Core/IoC',
    'Controls/EditableArea/Templates/Editors/Base'
 ], function(IoC, Control) {
@@ -6,7 +6,7 @@ define('Controls/EditAtPlace/EditAtPlaceTemplate', [
 
    var EditAtPlaceTemplate = Control.extend({
       constructor: function() {
-         IoC.resolve('ILogger').warn('Controls/EditAtPlace/EditAtPlaceTemplate', 'Контрол перенесен, используйте Controls/EditableArea/Templates/Editors/Base.');
+         IoC.resolve('ILogger').warn('Controls/EditableArea/Templates/Editors/Base', 'Контрол перенесен, используйте Controls/EditableArea/Templates/Editors/Base.');
          EditAtPlaceTemplate.superclass.constructor.apply(this, arguments);
       }
    });
