ddedc746cf92111e7f19cef6223830a01ea7ed93
aa.ibragimov <aa.ibragimov@tensor.ru>
2018-11-16 18:41:07 +0300

    Костыль для IOS12 сохранения base64
    https://online.sbis.ru/opendoc.html?guid=e6d8ebdd-40b4-4c14-9642-1128affe444b

--- File/Driver/Base64.ts
+++ File/Driver/Base64.ts
@@ -2,7 +2,9 @@
 import DriverInterface = require('File/Driver/Interface');
 import base64toblob = require('File/utils/b64toBlob');
 import BlobDriver = require('File/Driver/Blob');
+import detection = require('Core/detection');
 
+const BASE64_PREFIX = (type) => 'data:' + type + ';base64, ';
 /**
  * @public
  * @class File/Driver/Base64
@@ -19,39 +21,47 @@ import BlobDriver = require('File/Driver/Blob');
  * </pre>
  */
 class Base64 implements DriverInterface {
-   private base64Data: string;
-   private contentType: string;
+    private base64Data: string;
+    private contentType: string;
 
-   /**
-    * @constructor
-    * @param {String} data Строка в формате base64
-    */
-   constructor(data: string) {
-      if (data.indexOf('data:') === -1) {
-         this.base64Data = data;
-         return;
-      }
-      this.contentType = data.substring(data.indexOf(':') + 1, data.indexOf(';'));
-      this.base64Data = data.substring(data.indexOf(',') + 1);
-   }
+    /**
+     * @constructor
+     * @param {String} data Строка в формате base64
+     */
+    constructor(data: string) {
+        if (data.indexOf('data:') === -1) {
+            this.base64Data = data;
+            return;
+        }
+        this.contentType = data.substring(data.indexOf(':') + 1, data.indexOf(';'));
+        this.base64Data = data.substring(data.indexOf(',') + 1);
+    }
 
-   /**
-    * @public
-    * @method
-    * @param {DownloadOptions} oprions Параметры загрузки
-    * @description Начинает загрузку файла
-    */
+    /**
+     * @public
+     * @method
+     * @param {DownloadOptions} oprions Параметры загрузки
+     * @description Начинает загрузку файла
+     */
 
-   /** 
-    * @typedef {Object} DownloadOptions
-    * @property {String} name имя файла
-    * @property {String} contentType тип файла 
-    * @remark Игнорируется при загрузке URL
-    */
-   public download(options?: Object) {
-      let type = (options && options['contentType']) ? options['contentType'] : this.contentType;
-      let blob: Blob = base64toblob(this.base64Data, type);
-      new BlobDriver(blob).download(options);
-   }
+    /**
+     * @typedef {Object} DownloadOptions
+     * @property {String} name имя файла
+     * @property {String} contentType тип файла
+     * @remark Игнорируется при загрузке URL
+     */
+    public download(options?: Object) {
+        const type = (options && options.contentType) ? options.contentType : this.contentType;
+        if (detection.isMobileSafari && detection.IOSVersion >= 12) {
+            window.open(BASE64_PREFIX(type) + this.base64Data);
+            return;
+        }
+        if (detection.safari && detection.IOSVersion >= 12) {
+            window.location.href = (BASE64_PREFIX(type) + this.base64Data);
+            return;
+        }
+        const blob: Blob = base64toblob(this.base64Data, type);
+        new BlobDriver(blob).download(options);
+    }
 }
 export = Base64;
