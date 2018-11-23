947a9bd15e853c6e6badf1402197a722502348af
am.gerasimov <am.gerasimov@tensor.ru>
2018-11-23 14:09:22 +0300

    https://online.sbis.ru/opendoc.html?guid=b070f95c-7b49-4c5d-9a8d-ab76eaa84205 Называю переменные по спецификации

--- Controls/Filter/Fast/FastStyles.js
+++ Controls/Filter/Fast/FastStyles.js
@@ -7,21 +7,21 @@ define('Controls/Filter/Fast/FastStyles', [],
     *
     * @css @height_FastFilter Height of Filter/Fast control.
     *
-    * @css @color_FastFilter_text Text color.
-    * @css @color_FastFilter_text_hover Hover text color.
-    * @css @color_FastFilter_text_active Active text color.
-    * @css @color_FastFilter_text_readOnly readOnly text color.
+    * @css @color_FastFilter-text Text color.
+    * @css @color_FastFilter-text_hover Hover text color.
+    * @css @color_FastFilter-text_active Active text color.
+    * @css @color_FastFilter-text_readOnly readOnly text color.
     *
-    * @css @color_FastFilter_arrow Arrow color.
-    * @css @color_FastFilter_arrow_hover Hover arrow color.
-    * @css @color_FastFilter_arrow_active Active arrow color.
-    * @css @color_FastFilter_arrow_readOnly readOnly arrow color.
-    * @css @font-size_FastFilter_arrow font size arrow icon.
+    * @css @color_FastFilter-arrow Arrow color.
+    * @css @color_FastFilter-arrow_hover Hover arrow color.
+    * @css @color_FastFilter-arrow_active Active arrow color.
+    * @css @color_FastFilter-arrow_readOnly readOnly arrow color.
+    * @css @font-size_FastFilter-arrow font size arrow icon.
     *
-    * @css @color_FastFilter_reset Reset button color.
-    * @css @color_FastFilter_reset_hover Hover reset button color.
-    * @css @color_FastFilter_reset_active Active reset button color.
-    * @css @font-size_FastFilter_reset font size of reset button.
+    * @css @color_FastFilter-reset Reset button color.
+    * @css @color_FastFilter-reset_hover Hover reset button color.
+    * @css @color_FastFilter-reset_active Active reset button color.
+    * @css @font-size_FastFilter-reset font size of reset button.
     *
     * @css @shift-top_FastFilter-popup Top shift of Filter/Fast popup.
     * @css @shift-left_FastFilter-popup Left shift of Filter/Fast popup.
