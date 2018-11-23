336a06dfde5049fd4193843f838362917d5147a3
ee.zolotova <ee.zolotova@tensor.ru>
2018-11-23 14:32:54 +0300

    https://online.sbis.ru/opendoc.html?guid=d994a3cf-d9a2-4ca9-8efc-8146855c74a4

--- Controls/interface/IMenu.js
+++ Controls/interface/IMenu.js
@@ -9,6 +9,155 @@ define('Controls/interface/IMenu', [], function() {
     */
 
    /**
+    * @name Controls/interface/IMenu#viewMode
+    * @cfg {Enum} Button view mode.
+    * @variant link Decorated hyperlink.
+    * @variant button Default button.
+    * @variant toolButton Toolbar button.
+    * @default button
+    * @example
+    * Button with 'link' viewMode.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="primary" viewMode="link" size="xl"/>
+    * </pre>
+    * Button with 'toolButton' viewMode.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="danger" viewMode="toolButton"/>
+    * </pre>
+    * Button with 'button' viewMode.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="success" viewMode="button"/>
+    * </pre>
+    * @see Controls/Button#viewMode
+    * @see size
+    */
+
+   /**
+    * @name Controls/interface/IMenu#style
+    * @cfg {Enum} Button display style.
+    * @variant primary The display style of the attracting attention button.
+    * @variant success The display style of the success button.
+    * @variant warning The display style of the warning button.
+    * @variant danger The display style of the danger button.
+    * @variant info The display style of the simple information button.
+    * @variant secondary The display style of the secondary button.
+    * @variant default The display style of button as default text.
+    * @default secondary
+    * @example
+    * Primary link button with 'primary' style.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="primary" viewMode="link" size="xl"/>
+    * </pre>
+    * Toolbar button with 'danger' style.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="danger" viewMode="toolButton"/>
+    * </pre>
+    * @see Controls/Button#style
+    * @see size
+    */
+
+   /**
+    * @name Controls/interface/IMenu#size
+    * @cfg {String} Button size. The value is given by common size notations.
+    * @variant s Small button size.
+    * @variant m Medium button size.
+    * @variant l Large button size.
+    * @variant xl Extra large button size.
+    * @default m
+    * @remark
+    * Button size is different for different button viewModes.
+    * Sizes 's' and 'xl' don't supported by viewModes:
+    * <ul>
+    *     <li>button,</li>
+    *     <li>toolButton</li>
+    * </ul>
+    * @example
+    * 'L' size of primary button.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="primary" viewMode="button" size="l"/>
+    * </pre>
+    * Default size of primary button.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="primary" viewMode="button"/>
+    * </pre>
+    * Uncorrect size of primary button.
+    * <pre>
+    *    <Controls.Button.Menu caption="Send document" style="primary" viewMode="button" size="xl"/>
+    * </pre>
+    * @see Controls/Button#size
+    * @see style
+    */
+
+   /**
+    * @name Controls/interface/IMenu#icon
+    * @cfg {String} Button icon.
+    * @default Undefined
+    * @remark  When you customize a button, use the icon style instead of the css-classes.
+    * @example
+    * Primary button with icon 'Add'.
+    * <pre>
+    *    <Controls.Button.Menu icon="icon-Add" style="primary" viewMode="button"/>
+    * </pre>
+    * @see Controls/Button#icon
+    * @see iconStyle
+    */
+
+   /**
+    * @name Controls/interface/IMenu#iconStyle
+    * @cfg {Enum} Icon display style.
+    * @variant primary The display style of the attracting attention icon.
+    * @variant success The display style of the success icon.
+    * @variant warning The display style of the warning icon.
+    * @variant danger The display style of the danger icon.
+    * @variant info The display style of the simple information icon.
+    * @variant secondary The display style of the secondary icon.
+    * @default secondary
+    * @example
+    * Primary button with default icon style.
+    * <pre>
+    *    <Controls.Button.Menu icon="icon-Add" style="primary" viewMode="button"/>
+    * </pre>
+    * Primary button with 'success' icon style.
+    * <pre>
+    *    <Controls.Button.Menu icon="icon-Add" iconStyle="success" style="primary" viewMode="button"/>
+    * </pre>
+    * @see Controls/Button#iconStyle
+    * @see icon
+    */
+
+   /**
+    * @name Controls/interface/IMenu#keyProperty
+    * @cfg {String} Name of the item property that uniquely identifies collection item.
+    * @example
+    * TMPL:
+    * <pre>
+    *    <Controls.Button.Menu
+    *       keyProperty="id"
+    *       source="{{_source}}" />
+    * </pre>
+    * JS:
+    * <pre>
+    *    this._source = new Memory({
+    *      idProperty: 'id',
+    *      data: [
+    *         {
+    *            id: '1',
+    *            title: 'Yaroslavl'
+    *         },
+    *         {
+    *            id: '2',
+    *            title: 'Moscow'
+    *         },
+    *         {
+    *            id: '3',
+    *            title: 'St-Petersburg'
+    *         }
+    *      ]
+    *    });
+    * </pre>
+    */
+
+   /**
     * @name Controls/interface/IMenu#source
     * @cfg {WS.Data/Source/Base} Object that implements ISource interface for data access.
     * @example
@@ -25,15 +174,50 @@ define('Controls/interface/IMenu', [], function() {
     *      data: [
     *         {
     *            id: '1',
-    *            city: 'Yaroslavl'
+    *            title: 'Yaroslavl'
     *         },
     *         {
     *            id: '2',
-    *            city: 'Moscow'
+    *            title: 'Moscow'
     *         },
     *         {
     *            id: '3',
-    *            city: 'St-Petersburg'
+    *            title: 'St-Petersburg'
+    *         }
+    *      ]
+    *    });
+    * </pre>
+    */
+
+   /**
+    * @name Controls/interface/IMenu#filter
+    * @cfg {Object} Filter configuration - object with field names and their values.
+    * @example
+    * In this example, 2 items will be displayed in the dropdown menu..
+    * TMPL:
+    * <pre>
+    *    <Controls.Button.Menu
+    *       keyProperty="id"
+    *       filter={{_filter}}
+    *       source="{{_source}}" />
+    * </pre>
+    * JS:
+    * <pre>
+    *    this._filter = {id: ['1, '2']};
+    *    this._source = new Memory({
+    *      idProperty: 'id',
+    *      data: [
+    *         {
+    *            id: '1',
+    *            title: 'Yaroslavl'
+    *         },
+    *         {
+    *            id: '2',
+    *            title: 'Moscow'
+    *         },
+    *         {
+    *            id: '3',
+    *            title: 'St-Petersburg'
     *         }
     *      ]
     *    });
@@ -164,17 +348,17 @@ define('Controls/interface/IMenu', [], function() {
     *      data: [
     *         {
     *            id: '1',
-    *            city: 'Yaroslavl',
+    *            title: 'Yaroslavl',
     *            'parent@': true
     *         },
     *         {
     *            id: '2',
-    *            city: 'Moscow',
+    *            title: 'Moscow',
     *            'parent@': true
     *         },
     *         {
     *            id: '3',
-    *            city: 'St-Petersburg',
+    *            title: 'St-Petersburg',
     *            'parent@': false
     *         }
     *      ]
@@ -184,6 +368,11 @@ define('Controls/interface/IMenu', [], function() {
 
    /**
     * @name Controls/interface/IMenu#parentProperty
+    * @cfg {String} Name of the field that contains item's parent identifier.
+    */
+
+   /**
+    * @name Controls/interface/IMenu#navigation
     * @cfg {String} Name of the field that contains item's parent identifier.
     */
 
@@ -293,6 +482,79 @@ define('Controls/interface/IMenu', [], function() {
     */
 
    /**
-    *
+    * @name Controls/interface/IMenu#groupMethod
+    * @cfg {Function} Function that returns group identifier.
+    * @example
+    * TMPL:
+    * <pre>
+    *    <Controls.Button.Menu
+    *          keyProperty="id"
+    *          icon="icon-small icon-AddButtonNew"
+    *          source="{{_source}}"
+    *          groupMethod="{{_groupMethod}}"/>
+    * </pre>
+    * JS:
+    * <pre>
+    *    this._groupMethod = function(item) {
+    *        return item.get('group');
+    *    }
+    *    this._source = new Memory({
+    *        data: [
+    *                   { id: 1, title: 'Task in development', group: 'Select' },
+    *                   { id: 2, title: 'Error in development', group: 'Select' },
+    *                   { id: 3, title: 'Application', group: 'Select' },
+    *                   { id: 4, title: 'Assignment', group: 'Create' },
+    *                   { id: 5, title: 'Approval', group: 'Create' },
+    *                   { id: 6, title: 'Working out', group: 'Create' },
+    *                   { id: 7, title: 'Assignment for accounting', group: 'Create' },
+    *                   { id: 8, title: 'Assignment for delivery', group: 'Create' },
+    *                   { id: 9, title: 'Assignment for logisticians', group: 'Create' }
+    *            ],
+    *        idProperty: 'id'
+    * </pre>
+    */
+
+   /**
+    * @name Controls/interface/IMenu#groupTemplate
+    * @cfg {Function | String} Group template.
+    * @remark
+    * To determine the template, you should call the base template "wml!Controls/Dropdown/resources/template/defaultGroupTemplate".
+    * The template should be placed in the component using the <ws:partial> tag with the template attribute.
+    * By default, the base template wml!Controls/Dropdown/resources/template/defaultGroupTemplate only displays a separator.  You can change the separator display by setting the option:
+    *    -  showText - sets the display of the group name.
+    * You can redefine content using the contentTemplate option.
+    * The groupMethod option must also be set.
+    * @example
+    * TMPL:
+    * <pre>
+    *    <Controls.Button.Menu
+    *          keyProperty="id"
+    *          icon="icon-small icon-AddButtonNew"
+    *          groupMethod="{{_groupMethod}}"
+    *          source="{{_source}}">
+    *       <ws:groupTemplate>
+    *          <ws:partial template="wml!Controls/Dropdown/resources/template/defaultGroupTemplate" showText="{{true}}" />
+    *       </ws:groupTemplate>
+    *    </Controls.Button.Menu>
+    * </pre>
+    * JS:
+    * <pre>
+    *    this._groupMethod = function(item) {
+    *        return item.get('group');
+    *    }
+    *    this._source = new Memory({
+    *        data: [
+    *                   { id: 1, title: 'Task in development', group: 'Select' },
+    *                   { id: 2, title: 'Error in development', group: 'Select' },
+    *                   { id: 3, title: 'Application', group: 'Select' },
+    *                   { id: 4, title: 'Assignment', group: 'Create' },
+    *                   { id: 5, title: 'Approval', group: 'Create' },
+    *                   { id: 6, title: 'Working out', group: 'Create' },
+    *                   { id: 7, title: 'Assignment for accounting', group: 'Create' },
+    *                   { id: 8, title: 'Assignment for delivery', group: 'Create' },
+    *                   { id: 9, title: 'Assignment for logisticians', group: 'Create' }
+    *            ],
+    *        idProperty: 'id'
+    * </pre>
     */
 });
