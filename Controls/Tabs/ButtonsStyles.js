20ecc42d81c99a1dd9dd5c25ca6c181b4cd5cbe5
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-11-23 15:18:34 +0300

    https://online.sbis.ru/opendoc.html?guid=0f77e10c-4357-4282-b419-5f899bedd76a

---
+++ Controls/Tabs/ButtonsStyles.js
@@ -0,0 +1,33 @@
+define('Controls/Tabs/ButtonsStyles',
+   [
+   ],
+   function() {
+   }
+
+   /**
+    * @author Михайловский Д.С.
+    * @mixin Controls/Tabs/ButtonsStyles
+    * @public
+    *
+    * @css @height_Tabs-separator Height of separator between tabs items.
+    * @css @width_Tabs-separator Width of separator between tabs items.
+    * @css @border-width_Tabs Thickness of line under tabs items.
+    * @css @padding_Tabs-item_default Spacing between tabs item content and tabs separator.
+    * @css @line-height_Tabs-baselineFix Line height for align items content.
+    * @css @height_Tabs Height tabs control.
+    * @css @padding_Tabs-item Padding of tabs item.
+    * @css @font-size_Tabs-item Font size of tabs item text.
+    * @css @font-size_Tabs-item_selected Font size of tabs item in selected state text.
+    * @css @max-width_Tabs_item Maximum width of tabs item.
+    * @css @border-width_Tabs-item_hover_selected Thickness of line under tabs items.
+    *
+    * @css @border-color_Tabs Border color of line under tabs items.
+    * @css @background-color_Tabs-separator Color of tabs items separator.
+    * @css @text-color_Tabs Color of tabs items text.
+    * @css @text-color_Tabs-item_selected_primary Color of tabs items with style 'primary' and selected state.
+    * @css @border-color_Tabs-item_selected_primary Border-color of tabs items with style 'primary' and selected state.
+    * @css @text-color_Tabs-item_selected_secondary Color of tabs items with style 'secondary' and selected state.
+    * @css @border-color_Tabs-item_selected_secondary Border-color of tabs items with style 'secondary' and selected state.
+    *
+    */
+);
