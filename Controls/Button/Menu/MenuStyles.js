5f73f3610b309b2415107cb000c1d5819ee12a9f
ee.zolotova <ee.zolotova@tensor.ru>
2018-11-27 14:08:53 +0300

    https://online.sbis.ru/opendoc.html?guid=e7c51831-f83d-44e8-b692-960baca0fff6

---
+++ Controls/Button/Menu/MenuStyles.js
@@ -0,0 +1,29 @@
+define('Controls/Button/Menu/MenuStyles', [],
+   function() {}
+
+   /**
+    * @mixin Controls/Button/Menu/MenuStyles
+    * @public
+    *
+    * @css @margin-top_MenuButton_button Offset on the top for pop-up at the menu with button viewMode.
+    * @css @margin-left_MenuButton_button Offset on the left for pop-up at the menu with button viewMode.
+    * @css @margin-top_MenuButton_link Offset on the top for pop-up at the menu with link viewMode without icon.
+    * @css @margin-left_MenuButton_link Offset on the left for pop-up at the menu with link viewMode without icon.
+    * @css @margin-top_MenuButton_bordered-medium Offset on the top for pop-up at the menu with quickButton viewMode and medium icon size.
+    * @css @margin-left_MenuButton_bordered-medium Offset on the left for pop-up at the menu with quickButton viewMode and medium icon size.
+    * @css @margin-top_MenuButton_bordered-small Offset on the top for pop-up at the menu with quickButton viewMode and small icon size.
+    * @css @margin-left_MenuButton_bordered-small Offset on the left for pop-up at the menu with quickButton viewMode and small icon size.
+    * @css @margin-top_MenuButton_link_small Offset on the top for pop-up at the menu with link viewMode and small icon size.
+    * @css @margin-left_MenuButton_link_small Offset on the left for pop-up at the menu with link viewMode and small icon size.
+    * @css @margin-top_MenuButton_link_medium Offset on the top for pop-up at the menu with link viewMode and medium icon size.
+    * @css @margin-left_MenuButton_link_medium Offset on the left for pop-up at the menu with link viewMode and medium icon size.
+    * @css @margin-top_MenuButton_link_duplicate_medium Offset on the top for pop-up at the menu with the duplication of the icon next to the first element,
+    * link viewMode and medium icon size.
+    * @css @margin-left_MenuButton_link_duplicate_medium Offset on the left for pop-up at the menu with the duplication of the icon next to the first element,
+    * link viewMode and medium icon size.
+    * @css @margin-top_MenuButton_link_duplicate_small Offset on the top for pop-up at the menu with the duplication of the icon next to the first element,
+    * link viewMode and medium icon size.
+    * @css @margin-left_MenuButton_link_duplicate_small Offset on the left for pop-up at the menu with the duplication of the icon next to the first element,
+    * with link viewMode and medium icon size.
+    */
+);
