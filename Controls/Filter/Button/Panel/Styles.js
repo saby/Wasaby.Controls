ba8960cd8bd4989da60ded8aae5f18be69b12d1c
am.gerasimov <am.gerasimov@tensor.ru>
2018-11-27 15:02:09 +0300

    https://online.sbis.ru/opendoc.html?guid=768d93bf-e827-4e9e-9fae-b39ec8a3a8cd

---
+++ Controls/Filter/Button/Panel/Styles.js
@@ -0,0 +1,45 @@
+define('Controls/Filter/Button/Panel/Styles', [],
+   function() {}
+   
+   /**
+    * @author Золотова Элина
+    * @mixin Controls/Filter/Button/Panel/Styles
+    * @public
+    *
+    * @css @width_FilterPanel-size_m The width of the panel on which css class controls-FilterPanel__width-m is set.
+    * @css @width_FilterPanel-size_l The width of the panel on which css class controls-FilterPanel__width-l is set.
+    * @css @width_FilterPanel-size_xl The width of the panel on which css class controls-FilterPanel__width-xl is set.
+    *
+    * @css @spacing_FilterPanel-between-resetButton-applyButton Spacing between reset filter button and apply filter button.
+    * @css @spacing_FilterPanel-between-applyButton-closeButton Spacing between apply filter button and close panel button.
+    * @css @spacing_FilterPanel-between-panelHeading-topTemplate Spacing between panel heading and top template content.
+    * @css @spacing_FilterPanel-between-topTemplate-resetButton Spacing between top template content and reset filter button.
+    *
+    * @css @font-size_FilterPanel-block-heading Font size of the block heading.
+    * @css @color_FilterPanel-block-heading Color of the block heading.
+    * @css @height_FilterPanel-block-heading Height of the block heading.
+    *
+    * @css @spacing_FilterPanel-block-content-bottom-border Spacing between content and bottom border.
+    *
+    * @css @spacing_FilterPanel-between-left-border-propertyGrid Spacing between left border and the property grid block.
+    * @css @spacing_FilterPanel-between-right-border-propertyGrid Spacing between right border and the property grid block.
+    *
+    * @css @spacing_FilterPanel-between-left-border-additionalParams Spacing between left border and the additional params block.
+    * @css @spacing_FilterPanel-between-right-border-additionalParams Spacing between right border and the additional params block.
+    * @css @spacing_FilterPanel-between-propertyGrid-additionalParams Spacing between the property grid block and the additional params block.
+    *
+    * @css @spacing_FilterPanel-between-top-border-history_vertical Spacing between top border and history block, when option orientation set to "vertical".
+    * @css @spacing_FilterPanel-between-left-border-history_vertical Spacing between left border and history block, when option orientation set to "vertical".
+    * @css @spacing_FilterPanel-between-right-border-history_vertical Spacing between right border and history block, when option orientation set to "vertical".
+    * @css @spacing_FilterPanel-between-bottom-border-history_vertical Spacing between bottom border and history block, when option orientation set to "vertical".
+    *
+    * @css @spacing_FilterPanel-between-top-border-history_horizontal Spacing between top border and history block, when option orientation set to "horizontal".
+    * @css @spacing_FilterPanel-between-left-border-history_horizontal Spacing between left border and history block, when option orientation set to "horizontal".
+    * @css @spacing_FilterPanel-between-right-border-history_horizontal Spacing between right border and history block, when option orientation set to "horizontal".
+    * @css @spacing_FilterPanel-between-bottom-border-history_horizontal Spacing between bottom border and history block, when option orientation set to "horizontal".
+    *
+    * @css @border-left_FilterPanel-history_horizontal Left border of the history block, when option orientation set to "horizontal".
+    * @css @max-height_FilterPanel-expandable-block Max height of the expandable blocks in panel.
+    * @css @height_FilterPanel-button-expand Expand button height.
+    */
+);
