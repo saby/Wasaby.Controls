fb7e94e9d69a7b34801aea2383ee304ceb3baa50
am.gerasimov <am.gerasimov@tensor.ru>
2018-11-27 17:00:31 +0300

    https://online.sbis.ru/opendoc.html?guid=2dc7471a-cd20-4cd1-8053-1e882df331d9

---
+++ Controls/Filter/Button/Panel/AdditionalParams/Styles.js
@@ -0,0 +1,13 @@
+define('Controls/Filter/Button/Panel/AdditionalParams/Styles', [],
+   function() {}
+   
+   /**
+    * @author Золотова Элина
+    * @mixin Controls/Filter/Button/Panel/Dropdown/Styles
+    * @public
+    *
+    * @css @color_AdditionalParams-item Text color of item.
+    * @css @height_AdditionalParams-item Item height.
+    * @css @spacing_AdditionalParams-between-left-border-content Spacing between left border and content.
+    */
+);
