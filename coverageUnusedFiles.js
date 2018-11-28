e8466993cda6984b3c95d0ab4b6e1385930235b2
aa.ibragimov <aa.ibragimov@tensor.ru>
2018-11-28 15:45:24 +0300

    https://online.sbis.ru/opendoc.html?guid=1945afe6-8ea9-4e7c-9359-8d3ceebaf316

--- coverageUnusedFiles.js
+++ coverageUnusedFiles.js
@@ -2,7 +2,6 @@ let path = require('path'),
     fs = require('fs'),
     nyc = require('nyc'),
     controlsPath = path.join(__dirname, 'Controls'),
-    filePath = path.join(__dirname, 'File'),
     componentsPath = path.join(__dirname, 'SBIS3.CONTROLS'),
     coveragePath = path.join(__dirname, 'artifacts', 'coverage.json'),
     coverageAllPath = path.join(__dirname, 'artifacts', 'coverageAll.json'),
@@ -28,9 +27,8 @@ dirWalker = function (dir) {
     }
 };
 
-// пробегаем по папкам Controls, File, Components
+// пробегаем по папкам Controls, Components
 dirWalker(controlsPath);
-dirWalker(filePath);
 dirWalker(componentsPath);
 
 let rawCover = fs.readFileSync(coveragePath, 'utf8'),
@@ -39,7 +37,6 @@ let rawCover = fs.readFileSync(coveragePath, 'utf8'),
     instrumenter = new nyc().instrumenter(),
     transformer = instrumenter.instrumentSync.bind(instrumenter),
     controlsFiles = allFiles.filter(file => file.includes('/Controls/')),
-    fileFiles = allFiles.filter(file => file.includes('/File/')),
     componentsFiles = allFiles.filter(file => file.includes('/SBIS3.CONTROLS/'));
 
 // функция дописывает 0 покрытие для файлов которые не использовались в тестах
@@ -71,7 +68,6 @@ function coverFiles(files, replacer) {
 
 // дописываем 0 покрытия для файлов которые не использовались в тестах
 coverFiles(controlsFiles, controlsPath);
-coverFiles(fileFiles, filePath);
 coverFiles(componentsFiles, componentsPath);
 
 // функция возвращает покрытие для опредленного пути
@@ -86,11 +82,9 @@ function getCoverByPath(path) {
 }
 
 let controlsCoverage = getCoverByPath(controlsPath),
-    fileCoverage = getCoverByPath(filePath),
     componentsCoverage = getCoverByPath(componentsPath);
 
-// сохраняем покрытие Общее, Controls, File, components
+// сохраняем покрытие Общее, Controls, components
 fs.writeFileSync(coverageAllPath, JSON.stringify(newCover), 'utf8');
 fs.writeFileSync(coverageControlsPath, JSON.stringify(controlsCoverage), 'utf8');
-fs.writeFileSync(coverageFilePath, JSON.stringify(fileCoverage), 'utf8');
 fs.writeFileSync(coverageComponentsPath, JSON.stringify(componentsCoverage), 'utf8');
