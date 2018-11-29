b8b9964cf3a66608751f9bafbe4d23d4d210e9de
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2018-11-29 12:37:48 +0300

    https://online.sbis.ru/opendoc.html?guid=49c5bbc9-0b97-4306-9670-02aa7c9a1dc1

--- Controls/Toolbar.js
+++ Controls/Toolbar.js
@@ -115,12 +115,7 @@ define('Controls/Toolbar', [
          }
       },
       _beforeUpdate: function(newOptions) {
-         if (newOptions.keyProperty !== this._options.keyProperty ||
-            this._options.parentProperty !== newOptions.parentProperty ||
-            this._options.nodeProperty !== newOptions.nodeProperty ||
-            this._options.size !== newOptions.size) {
-            _private.setPopupOptions(this, newOptions);
-         }
+         _private.setPopupOptions(this, newOptions);
          if (newOptions.source && newOptions.source !== this._options.source) {
             _private.loadItems(this, newOptions.source).addCallback(function() {
                this._forceUpdate();
@@ -163,7 +158,7 @@ define('Controls/Toolbar', [
 
       _showMenu: function() {
          var config = {
-            className: 'controls-Toolbar__menu-position',
+            className: 'controls-Toolbar__menu-position ' + (this._options.popupClassName || ''),
             templateOptions: {
                items: this._menuItems,
                iconSize: this._options.size
