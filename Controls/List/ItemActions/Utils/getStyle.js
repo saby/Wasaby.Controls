5e34c020ce9e567090b903f755a727d463a4d510
as.zaycev <as.zaycev@tensor.ru>
2018-12-03 11:02:57 +0300

    https://online.sbis.ru/opendoc.html?guid=a0a15e2e-4da5-478a-9981-771ef27faf71

---
+++ Controls/List/ItemActions/Utils/getStyle.js
@@ -0,0 +1,27 @@
+define('Controls/List/ItemActions/Utils/getStyle', [
+   'Core/IoC'
+], function(
+   IoC
+) {
+   'use strict';
+
+   var deprecatedStyles = {
+      error: 'danger',
+      done: 'success',
+      attention: 'warning',
+      default: 'secondary'
+   };
+
+   return function getStyle(style, controlName) {
+      if (!style) {
+         return 'secondary';
+      }
+
+      if (deprecatedStyles.hasOwnProperty(style)) {
+         IoC.resolve('ILogger').error(controlName, 'Используются устаревшие стили. Используйте ' + deprecatedStyles[style] + ' вместо ' + style);
+         return deprecatedStyles[style];
+      }
+
+      return style;
+   };
+});
