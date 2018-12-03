e84ee6c01de1df37b3beef0b398652101d932dde
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-12-03 13:29:03 +0300

    https://online.sbis.ru/opendoc.html?guid=1fa43d3c-f078-4555-b426-552a223db021

--- Controls/Utils/TouchKeyboardHelper.js
+++ Controls/Utils/TouchKeyboardHelper.js
@@ -1,8 +1,7 @@
 /**
  * Утилита рассчета высоты клавиатуры на тач устройствах
  */
-define('Controls/Utils/TouchKeyboardHelper', ['Core/constants', 'Core/EventBus'], function( constants, EventBus) {
-
+define('Controls/Utils/TouchKeyboardHelper', ['Core/constants', 'Core/EventBus'], function(constants, EventBus) {
    var ipadCoefficient = {
       portrait: 0.3,
       landscape: 0.56
@@ -12,46 +11,46 @@ define('Controls/Utils/TouchKeyboardHelper', ['Core/constants', 'Core/EventBus']
       _keyboardVisible: false,
       _keyboardAnimation: false,
 
-      _keyboardShowHandler: function(){
+      _keyboardShowHandler: function() {
          this._keyboardVisible = true;
          this._keyboardHandler();
       },
 
-      _keyboardHideHandler: function(){
+      _keyboardHideHandler: function() {
          this._keyboardVisible = false;
          this._keyboardHandler();
       },
 
       _keyboardHandler: function() {
          var self = this;
-         if(!this._keyboardAnimation){
+         if (!this._keyboardAnimation) {
             // из-за анимации клавиатуры на мобильных устройствах происходит сдвиг контента
             // что приводит к скрытие меню. делаем задержку и на время анимации меню не закрываем
             // увеличили время с 300мс до 350мс, т.к. на ipad мини клавиутаура анимируется дольше чем 300мс
-            this._keyboardAnimation = setTimeout(function(){
+            this._keyboardAnimation = setTimeout(function() {
                self._keyboardAnimation = null;
             }, 350);
          }
       },
 
-      isPortrait: function(){
+      isPortrait: function() {
          return window.innerHeight > window.innerWidth;
       },
 
-      getKeyboardAnimation: function(){
+      getKeyboardAnimation: function() {
          return this._keyboardAnimation;
       },
 
-      getKeyboardHeight: function(){
-         if (this.isKeyboardVisible()){
-            if (constants.browser.isMobileIOS){
+      getKeyboardHeight: function() {
+         if (this.isKeyboardVisible()) {
+            if (constants.browser.isMobileIOS) {
                return window.innerHeight * (this.isPortrait() ? ipadCoefficient.portrait : ipadCoefficient.landscape);
             }
          }
          return 0;
       },
 
-      isKeyboardVisible: function(){
+      isKeyboardVisible: function() {
          var isVisible = this._keyboardVisible;
 
          // Отдельно проверяем, есть ли фокус в полях ввода, т.к. клавиатура показана только в этом случае.
@@ -72,7 +71,7 @@ define('Controls/Utils/TouchKeyboardHelper', ['Core/constants', 'Core/EventBus']
       }
    };
 
-   if (constants.compatibility.touch){
+   if (constants.compatibility.touch) {
       EventBus.globalChannel().subscribe('MobileInputFocus', TouchKeyboardHelper._keyboardShowHandler.bind(TouchKeyboardHelper));
       EventBus.globalChannel().subscribe('MobileInputFocusOut', TouchKeyboardHelper._keyboardHideHandler.bind(TouchKeyboardHelper));
    }
