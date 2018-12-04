599d4d49210139a2ec8a148cc6bd0c2958c0f0f5
am.gerasimov <am.gerasimov@tensor.ru>
2018-12-04 08:48:00 +0300

    https://online.sbis.ru/opendoc.html?guid=c45853c9-5bc5-4198-b103-57ab326ee651

---
+++ Controls/Container/Suggest/Layout/Styles.js
@@ -0,0 +1,33 @@
+define('Controls/Container/Suggest/Layout/Styles',
+   [
+   
+   ],
+   function() {
+      /**
+       * @mixin Controls/Container/Suggest/Layout/Styles
+       * @public
+       * @author Герасимов А.М.
+       *
+       * @css @box-shadow_Suggest Suggest box-shadow.
+       * @css @border-color_Suggest Border color of suggest.
+       * @css @border-width_Suggest Border width of suggest.
+       * @css @background-color_Suggest Suggest background color.
+       *
+       * @css @color_Suggest-showAll Color of "show all" button.
+       * @css @color_Suggest-showAll_hovered Color of "show all" button in hovered state.
+       * @css @color_Suggest-showAll_active Color of "show all" button in active state.
+       * @css @height_Suggest-showAll Height of "show all" button.
+       * @css @spacing_Suggest-showAll-between-leftBorder-content Spacing between left border and "show all" button content.
+       * @css @spacing_Suggest-showAll-between-rightBorder-content Spacing between right border and "show all" button content.
+       *
+       * @css @spacing_Suggest-between-tabs-suggestions Spacing between tabs and suggestions.
+       *
+       * @css @color_Suggest-empty Color of suggest empty text.
+       * @css @spacing_Suggest-empty-between-topBorder-content Spacing between top border and empty text.
+       * @css @spacing_Suggest-empty-between-bottomBorder-content Spacing between bottom border and empty text.
+       *
+       * @css @spacing_Suggest-misspell-between-leftBorder-content Spacing between left border and misspell.
+       * @css @spacing_Suggest-misspell-between-rightBorder-content Spacing between right border and misspell.
+       */
+   }
+);
