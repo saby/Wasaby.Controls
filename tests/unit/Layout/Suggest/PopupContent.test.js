35e2e7fb6925ef5e3dc3280632ce64f4cdcc17ac
am.gerasimov <am.gerasimov@tensor.ru>
2018-12-06 12:44:38 +0300

    https://online.sbis.ru/opendoc.html?guid=58488a83-9205-440f-abbe-e9fdb075c40c

--- tests/unit/Layout/Suggest/PopupContent.test.js
+++ tests/unit/Layout/Suggest/PopupContent.test.js
@@ -1,4 +1,8 @@
-define(['Controls/Container/Suggest/__PopupContent'], function(PopupContent) {
+define(['Controls/Container/Suggest/__PopupContent', 'wml!Controls/Container/Suggest/__PopupContent'], function(PopupContent, popupContentTemplate) {
+   
+   function removeConfigFromMarkup(markup) {
+      return markup.replace(/ ?(ws-delegates-tabfocus|ws-creates-context|__config|config|tabindex|name)=".+?"/g, '');
+   }
    
    describe('Controls.Container.Suggest.__PopupContent', function() {
       
@@ -24,6 +28,31 @@ define(['Controls/Container/Suggest/__PopupContent'], function(PopupContent) {
          layer._afterUpdate(oldOptions);
    
          assert.isTrue(resized);
+      });
+      
+   });
+   
+   describe('Controls.Container.Suggest.__PopupContent template tests', function() {
+      
+      it('showContent:false', function() {
+         var standardMarkup = '<div class="controls-Suggest__suggestionsContainer ws-invisible"><div class="controls-Scroll ws-flexbox ws-flex-column controls-Suggest__scrollContainer" style="width:undefinedpx;" hasMarkup="true" data-component="Controls/Container/Scroll"><span class="ws-flex-grow-1 controls-Scroll__content ws-BlockGroup controls-Scroll__content_hideNativeScrollbar controls-Scroll__content_hidden"></span><div></div></div></div>';
+         var templateMarkup = popupContentTemplate({_options: {showContent: false, content: ''}});
+         
+         assert.equal(removeConfigFromMarkup(templateMarkup), standardMarkup);
+      });
+      
+      it('showContent:true', function() {
+         var standardMarkup = '<div class="controls-Suggest__suggestionsContainer"><div class="controls-Scroll ws-flexbox ws-flex-column controls-Suggest__scrollContainer" style="width:undefinedpx;" hasMarkup="true" data-component="Controls/Container/Scroll"><span class="ws-flex-grow-1 controls-Scroll__content ws-BlockGroup controls-Scroll__content_hideNativeScrollbar controls-Scroll__content_hidden"></span><div></div></div></div>';
+         var templateMarkup = popupContentTemplate({_options: {showContent: true, content: ''}});
+         
+         assert.equal(removeConfigFromMarkup(templateMarkup), standardMarkup);
+      });
+      
+      it('target.offsetWidth:300px', function() {
+         var standardMarkup = '<div class="controls-Suggest__suggestionsContainer ws-invisible"><div class="controls-Scroll ws-flexbox ws-flex-column controls-Suggest__scrollContainer" style="width:300px;" hasMarkup="true" data-component="Controls/Container/Scroll"><span class="ws-flex-grow-1 controls-Scroll__content ws-BlockGroup controls-Scroll__content_hideNativeScrollbar controls-Scroll__content_hidden"></span><div></div></div></div>';
+         var templateMarkup = popupContentTemplate({_options: {target: {offsetWidth: 300}, content: ''}});
+         
+         assert.equal(removeConfigFromMarkup(templateMarkup), standardMarkup);
       });
       
    });
