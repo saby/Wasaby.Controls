e0a38d75a791ae173f1830788b7307d75a221482
ea.proshin <ea.proshin@tensor.ru>
2018-12-07 11:57:19 +0300

    edit script

--- tests/get_err_from_rc.py
+++ tests/get_err_from_rc.py
@@ -5,6 +5,8 @@
 """
 
 import requests
+import collections
+import itertools
 
 
 JC_API = 'http://jenkins-control.tensor.ru/api/'
@@ -13,11 +15,12 @@ rec.raise_for_status()
 rec = requests.post(JC_API + 'test_result/errors_from_last_build', json={'id_job': rec.json()['result']})
 rec.raise_for_status()
 errors = rec.json()['result']
+err_dict = collections.defaultdict(list)
+new_list = list(itertools.chain(*errors.values()))
+for k, v in list(zip(new_list[::2], new_list[1::2])):
+    err_dict[v].append(k)
 description = "ПАДАЕТ В RC:<br>"
-for err in errors:
-    _iter = iter(errors[err])
-    rc_err = dict(zip(_iter, _iter))
-    for test in rc_err:
-        description += "<b>{}</b> - {}<br>".format(test, rc_err[test])
+for err in err_dict:
+    description += "<a href='{0}'>{0}</a> - {1}<br>".format(err, ', '.join(err_dict[err]))
 print(description)
 
