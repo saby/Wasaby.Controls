f1569d810a7589f7faa118512927f391fbf13e10
as.zaycev <as.zaycev@tensor.ru>
2018-12-08 11:50:26 +0300

    https://online.sbis.ru/opendoc.html?guid=54c51619-9716-4aad-a928-4c6e63a622db

--- tests/unit/List/Grid/GridViewModel.test.js
+++ tests/unit/List/Grid/GridViewModel.test.js
@@ -374,7 +374,7 @@ define(['Controls/List/Grid/GridViewModel', 'Core/core-merge', 'WS.Data/Collecti
                   'updateIndexes', 'setItems', 'setActiveItem', 'appendItems', 'prependItems', 'setItemActions', 'getDragTargetPosition',
                   'getIndexBySourceItem', 'at', 'getCount', 'setSwipeItem', 'getSwipeItem', 'updateSelection', 'getItemActions', 'getCurrentIndex',
                   '_prepareDisplayItemForAdd', 'mergeItems', 'toggleGroup', '_setEditingItemData',
-                  'getChildren', 'getActiveItem', 'setDragItems', 'getDragTargetItem', 'setDragTargetItem', 'destroy'],
+                  'getChildren', 'getActiveItem', 'destroy'],
                callStackMethods = [];
 
             gridViewModel._model = {};
