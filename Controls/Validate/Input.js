08252ae7f00d192e0a231fbf004120fffb0b2682
ps.kolesova <ps.kolesova@tensor.ru>
2018-12-12 01:13:15 +0300

    https://online.sbis.ru/opendoc.html?guid=59aaa8ff-abfd-4958-8894-5642c2034565

--- Controls/Validate/Input.js
+++ Controls/Validate/Input.js
@@ -20,10 +20,14 @@ define('Controls/Validate/Input',
             this._shouldValidate = true;
             this._forceUpdate();
          },
-         _cleanValid: function() {
+         _cleanValid: function(event, value) {
+            this._notify('valueChanged', [value]);
             if (this._validationResult) {
                this.setValidationResult(null);
             }
+         },
+         _inputCompletedHandler: function(event,value) {
+             this._notify('inputCompleted', [value]);
          },
          _afterUpdate: function() {
             if (this._shouldValidate) {
