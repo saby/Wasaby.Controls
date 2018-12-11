70849fd95f22bbd4c03c34dbc2b396a4456ad5b9
aa.malcev2 <aa.malcev2@tensor.ru>
2018-12-11 10:05:58 +0300

    Switch testing to github. https://online.sbis.ru/opendoc.html?guid=60af32ed-4e9a-4cf5-af03-e6cf3212ace3

--- test-report.js
+++ test-report.js
@@ -3,10 +3,10 @@
 /**
  * Запускает тестирование в Node.js и записывает результат в файл отчета.
  * Использование:
- * node node_modules/ws-unit-testing/scripts/mocha -t 10000 test-report
+ * node node_modules/saby-units/scripts/mocha -t 10000 test-report
  */
 
-let app = require('ws-unit-testing/isolated');
+let app = require('saby-units/isolated');
 const config = require('./package.json').config;
 
 app.run({
