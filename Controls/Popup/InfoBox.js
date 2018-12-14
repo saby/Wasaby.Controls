5f3cfbf5287bf6dfdb3fa862591ef44e226e4c9a
aa.artemeva1 <aa.artemeva1@tensor.ru>
2018-12-14 17:59:44 +0300

    Доработка документации

--- Controls/Popup/InfoBox.js
+++ Controls/Popup/InfoBox.js
@@ -20,7 +20,40 @@ define('Controls/Popup/InfoBox',
        * @public
        * @author Красильников А.С.
        * @demo Controls-demo/InfoBox/InfoBox
+       *
+       * @css @spacing_Infobox-between-content-border-top Spacing between content and border-top .
+       * @css @spacing_Infobox-between-content-border-right Spacing between content and border-right.
+       * @css @spacing_Infobox-between-content-border-bottom Spacing between content and border-bottom.
+       * @css @spacing_Infobox-between-content-border-left Spacing between content and border-left.
+       *
+       * @css @max-width_Infobox Max-width of Infobox.
+       * @css @size_Infobox-arrow Size of Infobox arrow.
+       * @css @horizontal-offset_Infobox-arrow Spacing between arrow and border-left.
+       * @css @vertical-offset_Infobox-arrow  Spacing between arrow and border-top.
+       * @css @spacing_Infobox-between-top-close-button Spacing between close-button and border-top.
+       * @css @spacing_Infobox-between-right-close-button Spacing between close-button and border-right.
+       *
+       * @css @color_Infobox-close-button Color of close-button.
+       * @css @color_Infobox-close-button_hover Color of close-button in hovered state.
+       *
+       * @css @background-color_Infobox_default Default background color.
+       * @css @background-color_Infobox_lite Background color when option style is set to lite.
+       * @css @background-color_Infobox_help Background color when option style is set to help.
+       * @css @background-color_Infobox_error Background color when option style is set to error.
+       *
+       * @css @border-color_Infobox_default Default border color.
+       * @css @border-color_Infobox_lite Border color when option style is set to lite.
+       * @css @border-color_Infobox_help Border color when option style is set to help.
+       * @css @border-color_Infobox_error Border color when option style is set to error.
+       * @css @border-width_Infobox Thickness of border.
+       *
+       * @css @color_Infobox-shadow_default Default color of shadow.
+       * @css @color_Infobox-shadow_lite Color of shadow when option style is set to lite.
+       * @css @color_Infobox-shadow_help Color of shadow when option style is set to help.
+       * @css @color_Infobox-shadow_error Color of shadow when option style is set to lite.
+       * @css @box-shadow_Infobox Size of shadow.
        */
+
 
       /**
        * @name Controls/Popup/InfoBox#hideDelay
