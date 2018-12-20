17d1cd46046ea2aa39ea14a878ae4115bdbd13ff
as.krasilnikov <as.krasilnikov@tensor.ru>
2018-12-20 13:21:50 +0300

    https://online.sbis.ru/opendoc.html?guid=3602314a-b182-4bae-a6da-fce80caed02f

--- Controls/Popup/Opener/InfoBox/resources/template.js
+++ Controls/Popup/Opener/InfoBox/resources/template.js
@@ -12,7 +12,7 @@ define('Controls/Popup/Opener/InfoBox/resources/template',
          _close: function() {
             // todo Для совместимости
             // Удалить, как будет сделана задача https://online.sbis.ru/opendoc.html?guid=dedf534a-3498-4b93-b09c-0f36f7c91ab5
-            this._notify('sendResult', [{ type: 'close' }, { bubbling: true }]);
+            this._notify('sendResult', [{ type: 'close' }], { bubbling: true });
 
             this._notify('close');
          }
