38fad6107889b40b602abf77e97bb9760d1fb2c7
ee.zolotova <ee.zolotova@tensor.ru>
2018-12-20 16:21:52 +0300

    https://online.sbis.ru/opendoc.html?guid=fa2b6553-2bfc-426f-861b-8b7757f11d74

--- tests/unit/Dropdown/Controller.test.js
+++ tests/unit/Dropdown/Controller.test.js
@@ -340,7 +340,7 @@ define(
             dropdownController._open();
          });
          it('getFilter', () => {
-            let filter = Dropdown._private.getFilter({id: 'test'}, new historySource({}));
+            var filter = Dropdown._private.getFilter({id: 'test'}, new historySource({}));
             assert.deepEqual(filter, {$_history: true, id: 'test'});
             filter = Dropdown._private.getFilter({id: 'test2'}, new Memory({}));
             assert.deepEqual(filter, {id: 'test2'});
