848929bf98b90aa7fe9f690a4a89e7425bba796e
am.gerasimov <am.gerasimov@tensor.ru>
2018-12-24 15:33:04 +0300

    https://online.sbis.ru/opendoc.html?guid=9d610490-899b-4ecd-a54c-13dcd44fc618

--- Controls/Search/Misspell/getSwitcherStrFromData.js
+++ Controls/Search/Misspell/getSwitcherStrFromData.js
@@ -6,6 +6,10 @@ define('Controls/Search/Misspell/getSwitcherStrFromData', [], function() {
    
    return function(data) {
       var metaData = data && data.getMetaData();
+      if (metaData && metaData.hasOwnProperty(SWITCHED_STR_FIELD)) {
+         return metaData[SWITCHED_STR_FIELD];
+      }
+      //FIXME delete after https://online.sbis.ru/opendoc.html?guid=46b1d157-6458-42a9-b863-89bea9be9a7d
       return metaData && metaData.results ? metaData.results.get(SWITCHED_STR_FIELD) : null;
    };
 });
