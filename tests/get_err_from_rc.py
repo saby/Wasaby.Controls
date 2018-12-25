2839bc9768327881bcd02c2ceaf153397e953f7e
ea.proshin <ea.proshin@tensor.ru>
2018-12-25 17:18:55 +0300

    fix script

--- tests/get_err_from_rc.py
+++ tests/get_err_from_rc.py
@@ -36,17 +36,6 @@ class RC:
             for k, v in list(zip(self.test_names, self.err_links)):
                 self.err_dict[v].append(k)
 
-    def get_errors_mock(self, jobs):
-        for job in jobs:
-            if 'reg' in job:
-                self.test_names = ['RegressionVDOMBreadCrumbs.test_06_opened_menu','RegressionVDOMBreadCrumbs.test_04_2_crumbs',
-                                         'RegressionVDOMBreadCrumbs.test_02_half_crumbs', 'RegressionVDOMBreadCrumbs.test_01_all_crumbs',
-                                         'RegressionVDOMBreadCrumbs.test_05_resize','RegressionVDOMBreadCrumbs.test_03_half_crumbs_2']
-                self.err_dict = {'http://err.ru':self.test_names}
-            elif 'int' in job:
-                pass
-
-
     def get_status_title(self, rc_list, new_list):
         """Возвращает статус сборки с описанием
         - списки ошибок равны ОК
@@ -60,16 +49,12 @@ class RC:
         new_set = set(new_list)
 
         if rc_set == new_set and bool(new_set):
-            # print('rc_set == new_set')
             return "OK|Ошибки в сборке уже попали в RC."
         elif new_set.issubset(rc_set) and bool(new_set):
-            # print('new_set.issubset(rc_set)')
             return "OK|Ошибки в сборке уже попали в RC."
         elif rc_set.issubset(new_set) or not bool(rc_set.intersection(new_set)):
-            # print('rc_set.issubset(new_set) or not rc_set.intersection(new_set)')
             return "FAIL|В сборке падает UI тесты по новым ошибкам! В RC таких нет."
         elif not bool(rc_set) and bool(new_set):
-            # print('not rc_set and new_set')
             return "FAIL|В сборке падает UI тесты по новым ошибкам! В RC таких нет."
 
     def description(self, fail_tests_path, skip):
@@ -103,7 +88,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--skip', help='Test was skipped.', action='store_true', default=False)
     args = parser.parse_args()
     rc = RC()
-    rc.get_errors_mock(args.jobs)
+    rc.get_errors(args.jobs)
     result = rc.description(args.fail_tests_path, args.skip)
     if rc.err_exist or rc.head:
         print('{}\n{}'.format(rc.head, result))
