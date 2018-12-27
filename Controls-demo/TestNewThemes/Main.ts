bb9944531ea3323c06fda3ef59eb3577371777c3
nv.belotelov <nv.belotelov@tensor.ru>
2018-12-27 12:35:48 +0300

    https://online.sbis.ru/opendoc.html?guid=2f857490-ae21-4e23-afc4-6448d8debfb6

--- Controls-demo/TestNewThemes/Main.ts
+++
@@ -1,9 +0,0 @@
-/// <amd-module name="Controls-demo/TestNewThemes/Main"/>
-import * as BaseControl from 'Core/Control';
-import template = require('wml!Controls-demo/TestNewThemes/Main');
-
-class Main extends BaseControl {
-    private _template: Function = template;
-}
-
-export = Main;
