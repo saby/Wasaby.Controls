0f12056fd01619c9dbe02899ceb8a51384172a19
ma.baranov1 <ma.baranov1@tensor.ru>
2019-01-09 14:52:07 +0300

    Поправил require ___ https://online.sbis.ru/opendoc.html?guid=3c72380c-9683-4c33-a0ff-0d691d2f2081

--- Controls/Popup/Compatible/Layer.js
+++ Controls/Popup/Compatible/Layer.js
@@ -137,8 +137,8 @@ define('Controls/Popup/Compatible/Layer', [
             data = window.userInfo;
          }
       } else {
-         moduleStubs.require(['EngineUser/Info:Info']).addCallbacks(function(modules) {
-            data = modules[0].getAll();
+         moduleStubs.require(['EngineUser/Info']).addCallbacks(function(modules) {
+            data = modules[0].Info.getAll();
          }, function(err) {
             IoC.resolve('ILogger').error('Layer', 'Can\'t load EngineUser/Info', err);
          });
