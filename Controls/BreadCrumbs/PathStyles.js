16baa160b0dedf627aba88b22cad05bf3061e5de
as.zaycev <as.zaycev@tensor.ru>
2019-01-10 13:22:55 +0300

    новый стандарт https://online.sbis.ru/opendoc.html?guid=9247592e-fdee-4112-a382-295cad0d4e13

--- Controls/BreadCrumbs/PathStyles.js
+++ Controls/BreadCrumbs/PathStyles.js
@@ -11,6 +11,7 @@ define('Controls/BreadCrumbs/PathStyles',
        * @css @spacing_Path-between-home-and-crumbs Spacing between the "Home" icon and breadcrumbs.
        * @css @color_Path-backButtonArrow Color of the backbutton's text.
        * @css @color_Path-backButtonArrow_hovered Color of the backbutton's text on hover.
+       * @css @color_Path-backButtonArrow_readonly Color of the backbutton's text in readonly mode.
        * @css @background-color_Path-backButtonArrow Background color of the "View record" icon.
        * @css @color_Path-home Color of the "Home" icon.
        * @css @color_Path-home_hovered Color of the "Home" icon on hover.
