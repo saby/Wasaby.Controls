81042024bb3521789cfa4eb56fc9bd564e24c9b4
rn.kondakov <rn.kondakov@tensor.ru>
2019-01-14 11:29:24 +0300

    https://online.sbis.ru/opendoc.html?guid=826b4b3d-d235-4e47-bade-57f527e7d811

--- tests/unit/Decorator/Markup.test.js
+++ tests/unit/Decorator/Markup.test.js
@@ -324,6 +324,11 @@ define([
                '</div>';
             assert.isTrue(equalsHtml(Converter.jsonToHtml(json, highlightResolver, { textToHighlight: 'aBa' }), html));
          });
+         it('only text', function() {
+            // TODO: remove case in https://online.sbis.ru/opendoc.html?guid=a8a904f8-6c0d-4754-9e02-d53da7d32c99.
+            assert.equal(Converter.jsonToHtml(['some text']), '<div>some text</div>');
+            assert.equal(Converter.jsonToHtml(['p', 'some text']), '<div><p>some text</p></div>');
+         });
       });
    });
 });
