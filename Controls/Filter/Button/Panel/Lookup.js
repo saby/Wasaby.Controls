713a04b7fa3711bc13e42a5e3b3f0819f03c91af
mi.platov <mi.platov@tensor.ru>
2019-01-18 11:53:52 +0300

    add interface

--- Controls/Filter/Button/Panel/Lookup.js
+++ Controls/Filter/Button/Panel/Lookup.js
@@ -5,10 +5,23 @@ define('Controls/Filter/Button/Panel/Lookup', [
 ], function(Control, template) {
    /**
     * Control link with lookup
+    * Here you can see <a href="/materials/demo-ws4-engine-selector-lookup">demo-example</a>.
     * @class Controls/Filter/Button/Panel/Lookup
-    * @extends Controls/Control
+    * @mixes Controls/interface/ISelectedCollection
+    * @mixes Controls/Input/interface/ISearch
+    * @mixes Controls/interface/ISource
+    * @mixes Controls/interface/IItemTemplate
+    * @mixes Controls/interface/IFilter
+    * @mixes Controls/Input/interface/ISuggest
+    * @mixes Controls/interface/ILookup
+    * @mixes Controls/interface/INavigation
+    * @mixes Controls/interface/IMultiSelectable
+    * @mixes Controls/Input/interface/IInputPlaceholder
+    * @mixes Controls/Input/interface/IInputText
+    * @mixes Controls/Selector/Lookup/LookupStyles
     * @control
     * @public
+    * @author Герасимов А.М.
     */
 
    /**
