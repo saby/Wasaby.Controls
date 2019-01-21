8392970db7f020879dc988f4d6bd2b441611bd2c
ma.baranov1 <ma.baranov1@tensor.ru>
2019-01-21 17:28:57 +0300

    https://online.sbis.ru/opendoc.html?guid=20244caf-47e7-4b18-9d96-b903385a7bc6

---
+++ Controls/breadCrumbs.ts
@@ -0,0 +1,6 @@
+import ItemTemplate = require('wml!Controls/_breadCrumbs/View/resources/itemTemplate');
+
+export {default as Path} from './_breadCrumbs/Path';
+export {default as View} from './_breadCrumbs/View';
+export {default as HeadingPath} from './_breadCrumbs/HeadingPath';
+export {ItemTemplate};
