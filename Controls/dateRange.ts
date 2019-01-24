b2e717d57f362a138409fad62748c21e3da2a4b2
ma.baranov1 <ma.baranov1@tensor.ru>
2019-01-24 17:32:09 +0300

    https://online.sbis.ru/opendoc.html?guid=7ff6649a-cc2b-4635-96aa-a83f615ae7fa

--- Controls/dateRange.ts
+++ Controls/dateRange.ts
@@ -1,3 +1,3 @@
-export {default as Range} from './_dateRange/Range';
-export {default as RangeLink} from './_dateRange/RangeLink';
-export {default as RangeLinkLite} from './_dateRange/RangeLinkLite';
+export {default as Range} from './_dateRange/Input';
+export {default as RangeLink} from './_dateRange/Selector';
+export {default as RangeLinkLite} from './_dateRange/LiteSelector';
