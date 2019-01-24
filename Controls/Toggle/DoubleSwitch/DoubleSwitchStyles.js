35967767fd32a4b2bcc37510a3f11ca5205e009b
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2019-01-24 15:10:23 +0300

    https://online.sbis.ru/opendoc.html?guid=84d77452-2cf9-42a7-9b35-8a393665edc7

---
+++ Controls/Toggle/DoubleSwitch/DoubleSwitchStyles.js
@@ -0,0 +1,16 @@
+define('Controls/Toggle/DoubleSwitch/DoubleSwitchStyles',
+   [
+   ],
+   function() {
+   }
+
+   /**
+    *
+    * @mixin Controls/Toggle/Switch/SwitchStyles
+    * @public
+    * @author Михайловский Д.С.
+    *
+    * @css @color_Switch-text_enabled_selected Selected text color of enabled switch.
+    * @css @color_Switch-text_disabled_unselected Unselected text color of disabled switch.
+    */
+);
