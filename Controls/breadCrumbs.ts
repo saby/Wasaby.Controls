d7bfa50927e6f98f45195f7529683f665eba57de
ma.baranov1 <ma.baranov1@tensor.ru>
2019-01-25 19:05:12 +0300

    Обратная совместимость

--- Controls/breadCrumbs.ts
+++ Controls/breadCrumbs.ts
@@ -1,14 +1,6 @@
 import ItemTemplate = require('wml!Controls/_breadCrumbs/View/resources/itemTemplate');
-import BC_menuItemTemplate = require('wml!Controls/_breadCrumbs/resources/menuItemTemplate');
-import BC_menuContentTemplate = require('wml!Controls/_breadCrumbs/resources/menuContentTemplate');
-import BC_HeadingPath = require('wml!Controls/_breadCrumbs/HeadingPath/HeadingPath');
 
 export {default as Path} from './_breadCrumbs/Path';
 export {default as View} from './_breadCrumbs/View';
 export {default as HeadingPath} from './_breadCrumbs/HeadingPath';
 export {ItemTemplate};
-
-// Для обратной совместимости
-export {BC_menuItemTemplate}
-export {BC_menuContentTemplate}
-export {BC_HeadingPath}
