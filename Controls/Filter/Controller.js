0e5da9921fc94d682e6648fae5a39b7fd862ed3e
aa.shipin <aa.shipin@tensor.ru>
2019-01-25 16:10:58 +0300

    Controls/Filter/Controller getCalculatedFilter static method

--- Controls/Filter/Controller.js
+++ Controls/Filter/Controller.js
@@ -243,7 +243,7 @@ define('Controls/Filter/Controller',
             });
          },
 
-         applyItemsToFilter: function(self, filter, filterButtonItems, fastFilterItems) {
+         calculateFilterByItems: function(filter, filterButtonItems, fastFilterItems) {
             var filterClone = clone(filter || {});
             var itemsFilter = _private.getFilterByItems(filterButtonItems, fastFilterItems);
             var emptyFilterKeys = _private.getEmptyFilterKeys(filterButtonItems, fastFilterItems);
@@ -254,6 +254,10 @@ define('Controls/Filter/Controller',
 
             merge(filterClone, itemsFilter);
 
+            return filterClone;
+         },
+         applyItemsToFilter: function(self, filter, filterButtonItems, fastFilterItems) {
+            var filterClone = _private.calculateFilterByItems(filter, filterButtonItems, fastFilterItems);
             _private.setFilter(self, filterClone);
          },
 
@@ -284,6 +288,26 @@ define('Controls/Filter/Controller',
             return clone(items);
          },
       };
+
+      function getCalculatedFilter(cfg) {
+         var def = new Deferred();
+         var tmpStorage = {};
+         _private.resolveItems(tmpStorage, cfg.historyId, cfg.filterButtonSource, cfg.fastFilterSource).addCallback(function(items) {
+            var calculatedFilter;
+            try {
+               calculatedFilter = _private.calculateFilterByItems(cfg.filter, tmpStorage._filterButtonItems, tmpStorage._fastFilterItems);
+            } catch(err) {
+               def.errback(err);
+               throw err;
+            }
+            def.callback(calculatedFilter);
+            return items;
+         }).addErrback(function(err) {
+            def.errback(err);
+            return err;
+         });
+         return def;
+      }
 
       /**
        * The filter controller allows you to filter data in a {@link Controls/List} using {@link Filter/Button} or {@link Filter/Fast}.
@@ -438,5 +462,6 @@ define('Controls/Filter/Controller',
       });
 
       Container._private = _private;
+      Container.getCalculatedFilter = getCalculatedFilter;
       return Container;
    });
