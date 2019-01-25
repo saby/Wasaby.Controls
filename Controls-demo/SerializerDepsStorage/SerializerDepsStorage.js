afa7497b645fa9951dcd9736f451003999aa4c50
rn.kondakov <rn.kondakov@tensor.ru>
2019-01-25 18:10:48 +0300

    https://online.sbis.ru/opendoc.html?guid=a4f2a0f1-54fb-4ee0-b6e1-0ac0826ff39c

--- Controls-demo/SerializerDepsStorage/SerializerDepsStorage.js
+++ Controls-demo/SerializerDepsStorage/SerializerDepsStorage.js
@@ -14,10 +14,13 @@ define('Controls-demo/SerializerDepsStorage/SerializerDepsStorage', [
          var self = this;
          return new Promise(function(resolve) {
             if (typeof document === 'undefined') {
+               // Выполним на сервере асинхронный require модуля, генерирующего объект, который содержит функцию.
                require(['Controls-demo/SerializerDepsStorage/ModuleToRequire'], function(result) {
                   resolve({ opt: result.myFunc });
                });
             } else {
+               // На клиенте функция из модуля, полученного на сервере, должна прийти в третьем аргументе.
+               // В зависимости от того, выполнено это условие или нет, в шаблоне отрисуется "верно" или "неверно".
                self.result = typeof rs.opt === 'function' && rs.opt() === 'function result' ? 'верно' : 'неверно';
                resolve();
             }
