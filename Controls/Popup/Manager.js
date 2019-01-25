5184d4748ec7c8dac453df227cc4bffdc56ec89e
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-01-25 11:32:29 +0300

    https://online.sbis.ru/opendoc.html?guid=329217dc-3e92-4ad9-9fb8-351a3d6cc244

--- Controls/Popup/Manager.js
+++ Controls/Popup/Manager.js
@@ -35,7 +35,6 @@ define('Controls/Popup/Manager',
 
             self._notify('managerPopupBeforeDestroyed', [element, self._popupItems, container], { bubbling: true });
             return removeDeferred.addCallback(function afterRemovePopup() {
-               self._popupItems.remove(element);
                _private.activatePopup(self._popupItems);
                _private.updateOverlay.call(self);
                self._notify('managerPopupDestroyed', [element, self._popupItems], { bubbling: true });
@@ -74,7 +73,7 @@ define('Controls/Popup/Manager',
             var element = ManagerController.find(id);
             if (element) {
                // Register new popup
-               _private.fireEventHandler(id, 'onOpen');
+               _private.fireEventHandler.call(this, id, 'onOpen');
                element.controller._elementCreated(element, _private.getItemContainer(id), id);
                this._notify('managerPopupCreated', [element, this._popupItems], { bubbling: true });
                return true;
@@ -170,7 +169,7 @@ define('Controls/Popup/Manager',
 
          popupResult: function(id) {
             var args = Array.prototype.slice.call(arguments, 1);
-            return _private.fireEventHandler.apply(_private, [id, 'onResult'].concat(args));
+            return _private.fireEventHandler.apply(this, [id, 'onResult'].concat(args));
          },
 
          popupClose: function(id) {
@@ -201,7 +200,7 @@ define('Controls/Popup/Manager',
          },
 
          fireEventHandler: function(id, event) {
-            var item = ManagerController.find(id);
+            var item = _private.findItemById(this, id);
             var args = Array.prototype.slice.call(arguments, 2);
             if (item) {
                if (item.popupOptions._events) {
@@ -273,7 +272,7 @@ define('Controls/Popup/Manager',
          },
 
          findItemById: function(self, id) {
-            var index = self._popupItems.getIndexByValue('id', id);
+            var index = self._popupItems && self._popupItems.getIndexByValue('id', id);
             if (index > -1) {
                return self._popupItems.at(index);
             }
@@ -382,7 +381,8 @@ define('Controls/Popup/Manager',
             var element = this.find(id);
             if (element) {
                _private.removeElement.call(this, element, _private.getItemContainer(id), id).addCallback(function() {
-                  _private.fireEventHandler(id, 'onClose');
+                  _private.fireEventHandler.call(self, id, 'onClose');
+                  self._popupItems.remove(element);
                   _private.redrawItems(self._popupItems);
                   return element;
                });
