6ee06d1218b068cded2285a6588a9b7c990f5e6b
ia.kapustin <ia.kapustin@tensor.ru>
2019-02-05 14:17:14 +0300

    https://online.sbis.ru/opendoc.html?guid=d63b0ee7-1574-4b76-986a-d275fdc02406

--- tests/unit/Input/LookupSingle.test.js
+++ tests/unit/Input/LookupSingle.test.js
@@ -218,5 +218,14 @@ define([
          lookup._options.multiSelect = true;
          assert.isTrue(lookup._determineAutoDropDown());
       });
+
+      it('_onClickShowSelector', function() {
+         var lookup = new Lookup();
+
+         lookup._suggestState = true;
+         lookup._onClickShowSelector();
+
+         assert.isFalse(lookup._suggestState);
+      });
    });
 });
