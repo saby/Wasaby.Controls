65035c83daf72338ad9572f397be456c65a380e3
am.gerasimov <am.gerasimov@tensor.ru>
2019-02-05 10:55:09 +0300

    https://online.sbis.ru/opendoc.html?guid=03c5d13d-f0cf-4b25-9ac0-f354ba9aeb4f

--- Controls/History/Source.js
+++ Controls/History/Source.js
@@ -329,6 +329,8 @@ define('Controls/History/Source', [
    };
 
    var Source = CoreExtend.extend([sourceLib.ISource, entity.OptionsToPropertyMixin], {
+      //for compatibility with Types lib, will removed after rewriting module on typescript
+      '[Types/_source/ICrud]': true,
       _history: null,
       _oldItems: null,
       _parentProperty: null,
