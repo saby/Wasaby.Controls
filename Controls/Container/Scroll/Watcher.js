a58a46f9d2e5c6eb75830d3c83a440ac174a76a9
dv.zuev <dv.zuev@tensor.ru>
2019-02-05 15:45:16 +0300

    http://online.sbis.ru/opendoc.html?guid=bd76ad64-6d29-4339-a02f-08b7b616984c

--- Controls/Container/Scroll/Watcher.js
+++ Controls/Container/Scroll/Watcher.js
@@ -238,7 +238,7 @@ define('Controls/Container/Scroll/Watcher',
          },
 
          _beforeMount: function() {
-            this._forceUpdate = function(){};
+            this._forceUpdate = function() {};
             this._registrar = new Registrar({register: 'listScroll'});
          },
 
