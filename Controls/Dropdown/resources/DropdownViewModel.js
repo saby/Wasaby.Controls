068f6cdfd5c287ffb73e91169f70a7e92970cc3b
am.gerasimov <am.gerasimov@tensor.ru>
2019-02-07 16:06:21 +0300

    https://online.sbis.ru/opendoc.html?guid=d431d3a2-ca39-4fe1-85aa-102354df6ccf

--- Controls/Dropdown/resources/DropdownViewModel.js
+++ Controls/Dropdown/resources/DropdownViewModel.js
@@ -62,6 +62,7 @@ define('Controls/Dropdown/resources/DropdownViewModel',
          _expanded: false,
 
          constructor: function(cfg) {
+            var self = this;
             this._options = cfg;
             DropdownViewModel.superclass.constructor.apply(this, arguments);
             this._itemsModel = new ItemsViewModel({
@@ -71,6 +72,9 @@ define('Controls/Dropdown/resources/DropdownViewModel',
                items: cfg.items,
                keyProperty: cfg.keyProperty,
                displayProperty: 'title'
+            });
+            this._itemsModel.subscribe('onListChange', function() {
+               self._nextVersion();
             });
             this._hierarchy = new entity.relation.Hierarchy({
                idProperty: cfg.keyProperty,
@@ -82,7 +86,6 @@ define('Controls/Dropdown/resources/DropdownViewModel',
 
          setFilter: function(filter) {
             this._itemsModel.setFilter(filter);
-            this._nextVersion();
          },
 
          getDisplayFilter: function() {
@@ -95,13 +98,11 @@ define('Controls/Dropdown/resources/DropdownViewModel',
          setItems: function(options) {
             this._options.items = options.items;
             this._itemsModel.setItems(options.items);
-            this._nextVersion();
          },
 
          setRootKey: function(key) {
             this._options.rootKey = key;
             this.setFilter(this.getDisplayFilter());
-            this._nextVersion();
          },
 
          destroy: function() {
@@ -162,7 +163,6 @@ define('Controls/Dropdown/resources/DropdownViewModel',
          },
          setSwipeItem: function(itemData) {
             this._swipeItem = itemData;
-            this._nextVersion();
          },
          hasHierarchy: function() {
             if (!this._options.parentProperty || !this._options.nodeProperty) {
