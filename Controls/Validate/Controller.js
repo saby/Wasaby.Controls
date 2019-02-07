c55df3c7753b9d098ce5ac007696d885d759ba88
ps.kolesova <ps.kolesova@tensor.ru>
2019-02-07 23:51:19 +0300

    https://online.sbis.ru/opendoc.html?guid=f188574c-3ec7-4a0b-9cf2-e10a526dd1e6

--- Controls/Validate/Controller.js
+++ Controls/Validate/Controller.js
@@ -113,8 +113,9 @@ define('Controls/Validate/Controller',
          },
          _beforeUnmount: function() {
             this._notify('validateDestroyed', [this], { bubbling: true });
-            //Infobox must be closed when controller is removed.
-            //Will be fixed by https://online.sbis.ru/opendoc.html?guid=3629c103-a23a-40f2-8647-2ae50d6e0ea4
+
+            // Infobox must be closed when controller is removed.
+            // Will be fixed by https://online.sbis.ru/opendoc.html?guid=3629c103-a23a-40f2-8647-2ae50d6e0ea4
             if (this._isNewEnvironment) {
                this._notify('closeInfoBox', [0], { bubbling: true });
             } else {
@@ -256,7 +257,6 @@ define('Controls/Validate/Controller',
          _valueChangedHandler: function(event, value) {
             this._notify('valueChanged', [value]);
             this._cleanValid();
-
          },
          _cleanValid: function() {
             if (this._validationResult) {
