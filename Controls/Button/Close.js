de71dd283e528272cbcc53faea99e2cbaaf3aa6a
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2019-02-07 16:36:55 +0300

    https://online.sbis.ru/opendoc.html?guid=fb5aa7fe-28af-4f56-b969-3721c3730d85

--- Controls/Button/Close.js
+++ Controls/Button/Close.js
@@ -86,10 +86,10 @@ define('Controls/Button/Close', [
             self._viewMode = (options.style === 'light' ? 'link' : 'toolButton');
             if (options.style !== undefined) {
                IoC.resolve('ILogger').warn('Close', 'Option "style" is deprecated and removed in 19.200. Use option "viewMode".');
-            }
-            if (options.style !== 'default') {
-               IoC.resolve('ILogger').warn('Close', 'Option "style" is deprecated and not regulated transparency. Use option "transparent".');
-               self._transparent = false;
+               if (options.style !== 'default') {
+                  IoC.resolve('ILogger').warn('Close', 'Option "style" is deprecated and not regulated transparency. Use option "transparent".');
+                  self._transparent = false;
+               }
             }
          }
       }
