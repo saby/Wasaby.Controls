d8ff2ed29ddce881ec014f8b2aae1d6a44685967
ia.kapustin <ia.kapustin@tensor.ru>
2019-02-14 11:03:53 +0300

    https://online.sbis.ru/opendoc.html?guid=822a8dbe-b265-4eeb-9583-662690bfeaf3

--- tests/unit/Input/LookupSingle.test.js
+++ tests/unit/Input/LookupSingle.test.js
@@ -114,7 +114,9 @@ define([
       });
 
       it('_beforeUpdate', function() {
-         var lookup = new Lookup();
+         var
+            items = new collection.List(),
+            lookup = new Lookup();
 
          lookup._beforeMount({multiLine: true});
          lookup._beforeUpdate({});
@@ -137,7 +139,14 @@ define([
          assert.equal(lookup._availableWidthCollection, undefined);
 
          lookup._counterWidth = 30;
+         lookup._options.items = items;
          lookup._beforeUpdate({
+            items: items
+         });
+         assert.equal(lookup._counterWidth, 30);
+
+         lookup._beforeUpdate({
+            items: items,
             readOnly: true
          });
          assert.equal(lookup._counterWidth, undefined);
