5569c98cf272f1f26edf3228190e55c25560057e
as.zaycev <as.zaycev@tensor.ru>
2019-02-15 12:36:26 +0300

    https://online.sbis.ru/opendoc.html?guid=11a5be00-7067-4a3d-a263-0e019881443e

--- Controls/Utils/tmplNotify.js
+++ Controls/Utils/tmplNotify.js
@@ -6,6 +6,11 @@ define('Controls/Utils/tmplNotify', [], function() {
     * A handler to use in templates to proxy events to the logic parent.
     */
    return function(event, eventName) {
+      /**
+       * We can't ignore bubbling events here, because no one guarantees they're the same.
+       * E.g. on:myEvent="_tmplNotify('anotherEvent')"
+       * Here, event gets forwarded but under a different name.
+       */
       var args = Array.prototype.slice.call(arguments, 2);
       event.blockUpdate = true;
 
