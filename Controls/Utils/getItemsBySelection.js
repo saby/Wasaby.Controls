a4e4bdc4a04bf014d00f7f07f61237ad425ab92a
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2019-02-22 11:38:33 +0300

    https://online.sbis.ru/opendoc.html?guid=111ef3a3-32e4-4217-8935-ab8d3043f957

--- Controls/Utils/getItemsBySelection.js
+++ Controls/Utils/getItemsBySelection.js
@@ -28,21 +28,6 @@ define('Controls/Utils/getItemsBySelection', [
          result.set('excluded', prepareArray(selection.excluded));
 
          return result;
-      },
-      getSourceAdapter = function getSourceModel(dataSource) {
-         var adapter;
-
-         //до выполнения задачи https://online.sbis.ru/opendoc.html?guid=4190d360-e9de-49ed-a1a4-7420686134d0
-         if (dataSource instanceof source.PrefetchProxy) {
-
-            //Т.к. у PrefetchProxy, в target может лежать PrefetchProxy, рекурсивно найдем обычный сорс, у которого
-            //есть метод getAdapter. В 210 откажемся от поиска адаптера по сорсу и всегда будем использовать 'adapter.sbis'.
-            adapter = getSourceAdapter(dataSource._$target);
-         } else {
-            adapter = dataSource.getAdapter();
-         }
-
-         return adapter;
       };
 
    return function(selection, dataSource, items, filter) {
@@ -66,7 +51,7 @@ define('Controls/Utils/getItemsBySelection', [
          query = new source.Query();
 
          filter = filter ? cClone(filter) : {};
-         filter.selection = selectionToRecord(selection, getSourceAdapter(dataSource));
+         filter.selection = selectionToRecord(selection, 'adapter.sbis');
 
          result = dataSource.query(query.where(filter)).addCallback(function(list) {
             return chain.factory(list.getAll()).toArray().map(function(item) {
