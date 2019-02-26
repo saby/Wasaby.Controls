a241ab92e6f31e50f8bd775b5958002e55570370
ee.zolotova <ee.zolotova@tensor.ru>
2019-02-26 09:21:50 +0300

    https://online.sbis.ru/opendoc.html?guid=988eedb6-8a18-4b5d-8f37-4d7442fa69aa

--- Controls/Input/Dropdown/Styles.js
+++ Controls/Input/Dropdown/Styles.js
@@ -6,7 +6,19 @@ define('Controls/Input/Dropdown/Styles', [],
     * @mixin Controls/Input/Dropdown/Styles
     * @public
     *
-    * @css @border-color_Dropdown_style_invalid Color of the dropdown border when style option is set to invalid.
-    * @css @border-width_Dropdown_style_invalid Width of the dropdown border when style option is set to invalid
+    * @css @height_Dropdown Dropdown height.
+    *
+    * @css @color_Dropdown-text Text color.
+    * @css @color_Dropdown-text_hovered Text color in hovered state.
+    * @css @font-size_DropdownList-text Font size of text.
+    *
+    * @css @shift-top_Dropdown-popup Offset on the top for dropdown list.
+    * @css @shift-top_Dropdown-head-popup Offset on the top for dropdown list with header.
+    * @css @shift-left_Dropdown-popup Offset on the left for dropdown list.
+    *
+    * @css @color_Dropdown-emptyItem Text color of empty item.
+    *
+    * @css @border-color_Dropdown-style_invalid Color of the dropdown border when style option is set to invalid.
+    * @css @border-width_Dropdown-style_invalid Width of the dropdown border when style option is set to invalid.
     */
 );
