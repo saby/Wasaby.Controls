5a3c1308572b40d427fc74e1b4efcabba6bbdb5a
av.zalyaev <av.zalyaev@tensor.ru>
2019-02-26 19:23:55 +0300

    https://online.sbis.ru/opendoc.html?guid=7d5a831d-df9b-4156-9903-5d7130b690c6 delete js

--- Controls/_dataSource/_error/Handler.js
+++
@@ -1,4 +0,0 @@
-define("Controls/_dataSource/_error/Handler", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
--- Controls/_dataSource/_parking/Handler.js
+++
@@ -1,5 +0,0 @@
-/// <amd-module name="Controls/_dataSource/_parking/Handler" />
-define("Controls/_dataSource/_parking/Handler", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-});
