838e0dbef710138e7a90edc03df7c0a634becf48
ma.baranov1 <ma.baranov1@tensor.ru>
2019-02-27 09:39:56 +0300

    TreeGridViewModel на ts ___ https://online.sbis.ru/opendoc.html?guid=de0d946c-b937-46d6-9ec7-86eca47a9d97

--- Controls/List/TreeGridView/TreeGridViewModel.ts
+++ Controls/List/TreeGridView/TreeGridViewModel.ts
@@ -1,75 +1,70 @@
-define('Controls/List/TreeGridView/TreeGridViewModel', [
-   'Controls/List/Tree/TreeViewModel',
-   'Controls/List/Grid/GridViewModel'
-], function(TreeViewModel, GridViewModel) {
+import TreeViewModel = require('Controls/List/Tree/TreeViewModel');
+import GridViewModel = require('Controls/List/Grid/GridViewModel');
 
-   'use strict';
-
-   var
-      TreeGridViewModel = GridViewModel.extend({
-         _onNodeRemovedFn: null,
-         constructor: function() {
+var
+    TreeGridViewModel = GridViewModel.extend({
+        _onNodeRemovedFn: null,
+        constructor: function () {
             TreeGridViewModel.superclass.constructor.apply(this, arguments);
             this._onNodeRemovedFn = this._onNodeRemoved.bind(this);
             this._model.subscribe('onNodeRemoved', this._onNodeRemovedFn);
-         },
-         _createModel: function(cfg) {
+        },
+        _createModel: function (cfg) {
             return new TreeViewModel(cfg);
-         },
-         toggleExpanded: function(dispItem, expand) {
+        },
+        toggleExpanded: function (dispItem, expand) {
             this._model.toggleExpanded(dispItem, expand);
-         },
-         isExpanded: function(dispItem) {
+        },
+        isExpanded: function (dispItem) {
             return this._model.isExpanded(dispItem);
-         },
-         isExpandAll: function() {
+        },
+        isExpandAll: function () {
             return this._model.isExpandAll();
-         },
-         setExpandedItems: function(expandedItems) {
+        },
+        setExpandedItems: function (expandedItems) {
             this._model.setExpandedItems(expandedItems);
-         },
-         getExpandedItems: function() {
+        },
+        getExpandedItems: function () {
             return this._model.getExpandedItems();
-         },
-         setRoot: function(root) {
+        },
+        setRoot: function (root) {
             this._model.setRoot(root);
-         },
-         setNodeFooterTemplate: function(nodeFooterTemplate) {
+        },
+        setNodeFooterTemplate: function (nodeFooterTemplate) {
             this._model.setNodeFooterTemplate(nodeFooterTemplate);
-         },
-         setExpanderDisplayMode: function(expanderDisplayMode) {
+        },
+        setExpanderDisplayMode: function (expanderDisplayMode) {
             // Выпилить в 19.200
             this._model.setExpanderDisplayMode(expanderDisplayMode);
-         },
-         setExpanderVisibility: function(expanderVisibility) {
+        },
+        setExpanderVisibility: function (expanderVisibility) {
             this._model.setExpanderVisibility(expanderVisibility);
-         },
-         resetExpandedItems: function() {
+        },
+        resetExpandedItems: function () {
             this._model.resetExpandedItems();
-         },
-         getCurrent: function() {
+        },
+        getCurrent: function () {
             var
-               current = TreeGridViewModel.superclass.getCurrent.apply(this, arguments),
-               superGetCurrentColumn = current.getCurrentColumn;
-            current.getCurrentColumn = function() {
-               var
-                  currentColumn = superGetCurrentColumn();
-               currentColumn.isExpanded = current.isExpanded;
-               return currentColumn;
+                current = TreeGridViewModel.superclass.getCurrent.apply(this, arguments),
+                superGetCurrentColumn = current.getCurrentColumn;
+            current.getCurrentColumn = function () {
+                var
+                    currentColumn = superGetCurrentColumn();
+                currentColumn.isExpanded = current.isExpanded;
+                return currentColumn;
             };
             return current;
-         },
-         _onNodeRemoved: function(event, nodeId) {
+        },
+        _onNodeRemoved: function (event, nodeId) {
             this._notify('onNodeRemoved', nodeId);
-         },
-         setHasMoreStorage: function(hasMoreStorage) {
+        },
+        setHasMoreStorage: function (hasMoreStorage) {
             this._model.setHasMoreStorage(hasMoreStorage);
-         },
-         destroy: function() {
+        },
+        destroy: function () {
             this._model.unsubscribe('onNodeRemoved', this._onNodeRemovedFn);
             TreeGridViewModel.superclass.destroy.apply(this, arguments);
-         }
-      });
+        }
+    });
 
-   return TreeGridViewModel;
-});
+export = TreeGridViewModel;
