5b9d8d0fe78343de2bc82b1bdb6d5222e5e641c6
va.kolesov <va.kolesov@tensor.ru>
2019-03-01 17:55:29 +0300

    https://online.sbis.ru/opendoc.html?guid=1f58f983-dac2-4260-a522-3b7c459a717e

--- Controls/Indicator/Progress/Bar.js
+++ Controls/Indicator/Progress/Bar.js
@@ -1,33 +1,32 @@
 define('Controls/Indicator/Progress/Bar', [
-      'Controls/progress',
-      'Env/Env'
+   'Controls/progress',
+   'Env/Env'
 ], function(progressLib, Env) {
-      'use strict';
+   'use strict';
 
-      /**
-       * Control that renders progress bar
-       *
-       * @class Controls/Indicator/Progress/Bar
-       * @extends Core/Control
-       *
-       * @public
-       *
-       * @author Baranov M.A.
-       *
-       * @demo Controls-demo/Indicator/ProgressBar/ProgressBar
-       *
-       * @css @color-ProgressBar__bar Progress bar background color
-       * @css @height-ProgressBar_bar Progress bar height
-       * @css @color-ProgressBar__progress Progress bar fill color
-       */
+   /**
+   * Control that renders progress bar
+   *
+   * @class Controls/Indicator/Progress/Bar
+   * @extends Core/Control
+   *
+   * @public
+   *
+   * @author Baranov M.A.
+   *
+   * @demo Controls-demo/Indicator/ProgressBar/ProgressBar
+   *
+   * @css @color-ProgressBar__bar Progress bar background color
+   * @css @height-ProgressBar_bar Progress bar height
+   * @css @color-ProgressBar__progress Progress bar fill color
+   */
 
+   /**
+   * @name Controls/Indicator/Progress/Bar#value
+   * @cfg {Number} Progress in percents (ratio of the filled part)
+   */
 
-      /**
-       * @name Controls/Indicator/Progress/Bar#value
-       * @cfg {Number} Progress in percents (ratio of the filled part)
-       */
-
-      Env.IoC.resolve('ILogger').error(
+   Env.IoC.resolve('ILogger').error(
       'Controls/Indicator/Progress/Bar',
       'This control is deprecated. Use \'Controls/progress:Bar\' instead'
    );
