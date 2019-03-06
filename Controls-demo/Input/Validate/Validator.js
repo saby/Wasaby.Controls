4e97324548db48de08333e25a4bed7220a632346
ps.kolesova <ps.kolesova@tensor.ru>
2019-03-06 14:09:53 +0300

    https://online.sbis.ru/opendoc.html?guid=7daf1dce-e42b-4324-85a5-cb834cd520ba

---
+++ Controls-demo/Input/Validate/Validator.js
@@ -0,0 +1,13 @@
+define('Controls-demo/Input/Validate/Validator', ['wml!Controls-demo/Input/Validate/ErrorMssg'], function() {
+   'use strict';
+
+   return function(args) {
+      if (!args.value) {
+         return true;
+      }
+      var value = args.value.toString();
+      if (value.length !== 3) {
+         return 'wml!Controls-demo/Input/Validate/ErrorMssg';
+      }
+   };
+});
