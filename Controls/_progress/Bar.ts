fa46a12db98733de0252176f0e9526a319786027
va.kolesov <va.kolesov@tensor.ru>
2019-03-07 14:39:45 +0300

    https://online.sbis.ru/opendoc.html?guid=881ed0a8-0390-4332-853c-ff8a90fbf6d3

--- Controls/_progress/Bar.ts
+++ Controls/_progress/Bar.ts
@@ -3,8 +3,21 @@ import entity = require('Types/entity');
 import template = require('wml!Controls/_progress/Bar/Bar');
 
 var
+   _private = {
+      getWidth: function(val){
+        return val > 0 ? val+'%' : '0px'
+      }
+   },
    Bar = Control.extend({
-      _template: template
+      _template: template,
+      _width: '0px',
+
+      _beforeMount: function(opts){
+         this._width = _private.getWidth(opts.value);
+      },
+      _beforUpdate: function(opts){
+         this._width = _private.getWidth(opts.value);
+      },
    });
 
    Bar.getOptionTypes = function() {
