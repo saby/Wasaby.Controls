acdb527084c97c32083a0a3bd4c85eeea95d6a6c
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2019-03-07 10:54:02 +0300

    https://online.sbis.ru/opendoc.html?guid=5ec72ef1-ee4c-43f7-a0a1-ef243dc63fe4

--- Controls/DragNDrop/Container/Compound.js
+++ Controls/DragNDrop/Container/Compound.js
@@ -2,12 +2,16 @@ define('Controls/DragNDrop/Container/Compound',
    [
       'Core/Control',
       'wml!Controls/DragNDrop/Container/Compound/Compound',
+      'wml!Controls/DragNDrop/DraggingTemplateWrapper',
       'Vdom/Vdom'
    ],
 
-   function(Control, template, Vdom) {
+   function(Control, template, draggingTemplateWrapper, Vdom) {
+      var ZINDEX_FOR_OLD_PAGE = 10000;
+
       return Control.extend({
          _template: template,
+         _draggingTemplate: null,
 
          _onRegisterHandler: function(event, eventName, emitter, handler) {
             if (['mousemove', 'touchmove', 'mouseup', 'touchend'].indexOf(eventName) !== -1) {
@@ -21,6 +25,30 @@ define('Controls/DragNDrop/Container/Compound',
                   document.body.removeEventListener(eventName, this._compoundHandlers[eventName]);
                   this._compoundHandlers[eventName] = null;
                }
+            }
+         },
+
+         _removeDraggingTemplate: function() {
+            if (this._draggingTemplate) {
+               this._draggingTemplate.remove();
+               this._draggingTemplate = null;
+            }
+         },
+
+         _documentDragEnd: function() {
+            this._removeDraggingTemplate();
+         },
+
+         _updateDraggingTemplate: function(event, draggingTemplateOptions, draggingTemplate) {
+            this._removeDraggingTemplate();
+
+            if (draggingTemplate) {
+               this._draggingTemplate = $(draggingTemplateWrapper({
+                  draggingTemplateOptions: draggingTemplateOptions,
+                  draggingTemplate: draggingTemplate
+               }));
+               this._draggingTemplate.appendTo(document.body);
+               this._draggingTemplate.css('z-index', ZINDEX_FOR_OLD_PAGE);
             }
          }
       });
