aa850a8b92331f6949c6100ef9af2c4ed499f912
va.kolesov <va.kolesov@tensor.ru>
2019-03-07 15:31:48 +0300

    https://online.sbis.ru/opendoc.html?guid=d769ad9c-2bec-47ad-8759-b606be8d89e4

--- Controls/Indicator/Progress/Bar.js
+++ Controls/Indicator/Progress/Bar.js
@@ -4,27 +4,7 @@ define('Controls/Indicator/Progress/Bar', [
 ], function(progressLib, Env) {
    'use strict';
 
-   /**
-   * Control that renders progress bar
-   *
-   * @class Controls/Indicator/Progress/Bar
-   * @extends Core/Control
-   *
-   * @public
-   *
-   * @author Baranov M.A.
-   *
-   * @demo Controls-demo/Indicator/ProgressBar/ProgressBar
-   *
-   * @css @color-ProgressBar__bar Progress bar background color
-   * @css @height-ProgressBar_bar Progress bar height
-   * @css @color-ProgressBar__progress Progress bar fill color
-   */
-
-   /**
-   * @name Controls/Indicator/Progress/Bar#value
-   * @cfg {Number} Progress in percents (ratio of the filled part)
-   */
+   
 
    Env.IoC.resolve('ILogger').error(
       'Controls/Indicator/Progress/Bar',
--- Controls/_progress/Bar.ts
+++ Controls/_progress/Bar.ts
@@ -1,9 +1,23 @@
 /// <amd-module name="Controls/_progress/Bar" />
-/**
- * Progress Bar
- * @class Controls/_progress/Bar 
- * @author Колесов В.А.
- */
+ /**
+   * Control that renders progress bar
+   * @class Controls/_progress/Bar 
+   * @extends Core/Control
+   * @author Колесов В.А.
+   *
+   * @public 
+   *
+   * @demo Controls-demo/Indicator/ProgressBar/ProgressBar
+   *
+   * @css @color-ProgressBar__bar Progress bar background color
+   * @css @height-ProgressBar_bar Progress bar height
+   * @css @color-ProgressBar__progress Progress bar fill color
+   */
+
+   /**
+   * @name Controls/Indicator/Progress/Bar#value
+   * @cfg {Number} Progress in percents (ratio of the filled part)
+   */
 
 import Control = require('Core/Control');
 import entity = require('Types/entity');
