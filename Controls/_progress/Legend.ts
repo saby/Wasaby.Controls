0887faaba457844af72abc6769ae79eea46d030c
va.kolesov <va.kolesov@tensor.ru>
2019-03-07 13:59:48 +0300

    https://online.sbis.ru/opendoc.html?guid=1ae2626a-e2e3-442a-b36d-1b83fc059c23

---
+++ Controls/_progress/Legend.ts
@@ -0,0 +1,48 @@
+/// <amd-module name="Controls/_progress/Legend" />
+/**
+ * Legend for StateIndicator
+ * @class Controls/_progress/Legend 
+ * @author Колесов В.А.
+ */
+
+import Control = require('Core/Control');
+import entity = require('Types/entity');
+import template = require('wml!Controls/_progress/Legend/Legend');
+
+var Legend;
+   
+var Legend = Control.extend(
+   {
+      _template: template      
+   });
+
+Legend.getDefaultOptions = function getDefaultOptions() {
+   return {
+      theme: "default",
+      /**
+       * @typedef {Object} IndicatorCategory
+       * @property {Number} value=0 Percents of the corresponding category
+       * @property {String} className='' Name of css class, that will be applied to sectors of this category. If not specified, default color will be used
+       * @property {String} title='' category note
+       */
+      /**
+       * @cfg {Array.<IndicatorCategory>} Array of indicator categories
+       * <pre class="brush:html">
+       *   <Controls.progress:Legend data="{{[{value: 10, className: '', title: 'done'}]]}}"/>      
+       * </pre>
+       */
+      data: [{value:0, className:'', title:''}],
+   };
+};
+
+Legend.getOptionTypes = function getOptionTypes() {
+   return {
+      data: entity.descriptor(Array),
+   };
+};
+
+Legend._theme = ['Controls/_progress/progress'];
+
+export default Legend;
+
+
--- Controls/progress.ts
+++ Controls/progress.ts
@@ -1,3 +1,13 @@
+/// <amd-module name="Controls/progress" />
+/**
+ * Progress indicators library
+ * @library Controls/progress
+ * @includes StateIndicator Controls/_progress/StateIndicator
+ * @includes Legend Controls/_progress/Lenend
+ * @author Колесов В.А.
+ */
+
 import 'css!theme?Controls/_progress/progress';
 
 export {default as StateIndicator} from './_progress/StateIndicator';
+export {default as Legend} from './_progress/Legend';
