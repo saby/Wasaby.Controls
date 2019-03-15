a1698181532f01743366cef87fe8c7f851810489
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-03-15 10:12:19 +0300

    https://online.sbis.ru/opendoc.html?guid=00a8e7a6-c4b7-4301-a4eb-700d2ef01e9f

--- Controls/Popup/Manager.js
+++ Controls/Popup/Manager.js
@@ -434,6 +434,7 @@ define('Controls/Popup/Manager',
          _contentClick: function(event) {
             _private.popupItems.each(function(item) {
                // Закрываем только те окна, которые были открыты до mousedown'a
+               // todo: https://online.sbis.ru/opendoc.html?guid=00a8e7a6-c4b7-4301-a4eb-700d2ef01e9f
                if (item && item.waitDeactivated && _private.popupItemsClone && _private.popupItemsClone.getIndexByValue('id', item.id) > -1) {
                   if (!_private.isIgnoreActivationArea(event.target)) {
                      _private.popupDeactivated(item.id);
