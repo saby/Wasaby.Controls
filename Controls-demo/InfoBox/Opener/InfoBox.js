ee2d07c2636b945bffdf88809f296ed7c1485dac
ps.kolesova <ps.kolesova@tensor.ru>
2019-03-17 21:47:21 +0300

    https://online.sbis.ru/opendoc.html?guid=c84ea0a0-4650-4042-846d-d1590173a722

--- Controls-demo/InfoBox/Opener/InfoBox.js
+++ Controls-demo/InfoBox/Opener/InfoBox.js
@@ -6,13 +6,13 @@ define('Controls-demo/InfoBox/Opener/InfoBox',
       'css!Controls-demo/InfoBox/Opener/InfoBox'
    ],
    function(Control, contentTpl, template) {
-
       'use strict';
 
       var message = 'MESSAGE';
       var InfoBox = Control.extend({
          _template: template,
          _blocks: null,
+         _icons: null,
          _minTargetMode: false,
          _beforeMount: function() {
             this._blocks = [{
@@ -138,18 +138,114 @@ define('Controls-demo/InfoBox/Opener/InfoBox',
                   }
                }]
             }];
+            this._icons = [{
+               items: [
+                  {
+                     cfg: {
+                        size: 'small',
+                        position: 'lc',
+                        message: 'Small left-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'small',
+                        position: 'tc',
+                        message: 'Small top-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'small',
+                        position: 'bc',
+                        message: 'Small bottom-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'small',
+                        position: 'rc',
+                        message: 'Small right-center'
+                     }
+                  }
+               ]
+            },
+            {
+               items: [
+                  {
+                     cfg: {
+                        size: 'medium',
+                        position: 'lc',
+                        message: 'Medium left-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'medium',
+                        position: 'tc',
+                        message: 'Medium top-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'medium',
+                        position: 'bc',
+                        message: 'Medium bottom-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'medium',
+                        position: 'rc',
+                        message: 'Medium right-center'
+                     }
+                  },
+               ]
+            },
+            {
+               items: [
+                  {
+                     cfg: {
+                        size: 'large',
+                        position: 'lc',
+                        message: 'Large left-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'large',
+                        position: 'tc',
+                        message: 'Large top-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'large',
+                        position: 'bc',
+                        message: 'Large bottom-center'
+                     }
+                  },
+                  {
+                     cfg: {
+                        size: 'large',
+                        position: 'rc',
+                        message: 'Large right-center'
+                     }
+                  },
+               ]
+            }
+            ];
          },
 
-         _open: function(e, cfg){
+         _open: function(e, cfg) {
             cfg.target = e.target;
             this._children.IBOpener.open(cfg);
          },
 
-         _toggleMinTargetMode: function(){
+         _toggleMinTargetMode: function() {
             this._minTargetMode = !this._minTargetMode;
          }
       });
 
       return InfoBox;
-   }
-);
+   });
