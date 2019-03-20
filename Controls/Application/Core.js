30339ea144840f6bc85f6f9c5c3304eec23e6616
ka.sannikov <ka.sannikov@tensor.ru>
2019-03-20 17:31:51 +0300

    https://online.sbis.ru/opendoc.html?guid=8d8860ce-8579-4838-876a-b6c9aeb194d7 Поправил инициализацию Application

--- Controls/Application/Core.js
+++ Controls/Application/Core.js
@@ -50,15 +50,18 @@ define('Controls/Application/Core',
             //__hasRequest - для совместимости, пока не смержено WS. Нужно чтобы работало
             //и так и сяк
             var stateReceiverInst = new StateReceiver();
+            var environmentFactory = undefined;
+            if (typeof window === 'undefined') {
+               environmentFactory = PresentationService.default
+            }
+            AppInit.default(cfg, environmentFactory, stateReceiverInst);
+
             if (typeof window === 'undefined' || window.__hasRequest === undefined) {
                //need create request for SSR
                //on client request will create in app-init.js
-               AppInit.default(cfg, PresentationService.default, stateReceiverInst);
                if (typeof window !== 'undefined' && window.receivedStates) {
                   stateReceiverInst.deserialize(window.receivedStates);
                }
-            } else {
-               AppInit.default(cfg, undefined, stateReceiverInst);
             }
 
             var headData = new HeadData([], true);
