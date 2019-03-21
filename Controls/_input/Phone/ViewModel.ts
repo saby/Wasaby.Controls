b7eaa36b0f922b0ac2da844f8abc34877ceb4aba
aa.shipin <aa.shipin@tensor.ru>
2019-03-21 15:39:52 +0300

    fix

--- Controls/_input/Phone/ViewModel.ts
+++ Controls/_input/Phone/ViewModel.ts
@@ -77,7 +77,7 @@ import InputProcessor = require('Controls/_input/Mask/InputProcessor');
 
          moveCarriageToEnd: function() {
             this.selection = this.displayValue.length;
-
+            this._nextVersion();
             this._shouldBeChanged = true;
          }
       });
