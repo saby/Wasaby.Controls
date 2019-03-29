b80abebf3c474f2da60037be59d5c87eccc8ec3d
ma.baranov1 <ma.baranov1@tensor.ru>
2019-03-29 14:38:10 +0300

    https://online.sbis.ru/opendoc.html?guid=1833666f-e344-4326-a3e7-f52d269367b7

---
+++ Controls/Input/Render.js
@@ -0,0 +1,6 @@
+define('Controls/Input/Render',
+   ['Controls/_input/Render'],
+   function(Control) {
+      'use strict';
+      return Control;
+   });
---
+++ Controls/Input/Render/Styles.js
@@ -0,0 +1,6 @@
+define('Controls/Input/Render/Styles',
+   ['Controls/_input/Render/Styles'],
+   function(Control) {
+      'use strict';
+      return Control;
+   });
