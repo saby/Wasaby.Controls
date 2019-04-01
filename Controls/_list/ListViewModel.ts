79c45422aebe78e93c9ff3a40fde2dd478de8520
Розов М.А <ma.rozov@tensor.ru>
2019-04-01 16:28:35 +0300

    Revert "Merge branch '19.220/bugfix/aas/grid_columns_render_optimize' into 'rc-19.220'"
    This reverts merge request !69812

--- Controls/_list/ListViewModel.ts
+++ Controls/_list/ListViewModel.ts
@@ -169,12 +169,8 @@ var ListViewModel = ItemsViewModel.extend([entityLib.VersionableMixin], {
     },
 
     _calcItemVersion: function(item, key) {
-        var version;
-        if (this._editingItemData && this._editingItemData.key === key) {
-           version = 'EDITING_';
-        } else {
-           version = ListViewModel.superclass._calcItemVersion.apply(this, arguments);
-        }
+        var
+            version = ListViewModel.superclass._calcItemVersion.apply(this, arguments);
 
         if (this._dragEntity && this._dragEntity.getItems().indexOf(key) !== -1) {
             version = 'DRAG_ITEM_' + version;
@@ -434,7 +430,7 @@ var ListViewModel = ItemsViewModel.extend([entityLib.VersionableMixin], {
 
     updateSelection: function(selectedKeys) {
         this._selectedKeys = selectedKeys || [];
-        this._nextModelVersion(true);
+        this._nextModelVersion();
     },
 
     getActiveItem: function() {
@@ -448,7 +444,7 @@ var ListViewModel = ItemsViewModel.extend([entityLib.VersionableMixin], {
 
     setMultiSelectVisibility: function(multiSelectVisibility) {
         this._options.multiSelectVisibility = multiSelectVisibility;
-        this._nextModelVersion(true);
+        this._nextModelVersion();
     },
 
     getMultiSelectVisibility: function() {
