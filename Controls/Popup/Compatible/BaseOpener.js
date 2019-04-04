2cb413e05d2b84b631220e73054397ba43a5c257
ds.mihaylovskiy <ds.mihaylovskiy@tensor.ru>
2019-04-05 00:58:56 +0300

    https://online.sbis.ru/opendoc.html?guid=8ef36eba-9a74-480e-aea6-ff38e0a148b9

--- Controls/Popup/Compatible/BaseOpener.js
+++ Controls/Popup/Compatible/BaseOpener.js
@@ -416,20 +416,12 @@ function(cMerge,
          if (cfg.targetPoint) {
             cfg.corner = cfg.targetPoint;
          }
-         if (cfg.direction) {
-            if (cfg.horizontalAlign) {
-               cfg.horizontalAlign.side = cfg.direction.horizontal;
-            } else {
-               cfg.horizontalAlign = { side: cfg.direction.horizontal };
-            }
-            if (cfg.verticalAlign) {
-               cfg.verticalAlign.side = cfg.direction.vertical;
-            } else {
-               cfg.verticalAlign = { side: cfg.direction.vertical };
-            }
+         if (cfg.direction && typeof cfg.direction === 'object') {
+            cfg.horizontalAlign = { side: cfg.direction.horizontal };
+            cfg.verticalAlign = { side: cfg.direction.vertical };
             cfg.direction = null;
          }
-         if (cfg.offset) {
+         if (cfg.offset && typeof cfg.offset === 'object') {
             if (cfg.horizontalAlign) {
                cfg.horizontalAlign.offset = cfg.offset.horizontal;
             } else {
