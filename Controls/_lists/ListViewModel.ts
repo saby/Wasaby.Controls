53b4c27416cc40efd6252a5ade0c45a966e78305
as.avramenko <as.avramenko@tensor.ru>
2019-04-05 13:05:07 +0300

    https://online.sbis.ru/opendoc.html?guid=ee91b271-c58c-43ca-8846-f871dc805e4a

--- Controls/_lists/ListViewModel.ts
+++ Controls/_lists/ListViewModel.ts
@@ -169,12 +169,7 @@ var ListViewModel = ItemsViewModel.extend([entityLib.VersionableMixin], {
     },
 
     _calcItemVersion: function(item, key) {
-        var version;
-        if (this._editingItemData && this._editingItemData.key === key) {
-            version = 'EDITING_';
-        } else {
-            version = ListViewModel.superclass._calcItemVersion.apply(this, arguments);
-        }
+        var version = ListViewModel.superclass._calcItemVersion.apply(this, arguments);
 
         if (this._dragEntity && this._dragEntity.getItems().indexOf(key) !== -1) {
             version = 'DRAG_ITEM_' + version;
