a5e0130e39d0160be001e12919a486cdda779379
am.gerasimov <am.gerasimov@tensor.ru>
2019-04-09 11:46:46 +0300

    https://online.sbis.ru/opendoc.html?guid=e52fe60c-af05-486a-9948-f9fc0d8f6235

--- Controls/Container/Data.js
+++ Controls/Container/Data.js
@@ -135,16 +135,11 @@ define('Controls/Container/Data',
             var self = this;
             _private.resolveOptions(this, options);
             if (receivedState) {
-               // need to create prefetchSource with source from options, because event subscriptions is not work on server
+               // need to create data context with source from options, because event subscriptions is not work on server
                // and source from receivedState will lose all subscriptions
                _private.createDataContextBySourceResult(this, {
                   data: receivedState,
-                  source: new source.PrefetchProxy({
-                     data: {
-                        query: receivedState
-                     },
-                     target: self._source
-                  })
+                  source: options.source
                });
             } else if (self._source) {
                return _private.createPrefetchSource(this).addCallback(function(result) {
