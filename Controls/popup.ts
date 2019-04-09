4c8d8abb193a6162d58658d25e2855e33b7e9d1b
ma.baranov1 <ma.baranov1@tensor.ru>
2019-04-09 19:29:48 +0300

    https://online.sbis.ru/opendoc.html?guid=aa5350f7-cbaf-49a0-9738-852830323d6b

---
+++ Controls/popup.ts
@@ -0,0 +1,49 @@
+/**
+ * Popup library
+ * @library Controls/popup
+ * @includes Confirmation Controls/_popup/Opener/Confirmation
+ * @includes Dialog Controls/_popup/Opener/Dialog
+ * @includes Stack Controls/_popup/Opener/Stack
+ * @includes Edit Controls/_popup/Opener/Edit
+ * @includes Infobox Controls/_popup/Opener/Infobox
+ * @includes Notification Controls/_popup/Opener/Notification
+ * @includes Previewer Controls/_popup/Opener/Previewer
+ * @includes Sticky Controls/_popup/Opener/Sticky
+ * @includes InfoboxTarget Controls/_popup/InfoBox
+ * @includes PreviewerTarget Controls/_popup/Previewer
+ * @includes Manager Controls/_popup/Manager
+ * @includes Controller Controls/_popup/Manager/ManagerController
+ * @includes Container Controls/_popup/Manager/Container
+ * @public
+ * @author Kraynov D.
+ */
+
+import Confirmation = require('Controls/_popup/Opener/Confirmation');
+import Dialog = require('Controls/_popup/Opener/Dialog');
+import Stack = require('Controls/_popup/Opener/Stack');
+import Edit = require('Controls/_popup/Opener/Edit');
+import Infobox = require('Controls/_popup/Opener/Infobox');
+import Notification = require('Controls/_popup/Opener/Notification');
+import Previewer = require('Controls/_popup/Opener/Previewer');
+import Sticky = require('Controls/_popup/Opener/Sticky');
+import InfoboxTarget = require('Controls/_popup/InfoBox');
+import PreviewerTarget = require('Controls/_popup/Previewer');
+import Manager = require('Controls/_popup/Manager');
+import Controller = require('Controls/_popup/Manager/ManagerController');
+import Container = require('Controls/_popup/Manager/Container');
+
+export {
+    Confirmation,
+    Dialog,
+    Stack,
+    Edit,
+    Infobox,
+    Notification,
+    Previewer,
+    Sticky,
+    InfoboxTarget,
+    PreviewerTarget,
+    Manager,
+    Controller,
+    Container
+}
