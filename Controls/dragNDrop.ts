b8c4d8eac00324d5820c821eb9c39a5b5837cece
lv.karachevcev <lv.karachevcev@tensor.ru>
2019-04-11 10:36:32 +0300

    https://online.sbis.ru/opendoc.html?guid=3a9d5325-4b14-4448-a259-f3be4448599d

---
+++ Controls/dragNDrop.ts
@@ -0,0 +1,25 @@
+/**
+ * DragNDrop library
+ * @library Controls/_dragNDrop
+ * @includes Container Controls/_dragNDrop/Container
+ * @includes Controller Controls/_dragNDrop/Controller
+ * @includes DraggingTemplate Controls/_dragNDrop/DraggingTemplate
+ * @includes Entity Controls/_dragNDrop/Entity
+ * @includes Items Controls/_dragNDrop/Entity/Items
+ * @public
+ * @author Авраменко А. С.
+ */
+
+import Container = require('Controls/_dragNDrop/Container');
+import Controller = require('Controls/_dragNDrop/Controller');
+import DraggingTemplate = require('Controls/_dragNDrop/DraggingTemplate');
+import Entity = require('Controls/_dragNDrop/Entity');
+import Items = require('Controls/_dragNDrop/Entity/Items');
+
+export {
+    Container,
+    Controller,
+    DraggingTemplate,
+    Entity,
+    Items
+}
