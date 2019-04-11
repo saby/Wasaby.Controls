9fd04fc6c28ee1d89684a2912bc46910fb3b614f
lv.karachevcev <lv.karachevcev@tensor.ru>
2019-04-11 09:44:55 +0300

    https://online.sbis.ru/opendoc.html?guid=f4c028bd-f276-4f68-bc96-ecc17611f51f

--- Controls/_decorator/Money.ts
+++ Controls/_decorator/Money.ts
@@ -4,124 +4,154 @@ import entity = require('Types/entity');
 import splitIntoTriads = require('Controls/Utils/splitIntoTriads');
 import template = require('wml!Controls/_decorator/Money/Money');
 import 'css!theme?Controls/_decorator/Money/Money';
-      
 
-      /**
-       * Converts a number to money.
-       *
-       * @class Controls/_decorator/Money
-       * @extends Core/Control
-       * @control
-       * @public
-       * @category Decorator
-       *
-       * @author Журавлев М.С.
-       */
 
-      /**
-       * @name Controls/_decorator/Money#number
-       * @cfg {Number} Number to convert.
-       */
+'use strict';
 
-      /**
-       * @name Controls/_decorator/Money#delimiters
-       * @cfg {Boolean} Determines whether the number should be split into triads.
-       * @default false
-       * @remark
-       * true - the number split into triads.
-       * false - does not do anything.
-       * @deprecated Use option {@link Controls/input:Number#useGrouping}
-       */
+/**
+ * Converts a number to money.
+ *
+ * @class Controls/Decorator/Money
+ * @extends Core/Control
+ *
+ * @mixes Controls/Decorator/Money/Styles
+ *
+ * @public
+ * @demo Controls-demo/Decorators/Money/Money
+ *
+ * @author Журавлев М.С.
+ */
 
-      /**
-       * @name Controls/_decorator/Money#useGrouping
-       * @cfg {Boolean} Determines whether to use grouping separators, such as thousands separators.
-       * @default true
-       * @remark
-       * true - the number is separated into grouping.
-       * false - does not do anything.
-       */
+/**
+ * @name Controls/Decorator/Money#number
+ * @cfg {Number} Number to convert.
+ * @deprecated Use option {@link value}
+ */
 
-      /**
-       * @name Controls/_decorator/Money#style
-       * @cfg {String} The type with which you want to display money.
-       * @variant accentResults
-       * @variant noAccentResults
-       * @variant group
-       * @variant basicRegistry
-       * @variant noBasicRegistry
-       * @variant accentRegistry
-       * @variant noAccentRegistry
-       * @variant error
-       */
+/**
+ * @name Controls/Decorator/Money#value
+ * @cfg {String} Value in number format to convert.
+ */
 
-      var _private = {
-         searchPaths: /(-?[0-9]*?)(\.[0-9]{2})/,
+/**
+ * @name Controls/Decorator/Money#delimiters
+ * @cfg {Boolean} Determines whether the number should be split into triads.
+ * @default false
+ * @remark
+ * true - the number split into triads.
+ * false - does not do anything.
+ * @deprecated Use option {@link Controls/input:Number#useGrouping}
+ */
 
-         parseNumber: function(number, delimiters) {
-            var exec = this.searchPaths.exec(number.toFixed(2));
+/**
+ * @name Controls/Decorator/Money#useGrouping
+ * @cfg {Boolean} Determines whether to use grouping separators, such as thousands separators.
+ * @default true
+ * @remark
+ * true - the number is separated into grouping.
+ * false - does not do anything.
+ */
 
-            if (!exec) {
-               Env.IoC.resolve('ILogger').error('Controls/_decorator/Money', 'That is not a valid option number: ' + number + '.');
-               exec = ['0.00', '0', '.00'];
-            }
+/**
+ * @name Controls/Decorator/Money#style
+ * @cfg {String} The type with which you want to display money.
+ * @variant accentResults
+ * @variant noAccentResults
+ * @variant group
+ * @variant basicRegistry
+ * @variant noBasicRegistry
+ * @variant accentRegistry
+ * @variant noAccentRegistry
+ * @variant error
+ * @variant default
+ * @default default
+ */
 
-            var integer = delimiters ? splitIntoTriads(exec[1]) : exec[1];
-            var fraction = exec[2];
+var _private = {
+   searchPaths: /(-?[0-9]*?)(\.[0-9]{2})/,
 
-            return {
-               integer: integer,
-               fraction: fraction,
-               number: integer + fraction
-            };
-         },
+   parseNumber: function (value, delimiters) {
+      var exec = this.searchPaths.exec(parseFloat(value).toFixed(2));
 
-         isUseGrouping: function(options, useLogging) {
-            if ('delimiters' in options) {
-               if (useLogging) {
-                  Env.IoC.resolve('ILogger').warn('Controls/_decorator/Money', 'Опция delimiters устарела, используйте useGrouping.');
-               }
+      if (!exec) {
+         Env.IoC.resolve('ILogger').error('Controls/Decorator/Money', 'That is not a valid option value: ' + value + '.');
+         exec = ['0.00', '0', '.00'];
+      }
 
-               return options.delimiters;
-            }
+      var integer = delimiters ? splitIntoTriads(exec[1]) : exec[1];
+      var fraction = exec[2];
 
-            return options.useGrouping;
+      return {
+         integer: integer,
+         fraction: fraction,
+         number: integer + fraction
+      };
+   },
+
+   isUseGrouping: function (options, useLogging) {
+      if ('delimiters' in options) {
+         if (useLogging) {
+            Env.IoC.resolve('ILogger').warn('Controls/Decorator/Money', 'Опция delimiters устарела, используйте useGrouping.');
          }
-      };
 
-      var Money = Control.extend({
-         _template: template,
+         return options.delimiters;
+      }
 
-         _parsedNumber: null,
+      return options.useGrouping;
+   },
 
-         _beforeMount: function(options) {
-            this._parsedNumber = _private.parseNumber(options.number, _private.isUseGrouping(options, true));
-         },
-
-         _beforeUpdate: function(newOptions) {
-            var newUseGrouping = _private.isUseGrouping(newOptions, false);
-            var oldUseGrouping = _private.isUseGrouping(this._options, false);
-
-            if (newOptions.number !== this._options.number || newUseGrouping !== oldUseGrouping) {
-               this._parsedNumber = _private.parseNumber(newOptions.number, newUseGrouping);
-            }
+   getValue: function (options, useLogging) {
+      if ('number' in options) {
+         if (useLogging) {
+            Env.IoC.resolve('ILogger').warn('Controls/Decorator/Money', 'Опция number устарела, используйте value.');
          }
-      });
 
-      Money.getDefaultOptions = function() {
-         return {
-            useGrouping: true
-         };
-      };
+         return options.number.toString();
+      }
 
-      Money.getOptionTypes = function() {
-         return {
-            useGrouping: entity.descriptor(Boolean),
-            number: entity.descriptor(Number).required()
-         };
-      };
+      return options.value;
+   }
+};
 
-      Money._private = _private;
+var Money = Control.extend({
+   _template: template,
 
-      export = Money;
-   
+   _parsedNumber: null,
+
+   _beforeMount: function (options) {
+      this._parsedNumber = _private.parseNumber(
+         _private.getValue(options, true),
+         _private.isUseGrouping(options, true)
+      );
+   },
+
+   _beforeUpdate: function (newOptions) {
+      var newUseGrouping = _private.isUseGrouping(newOptions, false);
+      var oldUseGrouping = _private.isUseGrouping(this._options, false);
+      var newValue = _private.getValue(newOptions, false);
+      var oldValue = _private.getValue(this._options, false);
+
+      if (newValue !== oldValue || newUseGrouping !== oldUseGrouping) {
+         this._parsedNumber = _private.parseNumber(newValue, newUseGrouping);
+      }
+   }
+});
+
+Money.getDefaultOptions = function () {
+   return {
+      style: 'default',
+      useGrouping: true
+   };
+};
+
+Money.getOptionTypes = function () {
+   return {
+      style: entity.descriptor(String),
+      useGrouping: entity.descriptor(Boolean),
+      value: entity.descriptor(String)
+   };
+};
+
+Money._private = _private;
+
+export = Money;
