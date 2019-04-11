af736f77c567647018a2425be70be762b182afea
lv.karachevcev <lv.karachevcev@tensor.ru>
2019-04-11 15:42:34 +0300

    https://online.sbis.ru/opendoc.html?guid=7184401c-4284-40ef-a619-198e81a6dbe3

---
+++ Controls/dropdown.ts
@@ -0,0 +1,28 @@
+/**
+ * dropdown library
+ * @library Controls/dropdown
+ * @includes Button Controls/Button/Menu
+ * @includes Input Controls/Input/DropDown
+ * @includes _Controller Controls/DropDown/Controller
+ * @includes Opener Controls/DropDown/Opener
+ * @includes Combobox Controls/Input/Combobox
+ * @includes ItemTemplate Controls/Dropdown/resources/template/itemTemplate
+ * @public
+ * @author Kraynov D.
+ */
+
+import Highlight = require('Controls/_decorator/Highlight');
+import Markup = require('Controls/_decorator/Markup');
+import Money = require('Controls/_decorator/Money');
+import Number = require('Controls/_decorator/Number');
+import PhoneNumber = require('Controls/_decorator/PhoneNumber');
+import WrapURLs = require('Controls/_decorator/WrapURLs');
+
+export {
+    Highlight,
+    Markup,
+    Money,
+    Number,
+    PhoneNumber,
+    WrapURLs
+}
