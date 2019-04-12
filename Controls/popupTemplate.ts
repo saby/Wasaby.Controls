ef0d6f77b3e54b9aefbc0649a93936af0a97a6e8
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-04-12 23:17:06 +0300

    https://online.sbis.ru/opendoc.html?guid=24c4ff19-3c30-44f0-a994-52801d047ea5

--- Controls/popupTemplate.ts
+++ Controls/popupTemplate.ts
@@ -2,16 +2,27 @@
  * popupTemplate library
  * @library Controls/popupTemplate
  * @includes CloseButton Controls/_popupTemplate/CloseButton
- * @includes Dialog Controls/_popup/Opener/Dialog
- * @includes Stack Controls/_popup/Opener/Stack
- * @includes Edit Controls/_popup/Opener/Edit
- * @includes Infobox Controls/_popup/Opener/Infobox
+ * @includes Stack Controls/_popupTemplate/Stack
+ * @includes Dialog Controls/_popupTemplate/Dialog
+ * @includes Confirmation Controls/_popupTemplate/Confirmation
+ * @includes Notification Controls/_popupTemplate/Notification/Base
+ * @includes NotificationSimple Controls/_popupTemplate/Notification/Simple
  * @public
  * @author Kraynov D.
  */
 
 import CloseButton = require('Controls/_popupTemplate/CloseButton');
+import Stack = require('Controls/_popupTemplate/Stack');
+import Dialog = require('Controls/_popupTemplate/Dialog');
+import Confirmation = require('Controls/_popupTemplate/Confirmation');
+import Notification = require('Controls/_popupTemplate/Notification/Base');
+import NotificationSimple = require('Controls/_popupTemplate/Notification/Simple');
 
 export {
-   CloseButton
+   CloseButton,
+   Stack,
+   Dialog,
+   Confirmation,
+   Notification,
+   NotificationSimple
 }
