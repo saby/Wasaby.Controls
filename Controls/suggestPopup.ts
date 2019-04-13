57b91a65cb783ad41aee04567a005065587b4e1f
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-04-14 00:50:58 +0300

    https://online.sbis.ru/opendoc.html?guid=24c4ff19-3c30-44f0-a994-52801d047ea5

---
+++ Controls/suggestPopup.ts
@@ -0,0 +1,16 @@
+/**
+ * suggestPopup library
+ * @library Controls/suggestPopup
+ * @includes ListContainer Controls/_suggestPopup/List
+ * @includes FooterTemplate wml!Controls/_suggestPopup/footer
+ * @public
+ * @author Kraynov D.
+ */
+
+import ListContainer = require('Controls/_suggestPopup/List');
+import FooterTemplate = require('wml!Controls/_suggestPopup/footer');
+
+export {
+   ListContainer,
+   FooterTemplate
+};
