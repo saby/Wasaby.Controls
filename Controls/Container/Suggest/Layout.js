7523b56080c7d760781f879178550367cde3f8f4
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-04-13 23:44:51 +0300

    https://online.sbis.ru/opendoc.html?guid=24c4ff19-3c30-44f0-a994-52801d047ea5

---
+++ Controls/Container/Suggest/Layout.js
@@ -0,0 +1,6 @@
+define('Controls/Container/Suggest/Layout',
+   ['Controls/_suggest/_InputController'],
+   function(Control) {
+      'use strict';
+      return Control;
+   });
---
+++ Controls/Container/Suggest/Layout/Styles.js
@@ -0,0 +1,6 @@
+define('Controls/Container/Suggest/Layout/Styles',
+   ['Controls/_suggest/_InputController/Styles'],
+   function(Control) {
+      'use strict';
+      return Control;
+   });
