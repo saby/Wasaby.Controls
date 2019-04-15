724871ffb514b5f4bcf4123c40b8832bc33c286f
leonid <leonid.karachevtsev@gmail.com>
2019-04-15 17:18:30 +0300

    https://online.sbis.ru/opendoc.html?guid=805842fa-5d91-43e2-ad3b-75158b61025f

---
+++ Controls/EditableArea/Template/Editors/Base.js
@@ -0,0 +1,6 @@
+define('Controls/EditableArea/Templates/Editors/Base',
+   ['Controls/_EditableArea/Templates/Editors/Base'],
+   function(Control) {
+      'use strict';
+      return Control;
+   });
---
+++ Controls/EditableArea/Template/Editors/DateTime.js
@@ -0,0 +1,6 @@
+define('Controls/EditableArea/Templates/Editors/DateTime',
+   ['Controls/_EditableArea/Templates/Editors/DateTime'],
+   function(Control) {
+      'use strict';
+      return Control;
+   });
