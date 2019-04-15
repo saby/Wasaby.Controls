9fcd6193486132246217053904b760f78762596a
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-04-15 14:33:06 +0300

    https://online.sbis.ru/opendoc.html?guid=24c4ff19-3c30-44f0-a994-52801d047ea5

--- Controls/_suggest/_InputController.ts
+++ Controls/_suggest/_InputController.ts
@@ -378,6 +378,7 @@ var SuggestLayout = Control.extend({
          });
       } else {
          _private.setSearchValue(self, shouldSearch ? value : '');
+         _private.setFilter(self, self._options.filter);
          _private.updateSuggestState(this);
       }
    },
