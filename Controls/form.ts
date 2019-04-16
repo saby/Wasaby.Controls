53e3ae2414a4d9da5bee66e59cec84f4418def31
lv.karachevcev <lv.karachevcev@tensor.ru>
2019-04-16 10:23:03 +0300

    https://online.sbis.ru/opendoc.html?guid=d28a6ef6-0687-4c2f-8c60-b25aec1bdf5c

---
+++ Controls/form.ts
@@ -0,0 +1,16 @@
+/**
+ * form library
+ * @library Controls/form
+ * @includes Controller Controls/_form/FormController
+ * @includes Crud Controls/_form/Crud
+ * @public
+ * @author Kraynov D.
+ */
+
+import Controller = require('Controls/_form/FormController');
+import Crud = require('Controls/_form/Crud');
+
+export {
+    Controller,
+    Crud
+}
