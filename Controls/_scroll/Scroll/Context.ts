0bc34a63e654afa2a2ea0d196ee96d13a26c46d2
ganshinyao <yo.ganshin@tensor.ru>
2019-04-17 13:42:16 +0300

    https://online.sbis.ru/opendoc.html?guid=86a1066f-7e3e-4db0-b176-e7241e0af1a8

--- Controls/_scroll/Scroll/Context.ts
+++ Controls/_scroll/Scroll/Context.ts
@@ -1,7 +1,8 @@
 import DataContext = require('Core/DataContext');
       export = DataContext.extend({
+         _moduleName: 'Controls/_scroll/Scroll/Context',
          constructor: function(cfg) {
             this.pagingVisible = cfg.pagingVisible;
          }
       });
-   
+
