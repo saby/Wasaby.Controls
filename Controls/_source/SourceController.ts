9c7aa02449cd398939bc8e8344ed20bce3abb780
ganshinyao <yo.ganshin@tensor.ru>
2019-04-17 14:43:23 +0300

    конвертиция в ts https://online.sbis.ru/opendoc.html?guid=86a1066f-7e3e-4db0-b176-e7241e0af1a8

--- Controls/_source/SourceController.ts
+++ Controls/_source/SourceController.ts
@@ -1,229 +1,225 @@
-define('Controls/Controllers/SourceController',
-   [
-      'Core/core-simpleExtend',
-      'Core/core-instance',
-      'Env/Env',
-      'Controls/Controllers/QueryParamsController/Page',
-      'Controls/Controllers/QueryParamsController/Offset',
-      'Controls/Controllers/QueryParamsController/Position',
-      'Types/source',
-      'Core/Deferred',
-      'Core/core-clone',
-      'require'
-   ],
-   function(cExtend, cInstance, Env, Page, Offset, Position, sourceLib, cDeferred, cClone) {
-      var _private = {
-         prepareSource: function(sourceOpt) {
-            if (!cInstance.instanceOfMixin(sourceOpt, 'Types/_source/ICrud')) {
-               Env.IoC.resolve('ILogger').error('SourceController', 'Source option has incorrect type');
-            }
-            return sourceOpt;
-         },
+import cExtend = require('Core/core-simpleExtend');
+import cInstance = require('Core/core-instance');
+import Env = require('Env/Env');
+import Page = require('Controls/Controllers/QueryParamsController/Page');
+import Offset = require('Controls/Controllers/QueryParamsController/Offset');
+import Position = require('Controls/Controllers/QueryParamsController/Position');
+import sourceLib = require('Types/source');
+import cDeferred = require('Core/Deferred');
+import cClone = require('Core/core-clone');
 
-         getQueryInstance: function(filter, sorting, offset, limit, meta) {
-            var query = new sourceLib.Query();
-            query.where(filter)
-               .offset(offset)
-               .limit(limit)
-               .orderBy(sorting)
-               .meta(meta);
-            return query;
-         },
+var _private = {
+   prepareSource: function(sourceOpt) {
+      if (!cInstance.instanceOfMixin(sourceOpt, 'Types/_source/ICrud')) {
+         Env.IoC.resolve('ILogger').error('SourceController', 'Source option has incorrect type');
+      }
+      return sourceOpt;
+   },
 
-         callQuery: function(dataSource, idProperty, filter, sorting, offset, limit, meta) {
-            var queryDef, queryIns;
+   getQueryInstance: function(filter, sorting, offset, limit, meta) {
+      var query = new sourceLib.Query();
+      query.where(filter)
+         .offset(offset)
+         .limit(limit)
+         .orderBy(sorting)
+         .meta(meta);
+      return query;
+   },
 
-            queryIns = _private.getQueryInstance(filter, sorting, offset, limit, meta);
+   callQuery: function(dataSource, idProperty, filter, sorting, offset, limit, meta) {
+      var queryDef, queryIns;
 
-            queryDef = dataSource.query(queryIns).addCallback((function(dataSet) {
-               if (idProperty && idProperty !== dataSet.idProperty) {
-                  dataSet.setIdProperty(idProperty);
-               }
-               return dataSet.getAll ? dataSet.getAll() : dataSet;
-            }));
+      queryIns = _private.getQueryInstance(filter, sorting, offset, limit, meta);
 
-            if (cInstance.instanceOfModule(dataSource, 'Types/source:Memory')) {
-
-               /*Проблема в том что деферред с синхронным кодом статического источника выполняется сихронно.
-                в итоге в коолбэк релоада мы приходим в тот момент, когда еще не отработал _beforeMount и заполнение опций, и не можем обратиться к this._options*/
-               var queryDefAsync = cDeferred.fromTimer(0);
-               queryDefAsync.addCallback(function() {
-                  return queryDef;
-               });
-               return queryDefAsync;
-            } else {
-               return queryDef;
-            }
-         },
-
-         createQueryParamsController: function(type, cfg) {
-            var cntCtr, cntInstance;
-
-            switch (type) {
-               case 'page':
-                  cntCtr = Page;
-                  break;
-               case 'offset':
-                  cntCtr = Offset;
-                  break;
-               case 'position':
-                  cntCtr = Position;
-                  break;
-               default:
-                  Env.IoC.resolve('ILogger').error('SourceController', 'Undefined navigation source type "' + type + '"');
-            }
-            if (cntCtr) {
-               cntInstance = new cntCtr(cfg);
-            }
-            return cntInstance;
-         },
-
-         modifyQueryParamsWithNavigation: function(cleanParams, direction, paramsController) {
-            var resultParams, navigParams, navFilter;
-
-            resultParams = cleanParams;
-            navigParams = paramsController.prepareQueryParams(direction);
-
-            resultParams.limit = navigParams.limit;
-            resultParams.offset = navigParams.offset;
-
-            if (navigParams.filter) {
-               //we can't modify original filter
-               resultParams.filter = cClone(resultParams.filter);
-               navFilter = navigParams.filter;
-               for (var i in navFilter) {
-                  if (navFilter.hasOwnProperty(i)) {
-                     resultParams.filter[i] = navFilter[i];
-                  }
-               }
-            }
-
-            if (navigParams.meta) {
-               resultParams.meta = navigParams.meta;
-            }
-
-            return resultParams;
+      queryDef = dataSource.query(queryIns).addCallback((function(dataSet) {
+         if (idProperty && idProperty !== dataSet.idProperty) {
+            dataSet.setIdProperty(idProperty);
          }
+         return dataSet.getAll ? dataSet.getAll() : dataSet;
+      }));
+
+      if (cInstance.instanceOfModule(dataSource, 'Types/source:Memory')) {
+
+         /*Проблема в том что деферред с синхронным кодом статического источника выполняется сихронно.
+          в итоге в коолбэк релоада мы приходим в тот момент, когда еще не отработал _beforeMount и заполнение опций, и не можем обратиться к this._options*/
+         var queryDefAsync = cDeferred.fromTimer(0);
+         queryDefAsync.addCallback(function() {
+            return queryDef;
+         });
+         return queryDefAsync;
+      } else {
+         return queryDef;
+      }
+   },
+
+   createQueryParamsController: function(type, cfg) {
+      var cntCtr, cntInstance;
+
+      switch (type) {
+         case 'page':
+            cntCtr = Page;
+            break;
+         case 'offset':
+            cntCtr = Offset;
+            break;
+         case 'position':
+            cntCtr = Position;
+            break;
+         default:
+            Env.IoC.resolve('ILogger').error('SourceController', 'Undefined navigation source type "' + type + '"');
+      }
+      if (cntCtr) {
+         cntInstance = new cntCtr(cfg);
+      }
+      return cntInstance;
+   },
+
+   modifyQueryParamsWithNavigation: function(cleanParams, direction, paramsController) {
+      var resultParams, navigParams, navFilter;
+
+      resultParams = cleanParams;
+      navigParams = paramsController.prepareQueryParams(direction);
+
+      resultParams.limit = navigParams.limit;
+      resultParams.offset = navigParams.offset;
+
+      if (navigParams.filter) {
+         //we can't modify original filter
+         resultParams.filter = cClone(resultParams.filter);
+         navFilter = navigParams.filter;
+         for (var i in navFilter) {
+            if (navFilter.hasOwnProperty(i)) {
+               resultParams.filter[i] = navFilter[i];
+            }
+         }
+      }
+
+      if (navigParams.meta) {
+         resultParams.meta = navigParams.meta;
+      }
+
+      return resultParams;
+   }
+};
+var SourceController = cExtend.extend({
+   _source: null,
+   _queryParamsController: null,
+   _loader: null,
+   constructor: function(cfg) {
+      this._options = cfg;
+      SourceController.superclass.constructor.apply(this, arguments);
+      this._source = _private.prepareSource(this._options.source);
+
+      if (this._options.navigation && this._options.navigation.source) {
+         this._queryParamsController = _private.createQueryParamsController(this._options.navigation.source, this._options.navigation.sourceConfig);
+         this._queryParamsController.prepareSource(this._source);
+      }
+   },
+
+   load: function(filter, sorting, direction) {
+      var def, queryParams, self, navFilter;
+
+      queryParams = {
+         filter: filter || {},
+         sorting: sorting,
+         limit: undefined,
+         offset: undefined
       };
-      var SourceController = cExtend.extend({
-         _source: null,
-         _queryParamsController: null,
-         _loader: null,
-         constructor: function(cfg) {
-            this._options = cfg;
-            SourceController.superclass.constructor.apply(this, arguments);
-            this._source = _private.prepareSource(this._options.source);
+      this.cancelLoading();
 
-            if (this._options.navigation && this._options.navigation.source) {
-               this._queryParamsController = _private.createQueryParamsController(this._options.navigation.source, this._options.navigation.sourceConfig);
-               this._queryParamsController.prepareSource(this._source);
+      if (this._queryParamsController) {
+         queryParams = _private.modifyQueryParamsWithNavigation(queryParams, direction, this._queryParamsController);
+      }
+
+      self = this;
+      def = _private.callQuery(this._source, this._options.idProperty,
+         queryParams.filter,
+         queryParams.sorting,
+         queryParams.offset,
+         queryParams.limit,
+         queryParams.meta)
+         .addCallback(function(list) {
+            if (self._queryParamsController) {
+               self._queryParamsController.calculateState(list, direction);
             }
-         },
+            return list;
+         }).addErrback(function(error) {
+            return error;
+         });
+      this._loader = def;
+      return def;
+   },
 
-         load: function(filter, sorting, direction) {
-            var def, queryParams, self, navFilter;
+   cancelLoading: function() {
+      if (this._loader && !this._loader.isReady()) {
+         this._loader.cancel();
+         this._loader = null;
+      }
+   },
 
-            queryParams = {
-               filter: filter || {},
-               sorting: sorting,
-               limit: undefined,
-               offset: undefined
-            };
-            this.cancelLoading();
+   getLoadedDataCount: function() {
+      if (this._queryParamsController) {
+         return this._queryParamsController.getLoadedDataCount();
+      }
+   },
 
-            if (this._queryParamsController) {
-               queryParams = _private.modifyQueryParamsWithNavigation(queryParams, direction, this._queryParamsController);
-            }
+   getAllDataCount: function() {
+      if (this._queryParamsController) {
+         return this._queryParamsController.getAllDataCount();
+      }
+   },
 
-            self = this;
-            def = _private.callQuery(this._source, this._options.idProperty,
-               queryParams.filter,
-               queryParams.sorting,
-               queryParams.offset,
-               queryParams.limit,
-               queryParams.meta)
-               .addCallback(function(list) {
-                  if (self._queryParamsController) {
-                     self._queryParamsController.calculateState(list, direction);
-                  }
-                  return list;
-               }).addErrback(function(error) {
-                  return error;
-               });
-            this._loader = def;
-            return def;
-         },
+   hasMoreData: function(direction) {
+      if (this._queryParamsController) {
+         return this._queryParamsController.hasMoreData(direction);
+      }
+   },
 
-         cancelLoading: function() {
-            if (this._loader && !this._loader.isReady()) {
-               this._loader.cancel();
-               this._loader = null;
-            }
-         },
+   calculateState: function(list) {
+      if (this._queryParamsController) {
+         this._queryParamsController.calculateState(list);
+      }
+   },
 
-         getLoadedDataCount: function() {
-            if (this._queryParamsController) {
-               return this._queryParamsController.getLoadedDataCount();
-            }
-         },
+   setState: function(state) {
+      if (this._queryParamsController) {
+         this._queryParamsController.setState(state);
+      }
+   },
 
-         getAllDataCount: function() {
-            if (this._queryParamsController) {
-               return this._queryParamsController.getAllDataCount();
-            }
-         },
+   isLoading: function() {
+      return this._loader && !this._loader.isReady();
+   },
 
-         hasMoreData: function(direction) {
-            if (this._queryParamsController) {
-               return this._queryParamsController.hasMoreData(direction);
-            }
-         },
+   setEdgeState: function(direction) {
+      if (this._queryParamsController) {
+         this._queryParamsController.setEdgeState(direction);
+      }
+   },
 
-         calculateState: function(list) {
-            if (this._queryParamsController) {
-               this._queryParamsController.calculateState(list);
-            }
-         },
+   create: function(meta) {
+      return this._source.create(meta);
+   },
 
-         setState: function(state) {
-            if (this._queryParamsController) {
-               this._queryParamsController.setState(state);
-            }
-         },
+   update: function(item) {
+      return this._source.update(item);
+   },
 
-         isLoading: function() {
-            return this._loader && !this._loader.isReady();
-         },
+   read: function(key, meta) {
+      return this._source.read(key, meta);
+   },
 
-         setEdgeState: function(direction) {
-            if (this._queryParamsController) {
-               this._queryParamsController.setEdgeState(direction);
-            }
-         },
+   destroy: function() {
+      if (this._queryParamsController) {
+         this._queryParamsController.destroy();
+      }
+      this.cancelLoading();
+      this._options = null;
+   }
+});
 
-         create: function(meta) {
-            return this._source.create(meta);
-         },
+//для тестов
+SourceController._private = _private;
 
-         update: function(item) {
-            return this._source.update(item);
-         },
+export = SourceController;
 
-         read: function(key, meta) {
-            return this._source.read(key, meta);
-         },
-
-         destroy: function() {
-            if (this._queryParamsController) {
-               this._queryParamsController.destroy();
-            }
-            this.cancelLoading();
-            this._options = null;
-         }
-      });
-
-      //для тестов
-      SourceController._private = _private;
-
-      return SourceController;
-   });
