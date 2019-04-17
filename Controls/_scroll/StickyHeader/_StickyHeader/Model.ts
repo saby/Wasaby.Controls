2813568a1c590c5500f6b181433b50b57c3fd2fe
ganshinyao <yo.ganshin@tensor.ru>
2019-04-17 11:00:44 +0300

    https://online.sbis.ru/opendoc.html?guid=86a1066f-7e3e-4db0-b176-e7241e0af1a8

--- Controls/_scroll/StickyHeader/_StickyHeader/Model.ts
+++ Controls/_scroll/StickyHeader/_StickyHeader/Model.ts
@@ -3,108 +3,108 @@ import simpleExtend = require('Core/core-simpleExtend');
 
 
 
-      /**
-       * @extends Core/core-simpleExtend
+/**
+ * @extends Core/core-simpleExtend
        * @class Controls/_scroll/Model
-       */
+ */
 
-      /**
-       * @typedef {Object} Intersection
-       * @property {Boolean} top Determines whether the upper boundary of content is crossed.
-       * @property {Boolean} bottom Determines whether the lower boundary of content is crossed.
-       */
+/**
+ * @typedef {Object} Intersection
+ * @property {Boolean} top Determines whether the upper boundary of content is crossed.
+ * @property {Boolean} bottom Determines whether the lower boundary of content is crossed.
+ */
 
-      /**
-       * typedef {String} TrackedTarget
-       * @variant top Top target.
-       * @variant bottom Bottom target.
-       */
+/**
+ * typedef {String} TrackedTarget
+ * @variant top Top target.
+ * @variant bottom Bottom target.
+ */
 
-      export = simpleExtend.extend({
+export = simpleExtend.extend({
 
-         /**
-          * @type {Intersection|null} Determines whether the boundaries of content crossed.
-          * @private
-          */
-         _intersection: null,
+   /**
+    * @type {Intersection|null} Determines whether the boundaries of content crossed.
+    * @private
+    */
+   _intersection: null,
 
-         /**
-          * type {String} Determines whether the content is fixed.
-          * @private
-          */
-         _fixedPosition: '',
+   /**
+    * type {String} Determines whether the content is fixed.
+    * @private
+    */
+   _fixedPosition: '',
 
-         get fixedPosition() {
-            return this._fixedPosition;
-         },
+   get fixedPosition() {
+      return this._fixedPosition;
+   },
 
-         /**
-          * @param {Object} config
-          * @param {Object} config.topTarget DOM element
-          * @param {Object} config.bottomTarget DOM element
-          * @param {String} config.position Sticky position
-          */
-         constructor: function(config) {
-            this._intersection = {};
-            this._topTarget = config.topTarget;
-            this._bottomTarget = config.bottomTarget;
-            this._position = config.position;
-            this._updateStateIntersection = this._updateStateIntersection.bind(this);
-         },
+   /**
+    * @param {Object} config
+    * @param {Object} config.topTarget DOM element
+    * @param {Object} config.bottomTarget DOM element
+    * @param {String} config.position Sticky position
+    */
+   constructor: function(config) {
+      this._intersection = {};
+      this._topTarget = config.topTarget;
+      this._bottomTarget = config.bottomTarget;
+      this._position = config.position;
+      this._updateStateIntersection = this._updateStateIntersection.bind(this);
+   },
 
-         update: function(entries) {
-            entries.forEach(this._updateStateIntersection);
+   update: function(entries) {
+      entries.forEach(this._updateStateIntersection);
 
-            this._fixedPosition = this._getFixedPosition();
-         },
+      this._fixedPosition = this._getFixedPosition();
+   },
 
-         destroy: function() {
-            this._updateStateIntersection = undefined;
-         },
+   destroy: function() {
+      this._updateStateIntersection = undefined;
+   },
 
-         /**
-          * @param {IntersectionObserverEntry} entry
-          * @private
-          */
-         _updateStateIntersection: function(entry) {
-            var target = this._getTarget(entry);
+   /**
+    * @param {IntersectionObserverEntry} entry
+    * @private
+    */
+   _updateStateIntersection: function(entry) {
+      var target = this._getTarget(entry);
 
-            this._intersection[target] = entry.isIntersecting;
-         },
+      this._intersection[target] = entry.isIntersecting;
+   },
 
-         /**
-          * Get the name of the intersection target.
-          * @param {IntersectionObserverEntry} entry The intersection between the target element and its root container at a specific moment of transition.
-          * @returns {TrackedTarget} The name of the intersection target.
-          * @private
-          */
-         _getTarget: function(entry) {
-            switch (entry.target) {
-               case this._topTarget:
-                  return 'top';
-               case this._bottomTarget:
-                  return 'bottom';
-               default:
+   /**
+    * Get the name of the intersection target.
+    * @param {IntersectionObserverEntry} entry The intersection between the target element and its root container at a specific moment of transition.
+    * @returns {TrackedTarget} The name of the intersection target.
+    * @private
+    */
+   _getTarget: function(entry) {
+      switch (entry.target) {
+         case this._topTarget:
+            return 'top';
+         case this._bottomTarget:
+            return 'bottom';
+         default:
                   Env.IoC.resolve('ILogger').error('Controls/_scroll/StickyHeader/Model', 'Unexpected target');
-                  return 'bottom';
-            }
-         },
+            return 'bottom';
+      }
+   },
 
-         /**
-          * Checks the content is fixed.
-          * @returns {String} Determines whether the content is fixed.
-          * @private
-          */
-         _getFixedPosition: function() {
-            var result = '';
+   /**
+    * Checks the content is fixed.
+    * @returns {String} Determines whether the content is fixed.
+    * @private
+    */
+   _getFixedPosition: function() {
+      var result = '';
 
-            if (this._position.indexOf('top') !== -1 && !this._intersection.top && this._intersection.bottom) {
-               result = 'top';
-            } else if (this._position.indexOf('bottom') !== -1 && !this._intersection.bottom) {
-               result = 'bottom';
-            }
+      if (this._position.indexOf('top') !== -1 && !this._intersection.top && this._intersection.bottom) {
+         result = 'top';
+      } else if (this._position.indexOf('bottom') !== -1 && !this._intersection.bottom && this._intersection.top) {
+         result = 'bottom';
+      }
 
-            return result;
-         }
-      });
+      return result;
+   }
+});
 
