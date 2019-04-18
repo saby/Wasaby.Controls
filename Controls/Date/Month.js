cb182af2454d418dfa82632d231f0729458c8eb4
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-04-18 13:28:47 +0300

    https://online.sbis.ru/opendoc.html?guid=090b60be-f3c3-451e-8730-36ffee76b6b0

--- Controls/Date/Month.js
+++ Controls/Date/Month.js
@@ -8,22 +8,6 @@ define('Controls/Date/Month', [
 
    'use strict';
 
-   /**
-    * Календарь отображающий 1 месяц.
-    * Предназначен для задания даты или диапазона дат в рамках одного месяца путём выбора периода с помощью мыши.
-    * 
-    * @class Controls/Date/Month
-    * @extends Core/Control
-    * @mixes Controls/_calendar/interfaces/IMonth
-    * @mixes Controls/Date/interface/IRangeSelectable
-    * @mixes Controls/Date/interface/IDateRangeSelectable
-    * @control
-    * @public
-    * @author Миронов А.Ю.
-    * @demo Controls-demo/Date/Month
-    *
-    */
-
    Env.IoC.resolve('ILogger').error(
       'Controls/Date/Month' +
       'This control is deprecated. Use \'Controls/calendar:Month\' instead'
