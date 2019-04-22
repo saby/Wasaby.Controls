dcca508713146b805421bc2b91bca4c1b7ad57c2
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-04-22 14:27:37 +0300

    https://online.sbis.ru/opendoc.html?guid=2f6ae3a4-a3c6-431e-a43e-b9e5ab2f8211

--- Controls/_tile/View.ts
+++ Controls/_tile/View.ts
@@ -9,7 +9,7 @@ import {TreeControl} from 'Controls/treeGrid';
  * List in which items are displayed as tiles. Can load data from data source.
  * <a href="/materials/demo-ws4-tile">Demo examples</a>.
  *
- * @class Controls/_tile/Tile
+ * @class Controls/_tile/View
  * @extends Controls/list:View
  * @mixes Controls/interface/ISource
  * @mixes Controls/interface/IItemTemplate
@@ -26,7 +26,7 @@ import {TreeControl} from 'Controls/treeGrid';
  *
  * @mixes Controls/List/BaseControlStyles
  * @mixes Controls/List/ListStyles
- * @mixes Controls/List/Tile/Styles
+ * @mixes Controls/List/View/Styles
  * @mixes Controls/List/ItemActions/ItemActionsStyles
  * @mixes Controls/_tile/Swipe/SwipeStyles
  *
@@ -40,7 +40,7 @@ import {TreeControl} from 'Controls/treeGrid';
  * @category List
  */
 
-var Tile = View.extend({
+var View = View.extend({
    _viewName: TreeTileView,
    _viewTemplate: TreeControl,
    _getModelConstructor: function() {
@@ -48,11 +48,11 @@ var Tile = View.extend({
    }
 });
 
-Tile.getDefaultOptions = function() {
+View.getDefaultOptions = function() {
    return {
       actionAlignment: 'vertical',
       actionCaptionPosition: 'none'
    };
 };
 
-export = Tile;
+export = View;
