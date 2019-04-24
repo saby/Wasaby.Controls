a6f6ad934e25ecc3e2c9b55a2aa52acf4fa6af32
ps.kolesova <ps.kolesova@tensor.ru>
2019-04-24 15:55:40 +0300

    https://online.sbis.ru/opendoc.html?guid=43a62b05-3844-438b-8c6a-dccf354e94a2

--- Controls/_validate/Controller.ts
+++ Controls/_validate/Controller.ts
@@ -24,7 +24,8 @@ import 'css!theme?Controls/_validate/ErrorMessage';
                   template: errorMessage,
                   templateOptions: { content: self._validationResult },
                   eventHandlers: {
-                     onResult: self._mouseInfoboxHandler.bind(self)
+                     onResult: self._mouseInfoboxHandler.bind(self),
+                     onClose: self._closeHandler.bind(self)
                   }
                };
 
@@ -214,6 +215,9 @@ import 'css!theme?Controls/_validate/ErrorMessage';
                _private.openInfoBox(this);
             }
          },
+         _closeHandler:function(){
+            this._isOpened = false;
+         },
          _focusInHandler: function() {
             if (!this._isOpened) {
                _private.openInfoBox(this);
@@ -222,7 +226,7 @@ import 'css!theme?Controls/_validate/ErrorMessage';
          _mouseInfoboxHandler: function(event) {
             if (event.type === 'mouseenter') {
                this._hoverInfoboxHandler(this);
-            } else {
+            } else if (event.type === 'mouseleave'){
                this._mouseLeaveHandler(this);
             }
          },
