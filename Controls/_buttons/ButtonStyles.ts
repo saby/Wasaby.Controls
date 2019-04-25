d41b42a5ebf15243dac28b14e12757263fb2f5b4
lv.karachevcev <lv.karachevcev@tensor.ru>
2019-04-25 12:58:42 +0300

    https://online.sbis.ru/opendoc.html?guid=7be0d27c-4e59-4bb9-9a02-6d4adec86a59

--- Controls/_buttons/ButtonStyles.ts
+++ Controls/_buttons/ButtonStyles.ts
@@ -1,117 +1,110 @@
-define('Controls/Button/ButtonStyles',
-   [
-   ],
-   function() {
-   }
-
-   /**
-    * @author Михайловский Д.С.
-    * @mixin Controls/Button/ButtonStyles
-    * @public
-    *
-    * @css @height_Button_size_s Height of button with options viewMode 'button' and size 's'.
-    * @css @font-size_Button_size_s Font size of button with options viewMode 'button' and size 's'.
-    * @css @border-radius_Button_size_s Border radius of button with options viewMode 'button' and size 's'.
-    * @css @padding_Button-wrapper_size_s Padding from content to border of button with options viewMode 'button' and size 's'.
-    *
-    * @css @height_Button_size_m Height of button with options viewMode 'button' and size 'm'.
-    * @css @font-size_Button_size_m Font size of button with options viewMode 'button' and size 'm''.
-    * @css @border-radius_Button_size_m Border radius of button with options viewMode 'button' and size 'm'.
-    * @css @padding_Button-wrapper_size_m Padding from content to border of button with options viewMode 'button' and size 'm'.
-    *
-    * @css @height_Button_size_l Height of button with options viewMode 'button' and size 'l'.
-    * @css @font-size_Button_size_l Font size of button with options viewMode 'button' and size 'l'.
-    * @css @border-radius_Button_size_l Border radius of button with options viewMode 'button' and size 'l'.
-    * @css @padding_Button__wrapper_size_l Padding from content to border of button with options viewMode 'button' and size 'l'.
-    *
-    * @css @thickness_Button__border Border thickness of button with option viewMode 'button'.
-    * @css @min-width_Button_common Minimal width of button with option viewMode 'button'.
-    * @css @box-shadow_Button_common Box shadow of button with option viewMode 'button'.
-    * @css @box-shadow_Button_active Box shadow of button in active state with option viewMode 'button'.
-    * @css @spacing_Button-between-icon-and-text Spacing between icon and caption in button with seted option icon.
-    *
-    * @css @color_Button_readOnly Color of button with options viewMode 'button' and readOnly 'true'.
-    * @css @border-color_Button_readOnly Border color of button with options viewMode 'button' and readOnly 'true'.
-    *
-    * @css @color_Button__text_primary Text color of button with options style 'primary' and viewMode 'button'.
-    * @css @border-color_Button_button_primary Border color of button with options style 'primary' and viewMode 'button'.
-    * @css @border-color_Button_button_primary_hovered Border color of button in hovered state with options style 'primary' and viewMode 'button'.
-    *
-    * @css @background_Button_button_primary Background color of button with style options 'primary' and  viewMode 'button'.
-    * @css @background_Button_button_primary_hovered Background color of button in hovered state with options style 'primary' and viewMode 'button'.
-    * @css @background_Button_button_primary_active Background color of button in hovered state with options style 'primary' and viewMode 'button'.
-    * @css @background_Button-primary_readOnly Background color of button with options style 'primary', readOnly 'true' and viewMode 'button'.
-    *
-    * @css @color_Button__text_secondary Text color of button with options style 'secondary' and viewMode 'button'.
-    * @css @border-color_Button_secondary Border color of button with options style 'secondary' and viewMode 'button'.
-    * @css @border-color_Button_secondary_hovered Border color of button in hovered state with options style 'secondary' and viewMode 'button'.
-    *
-    * @css @background_Button_secondary Background of button with options style 'secondary' and viewMode 'button'.
-    * @css @background_Button_secondary_hovered Background color of button in hovered state with options style 'secondary' and viewMode 'button'.
-    * @css @background_Button_secondary_active Background color of button in hovered state with options style 'secondary' and viewMode 'button'.
-    * @css @background_Button-secondary_readOnly Background color of button with options style 'secondary', readOnly 'true' and viewMode 'button'.
-    *
-    * @css @height-ToolButton_size-s Height of button with options viewMode 'toolButton' and size 's'.
-    * @css @width-ToolButton_size-s Minimum width of button with options viewMode 'toolButton' and size 's'.
-    *
-    * @css @height-ToolButton_size-m Height of button with options viewMode 'toolButton' and size 'm'.
-    * @css @width-ToolButton_size-m Minimum width of button with options viewMode 'toolButton' and size 'm'.
-    *
-    * @css @height-ToolButton_size-l Height of button with options viewMode 'toolButton' and size 'l'.
-    * @css @width-ToolButton_size-l Minimum width of button with options viewMode 'toolButton' and size 'l'.
-    *
-    * @css @border-radius-ToolButton Border radius of button with option viewMode 'toolButton'.
-    * @css @border-width-ToolButton Border width of button with option viewMode 'toolButton'.
-    * @css @border-style-ToolButton Border style of button with option viewMode 'toolButton'.
-    * @css @border-color-ToolButton Border color of button with option viewMode 'toolButton'.
-    * @css @border-color-ToolButton_readOnly Border color of button with options viewMode 'toolButton' and readOnly 'true'.
-    *
-    * @css @box-shadow-ToolButton Box shadow of button with option viewMode 'toolButton'.
-    * @css @box-shadow-ToolButton_activated Box shadow of button in active state with option viewMode 'toolButton'.
-    *
-    * @css @color-ToolButton_readOnly Height of button with options viewMode 'toolButton' and readOnly 'true'.
-    * @css @color-ToolButton__border_iconStyle-secondary_hovered Border color of button with options viewMode 'toolButton' and iconStyle 'default'.
-    *
-    * @css @background-color-ToolButton Background color of button with options viewMode 'toolButton' and transparent 'false'.
-    * @css @background-color-ToolButton_transparent Background color of button with options viewMode 'toolButton' and transparent 'true'.
-    * @css @background-color-ToolButton_hovered Background color of button in hover state with options viewMode 'toolButton' and transparent 'false'.
-    * @css @background-color-IconButton_transparent_hovered Background color of button in hover state with options viewMode 'toolButton' and transparent 'true'.
-    *
-    * @css @font-size_Link-s Font size of button with options size 's' and viewMode 'link'.
-    * @css @font-size_Link-m Font size of button with options size 'm' and viewMode 'link'.
-    * @css @font-size_Link-l Font size of button with options size 'l' and viewMode 'link'.
-    * @css @font-size_Link-xl Font size of button with options size 'xl' and viewMode 'link'.
-    *
-    * @css @color_Link-secondary_hovered Color of button with options style 'secondary' and viewMode 'link'.
-    * @css @color_Link_readOnly Color of button with options viewMode 'link' and readOnly 'true'.
-    *
-    * @css @color_Link_info Color of button with options style 'info' and viewMode 'link'.
-    * @css @color_Link_info_hovered Color of button in hovered state with options style 'info' and viewMode 'link'.
-    * @css @color_Link_primary Color of button with options style 'primary' and viewMode 'link'.
-    * @css @color_Link_primary_hovered Color of button in hovered state with options style 'primary' and viewMode 'link'.
-    * @css @color_Link_secondary Color of button with options style 'secondary' and viewMode 'link'.
-    * @css @color_Link_secondary_hovered Color of button in hovered state with options style 'secondary' and viewMode 'link'.
-    * @css @color_Link_danger Color of button with options style 'danger' and viewMode 'link'.
-    * @css @color_Link_danger_hovered Color of button in hovered state with options style 'danger' and viewMode 'link'.
-    * @css @color_Link_success Color of button with options style 'success' and viewMode 'link'.
-    * @css @color_Link_success_hovered Color of button in hovered state with options style 'success' and viewMode 'link'.
-    * @css @color_Link_warning Color of button with options style 'warning' and viewMode 'link'.
-    * @css @color_Link_warning_hovered Color of button in hovered state with options style 'info' and viewMode 'link'.
-    * @css @color_Link_default Color of button with options style 'default' and viewMode 'link'.
-    * @css @color_Link_default_hovered Color of button in hovered state with options style 'default' and viewMode 'link'.
-    *
-    * @css @text-shadow_Button_button Text shadow of button with viewMode 'button'.
-    * @css @text-shadow_Button_readOnly Text shadow of button with viewMode 'button' and readOnly 'true'.
-    *
-    * @css @text-shadow_ToolButton Text shadow of button with viewMode 'toolButton'.
-    * @css @text-shadow_ToolButton_readOnly Text shadow of button with viewMode 'toolButton' and readOnly 'true'.
-    *
-    * @css @text-decoration_Link_default Text decoration of button with options style 'default' and viewMode 'link'.
-    * @css @text-decoration_Link_secondary Text decoration of button with options style 'secondary' and viewMode 'link'.
-    * @css @text-decoration_Link_primary Text decoration of button with options style 'primary' and viewMode 'link'.
-    * @css @text-decoration_Link_danger decoration of button with options style 'danger' and viewMode 'link'.
-    * @css @text-decoration_Link_warning Text decoration of button with options style 'warning' and viewMode 'link'.
-    * @css @text-decoration_Link_success Text decoration of button with options style 'success' and viewMode 'link'.
-    * @css @text-decoration_Link_info Text decoration of button with options style 'info' and viewMode 'link'.
-    */
-);
+/**
+ * @author Михайловский Д.С.
+ * @mixin Controls/_buttons/ButtonStyles
+ * @public
+ *
+ * @css @height_Button_size_s Height of button with options viewMode 'button' and size 's'.
+ * @css @font-size_Button_size_s Font size of button with options viewMode 'button' and size 's'.
+ * @css @border-radius_Button_size_s Border radius of button with options viewMode 'button' and size 's'.
+ * @css @padding_Button-wrapper_size_s Padding from content to border of button with options viewMode 'button' and size 's'.
+ *
+ * @css @height_Button_size_m Height of button with options viewMode 'button' and size 'm'.
+ * @css @font-size_Button_size_m Font size of button with options viewMode 'button' and size 'm''.
+ * @css @border-radius_Button_size_m Border radius of button with options viewMode 'button' and size 'm'.
+ * @css @padding_Button-wrapper_size_m Padding from content to border of button with options viewMode 'button' and size 'm'.
+ *
+ * @css @height_Button_size_l Height of button with options viewMode 'button' and size 'l'.
+ * @css @font-size_Button_size_l Font size of button with options viewMode 'button' and size 'l'.
+ * @css @border-radius_Button_size_l Border radius of button with options viewMode 'button' and size 'l'.
+ * @css @padding_Button__wrapper_size_l Padding from content to border of button with options viewMode 'button' and size 'l'.
+ *
+ * @css @thickness_Button__border Border thickness of button with option viewMode 'button'.
+ * @css @min-width_Button_common Minimal width of button with option viewMode 'button'.
+ * @css @box-shadow_Button_common Box shadow of button with option viewMode 'button'.
+ * @css @box-shadow_Button_active Box shadow of button in active state with option viewMode 'button'.
+ * @css @spacing_Button-between-icon-and-text Spacing between icon and caption in button with seted option icon.
+ *
+ * @css @color_Button_readOnly Color of button with options viewMode 'button' and readOnly 'true'.
+ * @css @border-color_Button_readOnly Border color of button with options viewMode 'button' and readOnly 'true'.
+ *
+ * @css @color_Button__text_primary Text color of button with options style 'primary' and viewMode 'button'.
+ * @css @border-color_Button_button_primary Border color of button with options style 'primary' and viewMode 'button'.
+ * @css @border-color_Button_button_primary_hovered Border color of button in hovered state with options style 'primary' and viewMode 'button'.
+ *
+ * @css @background_Button_button_primary Background color of button with style options 'primary' and  viewMode 'button'.
+ * @css @background_Button_button_primary_hovered Background color of button in hovered state with options style 'primary' and viewMode 'button'.
+ * @css @background_Button_button_primary_active Background color of button in hovered state with options style 'primary' and viewMode 'button'.
+ * @css @background_Button-primary_readOnly Background color of button with options style 'primary', readOnly 'true' and viewMode 'button'.
+ *
+ * @css @color_Button__text_secondary Text color of button with options style 'secondary' and viewMode 'button'.
+ * @css @border-color_Button_secondary Border color of button with options style 'secondary' and viewMode 'button'.
+ * @css @border-color_Button_secondary_hovered Border color of button in hovered state with options style 'secondary' and viewMode 'button'.
+ *
+ * @css @background_Button_secondary Background of button with options style 'secondary' and viewMode 'button'.
+ * @css @background_Button_secondary_hovered Background color of button in hovered state with options style 'secondary' and viewMode 'button'.
+ * @css @background_Button_secondary_active Background color of button in hovered state with options style 'secondary' and viewMode 'button'.
+ * @css @background_Button-secondary_readOnly Background color of button with options style 'secondary', readOnly 'true' and viewMode 'button'.
+ *
+ * @css @height-ToolButton_size-s Height of button with options viewMode 'toolButton' and size 's'.
+ * @css @width-ToolButton_size-s Minimum width of button with options viewMode 'toolButton' and size 's'.
+ *
+ * @css @height-ToolButton_size-m Height of button with options viewMode 'toolButton' and size 'm'.
+ * @css @width-ToolButton_size-m Minimum width of button with options viewMode 'toolButton' and size 'm'.
+ *
+ * @css @height-ToolButton_size-l Height of button with options viewMode 'toolButton' and size 'l'.
+ * @css @width-ToolButton_size-l Minimum width of button with options viewMode 'toolButton' and size 'l'.
+ *
+ * @css @border-radius-ToolButton Border radius of button with option viewMode 'toolButton'.
+ * @css @border-width-ToolButton Border width of button with option viewMode 'toolButton'.
+ * @css @border-style-ToolButton Border style of button with option viewMode 'toolButton'.
+ * @css @border-color-ToolButton Border color of button with option viewMode 'toolButton'.
+ * @css @border-color-ToolButton_readOnly Border color of button with options viewMode 'toolButton' and readOnly 'true'.
+ *
+ * @css @box-shadow-ToolButton Box shadow of button with option viewMode 'toolButton'.
+ * @css @box-shadow-ToolButton_activated Box shadow of button in active state with option viewMode 'toolButton'.
+ *
+ * @css @color-ToolButton_readOnly Height of button with options viewMode 'toolButton' and readOnly 'true'.
+ * @css @color-ToolButton__border_iconStyle-secondary_hovered Border color of button with options viewMode 'toolButton' and iconStyle 'default'.
+ *
+ * @css @background-color-ToolButton Background color of button with options viewMode 'toolButton' and transparent 'false'.
+ * @css @background-color-ToolButton_transparent Background color of button with options viewMode 'toolButton' and transparent 'true'.
+ * @css @background-color-ToolButton_hovered Background color of button in hover state with options viewMode 'toolButton' and transparent 'false'.
+ * @css @background-color-IconButton_transparent_hovered Background color of button in hover state with options viewMode 'toolButton' and transparent 'true'.
+ *
+ * @css @font-size_Link-s Font size of button with options size 's' and viewMode 'link'.
+ * @css @font-size_Link-m Font size of button with options size 'm' and viewMode 'link'.
+ * @css @font-size_Link-l Font size of button with options size 'l' and viewMode 'link'.
+ * @css @font-size_Link-xl Font size of button with options size 'xl' and viewMode 'link'.
+ *
+ * @css @color_Link-secondary_hovered Color of button with options style 'secondary' and viewMode 'link'.
+ * @css @color_Link_readOnly Color of button with options viewMode 'link' and readOnly 'true'.
+ *
+ * @css @color_Link_info Color of button with options style 'info' and viewMode 'link'.
+ * @css @color_Link_info_hovered Color of button in hovered state with options style 'info' and viewMode 'link'.
+ * @css @color_Link_primary Color of button with options style 'primary' and viewMode 'link'.
+ * @css @color_Link_primary_hovered Color of button in hovered state with options style 'primary' and viewMode 'link'.
+ * @css @color_Link_secondary Color of button with options style 'secondary' and viewMode 'link'.
+ * @css @color_Link_secondary_hovered Color of button in hovered state with options style 'secondary' and viewMode 'link'.
+ * @css @color_Link_danger Color of button with options style 'danger' and viewMode 'link'.
+ * @css @color_Link_danger_hovered Color of button in hovered state with options style 'danger' and viewMode 'link'.
+ * @css @color_Link_success Color of button with options style 'success' and viewMode 'link'.
+ * @css @color_Link_success_hovered Color of button in hovered state with options style 'success' and viewMode 'link'.
+ * @css @color_Link_warning Color of button with options style 'warning' and viewMode 'link'.
+ * @css @color_Link_warning_hovered Color of button in hovered state with options style 'info' and viewMode 'link'.
+ * @css @color_Link_default Color of button with options style 'default' and viewMode 'link'.
+ * @css @color_Link_default_hovered Color of button in hovered state with options style 'default' and viewMode 'link'.
+ *
+ * @css @text-shadow_Button_button Text shadow of button with viewMode 'button'.
+ * @css @text-shadow_Button_readOnly Text shadow of button with viewMode 'button' and readOnly 'true'.
+ *
+ * @css @text-shadow_ToolButton Text shadow of button with viewMode 'toolButton'.
+ * @css @text-shadow_ToolButton_readOnly Text shadow of button with viewMode 'toolButton' and readOnly 'true'.
+ *
+ * @css @text-decoration_Link_default Text decoration of button with options style 'default' and viewMode 'link'.
+ * @css @text-decoration_Link_secondary Text decoration of button with options style 'secondary' and viewMode 'link'.
+ * @css @text-decoration_Link_primary Text decoration of button with options style 'primary' and viewMode 'link'.
+ * @css @text-decoration_Link_danger decoration of button with options style 'danger' and viewMode 'link'.
+ * @css @text-decoration_Link_warning Text decoration of button with options style 'warning' and viewMode 'link'.
+ * @css @text-decoration_Link_success Text decoration of button with options style 'success' and viewMode 'link'.
+ * @css @text-decoration_Link_info Text decoration of button with options style 'info' and viewMode 'link'.
+ */
