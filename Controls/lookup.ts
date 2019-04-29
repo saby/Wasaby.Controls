aa8e1f7e4cca7bc334c82d7248911318954e119d
ganshinyao <yo.ganshin@tensor.ru>
2019-04-29 16:01:39 +0300

    https://online.sbis.ru/opendoc.html?guid=e7dc615a-1764-4dc7-a33d-ab83c3906d76

--- Controls/lookup.ts
+++ Controls/lookup.ts
@@ -7,10 +7,10 @@
  * @includes _CollectionController Controls/_lookup/SelectedCollection/Controller
  * @includes ItemTemplate wml!Controls/_lookup/Lookup/itemTemplate
  * @includes Opener Controls/_lookup/Opener
- * @includes Controls/_lookup/Lookup/LookupStyles
- * @includes Controls/_lookup/Button/SelectorButtonStyles
- * @includes Controls/_lookup/Lookup/Link/LookupLinkStyles
- * @includes Controls/_lookup/SelectedCollection/SelectedCollectionStyles
+ * @includes LookupStyles Controls/_lookup/Lookup/LookupStyles
+ * @includes SelectorButtonStyles Controls/_lookup/Button/SelectorButtonStyles
+ * @includes LookupLinkStyles Controls/_lookup/Lookup/Link/LookupLinkStyles
+ * @includes SelectedCollectionStyles Controls/_lookup/SelectedCollection/SelectedCollectionStyles
  * @public
  * @author Kraynov D.
  */
