f4cc2ffa5edb265c983b630651f5d8ec0d72940a
am.gerasimov <am.gerasimov@tensor.ru>
2019-05-07 10:12:01 +0300

    https://online.sbis.ru/opendoc.html?guid=e5f29365-7308-493e-90dc-11d4988b9896

--- Controls/_filterPopup/Panel/Dropdown.ts
+++ Controls/_filterPopup/Panel/Dropdown.ts
@@ -11,11 +11,26 @@ import 'css!theme?Controls/filterPopup';
     * @default false
     * @example
     * <pre>
-    *     <Controls.filterPopup:Panel.Dropdown showCross="{{true}}"/>
+    *     <Controls.filterPopup:Dropdown showCross="{{true}}"/>
     * </pre>
     *
     * @class Controls/_filterPopup/Panel/Dropdown
     * @extends Control/_dropdown/Input
+    * @mixes Controls/interface/ISource
+    * @mixes Controls/_list/interface/IHierarchy
+    * @mixes Controls/interface/IFilter
+    * @mixes Controls/interface/INavigation
+    * @mixes Controls/Input/interface/IValidation
+    * @mixes Controls/interface/IMultiSelectable
+    * @mixes Controls/_dropdown/interface/IFooterTemplate
+    * @mixes Controls/_dropdown/interface/IHeaderTemplate
+    * @mixes Controls/interface/ISelectorDialog
+    * @mixes Controls/interface/IDropdownEmptyText
+    * @mixes Controls/Input/interface/IInputDropdown
+    * @mixes Controls/interface/IDropdown
+    * @mixes Controls/_dropdown/interface/IGrouped
+    * @mixes Controls/interface/IInputDropdown
+    * @mixes Controls/interface/ITextValue
     * @control
     * @public
     * @author Герасимов А.М.
