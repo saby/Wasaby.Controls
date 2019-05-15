9a8cb788c73b6c74ae7ef40b0f84abab8f59d690
aa.malcev2 <aa.malcev2@tensor.ru>
2019-05-15 15:53:28 +0300

    Restore modules used by RichEditor/_base/Helpers/history https://online.sbis.ru/opendoc.html?guid=6e558118-7504-4475-b36e-f07098085595

---
+++ Controls/_decorator/Markup/Converter.js
@@ -0,0 +1,8 @@
+// Required by RichEditor/_base/Helpers/history
+define('Controls/Decorator/Markup/Converter',
+   ['Controls/decorator'],
+   function(Control) {
+      'use strict';
+      return Control.Converter;
+   }
+);
