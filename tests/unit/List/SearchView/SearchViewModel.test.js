2c6c9dfd31747c6e94ffdd7e39d6e4cf456c9372
as.avramenko <as.avramenko@tensor.ru>
2019-05-16 15:59:23 +0300

    https://online.sbis.ru/opendoc.html?guid=f74cff25-4b26-4827-b063-273d015a7681

--- tests/unit/List/SearchView/SearchViewModel.test.js
+++ tests/unit/List/SearchView/SearchViewModel.test.js
@@ -9,5 +9,32 @@ define(['Controls/List/SearchView/SearchViewModel', 'Types/collection'], functio
             ];
          assert.deepEqual(searchViewModel.getDisplayFilter(null, {itemsFilterMethod: itemsFilterMethod}), result);
       });
+      it('itemTemplate', function() {
+         var
+            itemData,
+            searchViewModel,
+            superclassGetItemDataByItem = SearchViewModel.superclass.getItemDataByItem;
+         SearchViewModel.superclass.getItemDataByItem = function() {
+            return {};
+         };
+
+         searchViewModel = new SearchViewModel({});
+         itemData = searchViewModel.getItemDataByItem();
+         assert.equal(itemData.resolveItemTemplate({}).prototype._moduleName, 'Controls/treeGrid:ItemTemplate');
+         assert.equal(itemData.resolveItemTemplate({ getId: function() {} }).prototype._moduleName, 'Controls/treeGrid:ItemTemplate');
+
+         searchViewModel = new SearchViewModel({
+            itemTemplate: {
+               prototype: {
+                  _moduleName: 'testItemTemplate'
+               }
+            }
+         });
+         itemData = searchViewModel.getItemDataByItem();
+         assert.equal(itemData.resolveItemTemplate({}).prototype._moduleName, 'Controls/treeGrid:ItemTemplate');
+         assert.equal(itemData.resolveItemTemplate({ getId: function() {} }).prototype._moduleName, 'testItemTemplate');
+
+         SearchViewModel.superclass.getItemDataByItem = superclassGetItemDataByItem;
+      });
    });
 });
