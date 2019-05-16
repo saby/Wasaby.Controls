72524cebd48e0a2f05334100ad7d913b11e223bb
aa.malcev2 <aa.malcev2@tensor.ru>
2019-05-16 12:37:32 +0300

    Restore modules used by tmpl!Engine-demo/Browser/Browser https://online.sbis.ru/opendoc.html?guid=6e558118-7504-4475-b36e-f07098085595

---
+++ Controls/Filter/Fast.js
@@ -0,0 +1,8 @@
+// Used by tmpl!Engine-demo/Browser/Browser
+define('Controls/Filter/Fast',
+   ['Controls/filter'],
+   function(Control) {
+      'use strict';
+      return Control.Fast;
+   }
+);
