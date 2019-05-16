bbfbb35e0f35a997697a3a66910c55fbd01fa645
av.zalyaev <av.zalyaev@tensor.ru>
2019-05-16 16:20:25 +0300

    https://online.sbis.ru/opendoc.html?guid=22130989-bc42-45c7-ba6f-2c799ec532a7

--- Controls/_dataSource/_parking/Handler.ts
+++ Controls/_dataSource/_parking/Handler.ts
@@ -2,10 +2,11 @@
 
 /**
  * Возвращаемый обработчиком ошибки результат
- * @typedef {Object}
- * @name Controls/_dataSource/_parking/ViewConfig
+ * @typedef {Object} Controls/_dataSource/_parking/ViewConfig
  * @property {Function | String} template Шаблон отображения ошибки
  * @property {Object} [options] параметры построяния шаблона ошибки
+ * @private
+ * @author Заляев А.В.
  */
 export type ViewConfig<TOptions = object> = {
     template: any;
@@ -14,9 +15,10 @@ export type ViewConfig<TOptions = object> = {
 
 /**
  * Обработчик парковочной
- * @typedef {Function}
- * @name Controls/_dataSource/_parking/Handler
+ * @typedef {Function} Controls/_dataSource/_parking/Handler
  * @param {*} объект с параметрами
  * @return {void | Controls/_dataSource/_parking/ViewConfig}
+ * @private
+ * @author Заляев А.В.
  */
 export type Handler<TOptions = object> = (config: any) => ViewConfig<TOptions> | void;
