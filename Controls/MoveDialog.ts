3d2cbc95e479011e3239a9d6f80deca5e8609c54
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2019-05-16 15:29:30 +0300

    https://online.sbis.ru/opendoc.html?guid=6984a08f-025a-48fc-9944-0337173e8bd5

--- Controls/MoveDialog.ts
+++ Controls/MoveDialog.ts
@@ -40,7 +40,12 @@ import 'css!theme?Controls/_MoveDialog/MoveDialog';
             showType: 2
          }];
          this._root = options.root;
-         this._columns = options.columns;
+
+         // TODO: сейчас прикладной программист передает в MoveDialog опцию columns, что плохо, т.к. он может повлиять на
+         // отображение колонки, а диалог во всех реестрах должен выглядеть одинаково. Нужно убрать возможно передавать
+         // конфигурации колонки и дать возможность настривать имя поля, из которого необходимо брать название папок.
+         // Выписана задача: https://online.sbis.ru/opendoc.html?guid=aeaff20a-ee07-4d1b-8a9d-2528a269bc91
+         this._columns = options.columns.slice();
          this._columns[0].textOverflow = 'ellipsis';
          this._onItemClick = this._onItemClick.bind(this);
          this._itemsFilterMethod = this._itemsFilterMethod.bind(this);
