aad21f762b1d6d62dc3203c804c255bead93d27e
aa.malcev2 <aa.malcev2@tensor.ru>
2019-05-20 11:01:34 +0300

    Restore modules used by wml!Engine-demo/Browser/Tabs/resources/topTemplate2 https://online.sbis.ru/opendoc.html?guid=6e558118-7504-4475-b36e-f07098085595

---
+++ Controls/Heading/BackButton.js
@@ -0,0 +1,8 @@
+// Used by wml!Engine-demo/Browser/Tabs/resources/topTemplate2
+define('Controls/Heading/BackButton',
+   ['Controls/_heading/BackButton'],
+   function(Control) {
+      'use strict';
+      return Control;
+   }
+);
