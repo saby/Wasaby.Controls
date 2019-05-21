c6b08853e95f3378e78a304c8b298914a9fba341
aa.malcev2 <aa.malcev2@tensor.ru>
2019-05-21 12:31:01 +0300

    Restore modules used by wml!Engine-demo/Selector/FlatListSelector/FlatListSelector https://online.sbis.ru/opendoc.html?guid=6e558118-7504-4475-b36e-f07098085595

---
+++ Controls/Input/Search.js
@@ -0,0 +1,8 @@
+// USed by wml!Engine-demo/Selector/FlatListSelector/FlatListSelector
+define('Controls/Input/Search',
+   ['Controls/search'],
+   function(Control) {
+      'use strict';
+      return Control.Input;
+   }
+);
