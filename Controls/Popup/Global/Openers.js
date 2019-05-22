cefd7539fe6cdeaf094ef5122cad4233f9eec9c5
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-05-22 11:51:24 +0300

    https://online.sbis.ru/opendoc.html?guid=24ffc043-8de3-48d7-bb71-8f9274c7c132

--- Controls/Popup/Global/Openers.js
+++ Controls/Popup/Global/Openers.js
@@ -2,6 +2,8 @@ define('Controls/Popup/Global/Openers', ['Core/Control', 'wml!Controls/Popup/Glo
    function(Control, template) {
       return Control.extend({
          _template: template,
+
+         // todo: https://online.sbis.ru/opendoc.html?guid=ca9e3c9e-88e4-4a1c-8b6f-709ece491da7
          getPreviewer: function() {
             return this._children.previewerOpener;
          },
