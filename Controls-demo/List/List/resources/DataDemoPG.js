fc2ca8abe25061f6c5f04edc51e69069dbe6d2fc
va.kolesov <va.kolesov@tensor.ru>
2019-05-23 17:26:04 +0300

    https://online.sbis.ru/opendoc.html?guid=1c3747f4-3b76-488d-aec9-e9c6bf41cf0f

--- Controls-demo/List/List/resources/DataDemoPG.js
+++ Controls-demo/List/List/resources/DataDemoPG.js
@@ -14,14 +14,14 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
       return {
          gadgets: [
             {
-               id: 1,
+               id: '1',
                title: 'So long name of the record that it does not fit into the maximum size 1',
                description: 'Some item 1',
                mytemp: 'wml!Controls-demo/List/List/resources/ItemTemplatePG/CustomItemTemplate',
                saleTemplate: 'wml!Controls-demo/List/List/resources/ItemTemplatePG/CustomItemTemplateTwo'
             },
             {
-               id: 2,
+               id: '2',
                title: 'Notebooks 2',
                description: 'Some description',
                boldTemplate: 'wml!Controls-demo/List/List/resources/ItemTemplatePG/CustomItemTemplateOne',
@@ -29,19 +29,19 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
 
             },
             {
-               id: 3,
+               id: '3',
                title: 'Smartphones 3 ',
                description: 'Don\'t know',
                mytemp: 'wml!Controls-demo/List/List/resources/ItemTemplatePG/CustomItemTemplate',
             },
             {
-               id: 4,
+               id: '4',
                title: 'Tablets 4',
                description: 'iPads, Samsungs and others',
                boldTemplate: 'wml!Controls-demo/List/List/resources/ItemTemplatePG/CustomItemTemplateOne',
             },
             {
-               id: 5,
+               id: '5',
                title: 'Ultra 4K TV 5',
                description: 'TV with large displays',
                saleTemplate: 'wml!Controls-demo/List/List/resources/ItemTemplatePG/CustomItemTemplateTwo'
@@ -49,21 +49,21 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
          ],
          groupGadgets: [
             {
-               id: 1,
+               id: '1',
                title: 'Apple MacBook Pro MF839',
                description: 'description of item with id 1',
                brand: 'apple',
                year: 2007
             },
             {
-               id: 2,
+               id: '2',
                title: 'ASUS X751SA-TY124D',
                description: 'description of item with id 2',
                brand: 'asus',
                year: 2008
             },
             {
-               id: 3,
+               id: '3',
                title: 'HP 250 G5 (W4N28EA)',
                description: 'description of item with id 3',
                brand: 'hp',
@@ -71,28 +71,28 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
 
             },
             {
-               id: 4,
+               id: '4',
                title: 'ACER One 10 S1002-15GT',
                description: 'description of item with id 4',
                brand: 'acer',
                year: 2008
             },
             {
-               id: 5,
+               id: '5',
                title: 'ASUS X541SA-XO056D',
                description: 'description of item with id 5',
                brand: 'asus',
                year: 2008
             },
             {
-               id: 6,
+               id: '6',
                title: 'ACER Aspire F 15 F5-573G-51Q7',
                description: 'description of item with id 6',
                brand: 'acer',
                year: 2006
             },
             {
-               id: 7,
+               id: '7',
                title: 'HP 250 G5 (W4M56EA)',
                description: 'description of item with id 7',
                brand: 'hp',
@@ -101,35 +101,35 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
          ],
          music: [
             {
-               id: 1,
+               id: '1',
                title: 'Love The Way You Lie 1',
                description: 'Skylar Grey'
             },
             {
-               id: 2,
+               id: '2',
                title: 'My Life Be Like(Grits) 2',
                description: 'L. Starz'
             },
             {
-               id: 3,
+               id: '3',
                title: 'Veorra 3 ',
                description: 'Veorra'
 
             },
             {
-               id: 4,
+               id: '4',
                title: 'Partystarter 4',
                description: 'Bakermat'
             },
             {
-               id: 5,
+               id: '5',
                title: 'White Flag VIP 5',
                description: 'Delta Heavy'
             }
          ],
          firstItemActionsArray: [
             {
-               id: 5,
+               id: '5',
                title: 'Mark as read',
                showType: showType.TOOLBAR,
                handler: function() {
@@ -137,7 +137,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 1,
+               id: '1',
                icon: 'icon-PhoneNull',
                title: 'Call',
                handler: function(item) {
@@ -145,7 +145,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 2,
+               id: '2',
                icon: 'icon-EmptyMessage',
                title: 'Send',
                parent: null,
@@ -155,7 +155,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 3,
+               id: '3',
                icon: 'icon-Profile',
                title: 'Profile',
                showType: showType.MENU_TOOLBAR,
@@ -166,7 +166,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 6,
+               id: '6',
                icon: 'icon-Email',
                title: 'Email',
                parent: 2,
@@ -176,7 +176,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 4,
+               id: '4',
                icon: 'icon-Erase',
                iconStyle: 'danger',
                title: 'Remove',
@@ -188,7 +188,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
          ],
          secondItemActionsArray: [
             {
-               id: 1,
+               id: '1',
                icon: 'icon-PhoneNull',
                title: 'Skype',
                showType: showType.MENU,
@@ -197,7 +197,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 2,
+               id: '2',
                icon: 'icon-EmptyMessage',
                title: 'Viber Message',
                showType: showType.MENU,
@@ -206,7 +206,7 @@ define('Controls-demo/List/List/resources/DataDemoPG', [],
                }
             },
             {
-               id: 3,
+               id: '3',
                icon: 'icon-Erase',
                iconStyle: 'danger',
                title: 'Remove',
