09537c87ae0b53bac70ebbb66b3f9de8f50ce9ff
am.gerasimov <am.gerasimov@tensor.ru>
2019-05-24 08:44:45 +0300

    https://online.sbis.ru/opendoc.html?guid=25935074-2389-4955-856b-4e5f7b08c7c0

--- Controls/interface/IFilter.js
+++ Controls/interface/IFilter.js
@@ -44,4 +44,61 @@ define('Controls/interface/IFilter', [
     * </pre>
     */
 
+   /**
+    * @event Controls/interface/IFilter#filterChanged Событие filterChanged срабатывает, когда фильтр был изменён.
+    * @param {Core/vdom/Synchronizer/resources/SyntheticEvent} eventObject Дескриптор события.
+    * @param {Object} Изменённый фильтр
+    * @remark
+    * Важно помнить, что опции имутабельны (https://ru.m.wikipedia.org/wiki/Неизменяемый_объект), поэтому фильтр в аргументах события отличается от фильтра в опциях контрола.
+    * @example
+    * WML:
+    * <pre>
+    *    <Controls.filter:Controller on:filterChanged="filterChanged()" filter="{{ _filter }}"/>
+    *    <pre>{{ _filterString }}</pre>
+    * </pre>
+    * JS:
+    * <pre>
+    *    _filter: null,
+    *    _beforeMount: function() {
+    *       this._filter = {
+    *          city: 'Yaroslavl'
+    *       }
+    *    },
+    *    filterChanged: function(e, filter) {
+    *       this._filter = filter; //Т.к. в приведённом примере опция filter не связана с помощью bind c состоянием, необходимо обновить фильтр на состоянии самостоятельно.
+    *       this._filterString = JSON.stringify(this._filter, null, 4);
+    *    }
+    * </pre>
+    * @see filter
+    */
+
+   /**
+    * @event Controls/interface/IFilter#filterChangedENG Occurs when filter were changed.
+    * @param {Core/vdom/Synchronizer/resources/SyntheticEvent} eventObject Descriptor of the event.
+    * @param {Object} new filter
+    * @noshow
+    * @remark
+    * It's important to remember that we don't mutate filter from options (or any other option). So filter in the event arguments and filter in the component's options are not the same.
+    * @example
+    * WML:
+    * <pre>
+    *    <Controls.filter:Controller on:filterChanged="filterChanged()" filter="{{ _filter }}"/>
+    *    <pre>{{ _filterString }}</pre>
+    * </pre>
+    * JS:
+    * <pre>
+    *    _filter: null,
+    *    _beforeMount: function() {
+    *       this._filter = {
+    *          city: 'Yaroslavl'
+    *       }
+    *    },
+    *    filterChanged: function(e, filter) {
+    *       this._filter = filter; //We don't use binding in this example so we have to update state manually.
+    *       this._filterString = JSON.stringify(this._filter, null, 4);
+    *    }
+    * </pre>
+    * @see filter
+    */
+
 });
