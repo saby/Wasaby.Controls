42f3ac18530a1b0497b812b99c476392daef31af
as.avramenko <as.avramenko@tensor.ru>
2019-05-24 18:31:20 +0300

    https://online.sbis.ru/opendoc.html?guid=ba8c5686-d924-4064-a8b9-f353860d6ef5

--- tests/unit/List/Grid/GridViewModel.test.js
+++ tests/unit/List/Grid/GridViewModel.test.js
@@ -1060,9 +1060,9 @@ define(['Controls/grid', 'Core/core-merge', 'Types/collection', 'Types/entity', 
             let
                 called = false,
                 nativeFn = gridViewModel._model._setEditingItemData,
-                initialStatus = GridLayoutUtil.isPartialSupport;
+                initialStatus = GridLayoutUtil.isPartialGridSupport;
 
-            GridLayoutUtil.isPartialSupport = true;
+            GridLayoutUtil.isPartialGridSupport = function() { return true };
 
             gridViewModel._model._setEditingItemData = (iData) => {
                called = true;
@@ -1074,7 +1074,7 @@ define(['Controls/grid', 'Core/core-merge', 'Types/collection', 'Types/entity', 
             });
             assert.isTrue(called);
 
-            GridLayoutUtil.isPartialSupport = initialStatus;
+            GridLayoutUtil.isPartialGridSupport = initialStatus;
             gridViewModel._model._setEditingItemData = nativeFn;
          });
 
