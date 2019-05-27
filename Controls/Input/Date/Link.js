e8d88982b3552b8b17e8f08ab3eecd438fab096f
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-27 15:42:57 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

---
+++ Controls/Input/Date/Link.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Input/Date/Link', ['Controls/dateRange'], function(Control) {
+   return Control.Link;
+});
---
+++ Controls/Input/Date/LinkView.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Input/Date/LinkView', ['Controls/dateRange'], function(Control) {
+   return Control.LinkView;
+});
