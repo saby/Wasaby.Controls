ae7e18a2b7431b9a2ee981e3b22370731cfd69ac
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-27 13:04:50 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/calendar.ts
+++ Controls/calendar.ts
@@ -11,9 +11,19 @@
  * @author Kraynov D.
  */
 
+import MonthViewDayTemplate = require('wml!Controls/_calendar/MonthView/dayTemplate');
+import MonthViewTemplate = require('wml!Controls/_calendar/MonthView/MonthView');
+import MonthViewTableBodyTemplate = require('wml!Controls/_calendar/MonthView/MonthViewTableBody');
+
 export {default as Month} from './_calendar/Month';
 export {default as MonthList} from './_calendar/MonthList';
 export {default as MonthSlider} from './_calendar/MonthSlider';
 export {Base as MonthSliderBase} from './_calendar/MonthSlider';
 export {default as MonthModel} from './_calendar/Month/Model';
 export {default as Utils} from './_calendar/Utils';
+
+export {
+   MonthViewDayTemplate,
+   MonthViewTemplate,
+   MonthViewTableBodyTemplate
+}
