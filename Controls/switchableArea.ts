e990c2f9f668eb2424b46d1eafcafc45674fb22a
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-29 13:03:08 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

---
+++ Controls/switchableArea.ts
@@ -0,0 +1,11 @@
+/**
+ * switchableArea library
+ * @library Controls/switchableArea
+ * @includes View Controls/_switchableArea/View
+ * @includes Base Controls/_editableArea/Templates/Editors/Base
+ * @includes DateTime Controls/_editableArea/Templates/Editors/DateTime
+ * @public
+ * @author Kraynov D.
+ */
+
+export {default as View} from './_switchableArea/View';
