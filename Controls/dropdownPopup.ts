eb948a624f38f43b4155cfbf4e6301c9b967787c
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-30 17:41:34 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/dropdownPopup.ts
+++ Controls/dropdownPopup.ts
@@ -4,6 +4,7 @@
  * @includes Template wml!Controls/_dropdownPopup/defaultHeadTemplate
  * @includes List Controls/_dropdownPopup/DropdownList
  * @includes ListStyles Controls/_dropdownPopup/DropdownListStyles
+ * @includes itemTemplate tmpl!Controls/_dropdownPopup/itemTemplate
  * @public
  * @author Kraynov D.
  */
@@ -13,11 +14,13 @@ import GroupTemplate = require('wml!Controls/_dropdownPopup/defaultGroupTemplate
 import List = require('Controls/_dropdownPopup/DropdownList');
 import _ForTemplate = require('wml!Controls/_dropdownPopup/For');
 import MoreButton = require('Controls/_dropdownPopup/MoreButton');
+import itemTemplate = require('tmpl!Controls/_dropdownPopup/itemTemplate');
 
 export {
     Template,
     GroupTemplate,
     List,
     _ForTemplate,
-    MoreButton
+    MoreButton,
+    itemTemplate
 }
