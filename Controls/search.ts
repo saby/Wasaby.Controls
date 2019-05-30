a871e04d1c3a88b112e8bd18e7aa2b75c9d622dd
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-30 17:23:39 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/search.ts
+++ Controls/search.ts
@@ -6,6 +6,8 @@
  * @includes MisspellContainer Controls/_search/Misspell/Container
  * @includes Controller Controls/_search/Controller
  * @includes Input Controls/_search/Input/Search
+ * @includes _Search Controls/_search/_Search
+ * @includes _SearchController Controls/_search/_SearchController
  * @public
  * @author Kraynov D.
  */
@@ -17,6 +19,8 @@ import Controller = require('Controls/_search/Controller');
 import Input = require('Controls/_search/Input/Search');
 
 import getSwitcherStrFromData = require('Controls/_search/Misspell/getSwitcherStrFromData');
+
+export {default as _Search} from './_search/_Search';
 
 export {
    InputContainer,
