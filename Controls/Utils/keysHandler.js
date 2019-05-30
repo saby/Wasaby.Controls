7d493bcaae2e1427d8ba82498490ea456394b757
vd.volockoy <vd.volockoy@tensor.ru>
2019-05-30 15:11:05 +0300

    Поправил ошибку с перехватом событий когда в качестве редактирования по месту БТР Ошибка: https://online.sbis.ru/opendoc.html?guid=3a6d9600-32ef-4187-bec2-95086d5aab00

--- Controls/Utils/keysHandler.js
+++ Controls/Utils/keysHandler.js
@@ -10,7 +10,15 @@ define('Controls/Utils/keysHandler', [], function() {
          if (keys.hasOwnProperty(action)) {
             if (event.nativeEvent.keyCode === keys[action]) {
                handlerSet[action](scope, event);
-               event.stopImmediatePropagation();
+
+               // Так как наша система событий ловит события на стадии capture,
+               // а подписки в БТРе на стадии bubbling, то не нужно звать stopPropagation
+               // так как обработчики БТРа в таком случае не отработают, потому что
+               // у события не будет bubbling фазы
+               // TODO: Нужно поправить после исправления https://online.sbis.ru/opendoc.html?guid=cefa8cd9-6a81-47cf-b642-068f9b3898b7
+               if (!event.target.closest('.richEditor_TinyMCE')) {
+                  event.stopImmediatePropagation();
+               }
                return;
             }
          }
