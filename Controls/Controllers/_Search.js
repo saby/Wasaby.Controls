aa932bbd6cd5ff1d433c4bf9955a3c7ae0c61526
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-30 17:14:14 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

---
+++ Controls/Controllers/_Search.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Controllers/_Search', ['Controls/search'], function(Control) {
+   return Control._Search;
+});
---
+++ Controls/Controllers/_SearchController.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Controllers/_SearchController', ['Controls/search'], function(Control) {
+   return Control._SearchController;
+});
