a7e8289cc931c8be57e517fd8b90cb4f592eb571
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-30 16:58:41 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/form.ts
+++ Controls/form.ts
@@ -3,12 +3,15 @@
  * @library Controls/form
  * @includes Controller Controls/_form/FormController
  * @includes Crud Controls/_form/Crud
+ * @includes PrimaryAction Controls/_form/PrimaryAction
  * @public
  * @author Kraynov D.
  */
 
 import Controller = require('Controls/_form/FormController');
 import Crud = require('Controls/_form/Crud');
+
+export {default as PrimaryAction} from './_form/PrimaryAction'
 
 export {
     Controller,
