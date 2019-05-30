943d8eb32462ae9f49b5ca1c26e2f5bd6dc3bd54
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-30 16:43:51 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/_source/Adapter/SelectedKey.ts
+++ Controls/_source/Adapter/SelectedKey.ts
@@ -1,63 +1,55 @@
-define('Controls/Container/Adapter/SelectedKey',
-   [
-      'Core/Control',
-      'wml!Controls/Container/Adapter/SelectedKey/SelectedKey'
-   ],
+import Control = require('Core/Control');
+import template = require('wml!Controls/_source/Adapter/SelectedKey/SelectedKey');
 
-   /**
-    * Container for controls that implement interface {@link Controls/interface/IMultiSelectable multiSelectable}.
-    * Container receives selectedKey option and transfers selectedKeys option to children.
-    * Listens for children "selectedKeysChanged" event and notify event "selectedKeyChanged".
-    * @class Controls/Container/Adapter/SelectedKey
-    * @extends Controls/Control
-    * @control
-    * @public
-    * @author Золотова Э.Е.
-    */
+/**
+ * Container for controls that implement interface {@link Controls/interface/IMultiSelectable multiSelectable}.
+ * Container receives selectedKey option and transfers selectedKeys option to children.
+ * Listens for children "selectedKeysChanged" event and notify event "selectedKeyChanged".
+ * @class Controls/_source/Adapter/SelectedKey
+ * @extends Controls/Control
+ * @control
+ * @public
+ * @author Золотова Э.Е.
+ */
 
-   function(Control, template) {
+var _private = {
+   getSelectedKeys: function(selectedKey, items) {
+      return selectedKey === null && !(items && items.getRecordById(null)) ? [] : [selectedKey];
+   },
 
-      'use strict';
+   dataLoadCallbackHandler: function(items) {
+      this._items = items;
+      if (this._options.dataLoadCallback) {
+         this._options.dataLoadCallback(items);
+      }
+   }
+};
 
-      var _private = {
-         getSelectedKeys: function(selectedKey, items) {
-            return selectedKey === null && !(items && items.getRecordById(null)) ? [] : [selectedKey];
-         },
+var Adapter = Control.extend({
 
-         dataLoadCallbackHandler: function(items) {
-            this._items = items;
-            if (this._options.dataLoadCallback) {
-               this._options.dataLoadCallback(items);
-            }
-         }
-      };
+   _template: template,
+   _selectedKeys: null,
+   _items: null,
 
-      var Adapter = Control.extend({
+   _beforeMount: function(options) {
+      this._dataLoadCallbackHandler = _private.dataLoadCallbackHandler.bind(this);
+      this._selectedKeys = _private.getSelectedKeys(options.selectedKey, this._items);
+   },
 
-         _template: template,
-         _selectedKeys: null,
-         _items: null,
+   _beforeUpdate: function(newOptions) {
+      if (this._options.selectedKey !== newOptions.selectedKey) {
+         this._selectedKeys = _private.getSelectedKeys(newOptions.selectedKey, this._items);
+      }
+   },
 
-         _beforeMount: function(options) {
-            this._dataLoadCallbackHandler = _private.dataLoadCallbackHandler.bind(this);
-            this._selectedKeys = _private.getSelectedKeys(options.selectedKey, this._items);
-         },
+   _selectedKeysChanged: function(event, keys) {
+      event.stopPropagation();
+      var selectedKey = keys.length ? keys[0] : null;
+      this._notify('selectedKeyChanged', [selectedKey]);
+   }
 
-         _beforeUpdate: function(newOptions) {
-            if (this._options.selectedKey !== newOptions.selectedKey) {
-               this._selectedKeys = _private.getSelectedKeys(newOptions.selectedKey, this._items);
-            }
-         },
+});
 
-         _selectedKeysChanged: function(event, keys) {
-            event.stopPropagation();
-            var selectedKey = keys.length ? keys[0] : null;
-            this._notify('selectedKeyChanged', [selectedKey]);
-         }
+Adapter._private = _private;
 
-      });
-
-      Adapter._private = _private;
-
-      return Adapter;
-   });
+export default Adapter;
