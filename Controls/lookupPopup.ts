d2a1268c280eb8e72449cecf0f6e60ed10490234
aa.malcev2 <aa.malcev2@tensor.ru>
2019-05-30 16:21:39 +0300

    Add module for export https://online.sbis.ru/opendoc.html?guid=601d648c-5973-496b-b984-311b6d87467b

--- Controls/lookupPopup.ts
+++ Controls/lookupPopup.ts
@@ -14,10 +14,12 @@ import Container = require("Controls/_lookupPopup/Container");
 import ListContainer = require("Controls/_lookupPopup/List/Container");
 import Controller = require("Controls/_lookupPopup/Controller");
 import Collection = require("Controls/_lookupPopup/SelectedCollection/Popup");
+import ListMemorySourceFilter = require('Controls/_lookupPopup/List/Utils/memorySourceFilter');
 
 export {
    Container,
    ListContainer,
    Controller,
-   Collection
+   Collection,
+   ListMemorySourceFilter
 }
