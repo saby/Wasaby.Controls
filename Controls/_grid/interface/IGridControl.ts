c8524d50bd6d710b874eab58066131dde62f96f7
va.kolesov <va.kolesov@tensor.ru>
2019-05-31 19:33:46 +0300

    https://online.sbis.ru/opendoc.html?guid=15797b0b-e5cb-4111-9e04-ec4cd54a0536

--- Controls/_grid/interface/IGridControl.ts
+++ Controls/_grid/interface/IGridControl.ts
@@ -87,7 +87,7 @@
  * @property {String} [width] Column width. Supported the value specified in pixels (for example, 4px) or percent (for example, 50%) and the value “auto”.
  * @property {String} [displayProperty] Name of the field that will shown in the column by default.
  * @property {String} [template] Template for cell rendering.
- * @property {String} [resultTemplate] Template for cell rendering in results row..
+ * @property {String} [resultTemplate] Template for cell rendering in results row. CSS class controls-Grid__header-cell_spacing_money sets the right indent for the content of the header cell to align by integers in money fields.
  * @property {GridCellAlign} [align] Horizontal cell content align.
  * @property {GridCellVAlign} [valign] Vertical cell content align.
  * @property {String} [stickyProperty] The name of the field used to sticking the column data.
