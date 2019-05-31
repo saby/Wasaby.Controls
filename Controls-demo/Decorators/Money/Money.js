81fe00849d0effb6551e86c9a16d51b65c291573
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2019-05-31 15:05:05 +0300

    https://online.sbis.ru/opendoc.html?guid=13021d90-f75a-4825-a660-ccdc7edabcc1

--- Controls-demo/Decorators/Money/Money.js
+++ Controls-demo/Decorators/Money/Money.js
@@ -38,7 +38,8 @@ define('Controls-demo/Decorators/Money/Money',
                value: '0.00',
                style: 'default',
                name: 'DecoratorMoney',
-               useGrouping: true
+               useGrouping: true,
+               displayEmptyDecimal: true
             };
             this._metaData = config[_private.CONTENT].properties['ws-config'].options;
          }
