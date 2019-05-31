c4c668d2b9e0d07c290cc7cac1c014a0fd090617
am.gerasimov <am.gerasimov@tensor.ru>
2019-05-31 13:45:57 +0300

    https://online.sbis.ru/opendoc.html?guid=7b73716a-fa1d-41ad-9b31-462ed8b77284

--- Controls/_interface/ISource.ts
+++ Controls/_interface/ISource.ts
@@ -16,6 +16,56 @@ export default interface ISource {
 }
 /**
  * @name Controls/_interface/ISource#source
+ * @cfg {Types/source:ICrud} Объект реализующий интерфейс {@link Types/source:ICrud}, необходимый для работы с источником данных.
+ * Более подробно об источниках данных вы можете почитать <a href='doc/platform/developmentapl/interface-development/data-sources/'>здесь</a>.
+ * @example
+ * В приведённом примере для контрола {@link Controls/list:View} в опцию source передаётся {@link Types/source:Memory} источник.
+ * Контрол получит данные из источника и выведет их.
+ *
+ * WML:
+ * <pre>
+ *    <Controls.list:View source="{{_source}}" keyProperty="key">
+ *        <ws:itemTemplate>
+ *            <ws:partial template="Controls/list:ItemTemplate">
+ *                <ws:contentTemplate>
+ *                    <span>{{contentTemplate.itemData.item.title}}</span>
+ *                </ws:contentTemplate>
+ *            </ws:partial>
+ *        </ws:itemTemplate>
+ *    </Controls.list:View>
+ * </pre>
+ *
+ * JS:
+ * <pre>
+ *     import {Memory} from "Types/source";
+ *
+ *     _source: null,
+ *     _beforeMount: function() {
+ *         this._source = new Memory({
+ *             idProperty: 'key',
+ *             data: [
+ *                {
+ *                   key: '1',
+ *                   title: 'Yaroslavl'
+ *                },
+ *                {
+ *                   key: '2',
+ *                   title: 'Moscow'
+ *                },
+ *                {
+ *                   key: '3',
+ *                   title: 'St-Petersburg'
+ *                }
+ *             ]
+ *          })
+ *     }
+ * </pre>
+ * @see https://wi.sbis.ru/docs/js/Types/source/ICrudPlus/
+ * @see https://wi.sbis.ru/docs/js/Types/source/ICrud/
+ * @see https://wi.sbis.ru/doc/platform/developmentapl/interface-development/data-sources/
+ */
+/*
+ * @name Controls/_interface/ISource#source
  * @cfg {Types/source:Base} Object that implements ISource interface for working with data.
  * @example
  * The list will be rendered data from _source
