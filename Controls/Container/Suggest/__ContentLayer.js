0f10f8ba38db75588ecdcde0543da22eb9a6e2ce
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-31 13:00:23 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

---
+++ Controls/Container/Suggest/__ContentLayer.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Container/Suggest/__ContentLayer', ['Controls/calendar'], function(Control) {
+   return Control.Utils;
+});
---
+++ Controls/Container/Suggest/__PopupLayer.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Container/Suggest/__PopupLayer', ['Controls/calendar'], function(Control) {
+   return Control.Utils;
+});
