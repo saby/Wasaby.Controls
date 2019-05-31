2234ec9b1f5fccbf989105780ce075ebdb5d0f4e
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-05-31 14:22:45 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/_list/getPrefetchSource.ts
+++ Controls/_list/getPrefetchSource.ts
@@ -1,55 +1,47 @@
-define('Controls/Container/Data/getPrefetchSource',
-   [
-      'Controls/source',
-      'Types/source',
-      'Core/Deferred'
-   ],
-   
-   function(scroll, sourceLib, Deferred) {
-      
-      'use strict';
-      function load(sourceOptions, data) {
-         if (data) {
-            return Promise.resolve(data);
-         }
+import {Controller} from 'Controls/source';
+import {PrefetchProxy} from 'Types/source';
 
-         var sourceController = new scroll.Controller({
-            source: sourceOptions.source,
-            navigation: sourceOptions.navigation,
-            idProperty: sourceOptions.keyProperty
-         });
+function load(sourceOptions, data) {
+   if (data) {
+      return Promise.resolve(data);
+   }
 
-         return sourceController.load(sourceOptions.filter, sourceOptions.sorting);
-      }
-
-      function getThenFunction(source) {
-         return function(result) {
-            var error;
-            var data;
-
-            if (result instanceof Error) {
-               error = result;
-               data = undefined;
-            } else {
-               error = undefined;
-               data = result;
-            }
-
-            return {
-               source: new sourceLib.PrefetchProxy({
-                  target: source,
-                  data: {
-                     query: error || data
-                  }
-               }),
-               data: data,
-               error: error
-            };
-         };
-      }
-
-      return function(sourceOptions, data) {
-         var thenFunction = getThenFunction(sourceOptions.source);
-         return load(sourceOptions, data).then(thenFunction, thenFunction);
-      };
+   var sourceController = new Controller({
+      source: sourceOptions.source,
+      navigation: sourceOptions.navigation,
+      idProperty: sourceOptions.keyProperty
    });
+
+   return sourceController.load(sourceOptions.filter, sourceOptions.sorting);
+}
+
+function getThenFunction(source) {
+   return function(result) {
+      var error;
+      var data;
+
+      if (result instanceof Error) {
+         error = result;
+         data = undefined;
+      } else {
+         error = undefined;
+         data = result;
+      }
+
+      return {
+         source: new PrefetchProxy({
+            target: source,
+            data: {
+               query: error || data
+            }
+         }),
+         data: data,
+         error: error
+      };
+   };
+}
+
+export default function(sourceOptions, data) {
+   var thenFunction = getThenFunction(sourceOptions.source);
+   return load(sourceOptions, data).then(thenFunction, thenFunction);
+}
