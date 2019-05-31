1803e4e89cbc5102cd71e67a8e85a25b34eb96f2
va.kolesov <va.kolesov@tensor.ru>
2019-05-31 18:30:37 +0300

    https://online.sbis.ru/opendoc.html?guid=15baebab-e7ab-4260-9c72-0634468c955d https://online.sbis.ru/opendoc.html?guid=d6e63719-5493-4b3c-93c4-1833e2225c59 https://online.sbis.ru/opendoc.html?guid=47c076ec-a165-481b-94cd-9d7fbbe1b2e7 https://online.sbis.ru/opendoc.html?guid=a252ed4d-6494-4e9a-a499-1b47e06e7b10 https://online.sbis.ru/opendoc.html?guid=04e23574-4bc0-4a82-b0d9-c6342b4c3c34 https://online.sbis.ru/opendoc.html?guid=3b92c3c6-c325-43d9-adfb-d04e973214f4

--- Controls/_list/interface/IList.ts
+++ Controls/_list/interface/IList.ts
@@ -89,8 +89,8 @@
  * @property {String} id Identifier of operation.
  * @property {String} title Operation name.
  * @property {String} icon Operation icon.
- * @property {Number} showType Location of operation.
- * @property {String} style Operation style.
+ * @property {Number} showType Location of operation. (1 - menu | 2 - toolbar and menu | 3 - toolbar).
+ * @property {String} style Operation style. (secondary | warning | danger | success).
  * @property {String} iconStyle Style of the action's icon. (secondary | warning | danger | success).
  * @property {Function} handler Operation handler.
  * @property {String} parent Key of the action's parent.
@@ -112,7 +112,7 @@
  */
 
 /**
- * @event Controls/_list/interface/IList#actionClick
+ * @event Controls/_list/interface/IList#actionClick Occurs when itemAction button is clicked.
  * @param {Core/vdom/Synchronizer/resources/SyntheticEvent} eventObject Descriptor of the event.
  * @param {ItemAction} action Object with configuration of the clicked action.
  * @param {Types/entity:Model} item Instance of the item whose action was clicked.
@@ -177,11 +177,32 @@
 /**
  * @name Controls/_list/interface/IList#itemsReadyCallback
  * @cfg {Function} Callback function that will be called when list data instance is ready.
+ * @example
+ * <pre class="brush:js">
+ * _myItemsReadyCallback = function(items) {
+ *    this._myItems = items;
+ * }
+ * ...
+ * deleteButtonClickHandler: function{
+ *    this._myItems.removeAt(0);
+ * }
+ * </pre>
  */
 
 /**
  * @name Controls/_list/interface/IList#dataLoadCallback
  * @cfg {Function} Callback function that will be called when list data loaded by source
+ * @remark
+ * dataLoadCallback takes to first argument the collection of loaded items.
+ * dataLoadCallback can be used for setting metadata or adjusting loaded items.
+ * @example
+ * <pre class="brush:js">
+ * _myDataLoadCallback = function(items) {
+ *    items.each(function(item){
+ *       item.set(field, value);
+ *    });
+ * }
+ * </pre>
  */
 
 /**
