51413ca9847f8f35e13aeefd62aaca9e6d4ba851
ea.rodionov1 <ea.rodionov1@tensor.ru>
2019-06-03 10:53:43 +0300

    https://online.sbis.ru/opendoc.html?guid=3047e9f4-c3c5-4938-8eae-41b939cc4629

--- tests/unit/List/Grid/GridViewModel.test.js
+++ tests/unit/List/Grid/GridViewModel.test.js
@@ -1223,7 +1223,7 @@ define(['Controls/grid', 'Core/core-merge', 'Types/collection', 'Types/entity', 
 
             assert.equal(
                 editingItemData.getEditingRowStyles(),
-                'display: -ms-grid; grid-template-columns: auto 1fr 123px 321px; grid-column: 1 / 5; grid-row: 3;'
+                'display: grid; display: -ms-grid; grid-template-columns: auto 1fr 123px 321px; grid-column: 1 / 5; grid-row: 3;'
             );
             assert.equal(groupItemData.gridGroupStyles, "grid-row: 3; -ms-grid-row: 3;");
          });
