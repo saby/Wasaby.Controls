4d5e0578abc060ab6331825b62672425c7441a80
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-06-03 10:43:12 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

---
+++ Controls/Container/Data/ContextOptions.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Container/Data/ContextOptions', ['Controls/context'], function(Control) {
+   return Control.ContextOptions;
+});
---
+++ Controls/Container/Search/SearchContextField.js
@@ -0,0 +1,4 @@
+// Created in 19.400
+define('Controls/Container/Search/SearchContextField', ['Controls/context'], function(Control) {
+   return Control.SearchContextField;
+});
