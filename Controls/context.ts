d534c385fdd47e0a392b4f6107ba4c8eedcb762c
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-06-03 10:36:15 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

---
+++ Controls/context.ts
@@ -0,0 +1,10 @@
+/**
+ * context library
+ * @library Controls/context
+ * @includes Path Controls/_breadcrumbs/Path
+ * @includes HeadingPath Controls/_breadcrumbs/HeadingPath
+ * @includes BreadCrumbsStyles Controls/_breadcrumbs/BreadCrumbsStyles
+ * @deprecated
+ * @author Kraynov D.
+ */
+
