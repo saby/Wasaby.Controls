6241f04bd8f70ff066e3fb5d73424600270d357d
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-06-03 15:24:11 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/Popup/Compatible/ManagerWrapper/Controller.ts
+++ Controls/Popup/Compatible/ManagerWrapper/Controller.ts
@@ -1,42 +1,41 @@
 /**
  * Created by as.krasilnikov on 20.11.2018.
  */
-define('Controls/Popup/Compatible/ManagerWrapper/Controller', [], function() {
-   return {
-      _managerWrapper: null,
-      _globalPopup: null,
-      registerManager: function(ManagerWrapper) {
-         this._managerWrapper = ManagerWrapper;
-      },
-      registerGlobalPopup: function(GlobalPopup) {
-         this._globalPopup = GlobalPopup;
-      },
-      getManagerWrapper: function() {
-         return this._managerWrapper;
-      },
-      getGlobalPopup: function() {
-         return this._globalPopup;
-      },
-      registerGlobalPopupOpeners: function(GlobalPopupOpeners) {
-         this._globalPopupOpeners = GlobalPopupOpeners;
-      },
-      getGlobalPopupOpeners: function() {
-         return this._globalPopupOpeners;
-      },
-      scrollHandler: function() {
-         if (this._managerWrapper) {
-            this._managerWrapper._scrollHandler();
-         }
-      },
-      registerListener: function(event, registerType, component, callback) {
-         if (this._managerWrapper) {
-            this._managerWrapper.registerListener(event, registerType, component, callback);
-         }
-      },
-      unregisterListener: function(event, registerType, component) {
-         if (this._managerWrapper) {
-            this._managerWrapper.unregisterListener(event, registerType, component);
-         }
+
+export default {
+   _managerWrapper: null,
+   _globalPopup: null,
+   registerManager: function(ManagerWrapper) {
+      this._managerWrapper = ManagerWrapper;
+   },
+   registerGlobalPopup: function(GlobalPopup) {
+      this._globalPopup = GlobalPopup;
+   },
+   getManagerWrapper: function() {
+      return this._managerWrapper;
+   },
+   getGlobalPopup: function() {
+      return this._globalPopup;
+   },
+   registerGlobalPopupOpeners: function(GlobalPopupOpeners) {
+      this._globalPopupOpeners = GlobalPopupOpeners;
+   },
+   getGlobalPopupOpeners: function() {
+      return this._globalPopupOpeners;
+   },
+   scrollHandler: function() {
+      if (this._managerWrapper) {
+         this._managerWrapper._scrollHandler();
       }
-   };
-});
+   },
+   registerListener: function(event, registerType, component, callback) {
+      if (this._managerWrapper) {
+         this._managerWrapper.registerListener(event, registerType, component, callback);
+      }
+   },
+   unregisterListener: function(event, registerType, component) {
+      if (this._managerWrapper) {
+         this._managerWrapper.unregisterListener(event, registerType, component);
+      }
+   }
+};
