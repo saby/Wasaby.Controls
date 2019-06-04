8652e8a2561016ca305445658dd2d3bf0f5606e1
ia.kapustin <ia.kapustin@tensor.ru>
2019-06-04 16:36:17 +0300

    https://online.sbis.ru/opendoc.html?guid=caf87c5d-c056-44f1-9912-8093d0a868de

--- Controls/_filterPopup/Panel/AdditionalParams/Styles.ts
+++ Controls/_filterPopup/Panel/AdditionalParams/Styles.ts
@@ -7,4 +7,6 @@
  * @css @height_AdditionalParams-item Item height.
  * @css @spacing_AdditionalParams-between-leftBorder-content Spacing between left border and content.
  * @css @spacing_AdditionalParams-between-columns Minimum spacing between column with items.
+ * @css @min-width_AdditionalParams-leftColumn Minimum width of the left column.
+ * @css @max-width_AdditionalParams-leftColumn Maximum width of the left column.
  */
