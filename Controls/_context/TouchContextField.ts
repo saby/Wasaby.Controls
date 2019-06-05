9c2b4f54fbf66bcb63f0299695cdac24462605b4
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-06-05 15:59:20 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/_context/TouchContextField.ts
+++ Controls/_context/TouchContextField.ts
@@ -1,19 +1,24 @@
-define('Controls/Context/TouchContextField', [
-   'Env/Env',
-   'Core/DataContext'
-], function(Env, DataContext) {
-   return DataContext.extend({
-      isTouch: null,
-      constructor: function(touch) {
-         // todo: https://online.sbis.ru/opendoc.html?guid=e277e8e0-8617-41c9-842b-5c7dcb116e2c
-         if (typeof touch === 'object') {
-            touch = Env.compatibility.touch;
-         }
-         this.isTouch = touch;
-      },
-      setIsTouch: function(touch) {
-         this.isTouch = touch;
-         this.updateConsumers();
+/**
+ * @class Controls/_context/TouchContextField
+ * @deprecated
+ */
+
+import DataContext = require('Core/DataContext');
+import {compatibility} from 'Env/Env';
+
+const Context = DataContext.extend({
+   isTouch: null,
+   constructor: function(touch) {
+      // todo: https://online.sbis.ru/opendoc.html?guid=e277e8e0-8617-41c9-842b-5c7dcb116e2c
+      if (typeof touch === 'object') {
+         touch = compatibility.touch;
       }
-   });
+      this.isTouch = touch;
+   },
+   setIsTouch: function(touch) {
+      this.isTouch = touch;
+      this.updateConsumers();
+   }
 });
+
+export default Context;
