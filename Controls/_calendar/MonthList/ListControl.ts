9e07e74da0931cd56707c2e096f351cc21c687ac
Розов М.А <ma.rozov@tensor.ru>
2019-06-05 21:51:20 +0300

    Revert "Merge branch '19.400/feature/monthlist' into 'rc-19.500'"
    This reverts merge request !75182

--- Controls/_calendar/MonthList/ListControl.ts
+++ Controls/_calendar/MonthList/ListControl.ts
@@ -1,9 +1,7 @@
 import {factory} from 'Types/chain';
-import {RecordSet} from 'Types/collection';
 import {Query} from 'Types/source';
 import {ListControl} from 'Controls/list';
 import tmpl = require('wml!Controls/_calendar/MonthList/ListControl');
-import monthListUtils from './Utils';
 import baseControlTmpl = require('wml!Controls/_list/ListControl/ListControl');
 
 /**
@@ -15,59 +13,22 @@ import baseControlTmpl = require('wml!Controls/_list/ListControl/ListControl');
  * @author Миронов А.Ю.
  */
 
-const MONTHS_IN_YEAR = 12;
-
 var _private = {
-    getQuery: function (items, viewMode) {
+    getQuery: function (items) {
         var length = factory(items).toArray().length,
-            startId = monthListUtils.idToDate(items.at(0).getId()),
-            endId = monthListUtils.idToDate(items.at(length - 1).getId()),
-            qDate = startId < endId ? startId : endId,
+            startId = items.at(0).getId(),
+            endId = items.at(length - 1).getId(),
             query = new Query();
 
-        if (viewMode === 'year') {
-            length *= MONTHS_IN_YEAR;
-        }
-
-        qDate.setMonth(qDate.getMonth() - 1);
-
-        return query.where({'id>=': monthListUtils.dateToId(qDate)}).limit(length);
+        return query.where({'id>=': (startId < endId ? startId : endId) - 1}).limit(length);
     },
 
     dataLoadCallback: function (self, items) {
         if (self._options.sourceExt) {
-            self._options.sourceExt.query(_private.getQuery(items, self._options.viewMode))
-                .addCallback(function(richItems) {
-                    const richData = _private.getDataForEnrich(richItems.getAll(), self._options.viewMode);
-                    self._children.baseControl.getViewModel().mergeItems(richData, {inject: true});
-                });
+            self._options.sourceExt.query(_private.getQuery(items)).addCallback(function (richItems) {
+                this._children.baseControl.getViewModel().mergeItems(richItems.getAll(), {inject: true});
+            }.bind(self));
         }
-    },
-
-    getDataForEnrich: function(richItems, viewMode) {
-        let
-            extData = {};
-        if (viewMode === 'year') {
-            factory(richItems).each(function(item, index) {
-                let year = item.getId().split("-")[0];
-                if (!extData[year]) {
-                    extData[year] = [item.get('extData')];
-                } else {
-                    extData[year].push(item.get('extData'));
-                }
-            });
-            richItems = new RecordSet({
-                rawData: Object.keys(extData).map(function(key) {
-                    const year = new Date(key, 0);
-                    return { id: monthListUtils.dateToId(year), date: year, extData: extData[key]};
-                })
-            });
-        } else {
-            factory(richItems).each(function(item, index) {
-                item.set('date', monthListUtils.idToDate(item.getId()));
-            });
-        }
-        return richItems;
     }
 };
 
