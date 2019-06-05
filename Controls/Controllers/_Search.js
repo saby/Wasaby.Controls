4331282788b4a330c2d07c858be3ef56022009d9
is.kudryavcev <is.kudryavcev@tensor.ru>
2019-06-05 17:18:47 +0300

    https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/Controllers/_Search.js
+++
@@ -1,4 +0,0 @@
-// Created in 19.400
-define('Controls/Controllers/_Search', ['Controls/search'], function(Control) {
-   return Control._Search;
-});
--- Controls/Date/Controllers/RangeSelectionController.js
+++
@@ -1,4 +0,0 @@
-// Created in 19.400
-define('Controls/Date/Controllers/RangeSelectionController', ['Controls/dateRange'], function(Control) {
-   return Control.RangeSelectionController;
-});
