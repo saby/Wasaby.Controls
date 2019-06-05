b3b1368619ddf8649c258e5536a63cc57230f760
aa.malcev2 <aa.malcev2@tensor.ru>
2019-06-05 10:25:23 +0300

    Remove aliases used in Engine. https://online.sbis.ru/opendoc.html?guid=dda59811-9e0d-492d-86e9-0e645a592323

--- Controls/Decorator/Markup/resolvers/linkDecorate.js
+++
@@ -1,7 +0,0 @@
-// Required by RichEditor/_base/Editor
-define('Controls/Decorator/Markup/resolvers/linkDecorate',
-   ['Controls/decorator'],
-   function(Control) {
-      'use strict';
-      return Control.linkDecorate;
-   });
