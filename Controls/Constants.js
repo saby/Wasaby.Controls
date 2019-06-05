3647c62fc06080b063a0126700e6b683e3a7d874
va.kolesov <va.kolesov@tensor.ru>
2019-06-05 11:38:49 +0300

    https://online.sbis.ru/opendoc.html?guid=0ab8a5af-5124-4ff3-9e99-c060d9caa337 https://online.sbis.ru/opendoc.html?guid=1f28f6a9-9b5a-44a3-b904-cc866d2a8ebc

--- Controls/Constants.js
+++ Controls/Constants.js
@@ -1,21 +1,22 @@
 define('Controls/Constants', [], function() {
    'use strict';
-   var
-      /**
-       * Constants
-       * <ul>
-       *    <li>
-       *       view.hiddenGropup - Constant for determining item in the hiddenGroup in the {@link Controls/interface/IGrouped#groupingKeyCallback gropingKeyCallback}
-       *    </li>
-       *    <li>
-       *       editing.CANCEL - Constant that can be returned in {@link Controls/interface/IEditableList#beforeBeginEdit beforeBeginEdit} to cancel egiting
-       *    </li>
-       * </ul>
-       *
-       * @class Controls/Constants
-       * @public
-       */
+   
+   /**
+    * Constants
+    * <ul>
+    *    <li>
+    *       view.hiddenGropup - Constant for determining item in the hiddenGroup in the {@link Controls/interface/IGrouped#groupingKeyCallback gropingKeyCallback}
+    *    </li>
+    *    <li>
+    *       editing.CANCEL - Constant that can be returned in {@link Controls/interface/IEditableList#beforeBeginEdit beforeBeginEdit} to cancel egiting
+    *    </li>
+    * </ul>
+    *
+    * @class Controls/Constants
+    * @public
+    */
 
+   var
       constants = {
          view: {
             hiddenGroup: 'CONTROLS_HIDDEN_GROUP'
