72da731be315354a8541b04a5788a3ff4e90d610
as.avramenko <as.avramenko@tensor.ru>
2019-06-07 12:01:59 +0300

    https://online.sbis.ru/opendoc.html?guid=7087411c-080e-4942-9f51-31e19a179d57

--- Controls/_scroll/Scroll.ts
+++ Controls/_scroll/Scroll.ts
@@ -552,7 +552,14 @@ var
           * Otherwise we can accidentally scroll a wrong element.
           */
          e.stopPropagation();
-         this._savedScrollPosition = this._children.content.scrollHeight - this._children.content.scrollTop;
+         function getScrollTop(element: Element): number {
+            const scrollTop = element.scrollTop;
+            if (Env.detection.isMobileIOS && scrollTop < 0) {
+               return 0;
+            }
+            return scrollTop;
+         }
+         this._savedScrollPosition = this._children.content.scrollHeight - getScrollTop(this._children.content);
       },
 
       _restoreScrollPosition: function(e) {
