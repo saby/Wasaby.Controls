2e47f3070e641896fa50ff54381387e3972cadc9
ps.kolesova <ps.kolesova@tensor.ru>
2019-06-10 10:37:28 +0300

    https://online.sbis.ru/opendoc.html?guid=3a58e252-cfac-4fa0-8cdf-da9ca37c441e

--- Controls/_heading/Heading.ts
+++ Controls/_heading/Heading.ts
@@ -1,7 +1,15 @@
-import Control = require('Core/Control');
+import{Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import template = require('wml!Controls/_heading/Heading/Heading');
-import entity = require('Types/entity');
+import {descriptor as EntityDescriptor} from 'Types/entity';
+import {ITooltip, ITooltipOptions, ICaption, ICaptionOptions} from 'Controls/interface';
+import {ICheckable} from "../_toggle/interface/ICheckable";
 
+export interface IHeadingOptions extends IControlOptions, ICaptionOptions, ITooltipOptions {
+   fontSize?: string;
+   fontColorStyle?: string;
+   size?: string;
+   style?: string;
+}
 
    /**
     * Heading with support different display styles and sizes. Can be used independently or as part of complex headings(you can see it in <a href="/materials/demo-ws4-header-separator">Demo-example</a>) consisting of a <a href="/docs/js/Controls/_heading/Counter/?v=3.18.500">counter</a>, a <a href="/docs/js/Controls/_heading/Separator/?v=3.18.500">header-separator</a> and a <a href="/docs/js/Controls/Button/Separator/?v=3.18.500">button-separator</a>.
@@ -31,6 +39,22 @@ import entity = require('Types/entity');
     * @default m
     */
 
+/**
+ * @name Controls/_heading/Heading#fontSize
+ * @cfg {String} Heading font-size.
+ * @variant xs Extra small text size.
+ * @variant s Small text size.
+ * @variant m Medium text size.
+ * @variant l Large text size.
+ * @variant xl Extra large text size.
+ * @variant 2xl 2*Extra large text size.
+ * @variant 3xl 3*Extra large text size.
+ * @variant 4xl 4*Extra large text size.
+ * @variant 5xl 5*Extra large text size.
+ * @default m
+ */
+
+
    /**
     * @name Controls/_heading/Heading#style
     * @cfg {String} Heading display style.
@@ -40,35 +64,33 @@ import entity = require('Types/entity');
     * @default primary
     */
 
-   var Header = Control.extend({
-      _template: template
-   });
+   class Header extends Control<IHeadingOptions> implements ITooltip, ICaption {
+      protected _template: TemplateFunction = template;
+      protected _theme: string[] = ['Controls/heading'];
 
-   Header.getOptionTypes =  function getOptionTypes() {
-      return {
-         caption: entity.descriptor(String),
-         style: entity.descriptor(String).oneOf([
-            'secondary',
-            'primary',
-            'info'
-         ]),
-         size: entity.descriptor(String).oneOf([
-            'xl',
-            'l',
-            'm',
-            's'
-         ])
-      };
-   };
-   Header._theme = ['Controls/heading'];
+      static getDefaultOptions(): object {
+         return {
+            style: 'secondary',
+            size: 'm',
+            theme: 'default'
+         };
+      }
+      static getOptionTypes(): object {
+         return {
+            caption: EntityDescriptor(String),
+            style: EntityDescriptor(String).oneOf([
+               'secondary',
+               'primary',
+               'info'
+            ]),
+            size: EntityDescriptor(String).oneOf([
+               'xl',
+               'l',
+               'm',
+               's'
+            ])
+         };
+      }
+   }
 
-   Header.getDefaultOptions = function() {
-      return {
-         style: 'secondary',
-         size: 'm',
-         theme: 'default'
-      };
-   };
-
-   export = Header;
-
+export default Header;
