be0dcdd95cc0f27420f704072998ed5433b47e9a
nv.belotelov <nv.belotelov@tensor.ru>
2019-06-12 15:56:16 +0300

    https://online.sbis.ru/opendoc.html?guid=a0244f06-6f2d-457a-b3cb-f623c3889524

--- Controls-demo/ContainerTheme.js
+++ Controls-demo/ContainerTheme.js
@@ -1,13 +1,14 @@
-/**
- * Created by kraynovdo on 25.01.2018.
- */
 define('Controls-demo/ContainerTheme', [
    'Core/Control',
    'wml!Controls-demo/ContainerTheme'
-], function (BaseControl,
-             template
+], function(BaseControl,
+            template
 ) {
    'use strict';
+
+   /**
+    * This control trying to get theme option from url and pass it to demo
+    */
    var ModuleClass = BaseControl.extend(
       {
          _template: template
