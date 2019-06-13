99fcb79a92c4a93a6deacbbc33236554fed6ce90
am.gerasimov <am.gerasimov@tensor.ru>
2019-06-13 13:11:44 +0300

    https://online.sbis.ru/opendoc.html?guid=54b65de4-1492-40c8-892a-85ab3babe5b8

--- Controls/_search/Misspell.ts
+++ Controls/_search/Misspell.ts
@@ -1,15 +1,28 @@
 import Control = require('Core/Control');
 import template = require('wml!Controls/_search/Misspell');
-import 'css!theme?Controls/search';
-/*
-* search misspell
-* @class Controls/_search/Misspell
-* @extends Core/Control
-* @control
-* @public
-* @author Kraynov D.
-*/
-export = Control.extend({
-   _template: template
-});
 
+/**
+  * Контрол, отображающий подсказку, если в запросе при поиске найдена и исправлена опечатка.
+  * @class Controls/_search/Misspell
+  * @mixes Controls/_interface/ICaption
+  * @extends Core/Control
+  * @control
+  * @public
+  * @author Kraynov D.
+  */
+/*
+ * //TODO KONGO A control that displays a tooltip if misspell was in search text was found.
+ * @class Controls/_search/Misspell
+ * @mixes Controls/_interface/ICaption
+ * @extends Core/Control
+ * @control
+ * @public
+ * @author Kraynov D.
+ */
+
+class Misspell extends Control {
+   private _template: Function = template;
+   static _theme: Array<string> = ['Controls/search'];
+}
+
+export default Misspell;
