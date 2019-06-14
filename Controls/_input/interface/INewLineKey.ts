80c68bc0d3c883514560c5feac62dcf1fc5f6555
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2019-06-14 15:12:24 +0300

    https://online.sbis.ru/opendoc.html?guid=b83b65d1-4b7e-4231-8475-d6e75a4971ac https://online.sbis.ru/opendoc.html?guid=8c5cc12d-ea67-47d3-8aa9-6d10b39e8395 https://online.sbis.ru/opendoc.html?guid=e71e8786-c5e1-469b-81d2-51161ef02d28 https://online.sbis.ru/opendoc.html?guid=cc55cbeb-7cfe-4f90-849e-1a8917d04f5c https://online.sbis.ru/opendoc.html?guid=f8eba1dd-9cc7-4794-a5c5-3c70f7a09f93

--- Controls/_input/interface/INewLineKey.ts
+++ Controls/_input/interface/INewLineKey.ts
@@ -8,7 +8,7 @@ interface INewLineKey {
     readonly _options: {
         /**
          * @name Controls/_input/interface/INewLineKey#newLineKey
-         * @cfg {string} The behavior of creating a new line.
+         * @cfg {Enum} The behavior of creating a new line.
          * @variant enter When user presses Enter.
          * @variant ctrlEnter When user presses Ctrl + Enter.
          * @default enter
