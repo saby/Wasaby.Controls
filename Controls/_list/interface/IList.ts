f7092a7b068cf3d445422377303cd607952a4a15
aa.artemeva1 <aa.artemeva1@tensor.ru>
2019-06-21 17:40:09 +0300

    Добавлено описание

--- Controls/_list/interface/IList.ts
+++ Controls/_list/interface/IList.ts
@@ -338,11 +338,13 @@
 
 /**
  * @event  Controls/_list/interface/IList#drawItems Происходит при отрисовке очередного набора данных.
+ * @param {Env/Event:Object} eventObject Дескриптор события.
  * <a href="/materials/demo-ws4-list-base">Example</a>.
  */
 
 /*
  * @event  Controls/_list/interface/IList#drawItems Occurs when the next batch of data is drawn.
+ * @param {Env/Event:Object} eventObject The event descriptor.
  * <a href="/materials/demo-ws4-list-base">Example</a>.
  */
 
