4a416bd643adad461b7fcff32449a18c21ef3326
ia.kapustin <ia.kapustin@tensor.ru>
2019-06-24 13:55:00 +0300

    https://online.sbis.ru/opendoc.html?guid=ca2a534d-4ed6-4a52-8c2f-6e046751eec3

--- Controls/_filterPopup/Panel/Lookup.ts
+++ Controls/_filterPopup/Panel/Lookup.ts
@@ -9,18 +9,21 @@ import 'css!theme?Controls/filterPopup';
     * Here you can see <a href="/materials/demo-ws4-engine-selector-lookup">demo-example</a>.
     * @class Controls/_filterPopup/Panel/Lookup
     * @extends Core/Control
+    * @mixes Controls/interface/ILookup
     * @mixes Controls/interface/ISelectedCollection
     * @mixes Controls/interface/ISelectorDialog
+    * @mixes Controls/interface/ISuggest
     * @mixes Controls/interface/ISearch
     * @mixes Controls/_interface/ISource
-    * @mixes Controls/interface/IItemTemplate
     * @mixes Controls/interface/IFilter
-    * @mixes Controls/interface/ISuggest
-    * @mixes Controls/interface/ILookup
     * @mixes Controls/interface/INavigation
     * @mixes Controls/_interface/IMultiSelectable
     * @mixes Controls/interface/IInputPlaceholder
+    * @mixes Controls/interface/IInputBase
     * @mixes Controls/interface/IInputText
+    * @mixes Controls/interface/IInputField
+    * @mixes Controls/interface/IInputTag
+    * @mixes Controls/interface/ISelectableInput
     * @mixes Controls/_lookup/Lookup/LookupStyles
     * @control
     * @public
