2153682c3b0d3e43ea6526120af95f89ce1d3dfa
aa.artemeva1 <aa.artemeva1@tensor.ru>
2019-06-25 11:26:32 +0300

    Доработка по замечаниям

--- Controls/_grid/interface/IGridControl.ts
+++ Controls/_grid/interface/IGridControl.ts
@@ -22,7 +22,7 @@
  * Установите ladderProperties отобразите шаблон элемента через ladderWrapper:
  * <pre>
  *    <div class="demoGrid">
- *       <Controls.Grid
+ *       <Controls.grid:View
  *          ...
  *          ladderProperties="{{ ['date'] }}">
  *          <ws:columns>
@@ -42,7 +42,7 @@
  *                </ws:Object>
  *             </ws:Array>
  *          </ws:columns>
- *       </Controls.Grid>
+ *       </Controls.grid:View>
  *    </div>
  * </pre>
  */
@@ -55,7 +55,7 @@
  * Set ladderProperties and render item template through the ladderWrapper:
  * <pre>
  *    <div class="demoGrid">
- *       <Controls.Grid
+ *       <Controls.grid:View
  *          ...
  *          ladderProperties="{{ ['date'] }}">
  *          <ws:columns>
@@ -75,16 +75,16 @@
  *                </ws:Object>
  *             </ws:Array>
  *          </ws:columns>
- *       </Controls.Grid>
+ *       </Controls.grid:View>
  *    </div>
  * </pre>
  */
 
 /**
  * @typedef {String} GridCellAlign
- * @variant left Выровнять содержимое ячейки по левой стороне.
+ * @variant left Выровнять содержимое ячейки по левому краю.
  * @variant center Выровнять содержимое ячейки по центру.
- * @variant right Выровнять содержимое ячейки по правой стороне.
+ * @variant right Выровнять содержимое ячейки по правому краю.
  */
 
 /*
@@ -96,9 +96,9 @@
 
 /**
  * @typedef {String} GridCellVAlign
- * @variant top Выровнять содержимое ячейки по верхней стороне.
+ * @variant top Выровнять содержимое ячейки по верхнему краю.
  * @variant center Выровнять содержимое ячейки по центру.
- * @variant bottom Выровнять содержимое ячейки по нижней стороне.
+ * @variant bottom Выровнять содержимое ячейки по нижнему краю.
  */
 
 /*
@@ -174,7 +174,7 @@
  * @property {String} [resultTemplate] Шаблон рендеринга ячеек в строке итогов. CSS-класс controls-Grid__header-cell_spacing_money задает правый отступ для заголовка ячейки для выравнивания по целым числам в денежных полях.
  * @property {GridCellAlign} [align] Выравнивание содержимого ячейки по горизонтали.
  * @property {GridCellVAlign} [valign] Выравнивание содержимого ячейки по вертикали.
- * @property {String} [stickyProperty] Имя поля, используемого для прилипания данных столбца.
+ * @property {String} [stickyProperty] Опция, которая используется для настройки прилипания столбца к верхней границе таблицы.
  * @property {TextOverflow} [textOverflow] Определяет параметры видимости текста в блоке, если текст целиком не помещается в заданную область.
  */
 
@@ -254,7 +254,7 @@
 
 /**
  * @name Controls/_grid/interface/IGridControl#stickyHeader
- * @cfg {Boolean} Устанавливает заголовок таблицы.
+ * @cfg {Boolean} Закрепляет заголовок таблицы.
  * <a href="/materials/demo-ws4-grid-sticky">Example</a>
  * @default true
  */
