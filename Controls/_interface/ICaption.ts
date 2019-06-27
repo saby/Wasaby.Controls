4101ada8cbf2b5c3175dd52ab103c6494be908bd
aa.artemeva1 <aa.artemeva1@tensor.ru>
2019-06-27 15:37:09 +0300

    Переведены файлы группы Кнопки и Переключатели

--- Controls/_interface/ICaption.ts
+++ Controls/_interface/ICaption.ts
@@ -4,6 +4,14 @@ export interface ICaptionOptions {
 }
 
 /**
+ * Текст заголовка.
+ *
+ * @interface Controls/_interface/ICaption
+ * @public
+ * @author Михайловский Д.С.
+ */
+
+/*
  * Caption text.
  *
  * @interface Controls/_interface/ICaption
@@ -14,6 +22,27 @@ export default interface ICaption {
    readonly '[Controls/_interface/ICaption]': boolean;
 }
 /**
+ * @name Controls/_interface/ICaption#caption
+ * @cfg {String} Определяет текст заголовка контрола.
+ * @remark Вы можете передать разметку в заголовок.
+ * @example
+ * Контрол с заголовком 'Dialog'.
+ * <pre>
+ *    <ControlsDirectory.Control caption="Dialog"/>
+ * </pre>
+ * Контрол имеет заголовок с разметкой.
+ * <pre>
+ *    <ControlsDirectory.Control caption="captionTemplate"/>
+ * </pre>
+ * captionTemplate
+ * <pre>
+ *    <span class='customDialog'>
+ *       Dialog
+ *    </span>
+ * </pre>
+ */
+
+/*
  * @name Controls/_interface/ICaption#caption
  * @cfg {String} Control caption text.
  * @remark You can submit the markup to the caption.
--- Controls/_toggle/interface/ICheckable.ts
+++ Controls/_toggle/interface/ICheckable.ts
@@ -3,6 +3,14 @@ export interface ICheckableOptions {
 }
 
 /**
+ * Интерфейс переключателя.
+ *
+ * @interface Controls/_toggle/interface/ICheckable
+ * @public
+ * @author Михайловский Д.С.
+ */
+
+/*
  * Interface for 2-position control.
  *
  * @interface Controls/_toggle/interface/ICheckable
@@ -14,9 +22,22 @@ export interface ICheckable {
 }
 /**
  * @name Controls/_toggle/interface/ICheckable#value
+ * @cfg {boolean} Текущее состояние.
+ */
+
+/*
+ * @name Controls/_toggle/interface/ICheckable#value
  * @cfg {boolean} Current state.
  */
+
 /**
+ * @event Controls/_toggle/interface/ICheckable#valueChanged Происходит при изменении значения.
+ * @param {Env/Event:Object} eventObject Дескриптор события.
+ * @param {boolean} value Новое значение.
+ */
+
+/*
  * @event Controls/_toggle/interface/ICheckable#valueChanged Occurs when value changes.
+ * @param {Env/Event:Object} eventObject The event descriptor.
  * @param {boolean} value New value.
  */
