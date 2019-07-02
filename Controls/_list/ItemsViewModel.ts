da1e2a5e36e846e0f27ac1dc9c9ed4a6219f0fb4
as.suhoruchkin <as.suhoruchkin@tensor.ru>
2019-07-02 19:06:36 +0300

    https://online.sbis.ru/opendoc.html?guid=5938549e-7bb6-4361-b1ef-633a5f3cbf2b

--- Controls/_list/ItemsViewModel.ts
+++ Controls/_list/ItemsViewModel.ts
@@ -363,20 +363,8 @@ var ItemsViewModel = BaseViewModel.extend({
     },
 
     appendItems: function(items) {
+        this._items.setMetaData(items.getMetaData());
         this._items.append(items);
-
-        // Тут нужно обновлять metaData, но полностью обновлять её в 19.412 опасно, т.к. та metaData, которая
-        // приходит с сервера может по формату отличаться от той, которая сейчас находится в рекордсете.
-        // В 19.412 обновим только more, а в 19.510 будем менять metaData целиком.
-        const metaData = {...this._items.getMetaData()};
-        const newMetaData = items.getMetaData();
-        if (typeof metaData.more === 'boolean') {
-            metaData.more = !!newMetaData.more;
-        } else if (metaData.more instanceof Object) {
-            metaData.more.after = !!newMetaData.more;
-        }
-
-        this._items.setMetaData(metaData);
     },
 
     mergeItems: function(items, options) {
