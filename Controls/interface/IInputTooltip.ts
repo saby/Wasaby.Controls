67d24d41fa814e1d705e5517ed9ec34b401e289b
aa.artemeva1 <aa.artemeva1@tensor.ru>
2019-07-03 14:43:17 +0300

    Переведены файлы группы Lookup/Selector

--- Controls/interface/IInputTooltip.ts
+++ Controls/interface/IInputTooltip.ts
@@ -1,4 +1,13 @@
 /**
+ * Интерфейс для ввода текста всплывающей подсказки.
+ *
+ * @interface Controls/interface/IInputTooltip
+ *
+ * @public
+ * @author Красильников А.С.
+ */
+
+/*
  * Interface for input tooltip
  *
  * @interface Controls/interface/IInputTooltip
@@ -9,6 +18,18 @@
 interface IInputTooltip {
     readonly _options: {
         /**
+         * @name Controls/interface/IInputTooltip#tooltip
+         * @cfg {String} Текст всплывающей подсказки, отображаемой при наведении указателя мыши на элемент.
+         * @remark
+         * Атрибут "Title" добавляется в корневой узел контрола и всплывающая подсказка браузера по умолчанию отображается при наведении курсора.
+         * @example
+         * В этом примере при наведении курсора мыши на поле появится подсказка "Enter your name".
+         * <pre>
+         *    <Controls.input:Text tooltip="Enter your name"/>
+         * </pre>
+         */
+
+        /*
          * @name Controls/interface/IInputTooltip#tooltip
          * @cfg {String} Text of the tooltip shown when the control is hovered over.
          * @remark
