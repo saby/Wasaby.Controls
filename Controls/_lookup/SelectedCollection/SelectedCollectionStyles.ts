23cba87770f7e750521d118aa2c2fba0a9618f35
ia.kapustin <ia.kapustin@tensor.ru>
2019-07-10 14:09:45 +0300

    Переведены алиасы для SelectedCollection

--- Controls/_lookup/SelectedCollection/SelectedCollectionStyles.ts
+++ Controls/_lookup/SelectedCollection/SelectedCollectionStyles.ts
@@ -3,66 +3,54 @@
  * @public
  * @author Капустин И.А.
  *
- * @css @color_SelectedCollection-item_accent Accent item color.
- * @css @color_SelectedCollection-item_primary Primary item color.
- * @css @background_SelectedCollection-counter
- * @css @font-size_SelectedCollection-counter
- * @ccs @width_SelectedCollection-counter-shadow
- * @css @color_SelectedCollection-counter Counter color.
- * @css @color_SelectedCollection-counter_hover
- * @css @color_SelectedCollection-counter_active
- * @css @color_SelectedCollection-cross Cross color.
- * @css @icon-size_SelectedCollection-cross Icon cross size.
- * @css @line-height_SelectedCollection-item SelectedCollection items line height.
- * @css @font-size_SelectedCollection-size_m Font size of item with size m.
- * @css @font-size_SelectedCollection-size_l Font size of item with size l.
- * @css @font-size_SelectedCollection-size_xl Font size of item with size xl.
- * @css @font-size_SelectedCollection-size_2xl Font size of item with size 2xl.
- * @css @font-size_SelectedCollection-size_3xl Font size of item with size 3xl.
- * @css @spacing_SelectedCollection-between-items Spacing between items.
- * @css @spacing_SelectedCollection-between-counter-items Spacing between counter and items.
- * @css @spacing_SelectedCollection-between-counter-ellipsis Spacing between counter and ellipsis.
- * @css @spacing_SelectedCollection-between-infoBoxContent-borderLeft Outdent to the left of the edge of InfoBox content.
- * @css @spacing_SelectedCollection-between-infoBoxContent-borderRight Outdent to the right of the edge of InfoBox content.
- * @css @spacing_SelectedCollection-between-infoBoxContent-borderTop Outdent to the top of the edge of InfoBox content.
- * @css @spacing_SelectedCollection-between-infoBoxContent-borderBottom Outdent to the bottom of the edge of InfoBox content.
- * @css @size_SelectedCollection-infoBoxContent-shadow Shadow size around InfoBox content.
- * @css @color_SelectedCollection-infoBoxContent-shadow Shadow color around InfoBox content.
- * @css @size_SelectedCollection-infoBoxContent-border Sizes of borders InfoBox content.
- * @css @color-border_SelectedCollection-infoBoxContent Color of borders InfoBox content.
- * @css @background-color_SelectedCollection-infoBoxContent Background color of InfoBox content.
+ * @css @color_SelectedCollection-item_accent Цвет элемента для стиля "accent".
+ * @css @color_SelectedCollection-item_primary Цвет элемента для стиля "primary".
+ * @css @background_SelectedCollection-counter Фон счетчика.
+ * @css @font-size_SelectedCollection-counter Размер текста счетчика.
+ * @ccs @width_SelectedCollection-counter-shadow Ширина тени счетчика.
+ * @css @color_SelectedCollection-counter Цвет счетчика.
+ * @css @color_SelectedCollection-counter_hover Цвет счетчика при наведении.
+ * @css @color_SelectedCollection-counter_active Цвет счетчика при фокусе.
+ * @css @color_SelectedCollection-cross Цвет крестика удаления записи.
+ * @css @icon-size_SelectedCollection-cross Размер крестика удаления записи.
+ * @css @line-height_SelectedCollection-item Высота строки выбранной коллекции.
+ * @css @font-size_SelectedCollection-size_m Размер текса элемента для значения "m".
+ * @css @font-size_SelectedCollection-size_l Размер текса элемента для значении "l".
+ * @css @font-size_SelectedCollection-size_xl Размер текса элемента для значении "xl".
+ * @css @font-size_SelectedCollection-size_2xl Размер текса элемента для значении "2xl".
+ * @css @font-size_SelectedCollection-size_3xl Размер текса элемента для значении "3xl".
+ * @css @spacing_SelectedCollection-between-items Рассстояние между элементами коллекции.
+ * @css @spacing_SelectedCollection-between-counter-items Расстояние между счетчиком и коллекцией.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderLeft Отступ слева от края содержимого всплывающего окна со всей выбранной коллекцией.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderRight Отступ справа от края содержимого всплывающего окна.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderTop Отступ сверху от края содержимого всплывающего окна.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderBottom Отступ снизу от края содержимого всплывающего окна.
  */
 /*
  * @mixin Controls/_lookup/SelectedCollection/SelectedCollectionStyles
  * @public
  * @author Kapustin I.A.
  *
- * @css @color_SelectedCollection-item_accent Accent item color.
- * @css @color_SelectedCollection-item_primary Primary item color.
- * @css @background_SelectedCollection-counter
- * @css @font-size_SelectedCollection-counter
- * @ccs @width_SelectedCollection-counter-shadow
+ * @css @color_SelectedCollection-item_accent The element color for the "accent" style.
+ * @css @color_SelectedCollection-item_primary The element color for the "primary" style.
+ * @css @background_SelectedCollection-counter Counter background.
+ * @css @font-size_SelectedCollection-counter The text size of the counter.
+ * @ccs @width_SelectedCollection-counter-shadow The width of the shadow of the counter.
  * @css @color_SelectedCollection-counter Counter color.
- * @css @color_SelectedCollection-counter_hover
- * @css @color_SelectedCollection-counter_active
- * @css @color_SelectedCollection-cross Cross color.
- * @css @icon-size_SelectedCollection-cross Icon cross size.
- * @css @line-height_SelectedCollection-item SelectedCollection items line height.
- * @css @font-size_SelectedCollection-size_m Font size of item with size m.
- * @css @font-size_SelectedCollection-size_l Font size of item with size l.
- * @css @font-size_SelectedCollection-size_xl Font size of item with size xl.
- * @css @font-size_SelectedCollection-size_2xl Font size of item with size 2xl.
- * @css @font-size_SelectedCollection-size_3xl Font size of item with size 3xl.
+ * @css @color_SelectedCollection-counter_hover The color of the counter when hover.
+ * @css @color_SelectedCollection-counter_active The color of the counter when focus.
+ * @css @color_SelectedCollection-cross The color of the delete mark.
+ * @css @icon-size_SelectedCollection-cross The size of the delete mark.
+ * @css @line-height_SelectedCollection-item The line height of the selected collection.
+ * @css @font-size_SelectedCollection-size_m The font size of the item for the value "m".
+ * @css @font-size_SelectedCollection-size_l The font size of the item for the value "l".
+ * @css @font-size_SelectedCollection-size_xl The font size of the item for the value "xl".
+ * @css @font-size_SelectedCollection-size_2xl The font size of the item for the value "2xl".
+ * @css @font-size_SelectedCollection-size_3xl The font size of the item for the value "3xl".
  * @css @spacing_SelectedCollection-between-items Spacing between items.
  * @css @spacing_SelectedCollection-between-counter-items Spacing between counter and items.
- * @css @spacing_SelectedCollection-between-counter-ellipsis Spacing between counter and ellipsis.
  * @css @spacing_SelectedCollection-between-infoBoxContent-borderLeft Outdent to the left of the edge of InfoBox content.
  * @css @spacing_SelectedCollection-between-infoBoxContent-borderRight Outdent to the right of the edge of InfoBox content.
  * @css @spacing_SelectedCollection-between-infoBoxContent-borderTop Outdent to the top of the edge of InfoBox content.
  * @css @spacing_SelectedCollection-between-infoBoxContent-borderBottom Outdent to the bottom of the edge of InfoBox content.
- * @css @size_SelectedCollection-infoBoxContent-shadow Shadow size around InfoBox content.
- * @css @color_SelectedCollection-infoBoxContent-shadow Shadow color around InfoBox content.
- * @css @size_SelectedCollection-infoBoxContent-border Sizes of borders InfoBox content.
- * @css @color-border_SelectedCollection-infoBoxContent Color of borders InfoBox content.
- * @css @background-color_SelectedCollection-infoBoxContent Background color of InfoBox content.
  */
