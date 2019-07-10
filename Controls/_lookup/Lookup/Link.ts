610ca80f49c33cca0205a59b9dc18ef2952f9602
ia.kapustin <ia.kapustin@tensor.ru>
2019-07-10 11:50:34 +0300

    Русфицирована часть алиасов
    # Conflicts:
    #	Controls/_lookup/Lookup/Link.ts

--- Controls/_lookup/Lookup/Link.ts
+++ Controls/_lookup/Lookup/Link.ts
@@ -5,44 +5,43 @@ import Control = require('Core/Control');
 import template = require('wml!Controls/_lookup/Lookup/Link/LookUp_Link');
 import 'css!theme?Controls/lookup';
 
-   /**
-    * Кнопка-ссылка для использования в Selector/Lookup.
-    *
-    * @class Controls/_lookup/Lookup/Link
-    * @extends Core/Control
-    * @mixes Controls/_interface/ICaption
-    * @mixes Controls/_lookup/Lookup/Link/LookupLinkStyles
-    * @control
-    * @public
-    * @author Капустин И.А.
-    */
+/**
+ * Кнопка-ссылка для использования в Selector/Lookup
+ *
+ * @class Controls/_lookup/Lookup/Link
+ * @extends Core/Control
+ * @mixes Controls/_interface/ICaption
+ * @mixes Controls/_lookup/Lookup/Link/LookupLinkStyles
+ * @control
+ * @public
+ * @author Капустин И.А.
+ */
+/*
+ * Link for use in Selector/Lookup
+ *
+ * @class Controls/_lookup/Lookup/Link
+ * @extends Core/Control
+ * @mixes Controls/_interface/ICaption
+ * @mixes Controls/_lookup/Lookup/Link/LookupLinkStyles
+ * @control
+ * @public
+ * @author Kapustin I.A.
+ */
 
-   /*
-    * Link for use in Selector/Lookup
-    *
-    * @class Controls/_lookup/Lookup/Link
-    * @extends Core/Control
-    * @mixes Controls/_interface/ICaption
-    * @mixes Controls/_lookup/Lookup/Link/LookupLinkStyles
-    * @control
-    * @public
-    * @author Капустин И.А.
-    */
+const Link = Control.extend({
+   _template: template,
 
-   const Link = Control.extend({
-      _template: template,
-
-      _keyUpHandler: function(e) {
-         if (e.nativeEvent.keyCode === 13 && !this._options.readOnly) {
-            this._notify('click');
-         }
-      },
-
-      _clickHandler: function(e) {
-         if (this._options.readOnly) {
-            e.stopPropagation();
-         }
+   _keyUpHandler: function (e) {
+      if (e.nativeEvent.keyCode === 13 && !this._options.readOnly) {
+         this._notify('click');
       }
-   });
+   },
+
+   _clickHandler: function (e) {
+      if (this._options.readOnly) {
+         e.stopPropagation();
+      }
+   }
+});
 
 export = Link;
--- Controls/_lookup/SelectedCollection/SelectedCollectionStyles.ts
+++ Controls/_lookup/SelectedCollection/SelectedCollectionStyles.ts
@@ -1,36 +1,68 @@
-
-
-   /**
-    * @mixin Controls/_lookup/SelectedCollection/SelectedCollectionStyles
-    * @public
-    * @author Капустин И.А.
-    *
-    * @css @color_SelectedCollection-item_accent Accent item color.
-    * @css @color_SelectedCollection-item_primary Primary item color.
-    * @css @background_SelectedCollection-counter
-    * @css @font-size_SelectedCollection-counter
-    * @ccs @width_SelectedCollection-counter-shadow
-    * @css @color_SelectedCollection-counter Counter color.
-    * @css @color_SelectedCollection-counter_hover
-    * @css @color_SelectedCollection-counter_active
-    * @css @color_SelectedCollection-cross Cross color.
-    * @css @icon-size_SelectedCollection-cross Icon cross size.
-    * @css @line-height_SelectedCollection-item SelectedCollection items line height.
-    * @css @font-size_SelectedCollection-size_m Font size of item with size m.
-    * @css @font-size_SelectedCollection-size_l Font size of item with size l.
-    * @css @font-size_SelectedCollection-size_xl Font size of item with size xl.
-    * @css @font-size_SelectedCollection-size_2xl Font size of item with size 2xl.
-    * @css @font-size_SelectedCollection-size_3xl Font size of item with size 3xl.
-    * @css @spacing_SelectedCollection-between-items Spacing between items.
-    * @css @spacing_SelectedCollection-between-counter-items Spacing between counter and items.
-    * @css @spacing_SelectedCollection-between-counter-ellipsis Spacing between counter and ellipsis.
-    * @css @spacing_SelectedCollection-between-infoBoxContent-borderLeft Outdent to the left of the edge of InfoBox content.
-    * @css @spacing_SelectedCollection-between-infoBoxContent-borderRight Outdent to the right of the edge of InfoBox content.
-    * @css @spacing_SelectedCollection-between-infoBoxContent-borderTop Outdent to the top of the edge of InfoBox content.
-    * @css @spacing_SelectedCollection-between-infoBoxContent-borderBottom Outdent to the bottom of the edge of InfoBox content.
-    * @css @size_SelectedCollection-infoBoxContent-shadow Shadow size around InfoBox content.
-    * @css @color_SelectedCollection-infoBoxContent-shadow Shadow color around InfoBox content.
-    * @css @size_SelectedCollection-infoBoxContent-border Sizes of borders InfoBox content.
-    * @css @color-border_SelectedCollection-infoBoxContent Color of borders InfoBox content.
-    * @css @background-color_SelectedCollection-infoBoxContent Background color of InfoBox content.
-    */
+/**
+ * @mixin Controls/_lookup/SelectedCollection/SelectedCollectionStyles
+ * @public
+ * @author Капустин И.А.
+ *
+ * @css @color_SelectedCollection-item_accent Accent item color.
+ * @css @color_SelectedCollection-item_primary Primary item color.
+ * @css @background_SelectedCollection-counter
+ * @css @font-size_SelectedCollection-counter
+ * @ccs @width_SelectedCollection-counter-shadow
+ * @css @color_SelectedCollection-counter Counter color.
+ * @css @color_SelectedCollection-counter_hover
+ * @css @color_SelectedCollection-counter_active
+ * @css @color_SelectedCollection-cross Cross color.
+ * @css @icon-size_SelectedCollection-cross Icon cross size.
+ * @css @line-height_SelectedCollection-item SelectedCollection items line height.
+ * @css @font-size_SelectedCollection-size_m Font size of item with size m.
+ * @css @font-size_SelectedCollection-size_l Font size of item with size l.
+ * @css @font-size_SelectedCollection-size_xl Font size of item with size xl.
+ * @css @font-size_SelectedCollection-size_2xl Font size of item with size 2xl.
+ * @css @font-size_SelectedCollection-size_3xl Font size of item with size 3xl.
+ * @css @spacing_SelectedCollection-between-items Spacing between items.
+ * @css @spacing_SelectedCollection-between-counter-items Spacing between counter and items.
+ * @css @spacing_SelectedCollection-between-counter-ellipsis Spacing between counter and ellipsis.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderLeft Outdent to the left of the edge of InfoBox content.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderRight Outdent to the right of the edge of InfoBox content.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderTop Outdent to the top of the edge of InfoBox content.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderBottom Outdent to the bottom of the edge of InfoBox content.
+ * @css @size_SelectedCollection-infoBoxContent-shadow Shadow size around InfoBox content.
+ * @css @color_SelectedCollection-infoBoxContent-shadow Shadow color around InfoBox content.
+ * @css @size_SelectedCollection-infoBoxContent-border Sizes of borders InfoBox content.
+ * @css @color-border_SelectedCollection-infoBoxContent Color of borders InfoBox content.
+ * @css @background-color_SelectedCollection-infoBoxContent Background color of InfoBox content.
+ */
+/*
+ * @mixin Controls/_lookup/SelectedCollection/SelectedCollectionStyles
+ * @public
+ * @author Kapustin I.A.
+ *
+ * @css @color_SelectedCollection-item_accent Accent item color.
+ * @css @color_SelectedCollection-item_primary Primary item color.
+ * @css @background_SelectedCollection-counter
+ * @css @font-size_SelectedCollection-counter
+ * @ccs @width_SelectedCollection-counter-shadow
+ * @css @color_SelectedCollection-counter Counter color.
+ * @css @color_SelectedCollection-counter_hover
+ * @css @color_SelectedCollection-counter_active
+ * @css @color_SelectedCollection-cross Cross color.
+ * @css @icon-size_SelectedCollection-cross Icon cross size.
+ * @css @line-height_SelectedCollection-item SelectedCollection items line height.
+ * @css @font-size_SelectedCollection-size_m Font size of item with size m.
+ * @css @font-size_SelectedCollection-size_l Font size of item with size l.
+ * @css @font-size_SelectedCollection-size_xl Font size of item with size xl.
+ * @css @font-size_SelectedCollection-size_2xl Font size of item with size 2xl.
+ * @css @font-size_SelectedCollection-size_3xl Font size of item with size 3xl.
+ * @css @spacing_SelectedCollection-between-items Spacing between items.
+ * @css @spacing_SelectedCollection-between-counter-items Spacing between counter and items.
+ * @css @spacing_SelectedCollection-between-counter-ellipsis Spacing between counter and ellipsis.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderLeft Outdent to the left of the edge of InfoBox content.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderRight Outdent to the right of the edge of InfoBox content.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderTop Outdent to the top of the edge of InfoBox content.
+ * @css @spacing_SelectedCollection-between-infoBoxContent-borderBottom Outdent to the bottom of the edge of InfoBox content.
+ * @css @size_SelectedCollection-infoBoxContent-shadow Shadow size around InfoBox content.
+ * @css @color_SelectedCollection-infoBoxContent-shadow Shadow color around InfoBox content.
+ * @css @size_SelectedCollection-infoBoxContent-border Sizes of borders InfoBox content.
+ * @css @color-border_SelectedCollection-infoBoxContent Color of borders InfoBox content.
+ * @css @background-color_SelectedCollection-infoBoxContent Background color of InfoBox content.
+ */
