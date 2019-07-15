409d0624ff35d516d88cf6b3a91448c862fa0775
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2019-07-15 14:34:53 +0300

    https://online.sbis.ru/opendoc.html?guid=764136ee-35c2-49df-b81c-92f1dcfd82f5

--- Controls/interface/IInputNumber.ts
+++ Controls/interface/IInputNumber.ts
@@ -1,4 +1,97 @@
 /**
+ * Интерфейс для контрола Input.Number.
+ *
+ * @interface Controls/interface/IInputNumber
+ * @public
+ * @author Журалев М.С.
+ */
+
+/**
+ * @name Controls/interface/IInputNumber#value
+ * @cfg {Number|String|null} Число, которое будет проецироваться на текст в поле.
+ * @default 0
+ * @remark
+ * Если вы не обновите параметр value, то не сможете ничего ввести в поле. Необходимо подписаться на событие _valueChanged и обновить значение, передаваемое элементу управления. Чтобы сделать его проще, вы можете использовать синтаксис связывания.
+ * Работая с типом числа можно столкнуться с проблемой потери точности. Например, номер 99999999999999999999 не существует. Он представлен как 100000000000000000000. Дело в том, что число в js представлено {@link http://www.softelectro.ru/ieee754.html IEEE 754} стандартом. Чтобы избежать этой проблемы, необходимо работать со строковым типом.
+ * @example
+ * В этом примере значение _input в состоянии элемента управления привязывается к значению поля ввода. В любое время жизненного цикла элемента управления значение _input будет содержать текущее значение поля ввода.
+ * <pre>
+ *    <Controls.input:Number bind:value="_inputValue" />
+ *    <Controls.Button on:click="_sendButtonClick()" />
+ * </pre>
+ *
+ * <pre>
+ *    Control.extend({
+ *       ...
+ *       _inputValue: 0,
+ *
+ *       _sendButtonClick() {
+ *             this._sendData(this._inputValue);
+ *       }
+ *       ...
+ *    });
+ * </pre>
+ * @see valueChanged
+ * @see inputCompleted
+ */
+
+/**
+ * @event Возникает при изменении значения поля.
+ * @name Controls/interface/IInputNumber#valueChanged
+ * @param {Number|null} value Число, которое будет проецироваться на текст в поле.
+ * @param {String} displayValue Отображаемое значение в поле.
+ * @remark
+ * Это событие должно использоваться для реагирования на изменения, вносимые Пользователем в поле. Значение, возвращаемое в событии, не вставляется в элемент управления, если не передать его обратно в поле в качестве опции. Обычно вы бы вместо того, чтобы использовать синтаксис связывания. Пример ниже показывает разницу.
+ * @example
+ * В этом примере мы покажем, как можно 'привязать' значение элемента управления поле. В первом поле, мы делаем это вручную, используя событие valueChanged. Во втором поле используется нотация привязки. Оба поля в этом примере будут иметь одинаковое поведение.
+ * <pre>
+ *    <Controls.input:Text value="_fieldValue" on:valueChanged="_valueChangedHandler()" />
+ *
+ *    <Controls.input:Text bind:value="_anotherFieldValue" />
+ * </pre>
+ *
+ * <pre>
+ *    Control.extend({
+ *       ...
+ *       _fieldValue: '',
+ *
+ *       _valueChangedHandler(value) {
+ *          this._fieldValue = value;
+ *       },
+ *
+ *       _anotherFieldValue: ''
+ *
+ *    });
+ * </pre>
+ * @see value
+ */
+
+/**
+ * @event Происходит, когда ввод завершен (поле потеряло фокус или пользователь нажал "enter").
+ * @name Controls/interface/IInputNumber#inputCompleted
+ * @param {Number|null} value Число, которое будет проецироваться на текст в поле.
+ * @param {String} displayValue Отображаемое значение в поле.
+ * @remark
+ * Это событие можно использовать в качестве триггера для проверки поля или отправки введенных данных в какой-либо другой элемент управления.
+ * @example
+ * В этом примере мы подписываемся на событие Input Completed и сохраняем значение поля в базе данных.
+ * <pre>
+ *    <Controls.input:Text on:inputCompleted="_inputCompletedHandler()" />
+ * </pre>
+ *
+ * <pre>
+ *    Control.extend({
+ *       ...
+ *       _inputCompletedHandler(value) {
+ *          this._saveEnteredValueToDatabase(value);
+ *       }
+ *       ...
+ *    });
+ * </pre>
+ * @see value
+ */
+
+/*
  * Interface for Input.Number.
  *
  * @interface Controls/interface/IInputNumber
@@ -6,12 +99,13 @@
  * @author Журалев М.С.
  */
 interface IInputNumber {
-    /**
+    /*
      * @name Controls/interface/IInputNumber#value
-     * @cfg {Number|null} The number that will be projected to the text in the field.
+     * @cfg {Number|String|null} The number that will be projected to the text in the field.
      * @default 0
      * @remark
      * If you don`t update value option, will not be able to enter anything in the field. You need to subscribe to _valueChanged event and update value that is passed to the control. To make it simpler, you can use bind notation.
+     * Working with the Number type you can face the problem of loss of accuracy. For example, the number 999999999999999999 does not exist. It is represented as 100000000000000000000. The thing is that the number in js is represented by the {@link http://www.softelectro.ru/ieee754.html IEEE 754} standard. To avoid this problem, you must work with the String type.
      * @example
      * In this example you bind _inputValue in control's state to the value of input field. At any time of control's lifecycle, _inputValue will contain the current value of the input field.
      * <pre>
@@ -21,14 +115,14 @@ interface IInputNumber {
      *
      * <pre>
      *    Control.extend({
- *       ...
- *       _inputValue: 0,
- *
- *       _sendButtonClick() {
- *             this._sendData(this._inputValue);
- *       }
- *       ...
- *    });
+     *        ...
+     *       _inputValue: 0,
+     *
+     *       _sendButtonClick() {
+     *          this._sendData(this._inputValue);
+     *       }
+     *       ...
+     *    });
      * </pre>
      * @see valueChanged
      * @see inputCompleted
@@ -36,7 +130,7 @@ interface IInputNumber {
     value: number | null;
 }
 
-/**
+/*
  * @event Occurs when field value was changed.
  * @name Controls/interface/IInputNumber#valueChanged
  * @param {Number|null} value The number that will be projected to the text in the field.
@@ -67,7 +161,7 @@ interface IInputNumber {
  * @see value
  */
 
-/**
+/*
  * @event Occurs when input is completed (field lost focus or user pressed ‘enter’).
  * @name Controls/interface/IInputNumber#inputCompleted
  * @param {Number|null} value The number that will be projected to the text in the field.
