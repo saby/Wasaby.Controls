d41ee5c28ed200782df53fe332160f52e7111848
nv.belotelov <nv.belotelov@tensor.ru>
2019-07-17 17:53:01 +0300

    https://online.sbis.ru/opendoc.html?guid=56ef06d8-3251-4084-b09c-3fc354a5bd964

--- Controls/Application/AppData.js
+++ Controls/Application/AppData.js
@@ -2,39 +2,9 @@
  * Created by dv.zuev on 01.02.2018.
  */
 define('Controls/Application/AppData', [
-   'Core/core-extend',
-   'Application/Env'
-], function(extend, AppEnv) {
-
-   var AppData = extend.extend({
-      constructor: function(cfg) {
-         this.appRoot = cfg.appRoot;
-         this.lite = cfg.lite;
-         this.application = cfg.application;
-         this.wsRoot = cfg.wsRoot;
-         this.resourceRoot = cfg.resourceRoot;
-         this.RUMEnabled = cfg.RUMEnabled;
-         this.pageName = cfg.pageName;
-         this.product = cfg.product;
-         this.cssBundles = cfg.cssBundles;
-         this.buildnumber = cfg.buildnumber;
-         this.servicesPath = cfg.servicesPath;
-         this.staticDomains = cfg.staticDomains;
-      },
-      registerConsumer: function() {
-
-         // Need this to pass AppData as context field
-      },
-      updateConsumers: function() {
-
-         // Need this to pass AppData as context field
-      }
-   });
-   AppData.initAppData = function(cfg) {
-      AppEnv.setStore('AppData', new AppData(cfg));
-   };
-   AppData.getAppData = function() {
-      return AppEnv.getStore('AppData');
-   };
-   return AppData;
+   'UI/Base',
+   'Env/Env'
+], function(UIBase, Env) {
+   Env.IoC.resolve('ILogger').warn('Controls/Application/AppData will be removed in 19.600. Please use AppData from UI/Base instead.');
+   return UIBase.AppData;
 });
