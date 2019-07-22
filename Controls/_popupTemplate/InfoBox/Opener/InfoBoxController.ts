7ddfb46cadfaf000bb400da334de59b4c839502d
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-07-22 09:10:35 +0300

    https://online.sbis.ru/opendoc.html?guid=b2660b31-e93e-4778-a106-e9b73f8edb61

--- Controls/_popupTemplate/InfoBox/Opener/InfoBoxController.ts
+++ Controls/_popupTemplate/InfoBox/Opener/InfoBoxController.ts
@@ -16,40 +16,40 @@ function getConstants() {
     });
 }
 
-//todo: https://online.sbis.ru/opendoc.html?guid=b385bef8-31dd-4601-9716-f3593dfc9d41
-var constants = {};
+// todo: https://online.sbis.ru/opendoc.html?guid=b385bef8-31dd-4601-9716-f3593dfc9d41
+let constants = {};
 if (document) {
     if (document.body) {
         constants = getConstants();
     } else {
-        document.addEventListener("DOMContentLoaded", function() {
+        document.addEventListener('DOMContentLoaded', function() {
             constants = getConstants();
         });
     }
 }
 
-var SIDES = {
-    't': 'top',
-    'r': 'right',
-    'b': 'bottom',
-    'l': 'left',
-    'c': 'center'
+const SIDES = {
+    t: 'top',
+    r: 'right',
+    b: 'bottom',
+    l: 'left',
+    c: 'center'
 };
 
-var INVERTED_SIDES = {
-    't': 'bottom',
-    'r': 'left',
-    'b': 'top',
-    'l': 'right',
-    'c': 'center'
+const INVERTED_SIDES = {
+    t: 'bottom',
+    r: 'left',
+    b: 'top',
+    l: 'right',
+    c: 'center'
 };
 
-var _private = {
+const _private = {
 
     // Checks if the target width is enough for the correct positioning of the arrow.
     // Returns offset to which you want to move the popup.
-    getOffset: function (targetSize, alignSide, arrowOffset, arrowWidth) {
-        var align = INVERTED_SIDES[alignSide];
+    getOffset(targetSize, alignSide, arrowOffset, arrowWidth) {
+        const align = INVERTED_SIDES[alignSide];
 
         // Check if we have enough width of the target for the correct positioning of the arrow, if not, just
         // move the popup arrow to the center of the target
@@ -67,12 +67,12 @@ var _private = {
     },
 
     // Return the configuration prepared for StickyStrategy
-    prepareConfig: function (position, target) {
-        var side = position[0];
-        var alignSide = position[1];
-        var topOrBottomSide = side === 't' || side === 'b';
+    prepareConfig(position, target) {
+        const side = position[0];
+        const alignSide = position[1];
+        const topOrBottomSide = side === 't' || side === 'b';
 
-        var config = {
+        const config = {
             verticalAlign: {
                 side: topOrBottomSide ? SIDES[side] : INVERTED_SIDES[alignSide]
             },
@@ -87,8 +87,8 @@ var _private = {
             }
         };
 
-        var verticalOffset = _private.getVerticalOffset(target, topOrBottomSide, side, alignSide);
-        var horizontalOffset = _private.getHorizontalOffset(target, topOrBottomSide, side, alignSide);
+        const verticalOffset = _private.getVerticalOffset(target, topOrBottomSide, side, alignSide);
+        const horizontalOffset = _private.getHorizontalOffset(target, topOrBottomSide, side, alignSide);
 
         if (verticalOffset) {
             config.verticalAlign.offset = verticalOffset;
@@ -101,18 +101,18 @@ var _private = {
         return config;
     },
 
-    getVerticalOffset: function (target, topOrBottomSide, side, alignSide) {
+    getVerticalOffset(target, topOrBottomSide, side, alignSide) {
         if (!topOrBottomSide) {
             // svg hasn't offsetHeight property
-            var targetHeight = target.offsetHeight || target.clientHeight;
+            const targetHeight = target.offsetHeight || target.clientHeight;
             return _private.getOffset(targetHeight, alignSide, constants.ARROW_V_OFFSET, constants.ARROW_WIDTH);
         }
     },
 
-    getHorizontalOffset: function (target, topOrBottomSide, side, alignSide) {
+    getHorizontalOffset(target, topOrBottomSide, side, alignSide) {
         if (topOrBottomSide) {
             // svg hasn't offsetWidth property
-            var targetWidth = target.offsetWidth || target.clientWidth;
+            const targetWidth = target.offsetWidth || target.clientWidth;
             return _private.getOffset(targetWidth, alignSide, constants.ARROW_H_OFFSET, constants.ARROW_WIDTH);
         }
     }
@@ -125,10 +125,10 @@ var _private = {
  * @private
  * @category Popup
  */
-var InfoBoxController = StickyController.constructor.extend({
+const InfoBoxController = StickyController.constructor.extend({
     _openedPopupId: null,
 
-    elementCreated: function (cfg, container, id) {
+    elementCreated(cfg, container, id) {
         // Only one popup can be opened
         if (this._openedPopupId) {
             require('Controls/popup').Controller.remove(this._openedPopupId);
@@ -141,40 +141,40 @@ var InfoBoxController = StickyController.constructor.extend({
         return InfoBoxController.superclass.elementCreated.apply(this, arguments);
     },
 
-    elementUpdated: function () {
+    elementUpdated() {
         // Hide popup then page scroll or resize
         require('Controls/popup').Controller.remove(this._openedPopupId);
     },
 
-    popupResize: function (element, container) {
+    popupResize(element, container) {
         return InfoBoxController.superclass.elementUpdated.call(this, element, container);
     },
 
-    elementDestroyed: function (item) {
+    elementDestroyed(item) {
         if (item.id === this._openedPopupId) {
             this._openedPopupId = null;
         }
         return (new Deferred()).callback();
     },
 
-    needRestoreFocus: function (isActive) {
+    needRestoreFocus(isActive) {
         return isActive;
     },
 
-    getDefaultConfig: function (item) {
+    getDefaultConfig(item) {
         InfoBoxController.superclass.getDefaultConfig.apply(this, arguments);
-        let defaultPosition = {
+        const defaultPosition = {
             left: -10000,
             top: -10000,
             right: undefined,
-            bottom: undefined,
+            bottom: undefined
         };
         if (item.popupOptions.target) {
             // Calculate the width of the infobox before its positioning.
             // It is impossible to count both the size and the position at the same time, because the position is related to the size.
             cMerge(item.popupOptions, _private.prepareConfig(item.popupOptions.position, item.popupOptions.target));
-            var sizes = {width: constants.MAX_WIDTH, height: 1, margins: {left: 0, top: 0}};
-            var position = StickyStrategy.getPosition(this._getPopupConfig(item, sizes), TargetCoords.get(item.popupOptions.target));
+            const sizes = {width: constants.MAX_WIDTH, height: 1, margins: {left: 0, top: 0}};
+            const position = StickyStrategy.getPosition(this._getPopupConfig(item, sizes), TargetCoords.get(item.popupOptions.target));
             this.prepareConfig(item, sizes);
             item.position.maxWidth = position.width;
         }
@@ -182,17 +182,17 @@ var InfoBoxController = StickyController.constructor.extend({
     },
 
     _getPopupConfig() {
-        let baseConfig = InfoBoxController.superclass._getPopupConfig.apply(this, arguments);
+        const baseConfig = InfoBoxController.superclass._getPopupConfig.apply(this, arguments);
         // Protection against incorrect page design
         baseConfig.checkNegativePosition = false;
         return baseConfig;
     },
 
-    prepareConfig: function (cfg, sizes) {
+    prepareConfig(cfg, sizes) {
         cMerge(cfg.popupOptions, _private.prepareConfig(cfg.popupOptions.position, cfg.popupOptions.target));
         return InfoBoxController.superclass.prepareConfig.apply(this, arguments);
-    }
+    },
+    TYPE: 'InfoBox',
+    _private
 });
-InfoBoxController.prototype._private = _private;
 export = new InfoBoxController();
-
--- Controls/_popupTemplate/Notification/Opener/NotificationController.ts
+++ Controls/_popupTemplate/Notification/Opener/NotificationController.ts
@@ -3,120 +3,122 @@ import collection = require('Types/collection');
 import BaseController = require('Controls/_popupTemplate/BaseController');
 import NotificationStrategy = require('Controls/_popupTemplate/Notification/Opener/NotificationStrategy');
 import NotificationContent = require('Controls/_popupTemplate/Notification/Opener/NotificationContent');
-      var timeAutoClose = 5000;
 
-      var _private = {
-         setNotificationContent: function(item) {
-            item.popupOptions.content = NotificationContent;
-         },
-         findItemById: function(popupItems, id) {
-            let index = popupItems && popupItems.getIndexByValue('id', id);
-            if (index > -1) {
-               return popupItems.at(index);
+const timeAutoClose = 5000;
+
+const _private = {
+    setNotificationContent(item) {
+        item.popupOptions.content = NotificationContent;
+    },
+    findItemById(popupItems, id) {
+        const index = popupItems && popupItems.getIndexByValue('id', id);
+        if (index > -1) {
+            return popupItems.at(index);
+        }
+        return null;
+    },
+    isLinkedPopup(popupItems, parentItem, item): Boolean {
+        while (item && item.parentId) {
+            item = _private.findItemById(popupItems, item.parentId);
+            if (item === parentItem) {
+                return true;
             }
-            return null;
-         },
-         isLinkedPopup(popupItems, parentItem, item):Boolean {
-            while (item && item.parentId) {
-               item = _private.findItemById(popupItems, item.parentId);
-               if (item === parentItem) {
-                  return true;
-               }
+        }
+        return false;
+    }
+};
+
+/**
+ * Notification Popup Controller
+ * @class Controls/_popupTemplate/Notification/Opener/NotificationController
+ * @control
+ * @private
+ * @category Popup
+ * @extends Controls/_popupTemplate/BaseController
+ */
+const NotificationController = BaseController.extend({
+    constructor(cfg) {
+        NotificationController.superclass.constructor.call(this, cfg);
+        this._stack = new collection.List();
+    },
+
+    elementCreated(item, container) {
+        item.height = container.offsetHeight;
+        _private.setNotificationContent(item);
+        this._stack.add(item, 0);
+        this._updatePositions();
+        if (item.popupOptions.autoClose) {
+            this._closeByTimeout(item);
+        }
+    },
+
+    elementUpdated(item, container) {
+        _private.setNotificationContent(item);
+        item.height = container.offsetHeight;
+        this._updatePositions();
+    },
+
+    elementDestroyed(item) {
+        this._stack.remove(item);
+        this._updatePositions();
+
+        NotificationController.superclass.elementDestroyed.call(item);
+
+        return new Deferred().callback();
+    },
+
+    popupMouseEnter(item) {
+        if (item.popupOptions.autoClose) {
+            clearTimeout(item.closeId);
+        }
+    },
+
+    popupMouseLeave(item) {
+        if (item.popupOptions.autoClose) {
+            this._closeByTimeout(item);
+        }
+    },
+
+    _closeByTimeout(item) {
+        item.closeId = setTimeout(function() {
+            require('Controls/popup').Controller.remove(item.id);
+        }, timeAutoClose);
+    },
+
+    getCustomZIndex(popupItems, item): Number {
+        // Notification windows must be above all popup windows
+        // will be fixed by https://online.sbis.ru/opendoc.html?guid=e6a136fc-be49-46f3-84d5-be135fae4761
+        const count = popupItems.getCount();
+        for (let i = 0; i < count; i++) {
+            // if popups are linked, then notification must be higher then parent
+            if (popupItems.at(i).popupOptions.maximize && !_private.isLinkedPopup(popupItems, popupItems.at(i), item)) {
+                const maximizedPopupZIndex = (i + 1) * 10;
+                return maximizedPopupZIndex - 1;
             }
-            return  false;
-         }
-      };
+        }
+        return 100;
+    },
 
-      /**
-       * Notification Popup Controller
-       * @class Controls/_popupTemplate/Notification/Opener/NotificationController
-       * @control
-       * @private
-       * @category Popup
-       * @extends Controls/_popupTemplate/BaseController
-       */
-      var NotificationController = BaseController.extend({
-         constructor: function(cfg) {
-            NotificationController.superclass.constructor.call(this, cfg);
-            this._stack = new collection.List();
-         },
+    getDefaultConfig(item) {
+        NotificationController.superclass.getDefaultConfig.apply(this, arguments);
+        _private.setNotificationContent(item);
+    },
 
-         elementCreated: function(item, container) {
-            item.height = container.offsetHeight;
-            _private.setNotificationContent(item);
-            this._stack.add(item, 0);
-            this._updatePositions();
-            if (item.popupOptions.autoClose) {
-               this._closeByTimeout(item);
-            }
-         },
+    _updatePositions() {
+        let height = 0;
 
-         elementUpdated: function(item, container) {
-            _private.setNotificationContent(item);
-            item.height = container.offsetHeight;
-            this._updatePositions();
-         },
+        /**
+         * In item.height is the height of the popup.
+         * It takes into account the indentation between the notification popups,
+         * specified in the template via css. This is done to support theming.
+         */
+        this._stack.each(function(item) {
+            item.position = NotificationStrategy.getPosition(height);
+            height += item.height;
+        });
+    },
+    TYPE: 'Notification'
+});
 
-         elementDestroyed: function(item) {
-            this._stack.remove(item);
-            this._updatePositions();
-
-            NotificationController.superclass.elementDestroyed.call(item);
-
-            return new Deferred().callback();
-         },
-
-         popupMouseEnter: function(item) {
-            if (item.popupOptions.autoClose) {
-               clearTimeout(item.closeId);
-            }
-         },
-
-         popupMouseLeave: function(item) {
-            if (item.popupOptions.autoClose) {
-               this._closeByTimeout(item);
-            }
-         },
-
-         _closeByTimeout: function(item) {
-            item.closeId = setTimeout(function() {
-               require('Controls/popup').Controller.remove(item.id);
-            }, timeAutoClose);
-         },
-
-         getCustomZIndex(popupItems, item):Number {
-            // Notification windows must be above all popup windows
-            // will be fixed by https://online.sbis.ru/opendoc.html?guid=e6a136fc-be49-46f3-84d5-be135fae4761
-            let count = popupItems.getCount();
-            for (let i = 0; i < count; i++) {
-               // if popups are linked, then notification must be higher then parent
-               if (popupItems.at(i).popupOptions.maximize && !_private.isLinkedPopup(popupItems, popupItems.at(i), item)) {
-                  let maximizedPopupZIndex = (i + 1) * 10;
-                  return maximizedPopupZIndex - 1;
-               }
-            }
-            return 100;
-         },
-
-         getDefaultConfig(item) {
-            NotificationController.superclass.getDefaultConfig.apply(this, arguments);
-            _private.setNotificationContent(item);
-         },
-
-         _updatePositions: function() {
-            var height = 0;
-
-            /**
-             * In item.height is the height of the popup.
-             * It takes into account the indentation between the notification popups,
-             * specified in the template via css. This is done to support theming.
-             */
-            this._stack.each(function(item) {
-               item.position = NotificationStrategy.getPosition(height);
-               height += item.height;
-            });
-         }
-      });
-
-      export = new NotificationController();
+export = new NotificationController();
 
