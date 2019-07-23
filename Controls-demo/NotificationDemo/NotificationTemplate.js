d33b58f9cc7eb0d1c345f55efdaab0009f16033c
em.tatanova <em.tatanova@tensor.ru>
2019-07-23 10:08:42 +0300

    https://online.sbis.ru/opendoc.html?guid=3598edd7-0f96-4e36-82fe-79728deddd1c

---
+++ Controls-demo/NotificationDemo/NotificationTemplate.js
@@ -0,0 +1,22 @@
+define('Controls-demo/NotificationDemo/NotificationTemplate',
+   [
+      'Core/Control',
+      'wml!Controls-demo/NotificationDemo/NotificationTemplate',
+      'wml!Controls-demo/Popup/Opener/resources/CustomNotification'
+   ],
+   function(Control, template) {
+
+      'use strict';
+
+      return Control.extend({
+         _template: template,
+
+         _openNotification: function(e, index) {
+            this._children['myOpener' + index].open();
+         },
+
+         _closeNotification: function(e, index) {
+            this._children['myOpener' + index].close();
+         }
+      });
+   });
