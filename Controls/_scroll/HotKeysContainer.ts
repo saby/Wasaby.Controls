51466b63779e28fdbd6d756ca6e0d6c13b5ade5d
Быканов А.А <aa.bykanov@tensor.ru>
2019-07-26 16:08:58 +0300

    Update HotKeysContainer.ts

--- Controls/_scroll/HotKeysContainer.ts
+++ Controls/_scroll/HotKeysContainer.ts
@@ -2,7 +2,7 @@ import {Control, IControlOptions} from 'UI/Base';
 import template = require('wml!Controls/_scroll/HotKeysContainer');
 
 /**
- * Контрол настраивает Controls.scroll:Container на перехват клавиш "up", "down", "page up", "page down", "home", "end" по умолчанию.
+ * Контрол настраивает Controls/scroll:Container на перехват клавиш "up", "down", "page up", "page down", "home", "end" по умолчанию.
  * @class Controls/_scroll/HotKeysContainer
  * @extends Core/Control
  * @author Шипин А.А.
@@ -10,7 +10,7 @@ import template = require('wml!Controls/_scroll/HotKeysContainer');
  */
 
 /*
- * Control makes Controls.scroll:Container to handle up, down, page up, page down, home, end keys by default
+ * Control makes Controls/scroll:Container to handle up, down, page up, page down, home, end keys by default
  * @class Controls/_scroll/HotKeysContainer
  * @extends Core/Control
  * @author Шипин А.А.
