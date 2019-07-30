111430c06d619b1bab1e3b87ad943dade6e81333
Быканов А.А <aa.bykanov@tensor.ru>
2019-07-30 17:24:57 +0300

    Update Picker.ts

--- Controls/_input/Time/Picker.ts
+++ Controls/_input/Time/Picker.ts
@@ -2,8 +2,8 @@
  * Поле ввода даты и времени.
  * В зависимости от маски можно вводить:
  * <ol>
- *    <li>дату,</li>
- *    <li>время,</li>
+ *    <li>дату;</li>
+ *    <li>время;</li>
  *    <li>дату и время.</li>
  * </ol>
  * @class Controls/_input/Time/Picker
@@ -21,8 +21,8 @@
  * Control for entering date and time.
  * Depending on {@link mask mask} can be used to enter:
  * <ol>
- *    <li>just date,</li>
- *    <li>just time,</li>
+ *    <li>just date;</li>
+ *    <li>just time;</li>
  *    <li>date and time.</li>
  * </ol>
  * @class Controls/_input/Time/Picker
