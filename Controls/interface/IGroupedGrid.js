5a2cbfbdb4ea9d0210f14c4f3e7eb25b4f026f30
aa.bykanov <aa.bykanov@tensor.ru>
2019-08-02 14:23:44 +0300

    Исправлена ошибка в описании интерфейса Controls/interface/IGroupedGrid https://online.sbis.ru/opendoc.html?guid=c3343a0e-c604-4097-a053-1359f1d54bb1

--- Controls/interface/IGroupedGrid.js
+++ Controls/interface/IGroupedGrid.js
@@ -10,7 +10,7 @@ define('Controls/interface/IGroupedGrid', [
     */
 
    /**
-    * @name Controls/interface/IGrouped#groupingKeyCallback
+    * @name Controls/interface/IGroupedGrid#groupingKeyCallback
     * @cfg {Function} Функция обратного вызова для получения идентификатора группы элемента списка.
     * <a href="/materials/demo-ws4-list-group">Example</a>.
     * @remark
@@ -36,48 +36,48 @@ define('Controls/interface/IGroupedGrid', [
     */
 
    /**
-    * @name Controls/interface/IGroupedList#groupTemplate
+    * @name Controls/interface/IGroupedGrid#groupTemplate
     * @cfg {Function} groupTemplate шаблон группировки списка.
     * <a href="/materials/demo-ws4-grid-group">Example</a>.
     * @remark
-    * Базовый шаблон для контрола Controls.Grid:View: "Controls/grid:GroupTemplate".
+    * Базовый шаблон для контрола {@link Controls/Grid:View}: "Controls/grid:GroupTemplate".
     * Шаблон поддерживает следующие параметры:
     * <ul>
-    *    <li>separatorVisibility {Boolean} - Видимость горизонтальной линии-разделителя.</li>
-    *    <li>expanderVisibility {Boolean} - Видимость кнопки-экспандера.</li>
-    *    <li>textAlign {String} - Горизонтальное выравнивание текста группы. Доступные значения опции: "left" и "right". По умолчанию используется выравнивание текста по центру.</li>
-    *    <li>rightTemplate {Function} - Шаблон, выводимый в правой части группы. Может использоваться, например, для вывода итогов по группе.</li>
+    *    <li>separatorVisibility {Boolean} — аидимость горизонтальной линии-разделителя.</li>
+    *    <li>expanderVisibility {Boolean} — аидимость кнопки-экспандера.</li>
+    *    <li>textAlign {String} — горизонтальное выравнивание текста группы. Доступные значения опции: "left" и "right". По умолчанию используется выравнивание текста по центру.</li>
+    *    <li>rightTemplate {Function} — шаблон, выводимый в правой части группы. Может использоваться, например, для вывода итогов по группе.</li>
     * </ul>
     * @example
-    * Использование пользовательских параметров для группового рендеринга в Controls.grid: просмотр без расширителя и с выравниванием текста по левому краю:
+    * Использование пользовательских параметров для группового рендеринга в Controls/grid:View просмотр без расширителя и с выравниванием текста по левому краю:
     * <pre>
-    *    <Controls.grid:View
-    *       <groupTemplate>
+    *    <Controls.grid:View>
+    *       <ws:groupTemplate>
     *          <ws:partial template="Controls/grid:GroupTemplate" expanderVisibility="{{ false }}" textAlign="left" />
-    *       </groupTemplate>
+    *       </ws:groupTemplate>
     *    </Controls.grid:View>
     * </pre>
     */
 
    /**
-    * @name Controls/interface/IGrouped#collapsedGroups
+    * @name Controls/interface/IGroupedGrid#collapsedGroups
     * @cfg {Array} Список идентификаторов свернутых групп. Идентификаторы групп получаются в результате вызова {@link groupingKeyCallback}.
     * <a href="/materials/demo-ws4-list-group">Example</a>.
     */
 
    /**
-    * @name Controls/interface/IGrouped#groupHistoryId
+    * @name Controls/interface/IGroupedGrid#groupHistoryId
     * @cfg {String} Идентификатор для сохранения в истории списка идентификаторов свернутых групп.
     */
 
    /**
-    * @event Controls/interface/IGrouped#groupExpanded Происходит при развертывании группы.
+    * @event Controls/interface/IGroupedGrid#groupExpanded Происходит при развертывании группы.
     * @param {Vdom/Vdom:SyntheticEvent} eventObject Дескриптор события.
     * <a href="/materials/demo-ws4-list-group">Example</a>.
     */
 
    /**
-    * @event Controls/interface/IGrouped#groupCollapsed Происходит при сворачивании группы.
+    * @event Controls/interface/IGroupedGrid#groupCollapsed Происходит при сворачивании группы.
     * @param {Vdom/Vdom:SyntheticEvent} eventObject Дескриптор события.
     * <a href="/materials/demo-ws4-list-group">Example</a>.
     */    
