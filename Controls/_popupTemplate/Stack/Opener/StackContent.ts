41f4a9f1215f792f4bdc6370d2423f9214261198
Красильников А.С <as.krasilnikov@tensor.ru>
2019-08-02 15:04:36 +0300

    Update StackContent.ts

--- Controls/_popupTemplate/Stack/Opener/StackContent.ts
+++ Controls/_popupTemplate/Stack/Opener/StackContent.ts
@@ -2,27 +2,27 @@ import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import Template = require('wml!Controls/_popupTemplate/Stack/Opener/StackContent');
 import * as tmplNotify from 'Controls/Utils/tmplNotify';
 
-interface ISackContentOptions extends IControlOptions {
+interface IStackContentOptions extends IControlOptions {
     stackMaxWidth?: number;
     stackMinWidth?: number;
     stackWidth?: number;
 }
 
-class StackContent extends Control<ISackContentOptions> {
+class StackContent extends Control<IStackContentOptions> {
     // TODO https://online.sbis.ru/opendoc.html?guid=0e449eff-bd1e-4b59-8a48-5038e45cab22
     protected _template: TemplateFunction = Template;
     protected _tmplNotify: Function = tmplNotify;
     protected _minOffset: number;
     protected _maxOffset: number;
-    protected _beforeMount(options: ISackContentOptions): void {
+    protected _beforeMount(options: IStackContentOptions): void {
         this._updateOffset(options);
     }
 
-    protected _beforeUpdate(options: ISackContentOptions): void {
+    protected _beforeUpdate(options: IStackContentOptions): void {
         this._updateOffset(options);
     }
 
-    private _updateOffset(options: ISackContentOptions): void {
+    private _updateOffset(options: IStackContentOptions): void {
         this._maxOffset = options.stackMaxWidth - options.stackWidth;
         this._minOffset = options.stackWidth - options.stackMinWidth;
     }
