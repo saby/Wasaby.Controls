7730bd7efabbffa4cdc34967f761b25f277b1313
aa.bykanov <aa.bykanov@tensor.ru>
2019-08-06 16:25:41 +0300

    В описание библиотеки добавлены ссылки на работу с шаблонами этой библиотеки

--- Controls/list.ts
+++ Controls/list.ts
@@ -1,5 +1,12 @@
 /**
  * Библиотека плоских списков.
+ * Работа с шаблонами библиотеки описана в руководстве разработчика:
+ * <ul>
+ *    <li><a href="/doc/platform/developmentapl/interface-development/controls/list/list/templates/item/">ItemTemplate (шаблон отображения элемента)</a><li>
+ *    <li><a href="/doc/platform/developmentapl/interface-development/controls/list/list/templates/grouping/">GroupTemplate (шаблон группировки)</a><li>
+ *    <li><a href="/doc/platform/developmentapl/interface-development/controls/list/list/templates/empty/">EmptyTemplate (шаблон пустого списка)</a><li>
+ *    <li><a href="/doc/platform/developmentapl/interface-development/controls/list/list/edit/">EditingTemplate (шаблон редактирования элемента)</a><li>
+ * </ul>
  * @library Controls/list
  * @includes AddButton Controls/_list/AddButton
  * @includes Container Controls/_list/Container
