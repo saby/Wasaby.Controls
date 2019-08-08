2861f4274df0facbe854d44e302fc6898444eb51
ea.rodionov1 <ea.rodionov1@tensor.ru>
2019-08-08 12:47:55 +0300

    https://online.sbis.ru/opendoc.html?guid=d732ab26-84ab-4b01-88a8-590997fa5bb0

---
+++ Controls-demo/list_new/Base/Index.ts
@@ -0,0 +1,26 @@
+import {Control, TemplateFunction} from "UI/Base"
+import * as Template from "wml!Controls-demo/list_new/Base/Base"
+import {Memory} from "Types/source";
+import 'css!Controls-demo/Controls-demo';
+
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+
+    private _viewSource: Memory;
+
+    constructor() {
+        super({});
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: [
+                {id: 1, title: 'Notebooks'},
+                {id: 2, title: 'Tablets'},
+                {id: 3, title: 'Laptop computers'},
+                {id: 4, title: 'Apple gadgets'},
+                {id: 5, title: 'Android gadgets'}
+            ]
+        });
+    }
+
+}
