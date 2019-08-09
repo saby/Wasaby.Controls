3c468bb0f072137fdb59637b7a0200316a709cd8
Быканов А.А <aa.bykanov@tensor.ru>
2019-08-09 14:23:44 +0300

    Update Lookup.ts

--- Controls/_filterPopup/Panel/Lookup.ts
+++ Controls/_filterPopup/Panel/Lookup.ts
@@ -117,7 +117,7 @@ import 'css!theme?Controls/filterPopup';
     * @default Controls/_lookup/Lookup
     * @example
     * <pre>
-    *   <Controls._filterPopup.Panel.Lookup lookupTempalteName="namePace/Lookup"/>
+    *   <Controls.filterPopup.Lookup lookupTempalteName="namePace/Lookup"/>
     * </pre>
     */
 
@@ -127,7 +127,7 @@ import 'css!theme?Controls/filterPopup';
     * @default Controls/_lookup/Lookup
     * @example
     * <pre>
-    *   <Controls._filterPopup.Panel.Lookup lookupTempalteName="namePace/Lookup"/>
+    *   <Controls.filterPopup.Lookup lookupTempalteName="namePace/Lookup"/>
     * </pre>
     */
 
