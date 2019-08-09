193409ffa5bde0579130c4189203bdf99a61052d
rn.kondakov <rn.kondakov@tensor.ru>
2019-08-09 13:52:10 +0300

    https://online.sbis.ru/opendoc.html?guid=f457ca83-90ae-49d3-b4ed-7da5030257e1

--- Controls/_decorator/Markup/resources/linkDecorateUtils.ts
+++ Controls/_decorator/Markup/resources/linkDecorateUtils.ts
@@ -14,6 +14,7 @@ const classes = {
    link: 'LinkDecorator__linkWrap',
    image: 'LinkDecorator__image'
 };
+const decoratedLinkService = '/linkdecorator/service/';
 const fakeNeedDecorateAttribute = '__needDecorate';
 
 function isAttributes(value) {
@@ -100,7 +101,14 @@ function getClasses() {
  * @returns {String|undefined}
  */
 function getService() {
-   return constants.decoratedLinkService;
+   //return constants.decoratedLinkService;
+
+   // FIXME: брать сервис из констант не вариант на данный момент. В WaSaby приложениях её нет по умолчанию. На главной
+   // она инициализируется явно, раньше этого хватало. Нужно инициализировать в другом месте.
+   // Пока будем явно получать хранить константу здесь, в 510 сервис жив везде, где используют декорирование ссылок.
+   // Нужно принять решение в ошибке:
+   // https://online.sbis.ru/opendoc.html?guid=724c4e49-0807-4f5c-8bf1-0f5b84c01f62
+   return decoratedLinkService;
 }
 
 /**
