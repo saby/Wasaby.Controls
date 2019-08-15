adcadf97f019900ef82b7533cede41b2131f6e2a
ps.kolesova <ps.kolesova@tensor.ru>
2019-08-15 14:14:58 +0300

    https://online.sbis.ru/opendoc.html?guid=ea62a667-f3cf-49a6-91ec-045333bfe535

--- Controls-demo/Popup/Opener/StackDemo.js
+++ Controls-demo/Popup/Opener/StackDemo.js
@@ -50,7 +50,6 @@ define('Controls-demo/Popup/Opener/StackDemo',
             this._children.sticky.open({
                target: this._children.stickyButton2._container,
                opener: this._children.stickyButton2,
-               height: 200,
                actionOnScroll: 'track',
                template: 'wml!Controls-demo/Popup/Opener/DialogTpl'
             });
@@ -59,7 +58,6 @@ define('Controls-demo/Popup/Opener/StackDemo',
             this._children.sticky.open({
                target: this._children.stickyButton._container,
                opener: this._children.stickyButton,
-               height: 200,
                actionOnScroll: 'close',
                template: 'wml!Controls-demo/Popup/Opener/DialogTpl'
             });
