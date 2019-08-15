fc7a7085d14676195cb2103f309732ff8e5f648a
as.avramenko <as.avramenko@tensor.ru>
2019-08-15 16:09:31 +0300

    https://online.sbis.ru/opendoc.html?guid=317a972e-3b99-48f3-9a51-bd0b4f948d1d

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -441,13 +441,17 @@ var _private = {
                 top: self._virtualScroll.PlaceholdersSizes.top,
                 bottom: self._virtualScroll.PlaceholdersSizes.bottom
             }], { bubbling: true });
-            self._notify('updateShadowMode', [{
-                top: self._virtualScroll.PlaceholdersSizes.top ||
-                    self._sourceController && self._sourceController.hasMoreData('up') ? 'visible' : 'auto',
-                bottom: self._virtualScroll.PlaceholdersSizes.bottom ||
-                    self._sourceController && self._sourceController.hasMoreData('down') ? 'visible' : 'auto'
-            }], { bubbling: true });
+            _private.updateShadowMode(self);
         }
+    },
+
+    updateShadowMode(self): void {
+        self._notify('updateShadowMode', [{
+            top: self._virtualScroll.PlaceholdersSizes.top ||
+            self._sourceController && self._sourceController.hasMoreData('up') ? 'visible' : 'auto',
+            bottom: self._virtualScroll.PlaceholdersSizes.bottom ||
+            self._sourceController && self._sourceController.hasMoreData('down') ? 'visible' : 'auto'
+        }], { bubbling: true });
     },
 
     checkVirtualScrollCapability: function(self) {
@@ -1406,6 +1410,7 @@ var BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototype
         }
         if (this._virtualScroll) {
             this._setScrollItemContainer();
+            _private.updateShadowMode(this);
         }
     },
 
