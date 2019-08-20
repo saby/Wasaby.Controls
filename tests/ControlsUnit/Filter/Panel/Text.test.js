610c3188b1dba39eac891d8d74a0b377569fd943
am.gerasimov <am.gerasimov@tensor.ru>
2019-08-20 15:14:02 +0300

    https://online.sbis.ru/opendoc.html?guid=7de90794-86a2-4d4a-9c62-6039fc184ce9

---
+++ tests/ControlsUnit/Filter/Panel/Text.test.js
@@ -0,0 +1,30 @@
+define(['Controls/_filterPopup/Panel/Text'
+], function(Text) {
+   describe('Controls/_filterPopup/Panel/Text', function() {
+
+      describe('life hooks', function() {
+
+         it('_beforeMount', function() {
+            var textControl = new Text();
+            var textValue;
+            var controlValue;
+
+            textControl.saveOptions({ caption: 'test' });
+            textControl._notify = function(eventName, value) {
+               if (eventName === 'valueChanged') {
+                  controlValue = value[0];
+               } else {
+                  textValue = value[0];
+               }
+            };
+
+            textControl._afterMount();
+
+            assert.equal(controlValue, true);
+            assert.equal(textValue, 'test');
+         });
+
+      });
+
+   });
+});
