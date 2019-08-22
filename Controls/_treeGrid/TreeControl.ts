d810147e29095574a1e6622f5684ea55e939114f
Изыгин Н.Р <nr.izygin@corp.tensor.ru>
2019-08-22 13:56:44 +0300

    https://online.sbis.ru/opendoc.html?guid=38315a8d-2006-4eb8-aeb3-05b9447cd629

--- Controls/_treeGrid/TreeControl.ts
+++ Controls/_treeGrid/TreeControl.ts
@@ -399,7 +399,6 @@ var TreeControl = Control.extend(/** @lends Controls/_treeGrid/TreeControl.proto
         _private.toggleExpanded(this, item);
     },
     _onExpanderClick: function(e, dispItem) {
-        this._children.baseControl.focus();
         _private.toggleExpanded(this, dispItem);
         if (this._options.markItemByExpanderClick) {
             this._children.baseControl.getViewModel().setMarkedKey(dispItem.getContents().getId());
