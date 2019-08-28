96ee0e5aa9158374a14b4fec84b03226c3f14073
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2019-08-28 08:34:24 +0300

    https://online.sbis.ru/opendoc.html?guid=7982c0a2-3bcf-4d66-be21-52001cf176a4

--- Controls/Application.js
+++ Controls/Application.js
@@ -269,6 +269,9 @@ define('Controls/Application',
             var bodyClasses = BodyClasses().replace('ws-is-touch', '').replace('ws-is-no-touch', '');
 
             return bodyClasses;
+         },
+         isHover: function(touchClass, dragClass) {
+            return touchClass === 'ws-is-no-touch' && dragClass === 'ws-is-no-drag';
          }
       };
 
@@ -314,6 +317,8 @@ define('Controls/Application',
           */
          _scrollingClass: 'controls-Scroll_webkitOverflowScrollingTouch',
 
+         _dragClass: 'ws-is-no-drag',
+
          _getChildContext: function() {
             return {
                ScrollData: this._scrollData
@@ -354,14 +359,26 @@ define('Controls/Application',
              */
             this._children.mousemoveDetect.start(ev);
          },
-         _touchclass: function() {
-            // Данный метод вызывается из вёрстки, и при первой отрисовке еще нет _children (это нормально)
+         _updateClasses: function() {
+            // Данный метод вызывается о построения вёрстки, и при первой отрисовке еще нет _children (это нормально)
             // поэтому сами детектим touch с помощью compatibility
-            return this._children.touchDetector
-               ? this._children.touchDetector.getClass()
-               : Env.compatibility.touch
-                  ? 'ws-is-touch'
-                  : 'ws-is-no-touch';
+            if (this._children.touchDetector) {
+               this._touchClass = this._children.touchDetector.getClass();
+            } else {
+               this._touchClass = Env.compatibility.touch ? 'ws-is-touch' : 'ws-is-no-touch';
+            }
+
+            this._hoverClass = _private.isHover(this._touchClass, this._dragClass) ? 'ws-is-hover' : 'ws-is-no-hover';
+         },
+
+         _dragStartHandler: function() {
+            this._dragClass = 'ws-is-drag';
+            this._updateClasses();
+         },
+
+         _dragEndHandler: function() {
+            this._dragClass = 'ws-is-no-drag';
+            this._updateClasses();
          },
 
          /**
@@ -420,6 +437,7 @@ define('Controls/Application',
                   this.headValidHtml = undefined;
                }
             }
+            this._updateClasses();
          },
 
          _beforeUpdate: function(cfg) {
@@ -427,6 +445,7 @@ define('Controls/Application',
                this._scrollData.pagingVisible = cfg.pagingVisible;
                this._scrollData.updateConsumers();
             }
+            this._updateClasses();
          },
 
          _afterUpdate: function(oldOptions) {
