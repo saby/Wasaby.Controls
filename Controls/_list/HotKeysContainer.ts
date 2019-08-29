18ce82ce00d707c37c12a47ff7878812b7df1d27
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-08-29 14:40:44 +0300

    https://online.sbis.ru/opendoc.html?guid=db1cc155-eb9a-44c3-8ee7-a57182b02b27

--- Controls/_list/HotKeysContainer.ts
+++ Controls/_list/HotKeysContainer.ts
@@ -12,6 +12,19 @@ import {constants} from 'Env/Env';
 class HotKeysContainer extends Control<IControlOptions> {
     protected _template: Function = template;
     protected _defaultActions = [{keyCode: constants.key.up}, {keyCode: constants.key.down}, {keyCode: constants.key.enter}];
+    // Этого кода не будет, когда добавится еще один хук жизненного цикла - "заморозка".
+    private _afterMount(): void {
+        this._notify('registerKeyHook', [this], { bubbling: true});
+    }
+    private _beforeUnmount(): void {
+        this._notify('unregisterKeyHook', [this], { bubbling: true});
+    }
+    register(): void {
+        this._children.KeyHook.register();
+    }
+    unregister(): void {
+        this._children.KeyHook.unregister();
+    }
 }
 
 export default HotKeysContainer;
