07ab9e1ef5c76e7e5ce240170122872a1975f1cd
nv.belotelov <nv.belotelov@tensor.ru>
2019-09-02 19:09:22 +0300

    https://online.sbis.ru/opendoc.html?guid=17ff94ab-2fa8-4c9e-b83b-b2a399a595bc

---
+++ Controls/Application/Compatible.js
@@ -0,0 +1,60 @@
+/**
+ * Created by dv.zuev on 02.02.2018.
+ */
+define('Controls/Application/Compatible', [
+   'Core/Control',
+   'Env/Event',
+   'Core/Deferred',
+   'Env/Env',
+   'Lib/Control/LayerCompatible/LayerCompatible',
+   'wml!Controls/Application/Compatible',
+   'wml!Controls/Application/CompatibleScripts'
+], function(Base,
+   EnvEvent,
+   Deferred,
+   Env,
+   Layer,
+   template) {
+   'use strict';
+
+   var ViewTemplate = Base.extend({
+      _template: template,
+      _wasPatched: false,
+      _beforeMount: function() {
+         try {
+            /* TODO: set to presentation service */
+            process.domain.req.compatible = true;
+         } catch (e) {
+         }
+         var rightsInitialized = new Deferred();
+         this._forceUpdate = function() {
+
+         };
+         if (typeof window !== 'undefined') {
+            Env.constants.rights = true;
+            Layer.load(undefined, true).addCallback(function() {
+               rightsInitialized.callback();
+            });
+            return rightsInitialized;
+         }
+      },
+      _afterMount: function() {
+         for (var i in this._children) {
+            this._children[i]._forceUpdate = function() {
+
+            };
+            this._children[i]._shouldUpdate = function() {
+               return false;
+            };
+         }
+         require(['Lib/StickyHeader/StickyHeaderMediator/StickyHeaderMediator'], function() {
+            EnvEvent.Bus.globalChannel().notify('bootupReady', { error: '' });
+         });
+      },
+      _shouldUpdate: function() {
+         return false;
+      }
+   });
+
+   return ViewTemplate;
+});
