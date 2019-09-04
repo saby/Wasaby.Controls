6d5504ab386e626a4bcb53653d574169b4e869fe
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-09-04 12:08:23 +0300

    https://online.sbis.ru/opendoc.html?guid=3ead15f4-bd08-4f1d-95cc-d6c310ac644f

--- Controls/_validate/Validators/IsValidDateRange.ts
+++ Controls/_validate/Validators/IsValidDateRange.ts
@@ -34,12 +34,20 @@ import dateUtils = require('Controls/Utils/Date');
  *
  */
 
+function isValidDate(startValue: Date, endValue: Date): boolean {
+   const start = new Date(startValue.getTime());
+   const end = new Date(endValue.getTime());
+   start.setHours(0, 0, 0, 0); // Убираю часы, минуты, секунды
+   end.setHours(0, 0, 0, 0); // Убираю часы, минуты, секунды
+   return start.getTime() <= end.getTime();
+}
+
 export default function isValidDateRange(args): boolean {
    if (args.doNotValidate || !args.startValue || !args.endValue ||
        !dateUtils.isValidDate(args.startValue) || !dateUtils.isValidDate(args.endValue) ||
-       args.startValue <= args.endValue) {
+       isValidDate(args.startValue, args.endValue)) {
       return true;
    }
 
-   return rk('Дата конца периода должна быть больше даты начала.');
+   return rk('Дата начала периода не может быть больше даты окончания');
 }
