d1e256649358988f50f8a9dd812d351b79b25343
aa.artemeva1 <aa.artemeva1@tensor.ru>
2019-09-04 15:02:35 +0300

    Перевод контрола

--- Controls/Pending.ts
+++ Controls/Pending.ts
@@ -5,45 +5,42 @@ import ParallelDeferred = require('Core/ParallelDeferred');
 
 
    /**
-    * Контрол (HOC), который помогает установить порядок выполнения действий в системе.
-    * Или, более конкретно, контролирует выполнение необходимых действий, которые должны быть завершены до начала текущего действия.
-    * Текущее действие запрашивает экземпляр класса Deferred, который будет выполнен после завершения всех необходимых действий.
+    * Контрол (HOC), контролирующий выполнение необходимых действий, которые должны быть завершены до начала текущего действия.
+    * Текущее действие запрашивает экземпляр класса Promise, который будет завершен после того, как завершатся все необходимые действия.
     * Например, всплывающее окно должно быть закрыто только после сохранения/не сохранения измененных данных, которые содержит это всплывающее окно.
     *
     * Pending - это зарегистрированное в текущем экземпляре класса Controls/Pending необходимое действие, которое должно завершиться до начала текущего действия.
-    * Поэтому все пендинги должны быть завершены для разблокировки следующего действия.
+    * Поэтому все пендинги должны быть завершены для разблокировки и запуска следующего действия.
     * @remark
     * Controls/Pending может запросить подтверждение перед закрытием вкладки/браузера, если пендинг зарегистрирован.
-    * Controls/Pending имеет собственный LoadingIndicator, который может отображаться во время ожидания выполнения. Этот LoadingIndicator имеет параметры по умолчанию.
+    * Controls/Pending имеет собственный LoadingIndicator, который может отображаться во время ожидания завершения. Этот LoadingIndicator имеет параметры по умолчанию.
     * В момент, когда будет зарегистрирован первый пендинг с параметром showLoadingIndicator = true, LoadingIndicator отобразит индикатор.
     * В момент, когда последний пендинг с параметром showLoadingIndicator = true завершится, индикатор скроется.
     *
     * Controls/Pending обрабатывает 2 события: registerPending и cancelFinishingPending.
     *
     * registerPending - регистрация пендинга
-    * registerPending имеет 2 аргумента: [deferred, config].
-    * dererred - пендинг будет отменен, когда Deferred будет выполнен.
+    * registerPending имеет 2 аргумента: [promise, config].
+    * promise - пендинг будет отменен, когда Promise будет завершен.
     * config - это объект с параметрами:
     *    - showLoadingIndicator (Boolean) - показывать индикатор загрузки или нет (во время регистрации пендинга)
     *    - onPendingFail (Function) - будет вызвана при попытке завершить пендинг (вызовы finishPendingOperations).
-    *    Функция помогает выполнить Deferred. Пользователь должен разрешить этот _deferred_ (второй аргумент) в этой функции.
-    *    It would be synchronous or asynchronous resolving.
+    *    Функция помогает завершить Promise. Пользователь должен завершить Promise (второй аргумент) в этой функции.
+    *    Это может быть синхронное или асинхронное завершение. 
     *
-    * onPendingFail имеет 2 аргумента - [forceFinishValue, deferred].
-    * forceFinishValue дает дополнительную информацию о том, как resolve deferred.
-    * deferred - это значение пендинга. User must resolve this deferred in onPendingFail function.
-    * forceFinishValue берется из аргумента finishPendingOperations (finishPendingOperations defines additional information of resolving).
+    * onPendingFail имеет 2 аргумента - [forceFinishValue, resultPromise].
+    * forceFinishValue дает дополнительную информацию о том, как resolve Promise.
+    * resultPromise - Promise, по завершению которого завершается пендинг. Мы должны завершить его в функции onPendingFail.
+    * forceFinishValue берется из аргумента finishPendingOperations (finishPendingOperations дает дополнительную информацию о завершении).
     * Пользователь может использовать этот аргумент в своей собственной функции onPendingFail.
     * Например, если в пендинге зарегистрирована измененная запись и нам нужно сохранить изменения, по умолчанию мы можем запросить подтверждение сохранения.
-    * Но forceFinishValue может давать принудительный овтет и мы можем сохранить (или отменить) запись без подтверждения действия.
+    * Но forceFinishValue может завершаться принудительно без подтверждения сохранения.
     *
-    * cancelFinishingPending - отменяет deferred, который возвращен finishPendingOperations. This deferred never resolve. It's need
-    * to request new deferred by finishPendingOperations for setting callback on pendings finish.
-    * It can be useful when pending can't be resolved now but will be resolve later another way.
-    * For example, popup waiting finish of pendings before close, but record can not be saved because of validation errors.
-    * In this case, if we don't cancel deferred by finishPendingOperations, popup will be closed later when validation errors
-    * will be corrected. It will be unexpected closing of popup for user who maybe don't want to close popup anymore
-    * in the light of developments.
+    * cancelFinishingPending - отменяет Promise, который возвращен finishPendingOperations. Этот Promise никогда не завершится. 
+    * Полезно использовать, когда пендинг не может быть завершен сейчас, но будет завершен позднее другим способом.
+    * Например, когда всплывающее окно ожидает завершения пендингов перед закрытием, но запись не может быть сохранена из-за ошибок валидации.
+    * В этом случае, если мы не отменим Promise с помощью finishPendingOperations, всплывающеe окно будет закрыто позже, когда будут исправлены ошибки валидации.
+    * Тогда окно закроется неожиданно для пользователя, который, возможно, не хотел его закрывать.
     *
     * @class Controls/Pending
     * @extends Core/Control
@@ -70,27 +67,27 @@ import ParallelDeferred = require('Core/ParallelDeferred');
     * Controls/Pending is waiting 2 events: registerPending and cancelFinishingPending.
     *
     * registerPending - registrate the pending
-    * registerPending has 2 arguments: [deferred, config].
-    * dererred - pending will be unregistered when this deferred resolves
+    * registerPending has 2 arguments: [Promise, config].
+    * dererred - pending will be unregistered when this Promise resolves
     * config is object having properties:
     *    - showLoadingIndicator (Boolean) - show loading indicator or not (during time while pending is registered)
     *    - onPendingFail (Function) - It will be called when trying to finish pendings (finishPendingOperations calls).
-    *    function helps to resolve deferred of pending. User must resolve this deferred (second argument) in this function.
+    *    function helps to resolve Promise of pending. User must resolve this Promise (second argument) in this function.
     *    It would be synchronous or asynchronous resolving.
     *
-    * onPendingFail has 2 arguments - [forceFinishValue, deferred].
-    * first argument (forceFinishValue) give additional information how to resolve deferred.
-    * second argument is deferred value of pending. User must resolve this deferred in onPendingFail function.
+    * onPendingFail has 2 arguments - [forceFinishValue, resultPromise].
+    * first argument (forceFinishValue) give additional information how to resolve Promise.
+    * second argument is Promise value of pending. User must resolve this Promise in onPendingFail function.
     * forceFinishValue is taken from finishPendingOperations argument (finishPendingOperations defines additional information of resolving).
     * User can use this argument in it's own onPendingFail function or not.
     * For example, if pending registered by changed record and we need to save changes, by default we can ask a question about it.
     * But forceFinishValue can means forced answer and we can save (or not) record without asking a question.
     *
-    * cancelFinishingPending - cancels deferred returned by finishPendingOperations. This deferred never resolve. It's need
-    * to request new deferred by finishPendingOperations for setting callback on pendings finish.
+    * cancelFinishingPending - cancels Promise returned by finishPendingOperations. This Promise never resolve. It's need
+    * to request new Promise by finishPendingOperations for setting callback on pendings finish.
     * It can be useful when pending can't be resolved now but will be resolve later another way.
     * For example, popup waiting finish of pendings before close, but record can not be saved because of validation errors.
-    * In this case, if we don't cancel deferred by finishPendingOperations, popup will be closed later when validation errors
+    * In this case, if we don't cancel Promise by finishPendingOperations, popup will be closed later when validation errors
     * will be corrected. It will be unexpected closing of popup for user who maybe don't want to close popup anymore
     * in the light of developments.
     *
@@ -103,8 +100,8 @@ import ParallelDeferred = require('Core/ParallelDeferred');
     
 
    /**
-    * @event pendingsFinished Событие произойдет в момент, в Controls/Pending не останется пендингов
-    * (after moment of last pending is resolving).
+    * @event pendingsFinished Событие произойдет в момент, когда в Controls/Pending не останется пендингов.
+    * (после того, как последний пендинг завершится).
     * @param {SyntheticEvent} eventObject.
     */
 
@@ -139,7 +136,7 @@ import ParallelDeferred = require('Core/ParallelDeferred');
          config = config || {};
          this._pendings[cnt] = {
 
-            // its deferred what signalling about pending finish
+            // its Promise what signalling about pending finish
             def: def,
 
             validate: config.validate,
@@ -153,7 +150,7 @@ import ParallelDeferred = require('Core/ParallelDeferred');
             showLoadingIndicator: config.showLoadingIndicator
          };
          if (config.showLoadingIndicator && !def.isReady()) {
-            // show indicator if deferred still not finished on moment of registration
+            // show indicator if Promise still not finished on moment of registration
             this._pendings[cnt].loadingIndicatorId = this._children.loadingIndicator.show({ id: this._pendings[cnt].loadingIndicatorId });
          }
 
@@ -204,27 +201,25 @@ import ParallelDeferred = require('Core/ParallelDeferred');
       },
 
       /**
-       * Method returns deferred resolving when all pendings will be resolved.
-       * deferred callbacks with array of results of pendings.
-       * If one of pending's deferred will be rejected (call errback), deferred of finishPendingOperations will be rejected too.
-       * If finishPendingOperations will be called some times, only last call will be actual, but another returned deferreds
-       * will be cancelled.
-       * When finishPendingOperations calling, every pending trying to finish by calling it's onPendingFail method.
-       * If onPendingFail is not setted, pending registration notified control is responsible for pending's deferred resolving.
-       * @param forceFinishValue this argument use as argument of onPendingFail.
-       * @returns {Deferred} deferred resolving when all pendings will be resolved
+       * Метод вернет завершенный Promise, когда все пендинги будут завершены.
+       * Функции обратного вызова Promise с массивом результатов пендингов.
+       * Если один из Promise'ов пендинга будет отклонен (вызовется errback), Promise также будет отклонен с помощью finishPendingOperations.
+       * Если finishPendingOperations будет вызываться несколько раз, будет актуален только последний вызов, а другие возвращенные Promise'ы будут отменены.
+       * Когда finishPendingOperations вызывается, каждый пендинг пытается завершится путем вызова метода onPendingFail.
+       * @param forceFinishValue этот аргумент используется в качестве аргумента onPendingFail.
+       * @returns {Deferred} Завершение Promise'а, когда все пендинги будут завершены.
        */
 
       /*
-       * Method returns deferred resolving when all pendings will be resolved.
-       * deferred callbacks with array of results of pendings.
-       * If one of pending's deferred will be rejected (call errback), deferred of finishPendingOperations will be rejected too.
-       * If finishPendingOperations will be called some times, only last call will be actual, but another returned deferreds
+       * Method returns Promise resolving when all pendings will be resolved.
+       * Promise callbacks with array of results of pendings.
+       * If one of pending's Promise will be rejected (call errback), Promise of finishPendingOperations will be rejected too.
+       * If finishPendingOperations will be called some times, only last call will be actual, but another returned Promises
        * will be cancelled.
        * When finishPendingOperations calling, every pending trying to finish by calling it's onPendingFail method.
-       * If onPendingFail is not setted, pending registration notified control is responsible for pending's deferred resolving.
+       * If onPendingFail is not setted, pending registration notified control is responsible for pending's Promise resolving.
        * @param forceFinishValue this argument use as argument of onPendingFail.
-       * @returns {Deferred} deferred resolving when all pendings will be resolved
+       * @returns {Deferred} Promise resolving when all pendings will be resolved
        */       
       finishPendingOperations: function(forceFinishValue) {
          var resultDeferred = new Deferred(),
@@ -276,7 +271,7 @@ import ParallelDeferred = require('Core/ParallelDeferred');
       },
       _cancelFinishingPending: function() {
          if (this._parallelDef) {
-            // its need to cancel result deferred of parallel defered. reset state of deferred to achieve it.
+            // its need to cancel result Promise of parallel defered. reset state of Promise to achieve it.
             this._parallelDef._fired = -1;
             this._parallelDef.cancel();
          }
