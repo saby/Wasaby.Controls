d016a272952e8f479d211f69b78194146e9e93fc
am.gerasimov <am.gerasimov@tensor.ru>
2019-09-05 12:34:15 +0300

    https://online.sbis.ru/opendoc.html?guid=bca4f969-9f27-4d4a-bc64-88bd88a5f012

--- Controls/_search/Input/Search.ts
+++ Controls/_search/Input/Search.ts
@@ -184,6 +184,14 @@ var Search = Base.extend({
       this.activate();
    },
 
+   _keyDownHandler: function(event) {
+      if (event.nativeEvent.which === Env.constants.key.enter) {
+         event.stopPropagation();
+      }
+
+      Search.superclass._keyDownHandler.apply(this, arguments);
+   },
+
    _keyUpHandler: function(event) {
       if (event.nativeEvent.which === Env.constants.key.enter) {
          this._searchClick();
