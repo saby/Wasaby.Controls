af9306b0db34e8487e2f68fda8cc68080f2478e8
ne.andruh <ne.andruh@tensor.ru>
2019-09-18 17:58:42 +0700

    add

--- Controls/_grid/utils/GridIsEqualUtil.ts
+++ Controls/_grid/utils/GridIsEqualUtil.ts
@@ -10,9 +10,18 @@ function isEqualWithSkip(obj1, obj2, skipFields) {
       return false;
    }
    for (var i = 0; i < obj1.length; i++) {
-      for (var j in obj1[i]) {
-         if ((!skipFields[j] || !(obj1[i][j] instanceof Object)) && obj1[i].hasOwnProperty(j)) {
-            if (!obj2[i].hasOwnProperty(j) || obj1[i][j] !== obj2[i][j]) {
+      const props = [];
+      for (const field1 in obj1[i]) {
+         props.push(field1);
+      }
+      for (const field2 in obj2[i]) {
+         if (props.indexOf(field2) === -1) {
+            props.push(field2);
+         }
+      }
+      for (const j of props) {
+         if ((!skipFields[j] || !(obj1[i][j] instanceof Object)) && (obj1[i].hasOwnProperty(j) || obj2[i].hasOwnProperty(j))) {
+            if (obj1[i][j] !== obj2[i][j]) {
                return false;
             }
          }
