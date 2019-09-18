c55e260095c680b1646474706bba147c0985e97e
ps.kolesova <ps.kolesova@tensor.ru>
2019-09-18 12:58:16 +0300

    https://online.sbis.ru/opendoc.html?guid=ccade1b1-3cdb-4f89-ac97-11724e7c0bf5

--- Controls/_popupTemplate/Dialog/Opener/DialogController.ts
+++ Controls/_popupTemplate/Dialog/Opener/DialogController.ts
@@ -1,6 +1,5 @@
 import BaseController = require('Controls/_popupTemplate/BaseController');
 import DialogStrategy = require('Controls/_popupTemplate/Dialog/Opener/DialogStrategy');
-import Env = require('Env/Env');
 
 let _private = {
     prepareConfig(item, sizes) {
@@ -74,14 +73,6 @@ let DialogController = BaseController.extend({
 
         /* end: We remove the set values that affect the size and positioning to get the real size of the content */
         this.prepareConfig(cfg, container);
-
-        // container could be hidden by scroll after keyboard animation.
-        // position popup in the visible part of the screen
-        if (Env.detection.isMobileIOS) {
-            setTimeout(() => {
-                container.scrollIntoView({behavior: 'smooth'});
-            }, 500);
-        }
 
         /* start: Return all values to the node. Need for vdom synchronizer */
         container.style.width = width;
