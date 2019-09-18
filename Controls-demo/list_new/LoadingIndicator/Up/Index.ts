89247f0edf1b461f369a7ce526ea67341abcca1c
ea.rodionov1 <ea.rodionov1@tensor.ru>
2019-09-18 13:32:25 +0300

    https://online.sbis.ru/opendoc.html?guid=190189c5-5b54-437c-bb0a-7a4036afd49f

---
+++ Controls-demo/list_new/LoadingIndicator/Up/Index.ts
@@ -0,0 +1,38 @@
+import {Control, TemplateFunction} from "UI/Base"
+import * as Template from "wml!Controls-demo/list_new/LoadingIndicator/Up/Up"
+import {Memory} from "Types/source"
+import {generateData} from "../../DemoHelpers/DataCatalog"
+import 'css!Controls-demo/Controls-demo'
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    private _viewSource: Memory;
+    private _dataArray = generateData<{id: number, title: string}>({
+        count: 100,
+        beforeCreateItemCallback: (item) => {
+            item.title = `Запись списка с id = ${item.id}.`
+        }
+    });
+
+    protected _beforeMount() {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: this._dataArray
+        });
+        this._slowDownSource(this._viewSource, 5000);
+    }
+
+    private _slowDownSource(source: Memory, timeMs: number) {
+        const ariginalQuery = source.query;
+
+        source.query = function() {
+            const args = arguments;
+            return new Promise(function(success) {
+                setTimeout(function() {
+                    success(ariginalQuery.apply(source, args));
+                }, timeMs);
+            });
+        };
+    }
+
+}
