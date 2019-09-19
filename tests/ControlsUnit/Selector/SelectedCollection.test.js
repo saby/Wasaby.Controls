5fa2a494582f4c673a164c7867dd144a103f816c
am.gerasimov <am.gerasimov@tensor.ru>
2019-09-19 10:38:44 +0300

    https://online.sbis.ru/opendoc.html?guid=fbf51b54-8ffe-4a0a-95fa-e45f70be8266

--- tests/ControlsUnit/Selector/SelectedCollection.test.js
+++ tests/ControlsUnit/Selector/SelectedCollection.test.js
@@ -1,8 +1,8 @@
-define(['Controls/lookup'], function(scroll) {
+define(['Controls/lookup', 'Types/entity'], function(lookup, entity) {
    describe('"Controls/_lookup/SelectedCollection', function() {
       if (typeof window === 'undefined') {
          // Кастыль, дабы избежать работы с версткой
-         scroll.Collection._private.getCounterWidth = function(itemsCount, readOnly, itemsLayout) {
+         lookup.Collection._private.getCounterWidth = function(itemsCount, readOnly, itemsLayout) {
             // in mode read only and single line, counter does not affect the collection
             if (readOnly && itemsLayout === 'oneRow') {
                return 0;
@@ -23,7 +23,7 @@ define(['Controls/lookup'], function(scroll) {
                _onResult: function(){}
             };
 
-         self._templateOptions = scroll.Collection._private.getTemplateOptions(self);
+         self._templateOptions = lookup.Collection._private.getTemplateOptions(self);
          assert.deepEqual(self._templateOptions.items, items);
 
          // Проверка на то что список элементов не будет меняться по ссылке
@@ -32,16 +32,16 @@ define(['Controls/lookup'], function(scroll) {
       });
 
       it('isShowCounter', function() {
-         assert.isFalse(scroll.Collection._private.isShowCounter(1, 2));
-         assert.isFalse(scroll.Collection._private.isShowCounter(2, 2));
-         assert.isTrue(scroll.Collection._private.isShowCounter(3, 2));
+         assert.isFalse(lookup.Collection._private.isShowCounter(1, 2));
+         assert.isFalse(lookup.Collection._private.isShowCounter(2, 2));
+         assert.isTrue(lookup.Collection._private.isShowCounter(3, 2));
 
       });
 
       it('_afterMount', function() {
          var
             isUpdate = false,
-            selectedCollection = new scroll.Collection();
+            selectedCollection = new lookup.Collection();
 
          selectedCollection._counterWidth = 0;
          selectedCollection._items = ['test', 7, 2];
@@ -58,6 +58,43 @@ define(['Controls/lookup'], function(scroll) {
          selectedCollection._afterMount();
          assert.notEqual(selectedCollection._counterWidth, 0);
          assert.isTrue(isUpdate);
+      });
+
+      it('_itemClick', function() {
+         var sandbox = sinon.createSandbox();
+         var propagationStopped = false;
+         var selectedCollection = new lookup.Collection();
+         var model = new entity.Model({
+            rawData: {
+               id: 'test',
+            },
+            keyProperty: 'id'
+         });
+         var currentSelector;
+         var event = {
+            target: {
+               closest: function(cssSelector) {
+                  return cssSelector === currentSelector;
+               }
+            },
+            stopPropagation: function() {
+               propagationStopped = true;
+            }
+         };
+
+         sandbox.stub(selectedCollection, '_notify');
+
+         currentSelector = '.js-controls-SelectedCollection__item__caption';
+         selectedCollection._itemClick(event, model);
+         sinon.assert.calledWith(selectedCollection._notify, 'itemClick', [model]);
+         assert.isTrue(propagationStopped);
+
+         currentSelector = '.js-controls-SelectedCollection__item__cross';
+         selectedCollection._itemClick(event, model);
+         sinon.assert.calledWith(selectedCollection._notify, 'crossClick', [model]);
+         assert.isTrue(propagationStopped);
+
+         sandbox.restore();
       });
    });
 });
