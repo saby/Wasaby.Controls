a73e549057dcd2ce7a06ae62791fb9b168723c0c
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-09-19 13:34:35 +0300

    https://online.sbis.ru/opendoc.html?guid=f95624db-bd54-412e-bf6f-a97314007f25 По правкам

--- Controls/_grid/interface/IGridControl.ts
+++ Controls/_grid/interface/IGridControl.ts
@@ -164,9 +164,10 @@
  * - <a href="/doc/platform/developmentapl/interface-development/controls/list/grid/templates/header/">См. руководство разработчика</a>
  * - <a href="/materials/demo-ws4-grid-base">См. демо-пример</a>
  * @remark
- * По умолчанию используется базовый шаблон {@link Controls/grid:HeaderContent https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/grid/templates/header/}.
- * Базовый шаблон поддерживает следующие css-модификаторы:
- * - controls-Grid__header-cell_spacing_money — используется для добавления отступа в заголовке столбца при рендере денежных данных в самом столбце.
+ * По умолчанию используется базовый шаблон Controls/grid:HeaderContent.
+ *Подробнее о работе с шаблоном читайте в <a href="https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/grid/templates/header/">документации</a>.
+ *
+ * 
  *
  * @example
  * Пример добавления пользовательскогого шаблона:
