9aa54e9b2bba92d351c9d46391aee048f5617a0b
am.gerasimov <am.gerasimov@tensor.ru>
2019-09-23 11:01:31 +0300

    https://online.sbis.ru/opendoc.html?guid=128e2090-e282-4816-8449-fac4acf93893

--- Controls/_popupTemplate/Sticky/StickyController.ts
+++ Controls/_popupTemplate/Sticky/StickyController.ts
@@ -215,6 +215,11 @@ const StickyController = BaseController.extend({
         container.style.height = height;
 
         /* end: Return all values to the node. Need for vdom synchronizer */
+
+        // toDO выписана задача https://online.sbis.ru/opendoc.html?guid=79cdc24c-cf4c-45da-97b4-7353540a2b1b
+        if (item.popupOptions.resizeCallback instanceof Function) {
+            item.popupOptions.resizeCallback();
+        }
         return true;
     },
 
