5e0baedc16ee2b17b1f4c1d911a57cebb982f227
vd.volockoy <vd.volockoy@tensor.ru>
2019-09-25 11:19:11 +0300

    Revert "Поправил юниты"
    This reverts commit c22db084af56dbd469a5430f6a00bef492a6c257.

--- Controls/Utils/reflow.ts
+++ Controls/Utils/reflow.ts
@@ -1,4 +1,5 @@
 import {throttle} from 'Types/function';
+import {constants} from 'Env/Env';
 
 const MINIMAL_DEVICE_REFLOW_DELAY = 100;
 
@@ -6,9 +7,11 @@ const MINIMAL_DEVICE_REFLOW_DELAY = 100;
  * Функция вызывающая forced reflow с корня DOM-дерева
  */
 export default throttle(() => {
-    document.body.style.transform = 'translate(0px)';
+    if (constants.isBrowserPlatform) {
+        document.body.style.transform = 'translate(0px)';
 
-    setTimeout(() => {
-        document.body.style.transform = '';
-    }, MINIMAL_DEVICE_REFLOW_DELAY);
+        setTimeout(() => {
+            document.body.style.transform = '';
+        }, MINIMAL_DEVICE_REFLOW_DELAY);
+    }
 }, MINIMAL_DEVICE_REFLOW_DELAY, true);
