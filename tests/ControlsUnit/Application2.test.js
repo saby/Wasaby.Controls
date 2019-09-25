63eb3553ab4f05b4efa88dbd8f5e6aaeb420b63a
vd.volockoy <vd.volockoy@tensor.ru>
2019-09-25 11:19:38 +0300

    Revert "Поправил юниты"
    This reverts commit dc9cccd06e321743a5950dbd919425057d87dda6.

--- tests/ControlsUnit/Application2.test.js
+++ tests/ControlsUnit/Application2.test.js
@@ -6,13 +6,6 @@ define([
    'Controls/Application'
 ], function(Env, Application) {
    describe('Controls.Application2', function() {
-      const trueMobileIOS = Env.detection.isMobileIOS;
-
-      if (typeof window === 'undefined') {
-         Env.detection['test::isMobileIOS'] = true;
-      } else {
-         Env.detection.isMobileIOS = true;
-      }
 
       describe('popup logic', function() {
          it('not mobile', function () {
@@ -36,7 +29,14 @@ define([
          });
 
          it('ios', function () {
-            let ctrl = new Application({});
+            let ctrl = new Application({}),
+               oldIsMobileIOS = Env.detection.isMobileIOS;
+
+            if (typeof window === 'undefined') {
+               Env.detection['test::isMobileIOS'] = true;
+            } else {
+               Env.detection.isMobileIOS = true;
+            }
 
             ctrl._popupCreatedHandler();
             assert.equal(ctrl._scrollingClass, 'controls-Scroll_webkitOverflowScrollingAuto');
@@ -52,13 +52,14 @@ define([
             assert.equal(ctrl._scrollingClass, 'controls-Scroll_webkitOverflowScrollingAuto');
             ctrl._suggestStateChangedHandler(null, false);
             assert.equal(ctrl._scrollingClass, 'controls-Scroll_webkitOverflowScrollingTouch');
+
+
+            if (typeof window === 'undefined') {
+               Env.detection['test::isMobileIOS'] = undefined;
+            } else {
+               Env.detection.isMobileIOS = oldIsMobileIOS;
+            }
          });
       });
-
-      if (typeof window === 'undefined') {
-         Env.detection['test::isMobileIOS'] = undefined;
-      } else {
-         Env.detection.isMobileIOS = trueMobileIOS;
-      }
    });
 });
