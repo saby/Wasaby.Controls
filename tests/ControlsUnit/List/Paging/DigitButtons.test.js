a4ddb48eac38eeacb37d9006bb79fcb4e380e566
kraynovdo <do.kraynov@tensor.ru>
2019-09-26 16:33:59 +0300

    https://online.sbis.ru/opendoc.html?guid=c6a2ef49-509e-4890-bb44-fe14fa2cc2b0

--- tests/ControlsUnit/List/Paging/DigitButtons.test.js
+++ tests/ControlsUnit/List/Paging/DigitButtons.test.js
@@ -4,66 +4,63 @@
 define([
    'Controls/_paging/Paging/DigitButtons'
 ], function(DigitButtons) {
+   var DBClass = DigitButtons.default;
    describe('Controls.List.Paging.DigitButtons', function() {
       it('getDrawnDigits 10 pages', function() {
-         var db, digits;
-         db = new DigitButtons.default({});
-         digits = db._getDrawnDigits(10, 1);
+         var digits;
+         digits = DBClass._getDrawnDigits(10, 1);
          assert.deepEqual([1, 2, 3, 4, '...', 10], digits, 'getDrawnDigits10 test case 1: WrongResult');
 
-         digits = db._getDrawnDigits(10, 2);
+         digits = DBClass._getDrawnDigits(10, 2);
          assert.deepEqual([1, 2, 3, 4, 5, '...', 10], digits, 'getDrawnDigits10 test case 2: WrongResult');
 
-         digits = db._getDrawnDigits(10, 3);
+         digits = DBClass._getDrawnDigits(10, 3);
          assert.deepEqual([1, 2, 3, 4, 5, 6, '...', 10], digits, 'getDrawnDigits10 test case 3: WrongResult');
 
-         digits = db._getDrawnDigits(10, 4);
+         digits = DBClass._getDrawnDigits(10, 4);
          assert.deepEqual([1, 2, 3, 4, 5, 6, 7, '...', 10], digits, 'getDrawnDigits10 test case 4: WrongResult');
 
-         digits = db._getDrawnDigits(10, 5);
+         digits = DBClass._getDrawnDigits(10, 5);
          assert.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 5: WrongResult');
 
-         digits = db._getDrawnDigits(10, 6);
+         digits = DBClass._getDrawnDigits(10, 6);
          assert.deepEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 6: WrongResult');
 
-         digits = db._getDrawnDigits(10, 7);
+         digits = DBClass._getDrawnDigits(10, 7);
          assert.deepEqual([1, '...', 4, 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 7: WrongResult');
 
-         digits = db._getDrawnDigits(10, 8);
+         digits = DBClass._getDrawnDigits(10, 8);
          assert.deepEqual([1, '...', 5, 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 8: WrongResult');
 
-         digits = db._getDrawnDigits(10, 9);
+         digits = DBClass._getDrawnDigits(10, 9);
          assert.deepEqual([1, '...', 6, 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 9: WrongResult');
 
-         digits = db._getDrawnDigits(10, 10);
+         digits = DBClass._getDrawnDigits(10, 10);
          assert.deepEqual([1, '...', 7, 8, 9, 10], digits, 'getDrawnDigits10 test case 10: WrongResult');
       });
 
       it('getDrawnDigits 15 pages', function() {
-         var db, digits;
-         db = new DigitButtons.default({});
-
-         digits = db._getDrawnDigits(15, 7);
+         var digits;
+         digits = DBClass._getDrawnDigits(15, 7);
          assert.deepEqual([1, '...', 4, 5, 6, 7, 8, 9, 10, '...', 15], digits, 'getDrawnDigits15 test case 7: WrongResult');
       });
 
       it('getDrawnDigits 5 pages', function() {
-         var db, digits;
-         db = new DigitButtons.default({});
+         var digits;
 
-         digits = db._getDrawnDigits(5, 1);
+         digits = DBClass._getDrawnDigits(5, 1);
          assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 1: WrongResult');
 
-         digits = db._getDrawnDigits(5, 2);
+         digits = DBClass._getDrawnDigits(5, 2);
          assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 2: WrongResult');
 
-         digits = db._getDrawnDigits(5, 3);
+         digits = DBClass._getDrawnDigits(5, 3);
          assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 3: WrongResult');
 
-         digits = db._getDrawnDigits(5, 4);
+         digits = DBClass._getDrawnDigits(5, 4);
          assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 4: WrongResult');
 
-         digits = db._getDrawnDigits(5, 5);
+         digits = DBClass._getDrawnDigits(5, 5);
          assert.deepEqual([1, 2, 3, 4, 5], digits, 'getDrawnDigits5 test case 5: WrongResult');
       });
    });
