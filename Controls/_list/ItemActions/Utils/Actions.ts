0e770718d0b3163360dd915e51c35edb98c95131
ea.rodionov1 <ea.rodionov1@tensor.ru>
2019-09-27 13:56:27 +0300

    https://online.sbis.ru/opendoc.html?guid=fad3d7db-3d16-4095-857c-f9c168b4aca2

--- Controls/_list/ItemActions/Utils/Actions.ts
+++ Controls/_list/ItemActions/Utils/Actions.ts
@@ -8,12 +8,14 @@ export = {
         } else {
             //TODO: self._container может быть не HTMLElement, а jQuery-элементом, убрать после https://online.sbis.ru/opendoc.html?guid=d7b89438-00b0-404f-b3d9-cc7e02e61bb3
             var container = self._container.get ? self._container.get(0) : self._container;
-
-
-
             let targetContainer;
 
-            // FIXME: https://online.sbis.ru/opendoc.html?guid=fad3d7db-3d16-4095-857c-f9c168b4aca2
+            /*
+            * https://online.sbis.ru/opendoc.html?guid=5d2c482e-2b2f-417b-98d2-8364c454e635
+            * под опцией, чтобы не отломать никому ничего.
+            * опцию спилит Егор при переходе на table-layout и отказе от partialGrid
+            * https://online.sbis.ru/doc/5d2c482e-2b2f-417b-98d2-8364c454e635
+            * */
             if (self._options.fix1177894367) {
                 targetContainer = event.target.closest('.controls-Grid__row-cell');
             } else {
@@ -21,8 +23,6 @@ export = {
                     return item.className.indexOf('controls-ListView__itemV') !== -1;
                 })[itemData.index - listModel.getStartIndex()];
             }
-
-
 
             const args = [
                 action,
