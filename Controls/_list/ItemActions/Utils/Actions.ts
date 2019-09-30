bc862de038cccadeaa2a6c879057dd30fda7ee1f
ea.rodionov1 <ea.rodionov1@tensor.ru>
2019-09-30 18:15:17 +0300

    https://online.sbis.ru/opendoc.html?guid=fad3d7db-3d16-4095-857c-f9c168b4aca2

--- Controls/_list/ItemActions/Utils/Actions.ts
+++ Controls/_list/ItemActions/Utils/Actions.ts
@@ -1,3 +1,12 @@
+function getActionContainer(event, action, itemData, container, listModel): Element {
+    if (action.showType === 2) {
+        return event.target.closest('.controls-Grid__row-cell');
+    } else {
+        const rowCells = container.querySelectorAll(`.controls-Grid__row-cell[data-r="${itemData.index - listModel.getStartIndex()}"]`);
+        return rowCells[itemData.columnIndex - 1];
+    }
+}
+
 export = {
     itemActionsClick: function (self, event, action, itemData, listModel, showAll) {
         event.stopPropagation();
@@ -7,7 +16,7 @@ export = {
            self._notify('menuActionClick', [itemData, event, action]);
         } else {
             //TODO: self._container может быть не HTMLElement, а jQuery-элементом, убрать после https://online.sbis.ru/opendoc.html?guid=d7b89438-00b0-404f-b3d9-cc7e02e61bb3
-            var container = self._container.get ? self._container.get(0) : self._container;
+            const container = self._container.get ? self._container.get(0) : self._container;
             let targetContainer;
 
             /*
@@ -16,8 +25,8 @@ export = {
             * опцию спилит Егор при переходе на table-layout и отказе от partialGrid
             * https://online.sbis.ru/doc/5d2c482e-2b2f-417b-98d2-8364c454e635
             * */
-            if (self._options.fix1177894367) {
-                targetContainer = event.target.closest('.controls-Grid__row-cell');
+            if (self._options.fix1177894367 && listModel.isPartialGridSupport()) {
+                targetContainer = getActionContainer(event, action, itemData, container, listModel);
             } else {
                 targetContainer = Array.prototype.filter.call(container.querySelector('.controls-ListView__itemV').parentNode.children, function (item) {
                     return item.className.indexOf('controls-ListView__itemV') !== -1;
