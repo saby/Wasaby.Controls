2907b5e70d688f46a893c58e2743ef47c66fe13f
kraynovdo <do.kraynov@tensor.ru>
2019-10-01 14:37:42 +0300

    https://online.sbis.ru/opendoc.html?guid=93e5281d-950e-4573-9dff-1d87edbe4372

--- Controls/_paging/Paging.ts
+++ Controls/_paging/Paging.ts
@@ -2,6 +2,9 @@ import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import pagingTemplate = require('wml!Controls/_paging/Paging/Paging');
 import {SyntheticEvent} from 'Vdom/Vdom';
 
+// TODO перевести на честную тему мешает наличие опции theme на scroll/Container
+import 'css!theme?Controls/paging';
+
 type TButtonState = 'normal' | 'disabled';
 
 export interface IPagingOptions extends IControlOptions {
@@ -104,8 +107,6 @@ class Paging extends Control<IPagingOptions> {
         }
         this._notify('onArrowClick', [btnName]);
     }
-
-    static _theme: string[] = ['Controls/paging'];
 }
 
 export default Paging;
