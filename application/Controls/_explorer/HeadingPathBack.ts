f9f4de0bec738d02ba66284ab90ad201e694344a
ea.melnikova <ea.melnikova@tensor.ru>
2019-10-07 17:10:25 +0300

    https://online.sbis.ru/opendoc.html?guid=eea577e3-9c46-4d4d-b315-272ca94064d8

---
+++ application/Controls/_explorer/HeadingPathBack.ts
@@ -0,0 +1,6 @@
+import Control = require('Core/Control');
+import template = require('wml!Controls/_explorer/HeadingPathBack/HeadingPathBack');
+var HeadingPathBack = Control.extend({
+   _template: template,
+   });
+export = HeadingPathBack;
