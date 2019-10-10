541cac6ef5252b2d5cccdd4058881c52f241fed6
vs.puhanov <vs.puhanov@tensor.ru>
2019-10-10 15:27:32 +0300

    removes code unnecessary for 610

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -597,17 +597,7 @@ var _private = {
 
     // Обновляет стартовый и конечный индексы виртуального окна
     applyVirtualScrollIndexes(self, direction): void {
-        let savedStart: number;
-        let savedStop: number;
-        if (self._options.useNewModel) {
-            savedStart = self._listViewModel.getStartIndex();
-            savedStop = self._listViewModel.getStopIndex();
-        }
         if (_private.applyVirtualScrollIndexesToListModel(self)) {
-            if (self._options.useNewModel) {
-                self._savedStartIndex = savedStart;
-                self._savedStopIndex = savedStop;
-            }
             self._saveAndRestoreScrollPosition = direction;
             self._shouldRestoreScrollPosition = true;
             _private.applyPlaceholdersSizes(self);
