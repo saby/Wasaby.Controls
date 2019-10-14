f02735ade0b85cf5dc2466fd78363b04494ac3fc
ai.konovalova <ai.konovalova@tensor.ru>
2019-10-14 15:42:44 +0300

    target - поправлено описание

--- Controls/_popup/Opener/Sticky.ts
+++ Controls/_popup/Opener/Sticky.ts
@@ -58,7 +58,7 @@ const Sticky = BaseOpener.extend({
      * @property {Number} maxHeight Максимальная высота всплывающего окна
      * @property {Number} height Текущая высота всплывающего окна
      * @property {Number} width Текущая ширина всплывающего окна
-     * @property {Node|Control} target Элемент, относительно которого позиционнируется всплывающее окно.
+     * @property {Node|Control} target Элемент (DOM-элемент или контрол), относительно которого позиционнируется всплывающее окно.
      * @property {Node} opener Логический инициатор открытия всплывающего окна
      * @property {String} fittingMode Определеяет поведение окна, в случае, если оно не помещается на экране с заданным позиционнированием.
      */
@@ -300,7 +300,7 @@ export = Sticky;
 
 /**
  * @name Controls/_popup/Opener/Sticky#target
- * @cfg {Node} target Элемент, относительно которого позиционнируется всплывающее окно.
+ * @cfg {Node} target Элемент (DOM-элемент или контрол), относительно которого позиционнируется всплывающее окно.
  */
 
 /**
