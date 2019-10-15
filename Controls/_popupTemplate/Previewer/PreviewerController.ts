069183b77839e3b9447349513165acaefcf5e895
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-10-15 10:02:05 +0300

    https://online.sbis.ru/opendoc.html?guid=4e4fcbb1-ac58-489b-acac-e2bed915464f

--- Controls/_popupTemplate/Previewer/PreviewerController.ts
+++ Controls/_popupTemplate/Previewer/PreviewerController.ts
@@ -1,14 +1,14 @@
 import Deferred = require('Core/Deferred');
 import StickyController = require('Controls/_popupTemplate/Sticky/StickyController');
+import {IPopupItem} from 'Controls/_popupTemplate/BaseController';
 import 'css!theme?Controls/popupTemplate';
 
 class PreviewerController extends StickyController.constructor {
-    _openedPopupId = null;
-
-    _destroyDeferred = {};
+    _openedPopupId: string|null = null;
+    _destroyDeferred: object = {};
     TYPE: string = 'Previewer';
 
-    elementCreated(element, container, id) {
+    elementCreated(item: IPopupItem, container: HTMLDivElement, id: string): boolean {
         /**
          * Only one window can be opened.
          */
@@ -16,30 +16,29 @@ class PreviewerController extends StickyController.constructor {
             require('Controls/popup').Controller.remove(this._openedPopupId);
         }
         this._openedPopupId = id;
-
         return super.elementCreated.apply(this, arguments);
     }
 
-    elementDestroyed(item) {
+    elementDestroyed(item: IPopupItem): boolean {
         if (item.id === this._openedPopupId) {
             this._openedPopupId = null;
         }
 
         this._destroyDeferred[item.id] = new Deferred();
-
         item.popupOptions.className = (item.popupOptions.className || '') + ' controls-PreviewerController_close';
-
         return this._destroyDeferred[item.id];
     }
 
-    elementAnimated(item) {
+    elementAnimated(item: IPopupItem): boolean {
         if (this._destroyDeferred[item.id]) {
             this._destroyDeferred[item.id].callback();
             delete this._destroyDeferred[item.id];
+            return true;
         }
+        return false;
     }
 
-    needRestoreFocus(isActive) {
+    needRestoreFocus(isActive: boolean): boolean {
         return isActive;
     }
 }
