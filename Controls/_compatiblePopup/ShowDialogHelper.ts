7ac4321967c1c580dac4bc80a3425e5658d2159a
te.maslova <te.maslova@tensor.ru>
2019-10-16 17:01:21 +0700

    Удалены теги

--- Controls/_compatiblePopup/ShowDialogHelper.ts
+++ Controls/_compatiblePopup/ShowDialogHelper.ts
@@ -2,11 +2,6 @@ import Deferred = require('Core/Deferred');
 import moduleStubs = require('Core/moduleStubs');
 import isNewEnvironment = require('Core/helpers/isNewEnvironment');
 
-/**
- * @class Controls/_compatiblePopup/ShowDialogHelpers
- * @public
- */
-
 var _private = {
    prepareDeps: function(config) {
       var dependencies = ['Controls/popup'];
