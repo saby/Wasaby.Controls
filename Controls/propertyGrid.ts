d07fc981f7e472e901df20adffe704418ab779a7
te.maslova <te.maslova@tensor.ru>
2019-10-17 11:47:19 +0700

    Правки

--- Controls/propertyGrid.ts
+++ Controls/propertyGrid.ts
@@ -6,6 +6,8 @@
  * @includes StringEditor Controls/_propertyGrid/defaultEditors/String
  * @includes TextEditor Controls/_propertyGrid/defaultEditors/Text
  * @includes EnumEditor Controls/_propertyGrid/defaultEditors/Enum
+ * @includes IEditor Controls/_propertyGrid/IEditor
+ * @includes IProperty Controls/_propertyGrid/IProperty
  * @author Герасимов А.М.
  */
 
@@ -19,6 +21,8 @@
  * @includes EnumEditor Controls/_propertyGrid/defaultEditors/Enum
  * @includes PropertyGridStyles Controls/_propertyGrid/PropertyGrid/Styles
  * @includes IPropertyGrid Controls/_propertyGrid/IPropertyGrid
+ * @includes IEditor Controls/_propertyGrid/IEditor
+ * @includes IProperty Controls/_propertyGrid/IProperty
  * @author Герасимов А.М.
  */
 
@@ -27,11 +31,15 @@ import BooleanEditor = require("Controls/_propertyGrid/defaultEditors/Boolean");
 import StringEditor = require("Controls/_propertyGrid/defaultEditors/String");
 import TextEditor = require("Controls/_propertyGrid/defaultEditors/Text");
 import EnumEditor = require("Controls/_propertyGrid/defaultEditors/Enum");
+import IEditor = require("Controls/_propertyGrid/IEditor");
+import IProperty = require("Controls/_propertyGrid/IProperty");
 
 export {
     PropertyGrid,
     BooleanEditor,
     StringEditor,
     TextEditor,
-    EnumEditor
+    EnumEditor,
+    IEditor,
+    IProperty
 }
