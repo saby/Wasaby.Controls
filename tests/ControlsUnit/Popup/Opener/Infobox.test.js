ec6df21af7c7c8bd9c74b656838dc8da7612ddc4
av.bondar <av.bondar@tensor.ru>
2019-10-29 11:08:01 +0300

    https://online.sbis.ru/opendoc.html?guid=63205c17-0327-4263-8ddc-43794ebf6858 add test

--- tests/ControlsUnit/Popup/Opener/Infobox.test.js
+++ tests/ControlsUnit/Popup/Opener/Infobox.test.js
@@ -48,7 +48,8 @@ define(
                floatCloseButton: true,
                style: 'error',
                position: 'tl',
-               template: popup.PreviewerTemplate
+               template: popup.PreviewerTemplate,
+               showDelay: 300,
             };
             let Infobox = new popup.InfoboxTarget(config);
             Infobox.saveOptions(config);
@@ -58,6 +59,7 @@ define(
             assert.equal(newConfig.style, 'error');
             assert.equal(newConfig.position, 'tl');
             assert.equal(newConfig.template, popup.PreviewerTemplate);
+            assert.equal(newConfig.showDelay, 300, 'error showDelay');
          });
 
          it('PopupInfoBox: resetTimeOut', () => {
