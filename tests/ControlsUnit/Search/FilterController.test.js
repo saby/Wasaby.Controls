1f7d60170277c130e89a4c13db24f99f0b89a5df
am.gerasimov <am.gerasimov@tensor.ru>
2019-10-29 10:46:36 +0300

    https://online.sbis.ru/opendoc.html?guid=7d2c2eb1-34ff-4f5e-a383-0ab861780fcf

--- tests/ControlsUnit/Search/FilterController.test.js
+++ tests/ControlsUnit/Search/FilterController.test.js
@@ -1,14 +1,34 @@
 define(['Controls/search'], function(search) {
    describe('Controls/search:FilterController', function() {
-      it('_beforeMount with option searchValue', function() {
-         var filterController = new search.FilterController();
+      it('_beforeMount with option searchValue', () => {
+         var filterController = new search.FilterController.default();
 
          filterController._beforeMount({
             searchParam: 'title',
             searchValue: 'test'
          });
 
-         assert.deepEqual(filterController._filter, {title: 'test'});
+         assert.deepEqual(filterController._filter, { title: 'test' });
+
+         filterController._filter = null;
+         filterController._beforeMount({
+            searchValue: 'test'
+         });
+         assert.deepEqual(filterController._filter, {});
+      });
+      it('_beforeUpdate', () => {
+         var filterController = new search.FilterController.default();
+         filterController.saveOptions({
+            filter: { title: 'test' }
+         });
+
+         filterController._beforeUpdate({
+            filter: {
+               title: 'test2'
+            }
+         });
+
+         assert.deepEqual(filterController._filter, { title: 'test2' });
       });
    });
 });
