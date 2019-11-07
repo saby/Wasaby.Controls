76063b22c50258fed7267269e980f771b6ee3d63
ia.kapustin <ia.kapustin@tensor.ru>
2019-11-07 15:31:50 +0300

    https://online.sbis.ru/opendoc.html?guid=f71c4160-141f-4970-aeb8-5edb5041812f
    https://online.sbis.ru/opendoc.html?guid=f71c4160-141f-4970-aeb8-5edb5041812f

--- Controls/_lookup/BaseLookupView/InputRender.ts
+++ Controls/_lookup/BaseLookupView/InputRender.ts
@@ -5,8 +5,17 @@ import template = require('wml!Controls/_lookup/BaseLookupView/InputRender/Input
    var InputRenderLookup = input.Text.extend({
       _template: template,
 
+      _getField: function() {
+         if (this._options.isInputVisible) {
+            return InputRenderLookup.superclass._getField.call(this);
+         } else {
+            // Это необходимо для правильной работы базового InputRender, он ожидает что поле ввода всегда есть в верстке
+            return document.createElement('input');
+         }
+      },
+
       _getReadOnlyField: function() {
-         return InputRenderLookup.superclass._getField.call(this);
+         return this._getField();
       },
 
       _keyDownInput: function(event) {
