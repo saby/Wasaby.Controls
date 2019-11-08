e5733338ef7feaa4fbb2f048b5a50cab4646515e
Герасимов А.М <am.gerasimov@tensor.ru>
2019-11-08 10:25:10 +0300

    Update InputRender.ts

--- Controls/_lookup/BaseLookupView/InputRender.ts
+++ Controls/_lookup/BaseLookupView/InputRender.ts
@@ -9,7 +9,12 @@ import template = require('wml!Controls/_lookup/BaseLookupView/InputRender/Input
          if (this._options.isInputVisible) {
             return InputRenderLookup.superclass._getField.call(this);
          } else {
-            // Это необходимо для правильной работы базового InputRender, он ожидает что поле ввода всегда есть в верстке
+            // В поле связи с единичным выбором после выбора записи
+            // скрывается инпут (технически он в шаблоне создаётся под if'ом),
+            // но базовый input:Text ожидает, что input в вёрстке есть всегда.
+            // Для корректной работы создаём виртуальный input.
+            // Если его скрывать через display: none, то начинаются проблемы с фокусом,
+            // поэтому данный способ нам не подходит.
             return document.createElement('input');
          }
       },
