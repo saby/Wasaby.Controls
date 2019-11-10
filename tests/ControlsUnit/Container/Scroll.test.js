0ccf02ad755304b5d02a41debe669368f264de39
vd.volockoy <vd.volockoy@tensor.ru>
2019-11-10 15:46:59 +0300

    Юниты

--- tests/ControlsUnit/Container/Scroll.test.js
+++ tests/ControlsUnit/Container/Scroll.test.js
@@ -349,6 +349,11 @@ define(
                const
                   addedHeight = 100,
                   oldScrollTop = scroll._children.content.scrollTop;
+               scroll._children.scrollWatcher = {
+                  setScrollTop(value) {
+                     scroll._children.content.scrollTop = value;
+                  }
+               };
                scroll._saveScrollPosition({stopPropagation: function(){}});
                scroll._children.content.scrollHeight += addedHeight;
                scroll._restoreScrollPosition({stopPropagation: function(){}}, 0);
