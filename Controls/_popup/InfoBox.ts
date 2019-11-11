6abba3254df72f0e82dedf545c9690a7f20f6488
aa.bykanov <aa.bykanov@tensor.ru>
2019-11-11 18:14:30 +0300

    Исправлена ссылка в описании к typedef PopupOptions https://online.sbis.ru/opendoc.html?guid=f5e55c73-bacc-484b-82df-7d24b3431f18

--- Controls/_popup/InfoBox.ts
+++ Controls/_popup/InfoBox.ts
@@ -451,7 +451,7 @@ InfoBox.getDefaultOptions = function() {
  * @typedef {Object} PopupOptions
  * @description Конфигурация всплывающей подсказки.
  * @property {function|String} template Шаблон всплывающей подсказки
- * @property {Object} templateOptions Опции для котнрола, переданного в {@link template}
+ * @property {Object} templateOptions Опции для контрола, переданного в {@link Controls/_popup/InfoBox#template}.
  * @property {String} trigger Определяет, какое событие будет иницировать открытие и закрытие всплывающей подсказки.
  * @property {String} targetSide Сторона таргета, относительно которой будет позиционнироваться всплывающая подсказка.
  * @property {String} alignment Выравнивание всплывающей подсказки относительно вызывающего её элемента.
