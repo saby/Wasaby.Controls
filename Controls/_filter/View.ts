f5472baad0a116bcc3976df38b1a1cfba6b320e3
ee.zolotova <ee.zolotova@tensor.ru>
2019-11-13 09:27:27 +0300

    https://online.sbis.ru/opendoc.html?guid=4db631f6-00e1-46f7-affc-449633815829

--- Controls/_filter/View.ts
+++ Controls/_filter/View.ts
@@ -123,10 +123,10 @@ var _private = {
         return popupItems;
     },
 
-    getFolderIds: function(items, nodeProperty, keyProperty) {
+    getFolderIds: function({items, nodeProperty, parentProperty, keyProperty}) {
         let folders = [];
         factory(items).each((item) => {
-            if (item.get(nodeProperty)) {
+            if (item.get(nodeProperty) && !item.get(parentProperty)) {
                 folders.push(item.get(keyProperty));
             }
         });
@@ -326,7 +326,7 @@ var _private = {
 
         let getHierarchySelectedKeys = () => {
             // selectedKeys - { folderId1: [selected keys for folder] , folderId2: [selected keys for folder], ... }
-            let folderIds = _private.getFolderIds(config.items, config.nodeProperty, config.keyProperty);
+            let folderIds = _private.getFolderIds(config);
             factory(folderIds).each((folderId, index) => {
                 selectedKeys[folderId] = [];
                 factory(items).each((item) => {
@@ -377,7 +377,7 @@ var _private = {
     },
 
     prepareHierarchySelection: function(selectedKeys, curConfig, resetValue) {
-        let folderIds = _private.getFolderIds(curConfig.items, curConfig.nodeProperty, curConfig.keyProperty);
+        let folderIds = _private.getFolderIds(curConfig);
         let isEmptySelection = true;
         let onlyFoldersSelected = true;
 
@@ -465,7 +465,7 @@ var _private = {
     },
 
     updateHierarchyHistory: function(currentFilter, selectedItems, source) {
-        let folderIds = _private.getFolderIds(currentFilter.items, currentFilter.nodeProperty, currentFilter.keyProperty);
+        let folderIds = _private.getFolderIds(currentFilter);
 
         let getNodeItems = (parentKey) => {
             let nodeItems = [];
