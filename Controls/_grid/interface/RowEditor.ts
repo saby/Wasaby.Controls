30a8890757fa939e20bac310b394397eaf7b86d8
aa.bykanov <aa.bykanov@tensor.ru>
2019-11-14 12:59:28 +0300

    Внесены правки в описание шаблонов библиотеки grid https://online.sbis.ru/opendoc.html?guid=089a00a3-6573-44a2-af0a-dcc16781cb1b

--- Controls/_grid/interface/RowEditor.ts
+++ Controls/_grid/interface/RowEditor.ts
@@ -2,6 +2,7 @@
  * Шаблон, который используют для настройки отображения элемента в режиме редактирования.
  * @interface Controls/grid:RowEditor
  * @author Авраменко А.С.
+ * @demo demo-ws4-edit-in-place
  * @see Controls/grid:View#itemTemplate
  * @remark
  * Подробнее о работе с шаблоном читайте {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/grid/edit/ здесь}.
