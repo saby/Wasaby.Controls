9bf8da838cb13470be69e0dae5863b684e2a4cee
ia.kapustin <ia.kapustin@tensor.ru>
2019-11-14 21:58:19 +0300

    https://online.sbis.ru/opendoc.html?guid=ebcc2c58-dc3e-40d1-8618-3eb0b2aa5061
    https://online.sbis.ru/opendoc.html?guid=ebcc2c58-dc3e-40d1-8618-3eb0b2aa5061

--- Controls/_list/BaseControl/SelectionController.ts
+++ Controls/_list/BaseControl/SelectionController.ts
@@ -158,6 +158,10 @@ var SelectionController = Control.extend(/** @lends Controls/_list/BaseControl/S
         let selectionChanged = !isEqual(newOptions.selectedKeys, this._multiselection.selectedKeys) ||
            !isEqual(newOptions.excludedKeys, this._multiselection.excludedKeys);
 
+        if (this._options.listModel !== newOptions.listModel) {
+            this._multiselection.setListModel(newOptions.listModel);
+        }
+
         if (newOptions.items !== this._options.items) {
             this._options.items.unsubscribe('onCollectionChange', this._onCollectionChangeHandler);
             newOptions.items.subscribe('onCollectionChange', this._onCollectionChangeHandler);
@@ -168,10 +172,6 @@ var SelectionController = Control.extend(/** @lends Controls/_list/BaseControl/S
             this._multiselection.selectedKeys = newOptions.selectedKeys;
             this._multiselection.excludedKeys = newOptions.excludedKeys;
             _private.notifyAndUpdateSelection(this, this._options.selectedKeys, this._options.excludedKeys);
-        }
-
-        if (this._options.listModel !== newOptions.listModel) {
-            this._multiselection.setListModel(newOptions.listModel);
         }
     },
 
