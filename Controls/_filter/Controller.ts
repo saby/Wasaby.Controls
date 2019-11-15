d57e1a5649d2c9f3720aae57e03b0ebeffc1f4d7
am.gerasimov <am.gerasimov@tensor.ru>
2019-11-15 13:41:57 +0300

    https://online.sbis.ru/opendoc.html?guid=6a026bd5-453c-4a42-aa19-fdb8fbbaecb4

--- Controls/_filter/Controller.ts
+++ Controls/_filter/Controller.ts
@@ -79,12 +79,15 @@ const _private = {
             const visibility = !isNeedSaveHistory && getPropValue(item, 'visibility') ? false : getPropValue(item, 'visibility');
             const minimizedItem = {};
             const value = getPropValue(item, 'value');
+            const isNeedSaveValue = getPropValue(item, 'resetValue') !== undefined ?
+                value !== undefined && isNeedSaveHistory :
+                true;
 
             if (visibility !== undefined) {
                 minimizedItem.visibility = visibility;
             }
 
-            if (isNeedSaveHistory && value !== undefined) {
+            if (isNeedSaveValue) {
                 minimizedItem.value = getPropValue(item, 'value');
             }
 
