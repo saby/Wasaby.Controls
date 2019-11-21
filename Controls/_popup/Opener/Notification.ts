c918466fff06006b0d1c36cea7b939eb460ffa2c
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-11-21 16:35:58 +0300

    https://online.sbis.ru/opendoc.html?guid=9295b37d-7926-447e-9862-a69c21e9ae54

--- Controls/_popup/Opener/Notification.ts
+++ Controls/_popup/Opener/Notification.ts
@@ -51,6 +51,7 @@ const _private = {
     },
     compatibleOpen(self, popupOptions): Promise<string> {
         const config = BaseOpener.getConfig({}, popupOptions);
+        delete config.id;
         return new Promise((resolve) => {
             Promise.all([
                 BaseOpener.requireModule('Controls/compatiblePopup:BaseOpener'),
