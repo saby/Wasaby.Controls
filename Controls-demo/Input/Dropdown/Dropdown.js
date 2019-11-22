42ba9f49f8e41d452fb2be5f94165ac8c981ce45
ee.zolotova <ee.zolotova@tensor.ru>
2019-11-22 14:52:22 +0300

    https://online.sbis.ru/opendoc.html?guid=3be0bd6a-4948-4342-884a-95426c5c0618

--- Controls-demo/Input/Dropdown/Dropdown.js
+++ Controls-demo/Input/Dropdown/Dropdown.js
@@ -38,6 +38,7 @@ define('Controls-demo/Input/Dropdown/Dropdown', [
       _groupTextItems: null,
       _groupHierarchyItems: null,
       _oneItem: null,
+      _itemsWithDescription: null,
       _selectedKeysSimple: null,
       _selectedKeysSub: null,
       _selectedKeysHierarchy: null,
@@ -66,6 +67,7 @@ define('Controls-demo/Input/Dropdown/Dropdown', [
       _selectedKeysGroupHierarchy: null,
       _selectedKeyOneItem: null,
       _selectedKeysOneEmpty: null,
+      _selectedKeysWithDescription: null,
 
 
       _beforeMount: function() {
@@ -252,6 +254,10 @@ define('Controls-demo/Input/Dropdown/Dropdown', [
             { id: 13, title: 'Coordination of change prices', parent: 6, group: 'coord' },
             { id: 14, title: 'Matching new dish', parent: 6, group: 'coord' }
          ];
+         this._itemsWithDescription = [
+            { id: 1, title: 'adaptive', description: 'infrequently used accordion sections are hidden' },
+            { id: 2, title: 'full', description: 'all accordion sections are visible' }
+         ];
          this._historySource = historySource.createMemory();
          this._historySourceMulti = historySource.createMemory();
          this._selectedKeysSimple = [1];
@@ -282,6 +288,7 @@ define('Controls-demo/Input/Dropdown/Dropdown', [
          this._selectedKeysGroupHierarchy = [1];
          this._selectedKeyOneItem = [1];
          this._selectedKeysOneEmpty = [1];
+         this._selectedKeysWithDescription = [1];
       },
       _createMemory: function(items) {
          return new source.Memory({
