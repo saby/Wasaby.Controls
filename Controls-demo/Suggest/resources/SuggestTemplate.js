e897138d55fdd286f71b25ef856c78eb993ae2ac
ea.melnikova <ea.melnikova@tensor.ru>
2019-11-27 15:59:21 +0300

    https://online.sbis.ru/opendoc.html?guid=6474c325-ffe2-4bc1-9870-53dcfa8c7eb8

--- Controls-demo/Suggest/resources/SuggestTemplate.js
+++ Controls-demo/Suggest/resources/SuggestTemplate.js
@@ -1,14 +1,19 @@
-define("Controls-demo/Suggest/resources/SuggestTemplate", ["require", "exports", "tslib", "UI/Base", "wml!Controls-demo/Suggest/resources/SuggestTemplate"], function (require, exports, tslib_1, Base_1, controlTemplate) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    var SuggestTemplate = /** @class */ (function (_super) {
-        tslib_1.__extends(SuggestTemplate, _super);
-        function SuggestTemplate() {
-            var _this = _super !== null && _super.apply(this, arguments) || this;
-            _this._template = controlTemplate;
-            return _this;
-        }
-        return SuggestTemplate;
-    }(Base_1.Control));
-    exports.default = SuggestTemplate;
+/**
+ * Created by am.gerasimov on 13.04.2018.
+ */
+/**
+ * Created by am.gerasimov on 13.12.2017.
+ */
+define('Controls-demo/Suggest/resources/SuggestTemplate', [
+   'Core/Control',
+   'wml!Controls-demo/Suggest/resources/SuggestTemplate',
+   'Controls/list'
+], function(Control, template) {
+
+   'use strict';
+
+   return Control.extend({
+      _template: template
+   });
+
 });
