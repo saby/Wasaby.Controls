c59280b6bd552de88d0420b8745e23b89c3b61d1
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-11-28 10:44:36 +0300

    Исправлена опечатка

--- Controls/_search/Misspell/Container.ts
+++ Controls/_search/Misspell/Container.ts
@@ -4,7 +4,6 @@ import template = require('wml!Controls/_search/Misspell/Container');
 /**
  * Конрол загружает и отображает {@link Controls/_search/Misspell Controls/search:Misspell}.
  * При клике по подсказке контрол генерируется всплывающее событие, которое можно обрабатывает {@link Controls/_search/Controller.ts Controls/search:Controller}.
-
  * 
  * @class Controls/_search/Misspell/Container
  * @extends Core/Control
