5f8448111945a60b39070f910c5f4aa83c2f26c1
va.ten <va.ten@tensor.ru>
2019-12-09 18:41:34 +0300

    https://online.sbis.ru/opendoc.html?guid=7c9c6cf6-8a70-4bb0-872d-fc9f01c95b22

---
+++ Controls-demo/MobileEventDemo/LongTap/LongTap.js
@@ -0,0 +1,29 @@
+define('Controls-demo/MobileEventDemo/LongTap/LongTap',
+   [
+      'Core/Control',
+      'wml!Controls-demo/MobileEventDemo/LongTap/LongTap',
+      'css!Controls-demo/MobileEventDemo/LongTap/LongTap'
+   ],
+   function(Control, template) {
+      'use strict';
+
+      var LongTapDemo = Control.extend({
+         _template: template,
+         longTapCount: null,
+         tapCount: null,
+
+         _beforeMount: function() {
+            this.longTapCount = 0;
+            this.tapCount = 0;
+         },
+
+         longClick: function() {
+            this.longTapCount += 1;
+         },
+         simpleClick: function() {
+            this.tapCount += 1;
+         }
+      });
+      return LongTapDemo;
+   }
+);
