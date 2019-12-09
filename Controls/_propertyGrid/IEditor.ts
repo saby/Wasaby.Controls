e66d8c819d54894170b58c5e700ffa0116ad9832
aa.artemeva1 <aa.artemeva1@tensor.ru>
2019-12-09 13:01:49 +0300

    Добавлено описание опции propertyValue

--- Controls/_propertyGrid/IEditor.ts
+++ Controls/_propertyGrid/IEditor.ts
@@ -1,12 +1,12 @@
 /**
  * Интерфейс редакторов propertyGrid.
- * @interface Controls/_propertyGrid/IEditor
+ * @interface Controls/propertyGrid/IEditor
  * @author Герасимов А.М.
  */
 
 /*
  * Interface of editor of PropertyGrid.
- * @interface Controls/_propertyGrid/IEditor
+ * @interface Controls/propertyGrid/IEditor
  * @author Герасимов А.М.
  */
 
@@ -23,3 +23,30 @@ export default interface IEditor {
      * @param {*} value New value of property.
      */
 }
+
+/**
+ * @name Controls/propertyGrid/IEditor#propertyValue
+ * @cfg {*} Текущее значение свойства.
+ * 
+ * @example
+ * WML:
+ * <pre>
+ * <Controls.dropdown:Input on:selectedKeysChanged="_selectedKeysChanged()" selectedKeys="{{_options.propertyValue}}"/>
+ * </pre>
+ * 
+ * TS:
+ * <pre>
+ * import { Control, TemplateFunction } from 'UI/Base';
+ * import template = require('wmlMyEditor');
+ *
+ * class MyEditor extends Control implements IEditor {
+ *    protected _template: Function = TemplateFunction;
+ *
+ *    _selectedKeysChanged(event: Event, selectedKeys: Array<number>): void {
+ *       this._notify('propertyValueChanged', [selectedKeys], {bubbling: true});
+ *    }
+ * }
+ * 
+ * export = MyEditor ; 
+ * </pre>
+ */
