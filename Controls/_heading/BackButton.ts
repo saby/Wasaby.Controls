57e834ef65e2678fe1c4776f8107116228538de3
as.krasilnikov <as.krasilnikov@tensor.ru>
2019-12-12 14:33:51 +0300

    https://online.sbis.ru/opendoc.html?guid=53a20f77-851b-40d6-b3f1-380a9d8caaae

--- Controls/_heading/BackButton.ts
+++
@@ -1,4 +0,0 @@
-import Menu = require('Controls/_heading/Back');
-import {Logger} from 'UI/Utils';
-Logger.error('Контрол "Controls/_heading/BackButton" перенесён, используйте "Controls/_heading/Back"');
-export = Menu;
