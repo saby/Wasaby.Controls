896aaeb8d565c31487c2794b48d13fb35338f055
vs.puhanov <vs.puhanov@tensor.ru>
2019-12-13 17:32:39 +0300

    specify controller interfaces
    Signed-off-by: vs.puhanov <vs.puhanov@tensor.ru>

--- Controls/_display/controllers/controllerUtils.ts
+++ Controls/_display/controllers/controllerUtils.ts
@@ -1,26 +1,39 @@
-import Collection from '../Collection';
+export type TCollectionKey = string | number;
 
-export function getItemByKey<T>(collection: Collection<unknown>, key: string|number): T {
+export interface IBaseCollection {
+    getItemBySourceId(key: TCollectionKey): unknown;
+    nextVersion(): void;
+    getCacheValue(cacheKey: string): unknown;
+    setCacheValue(cacheKey: string, value: unknown): void;
+}
+
+export function getItemByKey<T>(
+    collection: IBaseCollection,
+    key: TCollectionKey
+): T {
     if (key !== undefined && key !== null) {
-        return collection.getItemBySourceId(key) as unknown as T;
+        return collection.getItemBySourceId(key) as T;
     } else {
         return null;
     }
 }
 
-export function updateCollection(collection: Collection<unknown>, transaction: () => void): void {
+export function updateCollection(
+    collection: IBaseCollection,
+    transaction: () => void
+): void {
     transaction();
     collection.nextVersion();
 }
 
 export function updateCollectionWithCachedItem<T>(
-    collection: Collection<unknown>,
+    collection: IBaseCollection,
     cacheKey: string,
     transaction: (oldCacheValue: T) => T
 ): void {
     updateCollection(collection, () => {
-        const oldCacheValue: T = collection.getCacheValue(cacheKey);
-        const newCacheValue: T = transaction(oldCacheValue);
+        const oldCacheValue = collection.getCacheValue(cacheKey) as T;
+        const newCacheValue = transaction(oldCacheValue) as T;
         collection.setCacheValue(cacheKey, newCacheValue);
     });
 }
