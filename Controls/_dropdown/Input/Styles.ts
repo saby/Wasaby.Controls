75208cea90bb3ed52280455594e460423f8a52d3
ee.zolotova <ee.zolotova@tensor.ru>
2019-12-17 09:28:44 +0300

    https://online.sbis.ru/opendoc.html?guid=51963582-f14c-414c-ab1a-a79bdb961fe0

--- Controls/_dropdown/Input/Styles.ts
+++ Controls/_dropdown/Input/Styles.ts
@@ -14,9 +14,4 @@
     * @css @shift-top_Dropdown-popup Offset on the top for dropdown list.
     * @css @shift-top_Dropdown-head-popup Offset on the top for dropdown list with header.
     * @css @shift-left_Dropdown-popup Offset on the left for dropdown list.
-    *
-    * @css @color_Dropdown-emptyItem Text color of empty item.
-    *
-    * @css @border-color_Dropdown-style_invalid Color of the dropdown border when style option is set to invalid.
-    * @css @border-width_Dropdown-style_invalid Width of the dropdown border when style option is set to invalid.
     */
