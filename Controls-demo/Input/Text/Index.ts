904435627a7f4d82a4c5bb6f6e371933c1aeaea5
kraynovdo <do.kraynov@tensor.ru>
2019-12-17 08:19:03 +0300

    Возвращаю демку, она использовалась в тестах

---
+++ Controls-demo/Input/Text/Index.ts
@@ -0,0 +1,16 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import controlTemplate = require('wml!Controls-demo/Input/Text/Text');
+
+import 'Controls-demo/Input/Constraint/Index';
+import 'Controls-demo/Input/MaxLength/Index';
+import 'Controls-demo/Input/Trim/Index';
+
+import 'css!Controls-demo/Controls-demo';
+
+class Text extends Control<IControlOptions> {
+    protected _template: TemplateFunction = controlTemplate;
+
+    static _theme: string[] = ['Controls/Classes'];
+}
+
+export default Text;
