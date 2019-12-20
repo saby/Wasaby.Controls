a13c965da3330f4e7647f8e0ca709c3fe71191fe
av.bondar <av.bondar@tensor.ru>
2019-12-20 11:57:02 +0300

    https://online.sbis.ru/opendoc.html?guid=d0d6ed16-91c8-4bee-ac67-0c8adef8f363

--- Controls-demo/LoadingIndicator/Delay/Index.ts
+++ Controls-demo/LoadingIndicator/Delay/Index.ts
@@ -5,32 +5,5 @@ import 'css!Controls-demo/Controls-demo';
 class Delay extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
     static _theme: string[] = ['Controls/Classes'];
-    private _firstId = null;
-    private _secondId = null;
-
-    private _close(event, name): void {
-        const indicatorName = '_' + name + 'Id';
-        if (this[indicatorName]) {
-            this._notify('hideIndicator', [this[indicatorName]], { bubbling: true });
-            this[indicatorName] = null;
-        }
-    }
-    private _firstOpen(): void {
-        const cfg = {
-            id: this._firstId,
-            overlay: 'none',
-            message: 'Текст индикатора'
-        };
-        this._firstId = this._notify('showIndicator', [cfg], { bubbling: true });
-    }
-    private _secondOpen(): void {
-        const cfg = {
-            id: this._secondId,
-            overlay: 'none',
-            message: 'Текст индикатора',
-            delay: 0
-        };
-        this._secondId = this._notify('showIndicator', [cfg], { bubbling: true });
-    }
 }
 export default Delay;
