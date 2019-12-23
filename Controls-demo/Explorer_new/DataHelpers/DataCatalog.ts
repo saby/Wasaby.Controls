78beeac9f21c7ca3ec6567f6025b7e9256830747
ne.andruh <ne.andruh@tensor.ru>
2019-12-23 10:34:24 +0700

    https://online.sbis.ru/opendoc.html?guid=469a7438-3fe7-47d9-997a-487f802e3f90

--- Controls-demo/Explorer_new/DataHelpers/DataCatalog.ts
+++ Controls-demo/Explorer_new/DataHelpers/DataCatalog.ts
@@ -1,4 +1,6 @@
 import * as explorerImages from 'Controls-demo/Explorer/ExplorerImages';
+import * as editingColumnTemplate from 'wml!Controls-demo/Explorer/Editing/editingCellTemplate'
+import * as notEditableTemplate from 'wml!Controls-demo/Explorer/Editing/notEditableCell'
 
 export const Gadgets = {
    getData: () => [{
@@ -247,6 +249,18 @@ export const Gadgets = {
          displayProperty: 'price',
          width: '1fr'
       }
+   ],
+   getGridEditingCol: () => [
+      {
+         displayProperty: 'title',
+         width: '200px',
+         template: editingColumnTemplate
+      },
+      {
+         displayProperty: 'discr',
+         width: '1fr',
+         template: notEditableTemplate,
+      },
    ],
 
    getGridColumnsForScroll: () => [
