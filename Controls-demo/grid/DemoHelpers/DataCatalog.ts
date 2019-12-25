85f2809621b2cba4ad6c7150fcfe71feaa76e788
ne.andruh <ne.andruh@tensor.ru>
2019-12-25 17:17:52 +0700

    20.1000/bugfix/ane/group-grid-dem

--- Controls-demo/grid/DemoHelpers/DataCatalog.ts
+++ Controls-demo/grid/DemoHelpers/DataCatalog.ts
@@ -669,7 +669,21 @@ function getTasks() {
                 template: 'wml!Controls-demo/grid/_resources/CellTemplates/LadderTasksReceived',
                 width: 'auto'
             }
-        ]
+        ],
+        getDefaultColumns: () => [
+            {
+                displayProperty: 'id',
+                width: '30px'
+            },
+            {
+                displayProperty: 'state',
+                width: '200px'
+            },
+            {
+                displayProperty: 'date',
+                width: '100px'
+            }
+        ],
     }
 }
 
