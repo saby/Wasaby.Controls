c0243ae2ae477561b321b4ce8def50e386e3ad8a
ia.azin <ia.azin@tensor.ru>
2019-12-26 13:51:36 +0300

    [DOCS] modify doclet for error.Mode (typedef aka class crutch)
    https://online.sbis.ru/opendoc.html?guid=133a9714-2b30-4852-80ff-c16a50918025

--- Controls/_dataSource/_error/Mode.ts
+++ Controls/_dataSource/_error/Mode.ts
@@ -1,16 +1,26 @@
 /// <amd-module name="Controls/_dataSource/_error/Mode" />
 /**
  * Перечисляемое множество возможных способов отображения парковочного шаблона ошибки
- * @typedef {Object} Controls/_dataSource/_error/Mode
- * @property {string} dialog в диалоговом окне
- * @property {string} page во всю страницу
- * @property {string} include в области контрола
+ * @class Controls/_dataSource/_error/Mode
  * @public
  * @author Санников К.А.
+ * @todo Переписать как typedef, как появится возможность автодоки прогружать из библиотек тайпдефы
  */
 enum Mode {
+    /**
+     * @name Controls/_dataSource/_error/Mode#dialog
+     * @cfg {string} в диалоговом окне
+     */
     dialog = 'dialog',
+    /**
+     * @name Controls/_dataSource/_error/Mode#page
+     * @cfg {string} во всю страницу
+     */
     page = 'page',
+    /**
+     * @name Controls/_dataSource/_error/Mode#include
+     * @cfg {string} в области контрола
+     */
     include = 'include'
 }
 
