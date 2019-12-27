9bf50430ca041baf5a50ebcb57f7086d7f759bc5
kraynovdo <do.kraynov@tensor.ru>
2019-12-27 08:33:42 +0300

    https://online.sbis.ru/opendoc.html?guid=b3de5fbf-3053-4ea3-b298-a09786f8c978 демки для сепаратора

--- Controls-demo/toggle/Separator/Index.ts
+++ Controls-demo/toggle/Separator/Index.ts
@@ -4,6 +4,14 @@ import 'css!Controls-demo/Controls-demo';
 
 class ViewModes extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
+    protected _expanded1: boolean = false;
+    protected _expanded2: boolean = false;
+    protected _expanded3: boolean = false;
+
+    protected _headingClick(): void {
+        this._expanded3 = !this._expanded3;
+    }
+
     static _theme: string[] = ['Controls/Classes'];
 }
 export default ViewModes;
