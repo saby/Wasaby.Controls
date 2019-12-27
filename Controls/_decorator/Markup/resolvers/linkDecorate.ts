f1fe0b88d665b309e852f55282c04e2f42dc7148
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-12-27 14:27:50 +0300

    Правки по замечаниям.

--- Controls/_decorator/Markup/resolvers/linkDecorate.ts
+++ Controls/_decorator/Markup/resolvers/linkDecorate.ts
@@ -7,9 +7,9 @@ import { clearNeedDecorateGlobals, needDecorate, getDecoratedLink } from '../res
  *
  * Модуль с функцией замены общей ссылки на декорированную ссылку, если это необходимо.
  * Распознаватель тегов для {@link Controls/decorator:Markup}.
+ * а) Поиск в текстовых узлах JsonML ссылок, оборачивание их в тег a
  * @example
  * <pre class="brush: json">
- * а) Поиск в текстовых узлах JsonML ссылок, оборачивание их в тег a
  * [
  *    ["p", // Ссылка, подходящая для декорирования, сразу в json
  *       ["a",
@@ -28,7 +28,7 @@ import { clearNeedDecorateGlobals, needDecorate, getDecoratedLink } from '../res
  *    ]
  * ]
  * </pre>
- * б) Декорирование ссылок, подходящих спецификации @link{http://axure.tensor.ru/standarts/v7/%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D0%BA%D0%B8_%D0%B8_%D0%B4%D0%B5%D0%BA%D0%BE%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B8.html декорирования}.
+ * б) Декорирование ссылок, подходящих спецификации {@link http://axure.tensor.ru/standarts/v7/%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D0%BA%D0%B8_%D0%B8_%D0%B4%D0%B5%D0%BA%D0%BE%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B8.html декорирования}.
  * <pre class="html">
  * <div>
  *    <p>
