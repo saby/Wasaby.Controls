30fc6c6aa70e3499e31c3f6ed33bfe3695ae27f2
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-12-27 14:11:44 +0300

    https://online.sbis.ru/opendoc.html?guid=49ea8cf1-187f-49b1-9e16-b1923036b6de Добавлены примеры

--- Controls/_decorator/Markup/resolvers/linkDecorate.ts
+++ Controls/_decorator/Markup/resolvers/linkDecorate.ts
@@ -7,7 +7,52 @@ import { clearNeedDecorateGlobals, needDecorate, getDecoratedLink } from '../res
  *
  * Модуль с функцией замены общей ссылки на декорированную ссылку, если это необходимо.
  * Распознаватель тегов для {@link Controls/decorator:Markup}.
- *
+ * @example
+ * <pre class="brush: json">
+ * а) Поиск в текстовых узлах JsonML ссылок, оборачивание их в тег a
+ * [
+ *    ["p", // Ссылка, подходящая для декорирования, сразу в json
+ *       ["a",
+ *          {"href": "https://ya.ru"},
+ *          "https://ya.ru"
+ *       ]
+ *    ],
+ *    ["p", // Ссылка, не подходящая для декорирования, сразу в json
+ *       ["a",
+ *          {"href": "http://www.google.com"},
+ *          "www.google.com"
+ *       ]
+ *    ],
+ *    ["pre", // Не подходящая и подходящая ссылки прямо в plain/text строке, положенной в тег pre для отображения переноса строки \n
+ *       "www.google.com\nhttps://ya.ru"
+ *    ]
+ * ]
+ * </pre>
+ * б) Декорирование ссылок, подходящих спецификации @link{http://axure.tensor.ru/standarts/v7/%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D0%BA%D0%B8_%D0%B8_%D0%B4%D0%B5%D0%BA%D0%BE%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B8.html декорирования}.
+ * <pre class="html">
+ * <div>
+ *    <p>
+ *       <span class="...">
+ *          <a class="..." href="https://ya.ru">
+ *             <img class="..." src=".." alt="https://ya.ru"></img>
+ *          </a>
+ *       </span>
+ *    </p>
+ *    <p>
+ *       <a href="http://www.google.com">www.google.com></a>
+ *    </p>
+ *    <pre>
+ *       <a href="http://www.google.com">www.google.com</a>
+ *       \n
+ *       <span class="...">
+ *          <a class="..." href="https://ya.ru">
+ *             <img class="..." src="..." alt="https://ya.ru"></img>
+ *          </a>
+ *       </span>
+ *    </pre>
+ * </div>
+ * </pre>
+ * 
  * @class Controls/_decorator/Markup/resolvers/linkDecorate
  * @public
  * @author Кондаков Р.Н.
