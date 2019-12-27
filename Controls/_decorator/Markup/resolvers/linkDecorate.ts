ad89abf4f3d88de10be4895986c10e63fe69befb
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-12-27 14:29:57 +0300

    Добавлен brush

--- Controls/_decorator/Markup/resolvers/linkDecorate.ts
+++ Controls/_decorator/Markup/resolvers/linkDecorate.ts
@@ -29,7 +29,7 @@ import { clearNeedDecorateGlobals, needDecorate, getDecoratedLink } from '../res
  * ]
  * </pre>
  * б) Декорирование ссылок, подходящих спецификации {@link http://axure.tensor.ru/standarts/v7/%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D0%BA%D0%B8_%D0%B8_%D0%B4%D0%B5%D0%BA%D0%BE%D1%80%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D1%81%D1%81%D1%8B%D0%BB%D0%BA%D0%B8.html декорирования}.
- * <pre class="html">
+ * <pre class="brush: html">
  * <div>
  *    <p>
  *       <span class="...">
