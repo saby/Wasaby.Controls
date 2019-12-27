4c731350a9388a64ac8273900b93d8be8c371ab9
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-12-27 15:06:48 +0300

    Исправлена опечатка

--- Controls/_dateRange/LiteSelector.ts
+++ Controls/_dateRange/LiteSelector.ts
@@ -7,7 +7,7 @@ import componentTmpl = require('wml!Controls/_dateRange/LiteSelector/LiteSelecto
 import getOptions from 'Controls/Utils/datePopupUtils';
 
 /**
- * Контрол позволяет пользователю выбрать даипазон дат с начальным и конечным значениями в календаре. Выбор происходит с помощью панели быстрого выбора периода.
+ * Контрол позволяет пользователю выбрать диапазон дат с начальным и конечным значениями в календаре. Выбор происходит с помощью панели быстрого выбора периода.
  *
  * @class Controls/_dateRange/LiteSelector
  * @extends Core/Control
