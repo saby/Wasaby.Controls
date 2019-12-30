ec9302633fae428c62abadcfed9ce85ee6c9fa60
ia.kapustin <ia.kapustin@tensor.ru>
2019-12-30 18:44:34 +0300

    https://online.sbis.ru/opendoc.html?guid=172b62cc-88eb-4f39-ad7b-b42663a7c124

--- Controls/_operations/Button.ts
+++ Controls/_operations/Button.ts
@@ -20,6 +20,7 @@ export interface IOperationsButtonOptions extends IControlOptions, IExpandableOp
  * @css @thickness_OperationsButton-separator Thickness of the separator between the button and the rest of the content.
  * @css @height_OperationsButton-separator Height of the separator between the button and the rest of the content.
  * @css @color_OperationsButton-icon Color of the icon.
+ * @css @color_OperationsButton-icon_readOnly Цвет иконки в режиме чтения.
  * @css @color_OperationsButton-icon_hovered Color of the hovered icon.
  * @css @color_OperationsButton-icon_active Color of the active icon.
  * @css @color_OperationsButton-separator Color of the separator between the button and the rest of the content.
@@ -43,6 +44,7 @@ export interface IOperationsButtonOptions extends IControlOptions, IExpandableOp
  * @css @thickness_OperationsButton-separator Thickness of the separator between the button and the rest of the content.
  * @css @height_OperationsButton-separator Height of the separator between the button and the rest of the content.
  * @css @color_OperationsButton-icon Color of the icon.
+ * @css @color_OperationsButton-icon_readOnly The color of the icon in read-only mode.
  * @css @color_OperationsButton-icon_hovered Color of the hovered icon.
  * @css @color_OperationsButton-icon_active Color of the active icon.
  * @css @color_OperationsButton-separator Color of the separator between the button and the rest of the content.
@@ -56,7 +58,9 @@ class OperationsButton extends Control<IOperationsButtonOptions> implements IExp
    protected _template: TemplateFunction = ButtonTemplate;
 
    private _onClick(): void {
-      this._notify('expandedChanged', [!this._options.expanded]);
+      if (!this._options.readOnly) {
+         this._notify('expandedChanged', [!this._options.expanded]);
+      }
    }
    static _theme: string[] = ['Controls/operations'];
 }
