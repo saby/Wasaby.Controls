b99fd703293ce910e81b2883ab7985899ca29460
yan.sirotkin <yan.sirotkin@tensor.ru>
2019-12-30 13:50:50 +0300

    https://online.sbis.ru/opendoc.html?guid=49ea8cf1-187f-49b1-9e16-b1923036b6de По замечаниям.

--- Controls/_decorator/Markup/resolvers/linkDecorate.ts
+++ Controls/_decorator/Markup/resolvers/linkDecorate.ts
@@ -4,10 +4,79 @@
 import { clearNeedDecorateGlobals, needDecorate, getDecoratedLink } from '../resources/linkDecorateUtils';
 
 /**
- *
  * Модуль с функцией замены общей ссылки на декорированную ссылку, если это необходимо.
  * Распознаватель тегов для {@link Controls/decorator:Markup}.
- *
+ * Модуль содержит функцию, которая позволяет преобразовать обычную ссылку в декорированную.
+ * Декорация ссылки выполняется через {@link https://wi.sbis.ru/doc/platform/developmentapl/middleware/link-decorator/ Сервис декорирования ссылок}.
+ * Функция предназначена для использования в контроле {@link Controls/decorator:Markup} в опции {@link https://wi.sbis.ru/docs/js/Controls/decorator/Markup/options/tagResolver/ tagResolver}.
+ * @example
+ * WML:
+ * <pre class="brush: xml">
+ *    <Controls.decorator:Markup value="{{ _json }}" tagResolver="{{ _tagResolver }}" />
+ * </pre>
+ * JS:
+ * <pre class="brush: js">
+ * define("MyControl", ["UI/Base", "wml!MyControl", "Controls/decorator"], 
+ * function(Base, template, decorator) {
+ *    var ModuleClass = Base.Control.extend({
+ *       _template: template,
+ *       _tagResolver: decorator.linkDecorate
+ *       _json:
+ *          [
+ *             [
+ *                ["p", 
+ * &#160;
+ *                   // Ссылка, подходящая для декорирования, сразу в json
+ *                   ["a",
+ *                   {"href": "https://ya.ru
+ *                   "},
+ *                   "https://ya.ru"
+ *                   ]
+ *                ],
+ *                ["p", 
+ * &#160;
+ *                   // Ссылка, не подходящая для декорирования, сразу в json
+ *                   ["a",
+ *                   {"href": "http://www.google.com"},
+ *                   "www.google.com"
+ *                   ]
+ *                ],
+ *                ["pre", 
+ * &#160;
+ *                   // Не подходящая и подходящая ссылки прямо в plain/text строке, 
+ *                   //положенной в тег pre для отображения переноса строки \n
+ *                   "     www.google.com\nhttps://ya.ru"
+ *                ]
+ *             ]
+ *          ]
+ *       });
+ *    return ModuleClass;
+ * });
+ * </pre>
+ * Результат:
+ * <pre class="brush: html">
+ * <div>
+ *    <p>
+ *       <span class="...">
+ *          <a class="..." href="https://ya.ru">
+ *             <img class="..." src=".." alt="https://ya.ru"></img>
+ *          </a>
+ *       </span>
+ *    </p>
+ *    <p>
+ *       <a href="http://www.google.com">www.google.com</a>
+ *    </p>
+ *    <pre>
+ *       <a href="http://www.google.com">www.google.com</a>
+ *       \n
+ *       <span class="...">
+ *          <a class="..." href="https://ya.ru">
+ *             <img class="..." src="..." alt="https://ya.ru"></img>
+ *          </a>
+ *       </span>
+ *    </pre>
+ * </div>
+ * </pre>
  * @class Controls/_decorator/Markup/resolvers/linkDecorate
  * @public
  * @author Кондаков Р.Н.
@@ -23,14 +92,14 @@ import { clearNeedDecorateGlobals, needDecorate, getDecoratedLink } from '../res
  * @author Кондаков Р.Н.
  */
 export default function linkDecorate(value, parent) {
-    if (!parent) {
-        clearNeedDecorateGlobals();
-    }
-    let result;
-    if (needDecorate(value, parent)) {
-        result = getDecoratedLink(value);
-    } else {
-        result = value;
-    }
-    return result;
+	if (!parent) {
+		clearNeedDecorateGlobals();
+	}
+	let result;
+	if (needDecorate(value, parent)) {
+		result = getDecoratedLink(value);
+	} else {
+		result = value;
+	}
+	return result;
 };
