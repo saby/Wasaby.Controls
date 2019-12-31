eda4589ca3464ad476756fa7f3cf8257cee2db08
am.gerasimov <am.gerasimov@tensor.ru>
2019-12-31 11:06:44 +0300

    Revert "https://online.sbis.ru/opendoc.html?guid=caf672be-a161-4b52-9dfc-b70eddc826bb"
    This reverts commit 3137e256802e851faa04d80bf1897ab43c746822.
    
    # Conflicts:
    #	tests/ControlsUnit/Container/Data.test.js

--- tests/ControlsUnit/Container/Data.test.js
+++ tests/ControlsUnit/Container/Data.test.js
@@ -4,11 +4,9 @@ define(
       'Types/source',
       'Controls/context',
       'Core/Deferred',
-      'Types/collection',
-      'Application/Initializer',
-      'Application/Env'
+      'Types/collection'
    ],
-   function(lists, sourceLib, contexts, Deferred, collection,  AppInit, AppEnv) {
+   function(lists, sourceLib, contexts, Deferred, collection) {
       describe('Container/Data', function() {
 
          var sourceData = [
@@ -38,23 +36,6 @@ define(
             var data = new lists.DataContainer(config);
             data.saveOptions(config);
             return data;
-         };
-
-         var setNewEnvironmentValue = function(value) {
-            let sandbox = sinon.createSandbox();
-
-            if (value) {
-               sandbox.replace(AppInit, 'isInit', () => true);
-               sandbox.replace(AppEnv, 'getStore', () => ({
-                  isNewEnvironment: true
-               }));
-            } else {
-               sandbox.replace(AppInit, 'isInit', () => false);
-            }
-
-            return function resetNewEnvironmentValue() {
-               sandbox.restore();
-            };
          };
 
          it('update source', function(done) {
@@ -110,18 +91,15 @@ define(
          });
 
          it('_beforeMount with receivedState', function() {
-            let data = getDataWithConfig({source: source, keyProperty: 'id'});
-            let newSource = new sourceLib.Memory({
+            var data = getDataWithConfig({source: source, keyProperty: 'id'});
+            var newSource = new sourceLib.Memory({
                keyProperty: 'id',
                data: sourceData
             });
-            let resetCallback = setNewEnvironmentValue(true);
             data._beforeMount({source: newSource, idProperty: 'id'}, {}, sourceData);
 
             assert.deepEqual(data._items, sourceData);
             assert.isTrue(!!data._prefetchSource);
-
-            resetCallback();
          });
 
          it('_beforeMount with receivedState and prefetchProxy', function() {
@@ -136,14 +114,11 @@ define(
                }
             });
             let data = getDataWithConfig({source: prefetchSource, keyProperty: 'id'});
-            let resetCallback = setNewEnvironmentValue(true);
 
             data._beforeMount({source: prefetchSource, idProperty: 'id'}, {}, sourceData);
             assert.isTrue(data._prefetchSource.getOriginal() === memory);
             assert.isTrue(data._prefetchSource !== prefetchSource);
             assert.equal(data._prefetchSource._$data.query, sourceData);
-
-            resetCallback();
          });
 
          it('update equal source', function(done) {
