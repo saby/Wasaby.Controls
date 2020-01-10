a19018561c8276b7361877d732b83df9a19d2194
ne.andruh <ne.andruh@tensor.ru>
2020-01-10 18:09:19 +0700

    add

--- Controls-demo/list_new/Multiselect/MultiselectVisibility/Visible/Index.ts
+++ Controls-demo/list_new/Multiselect/MultiselectVisibility/Visible/Index.ts
@@ -1,14 +1,13 @@
 import {Control, TemplateFunction} from "UI/Base"
-import * as Template from "wml!Controls-demo/list_new/MultiSelect/AllSelected/AllSelected"
+import * as Template from "wml!Controls-demo/list_new/Multiselect/MultiselectVisibility/Visible/Visible"
 import {Memory} from "Types/source"
-import {getFewCategories as getData} from "../../DemoHelpers/DataCatalog"
+import {getFewCategories as getData} from "../../../DemoHelpers/DataCatalog"
 import 'css!Controls-demo/Controls-demo'
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     private _viewSource: Memory;
-    private _selectedKeys: Array<null> = [null];
-    private _excludedKeys: Array<number> = [];
+    private _selectedKeys: Array<number> = [];
 
     protected _beforeMount() {
         this._viewSource = new Memory({
