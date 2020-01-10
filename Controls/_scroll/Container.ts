7ece792ef15b3ac89cb363bc74aba5774b67fca0
yan.sirotkin <yan.sirotkin@tensor.ru>
2020-01-10 12:11:15 +0300

    https://online.sbis.ru/opendoc.html?guid=ddbf066c-7ddc-4798-8be7-d38291216085 Исправлена опечатка

--- Controls/_scroll/Container.ts
+++ Controls/_scroll/Container.ts
@@ -30,7 +30,7 @@ import {Logger} from "UI/Utils";
  * @category Container
  * @remark
  * Контрол работает как нативный скролл: скроллбар появляется, когда высота контента больше высоты контрола. Для корректной работы контрола необходимо ограничить его высоту.
- * Для работы скрола по курсору мыши, необходимо обернуть Controls/scroll:Container в Controls/dragnDrop:Compound.
+ * Для работы скролла по курсору мыши, необходимо обернуть Controls/scroll:Container в Controls/dragnDrop:Compound.
  * @demo Controls-demo/Container/Scroll
  *
  */
