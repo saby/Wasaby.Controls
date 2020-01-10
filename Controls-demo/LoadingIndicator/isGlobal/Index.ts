b189acb53a0767841b763d604fbf498013dac9b8
av.bondar <av.bondar@tensor.ru>
2020-01-10 14:17:28 +0300

    https://online.sbis.ru/opendoc.html?guid=e909e9a7-c658-4ed2-8a3b-3ab6ff2fb91d

--- Controls-demo/LoadingIndicator/isGlobal/Index.ts
+++ Controls-demo/LoadingIndicator/isGlobal/Index.ts
@@ -1,6 +1,7 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import template = require('wml!Controls-demo/LoadingIndicator/isGlobal/IsGlobal');
 import 'css!Controls-demo/Controls-demo';
+import 'css!Controls-demo/LoadingIndicator/IndicatorContainer';
 
 class IsGlobal extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
@@ -17,6 +18,12 @@ class IsGlobal extends Control<IControlOptions> {
         setTimeout(() => {
             this._notify('hideIndicator', [this._firstId], { bubbling: true });
         }, 5000);
+    }
+    _secondOpen(e, time): void {
+        this._children.loadingIndicator.show({});
+        setTimeout(function() {
+            this._children.loadingIndicator.hide();
+        }.bind(this), time);
     }
 }
 export default IsGlobal;
