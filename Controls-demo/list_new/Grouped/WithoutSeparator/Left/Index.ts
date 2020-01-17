52c2aef4e8bf2a0bc89539c5ae056934e0bdcc66
as.avramenko <as.avramenko@tensor.ru>
2020-01-17 16:40:29 +0300

    https://online.sbis.ru/opendoc.html?guid=9a3a6cc5-bb01-4778-88e3-433cc46edf77

---
+++ Controls-demo/list_new/Grouped/WithoutSeparator/Left/Index.ts
@@ -0,0 +1,22 @@
+import {Control, TemplateFunction} from "UI/Base"
+import * as Template from "wml!Controls-demo/list_new/Grouped/WithoutSeparator/Left/Left"
+import {Memory} from "Types/source"
+import {Model} from "Types/entity"
+import {getFewCategories as getData} from "../../../DemoHelpers/DataCatalog"
+import 'css!Controls-demo/Controls-demo'
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    private _viewSource: Memory;
+
+    protected _beforeMount() {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: getData()
+        });
+    }
+
+    private _groupingKeyCallback(item: Model): string {
+        return item.get('byDemand');
+    }
+}
