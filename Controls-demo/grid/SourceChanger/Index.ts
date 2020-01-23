c1614b53a33474c69fdef99a0f0af0d0b24497df
ne.andruh <ne.andruh@tensor.ru>
2020-01-23 16:43:50 +0700

    https://online.sbis.ru/opendoc.html?guid=f6de0cb0-30fd-4dc4-ac76-7f4a9fd0cb08

---
+++ Controls-demo/grid/SourceChanger/Index.ts
@@ -0,0 +1,7 @@
+import {Control, TemplateFunction} from "UI/Base"
+import * as Template from "wml!Controls-demo/grid/SourceChanger/SourceChanger"
+import 'css!Controls-demo/Controls-demo'
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+}
