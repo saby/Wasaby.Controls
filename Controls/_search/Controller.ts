ced84ce34541f75cec0ec78fd9b61bb552dfa38a
ea.melnikova <ea.melnikova@tensor.ru>
2020-01-24 09:49:15 +0300

    https://online.sbis.ru/opendoc.html?guid=6986ea81-3b14-44da-a23f-3ed8d087497c

--- Controls/_search/Controller.ts
+++ Controls/_search/Controller.ts
@@ -72,6 +72,8 @@ var _private = {
       if (self._viewMode === 'search') {
          self._searchValue = '';
          self._misspellValue = '';
+         self._viewMode = self._previousViewMode;
+         self._previousViewMode = null;
 
          if (self._options.parentProperty) {
             _private.deleteServiceFilters(filter);
