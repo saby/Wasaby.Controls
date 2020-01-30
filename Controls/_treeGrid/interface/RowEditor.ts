90e016cda65132948824ade936a6ea48cb7bd8e1
Быканов А.А <aa.bykanov@tensor.ru>
2020-01-30 12:00:37 +0300

    Update RowEditor.ts

--- Controls/_treeGrid/interface/RowEditor.ts
+++ Controls/_treeGrid/interface/RowEditor.ts
@@ -22,7 +22,7 @@
 export default interface IRowEditorOptions {
     /**
      * @name Controls/treeGrid:RowEditor#content
-     * @cfg {String|Function} Устанавливает пользовательский шаблон, описывающий содержимое элемента в {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/grid/edit/ режиме редактирования}.
+     * @cfg {String|Function} Устанавливает пользовательский шаблон, описывающий содержимое элемента в {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/tree/edit/ режиме редактирования}.
      * @default undefined
      */
     content?: string;
