89ec281958b6a21fc47a09093962e6a122a50f0c
gd.kovalev <gd.kovalev@tensor.ru>
2020-02-03 17:16:37 +0700

    Поправил демку https://online.sbis.ru/opendoc.html?guid=7691a369-0521-497c-b77f-18ed23cd4785

--- Controls-demo/Calendar/MonthView/LongCellName/LongCellName.ts
+++ Controls-demo/Calendar/MonthView/LongCellName/LongCellName.ts
@@ -5,6 +5,8 @@ import 'css!Controls-demo/Controls-demo';
 class LongCellName extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
 
+    private _month: Date = new Date(2019, 1);
+
     static _theme: string[] = ['Controls/Classes'];
 }
 
