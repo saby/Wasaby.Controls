d261914efcf04ccfaee5b8d53ff599693811caa8
te.maslova <te.maslova@tensor.ru>
2020-02-05 16:36:28 +0700

    Добавлено описание https://online.sbis.ru/opendoc.html?guid=62c56c1a-81dd-43a2-b1bf-d6a90e33216b

--- Controls/_masterDetail/List.ts
+++ Controls/_masterDetail/List.ts
@@ -1,9 +1,25 @@
 import Control = require('Core/Control');
 import template = require('wml!Controls/_masterDetail/List/List');
+
+/**
+ * Контрол используют в качестве контейнера для любого списочного контрола, который добавлен в шаблон {@link Controls/masterDetail:Base#master master}. Такой контейнер умеет порождать событие markedKeyChanged при смене выбранной записи списка.
+ * @class Controls/_masterDetail/List
+ * @extends UI/Base:Control
+ * @public
+ * @author Авраменко А.С.
+ * @demo Controls-demo/MasterDetail/Demo
+ */
+
+/**
+ * @event Происходит при смене выбранной записи.
+ * @name Controls/_masterDetail/List#markedKeyChanged
+ * @param {Vdom/Vdom:SyntheticEvent} eventObject Дескриптор события.
+ * @param {Number} key  Ключ выбранного элемента.
+ */
+
    export = Control.extend({
       _template: template,
       _markedKeyChangedHandler: function(event, key) {
          this._notify('selectedMasterValueChanged', [key], {bubbling: true});
       }
    });
-
