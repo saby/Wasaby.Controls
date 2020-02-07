af18b445d0bb072880f5b387c9395db1778f82c7
ne.andruh <ne.andruh@tensor.ru>
2020-02-07 16:50:58 +0700

    https://online.sbis.ru/opendoc.html?guid=db937d26-3327-4110-bb47-1491d8a2df6f

--- Controls/_scroll/Scroll/Watcher.ts
+++ Controls/_scroll/Scroll/Watcher.ts
@@ -139,7 +139,9 @@ import {SyntheticEvent} from "Vdom/Vdom"
             var sizeCache = _private.getSizeCache(self, container);
 
              const newScrollTop = container.scrollTop;
-
+             if (container.scrollLeft) {
+                 container.scrollLeft = 0;
+             }
              if (newScrollTop === self._scrollTopCache) {
                  return;
              }
