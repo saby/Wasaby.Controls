76bf36dc73773d975ff05391901496fafb7a6130
aa.ibragimov <aa.ibragimov@tensor.ru>
2020-02-07 16:26:43 +0300

    https://online.sbis.ru/opendoc.html?guid=024a3d8f-4165-4b17-b979-a5ec89755800

--- Controls/_suggestPopup/Layer/__PopupContent.ts
+++ Controls/_suggestPopup/Layer/__PopupContent.ts
@@ -1,6 +1,5 @@
 import BaseLayer from './__BaseLayer';
 import {detection} from 'Env/Env';
-import 'css!theme?Controls/suggest';
 import template = require('wml!Controls/_suggestPopup/Layer/__PopupContent');
 
 var _private = {
@@ -79,6 +78,7 @@ var __PopupContent = BaseLayer.extend({
    }
 });
 
+__PopupContent._theme = ['Controls/suggest'];
 __PopupContent._private = _private;
 
 export default __PopupContent;
