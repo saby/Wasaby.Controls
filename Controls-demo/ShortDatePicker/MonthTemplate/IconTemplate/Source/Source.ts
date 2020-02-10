6b01b184912066029420d027cd6cb5107ed8905b
gd.kovalev <gd.kovalev@tensor.ru>
2020-02-10 12:35:32 +0700

    Добавил демки https://online.sbis.ru/opendoc.html?guid=92cedc3a-0dde-4eec-8bbc-1f7b0b085334

--- Controls-demo/ShortDatePicker/MonthTemplate/IconTemplate/Source/Source.ts
+++ Controls-demo/ShortDatePicker/MonthTemplate/IconTemplate/Source/Source.ts
@@ -1,7 +1,6 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import DateLitePopupSource from "./DateLitePopupSource";
 import template = require("wml!Controls-demo/ShortDatePicker/MonthTemplate/IconTemplate/Source/Source");
-import 'css!Controls-demo/Controls-demo';
 
 class DemoControl extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
