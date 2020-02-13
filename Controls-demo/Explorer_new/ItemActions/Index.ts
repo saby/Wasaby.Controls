8e6998b1096cfaca386b93e4e47317790d913515
ea.melnikova <ea.melnikova@tensor.ru>
2020-02-13 10:39:16 +0300

    https://online.sbis.ru/opendoc.html?guid=fc9355ab-9820-44d0-a7d4-01ca7c770f93

---
+++ Controls-demo/Explorer_new/ItemActions/Index.ts
@@ -0,0 +1,49 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/Explorer_new/ItemActions/ItemActions';
+import {Gadgets} from '../DataHelpers/DataCatalog';
+import {Memory} from 'Types/source';
+import * as MemorySource from 'Controls-demo/Explorer/ExplorerMemory';
+
+import 'css!Controls-demo/Controls-demo';
+
+export default class extends Control {
+   protected _template: TemplateFunction = Template;
+   protected _viewSource;
+   protected _columns = Gadgets.getSearchColumns();
+   protected _root = null;
+   protected _searchStartingWith: string = 'root';
+   protected _searchStartingWithSource: Memory = null;
+   protected _filter = { demo: 123 };
+
+   protected _beforeMount() {
+      this._viewSource = new MemorySource({
+         keyProperty: 'id',
+         data: Gadgets.getSearchData()
+      });
+      this._searchStartingWithSource = new Memory({
+         keyProperty: 'id',
+         data: [
+            {
+               id: 'root', title: 'root'
+            },
+            {
+               id: 'current', title: 'current'
+            }
+         ]
+      });
+      this._itemActions = [
+         {
+            id: 1,
+            icon: 'icon-PhoneNull',
+            title: 'phone',
+            showType: 2
+         },
+         {
+            id: 2,
+            icon: 'icon-EmptyMessage',
+            title: 'message',
+            showType: 2
+         }
+      ];
+   }
+}
