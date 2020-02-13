eaf28ffb98dbd41c78e71381fdc85345374447b8
vd.volockoy <vd.volockoy@tensor.ru>
2020-02-13 15:28:19 +0300

    https://online.sbis.ru/opendoc.html?guid=50d3b434-861d-4a14-80e7-6688f41e208b

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -309,8 +309,8 @@ var _private = {
         }
     },
 
-    scrollToItem: function(self, key) {
-        return self._children.scrollController.scrollToItem(key);
+    scrollToItem: function(self, key, toBottom, force) {
+        return self._children.scrollController.scrollToItem(key, toBottom, force);
     },
     setMarkedKey: function(self, key) {
         if (key !== undefined) {
@@ -2064,8 +2064,8 @@ var BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototype
         });
     },
 
-    scrollToItem(key: string|number): void {
-        return _private.scrollToItem(this, key);
+    scrollToItem(key: string|number, toBottom: boolean, force: boolean): void {
+        return _private.scrollToItem(this, key, toBottom, force);
     },
 
     _beforeUnmount: function() {
