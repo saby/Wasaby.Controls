2c7a6cabcfeec99cdc3c6f1150b4547f5f01d670
av.shubin <av.shubin@tensor.ru>
2020-02-25 14:17:32 +0300

    Правки для переноса роутинга демок на wi.sbis-doc

--- Controls-demo/RootRouter.js
+++ Controls-demo/RootRouter.js
@@ -17,10 +17,11 @@ define('Controls-demo/RootRouter', [
       {
          _template: template,
          isReloading: false,
-         _pathName: '/Controls-demo/app/Controls-demo%2FIndexOld',
+         showMenu: true,
+         pathName: 'Controls-demo/app/Controls-demo%2FIndexOld',
          reload: function() {
             this.isReloading = true;
-            // РџСЂРё РѕР±РЅРѕРІР»РµРЅРёРё РґРµРјРєРё СЃР±СЂР°СЃС‹РІР°РµРј РІСЃРµ С‡С‚Рѕ Р»РµР¶РёС‚ РІ settingsController (Р·Р°РґР°РµС‚СЃСЏ РЅР° application);
+            // При обновлении демки сбрасываем все что лежит в settingsController (задается на application);
             window.localStorage.setItem('controlSettingsStorage', '{}');
          },
 
@@ -35,7 +36,7 @@ define('Controls-demo/RootRouter', [
          _isMenuButtonVisible: function() {
             var location = this._getLocation();
             if (location) {
-               return location.pathname !== this._pathName;
+               return location.pathname !== this._options.appRoot + this.pathName;
             }
             return null;
          },
@@ -45,7 +46,7 @@ define('Controls-demo/RootRouter', [
          },
 
          goHomeHandler: function() {
-            window.location = this._pathName;
+            window.location = this._options.appRoot+this.pathName;
          },
          _getLocation: function() {
             if (AppInit.isInit()) {
