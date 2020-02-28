db7e06f40d07f177823730a6cac0e5f96554c7ca
ps.kolesova <ps.kolesova@tensor.ru>
2020-02-28 12:04:23 +0300

    https://online.sbis.ru/opendoc.html?guid=aedf32aa-28db-40b7-abae-e740021cc782

--- Controls/_scroll/Scroll/Scrollbar.ts
+++ Controls/_scroll/Scroll/Scrollbar.ts
@@ -18,6 +18,7 @@ export interface IScrollBarOptions extends IControlOptions {
     contentSize: number;
     direction: TDirection;
     trackVisible: boolean;
+    thumbStyle?: string;
 }
 /**
  * Thin scrollbar.
@@ -69,6 +70,7 @@ class Scrollbar extends Control<IScrollBarOptions> {
     private _position: number = 0;
     private _thumbPosition: number = 0;
     private _thumbSize: number;
+    private _thumbStyle: string;
     private _scrollBarSize: number;
     // Запоминаемые на момент перетаскиваеия ползунка координаты самого скроллбара
     private _currentCoords: IScrollBarCoords | null = null;
@@ -76,9 +78,10 @@ class Scrollbar extends Control<IScrollBarOptions> {
     private _dragPointOffset: number | null = null;
     protected _trackVisible: boolean = false;
 
-    protected _beforeMount(): void {
+    protected _beforeMount(options: IScrollBarOptions): void {
         //TODO Compatibility на старых страницах нет Register, который скажет controlResize
         this._resizeHandler = this._resizeHandler.bind(this);
+        this._thumbStyle = this._getThumbStyle(options);
     }
 
     protected _afterMount(): void {
@@ -93,6 +96,10 @@ class Scrollbar extends Control<IScrollBarOptions> {
         if (!newEnv() && window) {
             window.addEventListener('resize', this._resizeHandler);
         }
+    }
+
+    protected _beforeUpdate(options: IScrollBarOptions): void {
+        this._thumbStyle = this._getThumbStyle(options);
     }
 
     protected _afterUpdate(oldOptions: IScrollBarOptions): void {
@@ -114,6 +121,16 @@ class Scrollbar extends Control<IScrollBarOptions> {
         if (!newEnv() && window) {
             window.removeEventListener('resize', this._resizeHandler);
         }
+    }
+
+    private _getThumbStyle(options: IScrollBarOptions): string {
+        if (options.thumbStyle) {
+            return options.thumbStyle;
+        }
+        if (options.theme === 'inverted') {
+            return 'inverted';
+        }
+        return (options.direction === 'vertical' ? 'accented' : 'unaccented');
     }
 
     private _getThumbCoordByScroll(scrollbarSize: number, thumbSize: number, scrollPosition: number): number {
