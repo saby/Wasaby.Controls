34fad36393b59d0726ee709a35991beb0f58a79c
ea.melnikova <ea.melnikova@tensor.ru>
2020-02-28 13:40:08 +0300

    https://online.sbis.ru/opendoc.html?guid=8c2d7254-6025-400c-9017-e8acc6cb65fd

--- Controls-demo/Suggest_new/Input/UpDownOpen/UpDownOpen.ts
+++ Controls-demo/Suggest_new/Input/UpDownOpen/UpDownOpen.ts
@@ -2,7 +2,7 @@ import {Control, TemplateFunction} from "UI/Base";
 import * as MemorySourceFilter from 'Controls-demo/Utils/MemorySourceFilter';
 import * as SearchMemory from 'Controls-demo/Search/SearchMemory';
 import {Memory} from 'Types/source';
-import {_departmentsDataLong} from 'Controls-demo/Suggest_new/DemoHelpers/DataCatalog';
+import {_departmentsDataLong, _departmentsDev} from 'Controls-demo/Suggest_new/DemoHelpers/DataCatalog';
 import controlTemplate = require('wml!Controls-demo/Suggest_new/Input/UpDownOpen/UpDownOpen');
 import suggestTemplate = require('wml!Controls-demo/Suggest_new/Input/UpDownOpen/resources/SuggestTemplate');
 import 'css!Controls-demo/Controls-demo';
@@ -12,11 +12,18 @@ export default class extends Control{
    protected _template: TemplateFunction = controlTemplate;
    protected _suggestTemplate: TemplateFunction = suggestTemplate;
    protected _source: Memory;
+   protected _indentSource: Memory;
    protected _navigation: object;
    protected _beforeMount() {
       this._source = new SearchMemory({
          keyProperty: 'id',
          data: _departmentsDataLong,
+         searchParam: 'title',
+         filter: MemorySourceFilter()
+      });
+      this._indentSource = new SearchMemory({
+         keyProperty: 'id',
+         data: _departmentsDev,
          searchParam: 'title',
          filter: MemorySourceFilter()
       });
