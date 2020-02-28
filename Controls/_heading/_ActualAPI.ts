b4aaf64690d5e695f1e1a58a702282acde479ef5
av.bondar <av.bondar@tensor.ru>
2020-02-28 15:52:48 +0300

    https://online.sbis.ru/opendoc.html?guid=0169eeb5-c80b-4e62-bfb9-1b63b6a7ea7d

--- Controls/_heading/_ActualAPI.ts
+++ Controls/_heading/_ActualAPI.ts
@@ -27,11 +27,20 @@ export function counterStyle(style: 'primary' | 'secondary' | 'disabled', fontCo
         return result || 'primary';
     }
 }
+export function backSizeOptions(size: string): string {
+    const fontSizesTable = {
+        s: 'l',
+        m: '3xl',
+        l: '4xl'
+    };
+    const result: string = fontSizesTable[size];
+    return result || '3xl';
+}
 
 export function backSize(options: IBackOptions): { fontSize: string, iconSize: string } {
     return {
-        fontSize: options.fontSize || options.size || 'm',
-        iconSize: options.iconSize || options.size || 'm'
+        fontSize: options.fontSize || backSizeOptions(options.size) ,
+        iconSize: options.iconSize || backSizeOptions(options.size)
     };
 }
 
