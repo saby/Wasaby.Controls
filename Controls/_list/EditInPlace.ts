0e53fd304dfd22842bcee9423a8eeb4363b5309f
as.avramenko <as.avramenko@tensor.ru>
2020-03-02 15:19:15 +0300

    https://online.sbis.ru/opendoc.html?guid=5a737942-e3c1-4adc-9d7b-089546c89c78

--- Controls/_list/EditInPlace.ts
+++ Controls/_list/EditInPlace.ts
@@ -32,13 +32,6 @@ var
             self._editingItem = options.item.clone();
             self._setEditingItemData(self._editingItem, self._options.listModel, self._options);
             self._notify('afterBeginEdit', [self._editingItem, isAdd]);
-
-            /**
-             * This code exists because there's no way to declaratively change editing item, so the users are forced to write something like this:
-             * editingItem.set('field', 'value').
-             */
-            self._editingItem.subscribe('onPropertyChange', self._resetValidation);
-
             return options;
         },
 
@@ -603,6 +596,12 @@ var EditInPlace = Control.extend(/** @lends Controls/_list/EditInPlace.prototype
             this._editingItemData = null;
             return;
         }
+
+        /**
+         * This code exists because there's no way to declaratively change editing item, so the users are forced to write something like this:
+         * editingItem.set('field', 'value').
+         */
+        this._editingItem.subscribe('onPropertyChange', this._resetValidation);
 
         let editingItemProjection;
         if (options.useNewModel) {
