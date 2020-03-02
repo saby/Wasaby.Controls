f2587b9b853661b1f69a9dea53b2a495e6d11438
aa.severyanov <aa.severyanov@tensor.ru>
2020-03-02 18:02:37 +0300

    https://online.sbis.ru/opendoc.html?guid=240be277-2859-41f3-b34a-eba7ececa4fd

--- Controls/compatibleError.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Библиотека с модулями для отображения дружелюбных ошибок на страницах WS3.
- * @library Controls/compatibleError
- * @includes process Controls/_compatibleError/process
- * @public
- * @author Северьянов А.А.
- */
-
-export { default as process } from './_compatibleError/process';
