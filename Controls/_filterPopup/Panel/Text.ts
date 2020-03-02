6568eefffa295f6c2271d539d047b593f7e63c73
se.mihaylov <se.mihaylov@tensor.ru>
2020-03-02 16:19:58 +0300

    https://online.sbis.ru/opendoc.html?guid=d4da4512-63f9-4046-bade-f53461b2bc8f

--- Controls/_filterPopup/Panel/Text.ts
+++ Controls/_filterPopup/Panel/Text.ts
@@ -23,7 +23,6 @@ import 'css!theme?Controls/filterPopup';
     * @author Герасимов А.М.
     */
 
-
    /**
     * @name Controls/_filterPopup/Panel/Text#caption
     * @cfg {String} Caption Текст, который будет отображаться рядом с кнопкой сброса.
@@ -45,8 +44,16 @@ import 'css!theme?Controls/filterPopup';
     *    </Controls.filterPopup:Text>
     * </pre>
     */
-
-
+   /**
+    * @name Controls/_filterPopup/Panel/Text#value
+    * @cfg {*} [value=true] Значение, которое будет установлено в конфигурацию фильтра после построения.
+    * @example
+    * <pre>
+    *    <Controls.filterPopup:Text>
+    *        <ws:value>-2</ws:value>
+    *    </Controls.filterPopup:Text>
+    * </pre>
+    */
 
    var Text = Control.extend({
       _template: template,
