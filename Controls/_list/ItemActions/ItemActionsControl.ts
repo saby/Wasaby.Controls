a8305a85ad81fb80b0fc4aece7c5c94ca9a68ab0
as.avramenko <as.avramenko@tensor.ru>
2020-03-04 14:26:29 +0300

    https://online.sbis.ru/opendoc.html?guid=fc1815f8-0ec0-463c-9f91-e16e4668a3cd

--- Controls/_list/ItemActions/ItemActionsControl.ts
+++ Controls/_list/ItemActions/ItemActionsControl.ts
@@ -58,13 +58,13 @@ var _private = {
             showed = showed.filter((action: any) => {
                 return action.showType === showType.TOOLBAR || action.showType === showType.MENU_TOOLBAR;
             });
-            // ACTION_ICON_CLASS нужен для отображаемых по ховеру операций - именно он обеспечивает их выравнивание
-            showed.forEach((action) => {
-                if (action.icon && !~action.icon.indexOf(ACTION_ICON_CLASS)) {
-                    action.icon += ' ' + ACTION_ICON_CLASS;
-                }
-            });
         }
+        // ACTION_ICON_CLASS нужен для отображаемых по ховеру операций - именно он обеспечивает их выравнивание
+        showed.forEach((action) => {
+            if (action.icon && !~action.icon.indexOf(ACTION_ICON_CLASS)) {
+                action.icon += ' ' + ACTION_ICON_CLASS;
+            }
+        });
 
         if (_private.needActionsMenu(all)) {
             showed.push({
