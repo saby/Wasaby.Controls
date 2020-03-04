bb0037ac1e138faf7f9ec4abe6fc01be2bc45f62
ma.rozov <ma.rozov@tensor.ru>
2020-03-04 16:25:07 +0300

    Revert "Merge pull request #3056 from saby/20.2000/bugfix/fix-fail-revert-2000"
    This reverts commit d00dc43b1ab06e3aa1d326cbedda1bb77d1e4197, reversing
    changes made to 0c6dc3e53ebd0f13d0965ea870a971773a2090df.

---
+++ Controls-demo/Filter_new/DetailPanel/HistoryId/Default/Index.ts
@@ -0,0 +1,15 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/Filter_new/DetailPanel/HistoryId/Default/Default';
+import 'Controls-demo/Filter_new/resources/HistorySourceDemo';
+import {getHistoryItems} from 'Controls-demo/Filter_new/resources/FilterItemsStorage';
+import 'css!Controls-demo/Controls-demo';
+import 'css!Controls-demo/Filter_new/Filter';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _source: unknown[] = [];
+
+    protected _beforeMount(): void {
+        this._source = getHistoryItems();
+    }
+}
--- Controls/_dropdown/_Controller.ts
+++ Controls/_dropdown/_Controller.ts
@@ -81,10 +81,10 @@ var _private = {
              self._filter = _private.prepareFilterForQuery(self, options);
 
              return sourceController.load(self._filter).addCallback((items) => {
-                self._setItems(items);
                 if (options.dataLoadCallback) {
                    options.dataLoadCallback(items);
                 }
+                self._setItems(items);
                 _private.updateSelectedItems(self,
                     options.emptyText,
                     options.selectedKeys,
@@ -410,6 +410,10 @@ var _Controller = Control.extend({
    },
 
    _beforeUpdate: function (newOptions) {
+      if (newOptions.readOnly && newOptions.readOnly !== this._options.readOnly) {
+          _private.closeDropdownList(this);
+      }
+
       if (_private.templateOptionsChanged(newOptions, this._options)) {
          this._depsDeferred = null;
          if (this._isOpened) {
