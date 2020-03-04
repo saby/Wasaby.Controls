01acb78cdd4872cf0eb4836715f6ec18c23947ca
vs.puhanov <vs.puhanov@tensor.ru>
2020-03-04 10:48:40 +0300

    adds "jump to end" button for position navigation https://online.sbis.ru/opendoc.html?guid=2f6be025-47e8-4a8c-a4b8-e48480aa8e27

--- Controls-demo/List/ScrollToEnd/Index.ts
+++ Controls-demo/List/ScrollToEnd/Index.ts
@@ -2,15 +2,18 @@ import { Control, TemplateFunction } from 'UI/Base';
 import template = require('wml!Controls-demo/List/ScrollToEnd/ScrollToEnd');
 import { Memory } from 'Types/source';
 
+const ITEMS_COUNT = 1000;
+const PAGE_SIZE = 40;
+
 export default class ScrollToEnd extends Control {
     protected _template: TemplateFunction = template;
 
-    protected _viewSource: Memory;
-    protected _navigation = {
+    protected _viewSourcePage: Memory;
+    protected _navigationPage = {
         source: 'page',
         view: 'infinity',
         sourceConfig: {
-            pageSize: 40,
+            pageSize: PAGE_SIZE,
             direction: 'both',
             page: 0,
             hasMore: false
@@ -21,11 +24,98 @@ export default class ScrollToEnd extends Control {
         }
     };
 
+    protected _viewSourcePosition: Memory;
+    protected _navigationPosition = {
+        source: 'position',
+        view: 'infinity',
+        sourceConfig: {
+            limit: PAGE_SIZE,
+            field: 'id',
+            position: 0,
+            direction: 'both'
+        },
+        viewConfig: {
+            pagingMode: 'direct',
+            showEndButton: true
+        }
+    };
+
     protected _beforeMount() {
-        this._viewSource = new Memory({
+        this._viewSourcePage = new Memory({
             keyProperty: 'id',
-            data: this._createItems(1000)
+            data: this._createItems(ITEMS_COUNT)
         });
+
+        this._viewSourcePosition = new Memory({
+            keyProperty: 'id',
+            data: this._createItems(ITEMS_COUNT),
+            filter: (item, filter) => {
+                // MemorySource не умеет работать с навигацией по курсору,
+                // эмулируем это поведение фильтром
+                if (!filter) {
+                    return true;
+                }
+
+                const id = item.get('id');
+                let rangeStart = 0;
+                let rangeEnd = ITEMS_COUNT;
+
+                if (typeof filter['id>='] !== 'undefined') {
+                    rangeStart = filter['id>='];
+                    rangeEnd = rangeStart + PAGE_SIZE;
+                } else if (typeof filter['id<='] !== 'undefined') {
+                    rangeEnd = filter['id<='];
+                    rangeStart = rangeEnd - PAGE_SIZE;
+                } else if (typeof filter._lastPage !== 'undefined') {
+                    rangeEnd = ITEMS_COUNT;
+                    rangeStart = rangeEnd - PAGE_SIZE;
+                }
+
+                return id >= rangeStart && id <= rangeEnd;
+            }
+        });
+        const originalQueryFn = this._viewSourcePosition.query;
+        this._viewSourcePosition.query = function(cursorQuery) {
+            // MemorySource не работает с отрицательными offset'ами,
+            // для демо выставим флаг последней страницы, который проверим
+            // в фильтре
+            if (cursorQuery.getOffset() === -1) {
+                cursorQuery
+                    .offset(0)
+                    .where({
+                        ...cursorQuery.getWhere(),
+                        _lastPage: true
+                    });
+            }
+            return originalQueryFn.apply(this, arguments).then((result) => {
+                const resultData = result.getRawData();
+
+                const nextKey = resultData.items[resultData.items.length - 1].id + 1;
+                const prevKey = resultData.items[0].id - 1;
+
+                // Чтобы правильно работала навигация по курсору, нужно выставить
+                // ключи предыдущей/следующей записи и наличие оставшихся записей
+                const filter = cursorQuery.getWhere() || {};
+                if (typeof filter['id>='] !== 'undefined') {
+                    resultData.meta.more = nextKey <= 999;
+                    resultData.meta.nextPosition = nextKey <= 999 ? [nextKey] : [];
+                } else if (typeof filter['id<='] !== 'undefined') {
+                    resultData.meta.more = prevKey >= 0;
+                    resultData.meta.nextPosition = prevKey >= 0 ? [prevKey] : [];
+                } else {
+                    resultData.meta.more = {
+                        before: prevKey >= 0,
+                        after: nextKey <= 999
+                    };
+                    resultData.meta.nextPosition = {
+                        before: prevKey >= 0 ? [prevKey] : [],
+                        after: nextKey <= 999 ? [nextKey] : []
+                    };
+                }
+
+                return result;
+            });
+        };
     }
 
     private _createItems(count: number): unknown[] {
