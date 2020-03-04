f5ff1a675a1d2d1137c77c1c83347ec96639f076
am.gerasimov <am.gerasimov@tensor.ru>
2020-03-04 09:41:39 +0300

    https://online.sbis.ru/opendoc.html?guid=df2b31d4-9fa4-4cc9-8c12-736fa47d390b

--- Controls/operationsPanel.ts
+++ Controls/operationsPanel.ts
@@ -2,7 +2,6 @@
  * Библиотека контролов.
  * @library Controls/operationsPanel
  * @includes Panel Controls/_operationsPanel/OperationsPanel
- * @includes Container Controls/_operationsPanel/Container
  * @private
  * @author Крайнов Д.О.
  */
@@ -11,7 +10,6 @@
  * operations library
  * @library Controls/operationsPanel
  * @includes Panel Controls/_operationsPanel/OperationsPanel
- * @includes Container Controls/_operationsPanel/Container
  * @private
  * @author Крайнов Д.О.
  */
@@ -20,6 +18,5 @@ import OperationsPanel = require('Controls/_operationsPanel/OperationsPanel');
 import Container from 'Controls/_operationsPanel/Container';
 
 export {
-   OperationsPanel,
-   Container
-}
+   OperationsPanel
+};
