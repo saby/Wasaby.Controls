e84ce9bcc59bd646deb24b52c4b91917722408c5
ps.kolesova <ps.kolesova@tensor.ru>
2020-03-04 18:12:25 +0300

    https://online.sbis.ru/opendoc.html?guid=d60d3772-204f-4a4a-97d8-8fa87b3f8b4b

--- Controls/_editableArea/View.ts
+++ Controls/_editableArea/View.ts
@@ -85,8 +85,9 @@ var
  * @author Авраменко А.С.
  * @public
  *
- * @css @background-color_EditableArea_style_withBackground Background color of the input field with the style option set to "accentHeader".
- * @css @spacing_EditableArea-between-editor-toolbar Spacing between the editor and the toolbar.
+ * @remark
+ * Для предотвращения скачков текста при переключении из режима редактирования в режим чтения используйте css-класс
+ * controls-Input_negativeOffset_theme_{{_options.theme}} на редактирумой области.
  *
  * @demo Controls-demo/EditableArea/EditableArea
  */
