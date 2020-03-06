e74924d2ad249cf9e2320eff70ef4a9ac09aefbb
av.bondar <av.bondar@tensor.ru>
2020-03-06 14:55:09 +0300

    https://online.sbis.ru/opendoc.html?guid=92ffe7a2-e4bb-425a-a114-461769b1c3f7

---
+++ Controls-demo/Input/Date/Range.ts
@@ -0,0 +1,19 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import template = require('wml!Controls-demo/Input/Date/Range');
+import 'css!Controls-demo/Input/Date/Range';
+
+class Range extends Control<IControlOptions> {
+   protected _template: TemplateFunction = template;
+   static _theme: string[] = ['Controls/Classes'];
+   protected _startDate: Date =  new Date(2017, 0, 1, 12, 15, 30, 123);
+   protected _endDate: Date =  new Date(2017, 0, 2, 12, 15, 30, 123);
+
+   protected _masks: Array<string> = [
+       'DD.MM.YY',
+       'DD.MM.YYYY'
+       ];
+   protected _afterMount(): void {
+      this._children.input0.activate();
+   }
+}
+export default Range;
