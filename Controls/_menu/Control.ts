dce540fe279d23e4bac38f0711ccac75bb8460ac
ee.zolotova <ee.zolotova@tensor.ru>
2020-03-13 08:22:47 +0300

    https://online.sbis.ru/opendoc.html?guid=51ba9ce0-5285-48a4-bd1f-82f4be891fc1

--- Controls/_menu/Control.ts
+++ Controls/_menu/Control.ts
@@ -16,6 +16,7 @@ import {factory} from 'Types/chain';
 import {isEqual} from 'Types/object';
 import scheduleCallbackAfterRedraw from 'Controls/Utils/scheduleCallbackAfterRedraw';
 import * as ControlsConstants from 'Controls/Constants';
+import {_scrollContext as ScrollData} from 'Controls/scroll';
 
 /**
  * Контрол меню.
@@ -602,6 +603,12 @@ class MenuControl extends Control<IMenuOptions> implements IMenuControl {
     }
 
     static _theme: string[] = ['Controls/menu', 'Controls/dropdownPopup'];
+
+    static _getChildContext(): object {
+        return {
+            ScrollData: new ScrollData({pagingVisible: false})
+        };
+    }
 
     static getDefaultOptions(): object {
         return {
