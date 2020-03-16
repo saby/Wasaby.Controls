d15473b5e6549113cc58abf020273d5083224b7a
aa.bykanov <aa.bykanov@tensor.ru>
2020-03-16 14:33:00 +0300

    исправлена формулировка https://online.sbis.ru/opendoc.html?guid=c79e02f5-6b1f-4961-9637-201ac88753f4

--- Controls/interface/IItemTemplate.js
+++ Controls/interface/IItemTemplate.js
@@ -35,8 +35,8 @@ define('Controls/interface/IItemTemplate', [
     * Опция itemTemplate в качестве значения принимает только шаблон.
     * В разделе "Примеры" показано как с помощью директивы {@link /doc/platform/developmentapl/interface-development/ui-library/template-engine/#ws-partial ws:partial} задать шаблон Controls/list:ItemTemplate с новой конфигурацией.
     * 
-    * Также в опцию itemTemplate можно передавать и более сложные шаблоны, которые содержат иные директивы, например, директивы {@link /doc/platform/developmentapl/interface-development/ui-library/template-engine/#ws-if ws:if}.
-    * Однако каждая ветка вычисления шаблона должна заканчиваться директивой ws:partial, которая встраивает Controls/list:ItemTemplate.
+    * Также в опцию itemTemplate можно передавать и более сложные шаблоны, которые содержат иные директивы, например {@link /doc/platform/developmentapl/interface-development/ui-library/template-engine/#ws-if ws:if}.
+    * В этом случае каждая ветка вычисления шаблона должна заканчиваться директивой ws:partial, которая встраивает Controls/list:ItemTemplate.
     * 
     * Дополнительно о работе с шаблоном вы можете прочитать в {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/list/templates/item/ руководстве разработчика}.
     * @example
