52ed18c187e2da403156d8b49182486d65643bed
ee.zolotova <ee.zolotova@tensor.ru>
2020-03-17 10:31:25 +0300

    https://online.sbis.ru/opendoc.html?guid=7543760e-061b-47c8-8489-9c5c66fb8f3e

--- Controls/interface/IDropdown.js
+++ Controls/interface/IDropdown.js
@@ -422,6 +422,39 @@ define('Controls/interface/IDropdown', [], function() {
     * @cfg {String} Unique id for save history.
     */
 
+   /**
+    * @name Controls/interface/IDropdown#menuPopupOptions
+    * @cfg {Controls/popup:IStickyPopupOptions} Опции для окна выпадающего списка
+    * @example
+    * Открываем окно выпадающего списка влево. По умолчанию окно открывается вправо.
+    * WML:
+    * <pre>
+    *    <Controls.dropdown:Button source="{{_source}}" displayProperty="title" keyProperty="id"
+    *       menuPopupOptions={{_menuPopupOptions}}/>
+    * </pre>
+    *
+    * JS:
+    * <pre>
+    *     import sourceLib from "Types/source"
+    *
+    *     _beforeMount() {
+    *         this._source = new sourceLib.Memory({
+    *             keyProperty: 'id',
+    *             data: [
+    *                {id: 1, title: 'Name'},
+    *                {id: 2, title: 'Date of change'}
+    *             ]
+    *         });
+    *         this._menuPopupOptions = {
+    *            direction: {
+    *               horizontal: 'left',
+    *               vertical: 'bottom'
+    *            }
+    *         }
+    *     }
+    * </pre>
+    *
+    */
 
    /**
     * @event Controls/interface/IDropdown#dropDownOpen Происходит при открытии выпадающего списка.
