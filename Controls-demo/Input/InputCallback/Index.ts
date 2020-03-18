d3f0231ba6d50301b83a1fee059d016af6949c06
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2020-03-18 10:17:10 +0300

    https://online.sbis.ru/opendoc.html?guid=40076891-6b62-4b72-b063-0c62d3ca68cc

--- Controls-demo/Input/InputCallback/Index.ts
+++ Controls-demo/Input/InputCallback/Index.ts
@@ -7,12 +7,17 @@ import 'css!Controls-demo/Controls-demo';
 const MAX_LENGTH = 5;
 
 class InputCallback extends Control<IControlOptions> {
+    protected _template: TemplateFunction = controlTemplate;
     protected _maxLength: number = MAX_LENGTH;
     protected _lengthCallback: ICallback<number> = IC.lengthConstraint(MAX_LENGTH);
     protected _hoursFormat: ICallback<TimeInterval> = IC.hoursFormat;
-    protected _timeIntervalValue = new TimeInterval();
-
-    protected _template: TemplateFunction = controlTemplate;
+    protected _timeIntervalValue: TimeInterval = new TimeInterval();
+    protected _upperCaseCallback: ICallback<string> = (data) => {
+        return {
+            displayValue: data.displayValue.toUpperCase(),
+            position: data.position
+        };
+    }
 
     static _theme: string[] = ['Controls/Classes'];
 }
