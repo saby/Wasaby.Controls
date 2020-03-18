75358a9be14449c6deeab5d5467fab0ee9f5e77e
vd.volockoy <vd.volockoy@tensor.ru>
2020-03-18 12:38:26 +0300

    https://online.sbis.ru/opendoc.html?guid=ea354034-fd77-4461-a368-1a8019fcb0d4

--- Controls/_list/ScrollContainer.ts
+++ Controls/_list/ScrollContainer.ts
@@ -72,6 +72,18 @@ export default class ScrollContainer extends Control<IOptions> {
 
     private _triggerVisibility: ITriggerState = {up: false, down: false};
 
+    // В IE иногда возникает ситуация, что смена видимости триггера срабатывает с задержкой
+    // вследствие чего получаем ошибку в вычислениях нового range и вообше делаем по сути
+    // лишние пересчеты, например: https://online.sbis.ru/opendoc.html?guid=ea354034-fd77-4461-a368-1a8019fcb0d4
+    // TODO: этот код должен быть убран после
+    // https://online.sbis.ru/opendoc.html?guid=702070d4-b401-4fa6-b457-47287e44e0f4
+    private get _IEtriggerVisibility(): ITriggerState {
+        return {
+            up: this._triggerOffset > this._lastScrollTop,
+            down: this._lastScrollTop + this._viewportHeight >= this._viewHeight - this._triggerOffset
+        };
+    }
+
     private _restoreScrollResolve: Function;
     private _applyScrollTopCallback: Function;
     private _checkTriggerVisibilityTimeout: number;
@@ -276,11 +288,11 @@ export default class ScrollContainer extends Control<IOptions> {
      */
     private _checkTriggerVisibility(): void {
         if (!this._applyScrollTopCallback) {
-            if (this._triggerVisibility.down) {
+            if (detection.isIE ? this._IEtriggerVisibility.down : this._triggerVisibility.down) {
                 this._recalcToDirection('down');
             }
 
-            if (this._triggerVisibility.up) {
+            if (detection.isIE ? this._IEtriggerVisibility.up : this._triggerVisibility.up) {
                 this._recalcToDirection('up');
             }
         }
