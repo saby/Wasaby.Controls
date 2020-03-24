547cc193b9b9c3eb0d60875735dc51c31b8ca298
yan.sirotkin <yan.sirotkin@tensor.ru>
2020-03-24 17:08:00 +0300

    https://online.sbis.ru/opendoc.html?guid=13911438-5dfd-40c2-8c99-7a096996ade0 Добавлены примеры

--- Controls/_grid/interface/IHeaderCell.ts
+++ Controls/_grid/interface/IHeaderCell.ts
@@ -51,6 +51,76 @@ export interface IHeaderCell extends IControlOptions {
      * @remark
      * Параметры шаблона Controls/grid:HeaderContent доступны {@link Controls/grid:HeaderContent здесь}.
      * Подробнее о работе с шаблоном читайте в {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/grid/templates/header/ документации}.
+     * @example
+    * **Пример 1.** Шаблон и контрол сконфигурированы в одном WML-файле.
+    * <pre class="brush: html">
+    * <Controls.grid:View>
+    *    <ws:header>
+    *       <ws:Array>
+    *          <ws:Object>
+    *             <ws:template>
+    *                <ws:partial template="Controls/grid:HeaderContent">
+    *                   <ws:contentTemplate>
+    *                      {{contentTemplate.colData.column.title}}
+    *                   </ws:contentTemplate>
+    *                </ws:partial>
+    *             </ws:template>
+    *          </ws:Object>
+    *       </ws:Array>
+    *    </ws:header>
+    * </Controls.grid:View>
+    * </pre>
+    * 
+    * **Пример 2.** Контрол и шаблоны сконфигурированы в отдельных WML-файлах.
+    * <pre class="brush: html">
+    * <!-- file1.wml --> 
+    * <Controls.grid:View>
+    *    <ws:header>
+    *       <ws:Array>
+    *          <ws:Object>
+    *             <ws:template>
+    *                <ws:partial template="wml!file2" scope="{{template}}"/>
+    *             </ws:template>
+    *          </ws:Object>
+    *       </ws:Array>
+    *    </ws:header>
+    * </Controls.grid:View>
+    * </pre>
+    * 
+    * <pre class="brush: html">
+    * <!-- file2.wml -->
+    * <ws:partial template="Controls/grid:HeaderContent">
+    *    <ws:contentTemplate>
+    *       {{contentTemplate.colData.column.title}}
+    *    </ws:contentTemplate>
+    * </ws:partial>
+    * </pre>
+    * 
+    * **Пример 3.** Шаблон contentTemplate сконфигурирован в отдельном WML-файле.
+    * 
+    * <pre class="brush: html">
+    * <!-- file1.wml --> 
+    * <Controls.grid:View>
+    *    <ws:header>
+    *       <ws:Array>
+    *          <ws:Object>
+    *             <ws:template>
+    *                <ws:partial template="Controls/grid:HeaderContent">
+    *                   <ws:contentTemplate>
+    *                      <ws:partial template="wml!file2" scope="{{contentTemplate}}"/>
+    *                   </ws:contentTemplate>
+    *                </ws:partial>
+    *             </ws:template>
+    *          </ws:Object>
+    *       </ws:Array>
+    *    </ws:header>
+    * </Controls.grid:View>
+    * </pre>
+    * 
+    * <pre class="brush: html">
+    * <!-- file2.wml -->
+    * {{contentTemplate.colData.column.title}}
+    * </pre>
      */
     template?: TemplateFunction;
     /**
