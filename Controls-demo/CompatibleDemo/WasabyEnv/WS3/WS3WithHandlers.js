5fb993dfe8ff83a2b8b45bbc691b0b489feca661
va.ten <va.ten@tensor.ru>
2020-03-26 11:48:48 +0300

    https://online.sbis.ru/opendoc.html?guid=7de32df5-465b-41f7-b0ea-8c7764d8b6a0

--- Controls-demo/CompatibleDemo/WasabyEnv/WS3/WS3WithHandlers.js
+++ Controls-demo/CompatibleDemo/WasabyEnv/WS3/WS3WithHandlers.js
@@ -12,9 +12,11 @@ define('Controls-demo/CompatibleDemo/WasabyEnv/WS3/WS3WithHandlers',
          _template: template,
          _compatibleReady: false,
          _text: null,
+         _textNew: null,
 
          _beforeMount: function() {
             this._text = 'Wait...';
+            this._textNew = this._text;
          },
 
          _afterMount: function() {
@@ -23,11 +25,17 @@ define('Controls-demo/CompatibleDemo/WasabyEnv/WS3/WS3WithHandlers',
                .addCallback(function() {
                   self._compatibleReady = true;
                   self._text = 'Init success!';
+                  self._setText(null, self._text);
                   self._forceUpdate();
                });
          },
-         _setText: function(e, value) {
+
+         _setTextNew: function(e, value) {
             this._text = value;
+         },
+
+         _setText: function(e, value) {
+            this._textNew = value;
          },
 
          _setTextOld: function(e, value) {
