24dcdaf26d29196cf8832e22ba27640f42826ce0
Pavel Averkiev <dr.rovico@gmail.com>
2020-03-31 10:12:05 +0300

    https://online.sbis.ru/opendoc.html?guid=5b04c8f6-ab31-401d-9860-3ccedf638157 Разделил демки на разные файлы

--- Controls-demo/treeGrid/ItemPadding/Horizontal/Index.ts
+++ Controls-demo/treeGrid/ItemPadding/Horizontal/Index.ts
@@ -1,7 +1,7 @@
 import {TemplateFunction} from 'UI/Base';
 import Baseindex from '../BaseIndex';
 
-import * as Template from 'wml!Controls-demo/treeGrid/ItemPadding/Custom/Custom';
+import * as Template from 'wml!Controls-demo/treeGrid/ItemPadding/Horizontal/Horizontal';
 
 export default class extends Baseindex {
     protected _template: TemplateFunction = Template;
--- Controls-demo/treeGrid/ItemPadding/Index.ts
+++ Controls-demo/treeGrid/ItemPadding/Index.ts
@@ -1,7 +1,20 @@
-import {Control, TemplateFunction} from "UI/Base"
-import * as Template from "wml!Controls-demo/treeGrid/ItemPadding/ItemPadding"
-import 'css!Controls-demo/Controls-demo'
+import {Control, TemplateFunction} from 'UI/Base';
+import {Memory} from 'Types/source';
+import 'css!Controls-demo/Controls-demo';
+
+import {Gadgets} from '../DemoHelpers/DataCatalog';
+
+import * as Template from 'wml!Controls-demo/treeGrid/ItemPadding/ItemPadding';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
+    protected _viewSource: Memory;
+    protected _columns = Gadgets.getColumnsForFlat();
+
+    protected _beforeMount() {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: Gadgets.getFlatData()
+        });
+    }
 }
