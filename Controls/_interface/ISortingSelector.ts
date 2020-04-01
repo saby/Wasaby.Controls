8df19e8623412c4d0992bb1081e443e877709122
aa.bykanov <aa.bykanov@tensor.ru>
2020-04-01 12:33:55 +0300

    Исправлено описание  Controls/grid:SortingSelector https://online.sbis.ru/opendoc.html?guid=c6a422d0-960a-459f-a736-355586776282

--- Controls/_interface/ISortingSelector.ts
+++ Controls/_interface/ISortingSelector.ts
@@ -6,93 +6,111 @@
  * @extends Core/Control
  * @mixes Controls/_interface/ISource
  * @public
- */
-
-/**
- * @typedef {Object} SortingParam Объект, содержащий данные о поле для сортировки и название, для отображения в выпадающем списке.
- * @property {String|null} paramName Поле для сортировки. Чтобы задать сброс сортировки, нужно указать значение null
- * @property {String} title Название, для отображения параметра в выпадающем списке
- * @example
- * В опцию передается массив вида
- * <pre class="brush: js;">
- * [{
- *     paramName: 'FirstParam',
- *     title: 'По первому параметру'
- * },
- * {
- *     paramName: 'SecondParam',
- *     title: 'По второму параметру'
- * }]
- * </pre>
- * где paramName - это название поля элемента данных, по которому может осуществляться сортировка,
- * а title - подпись пункта меню, соответствующего данному полю.
- *
- * Чтобы дать возможность сброса сортировки, нужно добваить пункт, со значением paramName = null
- * <pre class="brush: js;">
- * [{
- *     paramName: null,
- *     title: 'По умолчанию'
- * },
- * {
- *     paramName: 'FirstParam',
- *     title: 'По первому параметру'
- * }]
- * </pre>
- * @remark Если не задан пункт, сбрасывающий сортировку, то необходимо указать непустую конфигурацию сортировки в опции value.
- */
-
-/**
- * @name Controls/grid:SortingSelector#value
- * @cfg {Array<Object>} Конфигурация сортировки.
- * @remark Если нет возможности сброса сортировки, то опция value должна содерать данные для сортировки.
- * @example
- * <pre class="brush: js;">
- *  this._sortingSource = new source.Memory({
- *      keyProperty: 'key',
- *      data: [
- *         {
- *            key: '1',
- *            title: 'По цене',
- *            sortingParam: 'price'
- *         },
- *         {
- *            key: '2',
- *            title: 'По количеству',
- *            sortingParam: 'count'
- *         },
- *      ]
- *   });
- * this._sortingValue = [
- *       {
- *          price: 'DESC'
- *       }
- *   ];
- * </pre>
- * Следует использовать bind на опцию value.
- * <pre class="brush: html;">
- * <Controls.grid:SortingSelector
- *   bind:value="_sortingValue"
- *   source="{{_sortingSource}}"
- *   sortingParamProperty="sortingParam'
- *   displayProperty="title"
- * />
- * </pre>
- */
-
-/**
- * @name Controls/grid:SortingSelector#header
- * @cfg {String} Заголовок для меню сортировки.
- * @remark Если заголовок не требуется, можно не указывать.
+ * @demo Controls-demo/grid/Sorting/SortingSelector/Index
+ * @author Авраменко А.С.
  */
 
 import {IControlOptions} from 'UI/Base';
 
+/**
+ * @typedef {Object} SortingParam
+ * @property {String|null} paramName Поле поля элемента, по которому может осуществляться сортировка. Чтобы задать сброс сортировки, нужно указать значение null.
+ * @property {String} title Подпись пункта меню, соответствующего данному полю.
+ * @remark Если не задан пункт, сбрасывающий сортировку, то необходимо указать непустую конфигурацию сортировки в опции value.
+ */
 export interface ISortingParam {
    paramName: string | null;
    title: string;
 }
 export interface ISortingSelectorOptions extends IControlOptions {
+   
+   /**
+    * @name Controls/grid:SortingSelector#sortingParams
+    * @cfg {Array.<SortingParam>} Параметры сортировки.
+    * @example
+    * В опцию передается массив вида
+    * <pre class="brush: js;">
+    * _sortingParam: null,
+    * _beforeMount: function(options) {
+    *    this._sortingParam = [
+    *       {
+    *          paramName: 'FirstParam',
+    *          title: 'По первому параметру'
+    *       },
+    *       {
+    *          paramName: 'SecondParam',
+    *          title: 'По второму параметру'
+    *       }
+    *    ]
+    * }
+    * </pre>
+    *
+    * Чтобы дать возможность сброса сортировки, нужно добавить пункт со значением paramName = null.
+    * 
+    * <pre class="brush: js; highlight: [5]">
+    * _sortingParam: null,
+    * _beforeMount: function(options) {
+    *    this._sortingParam = [
+    *       {
+    *          paramName: null,
+    *          title: 'По умолчанию'
+    *       },
+    *       {
+    *          paramName: 'FirstParam',
+    *          title: 'По первому параметру'
+    *       }
+    *    ]
+    * }
+    * </pre>
+    */
    sortingParams: [ISortingParam];
+   /**
+    * @name Controls/grid:SortingSelector#value
+    * @cfg {Array.<Object>} Конфигурация сортировки.
+    * @remark Если нет возможности сброса сортировки, то опция value должна содержать данные для сортировки.
+    * @example
+    * <pre class="brush: js;">
+    * _sortingValue: null,
+    * _sortingSource: null,
+    * _beforeMount: function(options) {
+    *    this._sortingSource = new source.Memory({
+    *       keyProperty: 'key',
+    *       data: [
+    *          {
+    *             key: '1',
+    *             title: 'По цене',
+    *             sortingParam: 'price'
+    *          },
+    *          {
+    *             key: '2',
+    *             title: 'По количеству',
+    *             sortingParam: 'count'
+    *          },
+    *       ]
+    *    });
+    *    this._sortingValue = [
+    *       {
+    *          price: 'DESC'
+    *       }
+    *    ];
+    * }
+    * </pre>
+    * 
+    * Следует использовать директиву bind для опции value.
+    * 
+    * <pre class="brush: html; highlight: [2,4]">
+    * <Controls.grid:SortingSelector
+    *   bind:value="_sortingValue"
+    *   source="{{_sortingSource}}"
+    *   sortingParamProperty="sortingParam"
+    *   displayProperty="title" />
+    * </pre>
+    */
    value: [object];
+   /**
+    * @name Controls/grid:SortingSelector#header
+    * @cfg {String} Заголовок для меню сортировки.
+    * @remark Если заголовок не требуется, можно не указывать.
+    */
    header: string;
 }
