4bf9b46c856841f849e4ed5afc2462f92a675537
aa.bykanov <aa.bykanov@tensor.ru>
2020-04-02 15:38:58 +0300

    Исправлена ошибка в API https://online.sbis.ru/opendoc.html?guid=8d70676f-4d66-4884-bac3-49749b5cf28b

--- Controls/_interface/INavigation.ts
+++ Controls/_interface/INavigation.ts
@@ -50,8 +50,8 @@ export type TNavigationView = 'infinity' | 'pages' | 'demand' | 'maxCount';
 /**
  * @typedef {String} Direction
  * @description Направление выборки при навигации по курсору.
- * @variant after Вверх.
- * @variant before Вниз.
+ * @variant after Вниз.
+ * @variant before Вверх.
  * @variant both В обоих направлениях.
  */
 
