9d81c0d111d8cd6206e9565538eab0f3cc7a8496
av.bondar <av.bondar@tensor.ru>
2020-04-06 17:04:24 +0300

    https://online.sbis.ru/opendoc.html?guid=31c0d237-19be-46c3-977c-24821a380760

--- Controls/_editableArea/View.ts
+++ Controls/_editableArea/View.ts
@@ -117,6 +117,7 @@ var View = Control.extend( /** @lends Controls/List/View.prototype */ {
    _afterUpdate: function () {
       if (this._beginEditTarget) {
          // search closest input and focus
+         // В Controls.lookup:Input/Controls.Suggest:Input может не быть внутри инпута. Чтобы не падала ошибка, при отсутствии инпута фокус не ставим.
          this._beginEditTarget.getElementsByTagName('input')[0]?.focus();
          this._beginEditTarget = null;
       }
