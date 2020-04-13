a9fddecf461a43f259131f644010e35653f45a0a
av.bondar <av.bondar@tensor.ru>
2020-04-13 18:43:49 +0300

    https://online.sbis.ru/opendoc.html?guid=f34264c9-5895-485a-b5cb-d5a45f0930a6

--- Controls/interface/IBreadCrumbs.js
+++ Controls/interface/IBreadCrumbs.js
@@ -51,43 +51,13 @@ define('Controls/interface/IBreadCrumbs', [
    /**
     * @name Controls/interface/IBreadCrumbs#backgroundStyle
     * @cfg {string} Префикс стиля для настройки фона внутренних компонентов хлебных крошек с абсолютным позиционированием.
-    * @variant default (фон цвета темы)
-    * @variant stackHeader  Используется когда крошки располагаются в шапке стекового окна
-    * @variant masterDetail-master Используется, когда крошки располагаются в контенте master контрола masterDetail
-    * @variant masterDetail-details Используется, когда крошки располагаются в контенте detail контрола masterDetail
-    * @variant blockLayout__block_style_(number) Используется, когда крошки располагаются в области блоков controls-BlockLayout__block. Подробнее {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/content-managment/containers/blocks/ здесь}
-    * @variant другое - вы сами можете задать значение опции и определить класс с необходимым цветом
     * @default default (фон цвета темы)
-    * @example
-    * <pre>
-    *     <Controls.breadcrumbs:Path items="{{items}}" on:itemClick="_onItemClick()" keyProperty="id" backgroundStyle="MyColor"/>
-    *     ......
-    *    .controls-BreadCrumbsView__menu__dots-background-MyColor_theme-@{themeName} {
-    *       background: red;
-    *       box-shadow: @dots_shadow-offset_breadcrumbsView red;
-    *    }
-    * </pre>
     */
 
    /*
     * @name Controls/interface/IBreadCrumbs#backgroundStyle
     * @cfg {string} Style prefix to configure background for breadcrumbs components with absolute positioning.
-    * @variant default (theme background)
-    * @variant stackHeader Used when breadcrumbs are in the header of the stack.
-    * @variant masterDetail-master  Used when breadcrumbs are in "master" of masterDetail
-    * @variant masterDetail-details Used when breadcrumbs are in "detail" of masterDetail
-    * @variant blockLayout__block_style_(number) More details {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/content-managment/containers/blocks/ here}
-    * @variant other - you can set your option value and define a class with the desired color
     * @default default (theme background)
-    * @example
-    * <pre>
-    *     <Controls.breadcrumbs:Path items="{{items}}" on:itemClick="_onItemClick()" keyProperty="id" backgroundStyle="MyColor"/>
-    *     ......
-    *    .controls-BreadCrumbsView__menu__dots-background-MyColor_theme-@{themeName} {
-    *       background: red;
-    *       box-shadow: @dots_shadow-offset_breadcrumbsView red;
-    *    }
-    * </pre>
     */
 
 
