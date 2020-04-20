14e4b79a194a5aaaf638f3e008a9116e0a2b83cb
geraltofrivia99 <geralt.from.rivi9@yandex.ru>
2020-04-20 13:47:53 +0700

    https://online.sbis.ru/opendoc.html?guid=7035f6d2-d60b-4c60-8643-184a8f351a0c

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -2934,7 +2934,7 @@ BaseControl.contextTypes = function contextTypes() {
     };
 };
 
-BaseControl._theme = ['Controls/Classes', 'Controls/list_multi', 'Controls/list'];
+BaseControl._theme = ['Controls/Classes', 'Controls/list'];
 
 BaseControl.getDefaultOptions = function() {
     return {
