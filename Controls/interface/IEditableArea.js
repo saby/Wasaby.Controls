b26c2eceef2c31d050155afb366e03899849a465
yan.sirotkin <yan.sirotkin@tensor.ru>
2020-04-21 13:40:46 +0300

    https://online.sbis.ru/opendoc.html?guid=114aebaa-7274-4353-b11b-f64bb778ed11 Исправлено описание

--- Controls/interface/IEditableArea.js
+++ Controls/interface/IEditableArea.js
@@ -33,9 +33,10 @@ define('Controls/interface/IEditableArea', [
     */    
 
    /**
-    * @typedef {String|Promise} BeforeEndEditResult
-    * @variant {Promise} Deferred Используется для сохранения с пользовательской логикой.
-    * @variant {String} Cancel Отменяет окончание редактирования.
+    * @typedef {String|Promise|undefined} BeforeEndEditResult Результат, возвращаемый обработчиком события beforeBeginEdit.
+    * @variant Promise - Используется для сохранения с пользовательской логикой.
+    * @variant Cancel - Отменяет окончание редактирования.
+    * @variant undefined - Стандартное завершение редактирования. Сохранение происходит на стороне платформы.
     */
 
    /*
