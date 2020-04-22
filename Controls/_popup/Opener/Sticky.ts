03b1d5119b86acd4d1262db7bf417a66ff364750
av.bondar <av.bondar@tensor.ru>
2020-04-22 12:02:17 +0300

    https://online.sbis.ru/opendoc.html?guid=bfee3167-c719-4ab2-82b3-fb93c243ef7e

--- Controls/_popup/Opener/Sticky.ts
+++ Controls/_popup/Opener/Sticky.ts
@@ -66,8 +66,8 @@ class Sticky extends BaseOpener<IStickyOpenerOptions> implements IStickyOpener {
         if (this.isOpened() && event.type === 'scroll') {
             // Из-за флага listenAll на listener'e, подписка доходит до application'a всегда.
             // На ios при показе клавиатуры стреляет событие скролла, что приводит к вызову текущего обработчика
-            // и закрытию окна. Для ios отключаю реакцию на скролл, событие скролла стрельнуло на body.
-            if (detection.isMobileIOS && scrollEvent.target === document.body) {
+            // и закрытию окна. Для ios отключаю реакцию на скролл, событие скролла стрельнуло на body ил на html.
+            if (detection.isMobileIOS && (scrollEvent.target === document.body || scrollEvent.target === document)) {
                 return;
             } else if (this._actionOnScroll === 'close') {
                 this.close();
