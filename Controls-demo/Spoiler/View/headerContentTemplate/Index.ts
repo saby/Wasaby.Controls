1290734b93b7d1e7575a044acaffd024870c8976
av.bondar <av.bondar@tensor.ru>
2020-04-24 17:37:11 +0300

    https://online.sbis.ru/opendoc.html?guid=a71773aa-a729-406e-8740-ce2cb3c43881

--- Controls-demo/Spoiler/View/headerContentTemplate/Index.ts
+++ Controls-demo/Spoiler/View/headerContentTemplate/Index.ts
@@ -1,10 +1,10 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
-import controlTemplate = require('wml!Controls-demo/Spoiler/View/View');
+import controlTemplate = require('wml!Controls-demo/Spoiler/View/headerContentTemplate/headerContentTemplate');
 import 'css!Controls-demo/Controls-demo';
 
-class View extends Control<IControlOptions> {
-    protected _expanded: boolean = true;
+
+class HeaderContentTemplate extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
     static _theme: string[] = ['Controls/Classes'];
 }
-export default View;
+export default HeaderContentTemplate;
--- Controls-demo/Spoiler/View/headerContentTemplate/template/Index.ts
+++ Controls-demo/Spoiler/View/headerContentTemplate/template/Index.ts
@@ -1,10 +1,9 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
-import controlTemplate = require('wml!Controls-demo/Spoiler/View/View');
+import controlTemplate = require('wml!Controls-demo/Spoiler/View/headerContentTemplate/template/Template');
 import 'css!Controls-demo/Controls-demo';
 
-class View extends Control<IControlOptions> {
-    protected _expanded: boolean = true;
+class Template extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
     static _theme: string[] = ['Controls/Classes'];
 }
-export default View;
+export default Template;
