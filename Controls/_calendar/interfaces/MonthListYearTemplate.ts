e703ea3510cf2776215deab9a06728c8e2ebd6ce
gd.kovalev <gd.kovalev@tensor.ru>
2020-04-27 07:42:53 +0700

    https://online.sbis.ru/opendoc.html?guid=ab91e227-db77-4cf6-8569-138369f7ca48

---
+++ Controls/_calendar/interfaces/MonthListYearTemplate.ts
@@ -0,0 +1,30 @@
+/**
+ * Шаблон, который по умолчанию используется для отображения года в MonthList.
+ * @class Controls/calendar:MonthListMonthTemplate
+ * @public
+ */
+
+/*
+ * @name Controls/calendar:MonthListYearTemplate#bodyTemplate
+ * @cfg {String|Function} Шаблон отображения года
+ * @remark
+ * В шаблон будет переданы:
+ * <ul>
+ *      <li>date - дата месяа</li>
+ *      <li>extData - данные, загруженные через источник данных</li>
+ * </ul>
+ * @example
+ * <pre>
+ * <Controls.calendar:MonthList>
+ *           <ws:yearTemplate>
+ *              <ws:partial template="Controls/calendar:MonthListYearTemplate">
+ *                  <ws:bodyTemplate>
+ *                      <ws:for data="month in 12">
+ *                          <Controls.calendar:MonthView/>
+ *                      </ws:for>
+ *                  </ws:bodyTemplate>
+ *              </ws:partial>
+ *          </ws:yearTemplate>
+ * </Controls.calendar:MonthList>
+ * </pre>
+ */
