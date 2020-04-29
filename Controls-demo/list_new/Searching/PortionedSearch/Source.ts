5ce483511ac242abbe4f086657635b37ce0b92a8
gersasha <gersasha@yandex.ru>
2020-04-29 23:06:59 +0300

    https://online.sbis.ru/opendoc.html?guid=f1762b65-462f-4f4b-b70e-39ca804668c2

--- Controls-demo/list_new/Searching/PortionedSearch/Source.ts
+++ Controls-demo/list_new/Searching/PortionedSearch/Source.ts
@@ -1,16 +1,17 @@
 import * as Deferred from 'Core/Deferred';
-import {Memory} from 'Types/source';
+import {Memory, Query, DataSet} from 'Types/source';
 
 const SEARCH_DELAY = 35000;
 
-export default class PortionedSearchSource {
+export default class PortionedSearchSource extends Memory {
     private source: Memory = null;
 
     constructor(opts) {
+        super();
         this.source = opts.source;
     }
 
-    query(query) {
+    query(query: Query<unknown>): Promise<DataSet> {
         const isSearch = query.getWhere().title !== undefined && (query.getOffset() === 10 || query.getOffset() === 30);
         const origQuery = this.source.query.apply(this.source, arguments);
         const loadDef = new Deferred();
@@ -26,6 +27,4 @@ export default class PortionedSearchSource {
         });
         return loadDef;
     }
-
-    static '[Types/_source/ICrud]': boolean = true;
 }
