2d212761f4380fce20e003c7c88553a5b1bd7901
geraltofrivia99 <geralt.from.rivi9@yandex.ru>
2020-04-29 11:41:19 +0700

    https://online.sbis.ru/opendoc.html?guid=28edae33-62ba-46ae-882c-2bc282b4ee75

--- Controls/_list/ListViewModel.ts
+++ Controls/_list/ListViewModel.ts
@@ -260,7 +260,7 @@ var ListViewModel = ItemsViewModel.extend([entityLib.VersionableMixin], {
 
     isShouldBeDrawnItem: function(item) {
         var isInRange = ListViewModel.superclass.isShouldBeDrawnItem.apply(this, arguments);
-        return isInRange || item.isGroup || item.isSticky;
+        return isInRange || (item.isGroup && item.isStickyHeader) || item.isSticky;
     },
 
     _calcCursorClasses: function(clickable, cursor) {
