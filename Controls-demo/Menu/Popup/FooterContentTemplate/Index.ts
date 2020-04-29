955b805dcd8ad4eba79da2d19f25a381ac7eb049
Elina Zolotova <elinazolotov@gmail.com>
2020-04-29 10:49:17 +0300

    https://online.sbis.ru/opendoc.html?guid=fb36bc2a-729c-405b-8aa5-a9540dcfc95f

--- Controls-demo/Menu/Popup/FooterContentTemplate/Index.ts
+++ Controls-demo/Menu/Popup/FooterContentTemplate/Index.ts
@@ -1,25 +1,8 @@
-import {Control, TemplateFunction} from 'UI/Base';
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import controlTemplate = require('wml!Controls-demo/Menu/Popup/FooterContentTemplate/Index');
-import {Memory} from 'Types/source';
 import 'css!Controls-demo/Controls-demo';
-import 'css!Controls-demo/Menu/Menu';
 
-class FooterContentTemplate extends Control {
+class FooterContentTemplateDemo extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
-    protected _source: Memory;
-
-    protected _beforeMount(): void {
-        this._source = new Memory({
-            keyProperty: 'key',
-            data: [
-                {key: 1, title: 'Ярославль'},
-                {key: 2, title: 'Москва'},
-                {key: 3, title: 'Санкт-Петербург'}
-            ]
-        });
-    }
-
-    static _theme: string[] = ['Controls/Classes'];
-
 }
-export default FooterContentTemplate;
+export default FooterContentTemplateDemo;
