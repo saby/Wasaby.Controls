2e40c8dffe60e9466e1fbe33edcb081889ad7f0e
ms.zhuravlev <ms.zhuravlev@tensor.ru>
2020-04-30 09:07:03 +0300

    https://online.sbis.ru/opendoc.html?guid=459317a7-39e5-4d9e-a13f-225dd0611863

--- Controls/Utils/RegExp.ts
+++ Controls/Utils/RegExp.ts
@@ -1,4 +1,5 @@
-const specialChars = /[$^*+?.(){}[\]\\|]/g;
+const WORD_BOUNDARY = '^|[^\\wа-я]|$';
+const SPECIAL_CHARS = /[$^*+?.(){}[\]\\|]/g;
 
 /**
  * $1 - Minus.
@@ -14,5 +15,45 @@ export const partOfNumber: RegExp =  /(-?)([0-9]*)([.,]?)([0-9]*)/;
  * @return Escaped value.
  */
 export function escapeSpecialChars(original: string): string {
-   return original.replace(specialChars, '\\$&');
+   return original.replace(SPECIAL_CHARS, '\\$&');
+}
+
+/**
+ * Добавить к строке регулярного выражения проверку, является ли строка словом.
+ * @remark
+ * Используется для работы с кириллицей и латиницей.
+ * Устраняет недостаток работы специального символа \b(@{link https://learn.javascript.ru/regexp-boundary граница слова}.
+ * Граница слова \b не работает для алфавитов, не основанных на латинице.
+ * @param original строка регулярного выражения.
+ * @param flags влаги регулярного выражения.
+ * @return Регулярное выражение с проверкой, является ли исходная строка словом.
+ */
+export function addWordCheck(original: string, flags: string): RegExp {
+   const result = `(${WORD_BOUNDARY})(?:${original})(${WORD_BOUNDARY})`;
+   const resultRegExp = new RegExp(result, flags);
+   const originalExec = resultRegExp.exec;
+   resultRegExp.exec = (str) => {
+      const originalExecResult = originalExec.call(resultRegExp, str);
+
+      if (originalExecResult === null) {
+         return null;
+      }
+
+      let found = Array.prototype.shift.call(originalExecResult);
+
+      // Удаляем границы из набора результатов
+      const startBorder = Array.prototype.shift.call(originalExecResult);
+      found = found.replace(new RegExp(`^${escapeSpecialChars(startBorder)}`), '');
+      const endBorder = Array.prototype.pop.call(originalExecResult);
+      found = found.replace(new RegExp(`${escapeSpecialChars(endBorder)}$`), '');
+
+      originalExecResult.input = found;
+      originalExecResult.index += startBorder.length;
+      originalExecResult.lastIndex -= endBorder.length;
+      Array.prototype.unshift.call(originalExecResult, found);
+
+      return originalExecResult;
+   };
+
+   return resultRegExp;
 }
