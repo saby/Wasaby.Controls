75748b87b97fb358254cb1abb2f32ff8ff9de50f
ea.melnikova <ea.melnikova@tensor.ru>
2020-05-19 15:04:03 +0300

    https://online.sbis.ru/opendoc.html?guid=cb93999d-c5d8-46cb-b5a7-4d733e06997e

--- Controls/_menu/interface/IMenuControl.ts
+++ Controls/_menu/interface/IMenuControl.ts
@@ -13,6 +13,7 @@ export interface IMenuControlOptions extends IMenuBaseOptions, ISourceOptions, I
     selectorOpener?: Stack;
     itemActions?: IItemAction[];
     dataLoadCallback: Function;
+    applyButtonAlign?: string;
 }
 
 /**
@@ -27,6 +28,13 @@ export interface IMenuControlOptions extends IMenuBaseOptions, ISourceOptions, I
  * @public
  * @author Золотова Э.Е.
  */
+
+/**
+ * @typedef {String} ApplyButtonAlign
+ * @variant top Кнопка расположена наверху.
+ * @variant bottom Кнопка расположена внизу.
+ */
+
 export default interface IMenuControl {
     readonly '[Controls/_menu/interface/IMenuControl]': boolean;
 }
@@ -48,6 +56,12 @@ export default interface IMenuControl {
  * @cfg {String} Первичный ключ для пункта выпадающего списка, который создаётся при установке опции emptyText.
  * @demo Controls-demo/Menu/Control/EmptyText/EmptyKey/Index
  */
+
+/**
+ * @name Controls/_menu/interface/IMenuControl#applyButtonAlign
+ * @cfg {ApplyButtonAlign} Устанавливает вертикальное выравнивание для кнопки подтверждения выбора.
+ */
+
 
 /**
  * @name Controls/_menu/interface/IMenuControl#multiSelect
