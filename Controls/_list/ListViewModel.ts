cb0d91babfad149604a8e9387d03004a26223c46
va-kolesov <va.kolesov@tensor.ru>
2020-05-20 11:02:34 +0300

    https://online.sbis.ru/opendoc.html?guid=ca441e1f-e8fb-492c-aa4b-a2d71b1666ac

--- Controls/_list/ListViewModel.ts
+++ Controls/_list/ListViewModel.ts
@@ -231,7 +231,7 @@ var ListViewModel = ItemsViewModel.extend([entityLib.VersionableMixin], {
 
     isShouldBeDrawnItem: function(item) {
         var isInRange = ListViewModel.superclass.isShouldBeDrawnItem.apply(this, arguments);
-        return isInRange || (item.isGroup && item.isStickyHeader) || item.isSticky;
+        return isInRange || (item?.isGroup && item?.isStickyHeader) || item?.isSticky;
     },
 
     _calcCursorClasses: function(clickable, cursor) {
