4c010baf60894689368d549b483f40c304960660
Sergey364 <sergey.mixailov@gmail.com>
2020-05-25 21:35:07 +0300

    20.4100/bugfix/mikhaylov/show-selector-caption-button

--- Controls/_lookup/Button.ts
+++ Controls/_lookup/Button.ts
@@ -8,8 +8,6 @@ import template = require('wml!Controls/_lookup/Button/SelectorButton');
  * Поддерживает одиночный и множественный выбор.
  * Подробное описание и инструкции по настройке контрола можно найти <a href='https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/directory/lookup/'>здесь</a>
  * Здесь вы можете увидеть <a href="/materials/Controls-demo/app/Engine-demo%2FSelectorButton">демонстрационный пример</a>.
- * @name Controls/_lookup/Button#showSelectorCaption
- * @cfg {String} Заголовок кнопки для выбора записей из справочника
  *
  * @class Controls/_lookup/Button
  * @extends Core/Control
@@ -45,6 +43,11 @@ import template = require('wml!Controls/_lookup/Button/SelectorButton');
  * @public
  * @author Герасимов А.М.
  * @demo Controls-demo/Buttons/SelectorButtonPG
+ */
+
+/*
+ * @name Controls/_lookup/Button#showSelectorCaption
+ * @cfg {String} Заголовок кнопки для выбора записей из справочника
  */
 
 var Button = Control.extend({
