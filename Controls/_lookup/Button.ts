7d6e07ea2b3c206ebf3b04f8f978ce5b19a2d06c
Sergey364 <sergey.mixailov@gmail.com>
2020-05-25 18:20:52 +0300

    https://online.sbis.ru/opendoc.html?guid=90b3a3e5-c9aa-403c-8a2d-4c889c4b41b4

--- Controls/_lookup/Button.ts
+++ Controls/_lookup/Button.ts
@@ -9,7 +9,7 @@ import template = require('wml!Controls/_lookup/Button/SelectorButton');
  * Подробное описание и инструкции по настройке контрола можно найти <a href='https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/directory/lookup/'>здесь</a>
  * Здесь вы можете увидеть <a href="/materials/Controls-demo/app/Engine-demo%2FSelectorButton">демонстрационный пример</a>.
  * @name Controls/_lookup/Button#showSelectorCaption
- * @cfg {string} Заголовок кнопки для выбора записей из справочника
+ * @cfg {String} Заголовок кнопки для выбора записей из справочника
  *
  * @class Controls/_lookup/Button
  * @extends Core/Control
