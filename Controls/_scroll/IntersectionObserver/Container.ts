a4f54c00ba1b74898bd3abd1984ecf9f23c9c008
au.mironov <au.mironov@tensor.ru>
2020-05-26 12:38:10 +0700

    IntersectionObserver: Добавлена возможность навешивать обработчики на контэйнеры. Добавлена опция rootMargin. https://online.sbis.ru/opendoc.html?guid=eae6ecca-d9c2-4324-9daa-41895e4457b8

--- Controls/_scroll/IntersectionObserver/Container.ts
+++ Controls/_scroll/IntersectionObserver/Container.ts
@@ -1,8 +1,9 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import SyntheticEntry from './SyntheticEntry';
+import {IIntersectionObserverOptions} from './Types';
 import template = require('wml!Controls/_scroll/IntersectionObserver/Container');
 
-export interface IIntersectionObserverContainerOptions extends IControlOptions {
-   observerName: string;
+export interface IIntersectionObserverContainerOptions extends IControlOptions, IIntersectionObserverOptions {
    data: object;
 }
 
@@ -18,13 +19,40 @@ export interface IIntersectionObserverContainerOptions extends IControlOptions {
 
 /**
  * @name Controls/_scroll/IntersectionObserver/Container#observerName
- * @cfg {String} Контроллер следит только за элементами с таким же именем.
+ * @cfg {String} Имя которое используется при регистрации в контроллере {@link Controls/scroll:IntersectionObserverController}.
+ * Контроллер следит только за элементами с таким же именем. Если не задано, то используется контроллер на уровне
+ * ближайшей скроллируемой области {@link Controls/scroll:Container}.
+ * @demo Controls-demo/Scroll/IntersectionObserver/Default/Index
+ */
+
+/**
+ * @name Controls/_scroll/IntersectionObserver/Controller#threshold
+ * @cfg {Array} Число или массив чисел, указывающий, при каком проценте видимости целевого элемента должен
+ * сработать callback. Например, в этом случае callback функция будет вызываться при появлении в зоне видимости
+ * каждые 25% целевого элемента:  [0, 0.25, 0.5, 0.75, 1]
+ * @demo Controls-demo/Scroll/IntersectionObserver/Threshold/Index
+ */
+
+/**
+ * @name Controls/_scroll/IntersectionObserver/Controller#rootMargin
+ * @cfg {String} Смещение прямоугольника, применяемое к bounding box корня при расчёте пересечений/
+ * Эффективно сжимает или увеличивает корень для целей расчёта. Может быть выражено в пикселях (px) или в процентах (%).
+ * Например "50% 0px 0px 0px"
+ * @default "0px 0px 0px 0px".
+ * @demo Controls-demo/Scroll/IntersectionObserver/RootMargin/Index
  */
 
 /**
  * @name Controls/_scroll/IntersectionObserver/Container#data
  * @cfg {Object} Данные которые приходят в событие {@link Controls/_scroll/IntersectionObserver/Controller#intersect}
  * в качестве параметра.
+ * @demo Controls-demo/Scroll/IntersectionObserver/Default/Index
+ */
+
+/**
+ * @event Controls/_scroll/IntersectionObserver/Controller#intersect Происходит когда цель достигает порогового значения,
+ * указанного в опции threshold
+ * @demo Controls-demo/Scroll/IntersectionObserver/Default/Index
  */
 
 class  ModuleComponent extends Control<IIntersectionObserverContainerOptions> {
@@ -34,13 +62,25 @@ class  ModuleComponent extends Control<IIntersectionObserverContainerOptions> {
       //TODO remove after complete https://online.sbis.ru/opendoc.html?guid=7c921a5b-8882-4fd5-9b06-77950cbe2f79
       const container = this._container.get ? this._container.get(0) : this._container;
       this._notify(
-          'intersectionObserverRegister',
-          [this.getInstanceId(), this._options.observerName, container, this._options.data],
-          { bubbling: true });
+         'intersectionObserverRegister',
+         [{
+            instId: this.getInstanceId(),
+            observerName: this._options.observerName,
+            element: container,
+            threshold: this._options.threshold,
+            rootMargin: this._options.rootMargin,
+            data: this._options.data,
+            handler: this._observeHandler.bind(this)
+         }],
+         { bubbling: true });
+   }
+
+   private _observeHandler(item: SyntheticEntry): void {
+      this._notify('intersect', [item]);
    }
 
    protected _beforeUnmount(): void {
-      this._notify('intersectionObserverUnregister', [this.getInstanceId()], { bubbling: true });
+      this._notify('intersectionObserverUnregister', [this.getInstanceId(), this._options.observerName], { bubbling: true });
    }
 }
 
--- tests/ControlsUnit/scroll/IntersectionObserver/Controller.test.js
+++ tests/ControlsUnit/scroll/IntersectionObserver/Controller.test.js
@@ -14,7 +14,9 @@ define([
    };
 
    describe('Controls/scroll:IntersectionObserverController', function() {
-
+      const event = {
+         stopImmediatePropagation: function() {}
+      }
       describe('_registerHandler', function() {
          it('should create _observer, and observe container', function() {
             const
@@ -27,9 +29,10 @@ define([
             sandbox.stub(component, '_createObserver').returns({
                observe: sinon.fake()
             });
-            component._registerHandler(null, instId, options.observerName, element, data);
-            assert.deepEqual(component._items, { id: { instId: instId, element: element, data: data } });
-            sinon.assert.calledWith(component._observer.observe, element);
+            component._registerHandler(event,
+                {instId: instId, observerName: options.observerName, element: element, data: data});
+            assert.hasAllDeepKeys(component._items, { id: { instId: instId, element: element, data: data } });
+            sinon.assert.called(component._createObserver);
             sandbox.restore();
          });
       });
@@ -38,19 +41,27 @@ define([
             const
                component = calendarTestUtils.createComponent(scroll.IntersectionObserverController, options),
                instId = 'instId',
-               element = 'element';
-            component._observer = {
-               unobserve: sinon.fake()
+               element = 'element',
+               observer = {
+                  unobserve: sinon.fake()
+               };
+            component._observers = {
+               "t1_rm0px_0px_0px_0px": {
+                  count: 1,
+                  observer
+               }
             };
             component._items = {
                instId: {
                   instId: instId,
-                  element: element
+                  element: element,
+                  threshold: [1],
+                  rootMargin: '0px 0px 0px 0px'
                }
             };
-            component._unregisterHandler(null, instId);
+            component._unregisterHandler(event, instId, options.observerName);
             assert.isUndefined(component._items[instId]);
-            sinon.assert.calledWith(component._observer.unobserve, element);
+            sinon.assert.calledWith(observer.unobserve, element);
          });
       });
 
@@ -62,18 +73,21 @@ define([
                instId = 'id',
                element = 'element',
                data = { someField: 'fieldValue' },
-               entry = { target: element, entryData: 'data' };
+               entry = { target: element, entryData: 'data' },
+               handler = sinon.fake();
 
             sandbox.stub(component, '_createObserver').returns({
                observe: sinon.fake()
             });
             sandbox.stub(component, '_notify');
-            component._registerHandler(null, instId, options.observerName, element, data);
+            component._registerHandler(event,
+                {instId: instId, observerName: options.observerName, element: element, data: data, handler: handler});
             component._intersectionObserverHandler([entry]);
             sinon.assert.calledWith(
                component._notify,
                'intersect'
             );
+            sinon.assert.called(handler);
             sandbox.restore();
          });
 
