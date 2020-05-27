6b8fd748d41f2dae62bd36758de61ca01b8a0e65
Elefante <effective.gamer@gmail.com>
2020-05-28 01:27:20 +0700

    finished

--- Controls-demo/Scroll/ContainerBase/Index.ts
+++ Controls-demo/Scroll/ContainerBase/Index.ts
@@ -6,8 +6,39 @@ import 'css!Controls-demo/Scroll/ContainerBase/Style';
 export default class ContainerBaseDemo extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
 
-    scrollStateChangedHandler(e): void {
-        console.log('scrollStateChangedHandler');
+    private _contentAdded: boolean = false;
+    private _heightAdded: boolean = false;
+    private _scrollTopChanged: boolean = false;
+    private _eventStore: string[] = [];
+    private _eventCounter: number = 0;
+    private _state: object = null;
+
+    scrollStateChangedHandler(event, state): void {
+        this._eventStore.push('scrollStateChanged');
+        this._eventCounter++;
+        this._state = state.state;
+    }
+
+    _addContentHandler(): void {
+        this._contentAdded = !this._contentAdded;
+    }
+
+    _changeHeightContentHandler(): void {
+        this._heightAdded = !this._heightAdded;
+    }
+
+    _changeHeightAddContentHandler(): void {
+        this._heightAdded = !this._heightAdded;
+        this._contentAdded = !this._contentAdded;
+    }
+
+    _changeScrollTopHandler(): void {
+        if (!this._scrollTopChanged) {
+            this._children.containerBase._container.scrollTop = 30;
+        } else {
+            this._children.containerBase._container.scrollTop = 0;
+        }
+        this._scrollTopChanged = !this._scrollTopChanged;
     }
 
     static _theme: string[] = ['Controls/Classes'];
