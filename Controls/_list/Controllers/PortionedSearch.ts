e664760985a7aad562dfaccbc52aed636f950e3d
Gersasha <gersashaa@gmail.com>
2020-05-27 13:42:59 +0300

    https://online.sbis.ru/opendoc.html?guid=004dafde-1bc8-4e83-b56a-501be0adcab7

--- Controls/_list/Controllers/PortionedSearch.ts
+++ Controls/_list/Controllers/PortionedSearch.ts
@@ -59,10 +59,14 @@ export default class PortionedSearch<PortionedSearchOptions> {
         this._options.searchContinueCallback();
     }
 
+    stopSearch(): void {
+        this._clearTimer();
+        this._stopSearch();
+    }
+
     private _startTimer(): void {
         this._searchTimer = setTimeout(() => {
-            this._setSearchState(SEARCH_STATES.STOPPED);
-            this._options.searchStopCallback();
+            this._stopSearch();
         }, MAX_SEARCH_DURATION);
     }
 
@@ -83,5 +87,10 @@ export default class PortionedSearch<PortionedSearchOptions> {
 
     private _isSearchContinued(): boolean {
         return this._getSearchState() === SEARCH_STATES.CONTINUED;
+    }
+
+    private _stopSearch(): void {
+        this._setSearchState(SEARCH_STATES.STOPPED);
+        this._options.searchStopCallback();
     }
 }
