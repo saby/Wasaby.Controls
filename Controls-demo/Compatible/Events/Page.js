bf11006216ec548e488b7dcd22fbe2e9155a4f60
an.borisov <an.borisov@tensor.ru>
2020-05-28 14:08:49 +0500

    https://online.sbis.ru/opendoc.html?guid=5908096d-067b-4701-a211-e146aa00f6b1

--- Controls-demo/Compatible/Events/Page.js
+++ Controls-demo/Compatible/Events/Page.js
@@ -1,10 +1,9 @@
 define('Controls-demo/Compatible/Events/Page', [
     'Core/Control',
     'wml!Controls-demo/Compatible/Events/Page',
-    'Lib/Control/LayerCompatible/LayerCompatible',
     'Env/Env',
     'Controls-demo/Compatible/Events/CompatibleParent'
-], function (Control, template, layer, env) {
+], function (Control, template, env) {
     'use strict';
 
     var ModuleClass = Control.extend(
