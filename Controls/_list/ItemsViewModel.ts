0f6487b01e2e61549934664ed2ecba31e18e1560
as.avramenko <as.avramenko@yandex.ru>
2020-05-28 18:48:31 +0300

    https://online.sbis.ru/opendoc.html?guid=28edae33-62ba-46ae-882c-2bc282b4ee75

--- Controls/_list/ItemsViewModel.ts
+++ Controls/_list/ItemsViewModel.ts
@@ -146,9 +146,13 @@ var ItemsViewModel = BaseViewModel.extend({
         return Boolean(this._options.virtualScrollConfig) && (!this._options.task1179200403 || this._options.supportVirtualScroll);
     },
 
-    reset: function() {
+    _getCurIndexForReset(startIndex: number): number {
+        return startIndex;
+    },
+
+    reset(): void {
         this._startIndex = this._isSupportVirtualScroll() && !!this._startIndex ? this._startIndex : 0;
-        this._curIndex = 0;
+        this._curIndex = this._getCurIndexForReset(this._startIndex);
     },
 
     isEnd: function() {
