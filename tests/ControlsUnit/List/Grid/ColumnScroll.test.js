84603d92bf116440c24624105d19a69f1360b40b
ea.rodionov1 <ea.rodionov1@tensor.ru>
2020-05-28 16:14:28 +0300

    https://online.sbis.ru/opendoc.html?guid=e1f51e8d-94ff-47f5-bd7f-36ec5b10891d

--- tests/ControlsUnit/List/Grid/ColumnScroll.test.js
+++ tests/ControlsUnit/List/Grid/ColumnScroll.test.js
@@ -28,9 +28,9 @@ define(['Controls/_grid/ColumnScroll', 'Types/entity', 'Core/core-clone'], funct
          columnScroll = new ColumnScroll(cfg);
 
       const _innerHTMLTemplateStr = '.controls-ColumnScroll__transform-1234567890 .controls-Grid_columnScroll { transform: translateX(-{value}); }' +
-         '.controls-ColumnScroll__transform-1234567890 .controls-Grid__cell_fixed { transform: translateX({value}); }' +
-         '.controls-ColumnScroll__transform-1234567890 .js-controls-Grid_columnScroll_thumb-wrapper { transform: translateX({value}); }' +
+         '.controls-ColumnScroll__transform-1234567890 .controls-Grid__cell_fixed { transform: translateX({value}); }'  +
          '.controls-ColumnScroll__transform-1234567890 .controls-GridView__footer { transform: translateX({value}); }' +
+         '.controls-ColumnScroll__transform-1234567890 .js-controls-Grid_columnScroll_thumb-wrapper { transform: translateX({value}); }' +
          '.controls-ColumnScroll__transform-1234567890 .controls-Grid__itemAction { transform: translateX({value}); }';
 
       const _innerHTMLReplaceValueRegExp = new RegExp('{value}', 'g');
