84d92c8392accd7e49b59872084f707a362322fe
erik <errik25@gmail.com>
2020-05-28 18:08:35 +0500

    демка для slider https://online.sbis.ru/opendoc.html?guid=e8a5b512-bac2-4888-b49c-c3903d1ec5d3

---
+++ Controls-demo/Slider/Base/Base/Index.ts
@@ -0,0 +1,16 @@
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+import Template = require('wml!Controls-demo/Slider/Base/Base/Template');
+
+class Base extends Control<IControlOptions> {
+    protected _template: TemplateFunction = Template;
+    protected _value: number;
+
+    protected _beforeMount(): void {
+        this._value = 30;
+    }
+
+    static _theme: string[] = ['Controls/Classes'];
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
+
+export default Base;
