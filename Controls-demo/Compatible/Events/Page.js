caf4036bb1a48395836ad5936ce674159ffc2ead
an.borisov <an.borisov@tensor.ru>
2020-05-28 12:40:39 +0500

    https://online.sbis.ru/opendoc.html?guid=5908096d-067b-4701-a211-e146aa00f6b1

--- Controls-demo/Compatible/Events/Page.js
+++ Controls-demo/Compatible/Events/Page.js
@@ -1,15 +1,19 @@
 define('Controls-demo/Compatible/Events/Page', [
     'Core/Control',
     'wml!Controls-demo/Compatible/Events/Page',
+    'Lib/Control/LayerCompatible/LayerCompatible',
+    'Env/Env',
     'Controls-demo/Compatible/Events/CompatibleParent'
- ], function(Control, template) {
+], function (Control, template, layer, env) {
     'use strict';
- 
+
     var ModuleClass = Control.extend(
-       {
-          _template: template
-       }
+        {
+            _template: template,
+            _beforeMount: function() {
+                env.constants.compat = true;
+            }
+        }
     );
     return ModuleClass;
- });
- 
+});
