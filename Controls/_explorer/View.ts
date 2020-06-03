0cf186c169bea1875dfaec3a3898a00de754eeed
ea.rodionov1 <ea.rodionov1@tensor.ru>
2020-06-03 22:54:12 +0300

    https://online.sbis.ru/opendoc.html?guid=4cb4665b-2e51-4e1e-9ce6-cb638b89c7b9

--- Controls/_explorer/View.ts
+++ Controls/_explorer/View.ts
@@ -9,8 +9,14 @@ import {SearchGridViewModel, SearchView, TreeGridView, ViewModel as TreeGridView
 import {factory} from 'Types/chain';
 import {constants} from 'Env/Env';
 import {Logger} from 'UI/Utils';
-import 'Types/entity';
+import {Model} from 'Types/entity';
 import {ListView} from 'Controls/list';
+import {isEqual} from 'Types/object';
+import {
+   INavigationSourceConfig,
+   INavigationPositionSourceConfig as IPositionSourceConfig,
+   INavigationOptionValue as INavigation
+}  from '../_interface/INavigation';
 
 var
       HOT_KEYS = {
@@ -49,12 +55,17 @@ var
          },
          setRestoredKeyObject: function(self, root) {
             const curRoot = _private.getRoot(self, self._options.root);
-            self._restoredMarkedKeys[root] = {
+            const rootId = root.getId();
+            self._restoredMarkedKeys[rootId] = {
                parent: curRoot,
                markedKey: null
             };
             if (self._restoredMarkedKeys[curRoot]) {
-               self._restoredMarkedKeys[curRoot].markedKey = root;
+               self._restoredMarkedKeys[curRoot].markedKey = rootId;
+
+               if (_private.isCursorNavigation(self._options.navigation)) {
+                  self._restoredMarkedKeys[curRoot].cursorPosition = _private.getCursorPositionFor(root, self._options.navigation);
+               }
             }
          },
           cleanRestoredKeyObject: function(self, root) {
@@ -272,6 +283,33 @@ var
             if (cfg.searchNavigationMode !== 'expand') {
                self._children.treeControl.resetExpandedItems();
             }
+         },
+
+         isCursorNavigation(navigation: INavigation<INavigationSourceConfig>): boolean {
+            return !!navigation && navigation.source === 'position';
+         },
+
+         /**
+          * Собирает курсор для навигации относительно заданной записи.
+          * @param item - запись, для которой нужно "собрать" курсор
+          * @param positionNavigation - конфигурация курсорной навигации
+          */
+         getCursorPositionFor(item: Model, positionNavigation: INavigation<IPositionSourceConfig>): IPositionSourceConfig['position'] {
+            const position: unknown[] = [];
+            const optField = positionNavigation.sourceConfig.field;
+            const fields: string[] = (optField instanceof Array) ? optField : [optField];
+
+            fields.forEach((field) => {
+               position.push(item.get(field));
+            });
+
+            return position;
+         },
+
+         restorePositionNavigation(self, itemId): void {
+            if (self._restoredMarkedKeys[itemId]) {
+               this._navigation.sourceConfig.position = self._restoredMarkedKeys[itemId].cursorPosition;
+            }
          }
       };
 
@@ -282,12 +320,12 @@ var
     *
     * @remark
     * Сортировка применяется к запросу к источнику данных. Полученные от источника записи дополнительно не сортируются.
-    * 
+    *
     * Полезные ссылки:
     * * <a href="/doc/platform/developmentapl/interface-development/controls/list/explorer/">руководство разработчика</a>
     * * <a href="https://github.com/saby/wasaby-controls/blob/rc-20.4000/Controls-default-theme/aliases/_explorer.less">переменные тем оформления explorer</a>
     * * <a href="https://github.com/saby/wasaby-controls/blob/rc-20.4000/Controls-default-theme/aliases/_list.less">переменные тем оформления list</a>
-    * 
+    *
     * Демо-примеры:
     * <ul>
     *    <li><a href="/materials/Controls-demo/app/Controls-demo%2FExplorer%2FExplorer">Иерархический проводник в режимах "список" и "плитка"</a></li>
@@ -395,6 +433,7 @@ var
       _itemsPromise: null,
       _itemsResolver: null,
       _markerForRestoredScroll: null,
+      _navigation: null,
 
       _resolveItemsPromise() {
          this._itemsResolver();
@@ -421,12 +460,30 @@ var
          };
 
          this._dragControlId = randomId();
+         this._navigation = cfg.navigation;
          return _private.setViewMode(this, cfg.viewMode, cfg);
       },
       _beforeUpdate: function(cfg) {
          //todo: после доработки стандарта, убрать флаг _isGoingFront по задаче: https://online.sbis.ru/opendoc.html?guid=ffa683fa-0b8e-4faa-b3e2-a4bb39671029
          if (this._isGoingFront && this._options.hasOwnProperty('root') && cfg.root === this._options.root) {
             this._isGoingFront = false;
+         }
+
+         /*
+         * Позиция скрола при выходе из папки восстанавливается через скроллирование к отмеченной записи.
+         * Чтобы список мог восстановить позицию скрола по отмеченой записи, она должна быть в наборе данных.
+         * Чтобы обеспечить ее присутствие, нужно загружать именно ту страницу, на которой она есть.
+         * Восстановление работает только при курсорной навигации.
+         * Если в момент возвращения из папки был изменен тип навигации, не нужно восстанавливать, иначе будут смешаны опции
+         * курсорной и постраничной навигаций.
+         * */
+         const isNavigationHasBeenChanged = !isEqual(this._options.navigation, cfg.navigation);
+
+         if (this._isGoingBack && _private.isCursorNavigation(this._options.navigation) && !isNavigationHasBeenChanged) {
+            const newRootId = _private.getRoot(this, this._options.root);
+            _private.restorePositionNavigation(this, newRootId);
+         } else if (isNavigationHasBeenChanged) {
+            this._navigation = cfg.navigation;
          }
 
          if (
@@ -494,7 +551,7 @@ var
 
          const changeRoot = () => {
              // При проваливании ОБЯЗАТЕЛЬНО дополняем restoredKeyObject узлом, в который проваливаемся
-            _private.setRestoredKeyObject(this, item.getId());
+            _private.setRestoredKeyObject(this, item);
             _private.setRoot(this, item.getId());
             this._isGoingFront = true;
          };
