b4513604b6a5a2b771116201716778b9d50933c4
pa.averkiev <pa.averkiev@tensor.ru>
2020-06-03 09:12:13 +0300

    https://online.sbis.ru/opendoc.html?guid=de92cdd6-85ff-409d-ae07-3d76151f9e77 Убрал лишнее кеширование модели в контроллере ItemActions

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -2322,19 +2322,6 @@ var BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototype
             _private.updateMarkerController(this, newOptions);
         }
 
-        // UC1: Record might be editing on page load, then we should initialize Item Actions.
-        // UC2: We should reassign ItemActions on model change before drawing them in For template
-        if (
-           recreateSource ||
-           newOptions.itemActions !== this._options.itemActions ||
-           newOptions.itemActionVisibilityCallback !== this._options.itemActionVisibilityCallback ||
-           ((newOptions.itemActions || newOptions.itemActionsProperty) && this._modelRecreated) ||
-           newOptions.itemActionsProperty ||
-           (newOptions.editingConfig && newOptions.editingConfig.item)
-        ) {
-            this._updateItemActions(newOptions);
-        }
-
         if (filterChanged || recreateSource || sortingChanged) {
             _private.resetPagingNavigation(this, newOptions.navigation);
             if (this._itemActionsMenuId) {
@@ -2345,7 +2332,18 @@ var BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototype
             // return result here is for unit tests
             return _private.reload(self, newOptions).addCallback(() => {
                 this._needBottomPadding = _private.needBottomPadding(newOptions, this._items, this._listViewModel);
+                this._updateItemActions(newOptions);
             });
+        }
+
+        // UC1: Редактирование записи при загрузке (Может быть изменится после версии 20.5000, т.к. там появились опции, отображаемые всегда)
+        // UC2: Нужно переопределять ItemActions прежде чем они будут нарисованы в шаблоне For
+        if (
+            newOptions.itemActions !== this._options.itemActions ||
+            newOptions.itemActionVisibilityCallback !== this._options.itemActionVisibilityCallback ||
+            (newOptions.editingConfig && newOptions.editingConfig.item)
+        ) {
+            this._updateItemActions(newOptions);
         }
 
         if (this._itemsChanged) {
