098f37327b1ff4ee0d201eb14e5aab696a59aeb4
pa.averkiev <pa.averkiev@tensor.ru>
2020-06-05 12:45:09 +0300

    https://online.sbis.ru/opendoc.html?guid=4a5b4613-2afc-4232-a0d8-5d742f28fd1e вернул обновление опций при изменении опции readOnly. Добавил тест

--- tests/ControlsUnit/List/BaseControl.test.js
+++ tests/ControlsUnit/List/BaseControl.test.js
@@ -4567,6 +4567,17 @@ define([
             assert.exists(actionsOf0, 'actions for item at 0 pos. were not assigned');
             assert.equal(actionsOf0.all[0].title, '456', 'new actions for item at 0 pos. were not assigned');
          });
+
+         // при смене значения свойства readOnly необходимо делать переинициализвацию ItemActions
+         it('should update ItemActions when readOnly option has been changed', () => {
+            instance._beforeUpdate({
+               ...cfg,
+               source: instance._options.source,
+               readOnly: true,
+            });
+            const actionsOf0 = instance._listViewModel.at(0).getActions();
+            assert.exists(actionsOf0, 'actions for item at 0 pos. were not assigned');
+         });
       });
 
       it('_beforeMount with PrefetchProxy in source', function() {
@@ -5254,10 +5265,10 @@ define([
                   target: {},
                   nativeEvent: {}
                };
-               const event = { 
-                  stopPropagation: () => {} 
+               const event = {
+                  stopPropagation: () => {}
                };
-               const dragEvent = { 
+               const dragEvent = {
                   stopPropagation: () => {}
                };
                const dragObject = {
