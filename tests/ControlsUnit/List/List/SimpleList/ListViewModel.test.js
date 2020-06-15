e2b5252b384ec28b2507556c433e74a57bd2ffe8
ka.panihin <ka.panihin@tensor.ru>
2020-06-15 13:15:15 +0300

    https://online.sbis.ru/opendoc.html?guid=d383e047-fdf5-408e-8c06-750c8dd96ae3

--- tests/ControlsUnit/List/List/SimpleList/ListViewModel.test.js
+++ tests/ControlsUnit/List/List/SimpleList/ListViewModel.test.js
@@ -375,7 +375,7 @@ define([
          assert.equal(model.getMarkedKey(), undefined);
          assert.isFalse(model.getItemBySourceKey(2).isMarked());
          assert.isFalse(notifySpy.withArgs('onMarkedKeyChanged', 2).called);
-         assert.isFalse(nextModelVersionSpy.withArgs(true, 'markedKeyChanged').called);
+         assert.isTrue(nextModelVersionSpy.withArgs(true, 'markedKeyChanged').called);
       });
 
       // TODO SetItemActions
