ab14b668410422edd01a1a5d16857685e139cb6e
av.bondar <av.bondar@tensor.ru>
2020-06-16 17:44:07 +0300

    https://online.sbis.ru/opendoc.html?guid=fe824990-6e79-41bd-8727-03eb0973658f

--- Controls/_breadcrumbs/HeadingPath.ts
+++ Controls/_breadcrumbs/HeadingPath.ts
@@ -31,13 +31,13 @@ var _private = {
 
 /**
  * Хлебные крошки с кнопкой "Назад".
- * 
+ *
  * @remark
  * Полезные ссылки:
  * * <a href="/materials/Controls-demo/app/Controls-demo%2FBreadCrumbs%2FScenarios">демо-пример</a>
  * * <a href="https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/content-managment/bread-crumbs/">руководство разработчика</a>
  * * <a href="https://github.com/saby/wasaby-controls/blob/rc-20.4000/Controls-default-theme/aliases/_breadcrumbs.less">переменные тем оформления</a>
- * 
+ *
  * @class Controls/_breadcrumbs/HeadingPath
  * @extends Core/Control
  * @mixes Controls/interface/IBreadCrumbs
@@ -175,7 +175,8 @@ BreadCrumbsPath.getDefaultOptions = function () {
         displayProperty: 'title',
         root: null,
         backButtonStyle: 'secondary',
-        showActionButton: true
+        showActionButton: true,
+        isDouble: false
     };
 };
 
