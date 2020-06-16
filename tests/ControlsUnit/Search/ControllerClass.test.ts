7ec76c85ad56701600c58f13c99ab0292cc4614a
Gersasha <gersashaa@gmail.com>
2020-06-16 12:40:14 +0300

    https://online.sbis.ru/opendoc.html?guid=5c572c57-a033-41bb-8d50-c06ef49237ac

--- tests/ControlsUnit/Search/ControllerClass.test.ts
+++ tests/ControlsUnit/Search/ControllerClass.test.ts
@@ -88,4 +88,18 @@ describe('Controls/search:ControllerClass', () => {
         assert.isFalse(!!searchController._isSearchValueEmpty('', 'test'));
         assert.isFalse(!!searchController._isSearchValueEmpty('test', ''));
     });
+
+    describe('constructor', () => {
+
+        it('searchValueChangedCallback is not called on constructor', () => {
+            let searchValueChangedCallbackCalled = false;
+            const options = getDefaultOptions();
+            options.searchValueChangedCallback = () => {
+                searchValueChangedCallbackCalled = true;
+            }
+            options.searchValue = 'test';
+            new ControllerClass(options, {});
+            assert.isFalse(searchValueChangedCallbackCalled);
+        });
+    });
 })
