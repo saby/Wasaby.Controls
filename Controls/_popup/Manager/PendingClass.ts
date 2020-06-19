f9adc31f986425c4c7d995425913f910a15563e7
gd.kovalev <gd.kovalev@tensor.ru>
2020-06-19 12:58:23 +0700

    https://online.sbis.ru/opendoc.html?guid=9a77643a-1710-4435-85b8-b1541cdec152

--- Controls/_popup/Manager/PendingClass.ts
+++ Controls/_popup/Manager/PendingClass.ts
@@ -91,7 +91,7 @@ class PendingClass {
         }
     }
 
-    finishPendingOperations(forceFinishValue: boolean, isInside?: boolean, root: string = null): Deferred {
+    finishPendingOperations(forceFinishValue: boolean, root: string = null): Deferred {
         const resultDeferred = new Deferred();
         const parallelDef = new ParallelDeferred();
         const pendingResults = [];
@@ -101,7 +101,7 @@ class PendingClass {
             const pending = pendingRoot[key];
             let isValid = true;
             if (pending.validate) {
-                isValid = pending.validate(isInside);
+                isValid = pending.validate();
             } else if (pending.validateCompatible) { //todo compatible
                 isValid = pending.validateCompatible();
             }
