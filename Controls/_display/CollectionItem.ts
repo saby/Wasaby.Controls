fcf889a016f54cefbdabf5c231deec0940ea8fae
pa.averkiev <pa.averkiev@tensor.ru>
2020-06-19 18:41:10 +0300

    https://online.sbis.ru/opendoc.html?guid=0911460b-fa34-4d11-b9ca-dcd4f4a9788f Попрвил темизацию и паддинги

--- Controls/_display/CollectionItem.ts
+++ Controls/_display/CollectionItem.ts
@@ -442,8 +442,8 @@ export default class CollectionItem<T> extends mixin<
     }
 
     /**
-     * Для новой модели не вызывается, потому что в интеграционных тестах возникает ошибка
      * Возвращает Класс для позиционирования опций записи.
+     * Если новая модель, то в любом случае не считается класс, добавляющий padding
      * Если опции вне строки, то возвращает класс, добавляющий padding согласно itemActionsClass и itemPadding
      * Если опции вне строки и itemActionsClass не задан, возвращает пробел
      * Если опции внутри строки и itemActionsClass не задан, возвращает класс, добавляющий выравнивание bottomRight, без padding
@@ -456,21 +456,20 @@ export default class CollectionItem<T> extends mixin<
      * @param useNewModel
      */
     getItemActionPositionClasses(itemActionsPosition: string, itemActionsClass: string, itemPadding: {top?: string, bottom?: string}, theme: string, useNewModel?: boolean): string {
-        if (useNewModel) {
-            return ' ';
-        }
         const classes = itemActionsClass || ITEMACTIONS_POSITION_CLASSES.bottomRight;
         const result: string[] = [];
         if (itemActionsPosition !== 'outside') {
             result.push(classes);
         }
-        const themedPositionClassCompile = (position) => (
-            `controls-itemActionsV_padding-${position}_${(itemPadding && itemPadding[position] === 'null' ? 'null' : 'default')}_theme-${theme}`
-        );
-        if (classes.indexOf(ITEMACTIONS_POSITION_CLASSES.topRight) !== -1) {
-            result.push(themedPositionClassCompile('top'));
-        } else if (classes.indexOf(ITEMACTIONS_POSITION_CLASSES.bottomRight) !== -1) {
-            result.push(themedPositionClassCompile('bottom'));
+        if (!useNewModel) {
+            const themedPositionClassCompile = (position) => (
+                `controls-itemActionsV_padding-${position}_${(itemPadding && itemPadding[position] === 'null' ? 'null' : 'default')}_theme-${theme}`
+            );
+            if (classes.indexOf(ITEMACTIONS_POSITION_CLASSES.topRight) !== -1) {
+                result.push(themedPositionClassCompile('top'));
+            } else if (classes.indexOf(ITEMACTIONS_POSITION_CLASSES.bottomRight) !== -1) {
+                result.push(themedPositionClassCompile('bottom'));
+            }
         }
         return result.length ? ` ${result.join(' ')} ` : ' ';
     }
--- tests/ControlsUnit/display/CollectionItem.test.ts
+++ tests/ControlsUnit/display/CollectionItem.test.ts
@@ -675,22 +675,30 @@ describe('Controls/_display/CollectionItem', () => {
             item = new CollectionItem();
         });
 
-        // CSS класс для позиционирования опций записи. Для новой модели всегда возвращает пробел
-        it('getItemActionPositionClasses() should return space when itemActionsPosition === "outside" and itemActionsClass is not set', () => {
-            const result = item.getItemActionPositionClasses('inside', null, {top: 's', bottom: 's'}, 'default', true);
-            assert.equal(result, ' ');
-        });
+        // CSS класс для позиционирования опций записи.
 
-        // CSS класс для позиционирования опций записи. Если опции внутри строки и itemActionsClass не задан, возвращает класс, добавляющий выравнивание bottomRight, без padding
-        it('getItemActionPositionClasses() should return classes for bottom-right positioning when no itemActionClass is set', () => {
+        // Если опции внутри строки и itemActionsClass не задан, возвращает класс, добавляющий выравнивание bottomRight
+        it('getItemActionPositionClasses() should return classes for bottom-right positioning when itemActionClass is not set', () => {
             const result = item.getItemActionPositionClasses('inside', null, {top: 'null', bottom: 's'}, 'default');
             assert.equal(result, ' controls-itemActionsV_position_bottomRight controls-itemActionsV_padding-bottom_default_theme-default ');
         });
 
-        // CSS класс для позиционирования опций записи. Всегда происходит попытка рассчитать класс, добавляющий Padding, независимо от itemActionsPosition
-        it('getItemActionPositionClasses() should try to add padding', () => {
+        // Если опции внутри строки и itemActionsClass задан, возвращает класс, добавляющий выравнивание согласно itemActionsClass и itemPadding
+        it('getItemActionPositionClasses() should return classes for bottom-right positioning when itemActionClass is set', () => {
             const result = item.getItemActionPositionClasses('inside', 'controls-itemActionsV_position_topRight', {top: 'null', bottom: 's'}, 'default');
             assert.equal(result, ' controls-itemActionsV_position_topRight controls-itemActionsV_padding-top_null_theme-default ');
+        });
+
+        // Всегда, кроме новой модели происходит попытка рассчитать класс, добавляющий padding
+        it('getItemActionPositionClasses() should try to add padding class for any case except of useNewModel', () => {
+            const result = item.getItemActionPositionClasses('inside', 'controls-itemActionsV_position_topRight', {top: 's', bottom: 's'}, 'default', false);
+            assert.equal(result, ' controls-itemActionsV_position_topRight controls-itemActionsV_padding-top_default_theme-default ');
+        });
+
+        // Если новая модель, то в любом случае не считается класс, добавляющий padding
+        it('getItemActionPositionClasses() should not add padding class in case of useNewModel', () => {
+            const result = item.getItemActionPositionClasses('inside', null, {top: 's', bottom: 's'}, 'default', true);
+            assert.equal(result, ' controls-itemActionsV_position_bottomRight ');
         });
     })
 });
