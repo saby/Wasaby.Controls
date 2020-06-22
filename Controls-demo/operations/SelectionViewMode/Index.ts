1f365a432cdb0267fda20abc46b5ad3d899649fe
ea.melnikova <ea.melnikova@tensor.ru>
2020-06-22 10:59:27 +0300

    https://online.sbis.ru/opendoc.html?guid=b004a3ab-73c1-4b2f-a01d-9544303afb01

--- Controls-demo/operations/SelectionViewMode/Index.ts
+++ Controls-demo/operations/SelectionViewMode/Index.ts
@@ -2,7 +2,7 @@ import {Control} from 'UI/Base';
 import * as template from 'wml!Controls-demo/operations/SelectionViewMode/SelectionViewMode';
 import Memory from 'Controls-demo/operations/SelectionViewMode/Memory';
 import {getListData} from 'Controls-demo/OperationsPanelNew/DemoHelpers/DataCatalog';
-import 'wml!Controls-demo/OperationsPanel/Demo/PersonInfo';
+import 'wml!Controls-demo/operations/SelectionViewMode/resources/PersonInfo';
 
 export default class extends Control {
    _template = template;
@@ -12,7 +12,7 @@ export default class extends Control {
 
    _beforeMount() {
       this._gridColumns = [{
-         template: 'wml!Controls-demo/OperationsPanel/Demo/PersonInfo'
+         template: 'Controls-demo/operations/SelectionViewMode/resources/PersonInfo'
       }];
       this._viewSource = new Memory({
          keyProperty: 'id',
@@ -20,5 +20,5 @@ export default class extends Control {
       });
    }
 
-   static _styles: string[] = ['Controls-demo/OperationsPanel/Demo/Demo'];
+   static _styles: string[] = ['Controls-demo/operations/Index'];
 }
