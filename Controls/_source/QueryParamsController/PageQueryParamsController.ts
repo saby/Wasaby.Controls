38b4c5ccf856f67488e8ddc00ba487d0ea997eed
va-kolesov <va.kolesov@tensor.ru>
2020-06-24 15:13:49 +0300

    https://online.sbis.ru/opendoc.html?guid=15ab954a-3713-43cd-8a5c-7adcc3cb9012

--- Controls/_source/QueryParamsController/PageQueryParamsController.ts
+++ Controls/_source/QueryParamsController/PageQueryParamsController.ts
@@ -151,7 +151,7 @@ class PageQueryParamsController implements IQueryParamsController {
 
             // Если направление не указано,
             // значит это расчет параметров после начальной загрузки списка или после перезагрузки
-            if (config && config.page && config.pageSize) {
+            if (config && config.page !== undefined && config.pageSize !== undefined) {
                 const pageSizeRemainder = config.pageSize % this._options.pageSize;
                 const pageSizeCoef = (config.pageSize - pageSizeRemainder) / this._options.pageSize;
 
