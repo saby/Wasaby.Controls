67f0248bc98bbd26e652c6dbe98630b6c4a102b8
pa.averkiev <pa.averkiev@tensor.ru>
2020-06-27 14:36:18 +0300

    https://online.sbis.ru/opendoc.html?guid=7148c326-bcd3-4522-81fd-347c4e14ca1a правил тест

--- tests/ControlsUnit/List/BaseControl.test.js
+++ tests/ControlsUnit/List/BaseControl.test.js
@@ -1756,15 +1756,17 @@ define([
          const notifySpy = sinon.spy(baseControl, '_notify');
 
          onSelectedTypeChanged('selectAll', undefined);
-         assert.isFalse(notifySpy.withArgs('selectedKeysChanged').called);
+
+         // Получается, что не важно, что в multiSelectVisibility. Тут всегда будут вызываться эти методы?
+         assert.isTrue(notifySpy.withArgs('selectedKeysChanged').called);
          assert.isFalse(notifySpy.withArgs('excludedKeysChanged').called);
-         assert.isFalse(notifySpy.withArgs('listSelectedKeysCountChanged').called);
+         assert.isTrue(notifySpy.withArgs('listSelectedKeysCountChanged').called);
 
          lnCfg.multiSelectVisibility = 'visible';
 
          notifySpy.resetHistory();
          onSelectedTypeChanged('selectAll', undefined);
-         assert.isTrue(notifySpy.withArgs('selectedKeysChanged').called);
+         assert.isFalse(notifySpy.withArgs('selectedKeysChanged').called);
          assert.isFalse(notifySpy.withArgs('excludedKeysChanged').called);
          assert.isTrue(notifySpy.withArgs('listSelectedKeysCountChanged', [6, true], {bubbling: true}).called);
 
