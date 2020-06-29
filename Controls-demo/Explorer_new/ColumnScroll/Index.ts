9006ac0075b4a47c659c3fbc6950fa4e58117f73
ne.andruh <ne.andruh@tensor.ru>
2020-06-29 16:49:46 +0700

    https://online.sbis.ru/opendoc.html?guid=7da4b015-2316-484a-a603-057f5742a4aa

--- Controls-demo/Explorer_new/ColumnScroll/Index.ts
+++ Controls-demo/Explorer_new/ColumnScroll/Index.ts
@@ -2,17 +2,17 @@ import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/Explorer_new/ColumnScroll/ColumnScroll';
 import {Gadgets} from '../DataHelpers/DataCatalog';
 import * as MemorySource from 'Controls-demo/Explorer/ExplorerMemory';
-
+import { TRoot, IHeader } from 'Controls-demo/types';
 
 export default class extends Control {
    protected _template: TemplateFunction = Template;
-   protected _viewSource;
-   protected _columns = Gadgets.getGridColumnsForScroll();
+   protected _viewSource: MemorySource;
+   protected _columns: unknown = Gadgets.getGridColumnsForScroll();
    protected _viewMode: string = 'table';
-   protected _root = null;
-   protected _header = [...Gadgets.getHeader(),  {title: 'Подрядчик'}];
+   protected _root: TRoot = null;
+   protected _header: IHeader[] = [...Gadgets.getHeader(),  {title: 'Подрядчик'}];
 
-   protected _beforeMount() {
+   protected _beforeMount(): void {
       this._viewSource = new MemorySource({
          keyProperty: 'id',
          data: Gadgets.getData()
