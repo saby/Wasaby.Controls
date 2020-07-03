795b6012b32a822ce51807cd1dead9ec6f62408b
Pyankov <effective.gamer@gmail.com>
2020-07-03 18:47:51 +0700

    Доработал демо

--- Controls-demo/ResizingLine/Orientation/Index.ts
+++ Controls-demo/ResizingLine/Orientation/Index.ts
@@ -5,9 +5,13 @@ import {SyntheticEvent} from 'Vdom/Vdom';
 export default class ResizingLine extends Control<IControlOptions> {
     private _heightOfCenterContainer: number = 50;
     private _top: number = 50;
+    private _maxOffset: number = 30;
+    private _minOffset: number = 0;
 
     private _widthOfCenterContainer: number = 50;
     private _left: number = 50;
+    private _maxOffset2: number = 30;
+    private _minOffset2: number = 0;
 
     protected _template: TemplateFunction = controlTemplate;
 
@@ -18,19 +22,26 @@ export default class ResizingLine extends Control<IControlOptions> {
 
         switch (containerName) {
             case 'centerContainer':
+                prevSize = this._heightOfCenterContainer;
                 value = this._heightOfCenterContainer + offset;
                 this._heightOfCenterContainer = ResizingLine._limit(value);
+                offsetSize = this._heightOfCenterContainer - prevSize;
+                this.calcOffset1(offsetSize);
                 break;
             case 'centerContainerTop':
                 prevSize = this._heightOfCenterContainer;
                 value = this._heightOfCenterContainer + offset;
                 this._heightOfCenterContainer = ResizingLine._limit(value);
                 offsetSize = this._heightOfCenterContainer - prevSize;
+                this.calcOffset1(offsetSize);
                 this._top -= offsetSize;
                 break;
             case 'centerHorizontalContainer':
+                prevSize = this._widthOfCenterContainer;
                 value = this._widthOfCenterContainer + offset;
                 this._widthOfCenterContainer = ResizingLine._limit(value);
+                offsetSize = this._widthOfCenterContainer - prevSize;
+                this.calcOffset2(offsetSize);
                 break;
             case 'centerHorizontalContainerLeft':
                 prevSize = this._widthOfCenterContainer;
@@ -38,8 +49,19 @@ export default class ResizingLine extends Control<IControlOptions> {
                 this._widthOfCenterContainer = ResizingLine._limit(value);
                 offsetSize = this._widthOfCenterContainer - prevSize;
                 this._left -= offsetSize;
+                this.calcOffset2(offsetSize);
                 break;
         }
+    }
+
+    private calcOffset1(offsetSize: number): void {
+        this._maxOffset -= offsetSize;
+        this._minOffset += offsetSize;
+    }
+
+    private calcOffset2(offsetSize: number): void {
+        this._maxOffset2 -= offsetSize;
+        this._minOffset2 += offsetSize;
     }
 
     private static MIN_SIZE: number = 50;
