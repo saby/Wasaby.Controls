ddac58418c00685d354ef5eda6302cea769fe820
Maxim-M <max18071995@mail.ru>
2020-07-03 11:08:47 +0300

    https://online.sbis.ru/opendoc.html?guid=79e95bd7-32a1-47ee-8998-1589bad0dd0d

---
+++ Controls-demo/treeGrid/EditInPlace/Padding/Index.ts
@@ -0,0 +1,25 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/treeGrid/EditInPlace/Padding/InputFontSize';
+import {Memory} from 'Types/source';
+import {Gadgets} from '../../DemoHelpers/DataCatalog';
+import * as TitleCellTemplate from 'wml!Controls-demo/treeGrid/EditInPlace/Padding/ColumnTemplate/Title';
+import * as CountryCellTemplate from 'wml!Controls-demo/treeGrid/EditInPlace/Padding/ColumnTemplate/Country';
+import { IColumn } from 'Controls/_grid/interface/IColumn';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: Memory;
+    private _columns: IColumn[] = Gadgets.getGridColumnsForFlat();
+
+    protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: Gadgets.getFlatData()
+        });
+        this._columns[0].template = TitleCellTemplate;
+        // tslint:disable-next-line
+        this._columns[2].template = CountryCellTemplate;
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
