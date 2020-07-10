7e14c2d2c4bb73eac349a58891433225823beedc
ka.panihin <ka.panihin@tensor.ru>
2020-07-10 09:06:48 +0300

    https://online.sbis.ru/opendoc.html?guid=7207a727-4049-4162-bcff-7e2fb7fb1bc4

--- Controls/_interface/IFilter.ts
+++ Controls/_interface/IFilter.ts
@@ -1,4 +1,5 @@
 import {QueryWhere} from 'Types/source';
+import IBaseFilter from './IBaseFilter';
 
 export interface IFilterOptions {
     filter?: QueryWhere;
@@ -8,85 +9,13 @@ export interface IFilterOptions {
  * Интерфейс для контролов, которые поддерживают фильтрацию данных.
  *
  * @interface Controls/_interface/IFilter
+ * @mixin Controls/_interface/IBaseFilter
  * @public
  * @author Авраменко А.С.
  */
-export default interface IFilter {
+export default interface IFilter extends IBaseFilter {
     readonly '[Controls/_interface/IFilter]': boolean;
 }
-
-/**
- * @name Controls/_interface/IFilter#filter
- * @cfg {Object} Конфигурация объекта фильтра. Фильтр отправляется в запрос к источнику для получения данных.
- * @remark
- * При изменении фильтра важно передавать новый объект фильтра, изменение объекта по ссылке не приведет к желаемому результату.
- * @example
- * В данном примере в списке будет отображаться 2 элемента.
- * <pre>
- *    <Controls.list:View
- *       keyProperty="id"
- *       filter="{{_filter}}"
- *       source="{{_source}}" />
- * </pre>
- * <pre>
- *    this._filter = {id: ['1', '2']};
- *    this._source = new Memory({
- *      keyProperty: 'id',
- *      data: [
- *         {
- *            id: '1',
- *            title: 'Yaroslavl'
- *         },
- *         {
- *            id: '2',
- *            title: 'Moscow'
- *         },
- *         {
- *            id: '3',
- *            title: 'St-Petersburg'
- *         }
- *      ]
- *    });
- * </pre>
- * @see filterChanged
- */
-
-/*
- * @name Controls/_interface/IFilter#filter
- * @cfg {Object} Filter configuration - object with field names and their values.
- * @remark
- * You must pass a new filter object when you change the filter. You won't get the desired result if you change the object by reference.
- * @example
- * In this example, the list will be displayed 2 items.
- * TMPL:
- * <pre>
- *    <Controls.list:View
- *       keyProperty="id"
- *       filter={{_filter}}
- *       source="{{_source}}" />
- * </pre>
- * JS:
- * <pre>
- *    this._filter = {id: ['1', '2']};
- *    this._source = new Memory({
- *      keyProperty: 'id',
- *      data: [
- *         {
- *            id: '1',
- *            title: 'Yaroslavl'
- *         },
- *         {
- *            id: '2',
- *            title: 'Moscow'
- *         },
- *         {
- *            id: '3',
- *            title: 'St-Petersburg'
- *         }
- *      ]
- *    });
- * </pre>
- */
 
 /**
  * @event Controls/_interface/IFilter#filterChanged Происходит при изменении фильтра.
@@ -109,7 +38,7 @@ export default interface IFilter {
  *    }
  * },
  * filterChanged: function(e, filter) {
- *    
+ *
  *    // Т.к. в приведённом примере опция filter не связана с помощью bind c состоянием,
  *    // необходимо обновить фильтр на состоянии самостоятельно.
  *    this._filter = filter;
