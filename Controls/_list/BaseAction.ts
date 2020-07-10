d40a4cdd6bb7e314cc6ab88c2c9ece107fe38b99
aa.artemeva1 <aa.artemeva1@tensor.ru>
2020-07-10 12:48:28 +0300

    https://online.sbis.ru/opendoc.html?guid=7837d58a-516e-443b-9da3-e69e056566a0

--- Controls/_list/BaseAction.ts
+++ Controls/_list/BaseAction.ts
@@ -3,7 +3,25 @@ import rk = require('i18n!Controls');
 import {Confirmation} from 'Controls/popup';
 import {ISelectionObject} from 'Controls/interface';
 
+/**
+ * Контрол, на основе которого можно создавать другие action'ы.
+ * 
+ * @remark
+ * Посредством метода validate реализует валидацию. Не подходит для самостоятельного использования.
+ *
+ * @class Controls/_list/BaseAction
+ * @extends Core/Control
+ * @control
+ * @public
+ * @author Герасимов А.М.
+ */
+
 export default Control.extend({
+    /**
+     * Запускает валидацию.
+     * @function
+     * @name Controls/_list/BaseAction#validate
+     */
    validate(items: Array|ISelectionObject): boolean {
       let resultValidate: boolean = true;
 
