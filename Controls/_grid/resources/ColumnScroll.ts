c373e6b885fc474bae76fc5b933dc5102df2efb6
ea.rodionov1 <ea.rodionov1@tensor.ru>
2020-07-13 16:42:36 +0300

    https://online.sbis.ru/opendoc.html?guid=2573375e-7454-4745-8e13-a3d1ec86c4cc

--- Controls/_grid/resources/ColumnScroll.ts
+++ Controls/_grid/resources/ColumnScroll.ts
@@ -226,7 +226,6 @@ export class ColumnScroll {
         if (detection.safari) {
             this._fixSafariBug();
         }
-        this._drawTransform(0, isFullGridSupport);
 
         const newContentSize = this._contentContainer.scrollWidth;
         const newContainerSize = isFullGridSupport ? this._contentContainer.offsetWidth : this._scrollContainer.offsetWidth;
@@ -241,6 +240,7 @@ export class ColumnScroll {
             // reset scroll position after resize, if we don't need scroll
             if (newContentSize <= newContainerSize) {
                 this._scrollPosition = 0;
+                this._drawTransform(0, isFullGridSupport);
             }
 
             this._updateShadowState();
@@ -321,7 +321,12 @@ export class ColumnScroll {
         if (!stickyCellContainer) {
             return 0;
         }
-        const stickyCellOffsetLeft = stickyCellContainer.getBoundingClientRect().left - this._contentContainer.getBoundingClientRect().left;
+        // Необходимо получить отступ контейнера таблицы слева. Именно видимой части контейнера, т.к. по факту он может
+        // быть смещен вправо/влево из-зи прокрутки. Поэтому, при определении отступа, необходимо учесть значение горизонтальной
+        // прокрутки таблицы и скомпенсировать ее.
+        const contentContainerOffsetLeft = this._contentContainer.getBoundingClientRect().left + this._scrollPosition;
+
+        const stickyCellOffsetLeft = stickyCellContainer.getBoundingClientRect().left - contentContainerOffsetLeft;
         return stickyCellOffsetLeft + stickyCellContainer.offsetWidth;
     }
 
