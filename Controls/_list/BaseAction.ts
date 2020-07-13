944f10a51194ab88eeabc0bed114de9d00a1d531
nu.krukov <nu.krukov@tensor.ru>
2020-07-13 15:52:29 +0700

    https://online.sbis.ru/opendoc.html?guid=c636ea6d-bafc-40c3-8a64-6a3528dab824

--- Controls/_list/BaseAction.ts
+++ Controls/_list/BaseAction.ts
@@ -1,7 +1,7 @@
-import Control = require('Core/Control');
 import rk = require('i18n!Controls');
 import {Confirmation} from 'Controls/popup';
 import {ISelectionObject} from 'Controls/interface';
+import {Control, IControlOptions} from 'UI/Base';
 
 /**
  * Базовый класс action'a, на основе которого создаются другие action'ы. Не подходит для самостоятельного использования.
@@ -13,17 +13,17 @@ import {ISelectionObject} from 'Controls/interface';
  * @author Герасимов А.М.
  */
 
-export default Control.extend({
-    /**
-     * Производит проверку переданных параметров. Если массив значений пуст, возвращает false и выводит окно с текстом, иначе возвращает true.
-     * 
-     * @remark
-     * При необходимости метод нужно вызывать вручную из наследника.
-     * 
-     * @function
-     * @name Controls/_list/BaseAction#validate
-     */
-   validate(items: Array|ISelectionObject): boolean {
+export default class BaseAction extends Control<IControlOptions> {
+   /**
+    * Производит проверку переданных параметров. Если массив значений пуст, возвращает false и выводит окно с текстом, иначе возвращает true.
+    *
+    * @remark
+    * При необходимости метод нужно вызывать вручную из наследника.
+    *
+    * @function
+    * @name Controls/_list/BaseAction#validate
+    */
+   validate(items: unknown[]|ISelectionObject): boolean {
       let resultValidate: boolean = true;
 
       if (items instanceof Array && !items.length || items.selected && !items.selected.length) {
@@ -36,4 +36,4 @@ export default Control.extend({
 
       return resultValidate;
    }
-});
+}
