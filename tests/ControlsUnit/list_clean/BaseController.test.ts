83f8a03b2e88e640bd8856ad61954e93448fda9b
Maxim-M <max18071995@mail.ru>
2020-07-14 14:30:08 +0300

    https://online.sbis.ru/opendoc.html?guid=306f4e38-d255-4928-bbaf-f6dd43c7f917
    Пункт 3
    Добавлены тесты

---
+++ tests/ControlsUnit/list_clean/BaseController.test.ts
@@ -0,0 +1,52 @@
+import {assert} from 'chai';
+import {BaseControl, ListViewModel} from 'Controls/list';
+import {RecordSet} from 'Types/collection';
+import * as Config from 'Env/Config';
+
+describe('Controls/list_clean/BaseControl', () => {
+    // https://online.sbis.ru/opendoc.html?guid=9a6f0437-ea6d-4d7f-b163-25dc8f244c64 пункт 3
+    describe('BaseControl watcher groupHistoryId', () => {
+
+        const GROUP_HISTORY_ID_NAME: string = 'MY_NEWS';
+        const GROUP_HISTORY_ID_VALUE: string = 'Крайнов Дмитрий';
+
+        const baseControlCfg = {
+            viewName: 'Controls/List/ListView',
+            keyProperty: 'id',
+            viewModelConstructor: ListViewModel,
+            items: new RecordSet({
+                keyProperty: 'id',
+                rawData: []
+            })
+        };
+        let baseControl;
+
+        beforeEach(() => {
+            Config.UserConfig.setParam(
+                'LIST_COLLAPSED_GROUP_' + GROUP_HISTORY_ID_NAME, JSON.stringify([GROUP_HISTORY_ID_VALUE])
+            );
+            baseControl = new BaseControl(baseControlCfg);
+        });
+
+        afterEach(() => {
+            baseControl.destroy();
+            baseControl = undefined;
+        });
+
+        it('CollapsedGroup undefined', () => {
+            baseControl._beforeMount(baseControlCfg);
+            baseControl._container = {getElementsByClassName: () => ([{clientHeight: 100, offsetHeight: 0}])};
+            baseControl._afterMount();
+            assert.isFalse(!!baseControl._listViewModel._options.collapsedGroups);
+        });
+        it('updated CollapsedGroups', () => {
+            baseControl._beforeMount(baseControlCfg);
+            baseControl._container = {getElementsByClassName: () => ([{clientHeight: 100, offsetHeight: 0}])};
+            baseControl._afterMount();
+
+            await baseControl._beforeUpdate({...baseControlCfg, groupHistoryId: GROUP_HISTORY_ID_NAME});
+
+            assert.equal(baseControl._listViewModel._options.collapsedGroups[0], GROUP_HISTORY_ID_VALUE);
+        });
+    });
+});
