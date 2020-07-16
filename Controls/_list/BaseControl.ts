46f57b87ee24fc7997485cd22a301fff4f57f965
pa.averkiev <pa.averkiev@tensor.ru>
2020-07-16 17:31:16 +0300

    https://online.sbis.ru/opendoc.html?guid=48d2f247-8443-405a-ae15-5cae701ac053 Добавил гарантированное получение contents для item. + Тест на случай Breadcrumbs

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -1277,6 +1277,22 @@ const _private = {
         return self._listViewModel ? self._listViewModel.getCount() : 0;
     },
 
+    /**
+     * Закрывает меню опций записи у активной записи, если она есть
+     * @param self
+     * @param items
+     */
+    closeItemActionsMenuForActiveItem(self: typeof BaseControl, items: Array<CollectionItem<Model>>): void {
+        const activeItem = self._itemActionsController.getActiveItem();
+        if (activeItem && items && items.find((item) => {
+            const itemContents = _private.getPlainItemContents(item);
+            const activeItemContents = _private.getPlainItemContents(item);
+            return itemContents?.getKey() === activeItemContents?.getKey();
+        })) {
+            _private.closeActionsMenu(self);
+        }
+    },
+
     onListChange(self, event, changesType, action, newItems, newItemsIndex, removedItems, removedItemsIndex): void {
         // TODO Понять, какое ускорение мы получим, если будем лучше фильтровать
         // изменения по changesType в новой модели
@@ -1296,13 +1312,7 @@ const _private = {
                 }
             }
             if (action === IObservable.ACTION_REMOVE && self._itemActionsMenuId) {
-                const itemWithShownMenu = self._itemActionsController.getActiveItem();
-                if (itemWithShownMenu &&
-                    removedItems.find((item) => (
-                        item.getContents().getKey() === itemWithShownMenu.getContents().getKey()
-                    ))) {
-                    _private.closeActionsMenu(self);
-                }
+                _private.closeItemActionsMenuForActiveItem(self, removedItems);
             }
 
             if (self._selectionController) {
