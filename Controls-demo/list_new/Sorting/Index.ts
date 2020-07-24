982d912369540cda1e809399bd8a35f4f644a276
Maxim-M <max18071995@mail.ru>
2020-07-24 12:13:40 +0300

    https://online.sbis.ru/opendoc.html?guid=c113c73e-ac0f-49f8-8c53-ce008bca9cea

---
+++ Controls-demo/list_new/Sorting/Index.ts
@@ -0,0 +1,19 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/list_new/Base/Base';
+import {Memory} from 'Types/source';
+import {getFewCategories as getData} from '../DemoHelpers/DataCatalog';
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: Memory;
+    protected _sorting: Array = [{id: 'DESC'}];
+
+    protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: getData()
+        });
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
