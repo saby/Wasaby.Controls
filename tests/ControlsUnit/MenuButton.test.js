e0c064e8b894ad49a5a49215bb6cf360e2e0faba
ea.melnikova <ea.melnikova@tensor.ru>
2020-07-27 09:41:10 +0300

    https://online.sbis.ru/opendoc.html?guid=dd171873-dc79-4e97-b76e-48503ab5a222

--- tests/ControlsUnit/MenuButton.test.js
+++ tests/ControlsUnit/MenuButton.test.js
@@ -7,9 +7,10 @@ define(
       'Core/Deferred',
       'Types/entity',
       'Types/collection',
-      'Controls/popup'
+      'Controls/popup',
+      'Controls/buttons'
    ],
-   (dropdown, sourceLib, Clone, history, Deferred, entity, collection, popup) => {
+   (dropdown, sourceLib, Clone, history, Deferred, entity, collection, popup, buttons) => {
       describe('MenuButton', () => {
          let items = [
             {
@@ -413,6 +414,29 @@ define(
             };
             menu.openMenu();
             assert.equal(actualTarget, 'testTarget');
+         });
+
+         it('_updateState', () => {
+            let optionsUpdated = false;
+            const options = {
+               isNewOptionsUsed: false
+            };
+            buttons.ActualApi.styleToViewMode = () => {
+               optionsUpdated = true;
+               return {
+                  viewMode: '',
+                  style: '',
+                  buttonAdd: false
+               };
+            };
+            menu._updateState(options);
+            assert.isTrue(optionsUpdated);
+
+            options.isNewOptionsUsed = true;
+            optionsUpdated = false;
+
+            menu._updateState(options);
+            assert.isFalse(optionsUpdated);
          });
       });
    }
