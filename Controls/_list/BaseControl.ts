bfb7ba952cf6afff70133693212e5f4727d5a13d
ka.panihin <ka.panihin@tensor.ru>
2020-07-27 14:31:16 +0300

    https://online.sbis.ru/opendoc.html?guid=bb0c92b6-8e32-4e86-98ae-83944a549a71

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -402,7 +402,7 @@ const _private = {
                     // If received list is empty, make another request. If it’s not empty, the following page will be requested in resize event handler after current items are rendered on the page.
                     if (_private.needLoadNextPageAfterLoad(list, self._listViewModel, navigation)) {
                         _private.checkLoadToDirectionCapability(self, filter, navigation);
-                    } else {
+                    } else if (!self._wasScrollToEnd) {
                         _private.checkNeedAttachLoadTopTriggerToNull(self);
                     }
                 });
@@ -1786,6 +1786,8 @@ const _private = {
 
         const lastItemKey = ItemsUtil.getPropertyValue(lastItem, self._options.keyProperty);
 
+        self._wasScrollToEnd = true;
+
         // Последняя страница уже загружена но конец списка не обязательно отображается,
         // если включен виртуальный скролл. ScrollContainer учитывает это в scrollToItem
         _private.scrollToItem(self, lastItemKey, true, true).then(() => {
@@ -2798,7 +2800,9 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
 
         this._notify('register', ['documentDragStart', this, this._documentDragStart], {bubbling: true});
         this._notify('register', ['documentDragEnd', this, this._documentDragEnd], {bubbling: true});
-        _private.checkNeedAttachLoadTopTriggerToNull(this);
+        if (!this._wasScrollToEnd) {
+            _private.checkNeedAttachLoadTopTriggerToNull(this);
+        }
     },
 
     _beforeUpdate(newOptions) {
@@ -3233,6 +3237,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
             this._itemReloaded = false;
         }
 
+        this._wasScrollToEnd = false;
         this._scrollPageLocked = false;
         this._modelRecreated = false;
         if (this._callbackAfterUpdate) {
