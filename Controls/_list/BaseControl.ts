da29502bcf99a50a6f4231432796e5a5638c93a2
va-kolesov <va.kolesov@tensor.ru>
2020-07-29 15:26:15 +0300

    Update BaseControl.ts

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -2034,9 +2034,6 @@ const _private = {
             self._loadTriggerOffset = result.triggerOffset;
             self.applyTriggerOffset(result.triggerOffset);
         }
-        if (result.activeElement) {
-            self._notify('activeElementChanged', [result.activeElement]);
-        }
         if (result.scrollToActiveElement) {
             _private.scrollToItem(self, result.activeElement, false, true);
         }
