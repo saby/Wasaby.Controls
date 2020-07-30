86841275c1ba4475872ef663af617a62d943dde6
am.gerasimov <am.gerasimov@tensor.ru>
2020-07-30 13:41:44 +0300

    https://online.sbis.ru/opendoc.html?guid=afea699e-0810-4263-b0f8-592c7aa1d1ed

--- Controls/_operations/FilterController.ts
+++ Controls/_operations/FilterController.ts
@@ -1,18 +1,28 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
+// @ts-ignore
 import * as template from 'wml!Controls/_operations/FilterController';
 import {isEqual} from 'Types/object';
 import selectionToRecord = require('Controls/_operations/MultiSelector/selectionToRecord');
 
-import { TKeysSelection as TKeys } from 'Controls/interface/';
+import { TKeysSelection as TKeys } from 'Controls/interface';
 import { Rpc, PrefetchProxy } from 'Types/source';
-
+import {IHashMap} from 'Types/declarations';
 
 export interface IOperationsFilterController extends IControlOptions {
    selectionViewMode: string;
    selectedKeys: TKeys;
    excludedKeys: TKeys;
-   source: Rpc|PrefetchProxy
-   filter: Object;
+   source: Rpc|PrefetchProxy;
+   filter: IHashMap<unknown>;
+}
+
+interface IFilterConfig {
+   filter: IHashMap<unknown>;
+   selectedKeys: TKeys;
+   excludedKeys: TKeys;
+   source: Rpc|PrefetchProxy;
+   parentProperty?: string;
+   selectionViewMode?: string;
 }
 
 export default class OperationsFilterController extends Control<IOperationsFilterController> {
@@ -20,24 +30,37 @@ export default class OperationsFilterController extends Control<IOperationsFilte
    protected _filter: object = null;
 
    protected _beforeMount(options: IOperationsFilterController): void {
-      this._filter = OperationsFilterController.prepareFilter(options.filter, options.selectedKeys, options.excludedKeys, options.source, options.selectionViewMode);
+      this._filter = OperationsFilterController.prepareFilter(options);
    }
 
    protected _beforeUpdate(newOptions: IOperationsFilterController): void {
-      if (!isEqual(this._options.filter, newOptions.filter) || this._options.selectionViewMode !== newOptions.selectionViewMode) {
-         this._filter = OperationsFilterController.prepareFilter(newOptions.filter, newOptions.selectedKeys, newOptions.excludedKeys, newOptions.source, newOptions.selectionViewMode);
+      if (!isEqual(this._options.filter, newOptions.filter) ||
+          this._options.selectionViewMode !== newOptions.selectionViewMode) {
+         this._filter = OperationsFilterController.prepareFilter(newOptions);
       }
    }
 
-   private static prepareFilter(filter: object, selectedKeys: TKeys, excludedKeys: TKeys, source: Rpc|PrefetchProxy, selectionViewMode: string): object {
+   private static prepareFilter({
+        filter,
+        selectedKeys= [],
+        excludedKeys= [],
+        source,
+        selectionViewMode,
+        parentProperty
+     }: IFilterConfig): object {
       const preparedFilter = {...filter};
+      const addSelectionToFilter = selectionViewMode === 'selected' ||
+                                   (selectedKeys.length && parentProperty);
 
-      if (selectionViewMode === 'selected') {
-         source = source.getOriginal ? source.getOriginal() : source;
-         preparedFilter.SelectionWithPath = selectionToRecord({
+      if (addSelectionToFilter) {
+         const listSource = (source as PrefetchProxy).getOriginal ? (source as PrefetchProxy).getOriginal() : source;
+         const selection = selectionToRecord({
             selected: selectedKeys || [],
             excluded: excludedKeys || []
-         }, source.getAdapter(), 'all', false);
+         }, (listSource as Rpc).getAdapter(), 'all', false);
+         const filterField = selectionViewMode === 'selected' ? 'SelectionWithPath' : 'entries';
+
+         preparedFilter[filterField] = selection;
       }
 
       return preparedFilter;
