9e6c4da058fb7935043afdcd0142a8efff5a944a
va-kolesov <va.kolesov@tensor.ru>
2020-07-31 09:34:12 +0300

    https://online.sbis.ru/opendoc.html?guid=4d47497a-2452-4a93-a316-0cc7755bef15

--- Controls/_grid/GridViewModel.ts
+++ Controls/_grid/GridViewModel.ts
@@ -31,7 +31,7 @@ import * as Grouping from 'Controls/_list/Controllers/Grouping';
 import {JS_SELECTORS as COLUMN_SCROLL_JS_SELECTORS} from './resources/ColumnScroll';
 import { shouldAddActionsCell } from 'Controls/_grid/utils/GridColumnScrollUtil';
 import {createClassListCollection} from "../Utils/CssClassList";
-import { shouldAddStickyLadderCell, prepareLadder,  isSupportLadder, getStickyColumn} from 'Controls/_grid/utils/GridLadderUtil';
+import { stickyLadderCellsCount, prepareLadder,  isSupportLadder, getStickyColumn} from 'Controls/_grid/utils/GridLadderUtil';
 import {IHeaderCell} from './interface/IHeaderCell';
 import { ItemsEntity } from 'Controls/dragnDrop';
 import { IDragPosition, IFlatItemData } from 'Controls/listDragNDrop';
@@ -798,7 +798,7 @@ var
                 this._header,
                 this._options.multiSelectVisibility !== 'hidden',
                 this._shouldAddActionsCell(),
-                this.shouldAddStickyLadderCell()
+                this.stickyLadderCellsCount()
             );
             if (columns && columns.length) {
                 this._headerModel = {
@@ -850,10 +850,10 @@ var
             }
             return result;
         },
-        _prepareHeaderColumns: function(columns, multiSelectVisibility, actionsCell, stickyLadderCell) {
+        _prepareHeaderColumns: function(columns, multiSelectVisibility, actionsCell, stickyLadderCellsCount) {
             if (columns && columns.length) {
                 this._isMultiHeader = this.isMultiHeader(columns);
-                this._headerRows = getHeaderRowsArray(columns, multiSelectVisibility, this._isMultiHeader, actionsCell, stickyLadderCell);
+                this._headerRows = getHeaderRowsArray(columns, multiSelectVisibility, this._isMultiHeader, actionsCell, stickyLadderCellsCount);
                 const headerMaxEndCellData = getHeaderMaxEndCellData(this._headerRows);
                 this._maxEndRow = headerMaxEndCellData.maxRow;
                 this._maxEndColumn = headerMaxEndCellData.maxColumn;
@@ -880,8 +880,8 @@ var
         /**
          * Проверка необходимости добавлять ячейку для лесенки
          */
-        shouldAddStickyLadderCell(): boolean {
-            return shouldAddStickyLadderCell(
+        stickyLadderCellsCount(): number {
+            return stickyLadderCellsCount(
                 this._options.columns,
                 this._options.stickyColumn,
                 this.getDragItemData());
@@ -1179,8 +1179,11 @@ var
             } else {
                 this._resultsColumns = columns;
             }
-            if (this.shouldAddStickyLadderCell()) {
-                this._resultsColumns = [{}].concat(this._resultsColumns);
+            let stickyCellsCount = this.stickyLadderCellsCount();
+            if (stickyCellsCount === 2) {
+                this._resultsColumns = [{}, this._resultsColumns[0], {}].concat(this._resultsColumns.slice(1));
+            } else if (stickyCellsCount === 1) {
+                this._resultsColumns = [{}, ...this._resultsColumns];
             }
             if (this._shouldAddActionsCell()) {
                 this._resultsColumns = this._resultsColumns.concat([{ isActionCell: true }]);
@@ -1324,7 +1327,7 @@ var
             const hasMultiSelect = multiSelectVisibility !== 'hidden';
             this._model.setMultiSelectVisibility(multiSelectVisibility);
             this._prepareColgroupColumns(this._columns, hasMultiSelect);
-            this._prepareHeaderColumns(this._header, hasMultiSelect, this._shouldAddActionsCell(), this.shouldAddStickyLadderCell());
+            this._prepareHeaderColumns(this._header, hasMultiSelect, this._shouldAddActionsCell(), this.stickyLadderCellsCount());
 
             this._prepareResultsColumns(this._columns, hasMultiSelect);
         },
@@ -2089,7 +2092,7 @@ var
         getFooterStyles(): string {
             if (GridLayoutUtil.isFullGridSupport()) {
                 const offsetForMultiSelect: number = +(this.getMultiSelectVisibility() !== 'hidden');
-                const offsetForStickyColumn: number = +(this.shouldAddStickyLadderCell());
+                const offsetForStickyColumn: number = +(this.stickyLadderCellsCount());
 
                 return GridLayoutUtil.getColumnStyles({
                     columnStart: 0,
@@ -2174,9 +2177,8 @@ var
                 return GridLayoutUtil.getColumnStyles(columnCfg);
             } else {
                 columnCfg.columnSpan = params.columnsLength;
-                if (this.shouldAddStickyLadderCell()) {
-                    columnCfg.columnSpan += 1;
-                }
+                columnCfg.columnSpan += this.stickyLadderCellsCount();
+
                 return GridLayoutUtil.getColumnStyles(columnCfg);
             }
         },
