0ebe0a5b5f2b9563492bb431b4e6f3271e48305d
gd.kovalev <gd.kovalev@tensor.ru>
2020-08-05 18:30:46 +0700

    https://online.sbis.ru/opendoc.html?guid=c7ae1b80-cfa9-4dec-8726-5ff09b0ad389

---
+++ Controls/Utils/FontWidthConstants.ts
@@ -0,0 +1,11 @@
+import IEFontWidthConstants from 'Controls/Utils/FontWidthConstants/IEFontWidthConstants';
+import FFFontWidthConstants from 'Controls/Utils/FontWidthConstants/FFFontWidthConstants';
+import SafariFontWidthConstants from 'Controls/Utils/FontWidthConstants/SafariFontSizeConst';
+import ChromeFontWidthConstants from 'Controls/Utils/FontWidthConstants/ChromeFontWidthConstants';
+
+export {
+    IEFontWidthConstants,
+    FFFontWidthConstants,
+    SafariFontWidthConstants,
+    ChromeFontWidthConstants
+};
