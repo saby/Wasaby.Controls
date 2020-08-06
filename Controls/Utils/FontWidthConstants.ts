7361e96e5669fc10c59a508b3aa309411aca8e90
gd.kovalev <gd.kovalev@tensor.ru>
2020-08-06 18:24:58 +0700

    https://online.sbis.ru/opendoc.html?guid=c7ae1b80-cfa9-4dec-8726-5ff09b0ad389

--- Controls/Utils/FontWidthConstants.ts
+++ Controls/Utils/FontWidthConstants.ts
@@ -1,11 +1,46 @@
-import IEFontWidthConstants from 'Controls/Utils/FontWidthConstants/IEFontWidthConstants';
-import FFFontWidthConstants from 'Controls/Utils/FontWidthConstants/FFFontWidthConstants';
-import SafariFontWidthConstants from 'Controls/Utils/FontWidthConstants/SafariFontSizeConst';
-import ChromeFontWidthConstants from 'Controls/Utils/FontWidthConstants/ChromeFontWidthConstants';
+const IE = 'Controls/Utils/FontWidthConstants/IE';
+const FF = 'Controls/Utils/FontWidthConstants/FF';
+const SAFARI = 'Controls/Utils/FontWidthConstants/Safari';
+const CHROME = 'Controls/Utils/FontWidthConstants/Chrome';
 
-export {
-    IEFontWidthConstants,
-    FFFontWidthConstants,
-    SafariFontWidthConstants,
-    ChromeFontWidthConstants
+const loadFontWidthConstants = (module: string) => {
+    let promiseResolver;
+    const promise = new Promise((resolve) => {
+        promiseResolver = resolve;
+    });
+    if (requirejs.defined(module)) {
+        promiseResolver(requirejs(module));
+    } else {
+        requirejs([module], (loadedModule) => {
+            promiseResolver(loadedModule);
+        });
+    }
+    return promise;
+};
+
+export const getFontWidthConstants = (browser) => {
+    let module;
+    switch (browser) {
+        case 'IE': {
+            module = IE;
+            break;
+        }
+        case 'FF': {
+            module = FF;
+            break;
+        }
+        case 'Safari': {
+            module = SAFARI;
+            break;
+        }
+        case 'Chrome': {
+            module = CHROME;
+            break;
+        }
+        default: {
+            module = CHROME;
+            break;
+        }
+    }
+    return loadFontWidthConstants(module);
 };
