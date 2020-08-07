58b8e099d665d1deec683db86f798c813316559b
as.avramenko <as.avramenko@tensor.ru>
2020-08-07 14:07:07 +0300

    https://online.sbis.ru/opendoc.html?guid=7bd39d3f-8eaf-4c91-801e-d06d5afcc7b7

--- Controls/_grid/interface/IHeaderCell.ts
+++ Controls/_grid/interface/IHeaderCell.ts
@@ -133,9 +133,9 @@ export interface IHeaderCell extends IControlOptions {
      * <!-- file2.wml -->
      * {{contentTemplate.colData.column.title}}
      * </pre>
-     * 
+     *
      * **Пример 4.** Конфигурация колонки для выравнивания контента по копейкам. На шаблон добавлен CSS-класс "controls-Grid&#95;&#95;cell&#95;spacing&#95;money".
-     * 
+     *
      * <pre class="brush: html; highlight: [6]">
      * <Controls.grid:View>
      *    <ws:header>
@@ -152,7 +152,7 @@ export interface IHeaderCell extends IControlOptions {
      * </Controls.grid:View>
      * </pre>
      */
-    template?: TemplateFunction;
+    template?: TemplateFunction|string;
     /**
      * @name Controls/_grid/interface/IHeaderCell#sortingProperty
      * @cfg {String} Свойство, по которому выполняется сортировка.
