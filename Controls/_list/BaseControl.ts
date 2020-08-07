ad62c8b85dcaa98ca7552742444be815baf17adf
as.avramenko <as.avramenko@tensor.ru>
2020-08-07 10:49:11 +0300

    https://online.sbis.ru/opendoc.html?guid=ecbd2286-27af-4fe5-865a-a1f863ed76d1

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -4058,8 +4058,16 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
     },
 
     _observeScrollHandler( _: SyntheticEvent<Event>, eventName: string, params: any): void {
+        const checkingEvens = {
+            virtualPageTopStart: true,
+            virtualPageTopStop: true
+        };
+        // Не реагируем на события observer'a, если заказана прокрутка к первому элементу. Таблица ещё не готова к
+        // обработке событий от observer'a и реакция на событие приводит к лишней загрузке.
         if (this._scrollController) {
-            this._scrollController.observeScroll(eventName, params);
+            if (!this._needScrollToFirstItem || !checkingEvens[eventName]) {
+                this._scrollController.observeScroll(eventName, params);
+            }
         }
     },
 
