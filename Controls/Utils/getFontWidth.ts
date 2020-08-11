91d43761ede274134129619f85bca996c205c442
gd.kovalev <gd.kovalev@tensor.ru>
2020-08-11 17:59:09 +0700

    https://online.sbis.ru/opendoc.html?guid=c7ae1b80-cfa9-4dec-8726-5ff09b0ad389

--- Controls/Utils/getFontWidth.ts
+++ Controls/Utils/getFontWidth.ts
@@ -3,10 +3,8 @@ import {detection} from 'Env/Env';
 const constants = 'Controls/Utils/FontWidthConstants/';
 
 export const getFontWidth = (text, size) => {
-    let browser;
-    if (detection.chrome) {
-        browser = 'Chrome';
-    } else if (detection.firefox) {
+    let browser = 'Chrome';
+    if (detection.firefox) {
         browser = 'FF';
     } else if (detection.safari) {
         browser = 'Safari';
@@ -18,10 +16,9 @@ export const getFontWidth = (text, size) => {
 };
 
 const countTextWidth = (text, font, size) => {
-    const textArray = text.split('');
     let textWidth = 0;
-    for (let i = 0; i < textArray.length; i++) {
-        textWidth += Number(font[size][textArray[i]]);
+    for (let i = 0; i < text.length; i++) {
+        textWidth += font[size][text[i]];
     }
     return textWidth;
 };
