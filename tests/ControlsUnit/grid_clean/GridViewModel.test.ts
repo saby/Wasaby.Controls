4b7910c4ff2c73d08e5c814c95c94694b36285f9
ma.mochalov <ma.mochalov@tensor.ru>
2020-08-11 08:42:58 +0300

    https://online.sbis.ru/opendoc.html?guid=85e1ff71-574a-47a2-9c84-71090e880339

--- tests/ControlsUnit/grid_clean/GridViewModel.test.ts
+++ tests/ControlsUnit/grid_clean/GridViewModel.test.ts
@@ -247,6 +247,17 @@ describe('Controls/grid_clean/GridViewModel', () => {
                 gridViewModel.goToNext();
                 assert.isFalse(gridViewModel.getCurrent().isLastRow);
             });
+
+            it('hasMoreData ', () => {
+                gridViewModel._options.navigation = {
+                    view: 'infinity'
+                };
+                gridViewModel.goToNext();
+                gridViewModel.setHasMoreData(true);
+                assert.isFalse(gridViewModel.getCurrent().isLastRow);
+                gridViewModel.setHasMoreData(false);
+                assert.isTrue(gridViewModel.getCurrent().isLastRow);
+            });
         });
     });
 });
