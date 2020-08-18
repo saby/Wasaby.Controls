a19fa052efaa5a86688b0923506b67ccd1deb9e5
gd.kovalev <gd.kovalev@tensor.ru>
2020-08-18 17:47:05 +0700

    https://online.sbis.ru/opendoc.html?guid=dd8aad3f-162a-4cd5-b667-dbe8bd2b767a

--- Controls/_scroll/Container.ts
+++ Controls/_scroll/Container.ts
@@ -233,6 +233,7 @@ let
        _setScrollTop(self, value: number): void {
             // На айпаде скроллбар не строится. Чтобы изменение св-ва _scrollTop не приводило к _forceUpdate
             // его нельзя объявлять на шаблоне ( даже в ветке кода, которая не испольняется). Перевожу на сеттер.
+           //TODO: https://online.sbis.ru/opendoc.html?guid=65a30a09-0581-4506-9329-e472ea9630b5
             self._scrollTop = value;
             self._children.scrollBar?.setScrollPosition(value);
             _private.updateStates(self);
@@ -1014,6 +1015,7 @@ let
       _scrollbarTaken() {
          if (this._showScrollbarOnHover && (this._displayState.canScroll || this._displayState.canHorizontalScroll)) {
              // Обновляем позицию скроллабара, так как он появляется только при наведении на скролл контейнер
+             //TODO: https://online.sbis.ru/opendoc.html?guid=65a30a09-0581-4506-9329-e472ea9630b5
             this._children.scrollBar?.setScrollPosition(this._scrollTop);
             this._notify('scrollbarTaken', [], { bubbling: true });
          }
