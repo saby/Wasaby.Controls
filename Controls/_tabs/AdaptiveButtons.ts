724e40240aca91418453b45c3ade1c35c0ed0552
koshka-alesya <veronica.desmond@mail.ru>
2020-08-19 09:33:41 +0300

    https://online.sbis.ru/opendoc.html?guid=84a1fa3b-57c4-4fd7-bfc6-992d99df2202

---
+++ Controls/_tabs/AdaptiveButtons.ts
@@ -0,0 +1,245 @@
+import {Control, TemplateFunction} from 'UI/Base';
+// @ts-ignore
+import * as template from 'wml!Controls/_tabs/AdaptiveButtons/AdaptiveButtons';
+import {RecordSet} from 'Types/collection';
+import {loadFontWidthConstants, getFontWidth} from 'Controls/Utils/getFontWidth';
+import {SbisService} from 'Types/source';
+import {CrudWrapper} from 'Controls/dataSource';
+import {SyntheticEvent} from 'Vdom/Vdom';
+import {Logger} from 'UI/Utils';
+import {ITabsButtons, ITabsButtonsOptions} from './interface/ITabsButtons';
+import rk = require('i18n!Controls');
+
+const MARGIN = 13;
+const MIN_WIDTH = 26;
+const PADDING_OF_MORE_BUTTON = 6;
+const COUNT_OF_MARGIN = 2;
+const MORE_BUTTON_TEXT = rk('Ещё...');
+
+interface IReceivedState {
+    items: RecordSet<object>;
+    fontWidthConstants: unknown;
+}
+
+/**
+ * Интерфейс для опций контрола адаптивных вкладок.
+ * @interface Controls/_tabs/ITabsAdaptiveButtons
+ * @public
+ * @author Бондарь А.В.
+ */
+
+export interface ITabsAdaptiveButtonsOptions extends ITabsButtonsOptions {
+    align?: string;
+    containerWidth: number;
+}
+/**
+ * @name Controls/_tabs/ITabsAdaptiveButtons#align
+ * @cfg {String} Выравнивание вкладок по правому или левому краю.
+ * @variant left Вкладки выравниваются по левому краю.
+ * @variant right Вкладки выравниваются по правому краю.
+ * @default right
+ */
+
+/**
+ * @name Controls/_tabs/ITabsAdaptiveButtons#containerWidth
+ * @cfg {Number} Ширина контейнера вкладок. Необходимо указывать для правильного расчета ширины вкладок.
+ */
+
+/**
+ * Контрол предоставляет пользователю возможность выбрать между двумя или более адаптивными под ширину вкладками.
+ *
+ * @class Controls/_tabs/AdaptiveButtons
+ * @extends Core/Control
+ * @mixes Controls/_tabs/interface/ITabsButtons
+ * @mixes Controls/tabs:ITabsAdaptiveButtonsOptions
+ * @mixes Controls/interface:ISource
+ * @mixes Controls/interface:IItems
+ * @control
+ * @public
+ * @category List
+ * @author Бондарь А.В.
+ * @demo Controls-demo/Tabs/AdaptiveButtons/Index
+ */
+
+class AdaptiveButtons extends Control<ITabsAdaptiveButtonsOptions, IReceivedState> {
+    readonly '[Controls/_tabs/interface/ITabsButtons]': boolean = true;
+    protected _template: TemplateFunction = template;
+    protected _lastIndex: number = 0;
+    protected _items: RecordSet<object>;
+    protected _moreButtonWidth: number;
+    protected _visibleItems: RecordSet<object>;
+    protected _crudWrapper: CrudWrapper;
+    private _fontWidthConstants: unknown;
+
+    protected _beforeMount(options?: ITabsAdaptiveButtonsOptions,
+                           contexts?: object,
+                           receivedState?: IReceivedState): Promise<IReceivedState> | void {
+        if (!options.containerWidth) {
+            Logger.error('Option containerWidth is undefined');
+        }
+
+        if (receivedState) {
+            this._items = receivedState.items;
+            this._fontWidthConstants = receivedState.fontWidthConstants;
+            this._moreButtonWidth = this._getTextWidth(MORE_BUTTON_TEXT, 'm');
+            this._calcVisibleItems(this._items, options);
+        } else {
+            return new Promise((resolve) => {
+                loadFontWidthConstants().then((fontWidthConstants) => {
+                    this._fontWidthConstants = fontWidthConstants;
+                    const getReceivedData = (opts: ITabsAdaptiveButtonsOptions) => {
+                        this._prepareItems(opts);
+                        this._moreButtonWidth = this._getTextWidth(MORE_BUTTON_TEXT, 'm');
+                        resolve({
+                            items: this._items,
+                            fontWidthConstants
+                        });
+                    };
+
+                    if (options.items) {
+                        this._items = options.items;
+                        getReceivedData(options);
+                    } else if (options.source) {
+                        this._loadItems(options.source).then(() => {
+                            getReceivedData(options);
+                        });
+                    }
+                });
+            });
+        }
+    }
+
+    protected _beforeUpdate(newOptions?: ITabsAdaptiveButtonsOptions): void {
+        if (newOptions.source && newOptions.source !== this._options.source) {
+            this._loadItems(newOptions.source).then(() => {
+                this._prepareItems(newOptions);
+            });
+        }
+
+        const isItemsChanged = newOptions.items && newOptions.items !== this._options.items;
+        const isContainerWidthChanged = newOptions.containerWidth !== this._options.containerWidth;
+
+        if (isItemsChanged) {
+            this._items = newOptions.items;
+        }
+
+        if (isItemsChanged || isContainerWidthChanged) {
+            this._prepareItems(newOptions);
+        }
+    }
+
+    protected _selectedKeyHandler(event: SyntheticEvent<Event>, key: string): void {
+        this._notify('selectedKeyChanged', [key]);
+    }
+
+    private _loadItems(source: SbisService): Promise<void> {
+        this._crudWrapper = new CrudWrapper({
+            source
+        });
+        return this._crudWrapper.query({}).then((items: RecordSet) => {
+            this._items = items;
+        });
+    }
+
+    private _prepareItems(options: ITabsAdaptiveButtonsOptions): void {
+        this._items.forEach((item) => {
+            item.set('align', options.align);
+        });
+        this._calcVisibleItems(this._items, options);
+    }
+
+    private _calcVisibleItems(items: RecordSet<object>, options: ITabsAdaptiveButtonsOptions): void {
+        this._lastIndex = this._getLastTabIndex(items, options);
+        const clonedItems = items.clone();
+        const oldRawData = clonedItems.getRawData();
+        if (options.align === 'right') {
+            oldRawData.reverse();
+        }
+        const rawData = oldRawData.slice(0, this._lastIndex + 1);
+        // чтобы ужималась
+        rawData[this._lastIndex].isMainTab = true;
+        if (options.align === 'right') {
+            rawData.reverse();
+        }
+        this._visibleItems = clonedItems;
+        this._visibleItems.setRawData(rawData);
+    }
+
+    private _getLastTabIndex(items: RecordSet<object>, options: ITabsAdaptiveButtonsOptions): number {
+        // находим индекс последней уместившейся вкладки с учетом текста, отступов и разделителей.
+        let width = 0;
+        let indexLast = 0;
+        const arrWidth = this._getItemsWidth(items, options.displayProperty);
+        if (options.align === 'right') {
+            arrWidth.reverse();
+        }
+        while (width < options.containerWidth && indexLast !== items.getCount()) {
+            width += arrWidth[indexLast];
+            indexLast++;
+        }
+        indexLast -= 2;
+
+        if (indexLast === arrWidth.length - 1) {
+            return indexLast;
+        }
+
+        const currentWidth = arrWidth.reduce((sum, current) => {
+            return sum + current;
+        }, 0);
+
+        const minWidth = MIN_WIDTH + MARGIN;
+        if (indexLast === arrWidth.length - 2) {
+            const width = currentWidth - arrWidth[arrWidth.length - 1] + minWidth;
+            if (width < options.containerWidth) {
+                indexLast++;
+                return indexLast;
+            } else {
+                indexLast = this._getLastVisibleItemIndex(indexLast, arrWidth, currentWidth, options.containerWidth);
+            }
+        }
+
+        if (indexLast < arrWidth.length - 2) {
+            indexLast = this._getLastVisibleItemIndex(indexLast, arrWidth, currentWidth, options.containerWidth);
+        }
+        return indexLast;
+    }
+
+    private _getLastVisibleItemIndex(lastIndex: number,  arrWidth: number[],
+                                     currentWidth: number, containerWidth: number): number {
+        let i = arrWidth.length - 1;
+        let indexLast = lastIndex;
+        let width = currentWidth;
+        while (i !== lastIndex) {
+            width = width - arrWidth[i];
+            i--;
+        }
+        width = width + this._moreButtonWidth + MIN_WIDTH + MARGIN + PADDING_OF_MORE_BUTTON;
+        indexLast++;
+        while (width > containerWidth) {
+            indexLast--;
+            width = width - arrWidth[lastIndex];
+        }
+        return indexLast;
+    }
+
+    private _getItemsWidth(items: RecordSet<object>, displayProperty: string): number[] {
+        const widthArray = [];
+        for (let i = 0; i < items.getCount(); i++) {
+            widthArray.push(this._getTextWidth(items.at(i).get(displayProperty)));
+        }
+        return widthArray;
+    }
+
+    private _getTextWidth(text: string, size: string  = 'l'): number {
+       return getFontWidth(text, size, this._fontWidthConstants);
+    }
+
+    static getDefaultOptions(): Partial<ITabsAdaptiveButtonsOptions> {
+        return {
+            align: 'right',
+            displayProperty: 'title'
+        };
+    }
+}
+
+export default AdaptiveButtons;
--- Controls/_tabs/Buttons.ts
+++ Controls/_tabs/Buttons.ts
@@ -1,7 +1,7 @@
 /**
  * Created by kraynovdo on 25.01.2018.
  */
-import {Control, TemplateFunction} from 'UI/Base';
+import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import {CrudWrapper} from 'Controls/dataSource';
 import * as cInstance from 'Core/core-instance';
 import {RecordSet} from 'Types/collection';
@@ -15,6 +15,322 @@ import { constants } from 'Env/Env';
 
 import TabButtonsTpl = require('wml!Controls/_tabs/Buttons/Buttons');
 import ItemTemplate = require('wml!Controls/_tabs/Buttons/ItemTemplate');
+
+import {IItemTemplateOptions} from 'Controls/interface';
+import {IHeading} from "../_spoiler/Heading";
+export interface ITabsButtonsTemplate {
+    readonly '[Controls/_tabs/interface/ITabsButtonsTemplate]': boolean;
+}
+
+/**
+ * Интерфейс для шаблонных опций контрола вкладок.
+ * @interface Controls/_tabs/ITabsTemplateOptions
+ * @public
+ */
+
+export interface ITabsTemplate {
+    readonly '[Controls/_tabs/ITabsTemplate]': boolean;
+}
+
+export interface ITabsTemplateOptions extends IItemTemplateOptions {
+    leftTemplateProperty?: string;
+    rightTemplateProperty?: string;
+    tabSpaceTemplate?: TemplateFunction;
+    itemRightTemplate?: TemplateFunction;
+    itemLeftTemplate?: TemplateFunction;
+}
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#tabSpaceTemplate
+ * @cfg {Content} Шаблон, отображаемый между вкладками.
+ * @default undefined
+ * @remark
+ * Вкладка может быть выровнена по левому и правому краю, это определяется свойством align.
+ * Если у контрола есть левая и правая вкладки, то tabSpaceTemplate будет расположен между ними.
+ * @example
+ * Пример вкладок с шаблоном между ними:
+ * <pre class="brush: html; highlight: [2]">
+ * <Controls.tabs:Buttons
+ *     tabSpaceTemplate=".../spaceTemplate" />
+ * </pre>
+ *
+ * <pre class="brush: html;">
+ * <!-- spaceTemplate.wml -->
+ * <div class="additionalContent">
+ *     <Controls.buttons:Button .../>
+ *     <Controls.buttons:Button .../>
+ *     <Controls.buttons:Button .../>
+ * </div>
+ * </pre>
+ */
+
+/*
+ * @name Controls/_tabs/interface/ITabsButtons#tabSpaceTemplate
+ * @cfg {Content} Contents of the area near the tabs.
+ * @default undefined
+ * @remark
+ * Tab can be left and right aligned, this is determined by the item property 'align'.
+ * If control has left and right tabs then  TabSpaceTemplate will be between them.
+ * @example
+ * Tabs buttons with space template.
+ * <pre>
+ *    <Controls.tabs:Buttons
+ *       .....
+ *       tabSpaceTemplate=".../spaceTemplate'"
+ *       .....
+ *    />
+ * </pre>
+ * spaceTemplate:
+ * <pre>
+ *    <div class="additionalContent">
+ *       <Controls.buttons:Button .../>
+ *       <Controls.buttons:Button .../>
+ *       <Controls.buttons:Button .../>
+ *    </div>
+ * </pre>
+ */
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#itemTemplate
+ * @cfg {Function} Шаблон для рендеринга.
+ * @default Base template 'Controls/tabs:buttonsItemTemplate'
+ * @remark
+ * Чтобы определить шаблон, следует вызвать базовый шаблон 'Controls/tabs:buttonsItemTemplate'.
+ * Шаблон помещается в компонент с помощью тега ws:partial с атрибутом template.
+ * По умолчанию в шаблоне 'Controls/tabs:buttonsItemTemplate' будет отображаться только поле 'title'. Можно изменить формат отображения записей, задав следующие параметры:
+ * <ul>
+ *    <li>displayProperty - определяет поле отображения записи.</li>
+ * <ul>
+ * @example
+ * Вкладки со стандартным шаблоном элемента (шаблоном по умолчанию).
+ *
+ * <pre class="brush: html; highlight: [6,7,8,9,10]">
+ * <Controls.tabs:Buttons
+ *     bind:selectedKey="SelectedKey3"
+ *     keyProperty="id"
+ *     style="additional"
+ *     source="{{_source3}}">
+ *     <ws:itemTemplate>
+ *         <ws:partial template="Controls/tabs:buttonsItemTemplate"
+ *                     item="{{itemTemplate.item}}"
+ *                     displayProperty="caption" />
+ *     </ws:itemTemplate>
+ * </Controls.tabs:Buttons>
+ * </pre>
+ * @see itemTemplateProperty
+ */
+
+/*
+ * @name Controls/_tabs/interface/ITabsButtons#itemTemplate
+ * @cfg {Function} Template for item render.
+ * @default Base template 'Controls/tabs:buttonsItemTemplate'
+ * @remark
+ * To determine the template, you should call the base template 'Controls/tabs:buttonsItemTemplate'.
+ * The template is placed in the component using the ws:partial tag with the template attribute.
+ * By default, the base template 'Controls/tabs:buttonsItemTemplate' will display only the 'title' field. You can change the display of records by setting their values for the following options:
+ * <ul>
+ *    <li>displayProperty - defines the display field of the record.</li>
+ * <ul>
+ * @example
+ * Tabs buttons with item template.
+ * <pre>
+ *    <Controls.tabs:Buttons
+ *                   bind:selectedKey='SelectedKey3'
+ *                   keyProperty="id"
+ *                   style="additional"
+ *                   source="{{_source3}}">
+ *       <ws:itemTemplate>
+ *          <ws:partial template="Controls/tabs:buttonsItemTemplate"
+ *                      item="{{itemTemplate.item}}"
+ *                      displayProperty="caption"/>
+ *       </ws:itemTemplate>
+ *    </Controls.tabs:Buttons>
+ * </pre>
+ */
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#itemTemplateProperty
+ * @cfg {String} Имя поля, которое содержит шаблон отображения элемента.
+ * @default Если параметр не задан, вместо него используется itemTemplate.
+ * @remark
+ * Чтобы определить шаблон, вы должны вызвать базовый шаблон 'Controls/tabs:buttonsItemTemplate'.
+ * Шаблон помещается в компонент с помощью тега ws:partial с атрибутом template.
+ * По умолчанию в шаблоне 'Controls/tabs:buttonsItemTemplate' будет отображаться только поле 'title'. Можно изменить формат отображения записей, задав следующие параметры:
+ * <ul>
+ *    <li>displayProperty - определяет поле отображения записи.</li>
+ * <ul>
+ * @example
+ * Вкладки с шаблоном элемента.
+ * <pre class="brush: html; highlight: [2]">
+ * <Controls.tabs:Buttons
+ *     itemTemplateProperty="myTemplate"
+ *     source="{{_source}}" />
+ * </pre>
+ *
+ * <pre class="brush: html">
+ * <!-- myTemplate.wml -->
+ * <div class="controls-Tabs__item_custom">{{item.get(displayProperty || 'title')}}</div>
+ * </pre>
+ *
+ * <pre class="brush: js">
+ * _source: null,
+ * beforeMount: function() {
+ *    this._source: new Memory({
+ *       keyProperty: 'id',
+ *       data: [
+ *          {id: 1, title: 'I agree'},
+ *          {id: 2, title: 'I not decide'},
+ *          {id: 4, title: 'Will not seem', caption: 'I not agree',  myTemplate: 'wml!.../myTemplate'}
+ *       ]
+ *    });
+ * }
+ * </pre>
+ * @see itemTemplate
+ */
+
+/*
+ * @name Controls/_tabs/interface/ITabsButtons#itemTemplateProperty
+ * @cfg {String} Name of the item property that contains template for item render.
+ * @default If not set, itemTemplate is used instead.
+ * @remark
+ * To determine the template, you should call the base template 'Controls/tabs:buttonsItemTemplate'.
+ * The template is placed in the component using the ws:partial tag with the template attribute.
+ * By default, the base template 'Controls/tabs:buttonsItemTemplate' will display only the 'title' field. You can change the display of records by setting their values for the following options:
+ * <ul>
+ *    <li>displayProperty - defines the display field of the record.</li>
+ * <ul>
+ * @example
+ * Tabs buttons with item template.
+ * <pre>
+ *    <Controls.tabs:Buttons itemTemplateProperty="myTemplate"
+ *                           source="{{_source}}
+ *                           ...>
+ *    </Controls.tabs:Buttons>
+ * </pre>
+ * myTemplate
+ * <pre>
+ *    <div class="controls-Tabs__item_custom">{{item.get(displayProperty || 'title')}}</div>
+ * </pre>
+ * <pre>
+ *    _source: new Memory({
+ *              keyProperty: 'id',
+ *              data: [
+ *                     {id: 1, title: 'I agree'},
+ *                     {id: 2, title: 'I not decide'},
+ *                     {id: 4, title: 'Will not seem', caption: 'I not agree',  myTemplate: 'wml!.../myTemplate'}
+ *              ]
+ *    })
+ * </pre>
+ */
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#rightTemplateProperty
+ * @cfg {String} Имя поля, которое содержит шаблон отображения элемента, находящегося справа от основного содержимого.
+ * @example
+ * <pre class="brush: html; highlight: [2]">
+ * <Controls.tabs:Buttons
+ *     rightTemplateProperty="myTemplate"
+ *     source="{{_source}}" />
+ * </pre>
+ *
+ * <pre class="brush: html">
+ * <!-- myTemplate.wml -->
+ * <div class="{{item.get('icon')}} icon-small controls-icon_style-{{item.get('iconStyle')}}_theme-{{_options.theme}}"></div>
+ * </pre>
+ *
+ * <pre class="brush: js">
+ * _source: null,
+ * beforeMount: function() {
+ *    this._source: new Memory({
+ *       keyProperty: 'id',
+ *       data: [
+ *          {id: 1, title: 'I agree'},
+ *          {id: 2, title: 'I not decide'},
+ *          {id: 4, title: 'Will not seem', caption: 'I not agree',  myTemplate: 'wml!.../myTemplate'}
+ *       ]
+ *    });
+ * }
+ * </pre>
+ * @see leftTemplateProperty
+ */
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#leftTemplateProperty
+ * @cfg {String} Имя поля, которое содержит шаблон отображения элемента, находящегося слева от основного содержимого.
+ * @example
+ * <pre class="brush: html; highlight: [2]">
+ * <Controls.tabs:Buttons
+ *     leftTemplateProperty="myTemplate"
+ *     source="{{_source}}" />
+ * </pre>
+ *
+ * <pre class="brush: html">
+ * <!-- myTemplate.wml -->
+ * <div class="{{item.get('icon')}} icon-small controls-icon_style-{{item.get('iconStyle')}}_theme-{{_options.theme}}"></div>
+ * </pre>
+ *
+ * <pre class="brush: js">
+ * _source: null,
+ * beforeMount: function() {
+ *    this._source: new Memory({
+ *       keyProperty: 'id',
+ *       data: [
+ *          {id: 1, title: 'I agree'},
+ *          {id: 2, title: 'I not decide'},
+ *          {id: 4, title: 'Will not seem', caption: 'I not agree',  myTemplate: 'wml!.../myTemplate'}
+ *       ]
+ *    });
+ * }
+ * </pre>
+ * @see rightTemplateProperty
+ */
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#itemRightTemplate
+ * @cfg {String} Шаблон элемента, находящегося справа от основного содержимого.
+ * @remark
+ * Базовый шаблон itemRightTemplate поддерживает следующие параметры:
+ *
+ * - item {Model} — запись текущей вкладки;
+ * - selected {Boolean} — выбрана ли вкладка, на которой располагается шаблон;
+ * @example
+ * <pre class="brush: html; highlight: [2,3,4,5,6]">
+ * <Controls.tabs:Buttons bind:selectedKey="_mySelectedKey" keyProperty="id" source="{{_mySource}}">
+ *     <ws:itemRightTemplate>
+ *         <ws:if data="{{_counter}}">
+ *             <ws:partial template="{{ _myRightTpl }}" item="{{itemRightTemplate.item}}" counter="{{_counter}}" />
+ *         </ws:if>
+ *     </ws:itemRightTemplate>
+ * </Controls.tabs:Buttons>
+ * </pre>
+ * @see itemLeftTemplate
+ */
+
+/**
+ * @name Controls/_tabs/ITabsTemplateOptions#itemLeftTemplate
+ * @cfg {String} Шаблон элемента, находящегося слева от основного содержимого.
+ * @remark
+ * Базовый шаблон itemLeftTemplate поддерживает следующие параметры:
+ *
+ * - item {Model} — запись текущей вкладки.
+ * - selected {Boolean} — выбрана ли вкладка, на которой располагается шаблон.
+ * @example
+ * <pre class="brush: html;highlight: [2,3,4,5,6]">
+ * <Controls.tabs:Buttons bind:selectedKey="_mySelectedKey" keyProperty="id" source="{{_mySource}}">
+ *     <ws:itemLeftTemplate>
+ *         <ws:if data="{{_counter}}">
+ *             <ws:partial template="{{ _myRightTpl }}" item="{{itemLeftTemplate.item}}" counter="{{_counter}}" />
+ *         </ws:if>
+ *     </ws:itemRightTemplate>
+ * </Controls.tabs:Buttons>
+ * </pre>
+ * @see itemRightTemplate
+ */
+
+
+
+export interface ITabsOptions extends ITabsButtonsOptions, ITabsTemplateOptions {
+}
 
 /**
  * Контрол предоставляет пользователю возможность выбрать между двумя или более вкладками.
@@ -30,6 +346,7 @@ import ItemTemplate = require('wml!Controls/_tabs/Buttons/ItemTemplate');
  * @mixes Controls/interface:ISource
  * @mixes Controls/interface:IItems
  * @mixes Controls/_tabs/interface/ITabsButtons
+ * @mixes Controls/tabs:ITabsTemplateOptions
  * @control
  * @public
  * @category List
@@ -56,9 +373,10 @@ const isTemplateObject = (tmpl: any): boolean => {
     return isTemplate(tmpl);
 };
 
-class TabsButtons extends Control<ITabsButtonsOptions> implements ITabsButtons, IItems {
+class TabsButtons extends Control<ITabsOptions> implements ITabsButtons, IItems, ITabsTemplate {
     readonly '[Controls/_tabs/interface/ITabsButtons]': boolean = true;
     readonly '[Controls/_interface/IItems]': boolean = true;
+    readonly '[Controls/_tabs/ITabsTemplate]': boolean = true;
 
     protected _template: TemplateFunction = TabButtonsTpl;
     protected _defaultItemTemplate: TemplateFunction = ItemTemplate;
@@ -67,7 +385,7 @@ class TabsButtons extends Control<ITabsButtonsOptions> implements ITabsButtons, 
     private _items: RecordSet;
     private _crudWrapper: CrudWrapper;
 
-    protected _beforeMount(options: ITabsButtonsOptions,
+    protected _beforeMount(options: ITabsOptions,
                            context: object,
                            receivedState: IReceivedState): void | Promise<IReceivedState> {
         if (receivedState) {
@@ -86,7 +404,7 @@ class TabsButtons extends Control<ITabsButtonsOptions> implements ITabsButtons, 
         }
     }
 
-    protected _beforeUpdate(newOptions: ITabsButtonsOptions): void {
+    protected _beforeUpdate(newOptions: ITabsOptions): void {
         if (newOptions.source && newOptions.source !== this._options.source) {
             this._initItems(newOptions.source).then((result) => {
                 this._prepareState(result);
@@ -248,7 +566,7 @@ class TabsButtons extends Control<ITabsButtonsOptions> implements ITabsButtons, 
         return false;
     }
 
-    static getDefaultOptions(): ITabsButtonsOptions {
+    static getDefaultOptions(): ITabsOptions {
         return {
             style: 'primary',
             displayProperty: 'title'
