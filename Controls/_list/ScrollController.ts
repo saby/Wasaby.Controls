85b9f76d8a6333d04fbc7563b17ae4ea2b1aec6e
as.avramenko <as.avramenko@tensor.ru>
2020-08-26 09:04:02 +0300

    https://online.sbis.ru/opendoc.html?guid=b19ef2bd-3c88-4093-83ee-3aec29ec998d

--- Controls/_list/ScrollController.ts
+++ Controls/_list/ScrollController.ts
@@ -509,6 +509,11 @@ export default class ScrollController {
                     // @ts-ignore
                     collection.setIndexes(start, stop);
                 }
+                if (start === 0) {
+                    this._notify('disableVirtualNavigation', [], { bubbling: true });
+                } else {
+                    this._notify('enableVirtualNavigation', [], { bubbling: true });
+                }
             }
         }
         if (this._isMounted) {
