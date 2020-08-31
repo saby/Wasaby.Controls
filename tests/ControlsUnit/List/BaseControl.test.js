49fd65676ebe59a740d8d3de5877ee54e9f993de
ma.mochalov <ma.mochalov@tensor.ru>
2020-08-31 16:30:13 +0300

    https://online.sbis.ru/opendoc.html?guid=4d8fa813-b72b-4eb4-8c99-18b8defbac11

--- tests/ControlsUnit/List/BaseControl.test.js
+++ tests/ControlsUnit/List/BaseControl.test.js
@@ -6794,26 +6794,17 @@ define([
             it('isPagingNavigationVisible', () => {
                let isPagingNavigationVisible = lists.BaseControl._private.isPagingNavigationVisible;
 
-               // Известно общее количество записей, записей 0
-               let result = isPagingNavigationVisible(0, 0);
+               // Известно общее количество страниц, страниц 0
+               let result = isPagingNavigationVisible(0);
                assert.isFalse(result, 'paging should not be visible');
 
-               // Известно общее количество записей, записей 10
-               result = isPagingNavigationVisible(10, 2);
+               // Известно общее количество страниц, страниц 1
+               result = isPagingNavigationVisible(1);
+               assert.isFalse(result, 'paging should not be visible');
+
+               // Известно общее количество страниц, страниц 2
+               result = isPagingNavigationVisible(2);
                assert.isTrue(result, 'paging should be visible');
-
-               // Неизвестно общее количество записей, записей, известно текущее количество страниц = 0, hasMore = false
-               result = isPagingNavigationVisible(false, 0);
-               assert.isFalse(result, 'paging should not be visible');
-
-               // Неизвестно общее количество записей, записей, известно текущее количество страниц = 2, hasMore = false
-               result = isPagingNavigationVisible(false, 2);
-               assert.isTrue(result, 'paging should not be visible');
-
-               // Неизвестно общее количество записей, записей, известно текущее количество страниц = 2, hasMore = true
-               result = isPagingNavigationVisible(true, 2);
-               assert.isTrue(result, 'paging should not be visible');
-
             });
 
             describe('getPagingLabelData', function() {
