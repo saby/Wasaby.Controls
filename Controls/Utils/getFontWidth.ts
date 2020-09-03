e710fd60661dc1a8d56a864d42f63680cbe9f73d
Andrew <kr.andrew94@mail.ru>
2020-09-03 10:40:55 +0300

    https://online.sbis.ru/opendoc.html?guid=b91d76f9-7d91-41e3-a8a1-37f4ebcc1e07

--- Controls/Utils/getFontWidth.ts
+++ Controls/Utils/getFontWidth.ts
@@ -1,12 +1,42 @@
 import {detection} from 'Env/Env';
+import { headDataStore } from 'UI/Base';
+import {Logger} from 'UI/Utils';
+import ModuleLoader = require('Controls/Container/Async/ModuleLoader');
 
+const moduleLoader = new ModuleLoader();
 const constants = 'Controls/Utils/FontWidthConstants/';
-const fontSizes = ['xs', 's', 'm', 'l', 'xl', '2xl', '3xl', '4xl', '5xl'];
 
-export const getFontWidth = (text, size, fonts) => {
+let fonts;
+
+export const getFontWidth = (text, size) => {
+    // Инициализируем константы с размерами на клиенте
+    if (isClient()) {
+        if (!fonts) {
+            const browser = getBrowser();
+            const module = constants + browser;
+            if (moduleLoader.isLoaded(module)) {
+                fonts = moduleLoader.loadSync(module)[browser];
+            } else {
+                generateErrorMessage();
+            }
+        }
+    } else {
+        generateErrorMessage();
+    }
+    return getFontWidthWithFonts(fonts, text, size);
+};
+
+const generateErrorMessage = () => {
+    const message = `Получение размеров текста было вызвано до загрузки данных с размерами. Для корректной работы
+        необходимо вызвать метод loadFontWidthConstants`;
+    Logger.error(`Utils/getFontWidth: ${message}`);
+}
+
+const getFontWidthWithFonts = (font, text, size) => {
     let textWidth = 0;
+    console.log(font, text, size);
     for (let i = 0; i < text.length; i++) {
-        textWidth += fonts[size][text[i]];
+        textWidth += font[size][text[i]];
     }
     return textWidth;
 };
@@ -24,21 +54,35 @@ const getBrowser = () => {
     return 'Chrome';
 };
 
-export const loadFontWidthConstants = () => {
+const isClient = () => {
+    return typeof window !== 'undefined';
+};
+
+const getModuleName = () => {
     const browser = getBrowser();
-    let promiseResolver;
-    const module = constants + browser;
-    const promise = new Promise((resolve) => {
-        promiseResolver = resolve;
-    });
-    if (requirejs.defined(module)) {
-        promiseResolver(import(module).then((constant) => {
-            return constant[browser];
-        }));
-    } else {
-        import(module).then((constant) => {
-            promiseResolver(constant[browser]);
+    return constants + browser;
+};
+
+export const loadFontWidthConstants = () => {
+    const module = getModuleName();
+    const getCallbackFunction = (font) => {
+        return getFontWidthWithFonts.bind(null, font);
+    };
+    return new Promise((resolve) => {
+        if (!isClient()) {
+            // положим файл с константами в head, чтобы на клиенте запросить синхронно
+            headDataStore.read('pushDepComponent')(module, true);
+        } else if (fonts) {
+            return resolve(getCallbackFunction(fonts));
+        }
+
+        import(module).then((fontConst) => {
+            // Нельзя константы запоминать в глобальную область видимости.
+            // На сервере запросы с разных браузеров будут обрабатываться в одном скоупе, из-за чего
+            // константы, загруженные для одного браузера, будут применены для другого браузера.
+            // Из функции, загружающей данные, возвращаю колбэк функцию для расчета размеров с забинженными константами
+            const browserFontSizes = fontConst[getBrowser()];
+            resolve(getCallbackFunction(browserFontSizes));
         });
-    }
-    return promise;
+    });
 };
