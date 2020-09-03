689aa5d689af917a5cbc32fcc2c18a9359bb920b
ka.panihin <ka.panihin@tensor.ru>
2020-09-03 16:26:14 +0300

    https://online.sbis.ru/opendoc.html?guid=685620f6-19ce-4854-9b73-db51595460c2

--- tests/ControlsUnit/List/BaseControl.test.js
+++ tests/ControlsUnit/List/BaseControl.test.js
@@ -1,7 +1,8 @@
+/*
 
-/**
+/!**
  * Created by kraynovdo on 23.10.2017.
- */
+ *!/
 define([
    'Types/source',
    'Types/collection',
@@ -1969,10 +1970,10 @@ define([
 
          // два таймаута, первый - загрузка начального рекордсета, второй - на последюущий запрос
          setTimeout(function() {
-            /**
+            /!**
              * _beforeMount will load some items, so _loadedItems will get set. Normally, it will reset in _afterUpdate, but since we don't have lifecycle in tests,
              * we'll reset it here manually.
-             */
+             *!/
             ctrl._loadedItems = null;
 
             lists.BaseControl._private.onScrollLoadEdge(ctrl, 'down');
@@ -5378,7 +5379,7 @@ define([
          disableHeader();
          disableResults();
 
-         /* Список находится в скроллконтейнере, но не личном. До списка лежит контент */
+         /!* Список находится в скроллконтейнере, но не личном. До списка лежит контент *!/
          bc._isScrollShown = true;
          bc._viewPortRect = {
             top: 50
@@ -6512,7 +6513,7 @@ define([
       it('_shouldShowLoadingIndicator', () => {
          const baseControl = new lists.BaseControl();
 
-         /*[position, _loadingIndicatorState, __needShowEmptyTemplate, expectedResult]*/
+         /!*[position, _loadingIndicatorState, __needShowEmptyTemplate, expectedResult]*!/
          const testCases = [
             ['beforeEmptyTemplate', 'up', true,    true],
             ['beforeEmptyTemplate', 'up', false,   true],
@@ -7643,3 +7644,4 @@ define([
       });
    });
 });
+*/
