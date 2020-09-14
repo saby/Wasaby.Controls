60da7a1366c6eab3d67ff32068ff958ae00e11d6
ea.melnikova <ea.melnikova@tensor.ru>
2020-09-14 09:25:44 +0300

    https://online.sbis.ru/opendoc.html?guid=7741ff6a-60d2-4392-9cf7-0a55c75f2dd3

--- Controls/_dropdown/interface/ItemTemplate.ts
+++ Controls/_dropdown/interface/ItemTemplate.ts
@@ -48,9 +48,9 @@
  */
 
 /**
- * @name Controls/dropdown:ItemTemplate#markerVisibility
- * @cfg {Controls/_list/interface/IList/markerVisibility.typedef} Режим отображения {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/list/select/marker/ маркера} активного элемента.
- * @default 'onactivated'
+ * @name Controls/dropdown:ItemTemplate#marker
+ * @cfg {Boolean} Когда опция установлена в значение true, активный элемент будет выделяться {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list/list/select/marker/ маркером}.
+ * @default true
  */
 
 /**
