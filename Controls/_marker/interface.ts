c1b63de117fbe66c28997151b31591386d288348
ka.panihin <ka.panihin@tensor.ru>
2020-09-15 14:39:09 +0300

    https://online.sbis.ru/opendoc.html?guid=011c5839-b19d-4d90-9f9e-b03c1a8b91e1

--- Controls/_marker/interface.ts
+++ Controls/_marker/interface.ts
@@ -11,8 +11,58 @@ export enum Visibility { Visible = 'visible', Hidden = 'hidden', OnActivated = '
  * @public
  * @author Панихин К.А.
  */
-export interface IOptions {
+export interface IOptions extends IMarkerListOptions {
    model: Collection<Model, CollectionItem<Model>>;
-   markerVisibility: TVisibility;
+
+}
+
+/**
+ * Опции для списка, связанные с маркером
+ * @interface Controls/_marker/interface/IMarkerListOptions
+ * @public
+ * @author Панихин К.А.
+ */
+export interface IMarkerListOptions {
+   /**
+    * @typedef {String} TVisibility
+    * @variant visible Маркер отображается всегда, даже если не задан идентификатор элемента в опции {@link markedKey}.
+    * @variant hidden Маркер всегда скрыт.
+    * @variant onactivated Макер отображается при активации списка, например при клике по элементу.
+    */
+
+   /**
+    * @name Controls/_marker/interface/IMarkerListOptions#markerVisibility
+    * @cfg {TVisibility} Режим отображения маркера активного элемента.
+    * @remark
+    * В следующем примере маркер появляется только при активации списка.
+    * @demo Controls-demo/list_new/Marker/OnActivated/Index
+    * @default onactivated
+    * @see markedKey
+    */
+
+   /*ENG
+    * @name Controls/_marker/interface/IMarkerListOptions#markerVisibility
+    * @cfg {String} Determines when marker is visible.
+    * <a href="/materials/Controls-demo/app/Controls-demo%2FList%2FList%2FBasePG">Example</a>.
+    * @variant visible The marker is always displayed, even if the marked key entry is not specified.
+    * @variant hidden The marker is always hidden.
+    * @variant onactivated - The marker is displayed on List activating. For example, when user mark a record.
+    * @default onactivated
+    */
+   markerVisibility?: TVisibility;
+
+   /**
+    * @name Controls/_marker/interface/IMarkerListOptions#markedKey
+    * @cfg {CrudEntityKey} Идентификатор элемента, который выделен маркером.
+    * @demo Controls-demo/List/List/BasePG
+    * @see markerVisibility
+    */
+
+   /*ENG
+    * @name Controls/_marker/interface/IMarkerListOptions#markedKey
+    * @cfg {Number} Identifier of the marked collection item.
+    * @remark
+    * <a href="/materials/Controls-demo/app/Controls-demo%2FList%2FList%2FBasePG">Example</a>.
+    */
    markedKey?: CrudEntityKey;
 }
--- Controls/marker.ts
+++ Controls/marker.ts
@@ -1,4 +1,4 @@
 import { Controller as MarkerController } from 'Controls/_marker/Controller';
-import {TVisibility, Visibility} from 'Controls/_marker/interface';
+import {TVisibility, Visibility, IMarkerListOptions} from 'Controls/_marker/interface';
 
-export { MarkerController, TVisibility, Visibility };
+export { MarkerController, TVisibility, Visibility, IMarkerListOptions };
