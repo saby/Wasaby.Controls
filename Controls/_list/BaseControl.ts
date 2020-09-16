65a6c0b446a86365937ad769236c3df08fb24165
as.avramenko <as.avramenko@tensor.ru>
2020-09-16 15:59:18 +0300

    https://online.sbis.ru/opendoc.html?guid=f90c96e6-032c-404c-94df-cc1b515133d6

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -260,13 +260,9 @@ const _private = {
         }
     },
 
-    updateSearchValue(self, oldOptions, newOptions): void {
-        if (newOptions.searchValue !== oldOptions.searchValue) {
-            _private.doAfterUpdate(self, () => {
-                self._listViewModel.setSearchValue(newOptions.searchValue);
-            });
-            _private.getPortionedSearch(self).reset();
-        }
+    updateSearchValue(self, searchValue): void {
+        self._listViewModel.setSearchValue(searchValue);
+        _private.getPortionedSearch(self).reset();
     },
 
     setReloadingState(self, state): void {
@@ -3410,7 +3406,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
                         this._listViewModel.setCollapsedGroups(collapsedGroups ? collapsedGroups : []);
                         this._needBottomPadding = _private.needBottomPadding(newOptions, this._items, this._listViewModel);
                         _private.updateInitializedItemActions(this, newOptions);
-                        _private.updateSearchValue(this, this._options, newOptions);
+                        _private.updateSearchValue(this, newOptions.searchValue);
                     });
                 });
             } else {
@@ -3418,11 +3414,11 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
                 return _private.reload(self, newOptions).addCallback(() => {
                     this._needBottomPadding = _private.needBottomPadding(newOptions, this._items, this._listViewModel);
                     _private.updateInitializedItemActions(this, newOptions);
-                    _private.updateSearchValue(this, this._options, newOptions);
+                    _private.updateSearchValue(this, newOptions.searchValue);
                 });
             }
         } else {
-            _private.updateSearchValue(this, this._options, newOptions);
+            _private.updateSearchValue(this, newOptions.searchValue);
             if (!isEqual(newOptions.groupHistoryId, this._options.groupHistoryId)) {
                 this._prepareGroups(newOptions, (collapsedGroups) => {
                     self._listViewModel.setCollapsedGroups(collapsedGroups ? collapsedGroups : []);
