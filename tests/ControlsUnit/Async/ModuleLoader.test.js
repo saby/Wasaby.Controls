ec5818164c5bfb7fc34960111273db9b97d0d012
aa.malcev2 <aa.malcev2@tensor.ru>
2020-09-17 15:10:06 +0300

    Remove duplicate tests https://online.sbis.ru/opendoc.html?guid=a4f8697b-a106-4f7a-9486-034c59a201d3

--- tests/ControlsUnit/Async/ModuleLoader.test.js
+++ tests/ControlsUnit/Async/ModuleLoader.test.js
@@ -32,60 +32,6 @@ define([
          IoC.bind('ILogger', originalLogger);
       });
 
-      it('loadAsync success', function () {
-         var ml = new ModuleLoader();
-         return ml.loadAsync('ControlsUnit/Async/TestLibraryAsync').then(function (res) {
-            assert.notEqual(res, undefined, 'Module not loaded async');
-         }, function () {
-            assert.fail('Should not promise faild');
-         });
-      });
-
-      it('loadAsync from library success', function () {
-         var ml = new ModuleLoader();
-         return ml.loadAsync('ControlsUnit/Async/TestModuleAsync:exportFunction').then(function (exportFunction) {
-            assert.notEqual(exportFunction, undefined, 'Module not loaded async');
-            assert.equal(exportFunction('test'), 'test', 'Import from module is broken');
-         }, function() {
-            assert.fail('Should not promise faild');
-         });
-      });
-
-      /**
-       * Проверяем что повторный вызов тоже работает корректно.
-       */
-      it('loadAsync from library second is success', function () {
-         var ml = new ModuleLoader();
-         return ml.loadAsync('ControlsUnit/Async/TestModuleAsync:exportFunction').then(function (exportFunction) {
-            assert.notEqual(exportFunction, undefined, 'Module not loaded async');
-            assert.equal(exportFunction('test'), 'test', 'Import from module is broken');
-         }, function() {
-            assert.fail('Should not promise faild');
-         });
-      });
-      /**
-       * Проверяем что загрузка модуля по другому пути в библиотеке загружает корректный модуль.
-       */
-      it('loadAsync from library another path', function () {
-         var ml = new ModuleLoader();
-         var one = ml.loadAsync('ControlsUnit/Async/TestModuleAsyncTwice:exportFunction').then(function (exportFunction) {
-            assert.notEqual(exportFunction, undefined, 'Module not loaded async');
-            assert.equal(exportFunction('test'), 'test', 'Import from module is broken');
-         }, function() {
-            // если в юнитах упали по таймауту, то см ниже, баг в require под nodejs
-            assert.fail('Should not promise faild');
-         });
-         var two =  ml.loadAsync('ControlsUnit/Async/TestModuleAsyncTwice:exportFunctionTwice').then(function (exportFunction) {
-            assert.notEqual(exportFunction, undefined, 'Module not loaded async');
-            assert.equal(exportFunction('test'), 'testtest', 'Import from module is broken');
-         }, function () {
-            // если в юнитах упали по таймауту, то см ниже, баг в require под nodejs
-            assert.fail('Should not promise faild');
-         });
-
-         return Promise.all([one, two]);
-      });
-
       /* Хак. Потому что сломан require в юнит тестах
          @link https://online.sbis.ru/opendoc.html?guid=b2f93ecc-5b43-4bf1-a2c0-684cc621c314
       */
@@ -135,24 +81,5 @@ define([
             assert.equal(logErrors.length, 1);
          });
       });
-
-      it('loadSync success', function () {
-         var ml = new ModuleLoader();
-         var syncModule = ml.loadSync('ControlsUnit/Async/TestModuleSync');
-         assert.strictEqual(syncModule, TestModuleSync, 'Loaded module is wrong');
-      });
-
-      it('loadSync from library success', function () {
-         var ml = new ModuleLoader();
-         var syncFunction = ml.loadSync('ControlsUnit/Async/TestModuleSync:exportSyncFunction');
-         assert.equal(syncFunction('test'), 'test', 'Import from module is broken');
-      });
-
-      it('loadSync faild', function () {
-         var ml = new ModuleLoader();
-         var error = ml.loadSync('ControlsUnit/Async/TestModuleSyncFail');
-         assert.strictEqual(error, null, 'Failed loaded module must be null');
-      });
-
    });
 });
