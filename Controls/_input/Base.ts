3d140b4dbdf632c4bdcfbb3fb9637a13908c77ac
gd.kovalev <gd.kovalev@tensor.ru>
2020-09-17 17:00:26 +0700

    https://online.sbis.ru/opendoc.html?guid=978348da-30f6-4460-a7b2-e4682f6253b5

--- Controls/_input/Base.ts
+++ Controls/_input/Base.ts
@@ -1100,6 +1100,7 @@ Base.getDefaultOptions = function () {
         autoComplete: 'off',
         fontSize: 'm',
         fontColorStyle: 'default',
+        contrastBackground: true,
         spellCheck: true,
         selectOnClick: false
     };
