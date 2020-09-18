90fe34dfc7dcfc429e83425369368bc5f3d013c7
aa.malcev2 <aa.malcev2@tensor.ru>
2020-09-18 14:49:33 +0300

    Replace direct usage of RequireJS with abstraction level. https://online.sbis.ru/opendoc.html?guid=11da0c1d-55ac-4044-b2af-8c120c6cd1e5

--- Controls/Application/modulesLoader.ts
+++ Controls/Application/modulesLoader.ts
@@ -1,6 +1,7 @@
 /**
  * A modules loader on application level
  */
+import ModulesManager from 'RequireJsLoader/ModulesManager';
 import {Library as library} from 'UI/Utils';
 
 interface IParsedName {
@@ -10,12 +11,18 @@ interface IParsedName {
 
 let cache: Record<string, Promise<unknown>> = {};
 
-/**
- * requirejs.defined работает не корректно. Возвращает true, хотя callback в defined(name, callback) ещё не был вызван.
- */
-const asyncLoadedModules: Record<string, boolean | void> = {};
+let modulesManager: ModulesManager;
+
+function getModulesManager(): ModulesManager {
+    if (!modulesManager) {
+        modulesManager = new ModulesManager(requirejs);
+    }
+    return modulesManager;
+}
 
 function getFromLib<T, K>(lib: T, parsedName: IParsedName): K {
+    // return library.extract(lib, parsedName);
+
     let mod: unknown = lib;
 
     const processed = [];
@@ -35,44 +42,13 @@ function isCached(name: string): boolean {
     return !!cache[name];
 }
 
-function requireSync<T>(name: string): T {
-    return requirejs(name);
-}
-
-function requireAsync<T>(name: string): Promise<T> {
-    return new Promise((resolve, reject) => {
-        requirejs([name], resolve, reject);
-    });
-}
-
 /**
  * Возвращает признак, что модуль загружен
  * @param name Имя модуля в обычном (Foo/bar) или библиотечном (Foo/bar:baz) синтаксисе
  */
 export function isLoaded(name: string): boolean {
-    /*
-     Делаю проверку, что загруженный модуль имеет тип функции, т.к. может быть require уже реально загрузил.
-     Текущее решение, нужно для случая, когда модуль реально возвращает пустой объект, и нужно вернуть ответ,
-     что модуль загружен.
-    */
-
-     const parsedInfo: IParsedName = library.parse(name);
-    if (asyncLoadedModules[parsedInfo.name]) {
-        return true;
-    }
-
-    // Проверяем наличие, что бы не было сообщения в лог
-    if (!requirejs.defined(parsedInfo.name)) {
-        return false;
-    }
-
-    const module = requirejs(parsedInfo.name);
-    if (typeof module !== 'undefined' && (module instanceof Function || Object.keys(module).length > 0)) {
-        asyncLoadedModules[parsedInfo.name] = true;
-        return true;
-    }
-
-    return false;
+    const parsedInfo: IParsedName = library.parse(name);
+    return getModulesManager().isLoaded(parsedInfo.name);
 }
 
 /**
@@ -93,10 +69,12 @@ export function loadAsync<T>(name: string): Promise<T> {
         return cache[parsedInfo.name].then(loadFromModule) as Promise<T>;
     }
 
-    let promiseResult = requireAsync(parsedInfo.name);
+    let promiseResult = getModulesManager()
+        .load<[T]>([parsedInfo.name])
+        .then(([moduleBody]) => moduleBody);
+
     cache[parsedInfo.name] = promiseResult = promiseResult.then((res) => {
         delete cache[parsedInfo.name];
-        asyncLoadedModules[parsedInfo.name] = true;
         return res;
     }, (error) => {
         delete cache[parsedInfo.name];
@@ -112,7 +90,7 @@ export function loadAsync<T>(name: string): Promise<T> {
  */
 export function loadSync<T>(name: string): T {
     const parsedInfo = library.parse(name);
-    const module = requireSync(parsedInfo.name);
+    const module = getModulesManager().loadSync<T>(parsedInfo.name);
     return getFromLib(module, parsedInfo);
 
 }
