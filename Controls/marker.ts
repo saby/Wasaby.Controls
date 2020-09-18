c15d4d489d85f1c22618295eafaf9f6b7710c19b
as.avramenko <as.avramenko@tensor.ru>
2020-09-18 16:15:43 +0300

    https://online.sbis.ru/opendoc.html?guid=9370215f-d9c4-40f5-98ef-4a0c3f43160a

--- Controls/marker.ts
+++ Controls/marker.ts
@@ -1,3 +1,11 @@
+/**
+ * Библиотека, которая предоставляет функционал для отметки маркером
+ * @library Controls/marker
+ * @includes Controller Controls/_marker/Controller
+ * @public
+ * @author Панихин К.А.
+ */
+
 import { Controller as MarkerController } from 'Controls/_marker/Controller';
 import {TVisibility, Visibility, IMarkerListOptions} from 'Controls/_marker/interface';
 
