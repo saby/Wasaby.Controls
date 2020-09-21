245c6efa61e4ace2cd5df143fe1b9656d39efb37
va-kolesov <va.kolesov@tensor.ru>
2020-09-21 19:24:57 +0300

    https://online.sbis.ru/opendoc.html?guid=cf7650dd-b811-454c-adcf-39fb2ae364e6

--- Controls/_list/ScrollContainer/GetHeights.ts
+++ Controls/_list/ScrollContainer/GetHeights.ts
@@ -6,7 +6,8 @@ export function getItemsHeightsData(itemsContainer: HTMLElement): {itemsHeights:
     let itemHeightsData = {itemsHeights: [], itemsOffsets: []};
 
     for (let i = startChildrenIndex, len = itemsContainer.children.length; i < len; i++) {
-        if (!itemsContainer.children[i].classList.contains('controls-ListView__hiddenContainer')) {
+        if (!itemsContainer.children[i].classList.contains('controls-ListView__hiddenContainer') && 
+            !itemsContainer.children[i].classList.contains('js-controls-List_invisible-for-VirtualScroll')) {
             startChildrenIndex = i;
             break;
         }
