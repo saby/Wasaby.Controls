b8a334c96b51a71d53e144db922207fa9a8dce99
koshka-alesya <veronica.desmond@mail.ru>
2020-09-22 13:15:52 +0300

    https://online.sbis.ru/opendoc.html?guid=1e0d29d1-fc1c-49a1-95ca-71be8e18cbd0

--- Controls/_toggle/ButtonGroup.ts
+++ Controls/_toggle/ButtonGroup.ts
@@ -1,30 +1,30 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 // @ts-ignore
 import template = require('wml!Controls/_toggle/ButtonGroup/ButtonGroup');
-import {Record, Model} from 'Types/entity';
+import {Model} from 'Types/entity';
 import {ISingleSelectableOptions} from 'Controls/interface';
+import {IItems} from 'Controls/interface';
 import {SyntheticEvent} from 'Vdom/Vdom';
+import {ButtonTemplate} from 'Controls/buttons';
 
-export interface IButtonGroupOptions extends ISingleSelectableOptions, IControlOptions {
-    items: Record[];
+export interface IButtonGroupOptions extends ISingleSelectableOptions, IControlOptions, IItems {
 }
 /**
  * Контрол представляет собой набор из нескольких взаимосвязанных между собой кнопок. Используется, когда необходимо выбрать один из нескольких параметров.
  * @class Controls/_toggle/ButtonGroup
  * @extends Core/Control
  * @mixes Controls/_interface/ISingleSelectable
+ * @mixes Controls/interface/IItems
  * @control
  * @public
  * @author Бондарь А.В.
  * @category Toggle
  * @demo Controls-demo/toggle/ButtonGroup/Index
- *
- * @name Controls/_toggle/ButtonGroup#items
- * @cfg {Array<Record>} Данные кнопок
  */
 
 class ButtonGroup extends Control<IButtonGroupOptions> {
     protected _template: TemplateFunction = template;
+    protected _buttonTemplate: TemplateFunction = ButtonTemplate;
 
     protected _onItemClick(event: SyntheticEvent<Event>, item: Model): void {
         this._notify('selectedKeyChanged', [item.get(this._options.keyProperty)]);
@@ -32,7 +32,7 @@ class ButtonGroup extends Control<IButtonGroupOptions> {
 
     static _theme: string[] = ['Controls/buttons', 'Controls/toggle', 'Controls/Classes'];
 
-    static getDefaultOptions(): object {
+    static getDefaultOptions(): IButtonGroupOptions {
         return {
             keyProperty: 'id'
         };
