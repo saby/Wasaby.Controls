c595bca55f5d81a0aab1360ef1de10af4f7068f5
yan.sirotkin <yan.sirotkin@tensor.ru>
2020-09-24 17:49:24 +0300

    https://online.sbis.ru/opendoc.html?guid=bd4ec66b-14d8-46e6-bcce-c73fc6382984 Added mixin

--- Controls/_columns/ColumnsView.ts
+++ Controls/_columns/ColumnsView.ts
@@ -7,7 +7,25 @@
  * * <a href="https://github.com/saby/wasaby-controls/blob/rc-20.4000/Controls-default-theme/aliases/_list.less">набор переменных list</a>
  * 
  * @class Controls/columns:View
- * @extends Core/Control
+ * @implements Controls/_interface/IErrorController
+ * @implements Controls/_list/interface/IListNavigation
+ * @mixes Controls/_interface/ISource
+ * @mixes Controls/interface/IItemTemplate
+ * @mixes Controls/interface/IPromisedSelectable
+ * @mixes Controls/_interface/INavigation
+ * @mixes Controls/_interface/IFilterChanged
+ * @mixes Controls/interface/IHighlighter
+ * @mixes Controls/_list/interface/IList
+ * @mixes Controls/_itemActions/interface/IItemActionsOptions
+ * @mixes Controls/interface/IEditableList
+ * @mixes Controls/_interface/ISorting
+ * @mixes Controls/interface/IDraggable
+ * @mixes Controls/interface/IGroupedList
+ * @mixes Controls/_list/interface/IClickableView
+ * @mixes Controls/_list/interface/IReloadableList
+ * @mixes Controls/_list/interface/IMovableList
+ * @mixes Controls/_list/interface/IRemovableList
+ * @mixes Controls/_list/interface/IVirtualScroll
  * @author Авраменко А.С.
  * @public
  * @example
