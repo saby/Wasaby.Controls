516222acbd1c7b6f1c90c83952a23d11058a5f26
ea.melnikova <ea.melnikova@tensor.ru>
2020-09-28 15:40:08 +0300

    https://online.sbis.ru/opendoc.html?guid=d2367fb1-7cd7-4014-9e20-7b1f2bf0a552

--- Controls/_menu/Control.ts
+++ Controls/_menu/Control.ts
@@ -363,7 +363,7 @@ export default class MenuControl extends Control<IMenuControlOptions> implements
 
     private _getSelectionStrategyOptions(): IFlatSelectionStrategyOptions {
         return {
-            items: this._listModel.getCollection()
+            items: this._listModel.getItems()
         };
     }
 
