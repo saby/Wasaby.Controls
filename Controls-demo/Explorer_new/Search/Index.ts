03fb11395eab0b2e6df9b8473f3cffa6bbb8789e
ma.mochalov <ma.mochalov@tensor.ru>
2020-09-29 10:07:41 +0300

    https://online.sbis.ru/opendoc.html?guid=e48187af-eb2c-4bb6-8a02-597460b250cb

--- Controls-demo/Explorer_new/Search/Index.ts
+++ Controls-demo/Explorer_new/Search/Index.ts
@@ -1,52 +1,63 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import * as Template from 'wml!Controls-demo/Explorer_new/Search/Search';
+import * as Template from "wml!Controls-demo/Explorer_new/Search/Search";
 import {Gadgets} from '../DataHelpers/DataCatalog';
 import {Memory} from 'Types/source';
 import * as MemorySource from 'Controls-demo/Explorer/ExplorerMemory';
-import { IColumn } from 'Controls/_grid/interface/IColumn';
-import { TRoot } from 'Controls-demo/types';
-import {IItemAction} from "../../../Controls/_itemActions/interface/IItemAction";
+import {IColumn} from 'Controls/_grid/interface/IColumn';
+import {TRoot} from 'Controls-demo/types';
+import {IItemAction} from '../../../Controls/_itemActions/interface/IItemAction';
 
 export default class extends Control {
-   protected _template: TemplateFunction = Template;
-   protected _viewSource: MemorySource;
-   protected _columns: IColumn[] = Gadgets.getSearchColumns();
-   protected _root: TRoot = null;
-   protected _searchStartingWith: string = 'root';
-   protected _searchStartingWithSource: Memory = null;
-   // tslint:disable-next-line
-   protected _filter: object = { demo: 123 };
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: MemorySource;
+    protected _columns: IColumn[] = Gadgets.getSearchColumns();
+    protected _root: TRoot = 1;
+    protected _startingWith: 'root' | 'current' = 'root';
+    protected _startingWithBtnCaption: 'root' | 'current' = 'current';
+    protected _startingWithSource: Memory = null;
+    private _multiselect: 'visible' | 'hidden' = 'hidden';
+    // tslint:disable-next-line
+    protected _filter: object = {demo: 123};
 
-   protected _itemActions: IItemAction[] = [
-      {
-         id: 0,
-         icon: 'icon-Erase',
-         iconStyle: 'danger',
-         title: 'delete pls',
-         showType: 0,
-         handler: ( item) => {
-            this._children.remover.removeItems([item.getKey()]);
-         }
-      },
-   ]
-
-   protected _beforeMount(): void {
-      this._viewSource = new MemorySource({
-         keyProperty: 'id',
-         data: Gadgets.getSearchData()
-      });
-      this._searchStartingWithSource = new Memory({
-         keyProperty: 'id',
-         data: [
-            {
-               id: 'root', title: 'root'
-            },
-            {
-               id: 'current', title: 'current'
+    protected _itemActions: IItemAction[] = [
+        {
+            id: 0,
+            icon: 'icon-Erase',
+            iconStyle: 'danger',
+            title: 'delete pls',
+            showType: 0,
+            handler: (item) => {
+                this._children.remover.removeItems([item.getKey()]);
             }
-         ]
-      });
-   }
+        },
+    ];
 
-   static _styles: string[] = ['Controls-demo/Controls-demo'];
+    protected _beforeMount(): void {
+        this._viewSource = new MemorySource({
+            keyProperty: 'id',
+            data: Gadgets.getSearchData()
+        });
+        this._startingWithSource = new Memory({
+            keyProperty: 'id',
+            data: [
+                {
+                    id: 'root', title: 'root'
+                },
+                {
+                    id: 'current', title: 'current'
+                }
+            ]
+        });
+    }
+
+    protected _onToggle(): void {
+        this._multiselect = this._multiselect === 'visible' ? 'hidden' : 'visible';
+    }
+
+    protected _updateStartingWith(): void {
+        this._startingWithBtnCaption = this._startingWith;
+        this._startingWith = this._startingWith === 'root' ? 'current' : 'root';
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
