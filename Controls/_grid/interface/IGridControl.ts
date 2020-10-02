2758335d3b0f9d364684b5c71d21d4ef13280746
yan.sirotkin <yan.sirotkin@tensor.ru>
2020-10-02 12:02:34 +0300

    Update demo  showEditArrow

--- Controls/_grid/interface/IGridControl.ts
+++ Controls/_grid/interface/IGridControl.ts
@@ -457,7 +457,7 @@
  * Чтобы стрелка-шеврон отобразилась в прикладном шаблоне колонки, необходимо в опции contentTemplate явно указать позицию стрелки-шеврона.
  * Для этого используется переменная {@link Controls/grid:ColumnTemplate#editArrowTemplate} из области видимости самого шаблона.
  * Пример использования смотреть {@link Controls/grid:ColumnTemplate#contentTemplate тут}
- * @demo Controls-demo/List/Tree/EditArrow
+ * @demo Controls-demo/grid/ShowEditArrow/Index
  * @example
  * <pre>
  *    <ws:partial template="{{editArrowTemplate}}" itemData="{{itemData}}"/>
