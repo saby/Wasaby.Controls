f779cfd57ac60f1034f8d280f99b546dedc52596
sa.pyankov <sa.pyankov@tensor.ru>
2020-10-07 13:48:29 +0700

    Update Observer.ts

--- Controls/_scroll/IntersectionObserver/Observer.ts
+++ Controls/_scroll/IntersectionObserver/Observer.ts
@@ -1,6 +1,6 @@
 import SyntheticEntry from './SyntheticEntry';
 import {IIntersectionObserverObject, IIntersectionObserverOptions} from './Types';
-import {IntersectionObserver} from '../../sizeUtils';
+import {IntersectionObserver} from 'Controls/sizeUtils';
 
 interface IIntersectionContainersMap {
    [key: string]: IIntersectionObserverObject;
