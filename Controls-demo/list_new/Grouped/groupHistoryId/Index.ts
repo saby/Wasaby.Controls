cd4802fcfd8bd46ecb6a74c4cda58e3b807929ee
ma.mochalov <ma.mochalov@tensor.ru>
2020-10-12 14:42:40 +0300

    https://online.sbis.ru/opendoc.html?guid=65f6d31c-f99c-4adc-acb2-3dc9df9cdb99

--- Controls-demo/list_new/Grouped/groupHistoryId/Index.ts
+++ Controls-demo/list_new/Grouped/groupHistoryId/Index.ts
@@ -2,6 +2,7 @@ import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/list_new/Grouped/groupHistoryId/groupHistoryId';
 import {Memory} from 'Types/source';
 import {getGroupedCatalog as getData} from '../../DemoHelpers/DataCatalog';
+import * as Config from 'Env/Config';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
@@ -14,6 +15,10 @@ export default class extends Control {
             data: getData()
         });
         if (receivedState) {
+            if (!Config.UserConfig.getParam('LIST_COLLAPSED_GROUP_MY_BRAND').getResult()) {
+                Config.UserConfig.setParam('LIST_COLLAPSED_GROUP_MY_BRAND', JSON.stringify([]));
+            }
+
             this._groupHistoryId = 'MY_BRAND';
         } else {
             return new Promise((resolve) => {
