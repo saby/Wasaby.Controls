0b678176d19f91ee032469f3973f1731ac36f812
li.mustafin <>
2020-10-14 14:58:38 +0300

    https://online.sbis.ru/opendoc.html?guid=d4a880ac-2e2b-46d4-9843-cce486044787

--- tests/ControlsUnit/Application/addModules.test.ts
+++ tests/ControlsUnit/Application/addModules.test.ts
@@ -2,29 +2,30 @@ import { assert } from 'chai';
 import { Head } from 'Application/Page';
 import * as CApp from 'Controls/Application';
 
-const modules = [
-    'Module/File'
-];
+const module = {
+    name: 'Module/File',
+    path: '/Module/File.js'
+};
 describe('Controls/Application addModules', () => {
     beforeEach(() => {
-        Head.getInstance()._rawElements = {};
+        Head.getInstance()._elements = {};
     });
 
     it('addPrefetchModules', () => {
-        CApp.addPrefetchModules(modules);
+        CApp.addPrefetchModules([module.name]);
         const data = Head.getInstance().getData();
         assert.equal(data.length, 1);
         assert.equal(data[0][0], 'link');
         assert.equal(data[0][1].rel, 'prefetch');
-        assert.equal(data[0][1].href, modules[0]);
+        assert.include(data[0][1].href, module.path);
     });
 
     it('addPreloadModules', () => {
-        CApp.addPreloadModules(modules);
+        CApp.addPreloadModules([module.name]);
         const data = Head.getInstance().getData();
         assert.equal(data.length, 1);
         assert.equal(data[0][0], 'link');
         assert.equal(data[0][1].rel, 'preload');
-        assert.equal(data[0][1].href, modules[0]);
+        assert.include(data[0][1].href, module.path);
     });
 });
