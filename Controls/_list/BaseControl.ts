0d5e053bbff138226ff486a87dee4a0b0d8cb190
Gersasha <gersashaa@gmail.com>
2020-10-15 16:30:27 +0300

    https://online.sbis.ru/opendoc.html?guid=dc226a3d-fe29-48e8-b5fc-ee57deef20e6

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -3382,7 +3382,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
         const sortingChanged = !isEqual(newOptions.sorting, this._options.sorting);
         const sourceChanged = newOptions.source !== this._options.source;
         const recreateSource = navigationChanged || resetPaging || sortingChanged;
-        const searchStarted = !this._options.searchValue && newOptions.searchValue;
+        const searchValueChanged = this._options.searchValue !== newOptions.searchValue;
         const self = this;
         this._needBottomPadding = _private.needBottomPadding(newOptions, this._items, self._listViewModel);
         this._prevRootId = this._options.root;
@@ -3554,8 +3554,10 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
         }
 
         const needReload =
+            // если есть в оциях sourceController, то при смене источника Container/Data загрузит данные
             sourceChanged && !newOptions.sourceController ||
-            filterChanged && !searchStarted ||
+            // Если изменился поиск и фильтр, то данные меняет контроллер поиска через sourceController
+            filterChanged && (!searchValueChanged || !newOptions.sourceController) ||
             sortingChanged ||
             recreateSource;
 
@@ -3623,7 +3625,7 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
             }, INDICATOR_DELAY);
         }
 
-        if (this._options.searchValue !== newOptions.searchValue) {
+        if (searchValueChanged) {
             _private.getPortionedSearch(self).reset();
         }
 
