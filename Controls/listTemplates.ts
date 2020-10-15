b0017f71935530d31c4a70a78129ec5bcdd9c431
an.borisov <an.borisov@tensor.ru>
2020-10-15 13:13:14 +0500

    Добавлен шаблон элемента списка https://online.sbis.ru/opendoc.html?guid=c2a02fbd-f4e6-4264-9e2f-f2e184415a73

--- Controls/listTemplates.ts
+++ Controls/listTemplates.ts
@@ -1,9 +1,11 @@
 import * as TileItemTemplate from 'wml!Controls/_listTemplates/TileItemTemplate';
 import * as ListItemTemplate from 'wml!Controls/_listTemplates/ListItemTemplate';
 import * as TableCellTemplate from 'wml!Controls/_listTemplates/TableCellTemplate';
+import * as RichEditorViewerTemplate from 'wml!Controls/_listTemplates/RichEditorViewerTemplate';
 
 export {
     TileItemTemplate,
     ListItemTemplate,
-    TableCellTemplate
+    TableCellTemplate,
+    RichEditorViewerTemplate
 };
