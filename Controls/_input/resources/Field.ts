63d98a89b203006651463fb61c05d6399fddcebf
au.mironov <au.mironov@tensor.ru>
2020-10-15 12:22:57 +0700

    input: Field некорректно работал под ie https://online.sbis.ru/opendoc.html?guid=fcc04e3e-3cc3-45e2-be06-69df515a6faf

--- Controls/_input/resources/Field.ts
+++ Controls/_input/resources/Field.ts
@@ -351,7 +351,7 @@ class Field<Value, ModelOptions>
     }
 
     protected _keyDownHandler(event: SyntheticEvent<KeyboardEvent>): void {
-        const code: string = event.nativeEvent.code;
+        const code: string = detection.isIE ? event.nativeEvent.key : event.nativeEvent.code;
         const processedKeys: string[] = ['End', 'Home', 'Space', 'ArrowLeft', 'ArrowRight'];
 
         /**
@@ -366,9 +366,10 @@ class Field<Value, ModelOptions>
     }
 
     protected _keyUpHandler(event: SyntheticEvent<KeyboardEvent>): void {
+        const code: string = detection.isIE ? event.nativeEvent.key : event.nativeEvent.code;
         const processedKeys: string[] = ['End', 'Home', 'ArrowLeft', 'ArrowRight', 'ArrowUp', 'ArrowDown'];
 
-        if (processedKeys.includes(event.nativeEvent.code)) {
+        if (processedKeys.includes(code)) {
             this._selectionFromFieldToModel();
         }
     }
