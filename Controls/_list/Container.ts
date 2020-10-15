beebc01eb767f4f23e46647ac85ae6496fe1e4e0
aa.artemeva1 <aa.artemeva1@tensor.ru>
2020-10-15 13:45:44 +0300

    https://online.sbis.ru/opendoc.html?guid=7f136fad-6533-4958-a32b-d9ea31434010

--- Controls/_list/Container.ts
+++ Controls/_list/Container.ts
@@ -37,10 +37,9 @@ export default class ListContainer extends Control<IControlOptions> {
 /**
  * Контрол-контейнер для списка. Передает опции из контекста в список.
  *
- * @remark
- * Контейнер передает в список следующие опции: <a href="/docs/js/Controls/list/View/options/filter/">filter</a>, <a href="/docs/js/Controls/list/View/options/navigation/">navigation</a>, <a href="/docs/js/Controls/list/View/options/sorting/">sorting</a>, <a href="/docs/js/Controls/list/View/options/keyProperty/">keyProperty</a>, <a href="/docs/js/Controls/list/View/options/source/">source</a>, sourceController.
- * 
+ * @remark 
  * Контейнер ожидает поле контекста "dataOptions", которое поставляет Controls/list:DataContainer.
+ * Из поля контекста "dataOptions" контейнер передает в список следующие опции: <a href="/docs/js/Controls/list/View/options/filter/">filter</a>, <a href="/docs/js/Controls/list/View/options/navigation/">navigation</a>, <a href="/docs/js/Controls/list/View/options/sorting/">sorting</a>, <a href="/docs/js/Controls/list/View/options/keyProperty/">keyProperty</a>, <a href="/docs/js/Controls/list/View/options/source/">source</a>, sourceController.
  * 
  * Полезные ссылки:
  * * <a href="/doc/platform/developmentapl/interface-development/controls/list-environment/component-kinds/">руководство разработчика</a>
