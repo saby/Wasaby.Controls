caa5cbec9295c41b090c1a8c3cdb89f5fb3caaf5
pa.averkiev <pa.averkiev@tensor.ru>
2020-10-16 16:42:03 +0300

    https://online.sbis.ru/opendoc.html?guid=c363c40c-8459-477a-9ce2-7805c1c2ec4f

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -5489,7 +5489,7 @@ BaseControl.contextTypes = function contextTypes() {
         isTouch: TouchContextField
     };
 };
-BaseControl._theme = ['Controls/Classes', 'Controls/list'];
+BaseControl._theme = ['Controls/Classes', 'Controls/list', 'Controls/itemActions'];
 
 BaseControl.getDefaultOptions = function() {
     return {
