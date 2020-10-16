b6e426840f8fe590e4f687321f1ce5b2365cf477
pa.averkiev <pa.averkiev@tensor.ru>
2020-10-16 08:30:24 +0300

    https://online.sbis.ru/opendoc.html?guid=98e98a91-0736-402d-a373-57c1e3e21149

--- Controls/_menu/Render.ts
+++ Controls/_menu/Render.ts
@@ -98,9 +98,11 @@ class MenuRender extends Control<IMenuRenderOptions> {
         const item = treeItem.getContents();
         let classes = treeItem.getContentClasses(this._options.theme);
         if (item.get) {
-            classes += ' controls-Menu__row_state_' +
-                (item.get('readOnly') ? 'readOnly' : 'default') +
-                '_theme-' + this._options.theme;
+            if (item.getKey() !== undefined) {
+                classes += ' controls-Menu__row_state_' +
+                    (item.get('readOnly') ? 'readOnly' : 'default') +
+                    '_theme-' + this._options.theme;
+            }
             if (this._isEmptyItem(treeItem) && !this._options.multiSelect) {
                 classes += ' controls-Menu__emptyItem_theme-' + this._options.theme;
             } else {
