3c6539d1a16caba7f529b75c01bf10f9c325d0fe
an.borisov <an.borisov@tensor.ru>
2020-10-19 10:58:52 +0500

    Вынос шаблона элемента списка https://online.sbis.ru/opendoc.html?guid=67135d20-ba41-4bb3-8089-96cd045050f3

--- Controls/listTemplates.ts
+++ Controls/listTemplates.ts
@@ -1,9 +1,11 @@
 import * as TileItemTemplate from 'wml!Controls/_listTemplates/TileItemTemplate';
 import * as ListItemTemplate from 'wml!Controls/_listTemplates/ListItemTemplate';
 import * as TableCellTemplate from 'wml!Controls/_listTemplates/TableCellTemplate';
+import * as FlatItemTemplate from 'wml!Controls/_listTemplates/FlatItemTemplate';
 
 export {
     TileItemTemplate,
     ListItemTemplate,
-    TableCellTemplate
+    TableCellTemplate,
+    FlatItemTemplate
 };
