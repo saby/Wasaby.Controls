472499e2b978a5975419a39f65a0836d93bd8973
an.borisov <an.borisov@tensor.ru>
2020-10-21 18:04:29 +0500

    Изменено название шаблона https://online.sbis.ru/opendoc.html?guid=67135d20-ba41-4bb3-8089-96cd045050f3

--- Controls/listTemplates.ts
+++ Controls/listTemplates.ts
@@ -1,11 +1,11 @@
 import * as TileItemTemplate from 'wml!Controls/_listTemplates/TileItemTemplate';
-import * as ListItemTemplate from 'wml!Controls/_listTemplates/ListItemTemplate';
+import * as ListItemWrapperTemplate from 'wml!Controls/_listTemplates/ListItemWrapperTemplate';
 import * as TableCellTemplate from 'wml!Controls/_listTemplates/TableCellTemplate';
 import * as FlatItemTemplate from 'wml!Controls/_listTemplates/FlatItemTemplate';
 
 export {
     TileItemTemplate,
-    ListItemTemplate,
+    ListItemWrapperTemplate,
     TableCellTemplate,
     FlatItemTemplate
 };
