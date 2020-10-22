56592e282547fe4514e49b467cd7ece48e593277
an.borisov <an.borisov@tensor.ru>
2020-10-22 09:23:50 +0500

    https://online.sbis.ru/opendoc.html?guid=67135d20-ba41-4bb3-8089-96cd045050f3

--- Controls/listTemplates.ts
+++ Controls/listTemplates.ts
@@ -1,11 +1,11 @@
 import * as TileItemTemplate from 'wml!Controls/_listTemplates/TileItemTemplate';
-import * as ListItemWrapperTemplate from 'wml!Controls/_listTemplates/ListItemWrapperTemplate';
+import * as ListItemTemplate from 'wml!Controls/_listTemplates/ListItemTemplate';
 import * as TableCellTemplate from 'wml!Controls/_listTemplates/TableCellTemplate';
-import * as FlatItemTemplate from 'wml!Controls/_listTemplates/FlatItemTemplate';
+import * as ListItemWrapperTemplate from 'wml!Controls/_listTemplates/ListItemWrapperTemplate';
 
 export {
     TileItemTemplate,
-    ListItemWrapperTemplate,
+    ListItemTemplate,
     TableCellTemplate,
-    FlatItemTemplate
+    ListItemWrapperTemplate
 };
