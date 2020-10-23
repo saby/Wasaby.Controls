b77da38dcf66f107ebc41afdedf6ff41415208b0
ea.rodionov1 <ea.rodionov1@tensor.ru>
2020-10-23 09:57:58 +0300

    https://online.sbis.ru/opendoc.html?guid=4fa273fa-a72d-452f-958d-bb3fb944d2e9

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -4477,6 +4477,9 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
         if (this._options.readOnly) {
             return Promise.reject('Control is in readOnly mode.');
         }
+        if (!this._editInPlaceController) {
+            return Promise.resolve();
+        }
         this.showIndicator();
         return this._getEditInPlaceController().cancel().finally(() => {
             this.hideIndicator();
@@ -4490,6 +4493,9 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
     _commitEdit(commitStrategy?: 'hasChanges' | 'all') {
         if (this._options.readOnly) {
             return Promise.reject('Control is in readOnly mode.');
+        }
+        if (!this._editInPlaceController) {
+            return Promise.resolve();
         }
         this.showIndicator();
         return this._getEditInPlaceController().commit(commitStrategy).finally(() => {
