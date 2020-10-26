e28a773eb5f60fb203bb4ac40c719440c5d6b871
ma.mochalov <ma.mochalov@tensor.ru>
2020-10-26 10:08:11 +0300

    https://online.sbis.ru/opendoc.html?guid=7bc0bb01-100a-4771-af9c-9149f06fb5a7

--- Controls-demo/grid/Header/Multiheader/TextOverflow/Index.ts
+++ Controls-demo/grid/Header/Multiheader/TextOverflow/Index.ts
@@ -8,17 +8,10 @@ import {IHeader} from 'Controls-demo/types';
 export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
-    protected _header: IHeader[] = getCountriesStats().getMultiHeader();
+    protected _header: IHeader[] = getCountriesStats().getMultiHeaderForTextOverflow();
     protected _columns: IColumn[] = getCountriesStats().getColumnsWithWidthsForSortingDemo();
 
     protected _beforeMount(): void {
-        for (let i = 0; i < 4; i++) {
-            this._header[1].title += ` ${this._header[1].title}`;
-            this._header[4].title += ` ${this._header[4].title}`;
-        }
-        this._header[1].textOverflow = 'ellipsis';
-        this._header[4].textOverflow = 'none';
-
         this._viewSource = new Memory({
             keyProperty: 'id',
             data: getCountriesStats().getData()
