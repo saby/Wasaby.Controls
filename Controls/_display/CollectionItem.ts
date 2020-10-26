36d0842e924ba509a8487e9c86b2fd1a514465bc
pa.averkiev <pa.averkiev@tensor.ru>
2020-10-26 16:41:39 +0300

    https://online.sbis.ru/opendoc.html?guid=e8ef5bf1-b999-4826-bac0-47160782acd7

--- Controls/_display/CollectionItem.ts
+++ Controls/_display/CollectionItem.ts
@@ -407,7 +407,7 @@ export default class CollectionItem<T> extends mixin<
         }
         this._$animatedForSelection = animated;
         this._nextVersion();
-        this._notifyItemChangeToOwner('animated');
+        this._notifyItemChangeToOwner('animatedForSelection');
     }
 
     /**
