1297e568251df3f7b08fac40283bf31581bf3258
ma.mochalov <ma.mochalov@tensor.ru>
2020-10-27 09:44:09 +0300

    https://online.sbis.ru/opendoc.html?guid=26f3409e-6429-4b8f-a892-c4b0c5da61a7

--- Controls/_dataSource/GroupUtil.ts
+++ Controls/_dataSource/GroupUtil.ts
@@ -38,7 +38,7 @@ var
                     result.callback();
                 }
             }).catch((e) => {
-                Logger.error(`GroupUtil: An error occurred while getting data.\nError: ${e.message}`);
+                Logger.error('GroupUtil: An error occurred while getting data.');
                 result.callback();
             });
             return result;
