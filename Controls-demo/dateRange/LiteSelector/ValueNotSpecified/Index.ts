028208198f37203cd021bf231f6387adc90a56b2
gd.kovalev <gd.kovalev@tensor.ru>
2020-10-29 12:56:42 +0700

    https://online.sbis.ru/opendoc.html?guid=b6cab7c4-6728-4026-802e-9c499ad61e5a

--- Controls-demo/dateRange/LiteSelector/ValueNotSpecified/Index.ts
+++ Controls-demo/dateRange/LiteSelector/ValueNotSpecified/Index.ts
@@ -4,8 +4,8 @@ import template = require("wml!Controls-demo/dateRange/LiteSelector/ValueNotSpec
 class DemoControl extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
 
-    private _startValue: Date = new Date(2019, 1);
-    private _endValue: Date = new Date(2019, 2, 0);
+    protected _startValue: Date = new Date(2019, 1);
+    protected _endValue: Date = new Date(2019, 2, 0);
 
     static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/dateRange/LiteSelector/LiteSelector'];
 }
