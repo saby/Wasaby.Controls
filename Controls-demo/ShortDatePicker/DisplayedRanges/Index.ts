d756ca85c4292cf6d093a60f95f12f6e3d465565
gd.kovalev <gd.kovalev@tensor.ru>
2020-10-29 15:21:26 +0700

    https://online.sbis.ru/opendoc.html?guid=b6cab7c4-6728-4026-802e-9c499ad61e5a

--- Controls-demo/ShortDatePicker/DisplayedRanges/Index.ts
+++ Controls-demo/ShortDatePicker/DisplayedRanges/Index.ts
@@ -5,7 +5,7 @@ class DemoControl extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
 
     protected _startValue: Date = new Date(2020, 0);
-    protected _endValue: Date = new Date(2021, 0);
+    protected _endValue: Date = new Date(2020, 11, 31);
 
     protected _displayedRanges: Date[][] = [
         [new Date(2018, 1), new Date(2020, 1)],
--- Controls-demo/ShortDatePicker/Source/Index.ts
+++ Controls-demo/ShortDatePicker/Source/Index.ts
@@ -5,7 +5,7 @@ import template = require("wml!Controls-demo/ShortDatePicker/Source/Source");
 class DemoControl extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
     protected _startValue: Date = new Date(2020, 0);
-    protected _endValue: Date = new Date(2021, 0);
+    protected _endValue: Date = new Date(2020, 11, 31);
 
     private _source: DateLitePopupSource = new DateLitePopupSource();
 
