594756d073359268fd69eb95ac6126e9c28a9cae
ea.rodionov1 <ea.rodionov1@tensor.ru>
2020-10-30 10:40:11 +0300

    https://online.sbis.ru/opendoc.html?guid=7afab86a-5c27-483f-84a1-e05ae7e67db5

--- Controls-demo/grid/Multiselect/Index.ts
+++ Controls-demo/grid/Multiselect/Index.ts
@@ -1,39 +1,8 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/grid/Multiselect/Multiselect';
-import {Memory} from 'Types/source';
-import {getCountriesStats} from '../DemoHelpers/DataCatalog';
-import { IColumn } from 'Controls/grid';
-
-interface IColumn {
-    displayProperty: string;
-}
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _viewSource: Memory;
-    protected _columns: IColumn[] = getCountriesStats().getColumnsWithoutWidths();
-    private _multiselect: 'visible'|'hidden'|'onhover' = 'visible';
-
-    protected _beforeMount(): void {
-        this._viewSource = new Memory({
-            keyProperty: 'id',
-            data: getCountriesStats().getData()
-        });
-    }
-    protected _onToggle(): void {
-        switch (this._multiselect) {
-            case 'visible':
-               this._multiselect = 'hidden';
-               break;
-            case 'hidden':
-                this._multiselect = 'onhover';
-                break;
-            case 'onhover':
-                this._multiselect = 'visible';
-                break;
-        }
-        this._forceUpdate();
-    }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
