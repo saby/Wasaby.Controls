a3a8a18bcae49396fb78230f6dc666043cbd805d
rn.kondakov <rn.kondakov@tensor.ru>
2020-11-13 12:32:13 +0300

    https://online.sbis.ru/opendoc.html?guid=e8b55b3f-cb4f-4369-9b98-4ee795d5685f

--- tests/ControlsUnit/StickyHeader/StickyHeader.test.js
+++ tests/ControlsUnit/StickyHeader/StickyHeader.test.js
@@ -372,6 +372,7 @@ define([
                offsetHeight: 10
             };
             sinon.stub(component, '_notify');
+            component._updateHeight();
             component._fixationStateChangeHandler('', 'top');
             sinon.assert.calledWith(
                component._notify,
