057a64d00bee255b26706c5f4900e1a0bbd9416b
as.avramenko <as.avramenko@yandex.ru>
2020-11-16 16:54:36 +0300

    https://online.sbis.ru/opendoc.html?guid=a3ddb081-04a0-4c9b-8a04-2cdaa7096d7e

--- Controls/gridNew.ts
+++ Controls/gridNew.ts
@@ -3,7 +3,7 @@ import { default as View } from 'Controls/_gridNew/Grid';
 import * as ItemTemplate from 'wml!Controls/_gridNew/Render/grid/Item';
 import * as ColumnTemplate from 'wml!Controls/_gridNew/Render/grid/Column';
 import * as StickyLadderColumnTemplate from 'wml!Controls/_gridNew/Render/grid/StickyLadderColumn';
-import * as GroupContent from 'wml!Controls/_gridNew/Render/GroupContent';
+import * as GroupTemplate from 'wml!Controls/_gridNew/Render/GroupContent';
 import * as HeaderContent from 'wml!Controls/_gridNew/Render/HeaderCellContent';
 import * as ResultColumnTemplate from 'wml!Controls/_gridNew/Render/ResultsCellContent';
 import * as ResultsTemplate from 'wml!Controls/_gridNew/Render/ResultsCellContent';
@@ -18,7 +18,7 @@ export {
     ResultColumnTemplate,
     ColumnTemplate,
     StickyLadderColumnTemplate,
-    GroupContent,
+    GroupTemplate,
     HeaderContent,
     FooterContent,
     EmptyTemplate
