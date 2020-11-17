fa6415c2e9d2d01d42b26e4d882de22f6353f8b6
am.gerasimov <am.gerasimov@tensor.ru>
2020-11-17 17:41:39 +0300

    https://online.sbis.ru/opendoc.html?guid=0386414a-a621-43c8-a7c3-fd5caeb1396e

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -3853,6 +3853,14 @@ const BaseControl = Control.extend(/** @lends Controls/_list/BaseControl.prototy
             this._groupingLoader = null;
         }
 
+        const isSourceControllerLoadingNow = newOptions.sourceController &&
+                                             newOptions.sourceController.isLoading() &&
+                                             newOptions.sourceController.getState().source !== this._options.source;
+        const needReload =
+            !this._loadedBySourceController && !isSourceControllerLoadingNow &&
+            // если есть в оциях sourceController, то при смене источника Container/Data загрузит данные
+            (sourceChanged || filterChanged || sortingChanged || recreateSource);
+
         const shouldProcessMarker = newOptions.markerVisibility === 'visible'
             || newOptions.markerVisibility === 'onactivated' && newOptions.markedKey !== undefined;
 
