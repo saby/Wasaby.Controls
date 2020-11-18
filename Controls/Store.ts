e2c4bdd33288544aa6fb88eb9a10824e3ad2ca3b
pa.gubin <pa.gubin@tensor.ru>
2020-11-18 18:29:05 +0300

    https://online.sbis.ru/opendoc.html?guid=a1edda27-17b2-4fb0-90a1-cf693207e95c

--- Controls/Store.ts
+++ Controls/Store.ts
@@ -80,13 +80,13 @@ class Store implements IStore {
     }
 
     // обновляем название актуального контекста в зависимости от урла (сейчас это делает OnlineSbisRu/_router/Router)
-    updateStoreContext(contextName: string): void {
-        if ((Store._getState()[Store._getActiveContext()] || {}).searchValue) {
-            this._setValue('searchValue', '');
+    updateStoreContext(contextName: string, isSabyPage?: boolean): void {
+        if (isSabyPage && (Store._getState()[Store._getActiveContext()])) {
+            delete Store._getState()[Store._getActiveContext()];
         }
         Store._setActiveContext(contextName);
 
-        Store._notifySubscribers('_contextName', true);
+        this._notifySubscribers('_contextName', true);
     }
 
     private _setValue(propertyName: string, value: unknown, isGlobal?: boolean): void {
