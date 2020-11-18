dd596bd185b00531aaf1a72d8ce19f61b04070b9
ka.panihin <ka.panihin@tensor.ru>
2020-11-18 11:41:45 +0300

    https://online.sbis.ru/opendoc.html?guid=b8644084-87db-44dd-9223-059d5b5bfc50

--- tests/ControlsUnit/Tree/TreeControl/TreeControl.test.js
+++ tests/ControlsUnit/Tree/TreeControl/TreeControl.test.js
@@ -192,7 +192,8 @@ define([
                },
                setHasMoreStorage: function() {},
                appendItems: function() {},
-               mergeItems: function() {}
+               mergeItems: function() {},
+               getItemBySourceKey: () => undefined
             };
          };
 
@@ -649,7 +650,8 @@ define([
                   return {
                      at: function () {}
                   };
-               }
+               },
+               getItemBySourceKey: () => undefined
             };
          };
 
@@ -738,7 +740,8 @@ define([
             },
             subscribe: () => {},
             unsubscribe: () => {},
-            getCount: () => 2
+            getCount: () => 2,
+            getItemBySourceKey: () => undefined
          };
 
          // Need to know that list notifies when he has been changed after setting new root by treeControl._afterUpdate
@@ -826,7 +829,8 @@ define([
                treeViewModel._model._root = root;
             },
             getRoot: () => treeViewModel._model._root,
-            getCount: () => 1
+            getCount: () => 1,
+            getItemBySourceKey: () => undefined
          };
 
          treeControl._needResetExpandedItems = true;
@@ -915,7 +919,8 @@ define([
             getRoot: () => treeViewModel._model._root,
             getExpandedItems: () => [1, 2],
             getItems: () => items,
-            getCount: () => 2
+            getCount: () => 2,
+            getItemBySourceKey: () => undefined
          };
          treeControl._deepReload = true;
 
@@ -1169,6 +1174,9 @@ define([
                });
             },
             getItemBySourceItem: function () {
+               return null;
+            },
+            getItemBySourceKey: function () {
                return null;
             },
             getCollapsedGroups: () => undefined,
@@ -1780,6 +1788,7 @@ define([
          treeControl.saveOptions(cfg);
          treeControl._children = {
             baseControl: {
+               _options: {},
                getViewModel: function() {
                   return treeGridViewModel;
                },
