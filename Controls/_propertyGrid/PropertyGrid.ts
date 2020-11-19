f2c9be5bead19982d283eb2dea845214603a682b
nu.krukov <zirionneft@gmail.com>
2020-11-19 17:31:50 +0700

    https://online.sbis.ru/opendoc.html?guid=a90bbcb5-8a65-4201-a48d-c2ca3d599a81

--- Controls/_propertyGrid/PropertyGrid.ts
+++ Controls/_propertyGrid/PropertyGrid.ts
@@ -81,9 +81,10 @@ export default class PropertyGridView extends Control<IPropertyGridOptions> {
         nodeProperty: string,
         parentProperty: string,
         editingObject: Record<string, any>,
-        source: IPropertyGridItem[]
+        source: IPropertyGridItem[] | RecordSet<IPropertyGridItem>
     ): Tree<PropertyGridItem> | Collection<PropertyGridItem> {
         const propertyGridItems = this._getPropertyGridItems(source, editingObject);
+
         if (nodeProperty && parentProperty) {
             return new Tree({
                 collection: propertyGridItems,
@@ -124,17 +125,29 @@ export default class PropertyGridView extends Control<IPropertyGridOptions> {
     }
 
     private _getPropertyGridItems(
-        items: IPropertyGridItem[],
-        editingObject: Record<string, any>
+        items: IPropertyGridItem[] | RecordSet<IPropertyGridItem>,
+        editingObject: Record<string, any> | Model
     ): RecordSet<PropertyGridItem> {
-        const itemsWithPropertyValue = items.map((item: IPropertyGridItem): IPropertyGridItem => {
+        const itemsWithPropertyValue = [];
+
+        items.forEach((item: IPropertyGridItem | Model<IPropertyGridItem>): IPropertyGridItem => {
             const sourceItem = object.clone(item);
             const defaultItem = PropertyGridView.getDefaultPropertyGridItem();
-            defaultItem.propertyValue = editingObject[sourceItem.name];
-            return {
+            const nameProperty = (sourceItem instanceof Model) ? sourceItem.get('name') : sourceItem.name;
+            defaultItem.propertyValue = object.getPropertyValue(editingObject, nameProperty);
+
+            if (sourceItem instanceof Model) {
+                sourceItem.each((key: string, value: unknown) => {
+                    defaultItem[key] = value;
+                });
+                itemsWithPropertyValue.push(defaultItem);
+                return;
+            }
+
+            itemsWithPropertyValue.push({
                 ...defaultItem,
                 ...sourceItem
-            };
+            });
         });
 
         return new RecordSet({
@@ -146,10 +159,10 @@ export default class PropertyGridView extends Control<IPropertyGridOptions> {
 
     protected _propertyValueChanged(event: SyntheticEvent<Event>, item: PropertyGridItem, value: any): void {
         const name = item.get(PROPERTY_NAME_FIELD);
-        const editingObjectClone = object.clone(this._options.editingObject);
+        const editingObjectClone = this._options.editingObject instanceof Model ?
+           this._options.editingObject : object.clone(this._options.editingObject);
         const itemClone = item.clone(true);
-
-        editingObjectClone[name] = value;
+        object.setPropertyValue(editingObjectClone, name, value);
         itemClone.set(PROPERTY_VALUE_FIELD, value);
 
         (this._listModel.getCollection().getRecordById(name) as Model).set(PROPERTY_VALUE_FIELD, value);
