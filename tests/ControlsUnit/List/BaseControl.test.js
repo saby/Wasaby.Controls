9b84804eba44aac027ccdf6a5ca2a25474892fd4
ka.panihin <ka.panihin@tensor.ru>
2020-11-19 12:36:17 +0300

    https://online.sbis.ru/opendoc.html?guid=225ea6b1-ea6e-42eb-90bb-547135d0daac

--- tests/ControlsUnit/List/BaseControl.test.js
+++ tests/ControlsUnit/List/BaseControl.test.js
@@ -3907,51 +3907,57 @@ define([
 
       it('startDragNDrop', () => {
          const self = {
-               _options: {
-                  readOnly: false,
-                  itemsDragNDrop: true,
-                  source,
-                  filter: {},
-                  selectedKeys: [],
-                  excludedKeys: [],
-               },
-               _listViewModel: new lists.ListViewModel({
-                  items: new collection.RecordSet({
-                     rawData: data,
-                     keyProperty: 'id'
-                  }),
-                  keyProperty: 'key'
+            _options: {
+               readOnly: false,
+               itemsDragNDrop: true,
+               source,
+               filter: {},
+               selectedKeys: [],
+               excludedKeys: [],
+            },
+            _listViewModel: new lists.ListViewModel({
+               items: new collection.RecordSet({
+                  rawData: data,
+                  keyProperty: 'id'
                }),
-               _registerMouseMove: () => null,
-               _registerMouseUp: () => null
-            },
-            domEvent = {
-               nativeEvent: {},
-               target: {
-                  closest: function() {
-                     return false;
+               keyProperty: 'key'
+            }),
+            _registerMouseMove: () => null,
+            _registerMouseUp: () => null
+         },
+         domEvent = {
+            nativeEvent: {},
+            target: {
+               closest: function() {
+                  return false;
+               }
+            }
+         },
+         itemData = {
+            getContents() {
+               return {
+                  getKey() {
+                     return 2;
                   }
-               }
-            },
-            itemData = {
-               getContents() {
-                  return {
-                     getKey() {
-                        return 2;
-                     }
-                  };
-               }
-            };
-
-         // self._listViewModel.setItems(data);
+               };
+            }
+         };
 
          let notifyCalled = false;
+
          self._notify = function(eventName, args) {
             notifyCalled = true;
             assert.equal(eventName, 'dragStart');
             assert.deepEqual(args[0], [2]);
             assert.equal(args[1], 2);
          };
+
+         Env.compatibility.touch = 1;
+
+         lists.BaseControl._private.startDragNDrop(self, domEvent, itemData);
+         assert.isFalse(notifyCalled, 'On touch device can\'t drag');
+
+         Env.compatibility.touch = 0;
 
          lists.BaseControl._private.startDragNDrop(self, domEvent, itemData);
          assert.isTrue(notifyCalled);
