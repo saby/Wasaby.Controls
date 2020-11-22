7371cc64951bbfd09d60da3e1895037043867446
ma.mochalov <ma.mochalov@tensor.ru>
2020-11-22 12:33:48 +0300

    https://online.sbis.ru/opendoc.html?guid=c64bb7cd-f38c-4e6f-a47b-c55fccd2cab0

--- Controls/_list/BaseControl.ts
+++ Controls/_list/BaseControl.ts
@@ -1673,7 +1673,7 @@ const _private = {
 
                 if (typeof moreMetaCount === 'number' && itemsCount !== moreMetaCount) {
                     _private.prepareFooter(self, self._options, self._sourceController);
-                } else if (typeof moreMetaCount === 'number') {
+                } else if (typeof moreMetaCount === 'number' || moreMetaCount === false) {
                     self._shouldDrawFooter = false;
                 }
             }
