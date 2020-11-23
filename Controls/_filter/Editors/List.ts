532b1b295280633dd60f0eea1cb7467f0d92eab4
ea.melnikova <ea.melnikova@tensor.ru>
2020-11-23 11:44:27 +0300

    https://online.sbis.ru/opendoc.html?guid=0c438210-1d2f-409e-af69-58dc12314d74

--- Controls/_filter/Editors/List.ts
+++ Controls/_filter/Editors/List.ts
@@ -1,6 +1,8 @@
 import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import {SyntheticEvent} from 'Vdom/Vdom';
 import DateRangeTemplate = require('wml!Controls/_filter/Editors/List');
+import * as ColumnTemplate from 'wml!Controls/_filter/Editors/resources/ColumnTemplate';
+import * as clone from 'Core/core-clone';
 
 /**
  * Контрол используют в качестве редактора для выбора единичного значения из списка на {@link https://wi.sbis.ru/doc/platform/developmentapl/interface-development/controls/list-environment/filter-search/filter-view/base-settings/#step-3 панели фильтров}.
@@ -14,11 +16,11 @@ class ListEditor extends Control<IControlOptions> {
     protected _columns: object[] = null;
 
     protected _beforeMount(options?: IControlOptions): void {
-        this._columns = options.columns ? options.columns : [{displayProperty: options.displayProperty}];
+        this._columns = this._getColumns(options.columns, options.displayProperty, options.propertyValue);
     }
 
     protected _beforeUpdate(options?: IControlOptions): void {
-        this._columns = options.columns ? options.columns : [{displayProperty: options.displayProperty}];
+        this._columns = this._getColumns(options.columns, options.displayProperty, options.propertyValue);
     }
 
     protected _handleMarkedKeyChanged(event: SyntheticEvent, value: number): void {
@@ -35,5 +37,15 @@ class ListEditor extends Control<IControlOptions> {
         });
         return item[this._options.displayProperty];
     }
+
+    private _getColumns(columns: object[], displayProperty: string, propertyValue: number|string|unknown[]): object[] {
+        const customColumns = clone(columns) || [];
+        const additionalParameters = {template: ColumnTemplate, selected: propertyValue};
+        const firstColumn = columns ? customColumns.shift() : {displayProperty};
+        customColumns.unshift({...additionalParameters, ...firstColumn});
+        return customColumns;
+    }
+
+    static _theme: string[] = ['Controls/filter', 'Controls/toggle'];
 }
 export default ListEditor;
