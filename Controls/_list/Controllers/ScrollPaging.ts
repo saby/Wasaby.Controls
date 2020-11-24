006d3136553eec4f3528e3b89987599995d8f853
ma.mochalov <ma.mochalov@tensor.ru>
2020-11-24 16:10:42 +0300

    https://online.sbis.ru/opendoc.html?guid=2bb1c398-de7b-49e5-a005-07dd9d0624ec

--- Controls/_list/Controllers/ScrollPaging.ts
+++ Controls/_list/Controllers/ScrollPaging.ts
@@ -113,12 +113,7 @@ export default class ScrollPagingController {
     }
 
     protected updateStateByScrollParams(scrollParams: IScrollParams, hasMoreData: IHasMoreData): void {
-        // На масштабе появляются дробные пиксели в размерах скролл контейнера.
-        // Прибавляем 1.5 пикселя, чтобы избежать неправильных расчетов позиции скролла.
-        // В среднем для корректного рассчета не хватает 0.5px, но для подстраховки добавляем еще 1px.
-        // https://online.sbis.ru/opendoc.html?guid=f921aafd-eb99-4362-a5e7-6f60acf7ee6d
-        const canScrollForward = (scrollParams.clientHeight + scrollParams.scrollTop +
-            SCALE_ROUNDING_ERROR_FIX) < scrollParams.scrollHeight;
+        const canScrollForward = Math.ceil(scrollParams.clientHeight + scrollParams.scrollTop) < scrollParams.scrollHeight;
         const canScrollBackward = scrollParams.scrollTop > 0;
         if (canScrollForward && canScrollBackward) {
             this.handleScrollMiddle(hasMoreData);
