365695cdab1a3525499d67e7056f063861ac2c37
pv.sencov <pv.sencov@tensor.ru>
2020-11-24 19:17:47 +0500

    Переработан юнит для функции load в DataLoader Изменён положительный юнит с минимальным набором (обязательных) параметров (вынес функцию получения результата) Добавлен юнит с отрицательным результатом (504)

--- tests/ControlsUnit/dataSource/error/DataLoader.test.ts
+++ tests/ControlsUnit/dataSource/error/DataLoader.test.ts
@@ -3,29 +3,101 @@ import {deepStrictEqual} from "assert";
 
 import {DataLoader} from 'Controls/_dataSource/error';
 
-const data = [
-    {key: 1, title: 'Ярославль'},
-    {key: 2, title: 'Москва'},
-    {key: 3, title: 'Санкт-Петербург'}
-];
-
 describe('Controls/_dataSource/_error/DataLoader', () => {
     let memory: Memory;
 
     describe('load', () => {
-        it('load function is', async () => {
-            memory = new Memory({
-                data: [...data],
-                keyProperty: 'id'
-            });
-
-            const {sources} = await DataLoader.load([{source: memory}], 2000);
+        const getResult = async (sourcesProp, timeout = 0) => {
+            const {sources, errors} = await DataLoader.load(sourcesProp, timeout);
+            if (errors.length > 0) {
+                const {status} = errors[0];
+                return status;
+            }
             const {source}: { source: ICrud | PrefetchProxy } = sources[0];
             if (source instanceof PrefetchProxy) {
                 const {query} = source.getData();
-                const rawData = query.getRawData();
-                deepStrictEqual(rawData, data, 'Wrong responce data');
+                return query.getRawData();
             }
+            return false;
+        };
+
+        const data = [
+            {key: 1, title: 'Ярославль'},
+            {key: 2, title: 'Москва'},
+            {key: 3, title: 'Санкт-Петербург'}
+        ];
+        memory = new Memory({
+            data: [...data],
+            keyProperty: 'id'
         });
+
+        // only require props
+        const defaultSourcesProp = [
+            {
+                source: memory
+            }
+        ];
+        it(
+            'defaultSourcesProp',
+            async () => {
+                const wrapper = await getResult(defaultSourcesProp, 2000);
+                return deepStrictEqual(wrapper, data, 'Wrong response defaultSourcesProp data');
+            }
+        );
+
+        it(
+            'wrongResponse',
+            async () => {
+                const wrapper = await getResult(defaultSourcesProp);
+                return deepStrictEqual(wrapper, 504, 'This is not the wrong response');
+            }
+        );
+
+        // smokes test много вопросов по этому тесту, возможно он даже и не нужен.
+        // const dataCheckSmokes = [
+        //     {key: 1, title: 'Ярославль'}
+        // ];
+        // const filterButtonSource = [{
+        //     name: 'filterButtonSource',
+        //     value: 'filterButtonSource',
+        //     textValue: 'filterButtonSource'
+        // }];
+        // const fastFilterSource = [{value: '1'}];
+        // const navigation = [{
+        //     source: 'position',
+        //     view: 'pages',
+        //     sourceConfig: 'firstFilterButton'
+        //     // viewConfig: 'id' TODO вопрос в том, надо ли реализовывать так глубоко?
+        // }];
+        // const historyId = 'historyId';
+        // const groupHistoryId = 'groupHistoryId';
+        // const filter = 'Ярославль';
+        // const sorting = '1';
+        // const historyItems = filterButtonSource;
+        // const propStorageId = 'filterButtonSource';
+        // // const filterHistoryLoader = 'filterButtonSource';
+        // const allSourcesProp = [
+        //     {
+        //         source: memory,
+        //         filterButtonSource,
+        //         fastFilterSource,
+        //         navigation,
+        //         historyId,
+        //         groupHistoryId,
+        // TODO Record<string, unknown>, не очень понятно, по ключу или другому параметру должен фильтровать
+        //         filter,
+        //         // sorting,
+        //         historyItems,
+        //         // propStorageId
+        //         // filterHistoryLoader TODO тоже под вопросом...
+        //     }
+        // ];
+        // it(
+        //     'allSourcesProp',
+        //     async () => {
+        //         const wrapper = await getResult(allSourcesProp, 2000);
+        //         return deepStrictEqual(wrapper, 504, 'This is not the wrong response');
+        //     }
+        // );
     });
 });
