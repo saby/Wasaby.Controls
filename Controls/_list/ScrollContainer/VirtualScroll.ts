16935cff5b23997431b804166ea581d75e9ecb4d
va-kolesov <va.kolesov@tensor.ru>
2020-11-25 11:12:14 +0300

    https://online.sbis.ru/opendoc.html?guid=0a6e894e-7866-4390-b82b-909b1b153517

--- Controls/_list/ScrollContainer/VirtualScroll.ts
+++ Controls/_list/ScrollContainer/VirtualScroll.ts
@@ -154,12 +154,8 @@ export default class VirtualScroll {
             this._savedDirection = predicatedDirection;
         }
 
-        if (direction === 'down') {
-            if (!predicatedDirection && triggerState[direction]) {
-                return this.shiftRange(direction);
-            } else {
-                return this._setRange(this._shiftRangeBySegment(direction, count));
-            }
+        if (direction === 'down' && triggerState[direction]) {
+            return this.shiftRange(direction);
         } else {
             return this._setRange(this._shiftRangeBySegment(direction, count));
         }
