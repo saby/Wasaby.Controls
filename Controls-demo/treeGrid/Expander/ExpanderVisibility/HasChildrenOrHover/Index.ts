3ab933e086172c7cd72a8bf4b398be0916a50518
du.ufimcev <du.ufimcev@tensor.ru>
2020-11-26 17:11:31 +0500

    https://online.sbis.ru/opendoc.html?guid=f7a7d66c-e2fb-43ae-8b96-cda6a18e4ce4
    В демку добавил контент, который отображается при раскрытии пустых итемов

--- Controls-demo/treeGrid/Expander/ExpanderVisibility/HasChildrenOrHover/Index.ts
+++ Controls-demo/treeGrid/Expander/ExpanderVisibility/HasChildrenOrHover/Index.ts
@@ -2,13 +2,11 @@ import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/treeGrid/Expander/ExpanderVisibility/HasChildrenOrHover/HasChildrenOrHover';
 import {Memory} from 'Types/source';
 import {Gadgets} from '../../../DemoHelpers/DataCatalog';
-import {IColumn} from 'Controls/grid';
 import {TExpandOrColapsItems} from 'Controls-demo/types';
 
 export default class extends Control {
    protected _template: TemplateFunction = Template;
    protected _viewSource: Memory;
-   protected _columns: IColumn[] = Gadgets.getColumnsForFlat();
    protected _expandedItems: TExpandOrColapsItems = [null];
 
    protected _beforeMount(): void {
