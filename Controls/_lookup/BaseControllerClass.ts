71c2d907700e51eea31f363ebc6fa4f404775acc
Gersasha <gersashaa@gmail.com>
2020-11-26 21:03:44 +0300

    https://online.sbis.ru/opendoc.html?guid=e4837543-f8fe-4682-9e34-962547b7ceeb

--- Controls/_lookup/BaseControllerClass.ts
+++ Controls/_lookup/BaseControllerClass.ts
@@ -39,13 +39,8 @@ export default class LookupBaseControllerClass {
         let keysChanged;
 
         if (hasSelectedKeysInOptions) {
-            if (newOptions.task1180631048) {
-                keysChanged = !isEqual(newOptions.selectedKeys, this._options.selectedKeys) &&
-                              !isEqual(newOptions.selectedKeys, this.getSelectedKeys());
-            } else {
-                keysChanged = !isEqual(newOptions.selectedKeys, this._options.selectedKeys) ||
-                              !isEqual(newOptions.selectedKeys, this.getSelectedKeys());
-            }
+            keysChanged = !isEqual(newOptions.selectedKeys, this._options.selectedKeys) ||
+                          !isEqual(newOptions.selectedKeys, this.getSelectedKeys());
         }
 
         const sourceIsChanged = newOptions.source !== this._options.source;
