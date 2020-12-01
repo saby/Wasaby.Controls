6bbe58d9edc80bf9f61b908709236400905cbd7f
aa.bykanov <itdell@yandex.ru>
2020-12-01 12:49:20 +0300

    https://online.sbis.ru/opendoc.html?guid=dbf30550-2d79-4867-a202-0f5ccaf81aec

--- Controls/browser.ts
+++ Controls/browser.ts
@@ -1,1 +1,8 @@
+/**
+ * Библиотека контролов, которые реализуют базовую раскладку реестра.
+ * @library Controls/browser
+ * @includes IBrowser Controls/_browser/interface/IBrowse
+ * @author Герасимов А.М.
+ */
 export {default as Browser} from 'Controls/_browser/Browser';
+export {default as IBrowser} from 'Controls/_browser/interface/IBrowser';
