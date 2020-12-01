a125106aa23f123503ee07164607207d1be6ab5b
ea.melnikova <ea.melnikova@tensor.ru>
2020-12-01 15:38:27 +0300

    https://online.sbis.ru/opendoc.html?guid=07efdcb3-6fb8-4d59-9efc-5d1c4c9f564b

--- tests/ControlsUnit/browser/Browser.test.ts
+++ tests/ControlsUnit/browser/Browser.test.ts
@@ -246,6 +246,7 @@ describe('Controls/browser:Browser', () => {
                 browser._options.source = options.source;
                 browser._sourceController.updateOptions = () => { return true; };
                 await browser._getSearchController(browser._options);
+                options.searchValue = 'oldFilterValue';
                 await browser._beforeUpdate(options);
                 assert.deepStrictEqual(browser._searchController._options.filter, filter);
             });
