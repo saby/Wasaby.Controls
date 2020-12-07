c6a1ad2930ea4fd67eb630c3ad16b8e2882e58cf
pa.averkiev <pa.averkiev@tensor.ru>
2020-12-07 14:07:17 +0300

    https://online.sbis.ru/opendoc.html?guid=70969cac-5288-4861-b172-55055e9476f3

--- Controls-demo/treeGrid/EditArrow/Index.ts
+++
@@ -1,8 +0,0 @@
-import { Control, TemplateFunction } from 'UI/Base';
-import Template from "wml!Controls-demo/treeGrid/EditArrow/EditArrow";
-
-export default class extends Control {
-    protected _template: TemplateFunction = Template;
-
-    static _styles: string[] = ['Controls-demo/Controls-demo'];
-}
