741bd17ee268a7a157bd61acc42e4708cb173b3c
ma.mochalov <ma.mochalov@tensor.ru>
2020-12-07 12:33:01 +0300

    https://online.sbis.ru/opendoc.html?guid=0c45d8a9-24c7-40b3-9437-7f173195715d

--- Controls-demo/gridNew/DemoHelpers/DataCatalog.ts
+++ Controls-demo/gridNew/DemoHelpers/DataCatalog.ts
@@ -568,7 +568,7 @@ const getCountriesStats = () => {
                 textOverflow
             },
             {
-                title: 'Площадь км2',
+                title: 'Площадь территории км2',
                 textOverflow
             },
             {
