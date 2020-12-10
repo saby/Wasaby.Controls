af8c0bb10360bca5dd67fc64f4501d0e21c31fe6
pa.averkiev <pa.averkiev@tensor.ru>
2020-12-10 09:07:41 +0300

    https://online.sbis.ru/opendoc.html?guid=fc6daaea-fc2f-463e-8852-245b6412a713

--- Controls-demo/grid/ColumnScroll/ScrollEntireColumn/Index.ts
+++ Controls-demo/grid/ColumnScroll/ScrollEntireColumn/Index.ts
@@ -1,5 +1,5 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import * as Template from 'wml!Controls-demo/grid/ColumnScroll/Base/Base';
+import * as Template from 'wml!Controls-demo/grid/ColumnScroll/ScrollEntireColumn/ScrollEntireColumn';
 import {Memory} from 'Types/source';
 import {getCountriesStats} from '../../DemoHelpers/DataCatalog';
 import { IColumn } from 'Controls/grid';
