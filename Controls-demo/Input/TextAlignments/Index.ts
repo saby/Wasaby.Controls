7d5a3934ee46dcdfbe1e83c5aad5c37dd5dd77ee
ps.kolesova <ps.kolesova@tensor.ru>
2020-12-10 11:01:23 +0300

    https://online.sbis.ru/opendoc.html?guid=9442640c-3838-4b4a-973a-eacd74cbb8d5

--- Controls-demo/Input/TextAlignments/Index.ts
+++ Controls-demo/Input/TextAlignments/Index.ts
@@ -2,13 +2,14 @@ import {Control, IControlOptions, TemplateFunction} from 'UI/Base';
 import controlTemplate = require('wml!Controls-demo/Input/TextAlignments/TextAlignments');
 
 class TextAlignments extends Control<IControlOptions> {
-    protected _rightValue = TextAlignments._defaultValue;
-    protected _leftValue = TextAlignments._defaultValue;
-    protected _placeholder = 'Tooltip';
+    protected _rightValue: string = TextAlignments._defaultValue;
+    protected _leftValue: string = TextAlignments._defaultValue;
+    protected _centerValue: string = TextAlignments._defaultValue;
+    protected _placeholder: string = 'Tooltip';
 
     protected _template: TemplateFunction = controlTemplate;
 
-    private static _defaultValue = 'text';
+    private static _defaultValue: string = 'text';
     static _theme: string[] = ['Controls/Classes'];
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
