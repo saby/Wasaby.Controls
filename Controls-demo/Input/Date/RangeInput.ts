7b1c44facc88cd5955bb9db983ef3386f96916e2
ma.mochalov <ma.mochalov@tensor.ru>
2020-12-14 15:05:14 +0300

    https://online.sbis.ru/opendoc.html?guid=10f1379d-7f13-4f46-91a8-fc96873c9651

--- Controls-demo/Input/Date/RangeInput.ts
+++ Controls-demo/Input/Date/RangeInput.ts
@@ -8,6 +8,6 @@ class Range extends Control<IControlOptions> {
    protected _endDate: Date =  new Date(2017, 0, 2, 12, 15, 30, 123);
    protected _mask: string = 'DD.MM.YYYY';
 
-   static _styles: string[] = ['Controls-demo/Input/Date/Range/Range'];
+   static _styles: string[] = ['Controls-demo/Input/Date/Range'];
 }
 export default Range;
