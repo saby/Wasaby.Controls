d7d43e15f192cb32827b45771b6a7144d8621a8a
ka.panihin <ka.panihin@tensor.ru>
2020-12-15 19:35:50 +0300

    https://online.sbis.ru/opendoc.html?guid=3733b23d-d797-483c-b2b0-baf05330502e

--- Controls/_display/Collection.ts
+++ Controls/_display/Collection.ts
@@ -741,6 +741,7 @@ export default class Collection<S, T extends CollectionItem<S> = CollectionItem<
     protected _userStrategies: Array<IUserStrategy<S, T>>;
 
     protected _dragStrategy: Function = DragStrategy;
+    private _wasNotifyAddEventOnStartDrag: boolean = false;
 
     constructor(options: IOptions<S, T>) {
         super(options);
@@ -2239,6 +2240,7 @@ export default class Collection<S, T extends CollectionItem<S> = CollectionItem<
         const strategy = this.getStrategyInstance(this._dragStrategy) as DragStrategy<unknown>;
 
         if (!this.getItemBySourceKey(draggableItem.getContents().getKey())) {
+            this._wasNotifyAddEventOnStartDrag = true;
             this._notifyBeforeCollectionChange();
             this._notifyCollectionChange(
                 IObservable.ACTION_ADD,
@@ -2265,17 +2267,14 @@ export default class Collection<S, T extends CollectionItem<S> = CollectionItem<
         if (strategy) {
             const avatarItem = strategy.avatarItem;
             const avatarIndex = this.getIndex(strategy.avatarItem as T);
-            const avatarKey = avatarItem.getContents().getKey();
 
             this.removeStrategy(this._dragStrategy);
             this._reIndex();
 
-            // Событие remove нужно слать, только когда мы закончили перетаскивание в другом списке,
-            // т.к. только в этом случае мы отправим событие add на начало перетаскивания
-            // Если не найден индекс для перетаскиваемого элемента, значит его удалили прикладники на событие dragEnd
-            if (!this.getCollection().getRecordById(avatarKey) && avatarIndex !== -1) {
+            if (this._wasNotifyAddEventOnStartDrag) {
+                this._wasNotifyAddEventOnStartDrag = false;
                 this._notifyBeforeCollectionChange();
-                this._notifyCollectionChange(IObservable.ACTION_REMOVE, [], 0, [strategy.avatarItem], avatarIndex);
+                this._notifyCollectionChange(IObservable.ACTION_REMOVE, [], 0, [avatarItem], avatarIndex);
                 this._notifyAfterCollectionChange();
             }
         }
