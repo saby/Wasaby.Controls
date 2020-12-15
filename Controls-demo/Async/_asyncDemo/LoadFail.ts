27b385ba7a1a95617357a6317f6b70bb87652ef1
li.mustafin <>
2020-12-15 15:57:29 +0300

    https://online.sbis.ru/opendoc.html?guid=aba6ba18-dcae-40a7-b12f-474f2190f204
    удаление старых демок Async'а

--- Controls-demo/Async/_asyncDemo/LoadFail.ts
+++
@@ -1,6 +0,0 @@
-import Control = require('Core/Control');
-import template = require('wml!Controls-demo/Async/_asyncDemo/LoadFail');
-
-export default class extends Control {
-    protected _template: Function = template;
-}
