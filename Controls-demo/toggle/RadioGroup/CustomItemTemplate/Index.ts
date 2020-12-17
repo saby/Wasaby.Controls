78ce1b15c7d09ae2d3ff9ab8dc3fa407e64b88e3
ps.kolesova <ps.kolesova@tensor.ru>
2020-12-17 15:07:09 +0300

    https://online.sbis.ru/opendoc.html?guid=4022f27c-bd62-4ef6-8d84-3b4995803bb2

--- Controls-demo/toggle/RadioGroup/CustomItemTemplate/Index.ts
+++ Controls-demo/toggle/RadioGroup/CustomItemTemplate/Index.ts
@@ -9,6 +9,7 @@ class CustomItemTemplate extends Control<IControlOptions> {
     protected _selectedKey: string = '1';
     protected _selectedKey2: string = '1';
     protected _source: Memory;
+    protected _source2: Memory;
     protected _displayProperty: string = 'caption';
 
     protected _beforeMount(): void {
@@ -29,6 +30,30 @@ class CustomItemTemplate extends Control<IControlOptions> {
                 title: 'State3',
                 caption: 'Additional caption3'
             }]
+        });
+        this._source2 = new Memory({
+            keyProperty: 'id',
+            data: [
+                {
+                    id: '1',
+                    title: 'Оплата по терминалу в точке продаж',
+                    text: 'Заключите договор с банком о предоставлении в аренду эквайринговых терминалов. Настройте' +
+                        ' их на рабочем месте и получайте оплату банковскими картами от Клиентов.'
+                },
+                {
+                    id: '2',
+                    title: 'Онлайн-оплата через интернет',
+                    text: 'Включите возможность приема онлайн-оплат для выставленных счетов или для бронирования ' +
+                        'столика в ресторане. После успешной оплаты СБИС сам позаботиться о печати кассового чека' +
+                        ' на ККТ и отправит его покупателю.'
+                },
+                {
+                    id: '3',
+                    title: 'QR-код',
+                    text: 'Подключите возможность оплаты по счету в ресторане с помощью графического кода. Для ' +
+                        'оплаты клиенту достаточно открыть приложение банка и сканировать QR-код на счете.'
+                }
+            ]
         });
     }
 
