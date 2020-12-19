ce361110682f7f2c867e5fd571cb271b02133988
ea.rodionov1 <ea.rodionov1@tensor.ru>
2020-12-19 16:50:46 +0300

    https://online.sbis.ru/opendoc.html?guid=8014b2e3-cdbc-4a12-b5e4-f3ed6cbbfa66

--- Controls/_gridNew/GridView.ts
+++ Controls/_gridNew/GridView.ts
@@ -6,7 +6,6 @@ import { GridRow, GridLadderUtil, GridLayoutUtil } from 'Controls/display';
 import * as GridTemplate from 'wml!Controls/_gridNew/Render/grid/GridView';
 import * as GridItem from 'wml!Controls/_gridNew/Render/grid/Item';
 import * as GroupTemplate from 'wml!Controls/_gridNew/Render/GroupTemplate';
-import { prepareEmptyEditingColumns, prepareEmptyColumns } from 'Controls/Utils/GridEmptyTemplateUtil';
 import * as GridIsEqualUtil from 'Controls/Utils/GridIsEqualUtil';
 import { Model } from 'Types/entity';
 import { SyntheticEvent } from 'Vdom/Vdom';
@@ -31,8 +30,6 @@ const GridView = ListView.extend({
 
     _beforeMount(options): void {
         let result = GridView.superclass._beforeMount.apply(this, arguments);
-        this._prepareColumnsForEmptyEditingTemplate = this._prepareColumnsForEmptyEditingTemplate.bind(this);
-        this._prepareColumnsForEmptyTemplate = this._prepareColumnsForEmptyTemplate.bind(this);
 
         if (options.columnScroll && options.columnScrollStartPosition === 'end' && options.isFullGridSupport) {
             // В таблице с горизонтальным скроллом изначально прокрученным в конец используется фейковая таблица.
@@ -152,6 +149,10 @@ const GridView = ListView.extend({
         return this._getGridTemplateColumns(options);
     },
 
+    _isEmpty(): boolean {
+        return !this._listModel?.getCount();
+    },
+
     _onItemMouseMove(event, collectionItem) {
         GridView.superclass._onItemMouseMove.apply(this, arguments);
         this._setHoveredCell(collectionItem.item, event.nativeEvent);
@@ -210,54 +211,6 @@ const GridView = ListView.extend({
             }
             this._notify('hoveredCellChanged', [item, container, hoveredCellIndex, hoveredCellContainer]);
         }
-    },
-
-    // todo Переписать, сделать аналогично Footer/Header/Results
-    _prepareColumnsForEmptyEditingTemplate(columns, topSpacing, bottomSpacing) {
-        return prepareEmptyEditingColumns({
-            gridColumns: this._options.columns,
-            emptyTemplateSpacing: {
-                top: topSpacing,
-                bottom: bottomSpacing
-            },
-            isFullGridSupport: this._options.isFullGridSupport,
-            hasMultiSelect: this._options.multiSelectVisibility !== 'hidden' && this._options.multiSelectPosition === 'default',
-            colspanColumns: columns,
-            itemPadding: this._options.itemPadding || {},
-            theme: this._options.theme,
-            editingBackgroundStyle: (this._options.editingConfig ? this._options.editingConfig.backgroundStyle : 'default')
-        });
-    },
-
-    _prepareColumnsForEmptyTemplate(columns, content, topSpacing, bottomSpacing, theme) {
-        const ladderStickyColumn = GridLadderUtil.getStickyColumn({
-            columns: this._options.columns
-        });
-        let gridColumns;
-        if (ladderStickyColumn) {
-            gridColumns = (ladderStickyColumn.property.length === 2 ? [{}, {}] : [{}]).concat(this._options.columns);
-        } else {
-            gridColumns = this._options.columns;
-        }
-
-        return prepareEmptyColumns({
-            gridColumns,
-            emptyTemplateSpacing: {
-                top: topSpacing,
-                bottom: bottomSpacing
-            },
-            isFullGridSupport: this._options.isFullGridSupport,
-            hasMultiSelect: this._options.multiSelectVisibility !== 'hidden' && this._options.multiSelectPosition === 'default',
-            colspanColumns: content ? [{template: content}] : columns,
-            itemPadding: this._options.itemPadding || {},
-            theme: this._options.theme,
-            afterPrepareCallback(column, index, columns): void {
-                column.classes = 'controls-ListView__empty ' +
-                    'controls-ListView__empty_theme-default ' +
-                    `controls-ListView__empty_topSpacing_${topSpacing}_theme-${theme} ` +
-                    `controls-ListView__empty_bottomSpacing_${bottomSpacing}_theme-${theme}`;
-            }
-        });
     },
 
     _onWrapperMouseEnter: function() {
@@ -352,10 +305,8 @@ const GridView = ListView.extend({
     },
 
     _onHorizontalPositionChangedHandler(e, newScrollPosition: number): void {
-        if (this._columnScrollViewController && this.isColumnScrollVisible()) {
-            this._columnScrollViewController.onPositionChanged(newScrollPosition);
-            this._applyColumnScrollChanges();
-        }
+        this._columnScrollViewController.onPositionChanged(newScrollPosition);
+        this._applyColumnScrollChanges();
     },
 
     _onGridWrapperWheel(e) {
@@ -367,21 +318,17 @@ const GridView = ListView.extend({
 
     _onScrollBarMouseUp(e) {
         e.stopPropagation();
-        if (this._columnScrollViewController && this.isColumnScrollVisible()) {
-            this._columnScrollViewController.onScrollEnded();
-            this._applyColumnScrollChanges();
-        }
+        this._columnScrollViewController.onScrollEnded();
+        this._applyColumnScrollChanges();
     },
 
     _onStartDragScrolling(e, startBy: 'mouse' | 'touch'): void {
-        if (this._columnScrollViewController && this.isColumnScrollVisible()) {
-            this._columnScrollViewController?.startDragScrolling(e, startBy);
-            this._applyColumnScrollChanges();
-        }
+        this._columnScrollViewController?.startDragScrolling(e, startBy);
+        this._applyColumnScrollChanges();
     },
 
     _onMoveDragScroll(e, startBy: 'mouse' | 'touch') {
-        if (this._columnScrollViewController && this.isColumnScrollVisible()) {
+        if (this._columnScrollViewController) {
             const oldPosition = this._columnScrollViewController.getScrollPosition();
             const newPosition = this._columnScrollViewController.moveDragScroll(e, startBy);
 
@@ -392,10 +339,8 @@ const GridView = ListView.extend({
     },
 
     _onStopDragScrolling(e, startBy: 'mouse' | 'touch') {
-        if (this._columnScrollViewController && this.isColumnScrollVisible()) {
-            this._columnScrollViewController?.stopDragScrolling(e, startBy);
-            this._applyColumnScrollChanges();
-        }
+        this._columnScrollViewController?.stopDragScrolling(e, startBy);
+        this._applyColumnScrollChanges();
     },
 
     _resizeHandler(): void {
