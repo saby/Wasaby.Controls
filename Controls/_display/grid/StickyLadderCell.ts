cacb82b4843b10a577e812f5f13bc1e3b7bf9437
pa.averkiev <pa.averkiev@tensor.ru>
2020-12-22 06:37:08 +0300

    https://online.sbis.ru/opendoc.html?guid=47bd2890-be8f-4999-b0f9-669e4a00cb35

--- Controls/_display/grid/StickyLadderCell.ts
+++ Controls/_display/grid/StickyLadderCell.ts
@@ -31,7 +31,6 @@ export default class StickyLadderCell<T, TOwner extends DataRow<T>> extends Cell
 
     getContentClasses(theme: string, cursor: string = 'pointer', templateHighlightOnHover: boolean = true): string {
         let contentClasses = 'controls-Grid__row-main_ladderWrapper';
-        contentClasses += this._getContentSeparatorClasses(theme);
         return contentClasses;
     }
 
