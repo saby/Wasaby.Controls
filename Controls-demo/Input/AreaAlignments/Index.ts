a84fac948100ddf1ccf1d95e6862e0742d441266
pv.sencov <pv.sencov@tensor.ru>
2020-12-26 12:44:06 +0500

    Удалён файл стилей

--- Controls-demo/Input/AreaAlignments/Index.ts
+++ Controls-demo/Input/AreaAlignments/Index.ts
@@ -3,7 +3,7 @@ import controlTemplate = require('wml!Controls-demo/Input/AreaAlignments/AreaAli
 
 class TextAlignments extends Control<IControlOptions> {
     static _theme: string[] = ['Controls/Classes'];
-    static _styles: string[] = ['Controls-demo/Controls-demo', 'Controls-demo/Input/AreaAlignments/AreaAligments'];
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
     private static _defaultValue = 'text';
     protected _rightValue = TextAlignments._defaultValue;
     protected _leftValue = TextAlignments._defaultValue;
