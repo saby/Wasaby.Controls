49251ba5290182b36af50ceec309271212540a3e
aa.bykanov <aa.bykanov@tensor.ru>
2020-12-26 14:11:33 +0300

    https://online.sbis.ru/opendoc.html?guid=76813bc2-9dda-432e-926a-9a7f1314d117

--- Controls/list.ts
+++ Controls/list.ts
@@ -2,7 +2,7 @@
  * Библиотека контролов, которые реализуют плоский список. Список может строиться по данным, полученным из источника. Также можно организовать удаление и перемещение данных.
  * @library Controls/list
  * @includes ItemTemplate Controls/_list/interface/ItemTemplate
- * @includes Controls/_list/interface/IClickableView
+ * @includes IClickableView Controls/_list/interface/IClickableView
  * @includes IBaseItemTemplate Controls/_list/interface/IBaseItemTemplate
  * @includes EmptyTemplate Controls/_list/interface/EmptyTemplate
  * @includes GroupTemplate Controls/_list/interface/GroupTemplate
@@ -19,6 +19,7 @@
  * List library
  * @library Controls/list
  * @includes ItemTemplate Controls/_list/interface/ItemTemplate
+ * @includes IClickableView Controls/_list/interface/IClickableView
  * @includes IBaseItemTemplate Controls/_list/interface/IBaseItemTemplate
  * @includes EmptyTemplate Controls/_list/interface/EmptyTemplate
  * @includes GroupTemplate Controls/_list/interface/GroupTemplate
