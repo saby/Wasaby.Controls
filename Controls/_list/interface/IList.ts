5900f4d40141be998428e7cb8a2b4d3783a2897f
yan.sirotkin <yan.sirotkin@tensor.ru>
2020-12-28 12:43:15 +0300

    https://online.sbis.ru/opendoc.html?guid=706044b9-951c-49b5-9186-1a0e54b7e40a upgrade demo-example

--- Controls/_list/interface/IList.ts
+++ Controls/_list/interface/IList.ts
@@ -119,7 +119,7 @@ export interface IList extends IItemActionsOptions, IMarkerListOptions, IMovable
 /**
  * @name Controls/_list/interface/IList#pagingContentTemplate
  * @cfg {Function} Опция управляет отображением {@link /doc/platform/developmentapl/interface-development/controls/list/navigation/visual-mode/infinite-scrolling/#button-number счетчика непрочитанных сообщений}
- * @demo Controls-demo/list_new/Navigation/Paging/Compact/ContentTemplate/Index
+ * @demo Controls-demo/list_new/Navigation/Paging/End/ContentTemplate/Index
  * @see pagingMode
  */
 
