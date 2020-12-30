ec0ee153b2bea1b6a2e20504adab0ae901d5c0b0
ma.mochalov <ma.mochalov@tensor.ru>
2020-12-30 10:40:32 +0300

    https://online.sbis.ru/opendoc.html?guid=457e6642-254e-45bb-9b3d-c2f6a7821137

--- Controls-demo/list_new/Grouped/HiddenGroupSwitchingGroup/Index.ts
+++ Controls-demo/list_new/Grouped/HiddenGroupSwitchingGroup/Index.ts
@@ -1,51 +1,8 @@
 import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import {Memory} from 'Types/source';
 import {groupConstants} from 'Controls/list';
+import {getGroupedCatalogForSwitchingGroup as getData} from '../../data/HiddenGroup';
 import * as Template from 'wml!Controls-demo/list_new/Grouped/HiddenGroupSwitchingGroup/HiddenGroupSwitchingGroup';
-
-function getData(): Array<{
-    id: number
-    title: string
-    group?: string
-}> {
-    return [
-        {
-            id: 1,
-            title: '1 Первая запись в рекордсете',
-            group: groupConstants.hiddenGroup
-        },
-        {
-            id: 2,
-            title: '2 Вторая запись в рекордсете',
-            group: groupConstants.hiddenGroup
-        },
-        {
-            id: 3,
-            title: '3 Третья запись в рекордсете',
-            group: 'Архив'
-        },
-        {
-            id: 4,
-            title: '4 Четвертая запись в рекордсете',
-            group: 'Архив'
-        },
-        {
-            id: 5,
-            title: '5 Пятая запись в рекордсете',
-            group: groupConstants.hiddenGroup
-        },
-        {
-            id: 6,
-            title: '6 Шестая запись в рекордсете',
-            group: 'Архив'
-        },
-        {
-            id: 7,
-            title: '7 Седьмая запись в рекордсете',
-            group: 'Архив'
-        },
-    ];
-}
 
 export default class extends Control<IControlOptions> {
     protected _template: TemplateFunction = Template;
