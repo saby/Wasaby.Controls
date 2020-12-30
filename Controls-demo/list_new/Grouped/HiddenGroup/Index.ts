5f77b818991cdb856b69c7569de288ccb1cac0c0
ma.mochalov <ma.mochalov@tensor.ru>
2020-12-30 09:59:15 +0300

    https://online.sbis.ru/opendoc.html?guid=457e6642-254e-45bb-9b3d-c2f6a7821137

--- Controls-demo/list_new/Grouped/HiddenGroup/Index.ts
+++ Controls-demo/list_new/Grouped/HiddenGroup/Index.ts
@@ -1,9 +1,61 @@
 import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import {Memory} from 'Types/source';
-
-import {getGroupedCatalogWithHiddenGroup as getData} from '../../DemoHelpers/DataCatalog';
-
+import {groupConstants} from 'Controls/list';
 import * as Template from 'wml!Controls-demo/list_new/Grouped/HiddenGroup/HiddenGroup';
+
+function getData(): Array<{
+    id: number
+    title: string
+    brand?: string
+}> {
+    return [
+        {
+            id: 1,
+            title: 'WebCam X541SA-XO056D',
+            brand: groupConstants.hiddenGroup
+        },
+        {
+            id: 2,
+            title: 'MacBook Pro',
+            brand: 'apple'
+        },
+        {
+            id: 3,
+            title: 'ASUS X751SA-TY124D',
+            brand: 'asus'
+        },
+        {
+            id: 4,
+            title: 'HP 250 G5 (W4N28EA)',
+            brand: 'hp'
+        },
+        {
+            id: 5,
+            title: 'Apple iPad Pro 2016',
+            brand: 'apple'
+        },
+        {
+            id: 6,
+            title: 'KeyBoard 5RTX-zxs',
+            brand: 'unknown'
+        },
+        {
+            id: 7,
+            title: 'MagicMouse 2',
+            brand: groupConstants.hiddenGroup
+        },
+        {
+            id: 8,
+            title: 'iPhone X Max',
+            brand: 'apple'
+        },
+        {
+            id: 9,
+            title: 'ASUS Zenbook F-234',
+            brand: 'asus'
+        }
+    ];
+}
 
 export default class extends Control<IControlOptions> {
     protected _template: TemplateFunction = Template;
--- Controls-demo/list_new/Grouped/HiddenGroupSwitchingGroup/Index.ts
+++ Controls-demo/list_new/Grouped/HiddenGroupSwitchingGroup/Index.ts
@@ -1,10 +1,51 @@
 import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import {Memory} from 'Types/source';
-import { groupConstants } from 'Controls/list';
-import {getGroupedCatalogForSwitchingGroup as getData} from '../../DemoHelpers/DataCatalog';
-
+import {groupConstants} from 'Controls/list';
 import * as Template from 'wml!Controls-demo/list_new/Grouped/HiddenGroupSwitchingGroup/HiddenGroupSwitchingGroup';
 
+function getData(): Array<{
+    id: number
+    title: string
+    group?: string
+}> {
+    return [
+        {
+            id: 1,
+            title: '1 Первая запись в рекордсете',
+            group: groupConstants.hiddenGroup
+        },
+        {
+            id: 2,
+            title: '2 Вторая запись в рекордсете',
+            group: groupConstants.hiddenGroup
+        },
+        {
+            id: 3,
+            title: '3 Третья запись в рекордсете',
+            group: 'Архив'
+        },
+        {
+            id: 4,
+            title: '4 Четвертая запись в рекордсете',
+            group: 'Архив'
+        },
+        {
+            id: 5,
+            title: '5 Пятая запись в рекордсете',
+            group: groupConstants.hiddenGroup
+        },
+        {
+            id: 6,
+            title: '6 Шестая запись в рекордсете',
+            group: 'Архив'
+        },
+        {
+            id: 7,
+            title: '7 Седьмая запись в рекордсете',
+            group: 'Архив'
+        },
+    ];
+}
 
 export default class extends Control<IControlOptions> {
     protected _template: TemplateFunction = Template;
@@ -23,9 +64,10 @@ export default class extends Control<IControlOptions> {
             data: getData()
         });
     }
-    
-    protected _switchGroup(item) {
+
+    protected _switchGroup(item): void {
         item.set('group', item.get('group') === 'Архив' ? groupConstants.hiddenGroup : 'Архив');
     }
+
     static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
