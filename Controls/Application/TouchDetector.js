2dc93a75b8c81c4a86cfb64af69cb40d7e7c872d
aa.kornev <aa.kornev@tensor.ru>
2021-01-12 11:05:30 +0300

    https://online.sbis.ru/opendoc.html?guid=a03f2c37-94c3-45da-8e53-0361df31ee30

--- Controls/Application/TouchDetector.js
+++ Controls/Application/TouchDetector.js
@@ -1,34 +1,36 @@
 define('Controls/Application/TouchDetector', [
    'UI/Base',
    'wml!Controls/Application/TouchDetector/TouchDetector',
-   'Controls/Application/TouchDetectorController'
+   'Env/Touch',
+   'Controls/context'
 ], function(
    Base,
    template,
-   Controller
+   Env,
+   cContext
 ) {
    return Base.Control.extend({
       _template: template,
 
       _beforeMount: function() {
-         this._touchController = new Controller();
-         this._touchObjectContext = this._touchController.createContext();
+         this._touchDetector = Env.TouchDetect.getInstance();
+         this._touchObjectContext = new cContext.TouchContextField(this._touchDetector.isTouch());
+         this._touchChangedHandler = this._touchChangedHandler.bind(this);
+         this._touchDetector.subscribe('touchChanged', this._touchChangedHandler);
       },
-
-      touchHandler: function() {
-         this._touchController.touchHandler();
+      _beforeUnmount: function() {
+         this._touchDetector.unsibescribe('touchChanged', this._touchChangedHandler);
       },
-
-      moveHandler: function() {
-         this._touchController.moveHandler();
+      _touchChangedHandler: function(event, value){
+         this._touchObjectContext.setIsTouch(value);
       },
 
       isTouch: function() {
-         return this._touchController.isTouch();
+         return this._touchDetector.isTouch();
       },
 
       getClass: function() {
-         return this._touchController.getClass();
+         return this._touchDetector.getClass();
       },
 
       // Объявляем функцию, которая возвращает поля Контекста и их значения.
