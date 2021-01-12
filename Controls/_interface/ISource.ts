91e4457ef156b47189665b5cd6da63f54d1c3649
aa.artemeva1 <aa.artemeva1@tensor.ru>
2021-01-12 14:23:04 +0300

    https://online.sbis.ru/opendoc.html?guid=be1262ad-304c-4e24-b455-57aba219a10c

--- Controls/_interface/ISource.ts
+++ Controls/_interface/ISource.ts
@@ -76,7 +76,7 @@ export default interface ISource {
  * </pre>
  * @see Types/source:ICrudPlus
  * @see Types/source:ICrud
- * @see https://wi.sbis.ru/doc/platform/developmentapl/interface-development/data-sources/
+ * @see https://fix-wi.sbis.ru/doc/platform/developmentapl/interface-development/data-sources/
  */
 /*
  * @name Controls/_interface/ISource#source
