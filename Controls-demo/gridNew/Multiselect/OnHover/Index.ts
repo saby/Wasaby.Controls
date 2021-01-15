85e9bda64a3ac3b750f6996081ee43df741f3976
ma.mochalov <ma.mochalov@tensor.ru>
2021-01-15 15:44:37 +0300

    https://online.sbis.ru/opendoc.html?guid=836284ee-ff53-4c51-a8be-813dfd46f72f

---
+++ Controls-demo/gridNew/Multiselect/OnHover/Index.ts
@@ -0,0 +1,23 @@
+import {Control, TemplateFunction} from 'UI/Base';
+import * as Template from 'wml!Controls-demo/gridNew/Multiselect/OnHover/OnHover';
+import {Memory} from 'Types/source';
+import {getCountriesStats} from '../../DemoHelpers/DataCatalog';
+
+interface IColumn {
+    displayProperty: string;
+}
+
+export default class extends Control {
+    protected _template: TemplateFunction = Template;
+    protected _viewSource: Memory;
+    protected _columns: IColumn[] = getCountriesStats().getColumnsWithoutWidths();
+
+    protected _beforeMount(): void {
+        this._viewSource = new Memory({
+            keyProperty: 'id',
+            data: getCountriesStats().getData()
+        });
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
+}
