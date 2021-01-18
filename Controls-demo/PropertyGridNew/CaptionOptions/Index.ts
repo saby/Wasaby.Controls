785d32bd17ada6f2535c508e205aa638d132df41
Elina Zolotova <elinazolotov@gmail.com>
2021-01-18 20:38:56 +0300

    https://online.sbis.ru/opendoc.html?guid=698ea70d-05ed-4131-aeb7-8d2ede71e579

---
+++ Controls-demo/PropertyGridNew/CaptionOptions/Index.ts
@@ -0,0 +1,69 @@
+import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
+import * as template from 'wml!Controls-demo/PropertyGridNew/CaptionOptions/Index';
+import {getEditingObject} from 'Controls-demo/PropertyGridNew/resources/Data';
+
+export default class Demo extends Control<IControlOptions> {
+    protected _template: TemplateFunction = template;
+    protected _editingObject: object;
+    protected _source: object[];
+
+    protected _beforeMount(): void {
+        this._editingObject = getEditingObject();
+        this._source = [
+            {
+                name: 'description',
+                caption: 'Описание',
+                captionOptions: {
+                    fontSize: 'xl'
+                },
+                editorOptions: {
+                    minLines: 3
+                },
+                editorClass: 'controls-demo-pg-text-editor',
+                group: 'text',
+                type: 'text'
+            },
+            {
+                name: 'tileView',
+                caption: 'Список плиткой',
+                captionOptions: {
+                    underline: 'hovered'
+                },
+                group: 'boolean'
+            },
+            {
+                name: 'showBackgroundImage',
+                caption: 'Показывать изображение',
+                group: 'boolean'
+            },
+            {
+                caption: 'URL',
+                name: 'siteUrl',
+                captionOptions: {
+                    required: true
+                },
+                group: 'string'
+            },
+            {
+                caption: 'Источник видео',
+                name: 'videoSource',
+                captionOptions: {
+                    required: true
+                },
+                group: 'string'
+            },
+            {
+                caption: 'Тип фона',
+                captionOptions: {
+                    underline: 'fixed'
+                },
+                name: 'backgroundType',
+                group: 'enum',
+                editorClass: 'controls-demo-pg-enum-editor'
+            }
+        ];
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo',
+        'Controls-demo/PropertyGridNew/PropertyGrid'];
+}
---
+++ Controls-demo/PropertyGridNew/JumpingLabel/Index.ts
@@ -0,0 +1,43 @@
+import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
+import * as template from 'wml!Controls-demo/PropertyGridNew/JumpingLabel/Index';
+import {getEditingObject} from 'Controls-demo/PropertyGridNew/resources/Data';
+
+export default class Demo extends Control<IControlOptions> {
+    protected _template: TemplateFunction = template;
+    protected _editingObject: object;
+    protected _source: object[];
+
+    protected _beforeMount(): void {
+        this._editingObject = getEditingObject();
+        this._source = [
+                {
+                    name: 'description',
+                    caption: 'Описание',
+                    editorOptions: {
+                        minLines: 3
+                    },
+                    editorClass: 'controls-demo-pg-text-editor',
+                    type: 'text'
+                },
+                {
+                    name: 'tileView',
+                    caption: 'Список плиткой',
+                    type: 'boolean'
+                },
+                {
+                    caption: 'URL',
+                    name: 'siteUrl',
+                    type: 'string'
+                },
+                {
+                    caption: 'Тип фона',
+                    name: 'backgroundType',
+                    type: 'enum',
+                    editorClass: 'controls-demo-pg-enum-editor'
+                }
+            ];
+    }
+
+    static _styles: string[] = ['Controls-demo/Controls-demo',
+        'Controls-demo/PropertyGridNew/PropertyGrid'];
+}
