0c2ec2d391ba01eb3abe9d3d722bd6b67b4e658e
ma.mochalov <ma.mochalov@tensor.ru>
2021-01-18 16:41:46 +0300

    https://online.sbis.ru/opendoc.html?guid=836284ee-ff53-4c51-a8be-813dfd46f72f

--- Controls-demo/Explorer_new/Multiselect/Index.ts
+++ Controls-demo/Explorer_new/Multiselect/Index.ts
@@ -1,40 +1,8 @@
 import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/Explorer_new/Multiselect/Multiselect';
-import {Gadgets} from '../DataHelpers/DataCatalog';
-import * as MemorySource from 'Controls-demo/Explorer/ExplorerMemory';
-import { IColumn } from 'Controls/grid';
-import { TRoot, IHeader } from 'Controls-demo/types';
 
 export default class extends Control {
-   protected _template: TemplateFunction = Template;
-   protected _viewSource: MemorySource;
-   protected _columns: IColumn[] = Gadgets.getGridColumns();
-   protected _viewMode: string = 'table';
-   protected _root: TRoot = null;
-   private _multiselect: 'visible'|'hidden'|'onhover' = 'visible';
-   protected _header: IHeader[] = Gadgets.getHeader();
+    protected _template: TemplateFunction = Template;
 
-   protected _beforeMount(): void {
-      this._viewSource = new MemorySource({
-         keyProperty: 'id',
-         data: Gadgets.getData()
-      });
-   }
-
-   protected _onToggle(): void {
-      switch (this._multiselect) {
-         case 'visible':
-            this._multiselect = 'hidden';
-            break;
-         case 'hidden':
-            this._multiselect = 'onhover';
-            break;
-         case 'onhover':
-            this._multiselect = 'visible';
-            break;
-      }
-      this._forceUpdate();
-   }
-
-   static _styles: string[] = ['Controls-demo/Controls-demo'];
+    static _styles: string[] = ['Controls-demo/Controls-demo'];
 }
--- Controls-demo/grid/Multiselect/Base/Index.ts
+++ Controls-demo/grid/Multiselect/Base/Index.ts
@@ -2,7 +2,6 @@ import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/grid/Multiselect/Base/Base';
 import {Memory} from 'Types/source';
 import {getCountriesStats} from '../../DemoHelpers/DataCatalog';
-import { IColumn } from 'Controls/grid';
 
 interface IColumn {
     displayProperty: string;
@@ -12,27 +11,12 @@ export default class extends Control {
     protected _template: TemplateFunction = Template;
     protected _viewSource: Memory;
     protected _columns: IColumn[] = getCountriesStats().getColumnsWithoutWidths();
-    private _multiselect: 'visible'|'hidden'|'onhover' = 'visible';
 
     protected _beforeMount(): void {
         this._viewSource = new Memory({
             keyProperty: 'id',
             data: getCountriesStats().getData()
         });
-    }
-    protected _onToggle(): void {
-        switch (this._multiselect) {
-            case 'visible':
-                this._multiselect = 'hidden';
-                break;
-            case 'hidden':
-                this._multiselect = 'onhover';
-                break;
-            case 'onhover':
-                this._multiselect = 'visible';
-                break;
-        }
-        this._forceUpdate();
     }
 
     static _styles: string[] = ['Controls-demo/Controls-demo'];
