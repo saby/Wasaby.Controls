3fb0eb440dc5daabbf153e7c950082ce9a7cf7e8
yan.sirotkin <yan.sirotkin@tensor.ru>
2021-01-20 11:09:06 +0300

    https://online.sbis.ru/opendoc.html?guid=17b49235-c8fa-455e-ae16-b23db37e24ec Added option

--- Controls/_validate/interfaces/IValidateResult.ts
+++ Controls/_validate/interfaces/IValidateResult.ts
@@ -4,8 +4,13 @@ export default interface IValidateResult {
 }
 
 /**
- * Интерфейс ответа после валидации
- *
+ * Интерфейс ответа после валидации.
  * @interface Controls/_validate/interfaces/IValidateResult
  * @public
+ * @autor Ковалев Г.Д.
  */
+
+ /**
+  * @name Controls/_validate/interfaces/IValidateResult#hasErrors
+  * @cfg {boolean}  есть ли ошибки в результате валидации и собственно сам массив ошибок.
+  */
