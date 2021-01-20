aac97d4766b1188abd749663394cb8f354bbfcaf
yan.sirotkin <yan.sirotkin@tensor.ru>
2021-01-20 11:58:28 +0300

    fixed typos

--- Controls/_validate/interfaces/IValidateResult.ts
+++ Controls/_validate/interfaces/IValidateResult.ts
@@ -1,16 +1,13 @@
-export default interface IValidateResult {
-    [key: number]: boolean;
-    hasErrors?: boolean;
-}
-
 /**
  * Интерфейс ответа после валидации.
- * @interface Controls/_validate/interfaces/IValidateResult
  * @public
  * @autor Красильников А.С.
  */
 
- /**
-  * @name Controls/_validate/interfaces/IValidateResult#hasErrors
-  * @cfg {boolean}  есть ли ошибки в результате валидации и собственно сам массив ошибок.
-  */
+ export default interface IValidateResult {
+/** 
+ * Есть ли ошибки в результате валидации и собственно сам массив ошибок.
+ */
+    [key: number]: boolean;
+    hasErrors?: boolean;
+}
