893929a054159f5d2576854e21346645535af521
ea.melnikova <ea.melnikova@tensor.ru>
2021-01-21 13:40:04 +0300

    https://online.sbis.ru/opendoc.html?guid=3b984991-c2f0-4645-803b-2eed8e57c2ff

--- Controls-demo/PropertyGridNew/Source/Index.ts
+++ Controls-demo/PropertyGridNew/Source/Index.ts
@@ -1,7 +1,5 @@
 import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import * as template from 'wml!Controls-demo/PropertyGridNew/Source/Index';
-import { Model } from 'Types/entity';
-import {default as IPropertyGridItem} from 'Controls/_propertyGrid/IProperty';
 
 export default class Demo extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
@@ -12,35 +10,42 @@ export default class Demo extends Control<IControlOptions> {
         this._editingObject = {
             description: 'This is http://mysite.com',
             tileView: true,
-            showBackgroundImage: true
+            showBackgroundImage: true,
+            showVideo: true
         };
+
         this._source = [
-                {
-                    name: 'description',
-                    editorOptions: {
-                        minLines: 3
-                    },
-                    editorClass: 'controls-demo-pg-text-editor',
-                    group: 'text',
-                    type: 'text',
-                    parent: null,
-                    'parent@': true
+            {
+                name: 'description',
+                editorOptions: {
+                    minLines: 3
                 },
-                {
-                    name: 'tileView',
-                    caption: 'Список плиткой',
-                    group: 'booleanGroup',
-                    parent: null,
-                    'parent@': true
-                },
-                {
-                    name: 'showBackgroundImage',
-                    caption: 'Показывать изображение',
-                    group: 'background',
-                    parent: 'booleanGroup',
-                    'parent@': true
-                }
-            ];
+                editorClass: 'controls-demo-pg-text-editor',
+                group: 'text',
+                type: 'text',
+                '@parent': true,
+                parent: null
+            },
+            {
+                name: 'tileView',
+                caption: 'Список плиткой',
+                group: 'booleanGroup',
+                '@parent': true,
+                parent: null
+            },
+            {
+                name: 'showVideo',
+                caption: 'Показывать видео',
+                '@parent': null,
+                parent: 'tileView'
+            },
+            {
+                name: 'showBackgroundImage',
+                caption: 'Показывать изображение',
+                '@parent': null,
+                parent: 'description'
+            }
+        ];
     }
 
     static _styles: string[] = ['Controls-demo/PropertyGridNew/Editors/HighlightOnHover/Index',
