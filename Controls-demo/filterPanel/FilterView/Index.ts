1e1ce282ba19295039e37c58eb237371e0a2bc0a
ea.melnikova <ea.melnikova@tensor.ru>
2021-01-25 15:25:48 +0300

    https://online.sbis.ru/opendoc.html?guid=0c438210-1d2f-409e-af69-58dc12314d74

--- Controls-demo/filterPanel/FilterView/Index.ts
+++ Controls-demo/filterPanel/FilterView/Index.ts
@@ -6,39 +6,21 @@ import {departments} from 'Controls-demo/filterPanel/resources/DataStorage';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
-    protected _filterButtonData: object[] = [];
+    protected _filterSource: object[] = [];
     protected _source: Memory = null;
-    protected _navigation: object = null;
     protected _filterItems: object[] = null;
 
     protected _beforeMount(): void {
-        this._navigation = {
-            source: 'page',
-            view: 'page',
-            sourceConfig: {
-                pageSize: 20,
-                page: 0,
-                hasMore: false
-            }
-        };
         this._source = new Memory({
             data: departments,
             keyProperty: 'id',
             filter: (item, queryFilter) => {
                 let addToData = true;
-                const emptyFields = {
-                    owner: [],
-                    amount: []
-                };
                 for (const filterField in queryFilter) {
                     if (queryFilter.hasOwnProperty(filterField) && item.get(filterField) && addToData) {
                         const filterValue = queryFilter[filterField];
                         const itemValue = item.get(filterField);
-                        addToData = (itemValue >= filterValue[0] && itemValue <= filterValue[1]) || filterValue.includes(itemValue);
-
-                        if (emptyFields && isEqual(filterValue, emptyFields[filterField])) {
-                            addToData = true;
-                        }
+                        addToData = filterValue.includes(itemValue) || isEqual(filterValue, []);
                     }
                 }
                 return addToData;
@@ -47,24 +29,9 @@ export default class extends Control {
         this._filterItems = [
             { id: 1, title: 'Новиков Д.В.', owner: 'Новиков Д.В.' },
             { id: 2, title: 'Кошелев А.Е.', owner: 'Кошелев А.Е.' },
-            { id: 3, title: 'Субботин А.В.', owner: 'Субботин А.В.' },
-            { id: 4, title: 'Чеперегин А.С.', owner: 'Чеперегин А.С.' },
+            { id: 3, title: 'Субботин А.В.', owner: 'Субботин А.В.' }
         ];
-        this._filterButtonData = [
-            {
-                group: 'Количество сотрудников',
-                name: 'amount',
-                editorTemplateName: 'Controls/filterPanel:NumberRangeEditor',
-                resetValue: [],
-                caption: '',
-                value: [],
-                textValue: '',
-                editorOptions: {
-                    afterEditorTemplate: 'wml!Controls-demo/filterPanel/resources/AfterEditorTemplate',
-                    minValueInputPlaceholder: '0',
-                    maxValueInputPlaceholder: '1 000 000'
-                }
-            },
+        this._filterSource = [
             {
                 group: 'Ответственный',
                 name: 'owner',
@@ -85,15 +52,7 @@ export default class extends Control {
                         }
                     },
                     keyProperty: 'owner',
-                    additionalTextProperty: 'id',
                     displayProperty: 'title',
-                    selectorTemplate: {
-                        templateName: 'Controls-demo/filterPanel/resources/MultiSelectStackTemplate/StackTemplate',
-                        templateOptions: {items: this._filterItems},
-                        popupOptions: {
-                            width: 500
-                        }
-                    },
                     source: new Memory({
                         data: this._filterItems,
                         keyProperty: 'owner'
