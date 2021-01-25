8989db15f1aa27442935ad45d71d6c8e4f5df41c
pa.averkiev <pa.averkiev@tensor.ru>
2021-01-25 16:05:03 +0300

    https://online.sbis.ru/opendoc.html?guid=4512cc27-52c7-48ae-8daa-74b7e9c9a838

--- Controls/_scroll/StickyHeader.ts
+++ Controls/_scroll/StickyHeader.ts
@@ -517,7 +517,7 @@ export default class StickyHeader extends Control<IStickyHeaderOptions> {
 
         fixedPosition = this._model ? this._model.fixedPosition : undefined;
         // Включаю оптимизацию для всех заголовков на ios, в 5100 проблем выявлено не было
-        const isIosOptimizedMode = false; //this._isMobileIOS;
+        const isIosOptimizedMode = this._isMobileIOS;
 
         if (opts.position.indexOf(POSITION.top) !== -1 && this._stickyHeadersHeight.top !== null) {
             top = this._stickyHeadersHeight.top;
