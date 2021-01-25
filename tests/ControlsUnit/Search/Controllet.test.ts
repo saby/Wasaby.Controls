919a0bf541db2835b443a29cbcda21a518cfedd2
ea.melnikova <ea.melnikova@tensor.ru>
2021-01-25 12:23:50 +0300

    https://online.sbis.ru/opendoc.html?guid=28996820-7bb9-4e6a-b27e-553755eb8a69

--- tests/ControlsUnit/Search/Controllet.test.ts
+++ tests/ControlsUnit/Search/Controllet.test.ts
@@ -1,6 +1,7 @@
 import {Controller} from 'Controls/search';
 import {Memory} from 'Types/source';
 import {ok} from 'assert';
+import {assert} from 'chai';
 
 describe('Controls/search:Controller', () => {
 
@@ -23,8 +24,79 @@ describe('Controls/search:Controller', () => {
             options.root = 'testRoot2';
             searchController._beforeUpdate(options, {dataOptions: {}});
             ok(searchController._root === 'testRoot2');
-        })
+        });
 
-    })
+        it('searchValue wasn\'t changed', () => {
+            const source = new Memory();
+            const options = {
+                source,
+                keyProperty: 'id',
+                root: 'testRoot',
+                searchValue: 'testValue',
+                sourceController: 'sourceController'
+            };
+            let searchControllerUpdated = false;
+            const searchController = new Controller(options);
 
+            searchController._options = options;
+            searchController._searchController = {
+                update: () => {
+                    searchControllerUpdated = true;
+                }
+            };
+            searchController._searchValue = 'testValue';
+            searchController._beforeUpdate(options, {dataOptions: {}});
+            assert.isFalse(searchControllerUpdated);
+        });
+
+        it('searchValue was changed', () => {
+            const source = new Memory();
+            const options = {
+                source,
+                keyProperty: 'id',
+                root: 'testRoot',
+                searchValue: 'testValue',
+                sourceController: 'sourceController'
+            };
+            let searchControllerUpdated = false;
+
+            const searchController = new Controller(options);
+            searchController._beforeMount(options, {dataOptions: {}});
+
+            searchController._searchController = {
+                update: () => {
+                    searchControllerUpdated = true;
+                }
+            };
+            options.searchValue = 'newValue';
+
+            searchController._beforeUpdate(options, {dataOptions: {}});
+            assert.isTrue(searchControllerUpdated);
+        });
+
+        it('_searchValue not equal newOptions.searchValue', () => {
+            const source = new Memory();
+            const options = {
+                source,
+                keyProperty: 'id',
+                root: 'testRoot',
+                searchValue: 'testValue',
+                sourceController: 'sourceController'
+            };
+            let searchControllerUpdated = false;
+
+            const searchController = new Controller(options);
+            searchController._beforeMount(options, {dataOptions: {}});
+
+            searchController._searchController = {
+                update: () => {
+                    searchControllerUpdated = true;
+                }
+            };
+            searchController._searchValue = 'newValue';
+
+            searchController._beforeUpdate(options, {dataOptions: {}});
+            assert.isTrue(searchControllerUpdated);
+        });
+    });
 });
