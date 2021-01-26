e7aad243d266265525decc8f108bb9c0274eef01
nu.krukov <zirionneft@gmail.com>
2021-01-26 14:01:44 +0700

    https://online.sbis.ru/opendoc.html?guid=8d3a6a76-267a-4a09-abb2-4c4282b2d098

--- tests/ControlsUnit/browser/Browser.test.ts
+++ tests/ControlsUnit/browser/Browser.test.ts
@@ -422,7 +422,6 @@ describe('Controls/browser:Browser', () => {
            });
 
            assert.isTrue(notifyStub.withArgs('filterChanged', [{payload: 'something'}]).called);
-           assert.equal(browser._searchValue, '');
 
            notifyStub.restore();
        });
@@ -497,6 +496,22 @@ describe('Controls/browser:Browser', () => {
 
             browser._dataLoadCallback(new RecordSet());
             assert.isUndefined(browser._viewMode);
+        });
+
+        it('searchValue changed', async () => {
+            let options = getBrowserOptions();
+            options.searchValue = 'test';
+            const browser = getBrowser(options);
+
+            await browser._beforeMount(options);
+            browser.saveOptions(options);
+
+            browser._searchController = await browser._getSearchController(options);
+            browser._searchController._searchValue = 'newVal';
+            browser._searchValue = 'test';
+
+            browser._dataLoadCallback(new RecordSet());
+            assert.equal(browser._searchValue, 'newVal');
         });
     });
 
