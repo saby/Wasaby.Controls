44c0174af07118bb51e85cd580f543f644356f42
ps.kolesova <ps.kolesova@tensor.ru>
2021-01-27 17:00:44 +0300

    https://online.sbis.ru/opendoc.html?guid=55b4f314-2d84-4a84-b3c0-c9cbb946af8b

--- Controls/interface/IInputBase.ts
+++ Controls/interface/IInputBase.ts
@@ -1,10 +1,11 @@
 import {IInputPlaceholder, IInputTag} from 'Controls/interface';
-
+// TODO: https://online.sbis.ru/opendoc.html?guid=8bd3ee01-5821-4782-8a62-847980a4eacd
 type IInputBase =
     IInputTag & IInputPlaceholder & {
     readonly _options: {
         textAlign: 'left' | 'right' | 'center';
-        style: 'info' | 'invalid' | 'danger' | 'success' | 'warning' | 'primary'; //Todo нет такой опции больше
+        style: 'info' | 'invalid' | 'danger' | 'success' | 'warning' | 'primary';
+        // Todo: https://online.sbis.ru/opendoc.html?guid=1183a619-2f46-4c78-a2bc-09d41095e744
         autoComplete: 'on' | 'off' | 'username' | 'current-password';
         value: string | null;
     }
--- Controls/interface/IInputDateTime.ts
+++ Controls/interface/IInputDateTime.ts
@@ -1,2 +1,3 @@
 import {IInputDateTime} from 'Controls/input';
+// TODO: https://online.sbis.ru/opendoc.html?guid=e298278f-d12c-47ea-a5db-cf1c3691f0b8
 export default IInputDateTime;
