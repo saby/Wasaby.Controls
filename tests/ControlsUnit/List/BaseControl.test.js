995ca750e0f266d19e307f4b350e36e76f34c520
Gersasha <gersashaa@gmail.com>
2021-01-31 16:05:04 +0300

    Чиню тесты https://online.sbis.ru/opendoc.html?guid=b8c87cc4-2c4f-4fff-8495-7b1a4c5ff7aa

--- tests/ControlsUnit/List/BaseControl.test.js
+++ tests/ControlsUnit/List/BaseControl.test.js
@@ -4532,6 +4532,7 @@ define([
                          }
                      ]
                  }).then(() => {
+                     lists.BaseControl._private.createSelectionController(instance, instance._options);
                      lists.BaseControl._private.updateItemActions(instance, instance._options);
                      const item = instance._listViewModel.at(0);
                      instance._onItemSwipe({}, item, swipeEvent);
