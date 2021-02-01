34981400d9c5f55dfae9eeb68e0df1e6eb091ac0
dv.onischuk <dv.onischuk@tensor.ru>
2021-02-01 13:57:06 +0300

    https://online.sbis.ru/opendoc.html?guid=7f8104fd-c571-41eb-be93-ec262cd0d6b1 Добавил демки на wi

--- Controls-demo/Popup/SlidingPanel/PopupTemplate.ts
+++ Controls-demo/Popup/SlidingPanel/PopupTemplate.ts
@@ -3,10 +3,13 @@ import controlTemplate = require('wml!Controls-demo/Popup/SlidingPanel/PopupTemp
 
 class PopupTemplate extends Control<IControlOptions> {
     protected _template: TemplateFunction = controlTemplate;
+    protected _controlButtonVisibility: boolean = true;
+    protected _isLargeContent: boolean = false;
 
     static _theme: string[] = ['Controls/Classes'];
     static _styles: string[] = [
-        'Controls-demo/Controls-demo'
+        'Controls-demo/Controls-demo',
+        'Controls-demo/Popup/SlidingPanel/PopupTemplate/PopupTemplate'
     ];
 }
 export default PopupTemplate;
