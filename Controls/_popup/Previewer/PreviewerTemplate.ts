b926060d89b8fab2559036166bd206d420e1b584
oa.kashin <oa.kashin@tensor.ru>
2021-02-05 10:24:54 +0300

    https://online.sbis.ru/opendoc.html?guid=9a7ab9d3-a1d8-4ed8-bec7-e663affb407b

--- Controls/_popup/Previewer/PreviewerTemplate.ts
+++ Controls/_popup/Previewer/PreviewerTemplate.ts
@@ -3,6 +3,7 @@ import template = require('wml!Controls/_popup/Previewer/PreviewerTemplate');
 import 'Controls/Container/Async';
 import {load} from 'Core/library';
 import {constants} from 'Env/Env';
+import { error as errorLib } from 'Controls/dataSource';
 
 /**
  * @class Controls/_popup/Previewer/PreviewerTemplate
@@ -19,7 +20,7 @@ class PreviewerTemplate extends Control<IPreviewerOptions> {
 
     protected _beforeMount(options: IPreviewerOptions): void|Promise<void> {
         if (constants.isBrowserPlatform && this._needRequireModule(options.template)) {
-            return load(options.template as string);
+            return load(options.template as string).catch((error) => errorLib.process({ error }));
         }
     }
 
