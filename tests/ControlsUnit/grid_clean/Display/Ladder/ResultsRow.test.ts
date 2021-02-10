ad3fdb3e97cadf9dbdea12dcfe16e42c55ff4524
ea.rodionov1 <ea.rodionov1@tensor.ru>
2021-02-10 11:39:54 +0300

    https://online.sbis.ru/opendoc.html?guid=aee22d6f-ab8c-4dd0-9b47-7b737bfd76de

--- tests/ControlsUnit/grid_clean/Display/Ladder/ResultsRow.test.ts
+++ tests/ControlsUnit/grid_clean/Display/Ladder/ResultsRow.test.ts
@@ -1,5 +1,6 @@
 import { assert } from 'chai';
 import { GridCollection, GridResultsRow } from 'Controls/gridNew';
+import * as sinon from 'sinon';
 
 const getMockedOwner = (extendedProps = {}): GridCollection<unknown> => {
     return {
@@ -11,27 +12,38 @@ const getMockedOwner = (extendedProps = {}): GridCollection<unknown> => {
 };
 
 describe('Controls/grid_clean/Display/Ladder/ResultsRow', () => {
+    let sandbox: sinon.SinonSandbox;
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
 
     describe('no grid support', () => {
         it('should not add sticky columns in table layout', () => {
-            const columns = [{ title: 'firstColumn' }, { title: 'secondColumn' }];
+            const columns = [{ title: 'firstColumn' }];
             const resultsRow = new GridResultsRow({
                 owner: getMockedOwner({
                     isFullGridSupport: () => false,
                     getStickyLadder: () => ({
-                        first: {ladderLength: 2},
-                        second: {ladderLength: 1}
+                        first: {ladderLength: 2}
                     }),
                     getColumnsConfig: () => columns,
-                    getStickyLadderProperties: () => ['first', 'second']
+                    getStickyLadderProperties: () => ['first']
                 }),
                 columns
             });
 
+            const fakeFactoryMethod = (options) => {
+                assert.isNotTrue(options.ladderCell);
+            };
+            sandbox.replace(resultsRow, 'getColumnsFactory', () => fakeFactoryMethod);
+
             assert.isArray(resultsRow.getColumns());
-            assert.equal(resultsRow.getColumns().length, 2);
-            assert.deepEqual(resultsRow.getColumns()[0].getColumnConfig(), { title: 'firstColumn' });
-            assert.deepEqual(resultsRow.getColumns()[1].getColumnConfig(), { title: 'secondColumn' });
+            assert.equal(resultsRow.getColumns().length, 1);
         });
     });
 });
