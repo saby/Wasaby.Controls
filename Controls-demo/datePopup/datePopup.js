f3e8467fabf8f9723c2567d3f6ddd64799a7a7b9
ma.mochalov <ma.mochalov@tensor.ru>
2021-02-11 11:06:07 +0300

    https://online.sbis.ru/opendoc.html?guid=aaf4094d-4f9d-4e59-b7da-9ad34fa9cfed

--- Controls-demo/datePopup/datePopup.js
+++ Controls-demo/datePopup/datePopup.js
@@ -1,17 +1,15 @@
-define('Controls-demo/datePopup/datePopup', [
-   'UI/Base',
-   'wml!Controls-demo/datePopup/datePopup'
-], function(
-   Base,
-   template
-) {
-   'use strict';
-
-   var ModuleClass = Base.Control.extend({
-      _template: template,
-      _startValue: new Date(2019, 4, 3),
-      _endValue: new Date(2019, 5, 0),
-      _date: new Date(2021, 0, 27)
-   });
-   return ModuleClass;
+define("Controls-demo/datePopup/datePopup", ["require", "exports", "tslib", "UI/Base", "wml!Controls-demo/datePopup/datePopup"], function (require, exports, tslib_1, Base_1, template) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var default_1 = /** @class */ (function (_super) {
+        tslib_1.__extends(default_1, _super);
+        function default_1() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this._template = template;
+            return _this;
+        }
+        default_1._styles = ['Controls-demo/Controls-demo'];
+        return default_1;
+    }(Base_1.Control));
+    exports.default = default_1;
 });
