f5d6a4463b5f9ed2325f6bf9b296d88a01202d3d
am.gerasimov <am.gerasimov@tensor.ru>
2021-02-11 16:18:28 +0300

    https://online.sbis.ru/opendoc.html?guid=a9e362cb-06f8-4176-b022-efebfbba196b

--- tests/ControlsUnit/dataSource/Controller.test.ts
+++ tests/ControlsUnit/dataSource/Controller.test.ts
@@ -169,6 +169,18 @@ describe('Controls/dataSource:SourceController', () => {
             ok((loadedItems as RecordSet).getCount() === 5);
         });
 
+        it('load without direction',  async () => {
+            const controller = getControllerWithHierarchy({
+                navigation: getPagingNavigation()
+            });
+            await controller.reload();
+            await controller.load(undefined, 3);
+            ok(controller.hasLoaded(3));
+
+            await controller.load();
+            ok(!controller.hasLoaded(3));
+        });
+
         it('load call while loading',  async () => {
             const controller = getController();
             let loadPromiseWasCanceled = false;
