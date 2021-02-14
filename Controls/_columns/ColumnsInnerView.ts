7d65a26f95caf0ff04c220abb599a48e774c0f2a
va.kolesov <va.kolesov@tensor.ru>
2021-02-15 01:22:15 +0300

    https://online.sbis.ru/opendoc.html?guid=58402463-d4b5-4cbf-9e6a-db32f5f48916

--- Controls/_columns/ColumnsInnerView.ts
+++
@@ -1,7 +0,0 @@
-import {TemplateFunction, Control} from 'UI/Base';
-
-import * as template from 'wml!Controls/_columns/ColumnsInnerView';
-
-export default class ColumnsInnerView extends Control {
-    _template: TemplateFunction = template;
-}
