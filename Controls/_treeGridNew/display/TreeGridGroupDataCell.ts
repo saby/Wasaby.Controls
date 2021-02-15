825f36d9d9da4722cd4fa9396eef8471293b7f50
pa.averkiev <pa.averkiev@tensor.ru>
2021-02-15 15:41:52 +0300

    https://online.sbis.ru/opendoc.html?guid=e2bd5e2d-da9f-4288-a8fa-9171e855241d

--- Controls/_treeGridNew/display/TreeGridGroupDataCell.ts
+++ Controls/_treeGridNew/display/TreeGridGroupDataCell.ts
@@ -1,14 +1,13 @@
 import {Model} from 'Types/entity';
 import {mixin} from 'Types/util';
-import {GridDataCell, GridGroupCellMixin} from 'Controls/gridNew';
+import {GridDataCell, GridGroupCellMixin, IGridRowOptions} from 'Controls/gridNew';
 import TreeGridDataCell from 'Controls/_treeGridNew/display/TreeGridDataCell';
-import {IOptions} from "Controls/_gridNew/display/Row";
 
 export default class TreeGridGroupDataCell<T extends Model>
     extends mixin<TreeGridDataCell<T>, GridGroupCellMixin<any>>(GridDataCell, GridGroupCellMixin) {
     readonly '[Controls/treeGrid:TreeGridGroupDataCell]': boolean;
 
-    constructor(options?: IOptions<T>) {
+    constructor(options?: IGridRowOptions<T>) {
         super(options);
         GridGroupCellMixin.call(this, options);
     }
