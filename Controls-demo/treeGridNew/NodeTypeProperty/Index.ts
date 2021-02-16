13f608d61edbfbeb52c80688310ac258256ac3eb
pa.averkiev <pa.averkiev@tensor.ru>
2021-02-16 12:20:55 +0300

    https://online.sbis.ru/opendoc.html?guid=e2bd5e2d-da9f-4288-a8fa-9171e855241d

--- Controls-demo/treeGridNew/NodeTypeProperty/Index.ts
+++ Controls-demo/treeGridNew/NodeTypeProperty/Index.ts
@@ -1,5 +1,5 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import * as Template from 'wml!Controls-demo/treeGridNew/GroupView/GroupView';
+import * as Template from 'wml!Controls-demo/treeGridNew/NodeTypeProperty/NodeTypeProperty';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
--- Controls-demo/treeGridNew/NodeTypeProperty/TreeGridView/Index.ts
+++ Controls-demo/treeGridNew/NodeTypeProperty/TreeGridView/Index.ts
@@ -1,5 +1,5 @@
 import {Control, TemplateFunction} from 'UI/Base';
-import * as Template from 'wml!Controls-demo/treeGridNew/GroupView/TreeGridView/TreeGridView';
+import * as Template from 'wml!Controls-demo/treeGridNew/NodeTypeProperty/TreeGridView/TreeGridView';
 import {Memory} from 'Types/source';
 import {Gadgets} from '../../DemoHelpers/DataCatalog';
 
