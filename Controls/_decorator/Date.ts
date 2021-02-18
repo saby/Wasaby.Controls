c9dd530f56c4cb9272a7a5cae9f929da2dca9e50
mk.siryakov <mk.siryakov@tensor.ru>
2021-02-18 21:42:52 +0300

    https://online.sbis.ru/opendoc.html?guid=00d6e02d-bb48-44a1-af6d-df4277531422

---
+++ Controls/_decorator/Date.ts
@@ -0,0 +1,98 @@
+import {Control, IControlOptions, TemplateFunction } from 'UI/Base';
+import {
+    IFontColorStyle,
+    IFontColorStyleOptions,
+    IFontSize,
+    IFontSizeOptions,
+    IFontWeight,
+    IFontWeightOptions
+} from "Controls/interface";
+import {date} from 'Types/formatter';
+
+import * as template from 'wml!Controls/_decorator/Date/Date';
+import 'css!Controls/decorator';
+
+/**
+ * Интерфейс для опций контрола {@link Controls/decorator:Date}.
+ * @interface Controls/_decorator/IDate
+ * @public
+ * @author Сиряков М.К.
+ */
+export interface IDateOptions extends IControlOptions, IFontColorStyleOptions, IFontWeightOptions, IFontSizeOptions {
+    /**
+     * @name Controls/_decorator/IDate#value
+     * @cfg {Date} Декорируемая дата.
+     * @demo Controls-demo/Decorator/Date/Index
+     */
+    value: Date;
+    /**
+     * @name Controls/_decorator/IDate#format
+     * @cfg {string} Формат для преобразования даты.
+     * @demo Controls-demo/Decorator/Date/Index
+     */
+    format: string;
+    /**
+     * @name Controls/_decorator/IDate#timeZoneOffset
+     * @cfg {number} Опция, определяющая смещение часового пояса, в котором необходимо вывести значение.
+     * @demo Controls-demo/Decorator/Date/Index
+     */
+    timeZoneOffset?: number;
+}
+
+/**
+ * Графический контрол, декорирующий дату таким образом, что она приводится к заданному формату.
+ *
+ * @class Controls/_decorator/Date
+ * @extends UI/Base:Control
+ * @mixes Controls/_decorator/IDate
+ * @public
+ * @demo Controls-demo/Decorator/Date/Index
+ *
+ * @author Сиряков М.К.
+ */
+class DateDecorator extends Control<IDateOptions> implements IFontColorStyle, IFontSize, IFontWeight {
+    private _value: object;
+    private _format: string;
+    private _timeZoneOffset: number;
+    private _formattedDate: string;
+    private _fontColorStyle: string;
+
+    readonly '[Controls/_interface/IFontColorStyle]': boolean = true;
+    readonly '[Controls/_interface/IFontSize]': boolean = true;
+    readonly '[Controls/_interface/IFontWeight]': boolean = true;
+
+    protected _options: IDateOptions;
+    protected _template: TemplateFunction = template;
+
+    protected _beforeMount(options: IDateOptions): void {
+        this._setFontState(options);
+        this._value = options.value;
+        this._format = options.format;
+        this._timeZoneOffset = options.timeZoneOffset;
+        this._formattedDate = this._formatDate(options);
+    }
+
+    protected _beforeUpdate(newOptions: IDateOptions): void {
+        this._setFontState(newOptions);
+        if (
+            this._value !== newOptions.value
+            || this._format !== newOptions.format
+            || this._timeZoneOffset !== newOptions.timeZoneOffset
+        ) {
+            this._value = newOptions.value;
+            this._format = newOptions.format;
+            this._timeZoneOffset = newOptions.timeZoneOffset;
+            this._formattedDate = this._formatDate(newOptions);
+        }
+    }
+
+    private _setFontState(options: IDateOptions): void {
+        this._fontColorStyle = options.readOnly ? 'readonly' : options.fontColorStyle;
+    }
+
+    private _formatDate(options: IDateOptions): string {
+        return date(options.value, options.format, options.timeZoneOffset);
+    }
+}
+
+export default DateDecorator;
