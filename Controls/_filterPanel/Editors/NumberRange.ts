9bf7f1d7566eb7869e3304d794c0a3fde8b27e6b
ea.melnikova <ea.melnikova@tensor.ru>
2021-02-18 12:29:20 +0300

    https://online.sbis.ru/opendoc.html?guid=89018c55-89fa-424e-94cd-101e67137fbd

--- Controls/_filterPanel/Editors/NumberRange.ts
+++ Controls/_filterPanel/Editors/NumberRange.ts
@@ -85,8 +85,8 @@ class NumberRangeEditor extends Control<INumberRangeOptions> implements INumberR
     }
 
     protected _handleInputCompleted(event: SyntheticEvent, value: number): void {
+        this._notifyExtendedValue([this._minValue, this._maxValue]);
         if (this._minValue !== null && this._maxValue) {
-            this._notifyExtendedValue([this._minValue, this._maxValue]);
             this._children.numberRangeValidate.validate();
         }
     }
@@ -99,7 +99,7 @@ class NumberRangeEditor extends Control<INumberRangeOptions> implements INumberR
     private _notifyExtendedValue(value: number[]): void {
         const extendedValue = {
             value,
-            textValue: !this._isValueEmpty(value) && this._getTextValue(value)
+            textValue: !this._isValueEmpty(value) ? this._getTextValue(value) : ''
         };
         this._notify('propertyValueChanged', [extendedValue], {bubbling: true});
     }
