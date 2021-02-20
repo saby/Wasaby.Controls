a67a7dcbfd73d0bc6abc92900ae5bb29e273bfc5
pa.averkiev <pa.averkiev@tensor.ru>
2021-02-20 16:57:50 +0300

    https://online.sbis.ru/opendoc.html?guid=e2bd5e2d-da9f-4288-a8fa-9171e855241d

--- Controls/_tree/TreeControl.ts
+++ Controls/_tree/TreeControl.ts
@@ -1009,11 +1009,11 @@ var TreeControl = Control.extend(/** @lends Controls/_tree/TreeControl.prototype
     _onItemClick: function(e, item, originalEvent, columnIndex: number, returnExpandResult: boolean /* for tests */) {
         e.stopPropagation();
         const eventResult = this._notify('itemClick', [item, originalEvent, columnIndex], { bubbling: true });
-        const viewModel = this._children.baseControl.getViewModel();
-        const display = viewModel.getDisplay ? viewModel.getDisplay() : viewModel;
-        const dispItem = display.getItemBySourceItem(item);
-        const expandByClick = this._options.expandByItemClick || dispItem['[Controls/treeGrid:TreeGridGroupDataRow]'];
-        if (eventResult !== false && expandByClick && item.get(this._options.nodeProperty) !== null) {
+        if ((eventResult !== false && this._options.expandByItemClick && item.get(this._options.nodeProperty) !== null) ||
+            item.get(this._options.nodeTypeProperty) === 'group'
+        ) {
+            const display = this._options.useNewModel ? this._children.baseControl.getViewModel() : this._children.baseControl.getViewModel().getDisplay();
+            const dispItem = display.getItemBySourceItem(item);
 
             // Если в проекции нет такого элемента, по которому произошел клик, то это хлебная крошка, а не запись.
             // После исправления ошибки событие itemClick не будет стрелять при клике на крошку.
