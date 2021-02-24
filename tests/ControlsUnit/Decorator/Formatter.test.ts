54fe93c919b4f2525f9f39decf2b3c7d65ad9e5d
mk.siryakov <mk.siryakov@tensor.ru>
2021-02-24 12:57:32 +0300

    https://online.sbis.ru/opendoc.html?guid=01598edc-0b9d-4176-9ea3-fdd601f043ee

--- tests/ControlsUnit/Decorator/Formatter.test.ts
+++ tests/ControlsUnit/Decorator/Formatter.test.ts
@@ -4,7 +4,7 @@ import {abbreviateNumber} from 'Controls/_decorator/resources/Formatter';
 describe('Controls/decorator:__Formatter', () => {
     describe('.abbreviateNumber()', () => {
         const cases = [
-            {testName: 'Значение null', value: null, abbreviationType: 'long', expected: '1,2 млн'},
+            {testName: 'Значение null', value: null, abbreviationType: 'long', expected: '0'},
             {testName: 'Длинная аббревиатура', value: 1240450, abbreviationType: 'long', expected: '1,2 млн'},
             {testName: 'Короткая аббревиатура', value: 1240450, abbreviationType: 'short', expected: '1,2М'},
             {testName: 'Длинная аббревиатура с отрицательным числом', value: -1240450, abbreviationType: 'long', expected: '-1,2 млн'},
