f22bbb2e097c0300ec95d2d06f9b2473be8d2da4
pa.averkiev <pa.averkiev@tensor.ru>
2021-02-26 10:12:31 +0300

    https://online.sbis.ru/opendoc.html?guid=5afe07b6-d176-44a5-a229-08119c56db43 поправил обратно импорт в Controls-demo

--- Controls-demo/grid/Sorting/SortingSelector/IconStyle/Index.ts
+++ Controls-demo/grid/Sorting/SortingSelector/IconStyle/Index.ts
@@ -2,7 +2,7 @@ import {Control, TemplateFunction} from 'UI/Base';
 import * as Template from 'wml!Controls-demo/grid/Sorting/SortingSelector/IconStyle/Template';
 import {Memory} from 'Types/source';
 import {getCountriesStats} from '../../../DemoHelpers/DataCatalog';
-import { IColumn } from 'Controls/interface';
+import { IColumn } from 'Controls/grid';
 
 export default class extends Control {
     protected _template: TemplateFunction = Template;
