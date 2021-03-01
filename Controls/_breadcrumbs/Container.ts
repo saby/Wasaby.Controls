fa8ad507dfa938b51417e779d516efc634b822dd
ee.zolotova <ee.zolotova@tensor.ru>
2021-03-01 14:56:42 +0300

    https://online.sbis.ru/opendoc.html?guid=9653b09d-5e7e-4d60-a448-70747b83a998

--- Controls/_breadcrumbs/Container.ts
+++ Controls/_breadcrumbs/Container.ts
@@ -2,7 +2,7 @@ import {Control, TemplateFunction, IControlOptions} from 'UI/Base';
 import * as template from 'wml!Controls/_breadcrumbs/Container';
 import {ContextOptions as DataOptions} from 'Controls/context';
 import calculatePath, {Path} from 'Controls/_dataSource/calculatePath';
-import {ISourceControllerState} from 'Controls/dataSource';
+import {ISourceControllerState, NewSourceController as SourceController} from 'Controls/dataSource';
 import {SyntheticEvent} from 'Vdom/Vdom';
 import {Model} from 'Types/entity';
 
@@ -13,9 +13,11 @@ interface IDataContext {
 export default class BreadCrumbsContainer extends Control<IControlOptions> {
     protected _template: TemplateFunction = template;
     protected _dataOptions: ISourceControllerState = null;
+    protected _sourceController: SourceController;
     protected _items: Path;
 
     protected _beforeMount(options: IControlOptions, context: IDataContext): void {
+        this._collectionChange = this._collectionChange.bind(this);
         this._dataOptions = context.dataOptions;
         this._setPathItems(options);
     }
@@ -23,6 +25,14 @@ export default class BreadCrumbsContainer extends Control<IControlOptions> {
     protected _beforeUpdate(options: IControlOptions, context: IDataContext): void {
         this._dataOptions = context.dataOptions;
         this._setPathItems(options);
+    }
+
+
+    protected _beforeUnmount(): void {
+        if (this._sourceController) {
+            this._sourceController.unsubscribe('onCollectionChange', this._collectionChange);
+            this._sourceController.destroy();
+        }
     }
 
     protected _itemClickHandler(e: SyntheticEvent, item: Model): void {
@@ -34,14 +44,35 @@ export default class BreadCrumbsContainer extends Control<IControlOptions> {
     }
 
     private _setPathItems(options): void {
-        let sourceController;
-        if (this._dataOptions.sourceController) {
-            sourceController = this._dataOptions.sourceController;
-        } else if (options.sourceController) {
-            sourceController = options.sourceController;
-        }
+        let sourceController = this._getSourceController(options);
         if (sourceController) {
-            this._items = calculatePath(sourceController.getItems()).path;
+            this._items = this._getPathItems(sourceController.getItems());
+        }
+    }
+
+    private _getSourceController(options): SourceController {
+        if (this._dataOptions.sourceController) {
+            this._sourceController = this._dataOptions.sourceController;
+        } else if (options.sourceController && this._sourceController !== options.sourceController) {
+            this._sourceController = options.sourceController;
+            this._sourceController.getItems().subscribe('onCollectionChange', this._collectionChange);
+        }
+        return this._sourceController;
+    }
+
+    private _getPathItems(items): Path {
+        return calculatePath(items).path;
+    }
+
+    private _collectionChange(event: Event,
+                              action: string,
+                              newItems,
+                              newItemsIndex: number,
+                              oldItems,
+                              oldItemsIndex: number,
+                              reason: string): void {
+        if (reason === 'assign') {
+            this._items = this._getPathItems(this._sourceController.getItems());
         }
     }
 
