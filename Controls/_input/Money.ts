5c50e0e9e9977ad4769901a822a182141ae03d41
Andrew <kr.andrew94@mail.ru>
2021-03-10 08:25:42 +0300

    https://online.sbis.ru/doc/9f19723c-97b4-44f6-8c35-89288e37f488

--- Controls/_input/Money.ts
+++ Controls/_input/Money.ts
@@ -23,7 +23,6 @@ interface IMoneyOptions extends IBaseInputOptions, INumberLengthOptions, IOnlyPo
  *
  * @mixes Controls/input:IOnlyPositive
  * @mixes Controls/input:INumberLength
- * @mixes Controls/_interface/IInputPlaceholder
  *
  * @public
  * @demo Controls-demo/Input/Money/Base/Index
