77f6d46762c5e52073f9a921a3fe25ff2c6fc7ea
ka.panihin <ka.panihin@tensor.ru>
2021-03-12 08:56:36 +0300

    https://online.sbis.ru/opendoc.html?guid=5e0e040b-8dc0-4f54-ba8b-5705c7fa327f

--- Controls/gridNew.ts
+++ Controls/gridNew.ts
@@ -36,6 +36,7 @@ import * as StringTypeRender from 'wml!Controls/_gridNew/Render/types/string';
 import * as StringSearchTypeRender from 'wml!Controls/_gridNew/Render/types/StringSearch';
 
 import SortingButton from 'Controls/_gridNew/SortingButton';
+import SortingSelector from 'Controls/_gridNew/SortingSelector';
 import {register} from 'Types/di';
 
 export {
@@ -57,7 +58,8 @@ export {
     StringTypeRender,
     StringSearchTypeRender,
     TableTemplate,
-    SortingButton
+    SortingButton,
+    SortingSelector
 };
 
 import {
