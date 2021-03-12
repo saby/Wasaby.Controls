09248e01a200d36c0334ba90a1ba3e999c45cefd
ka.panihin <ka.panihin@tensor.ru>
2021-03-12 12:59:46 +0300

    https://online.sbis.ru/opendoc.html?guid=18ad64e0-c368-4a3a-a298-665ed68716b9

--- Controls/_treeGrid/SearchView/SearchViewModel.ts
+++ Controls/_treeGrid/SearchView/SearchViewModel.ts
@@ -13,14 +13,6 @@ var
             filter = this.getDisplayFilter(this.prepareDisplayFilterData(), cfg);
          return SearchItemsUtil.getDefaultDisplaySearch(items, cfg, filter);
       },
-      getDisplayFilter: function (data, cfg) {
-         var
-            filter = [];
-         if (cfg.itemsFilterMethod) {
-            filter.push(cfg.itemsFilterMethod);
-         }
-         return filter;
-      },
       setHoveredItem(item) {
          let actualItem = item;
          if (item && isBreadCrumbsItem(item)) {
