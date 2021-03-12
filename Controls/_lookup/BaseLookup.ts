90d905dcb1ff32a30546b4bfe1748e7348d3c277
ea.melnikova <ea.melnikova@tensor.ru>
2021-03-12 12:42:15 +0300

    https://online.sbis.ru/opendoc.html?guid=dcccdfc1-36b2-462e-8e44-a034c6285f36

--- Controls/_lookup/BaseLookup.ts
+++ Controls/_lookup/BaseLookup.ts
@@ -8,7 +8,7 @@ import {TKey} from 'Controls/interface';
 // @ts-ignore
 import * as isEmpty from 'Core/helpers/Object/isEmpty';
 import * as ArrayUtil from 'Controls/Utils/ArraySimpleValuesUtil';
-import {Base} from 'Controls/input';
+import {Render} from 'Controls/input';
 import 'css!Controls/lookup';
 import 'css!Controls/CommonClasses';
 
@@ -148,7 +148,7 @@ export default abstract class
 
     static getDefaultOptions(): object {
         return {
-            ...Base.getDefaultOptions(),
+            ...Render.getDefaultOptions(),
             multiSelect: false
         };
     }
